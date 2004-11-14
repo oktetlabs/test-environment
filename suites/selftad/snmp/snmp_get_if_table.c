4a2ec9d80052f0fe5bad87203909b17f4859df6e
Konstantin Abramenko <Konstantin.Abramenko@oktetlabs.ru>
2004-11-14 21:17:17 +0000

    SNMP get-table test written
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@6424 9d14d920-75d5-0310-8bd7-a52572981aee

--- suites/selftad/snmp/snmp_get_if_table.c
+++ suites/selftad/snmp/snmp_get_if_table.c
@@ -34,22 +34,110 @@
 #include <sys/time.h>
 #include <sys/types.h>
 #include <unistd.h>
+
+#include "te_defs.h"
 #include "te_stdint.h"
 #include "te_errno.h"
+#include "tapi_test.h"
 #include "rcf_api.h"
+#include "logger_api.h"
 #include "tapi_snmp.h"
+
 
 #include "tapi_snmp_iftable.h"
 
-int 
-test_walk_callback(const tapi_snmp_varbind_t *varbind, void *userdata)
+
+
+int
+main(int argc, char *argv[])
 {
-    printf ("walk CALLBACK: ");
-    print_objid(varbind->name.id, varbind->name.length);
-    return 0;
+    tapi_snmp_if_table_row_t * iftable_result;
+    int snmp_csap = 0;
+    int sid;
+    int snmp_version;
+    const char *ta;
+    const char *mib_name;
+    const char *snmp_agt;
+
+    TEST_START;
+    TEST_GET_STRING_PARAM(ta);
+    TEST_GET_STRING_PARAM(mib_name);
+    TEST_GET_STRING_PARAM(snmp_agt);
+    TEST_GET_INT_PARAM(snmp_version);
+    
+    /* Session */
+    {
+        if ((rc = rcf_ta_create_session(ta, &sid)) != 0)
+        {
+            TEST_FAIL("Session create error %X", rc);
+        }
+        VERB("Session created %d", sid);
+    }
+
+    do {
+        tapi_snmp_oid_t ifTable_oid;
+        int num;
+
+        rc = tapi_snmp_csap_create(ta, sid, snmp_agt, "public", 
+                                   snmp_version, &snmp_csap); 
+        if (rc) 
+            TEST_FAIL("Csap create error %X", rc);
+        VERB("New csap %d", snmp_csap); 
+
+        
+        if ((rc = tapi_snmp_load_mib_with_path("/usr/share/snmp/mibs", 
+                                                mib_name)) != 0)
+        {
+            TEST_FAIL("snmp_load_mib(%s) failed, rc %x\n", mib_name, rc);
+            return rc;
+        }
+
+        if ((rc = tapi_snmp_make_oid("ifTable", &ifTable_oid)) != 0)
+        {
+            TEST_FAIL("tapi_snmp_make_oid() failed, rc %x\n", rc);
+            return rc;
+        }
+        if (rc)
+            TEST_FAIL("SNMP GET NEXT failed with rc %X", rc);
+
+        iftable_result = NULL;
+
+        rc = tapi_snmp_get_table(ta, sid, snmp_csap, &ifTable_oid, &num, 
+                                 &iftable_result);
+        if (rc)
+            TEST_FAIL("SNMP GET NEXT failed with rc %X", rc);
+
+        INFO("snmp get table got num: %d, ptr %x", num, iftable_result); 
+
+        if (num > 0)
+        {
+            int i;
+            for (i = 0; i < num; i ++)
+            {
+                INFO("row %d: ", i );
+                if (iftable_result[i].index_suffix)
+                    INFO(" index_suffix %s", 
+                            print_oid(iftable_result[i].index_suffix));
+        #if 1
+                if (iftable_result[i].ifDescr)
+                    INFO(" ifDescr \"%s\"", iftable_result[i].ifDescr->data);
+        #endif
+            }
+        }
+
+        rcf_ta_csap_destroy(ta, sid, snmp_csap); 
+
+    } while(0);
+
+    TEST_SUCCESS;
+
+cleanup:
+    TEST_END;
 }
 
+#define  OLD 0
 
+#if OLD
 int
 main()
 {
@@ -159,3 +247,4 @@ main()
     return 0;
 }
 
+#endif
