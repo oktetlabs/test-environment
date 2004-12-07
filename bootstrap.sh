f21d3b15838e79ed7b5a55133404456c676b558f
Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
2004-12-07 08:00:12 +0000

    Added in repository
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@8162 9d14d920-75d5-0310-8bd7-a52572981aee

---
+++ bootstrap.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+#
+# Clean up TE sources to build from scratch:
+# 1. Delete all 'configure' scripts marked as ignored in Subversion.
+#    It is sufficient to force dispatcher.sh to call aclocal, autoconf,
+#    automake in all such directories.
+#
+# TE_BASE is required to be set. All actions are performed in this
+# directory.
+#
+#
+# Copyright (C) 2004 Test Environment authors (see file AUTHORS
+# in the root directory of the distribution).
+#
+# Test Environment is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as 
+# published by the Free Software Foundation; either version 2 of 
+# the License, or (at your option) any later version.
+#
+# Test Environment is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+# MA  02111-1307  USA
+#
+#
+# @author Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
+#
+# $Id$
+#
+
+if test -z "${TE_BASE}" ; then
+    echo TE_BASE environment variable is required
+    exit 1
+fi
+pushd ${TE_BASE}
+for i in `find . -name configure` ; do
+    i_status=`svn status $i` || echo "Failed to set SVN status of $i"
+    # Remove only 'configure' which are ignored by Subversion
+    if test "x$i_status" = "xI${i_status/#I/}" ; then
+        rm $i
+    fi
+done
+popd
