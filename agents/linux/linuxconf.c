0a35d9abf0f6d582b72e47d522899a80f39def4c
Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
2004-12-19 07:45:24 +0000

    Temporary fix
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@8646 9d14d920-75d5-0310-8bd7-a52572981aee

--- agents/linux/linuxconf.c
+++ agents/linux/linuxconf.c
@@ -2133,7 +2133,13 @@ env_del(unsigned int gid, const char *oid, const char *name)
 
     if (getenv(name) != NULL)
     {
+        /* FIXME */
+#if 0
+        /* There is some issues with unsetenv() in NetBSD 2.0 build */
         unsetenv(name);
+#else
+        setenv(name, NULL, TRUE);
+#endif
         return 0;
     }
     else
