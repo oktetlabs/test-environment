72a1b2a32cc3993b748ad938614df36507047265
Konstantin Abramenko <Konstantin.Abramenko@oktetlabs.ru>
2005-01-04 11:46:16 +0000

    detail configuration removed, all NDNs allways are included
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@9133 9d14d920-75d5-0310-8bd7-a52572981aee

--- lib/ndn/ndn_context.c
+++ lib/ndn/ndn_context.c
@@ -29,86 +29,51 @@
 #include "asn_impl.h"
 #include "ndn.h"
 
-#ifdef WITH_FILE
-#include "ndn_file.h"
-#endif
-#ifdef WITH_SNMP
+#ifdef HAS_SNMP
 #include "ndn_snmp.h"
 #endif
-#ifdef WITH_ETH
+
+#include "ndn_file.h"
 #include "ndn_eth.h"
-#endif 
-
-#ifdef WITH_IPSTACK
 #include "ndn_ipstack.h"
-#endif 
-
-#ifdef WITH_CLI
 #include "ndn_cli.h"
-#endif
-
-#ifdef WITH_BRIDGE
 #include "ndn_bridge.h"
-#endif
 
 /* Add here declaraions of protocol-specific CSAP init params */ 
-#ifdef WITH_FILE
 extern asn_type ndn_file_csap_s;
-#endif
-#ifdef WITH_SNMP
-extern asn_type ndn_snmp_csap_s;
-#endif
-#ifdef WITH_ETH
-extern asn_type ndn_eth_csap_s;
-#endif 
 
-#ifdef WITH_IPSTACK
+extern asn_type ndn_snmp_csap_s;
+
+extern asn_type ndn_eth_csap_s;
+
 extern asn_type ndn_ip4_csap_s;
 extern asn_type ndn_icmp4_csap_s;
 extern asn_type ndn_udp_csap_s;
-#endif 
 
-#ifdef WITH_CLI
 extern asn_type ndn_cli_csap_s;
-#endif
-#ifdef WITH_DHCP
-extern asn_type ndn_dhcpv4_csap_s;
-#endif
 
-#ifdef WITH_BRIDGE
+extern asn_type ndn_dhcpv4_csap_s;
+
 extern asn_type ndn_bridge_csap_s;
-#endif 
 
 static asn_named_entry_t _ndn_generic_csap_level_ne_array [] = 
 {
 /* Add here reference to protocol-specific CSAP init params */ 
-#ifdef WITH_FILE
     { "file", &ndn_file_csap_s },
-#endif
-#ifdef WITH_SNMP
+
+#ifdef HAS_SNMP
     { "snmp", &ndn_snmp_csap_s },
 #endif
-#ifdef WITH_ETH
     { "eth", &ndn_eth_csap_s },
-#endif 
 
-#ifdef WITH_IPSTACK
     { "ip4", &ndn_ip4_csap_s },
     { "icmp4", &ndn_icmp4_csap_s },
     { "udp", &ndn_udp_csap_s },
-#endif 
 
-
-#ifdef WITH_CLI
     { "cli", &ndn_cli_csap_s },
-#endif
-#ifdef WITH_DHCP
     { "dhcp", &ndn_dhcpv4_csap_s },
-#endif
 
-#ifdef WITH_BRIDGE
     { "bridge", &ndn_bridge_csap_s },
-#endif
 };
 
 asn_type ndn_generic_csap_level_s =
@@ -121,62 +86,41 @@ asn_type ndn_generic_csap_level_s =
 const asn_type * const ndn_generic_csap_level = &ndn_generic_csap_level_s;
 
 /* Add here declaraions of protocol-specific PDU ASN type */ 
-#ifdef WITH_FILE
 extern asn_type ndn_file_message_s;
-#endif
-#ifdef WITH_SNMP
+#ifdef HAS_SNMP
 extern asn_type ndn_snmp_message_s;
 #endif
-#ifdef WITH_ETH
 extern asn_type ndn_eth_header_s;
-#endif 
 
-#ifdef WITH_IPSTACK
 extern asn_type ndn_ip4_header_s;
 extern asn_type ndn_icmp4_message_s;
 extern asn_type ndn_udp_header_s;
-#endif 
 
-#ifdef WITH_CLI
 extern asn_type ndn_cli_message_s;
-#endif
-#ifdef WITH_DHCP
-extern asn_type ndn_dhcpv4_message_s;
-#endif
 
-#ifdef WITH_BRIDGE
+extern asn_type ndn_dhcpv4_message_s;
+
 extern asn_type ndn_bridge_pdu_s;
-#endif
 
 
 static asn_named_entry_t _ndn_generic_pdu_ne_array [] = 
 {
 /* Add here reference of protocol-specific CSAP init params */ 
-#ifdef WITH_FILE
     { "file", &ndn_file_message_s },
-#endif
-#ifdef WITH_SNMP
+#ifdef HAS_SNMP
     { "snmp", &ndn_snmp_message_s },
 #endif
-#ifdef WITH_ETH
     { "eth", &ndn_eth_header_s },
-#endif 
 
-#ifdef WITH_IPSTACK
     { "ip4", &ndn_ip4_header_s },
     { "icmp4", &ndn_icmp4_message_s },
     { "udp", &ndn_udp_header_s },
-#endif 
 
-#ifdef WITH_CLI
     { "cli", &ndn_cli_message_s },
-#endif
-#ifdef WITH_DHCP
+
     { "dhcp", &ndn_dhcpv4_message_s },
-#endif
-#ifdef WITH_BRIDGE
+
     { "bridge", &ndn_bridge_pdu_s },
-#endif
 };
 
 asn_type ndn_generic_pdu_s =
