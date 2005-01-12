34f4c2919b8f2cf0352579de6e10213b9031ae6d
Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
2005-01-12 08:18:39 +0000

    script updated
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@9272 9d14d920-75d5-0310-8bd7-a52572981aee

--- tools/trc/te_trc.sh
+++ tools/trc/te_trc.sh
@@ -2,8 +2,21 @@
 
 RAW_LOG_FILE=$1
 DB_FILE=$2
-XML_LOG_FILE=log.xml
 
-rgt-conv -m postponed -c rgt-filter.conf -f $RAW_LOG_FILE -o $XML_LOG_FILE
+RGT_FILTER=`mktemp`
+echo \
+"<?xml version="1.0"?>"\
+"<filters>"\
+"  <entity-filter>"\
+"    <!-- Exclude log from all entities -->"\
+"    <exclude entity=\"\"/>"\
+"  </entity-filter>"\
+"</filters>" > $RGT_FILTER
+
+XML_LOG_FILE=`mktemp`
+
+rgt-conv -m postponed -c $RGT_FILTER -f $RAW_LOG_FILE -o $XML_LOG_FILE
 te_trc -d $DB_FILE $XML_LOG_FILE
+
 rm $XML_LOG_FILE
+rm $RGT_FILTER
