dbcc2b61f6f93b57ac1aeaf3f032b963aee531f7
Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
2005-01-13 12:18:18 +0000

    Updated to process a directory
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@9400 9d14d920-75d5-0310-8bd7-a52572981aee

--- engine/builder/te_test_objective.sh
+++ engine/builder/te_test_objective.sh
@@ -28,23 +28,39 @@
 # 
 # $Id: $
 
-awk --posix '
-BEGIN { put = 0; }                               \
-{                                                \
-    if ($1 == "*" && $2 == "@objective")         \
-    {                                            \
-        for (i = 3; i <= NF; i++)                \
-            printf("%s ", $i);                   \
-        put = 1;                                 \
-    }                                            \
-    else if (put == 1 && $1 == "*" && NF == 1)   \
-        put = 0;                                 \
-    else if (put)                                \
-    {                                            \
-        for (i = 2; i <= NF; i++)                \
-            printf("%s ", $i);                   \
-    }                                            \
-} ' $1 > $2
 
+echo -en \
+"<?xml version=\"1.0\"?>\\n"\
+"<tests-info>\\n"
 
+for i in `find $1 -name *.c` ; do
+    OBJECTIVE=`awk --posix '
+    BEGIN { put = 0; }                               \
+    {                                                \
+        if ($1 == "*" && $2 == "@objective")         \
+        {                                            \
+            for (i = 3; i <= NF; i++)                \
+                printf("%s ", $i);                   \
+            put = 1;                                 \
+        }                                            \
+        else if (put == 1 && $1 == "*" && NF == 1)   \
+            put = 0;                                 \
+        else if (put)                                \
+        {                                            \
+            for (i = 2; i <= NF; i++)                \
+                printf("%s ", $i);                   \
+        }                                            \
+    } ' $i`
+    OBJECTIVE=`echo $OBJECTIVE | sed -e "s/@a //g"`
+    OBJECTIVE=`echo $OBJECTIVE | sed -e "s/@b //g"`
+    OBJECTIVE=`echo $OBJECTIVE | sed -e "s/@c //g"`
+    if test -n "${OBJECTIVE}" ; then
+        TEST_NAME=`basename $i`
+        TEST_NAME=${TEST_NAME/.c/}
+        echo "  <test name=\"${TEST_NAME}\">"
+        echo "    <objective>${OBJECTIVE}</objective>"
+        echo "  </test>"
+    fi
+done
 
+echo "</tests-info>"
