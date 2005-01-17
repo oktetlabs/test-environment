675afb0382c4f240969ba621e1604b31db801262
Oleg Kravtsov <Oleg.Kravtsov@oktetlabs.ru>
2005-01-17 15:47:46 +0000

    Initial version of TAPI ARP is added.
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@9629 9d14d920-75d5-0310-8bd7-a52572981aee

---
+++ lib/tapi_arp/tapi_arp.c
@@ -0,0 +1,211 @@
+/** @file
+ * @brief Proteos, Tester API for ARP CSAP.
+ *
+ * Implementation of Tester API for Ethernet CSAP.
+ *
+ * Copyright (C) 2003 Test Environment authors (see file AUTHORS in the
+ * root directory of the distribution).
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA  02111-1307  USA
+ *
+ * @author Oleg Kravtsov <Oleg.Kravtsov@oktetlabs.ru>
+ *
+ * $Id$
+ */ 
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#ifdef HAVE_STDLIB_H
+#include <stdlib.h>
+#endif
+#ifdef HAVE_STRING_H 
+#include <string.h>
+#endif
+#ifdef HAVE_STRINGS_H
+#include <strings.h>
+#endif
+
+
+#include <stdio.h>
+#include <assert.h>
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include "rcf_api.h"
+#include "ndn_eth.h"
+#include "tapi_arp.h"
+#include "tapi_eth.h"
+
+#define TE_LGR_USER     "TAPI ARP"
+#include "logger_api.h"
+
+/* See the description in tapi_arp.h */
+int
+tapi_arp_csap_create(const char *ta_name, int sid, const char *device,
+                     const uint8_t *remote_addr, const uint8_t *local_addr, 
+                     csap_handle_t *arp_csap)
+{
+    uint16_t eth_type = ETHERTYPE_ARP;
+
+    return tapi_eth_csap_create(ta_name, sid, device, remote_addr, local_addr,
+                                &eth_type, arp_csap);
+}
+
+/* See the description in tapi_arp.h */
+int
+tapi_arp_send(const char *ta_name, int sid,
+              csap_handle_t arp_csap, const asn_value *templ)
+{
+    return tapi_eth_send(ta_name, sid, arp_csap, templ);
+}
+
+/* See the description in tapi_arp.h */
+int
+tapi_arp_recv_start(const char *ta_name, int sid, csap_handle_t arp_csap,
+                    const asn_value *pattern,
+                    tapi_arp_frame_callback cb, void *cb_data,
+                    unsigned int timeout, int num)
+{
+    UNUSED(ta_name);
+    UNUSED(sid);
+    UNUSED(arp_csap);
+    UNUSED(pattern);
+    UNUSED(cb);
+    UNUSED(cb_data);
+    UNUSED(timeout);
+    UNUSED(num);
+}
+
+int
+tapi_arp_prepare_template(const tapi_arp_frame_t *frame, asn_value **templ)
+{
+    asn_value *traffic_templ;
+    asn_value *frame_tmpl;
+    asn_value *asn_pdus, *asn_pdu;
+    uint8_t   *arp_pkt;
+    uint32_t   arp_pkt_len;
+    int        rc = 0;
+
+    if (frame == NULL || templ == NULL)
+        return EINVAL;
+    
+    if ((uint32_t)(frame->data) ^ (uint32_t)(frame->data_len))
+    {
+        ERROR("'data' and 'data_len' fields should be 'NULL' and zero, "
+              "or non 'NULL' and not zero.");
+        return EINVAL;
+    }
+
+    if (frame->arp_hdr.hard_size > sizeof(frame->arp_hdr.snd_hw_addr))
+    {
+        ERROR("The value of 'hard_size' field is more than the length of "
+              "'snd_hw_addr' and 'tgt_hw_addr' fields");
+        return EINVAL;
+    }
+    if (frame->arp_hdr.proto_size > sizeof(frame->arp_hdr.snd_proto_addr))
+    {
+        ERROR("The value of 'proto_size' field is more than the length of "
+              "'snd_proto_addr' and 'tgt_proto_addr' fields");
+        return EINVAL;
+    }
+
+    /* Allocate memory under ARP packet */
+#define ARP_FIELD_SIZE(fld_) sizeof(frame->arp_hdr.fld_)
+    arp_pkt_len = ARP_FIELD_SIZE(hard_type) + ARP_FIELD_SIZE(proto_type) +
+                  ARP_FIELD_SIZE(hard_size) + ARP_FIELD_SIZE(proto_size) +
+                  ARP_FIELD_SIZE(op_code) + frame->arp_hdr.hard_size * 2 +
+                  frame->arp_hdr.proto_size * 2 + frame->data_len;
+#undef ARP_FIELD_SIZE
+    if ((arp_pkt = (uint8_t *)malloc(arp_pkt_len)) == NULL)
+        return ENOMEM;
+
+    traffic_templ = asn_init_value(ndn_traffic_template);
+    asn_pdus = asn_init_value(ndn_generic_pdu_sequence);
+    asn_pdu = asn_init_value(ndn_generic_pdu);
+
+    frame_tmpl = ndn_eth_plain_to_packet(&(frame->eth_hdr));
+    if (frame_tmpl == NULL)
+    {
+        free(arp_pkt);
+        return ENOMEM;
+    }
+
+    rc = asn_write_component_value(asn_pdu, frame_tmpl, "#eth");
+
+    if (rc == 0)
+        rc = asn_insert_indexed(asn_pdus, asn_pdu, -1, "");
+
+    if (rc == 0)
+        rc = asn_write_component_value(traffic_templ, asn_pdus, "pdus");
+
+    /** Create ARP header and add it as the payload to Ethernet frame */
+    if (rc == 0)
+    {
+        uint16_t  short_var;
+        uint8_t  *arp_pkt_start = arp_pkt;
+        
+#define ARP_FILL_SHORT_VAR(fld_) \
+        do {                                                \
+            short_var = htons(frame->arp_hdr.fld_);         \
+            memcpy(arp_pkt, &short_var, sizeof(short_var)); \
+            arp_pkt += sizeof(short_var);                   \
+        } while (0)
+    
+        ARP_FILL_SHORT_VAR(hard_type);
+        ARP_FILL_SHORT_VAR(proto_type);
+
+        *(arp_pkt++) = frame->arp_hdr.hard_size;
+        *(arp_pkt++) = frame->arp_hdr.proto_size;
+
+        ARP_FILL_SHORT_VAR(op_code);
+
+#undef ARP_FILL_SHORT_VAR
+
+#define ARP_FILL_ARRAY(fld_, size_fld_) \
+        do {                                     \
+            memcpy(arp_pkt, frame->arp_hdr.fld_, \
+                   frame->arp_hdr.size_fld_);    \
+            arp_pkt += frame->arp_hdr.size_fld_; \
+        } while (0)
+
+        ARP_FILL_ARRAY(snd_hw_addr, hard_size);
+        ARP_FILL_ARRAY(snd_proto_addr, proto_size);
+        ARP_FILL_ARRAY(tgt_hw_addr, hard_size);
+        ARP_FILL_ARRAY(tgt_proto_addr, proto_size);
+
+#undef ARP_FILL_ARRAY
+
+        assert((arp_pkt - arp_pkt_start) == arp_pkt_len);
+        rc = asn_write_value_field(traffic_templ, arp_pkt, arp_pkt_len,
+                                   "payload.#bytes");
+    }
+
+    free(arp_pkt);
+
+    if (rc != 0)
+    {
+        VERB("Cannot create traffic template %x\n", rc);
+        return rc;
+    }
+
+    *templ = traffic_templ;
+    return 0;
+}
---
+++ lib/tapi_arp/tapi_arp.h
@@ -0,0 +1,203 @@
+/** @file
+ * @brief Proteos, TAPI ARP.
+ *
+ * Declarations of API for TAPI ARP.
+ *
+ * Copyright (C) 2004 Test Environment authors (see file AUTHORS in the
+ * root directory of the distribution).
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA  02111-1307  USA
+ *
+ * @author Oleg Kravtsov <Oleg.Kravtsov@oktetlabs.ru>
+ *
+ * $Id$
+ */ 
+
+#ifndef TAPI_ARP_H__
+#define TAPI_ARP_H__
+
+#include <stdio.h>
+#ifdef HAVE_ASSERT_H
+#include <assert.h>
+#endif
+#ifdef HAVE_NETINET_IN_H
+#include <netinet/in.h>
+#endif
+
+#include <net/ethernet.h>
+#include <net/if_arp.h>
+
+#include "te_stdint.h"
+#include "te_defs.h"
+#include "tad_common.h"
+#include "asn_usr.h"
+
+/* ARP request operation - ARPOP_REQUEST */
+/* ARP reply operation - ARPOP_REPLY */
+
+/** Buffer size under hardware address */
+#define TAPI_ARP_MAX_HW_ADDR_LEN    6
+/** Buffer size under protocol address */
+#define TAPI_ARP_MAX_PROTO_ADDR_LEN 4
+
+/** Structure that represents ARP header */
+typedef struct tapi_arp_pkt {
+    uint16_t hard_type; /**< Hardware type filed */
+    uint16_t proto_type; /**< Protocol type filed */
+    uint8_t  hard_size; /**< Size of hardware address */
+    uint8_t  proto_size; /**< Size of protocol address */
+    uint16_t op_code; /**< Operation type */
+    uint8_t  snd_hw_addr[TAPI_ARP_MAX_HW_ADDR_LEN]; /**< Sender hardware 
+                                                         address */
+    uint8_t  snd_proto_addr[TAPI_ARP_MAX_PROTO_ADDR_LEN]; /**< Sender protocol
+                                                               address */
+    uint8_t  tgt_hw_addr[TAPI_ARP_MAX_HW_ADDR_LEN]; /**< Target hardware 
+                                                         address */
+    uint8_t  tgt_proto_addr[TAPI_ARP_MAX_PROTO_ADDR_LEN]; /**< Target protocol
+                                                               address */
+} tapi_arp_pkt_t;
+
+/** Structure that represents ARP frame: Ethernet header and ARP header */
+typedef struct tapi_arp_frame {
+    ndn_eth_header_plain  eth_hdr; /**< Ethernet header */
+    tapi_arp_pkt_t        arp_hdr; /**< APR packet */
+
+    uint32_t              data_len; /**< Data length */
+    uint8_t              *data; /**< The data that goes after ARP header */
+} tapi_arp_frame_t;
+
+/**
+ * Fills in Ethernet header of ARP frame with 802.3 source and destination 
+ * MAC addresses.
+ *
+ * @param arp_frame  ARP frame to be updated
+ * @param src_mac    Source MAC address
+ * @param dst_mac    Destination MAC address
+ */
+#define TAPI_ARP_FILL_ETH_HDR(arp_frame_, src_mac_, dst_mac_) \
+    do {                                                            \
+        memeset(&((arp_frame_)->eth_hdr), 0,                        \
+                sizeof((arp_frame_)->eth_hdr));                     \
+        assert(sizeof((arp_frame_)->eth_hdr.src_addr) == ETH_ALEN); \
+        memcpy((arp_frame_)->eth_hdr.src_addr, src_mac_, ETH_ALEN); \
+        assert(sizeof((arp_frame_)->eth_hdr.dst_addr) == ETH_ALEN); \
+        memcpy((arp_frame_)->eth_hdr.dst_addr, dst_mac_, ETH_ALEN); \
+        (arp_frame_)->eth_hdr.eth_type_len = ETHERTYPE_ARP;         \
+        (arp_frame_)->eth_hdr.is_tagged = FALSE;                    \
+    } while (0)
+
+#define TAPI_ARP_FILL_HDR(arp_frame_, op_, \
+                          snd_hw_, snd_proto_, tgt_hw_, tgt_proto_) \
+    do {                                                              \
+        (arp_frame_)->arp_hdr.hard_type = ARPHRD_ETHER;               \
+        (arp_frame_)->arp_hdr.proto_type = ETHERTYPE_IP;              \
+        (arp_frame_)->arp_hdr.hard_size = ETH_ALEN;                   \
+        (arp_frame_)->arp_hdr.proto_size = sizeof(struct in_addr);    \
+        (arp_frame_)->arp_hdr.op_code = op_;                          \
+        memcpy((arp_frame_)->arp_hdr.snd_hw_addr, snd_hw_, ETH_ALEN); \
+                                                                      \
+        memcpy((arp_frame_)->arp_hdr.snd_proto_addr, snd_proto_,      \
+               sizeof(struct in_addr));                               \
+        memcpy((arp_frame_)->arp_hdr.tgt_hw_addr, tgt_hw_, ETH_ALEN); \
+        memcpy((arp_frame_)->arp_hdr.tgt_proto_addr, tgt_proto_,      \
+               sizeof(struct in_addr));                               \
+                                                                      \
+        (arp_frame_)->data = NULL;                                    \
+        (arp_frame_)->data_len = 0;                                   \
+    } while (0)
+
+/**
+ * Create ARP CSAP that runs over Ethernet RFC 894.
+ *
+ * @param ta_name       Test Agent name
+ * @param sid           RCF session
+ * @param device        interface name on TA host
+ * @param remote_addr   default remote MAC address, may be NULL - in this
+ *                      case frames will be sent only if dst is specified in
+ *                      template, and frames from all src's will be
+ *                      catched.
+ *                      If NULL, CSAP will have remote address unconfigured 
+ *                      and will require it in traffic template.
+ * @param local_addr    default local MAC address. may be NULL - in this case
+ *                      frames will be sent with src specifed in template
+ *                      or native for outgoing device (if not present
+ *                      in template).
+ * @param arp_csap      Identifier of created CSAP (OUT)
+ *
+ * @return zero on success, otherwise standard or common TE error code. 
+ */
+extern int tapi_arp_csap_create(const char *ta_name, int sid,
+                                const char *device,
+                                const uint8_t *remote_addr, 
+                                const uint8_t *local_addr, 
+                                csap_handle_t *arp_csap);
+
+/**
+ * Sends traffic from specified CSAP according to the traffic template passed.
+ * This function blocks until all packets are sent.
+ *
+ * @param ta_name  - Test Agent name
+ * @param sid      - RCF session identifier
+ * @param arp_csap - CSAP handle
+ * @param templ    - Traffic template
+ * 
+ * @return zero on success, otherwise standard or common TE error code.
+ */
+extern int tapi_arp_send(const char *ta_name, int sid,
+                         csap_handle_t arp_csap,
+                         const asn_value *templ);
+
+/**
+ * Callback function for the tapi_arp_recv_start() routine, it is called
+ * for each packet received on CSAP.
+ *
+ * @param header        structure with ARP and Ethernet header of the frame.
+ * @param payload       payload of the frame.
+ * @param plen          length of the frame payload.
+ * @param userdata      Pointer to user data, provided by  the caller of 
+ *                      tapi_arp_recv_start.
+ */
+typedef void (*tapi_arp_frame_callback)(const tapi_arp_frame_t *header, 
+                                        void *userdata);
+
+/**
+ * Start receive process on specified ARP CSAP. If process
+ * was started correctly (rc is OK) it can be managed by common RCF
+ * methods 'rcf_ta_trrecv_wait', 'rcf_ta_trrecv_get' and 'rcf_ta_trrecv_stop'.
+ *
+ * @param ta_name   - Test Agent name
+ * @param sid       - RCF session
+ * @param arp_csap  - CSAP handle 
+ * @param pattern   - ASN value with receive pattern
+ * @param cb        - Callback function which will be called for each 
+ *                    received frame, may me NULL if frames are not need
+ * @param cb_data   - pointer to be passed to user callback
+ * @param timeout   - Timeout for receiving of packets, measured in 
+ *                    milliseconds
+ * @param num       - Number of packets caller wants to receive
+ *
+ * @return zero on success, otherwise standard or common TE error code.
+ */
+extern int tapi_arp_recv_start(const char *ta_name, int sid,
+                               csap_handle_t arp_csap,
+                               const asn_value *pattern,
+                               tapi_arp_frame_callback cb, void *cb_data,
+                               unsigned int timeout, int num);
+
+extern int tapi_arp_prepare_template(const tapi_arp_frame_t *frame,
+                                     asn_value **templ);
+
+#endif /* TAPI_ARP_H__ */
