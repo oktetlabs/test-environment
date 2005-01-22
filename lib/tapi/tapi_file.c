f80ded73e96c38ad1361620d44849b15d560ea29
Elena Vengerova <Elena.Vengerova@oktetlabs.ru>
2005-01-22 12:10:48 +0000

    Initially added
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@9856 9d14d920-75d5-0310-8bd7-a52572981aee

---
+++ lib/tapi/tapi_file.c
@@ -0,0 +1,96 @@
+/** @file
+ * @brief Test API
+ *
+ * Functions for convinient work with the files on the engine and TA.
+ *
+ * Copyright (C) 2005 OKTET Labs Ltd., St.-Petersburg, Russia
+ *
+ * @author Elena Vengerova <Elena.Vengerova@oktetlabs.ru>
+ *
+ * $Id$
+ */
+
+#define TE_LGR_USER     "File TAPI"
+
+#include "config.h"
+
+#include <stdio.h>
+
+#ifdef STDC_HEADERS
+#include <string.h>
+#endif
+
+#ifdef HAVE_STRING_H
+#include <string.h>
+#endif
+
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+
+#if HAVE_TIME_H
+#include <time.h>
+#endif
+
+#include "te_defs.h"
+#include "te_errno.h"
+#include "te_stdint.h"
+#include "rcf_api.h"
+#include "logger_api.h"
+#include "tapi_file.h"
+
+/**
+ * Create file in the TE temporary directory.
+ *
+ * @param len   file length
+ * @param c     file content
+ *
+ * @return name of the file or NULL in the case of failure
+ *
+ * @note the function is not thread-safe and re-enterable
+ */
+char *
+tapi_file_create(int len, char c)
+{
+    static char pathname[RCF_MAX_PATH];
+
+    char  buf[1024];
+    char *te_tmp;
+    FILE *f;
+    
+    if ((te_tmp = getenv("TE_TMP")) == NULL)
+    {
+        ERROR("TE_TMP is empty");
+        return NULL;
+    }
+
+    sprintf(pathname, "%s/%s", getenv("TE_TMP"), tapi_file_generate_name());
+    
+    if ((f = fopen(pathname, "w")) == NULL)
+    {
+        ERROR("Cannot open file %s errno %d\n", pathname, errno);
+        return NULL;
+    }
+    
+    memset(buf, c, sizeof(buf));
+    
+    while (len > 0)
+    {
+        int copy_len = ((unsigned int)len  > sizeof(buf)) ?
+                           (int)sizeof(buf) : len;
+
+        if ((copy_len = fwrite((void *)buf, sizeof(char), copy_len, f)) < 0)
+        {
+            fclose(f);
+            ERROR("fwrite() faled: file %s errno %d\n", pathname, errno);
+            return NULL;
+        }
+        len -= copy_len;
+    }
+    if (fclose(f) < 0)
+    {
+        ERROR("fclose() failed: file %s errno=%d", pathname, errno);
+        return NULL;
+    }
+    return pathname;
+}
---
+++ lib/tapi/tapi_file.h
@@ -0,0 +1,49 @@
+/** @file
+ * @brief Test API
+ *
+ * Functions for convinient work with the files on the engine and TA.
+ *
+ * Copyright (C) 2005 OKTET Labs Ltd., St.-Petersburg, Russia
+ *
+ * @author Elena Vengerova <Elena.Vengerova@oktetlabs.ru>
+ *
+ * $Id$
+ */
+
+#ifndef __TE_LIB_TAPI_FILE_H__
+#define __TE_LIB_TAPI_FILE_H__
+
+/**
+ * Generate unique basename for file.
+ *
+ * @return generated name
+ *
+ * @note the function is not thread-safe
+ */
+static inline char *
+tapi_file_generate_name()
+{
+    static int  num = 0;
+    static char buf[RCF_MAX_PATH];
+    
+    sprintf(buf, "te_tmp_%u_%u_%u", (uint32_t)time(NULL), getpid(), num++);
+    
+    return buf;
+}
+
+/**
+ * Create file in the TE temporary directory.
+ *
+ * @param len   file length
+ * @param c     file content
+ *
+ * @return name of the file or NULL in the case of failure
+ *
+ * @note the function is not thread-safe and re-enterable
+ */
+extern char *tapi_file_create(int len, char c);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+#endif /* !__TE_LIB_TAPI_FILE_H__ */
