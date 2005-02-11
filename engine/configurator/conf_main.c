812a873c04ed912b7e408df2707ad8620d451a02
Elena Vengerova <Elena.Vengerova@oktetlabs.ru>
2005-02-11 12:51:43 +0000

    Agents checking is added
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@11030 9d14d920-75d5-0310-8bd7-a52572981aee

--- engine/configurator/conf_main.c
+++ engine/configurator/conf_main.c
@@ -515,6 +515,12 @@ process_backup(cfg_backup_msg *msg)
 
         case CFG_BACKUP_RESTORE:
         {
+            /* Check agents */
+            int rc = rcf_check_agents();
+            
+            if (TE_RC_GET_ERROR(rc) == ETAREBOOTED)
+                cfg_ta_sync("/agent", TRUE);
+            
             /* Try to restore using dynamic history */
             if ((msg->rc = cfg_dh_restore_backup(msg->filename)) == 0)
                 return;
