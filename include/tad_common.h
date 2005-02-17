dfd4f045b48d59a5502577330b7c9d2f4c6134e5
Konstantin Abramenko <Konstantin.Abramenko@oktetlabs.ru>
2005-02-17 15:26:14 +0000

    coding style fix
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@11331 9d14d920-75d5-0310-8bd7-a52572981aee

--- include/tad_common.h
+++ include/tad_common.h
@@ -45,12 +45,14 @@
 #define CSAP_PARAM_LAST_PACKET_TIME     "last_pkt_time"
 
 
-/** Type for CSAP handle, should have semantic unsigned integer, 
+/** 
+ * Type for CSAP handle, should have semantic unsigned integer, 
  * because TAD Users Guide specify CSAP ID as positive integer, and
- * zero used as mark of invalid/uninitialized CSAP handle. */ 
+ * zero used as mark of invalid/uninitialized CSAP handle. 
+ */ 
 typedef unsigned int csap_handle_t;
 enum {
-    CSAP_INVALID_HANDLE=0, /**< Constant for invalid CSAP handle */
+    CSAP_INVALID_HANDLE = 0, /**< Constant for invalid CSAP handle */
 };
 
 
