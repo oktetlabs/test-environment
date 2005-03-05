95675c53f856f8eec9962abf321973c2852aa91a
Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
2005-03-05 12:54:12 +0000

    The first draft of jumps in TAPI
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@12171 9d14d920-75d5-0310-8bd7-a52572981aee

--- lib/tapi/tapi_jmp.c
+++ lib/tapi/tapi_jmp.c
@@ -28,15 +28,234 @@
  * $Id$
  */
 
+#define TE_LOG_LEVEL    0xff
 #define TE_LGR_USER     "TAPI Jumps"
 
 #include "te_config.h"
 
+#include <stdio.h>
 #ifdef STDC_HEADERS
 #include <stdlib.h>
+#else
+#if HAVE_STDLIB_H
+#include <stdlib.h>
+#endif
+#endif
+
+#if HAVE_PTHREAD_H
+#include <pthread.h>
+#else
+#error Required pthread.h not found
 #endif
 
 #include "te_defs.h"
+#include "te_errno.h"
 #include "logger_api.h"
+#include "tapi_jmp.h"
 
 
+/** List of jump points */
+typedef LIST_HEAD(tapi_jmp_stack, tapi_jmp_point) tapi_jmp_stack;
+
+/** Jumps context */
+typedef struct tapi_jmp_ctx {
+    tapi_jmp_stack  stack;      /**< Stack of jump points */
+    tapi_jmp_stack  garbage;    /**< Stack of garbage jump points */
+} tapi_jmp_ctx;
+
+
+static pthread_once_t   jmp_once_control = PTHREAD_ONCE_INIT;
+static pthread_key_t    jmp_key;
+
+
+static tapi_jmp_ctx *tapi_jmp_get_ctx(te_bool create);
+
+
+/**
+ * Free garbage in the jumps context.
+ *
+ * @param ctx       Jumps context
+ */
+static void
+tapi_jmp_ctx_free_garbage(tapi_jmp_ctx *ctx)
+{
+    tapi_jmp_point *p;
+
+    assert(ctx != NULL);
+    while ((p = ctx->garbage.lh_first) != NULL)
+    {
+        LIST_REMOVE(p, links);
+        free(p);
+    }
+}
+
+/**
+ * Destructor of resources assosiated with thread.
+ *
+ * @param handle    Handle assosiated with thread
+ */
+static void
+tapi_jmp_thread_ctx_destroy(void *handle)
+{
+    tapi_jmp_ctx   *ctx = handle;
+    tapi_jmp_point *p;
+
+    if (ctx == NULL)
+        return;
+
+    tapi_jmp_ctx_free_garbage(ctx);
+    while ((p = ctx->stack.lh_first) != NULL)
+    {
+        ERROR("Jump point destructed: ");
+        LIST_REMOVE(p, links);
+        free(p);
+    }
+}
+
+/**
+ * Callback to be called at application exit.
+ */
+static void
+tapi_jmp_atexit_callback(void)
+{
+    tapi_jmp_thread_ctx_destroy(tapi_jmp_get_ctx(FALSE));
+}
+
+/**
+ * Only once called function to create key.
+ */
+static void
+tapi_jmp_key_create(void)
+{
+    if (pthread_key_create(&jmp_key, tapi_jmp_thread_ctx_destroy) != 0)
+    {
+        ERROR("%s(): pthread_key_create() failed", __FUNCTION__);
+        fprintf(stderr, "pthread_key_create() failed\n");
+    }
+    atexit(tapi_jmp_atexit_callback);
+}
+
+/**
+ * Find thread context handle or initialize a new one.
+ *
+ * @return Context handle or NULL if some error occured
+ */
+static tapi_jmp_ctx *
+tapi_jmp_get_ctx(te_bool create)
+{
+    tapi_jmp_ctx   *ctx;
+
+    if (pthread_once(&jmp_once_control, tapi_jmp_key_create) != 0)
+    {
+        ERROR("%s(): pthread_once() failed", __FUNCTION__);
+        return NULL;
+    }
+
+    ctx = pthread_getspecific(jmp_key);
+    if (ctx == NULL && create)
+    {
+        ctx = calloc(1, sizeof(*ctx));
+        if (ctx == NULL)
+        {
+            ERROR("%s(): calloc(1, %u) failed",
+                  __FUNCTION__, sizeof(*ctx));
+        }
+        else
+        {
+            LIST_INIT(&ctx->stack);
+            LIST_INIT(&ctx->garbage);
+            if (pthread_setspecific(jmp_key, ctx) != 0)
+            {
+                ERROR("%s(): pthread_setspecific() failed", __FUNCTION__);
+                free(ctx);
+                ctx = NULL;
+            }
+        }
+    }
+
+    return ctx;
+}
+
+
+/* See description in tapi_jmp.h */
+tapi_jmp_point *
+tapi_jmp_alloc(te_bool enabled, const char *file, unsigned int lineno)
+{
+    tapi_jmp_ctx   *ctx = tapi_jmp_get_ctx(TRUE);
+    tapi_jmp_point *p;
+
+    if (ctx == NULL)
+    {
+        ERROR("%s(): No context");
+        return NULL;
+    }
+    tapi_jmp_ctx_free_garbage(ctx);
+
+    p = calloc(1, sizeof(*p));
+    if (p == NULL)
+    {
+        ERROR("%s(): calloc(1, %u) failed", __FUNCTION__, sizeof(*p));
+        return NULL;
+    }
+    LIST_INSERT_HEAD(&ctx->stack, p, links);
+    p->enabled = enabled;
+    p->file    = file;
+    p->lineno  = lineno;
+
+    INFO("%s(): %s:%u=%u", __FUNCTION__, file, lineno, enabled);
+    return p;
+}
+
+/* See description in tapi_jmp.h */
+int
+tapi_jmp_do(int val)
+{
+    tapi_jmp_ctx   *ctx;
+    tapi_jmp_point *p;
+
+    if (val < 0)
+    {
+        ERROR("%s(): Invalid return value %d for jump to do",
+              __FUNCTION__, val);
+        return TE_RC(TE_TAPI, EINVAL);
+    }
+    else if (val == 0)
+    {
+        val = ETEOK;
+    }
+
+    ctx = tapi_jmp_get_ctx(FALSE);
+    if (ctx == NULL)
+    {
+        ERROR("%s(): No context", __FUNCTION__);
+        return TE_RC(TE_TAPI, ENOMEM);
+    }
+    tapi_jmp_ctx_free_garbage(ctx);
+
+    if ((p = ctx->stack.lh_first) == NULL)
+    {
+        ERROR("%s(): Jumps stack is empty");
+        return TE_RC(TE_TAPI, ENOENT);
+    }
+    LIST_REMOVE(p, links);
+    if (p->enabled)
+    {
+        /* 
+         * We can't free point here, since 'env' should be used
+         * in the function which never returns.
+         */
+        LIST_INSERT_HEAD(&ctx->garbage, p, links);
+        INFO("%s(): GOTO %s:%u rc=%x",
+             __FUNCTION__, p->file, p->lineno, (unsigned int)val);
+        longjmp(p->env, val);
+        /* Unreachable */
+        assert(FALSE);
+    }
+    else
+    {
+        INFO("%s(): Jumps are disabled at %s:%d - ignore",
+              __FUNCTION__, p->file, p->lineno);
+        free(p);
+        return 0;
+    }
+}
--- lib/tapi/tapi_jmp.h
+++ lib/tapi/tapi_jmp.h
@@ -31,10 +31,74 @@
 #ifndef __TE_TAPI_JMP_H__
 #define __TE_TAPI_JMP_H__
 
+#if HAVE_SYS_QUEUE_H
+#include <sys/queue.h>
+#else
+#error Required sys/queue.h not found
+#endif
+#if HAVE_SETJMP_H
+#include <setjmp.h>
+#else
+#error Required setjmp.h not found
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+/** Jump point (stack context to jump to) */
+typedef struct tapi_jmp_point {
+    LIST_ENTRY(tapi_jmp_point)  links;      /**< List links */
+
+    te_bool         enabled;    /** Are jumps enalbed or disabled? */
+    jmp_buf         env;        /**< Stack context, if jumps are
+                                     enabled */
+    const char     *file;       /**< Name of the file where the point
+                                     is added */
+    unsigned int    lineno;     /**< Line number in the file where the
+                                     point is added */
+} tapi_jmp_point;
+
+#define TAPI_ON_JMP(rc, x) \
+    do  {                                                               \
+        tapi_jmp_point *p = tapi_jmp_alloc(TRUE, __FILE__, __LINE__);   \
+                                                                        \
+        if (p == NULL || (rc = setjmp(p->env)) != 0)                    \
+        {                                                               \
+            x;                                                          \
+        }                                                               \
+    } while (0)
+
+/**
+ * Allocate long jump point (saved stack context for non-local goto).
+ *
+ * The routine is thread-safe using per thread stacks of jump points.
+ *
+ * @param enabled   Point with enabled/disabled jump
+ * @param file      Name of the file where jump point is set
+ * @param lineno    Number of line in the file
+ *
+ * @return Pointer to allocated long jump point.
+ *
+ * @sa tapi_jmp_do, tapi_jmp_pop
+ */
+extern tapi_jmp_point *tapi_jmp_alloc(te_bool enabled, const char *file,
+                                      unsigned int lineno);
+
+/**
+ * Do does non-local goto a stack context saved using tapi_jmp_push().
+ *
+ * The routine is thread-safe using per thread stacks of jump points.
+ *
+ * @param val       Positive value to be returned by tapi_jmp_push
+ *                  (0 is mapped to ETEOK)
+ *
+ * @return Status code
+ * @retval ENOENT   Stack of of jump points is empty
+ *
+ * @sa tapi_jmp_push, tapi_jmp_pop
+ */
+extern int tapi_jmp_do(int val);
 
 #ifdef __cplusplus
 } /* extern "C" */
