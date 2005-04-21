6c768ad47cffffd9f19f563d1cf6531bf1891844
Alexander Kukuta <Alexander.Kukuta@oktetlabs.ru>
2005-04-21 15:53:06 +0000

    TAPI PCAP implemented
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@14213 9d14d920-75d5-0310-8bd7-a52572981aee

---
+++ lib/tapi_pcap/tapi_pcap.c
@@ -0,0 +1,354 @@
+/** @file
+ * @brief Proteos, Tester API for Ethernet-PCAP CSAP.
+ *
+ * Implementation of Tester API for Ethernet-PCAP CSAP.
+ *
+ *
+ * Copyright (C) 2003 Test Environment authors (see file AUTHORS in the
+ * root directory of the distribution).
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA  02111-1307  USA
+ *
+ *
+ * @author Alexander Kukuta <Alexander.Kukuta@oktetlabs.ru>
+ *
+ * $Id: $
+ */
+
+#include "te_config.h"
+
+#include <stdio.h>
+#if HAVE_STDLIB_H
+#include <stdlib.h>
+#endif
+#if HAVE_STRING_H
+#include <string.h>
+#endif
+#if HAVE_STRINGS_H
+#include <strings.h>
+#endif
+#if HAVE_ASSERT_H
+#include <assert.h>
+#endif
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include "te_errno.h"
+#include "rcf_api.h"
+#include "ndn_pcap.h"
+#include "tapi_pcap.h"
+
+#define TE_LOG_LEVEL 0xff
+#define TE_LGR_USER     "TAPI PCAP"
+#include "logger_api.h"
+
+#define TAPI_DEBUG 1
+
+#if 1
+#define PCAP_DEBUG(args...) \
+    do {                                        \
+        fprintf(stdout, "\nTAPI PCAP " args);    \
+        INFO("TAPI PCAP " args);                 \
+    } while (0)
+
+#undef ERROR
+#define ERROR(args...) PCAP_DEBUG("ERROR: " args)
+
+#undef RING
+#define RING(args...) PCAP_DEBUG("RING: " args)
+
+#undef WARN
+#define WARN(args...) PCAP_DEBUG("WARN: " args)
+
+#undef VERB
+#define VERB(args...) PCAP_DEBUG("VERB: " args)
+#endif
+
+/**
+ * Create common Ethernet-PCAP CSAP.
+ *
+ * @param ta_name       Test Agent name
+ * @param sid           RCF session
+ * @param ifname        Interface name on TA host
+ * @param iftype        Interface datalink type (see man pcap)
+ * @param recv_mode     Receive mode, bit scale defined by elements of
+ *                      'enum pcap_csap_receive_mode' in ndn_pcap.h.
+ * @param pcap_csap     Identifier of created CSAP (OUT)
+ *
+ * @return Zero on success, otherwise standard or common TE error code.
+ */
+int
+tapi_pcap_csap_create(const char *ta_name, int sid,
+                      const char *ifname, int iftype, int recv_mode,
+                      csap_handle_t *pcap_csap)
+{
+    int     rc;
+    char    tmp_name[100];
+    FILE   *f;
+
+    if ((ta_name == NULL) || (ifname == NULL) || (pcap_csap == NULL))
+        return TE_RC(TE_TAPI, EINVAL);
+
+    strcpy(tmp_name, "/tmp/te_pcap_csap_create.XXXXXX");
+    mkstemp(tmp_name);
+
+    VERB("file: %s\n", tmp_name);
+
+    f = fopen(tmp_name, "w+");
+    if (f == NULL)
+    {
+        ERROR("fopen() of %s failed(%d)", tmp_name, errno);
+        return TE_RC(TE_TAPI, errno); /* return system errno */
+    }
+
+    fprintf(f, "{ pcap:{ ifname plain:\"%s\", iftype %d, receive-mode %d } }",
+            ifname, iftype, recv_mode);
+    fclose(f);
+
+    rc = rcf_ta_csap_create(ta_name, sid, "pcap", tmp_name, pcap_csap);
+    if (rc != 0)
+    {
+        ERROR("rcf_ta_csap_create() failed(0x%x) on TA %s:%d file %s",
+              rc, ta_name, sid, tmp_name);
+    }
+
+#if !(TAPI_DEBUG)
+    unlink(tmp_name);
+#endif
+
+    return rc;
+}
+
+
+struct tapi_pkt_handler_data {
+    tapi_pcap_recv_callback     user_callback;
+    void                       *user_data;
+};
+
+static void
+tapi_pcap_pkt_handler(char *fn, void *user_param)
+{
+    struct tapi_pkt_handler_data *i_data =
+        (struct tapi_pkt_handler_data *)user_param;
+
+    int rc;
+    int syms = 0;
+
+    asn_value *frame_val;
+    asn_value *pcap_filtered_pdu;
+
+    uint8_t   *pkt;
+    int        pkt_len;
+
+    int        filter_id;
+    int        tmp_len;
+
+    VERB("%s() started", __FUNCTION__);
+
+    if (user_param == NULL)
+    {
+        fprintf(stderr, "TAPI ETH, pkt handler: bad user param!!!\n");
+        return;
+    }
+
+    rc = asn_parse_dvalue_in_file(fn, ndn_raw_packet, &frame_val, &syms);
+    if (rc)
+    {
+        ERROR("Parse value from file %s failed, rc %x, syms: %d\n",
+                fn, rc, syms);
+        return;
+    }
+
+    pcap_filtered_pdu = asn_read_indexed (frame_val, 0, "pdus");
+    if (pcap_filtered_pdu == NULL)
+    {
+        ERROR("tapi eth int cb, read_indexed error\n");
+        return;
+    }
+
+#if 0
+    asn_save_to_file(pcap_filtered_pdu, "/tmp/pcap_rx_pdu");
+#endif
+
+    tmp_len = sizeof(int);
+    rc = asn_read_value_field(pcap_filtered_pdu, &filter_id, &tmp_len, "filter_id");
+    if (rc < 0)
+    {
+        filter_id = -1;
+    }
+
+    pkt_len = asn_get_length(frame_val, "payload.#bytes");
+    if (pkt_len < 0)
+    {
+        ERROR( "tapi eth int cb, get_len error \n");
+        return;
+    }
+
+    pkt = malloc(pkt_len);
+    if (pkt == NULL)
+    {
+        ERROR("There is no enough memory to allocate for packet data");
+        return;
+    }
+    
+    rc = asn_read_value_field(frame_val, pkt, &pkt_len, "payload.#bytes");
+    if (rc < 0)
+    {
+        ERROR( "tapi eth int cb, read payload error %x\n", rc);
+        return;
+    }
+
+    i_data->user_callback(filter_id, pkt, pkt_len, i_data->user_data);
+
+    free(pkt);
+
+    asn_free_value(frame_val);
+    asn_free_value(pcap_filtered_pdu);
+
+    return;
+}
+
+/* See the description in tapi_pcap.h */
+int
+tapi_pcap_recv_start(const char *ta_name, int sid,
+                     csap_handle_t pcap_csap,
+                     const asn_value *pattern,
+                     tapi_pcap_recv_callback cb, void *cb_data,
+                     unsigned int timeout, int num)
+{
+    int    rc;
+    char   tmp_name[100];
+    struct tapi_pkt_handler_data *i_data;
+
+    if (ta_name == NULL || pattern == NULL)
+        return TE_RC(TE_TAPI, EINVAL);
+
+    if ((i_data = malloc(sizeof(*i_data))) == NULL)
+        return TE_RC(TE_TAPI, ENOMEM);
+
+    i_data->user_callback = cb;
+    i_data->user_data = cb_data;
+
+    strcpy(tmp_name, "/tmp/te_pcap_trrecv.XXXXXX");
+    mkstemp(tmp_name);
+    rc = asn_save_to_file(pattern, tmp_name);
+    if (rc)
+    {
+        free(i_data);
+        return TE_RC(TE_TAPI, rc);
+    }
+
+    rc = rcf_ta_trrecv_start(ta_name, sid, pcap_csap, tmp_name,
+                             (cb != NULL) ? tapi_pcap_pkt_handler : NULL,
+                             (cb != NULL) ? (void *) i_data : NULL,
+                             timeout, num);
+    if (rc != 0)
+    {
+        ERROR("rcf_ta_trrecv_start() failed(0x%x) on TA %s:%d CSAP %d "
+              "file %s", rc, ta_name, sid, pcap_csap, tmp_name);
+    }
+
+#if !(TAPI_DEBUG)
+    unlink(tmp_name);
+#endif
+
+    return rc;
+}
+
+/* See the description in tapi_pcap.h */
+int
+tapi_pcap_pattern_add(const char *filter,
+                      const int   filter_id,
+                      asn_value **pattern)
+{
+    asn_value            *pcap_pdu = NULL;
+    asn_value            *pcap_pattern = NULL;
+    int                   tmp_len;
+    int                   syms;
+    int                   rc;
+
+    RING("%s(\"%s\", %d) started", __FUNCTION__, filter, filter_id);
+
+    if (pattern == NULL)
+    {
+        ERROR("not NULL pattern pointer required");
+        return EINVAL;
+    }
+
+    VERB("Call asn_init_value()");
+    pcap_pdu = asn_init_value(ndn_pcap_filter);
+
+    tmp_len = strlen(filter);
+    VERB("Call asn_write_value_field(\"filter.#plain\")");
+    if ((rc = asn_write_value_field(pcap_pdu, filter, tmp_len,
+                                    "filter.#plain")) != 0)
+    {
+        ERROR("Cannot write ASN value \"filter.#plain\"");
+        return rc;
+    }
+
+    tmp_len = sizeof(filter_id);
+    VERB("Call asn_write_value_field(\"filter-id\")");
+    if ((rc = asn_write_value_field(pcap_pdu, &filter_id, tmp_len,
+                                    "filter-id")) != 0)
+    {
+        ERROR("Cannot write ASN value \"filter-id\"");
+        return rc;
+    }
+
+
+    VERB("Call asn_parse_value_text()");
+    if ((rc = asn_parse_value_text("{ pdus { pcap: {}}}",
+                                   ndn_traffic_pattern_unit,
+                                   &pcap_pattern, &syms)) != 0)
+    {
+        ERROR("Cannot initialise PCAP PDU value");
+        return rc;
+    }
+    
+    VERB("Call asn_write_component_value()");
+    if ((rc = asn_write_component_value(pcap_pattern, pcap_pdu,
+                                        "pdus.0.#pcap")) != 0)
+    {
+        ERROR("Cannot initialise PCAP pattern value");
+        return rc;
+    }
+
+
+    if (*pattern == NULL)
+    {
+        VERB("Call asn_init_value()");
+        *pattern = asn_init_value(ndn_traffic_pattern);
+        if (*pattern == NULL)
+        {
+            ERROR("Init traffic pattern value failed");
+            return rc;
+        }
+    }
+    
+    VERB("Call asn_insert_indexed()");
+    if ((rc = asn_insert_indexed(*pattern, pcap_pattern, -1, "")) != 0)
+    {
+        ERROR("Cannot insert PCAP pattern to traffic pattern");
+        return rc;
+    }
+
+    return 0;
+}
+
---
+++ lib/tapi_pcap/tapi_pcap.h
@@ -0,0 +1,131 @@
+/** @file
+ * @brief Proteos, TAPI Ethernet.
+ *
+ * Declarations of API for TAPI Ethernet.
+ *
+ *
+ * Copyright (C) 2004 Test Environment authors (see file AUTHORS in the
+ * root directory of the distribution).
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA  02111-1307  USA
+ *
+ *
+ * @author Konstantin Abramenko <konst@oktetlabs.ru>
+ *
+ * $Id: tapi_eth.h 13866 2005-04-08 16:25:17Z mamadou $
+ */
+
+#ifndef __TE_TAPI_ETH_H__
+#define __TE_TAPI_ETH_H__
+
+#include <stdio.h>
+#if HAVE_ASSERT_H
+#include <assert.h>
+#endif
+#if HAVE_NETINET_IN_H
+#include <netinet/in.h>
+#endif
+#if HAVE_NETINET_ETHER_H
+#include <netinet/ether.h>
+#endif
+
+#include "te_stdint.h"
+#include "te_defs.h"
+#include "tad_common.h"
+#include "asn_usr.h"
+#include "ndn_pcap.h"
+
+
+/* Default recv mode: all except OUTGOING packets. */
+#define PCAP_RECV_MODE_DEF (PCAP_RECV_HOST      | PCAP_RECV_BROADCAST | \
+                            PCAP_RECV_MULTICAST | PCAP_RECV_OTHERHOST )
+
+#define PCAP_LINKTYPE_DEFAULT   DLT_EN10MB
+
+
+/**
+ * Create common Ethernet-PCAP CSAP.
+ *
+ * @param ta_name       Test Agent name
+ * @param sid           RCF session
+ * @param ifname        Interface name on TA host
+ * @param iftype        Interface datalink type (see man pcap)
+ * @param recv_mode     Receive mode, bit scale defined by elements of
+ *                      'enum pcap_csap_receive_mode' in ndn_pcap.h.
+ * @param pcap_csap     Identifier of created CSAP (OUT)
+ *
+ * @return Zero on success, otherwise standard or common TE error code.
+ */
+extern int tapi_pcap_csap_create(const char *ta_name, int sid,
+                                 const char *ifname, int iftype, int recv_mode,
+                                 csap_handle_t *pcap_csap);
+
+
+/**
+ * Callback function for the tapi_pcap_recv_start() routine, it is called
+ * for each packet received for csap.
+ *
+ * @param filter_id     Filter ID that corresponds to received packet.
+ * @param pkt_data      Received packet in binary form.
+ * @param pkt_len       Length of the received packet.
+ * @param userdata      Pointer to user data, provided by  the caller of
+ *                      tapi_pcap_recv_start.
+ */
+typedef void (*tapi_pcap_recv_callback) (const int filter_id,
+                                         const uint8_t *pkt_data,
+                                         const uint16_t pkt_len,
+                                         void *userdata);
+
+/**
+ * Start receive process on specified Ethernet-PCAP CSAP. If process
+ * was started correctly (rc is OK) it can be managed by common RCF
+ * methods 'rcf_ta_trrecv_wait', 'rcf_ta_trrecv_get' and
+ * 'rcf_ta_trrecv_stop'.
+ *
+ * @param ta_name       Test Agent name
+ * @param sid           RCF session
+ * @param pcap_csap      CSAP handle
+ * @param pattern       ASN value with receive pattern
+ * @param cb            Callback function which will be called for each
+ *                      received frame, may me NULL if frames are not need
+ * @param cb_data       Pointer to be passed to user callback
+ * @param timeout       Timeout for receiving of packets, measured in
+ *                      milliseconds
+ * @param num           Number of packets caller wants to receive
+ *
+ * @return Zero on success, otherwise standard or common TE error code.
+ */
+extern int tapi_pcap_recv_start(const char *ta_name, int sid,
+                                csap_handle_t pcap_csap,
+                                const asn_value *pattern,
+                                tapi_pcap_recv_callback cb, void *cb_data,
+                                unsigned int timeout, int num);
+
+/**
+ * Creates traffic    pattern for a single Ethernet-PCAP frame.
+ *
+ * @param filter      Tcpdump-like filtering rule
+ * @param filter_id   Value that should be responsed when packet
+ *                    match filtering rule 
+ * @param pattern     Placeholder for the pattern (OUT)
+ *
+ * @returns Status of the operation
+ */
+extern int tapi_pcap_pattern_add(const char *filter,
+                                 const int   filter_id,
+                                 asn_value **pattern);
+
+#endif /* __TE_TAPI_ETH_H__ */
