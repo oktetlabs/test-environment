acc0539db1452ed09489f5817c8d6aa4d248d0ab
Konstantin Abramenko <Konstantin.Abramenko@oktetlabs.ru>
2005-06-01 16:32:45 +0000

    more complete test
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@14890 9d14d920-75d5-0310-8bd7-a52572981aee

--- lib/asn/tests/parse01.c
+++ lib/asn/tests/parse01.c
@@ -3,9 +3,8 @@
 #include "asn_usr.h"
 #include "ndn.h"
 
+#include "test_types.h"
 
-extern int asn_impl_parse_text(const char*text, asn_type_p, 
-                        asn_value_p *parsed, int *parsed_syms);
 
 char buffer[1000];
 
@@ -15,23 +14,37 @@ main (void)
     int rc;
     int s_parsed;
     asn_value_p new_val; 
+    const char *str;
 
-    rc = asn_impl_parse_text("\"berberber\"", &asn_base_charstring,
+    rc = asn_parse_value_text("\"berb\\\"erber\"", asn_base_charstring,
                              &new_val, &s_parsed);
     printf ("ret code from parse: %6x, syms: %d\n", rc, s_parsed);
 
     asn_sprint_value(new_val, buffer, 1000, 0);
-    printf ("\nparsed value: \n--\n%s\n--\n", buffer); 
+    printf ("\1. nparsed value: \n--\n%s\n--\n", buffer); 
+
+    rc = asn_parse_value_text(buffer, asn_base_charstring,
+                             &new_val, &s_parsed);
+    printf ("ret code from parse again printed: %6x, syms: %d\n",
+            rc, s_parsed);
+
+    asn_sprint_value(new_val, buffer, 1000, 0);
+    printf ("\n2. parsed value: \n--\n%s\n--\n", buffer); 
+
+    str = NULL;
+    rc = asn_get_field_data(new_val, &str, NULL);
+
+    printf ("\n3. directly got string, rc = %x: \n--\n%s\n--\n", rc, str); 
 
 #if 0
-    rc = asn_impl_parse_text("14", &asn_base_integer, &new_val, &s_parsed);
+    rc = asn_parse_value_text("14", asn_base_integer, &new_val, &s_parsed);
     printf ("ret code from parse: %6x, syms: %d\n", rc, s_parsed);
 
     asn_sprint_value(new_val, buffer, 1000, 0);
     printf ("\nparsed value: \n--\n%s\n--\n", buffer); 
 
     
-    rc = asn_impl_parse_text("\"berbe\"rber\"", &asn_base_charstring, 
+    rc = asn_parse_value_text("\"berbe\"rber\"", asn_base_charstring, 
                                 &new_val, &s_parsed);
     printf ("ret code from parse: %6x, syms: %d\n", rc, s_parsed);
 
@@ -39,24 +52,24 @@ main (void)
     printf ("\nparsed value: \n--\n%s\n--\n", buffer); 
     asn_free_value (new_val);
 
-    rc = asn_impl_parse_text("{ number 16, string \"lalala\" }",
-                            &at_plain_seq1, &new_val, &s_parsed);
+    rc = asn_parse_value_text("{ number 16, string \"lalala\" }",
+                            at_plain_seq1, &new_val, &s_parsed);
     printf ("ret code from parse sequence: %6x, syms: %d\n", rc, s_parsed);
     asn_sprint_value(new_val, buffer, 1000, 0);
     printf ("\nparsed value: \n--\n%s\n--\n", buffer); 
     asn_free_value (new_val);
 
 
-    rc = asn_impl_parse_text("{ name \"uuu\" , array {1, 2, 35  , 55 } }",
-                            &at_named_int_array, &new_val, &s_parsed);
+    rc = asn_parse_value_text("{ name \"uuu\" , array {1, 2, 35  , 55 } }",
+                            at_named_int_array, &new_val, &s_parsed);
     printf ("ret code from parse sequence: %6x, syms: %d\n", rc, s_parsed);
     asn_sprint_value(new_val, buffer, 1000, 0);
     printf ("\nparsed value: \n--\n%s\n--\n", buffer); 
     asn_free_value (new_val); 
 
 
-    rc = asn_impl_parse_text("number:222",
-                            &at_plain_choice1, &new_val, &s_parsed);
+    rc = asn_parse_value_text("number:222",
+                            at_plain_choice1, &new_val, &s_parsed);
     printf ("ret code from parse sequence: %6x, syms: %d\n", rc, s_parsed);
     asn_sprint_value(new_val, buffer, 1000, 0);
     printf ("\nparsed value: \n--\n%s\n--\n", buffer); 
