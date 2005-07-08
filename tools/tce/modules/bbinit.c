d8b2b866bb77b7a8cd35dc85c542161194f5d1fc
Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
2005-07-08 11:19:01 +0000

    TCE is separated as a tool
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@15945 9d14d920-75d5-0310-8bd7-a52572981aee

---
+++ tools/tce/modules/bbinit.c
@@ -0,0 +1,42 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+int
+init_module(void)
+{
+    return 0;
+}
+
+void
+cleanup_module(void)
+{
+}
+
+void
+__bb_init_func(void *unused)
+{
+}
+
+#if 0
+#undef ntohs
+unsigned short int
+ntohs(unsigned short int x)
+{
+    return ((x & 0xff) << 8) | (x >> 8);
+}
+EXPORT_SYMBOL(ntohs);
+#undef htons
+unsigned short int
+htons(unsigned short int x)
+{
+    return ((x & 0xff) << 8) | (x >> 8);
+}
+EXPORT_SYMBOL(htons);
+#endif
+
+EXPORT_SYMBOL(__bb_init_func);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("some");
+MODULE_DESCRIPTION("nothing");
