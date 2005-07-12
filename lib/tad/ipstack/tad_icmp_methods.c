76394068a1fd7ac20a1c9215a06b5f32800b1e6e
Konstantin Abramenko <Konstantin.Abramenko@oktetlabs.ru>
2005-07-12 08:30:23 +0000

    new source file for ICMP errors generating added
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@16104 9d14d920-75d5-0310-8bd7-a52572981aee

---
+++ lib/tad/ipstack/tad_icmp_methods.c
@@ -0,0 +1,165 @@
+/** @file
+ * @brief Test Environment: 
+ *
+ * Traffic Application Domain
+ * ICMP messages generating routines
+ *
+ * Copyright (C) 2003 Test Environment authors (see file AUTHORS in the
+ * root directory of the distribution).
+ *
+ * Test Environment is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * Test Environment is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA  02111-1307  USA
+ *
+ * Author: Konstantin Abramenko <Konstantin.Abramenko@oktetlabs.ru>
+ *
+ * @(#) $Id$
+ */
+
+#include <string.h>
+#include <stdlib.h>
+
+#define TE_LGR_USER "TAD ICMP" 
+
+
+#include "tad_ipstack_impl.h"
+
+/* for ETH_ALEN */
+#include "ndn_eth.h"
+
+
+/**
+ * Function for make ICMP error by IP packet, catched by '*.ip4.eth'
+ * raw CSAP.
+ * Prototype made according with 'tad_processing_pkt_method' function type.
+ * This method uses write_cb callback of passed 'eth' CSAP for send reply.
+ * User parameter should contain two numbers, separated by colon: 
+ * "<type>:<code>". Not all  kinds of ICMP messages are supported. 
+ * After <code> may present some details paramter.
+ *
+ * @param csap_descr  CSAP descriptor structure.
+ * @param usr_param   String passed by user.
+ * @param orig_pkt    Packet binary data, as it was caught from net.
+ * @param pkt_len     Length of pkt data.
+ *
+ * @return zero on success or error code.
+ */
+int 
+tad_icmp_error(csap_p csap_descr, const char *usr_param, 
+               const uint8_t *orig_pkt, size_t pkt_len)
+{
+    uint8_t type, 
+            code;
+    int     rc = 0;
+    char   *endptr; 
+    size_t  msg_len;
+
+    uint8_t *msg, *p;
+    uint16_t mtu;
+
+    if (csap_descr == NULL || usr_param == NULL ||
+        orig_pkt == NULL || pkt_len == 0)
+        return ETEWRONGPTR;
+
+    if (csap_descr->prepare_send_cb != NULL && 
+        (rc = csap_descr->prepare_send_cb(csap_descr)) != 0)
+    {
+        ERROR("%s(): prepare for recv failed %x", rc);
+        return rc;
+    }
+
+    type = strtol(usr_param, &endptr, 10);
+    if ((endptr == NULL) || (*endptr != ':'))
+    {
+        ERROR("%s(): wrong usr_param, should be <type>:<code>",
+              __FUNCTION__);
+        return EINVAL;
+    }
+    endptr++;
+    code = strtol(endptr, &endptr, 10);
+
+    if (type != 3 || code != 4)
+    {
+        ERROR("%s(): only Dest.Unreachable/Fragm.Needed supported", 
+              __FUNCTION__);
+        return ETENOSUPP;
+    }
+
+    if ((endptr == NULL) || (*endptr != ':'))
+    {
+        ERROR("%s(): wrong usr_param, for 'Frag.Needed' should be MTU",
+              __FUNCTION__);
+        return EINVAL;
+    }
+    endptr++;
+    mtu = strtol(endptr, &endptr, 10);
+
+#define ICMP_PLD_SIZE 28
+    msg_len = 14 /* eth */ + 20 /* IP */ + 8 /* ICMP */
+            + ICMP_PLD_SIZE /* IP header + 8 bytes of orig pkt */;
+
+    p = msg = calloc(1, msg_len); 
+
+    /* Ethernet header */
+    memcpy(p, orig_pkt + ETH_ALEN, ETH_ALEN); 
+    memcpy(p + ETH_ALEN, orig_pkt, ETH_ALEN); 
+    memcpy(p + 2 * ETH_ALEN, orig_pkt + 2 * ETH_ALEN, 2); 
+    p += 14; orig_pkt += 14;
+
+    /* IP header, now leave orig_pkt unchanged */
+    memcpy(p, orig_pkt, 2); /* vers, hlen, tos */
+    p += 2;
+    *(uint16_t *)p = htons(msg_len - 14); /* ip len */
+    p += 2;
+    *(uint16_t *)p = (uint16_t)rand(); /* ip ident */
+    p += 2;
+    *(uint16_t *)p = 0; /* flags & offset */
+    p += 2;
+    *p++ = 64;
+    *p++ = IPPROTO_ICMP;
+    p += 2; /* leave place for header checksum */
+    memcpy(p, orig_pkt + 16, 4);
+    p += 4;
+    memcpy(p, orig_pkt + 12, 4);
+    p += 4;
+
+    /* set header checksum */
+    *(uint16_t *)(msg + 14 + 10) = ~calculate_checksum(msg + 14, 20);
+
+    /* ICMP header */
+
+    *p++ = type;
+    *p++ = code;
+    p += 2; /* leave place for ICMP checksum */
+    p += 2; /* unused */
+    *(uint16_t *)p = htons(mtu); 
+    p += 2; 
+
+    memcpy(p, orig_pkt, ICMP_PLD_SIZE);
+
+    /* set ICMP checksum */
+    *(uint16_t *)(msg + 14 + 20 + 2) = 
+        ~calculate_checksum(msg + 14 + 20, ICMP_PLD_SIZE + 8);
+
+
+    rc = csap_descr->write_cb(csap_descr, msg, msg_len);
+    RING("%s(): sent %d bytes", __FUNCTION__, rc);
+    if (rc < 0)
+    {
+        ERROR("%s() write error", __FUNCTION__);
+        return csap_descr->last_errno;
+    }
+
+    return 0;
+}
