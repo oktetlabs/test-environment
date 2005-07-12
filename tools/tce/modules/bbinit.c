9f04b6eafba52d63bd06339537bfaaa1633a8b3b
Artem Andreev <Artem.Andreev@oktetlabs.ru>
2005-07-12 09:55:10 +0000

    further work on gcc-3.4 support
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@16113 9d14d920-75d5-0310-8bd7-a52572981aee

--- tools/tce/modules/bbinit.c
+++ tools/tce/modules/bbinit.c
@@ -2,9 +2,44 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 
+#if __GNUC__ > 3 || \
+    (__GNUC__ == 3 && __GNUC_MINOR__ >= 4)
+#define GCC_IS_3_4P 1
+#else
+#undef GCC_IS_3_4P 
+#endif
+
+
+#ifdef GCC_IS_3_4P
+unsigned __gcov_version_magic = 0;
+#endif
+
+
 int
 init_module(void)
 {
+#ifdef GCC_IS_3_4P
+    if (__gcov_version_magic != 0)
+    {
+        unsigned char v[4];
+        unsigned ix;
+        char *ptr = __VERSION__;
+        unsigned major, minor = 0;
+        
+        major = simple_strtoul(ptr, &ptr, 10);
+        if (*ptr)
+            minor = simple_strtoul(ptr + 1, &ptr, 10);
+    
+        v[0] = (major < 10 ? '0' : 'A' - 10) + major;
+        v[1] = (minor / 10) + '0';
+        v[2] = (minor % 10) + '0';
+        v[3] = strchr(ptr, '(') != NULL ? '(' : '*';
+        
+        for (ix = 0; ix != 4; ix++)
+            __gcov_version_magic = (__gcov_version_magic << 8) | v[ix];
+    }
+
+#endif
     return 0;
 }
 
@@ -13,10 +48,19 @@ cleanup_module(void)
 {
 }
 
+#ifndef GCC_IS_3_4P
 void
 __bb_init_func(void *unused)
 {
 }
+
+#else
+
+void
+__gcov_init(void *unused)
+{
+}
+#endif
 
 #if 0
 #undef ntohs
@@ -35,7 +79,12 @@ htons(unsigned short int x)
 EXPORT_SYMBOL(htons);
 #endif
 
+#ifdef GCC_IS_3_4P
+EXPORT_SYMBOL(__gcov_init);
+EXPORT_SYMBOL(__gcov_version_magic);
+#else
 EXPORT_SYMBOL(__bb_init_func);
+#endif
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("some");
