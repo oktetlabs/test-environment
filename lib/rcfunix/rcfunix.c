f280675622e3c2628ffad99583a55de23e2906c2
Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
2005-07-25 10:32:24 +0000

    [Bug 481]:  FIXED Buddy interval is set to 10ms
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@16645 9d14d920-75d5-0310-8bd7-a52572981aee

--- lib/rcfunix/rcfunix.c
+++ lib/rcfunix/rcfunix.c
@@ -382,7 +382,7 @@ rcfunix_start(char *ta_name, char *ta_type, char *conf_str,
     {
         sprintf(cmd + strlen(cmd), "\"");
     }
-    sprintf(cmd + strlen(cmd), " 2>&1 | te_tee %s %s 100 >ta.%s ", 
+    sprintf(cmd + strlen(cmd), " 2>&1 | te_tee %s %s 10 >ta.%s ", 
             TE_LGR_ENTITY, ta->ta_name, ta->ta_name);
 
     /* Always run in background */
