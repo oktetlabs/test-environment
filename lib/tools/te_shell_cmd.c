18b1834c3547ecd99b1b8a4443c17b34db8f2ecd
Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
2005-08-27 13:14:02 +0000

    Implementation temporary moved in header to avoid issues with RCF UNIX
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@17650 9d14d920-75d5-0310-8bd7-a52572981aee

--- lib/tools/te_shell_cmd.c
+++ lib/tools/te_shell_cmd.c
@@ -28,6 +28,12 @@
  * $Id$
  */
 
+/*
+ * The function is moved to the header in order to avoid issues with
+ * rcfunix dynamic linkage.
+ */
+#if 0
+
 #define TE_LGR_USER     "TE Shell Cmd"
 
 #include "te_config.h"
@@ -42,87 +48,6 @@
 #include <errno.h>
 #endif
 
-//#include "te_defs.h"
 #include "logger_api.h"
 
-
-/* See description in te_shell_cmd.h */
-pid_t
-te_shell_cmd(const char *cmd, uid_t uid, int *in_fd, int *out_fd)
-{
-    int   pid;
-    int   in_pipe[2], out_pipe[2];
-
-    if (cmd == NULL)
-    {
-        errno = EINVAL;
-        return -1;
-    }
-    if (in_fd != NULL && pipe(in_pipe) != 0)
-        return -1;
-    if (out_fd != NULL && pipe(out_pipe) != 0)
-    {
-        if (in_fd != NULL)
-        {
-            close(in_pipe[0]);
-            close(in_pipe[1]);
-        }
-        return -1;
-    }
-
-    pid = fork();
-    if (pid == 0)
-    {
-        if (in_fd != 0)
-        {
-            close(in_pipe[1]);
-            if (in_pipe[0] != STDIN_FILENO)
-            {
-                close(STDIN_FILENO);
-                dup2(in_pipe[0], STDIN_FILENO);
-            }
-        }
-        if (out_fd != 0)
-        {
-            close(out_pipe[0]);
-            if (out_pipe[1] != STDOUT_FILENO)
-            {
-                close(STDOUT_FILENO);
-                dup2(out_pipe[1], STDOUT_FILENO);
-            }
-        }
-        if (uid != (uid_t)(-1) && setuid(uid) != 0)
-        {
-            ERROR("Failed to set user %d before runing command \"%s\"",
-                  uid, cmd);
-        }
-        execl("/bin/sh", "sh", "-c", cmd, (char *) 0);
-        return 0;
-    }
-
-    if (in_fd != NULL)
-        close(in_pipe[0]);
-    if (out_fd != NULL)
-        close(out_pipe[1]);
-    if (pid < 0)
-    {
-        if (in_fd != NULL)
-        {
-            close(in_pipe[1]);
-            *in_fd = -1;
-        }
-        if (out_fd != NULL)
-        {
-            close(out_pipe[0]);
-            *out_fd = -1;
-        }
-    }
-    else
-    {
-        if (in_fd != NULL)
-            *in_fd = in_pipe[1];
-        if (out_fd != NULL)
-            *out_fd = out_pipe[0];
-    }
-    return pid;
-}
+#endif
