df022bcf3905a30ddd3c8306a4e0c9b3cb6a39f7
Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
2005-09-16 14:49:44 +0000

    Prologue added
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@18445 9d14d920-75d5-0310-8bd7-a52572981aee

---
+++ suites/rpctest/prologue.c
@@ -0,0 +1,39 @@
+/** @file
+ * @brief RPC Test Suite
+ *
+ * Test Suite prologue.
+ *
+ * Copyright (C) 2005 OKTET Labs Ltd., St.-Petersburg, Russia
+ *
+ * @author Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
+ *
+ * $Id$
+ */
+
+/** Logging subsystem entity name */
+#define TE_TEST_NAME    "prologue"
+
+#include <stdlib.h>
+
+#include "tapi_test.h"
+#include "tapi_cfg_net.h"
+
+
+int
+main(void)
+{
+    int             result = EXIT_FAILURE;
+    te_errno        rc;
+
+    signal(SIGINT, sigint_handler);
+    te_lgr_entity = TE_TEST_NAME;
+    TAPI_ON_JMP(TEST_ON_JMP_DO);
+
+    CHECK_RC(tapi_cfg_net_all_assign_ip4());
+
+    TEST_SUCCESS;
+
+cleanup:
+
+    return result;
+}
