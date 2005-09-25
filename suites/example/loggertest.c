e4bb212c3f897921ff83e0f65431b991bbb9654a
Ivan Soloducha <Ivan.Soloducha@oktetlabs.ru>
2005-09-25 12:39:36 +0000

    Some more tested features for loggertest
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@18661 9d14d920-75d5-0310-8bd7-a52572981aee

--- suites/example/loggertest.c
+++ suites/example/loggertest.c
@@ -34,7 +34,7 @@
 
 #define TE_TEST_NAME  "loggertest"
 
-#include "sockapi-test.h"
+#include "tapi_test.h"
 #include "te_config.h"
 
 
@@ -47,13 +47,24 @@ main(int argc, char *argv[])
     
     TEST_START;
 
-    RING("Decimal integers with '=' length modifiers: %=1d %=2d %=4d %=8d", 11, 1111, 11111111, 1111111111111111);
-    RING("Decimal integer with invalid '=' length modifier: %=3d", 777);
+    RING("Integers with standard length modifiers: %hhd %hd %ld %lld", 11, 1111, 11111111, 1111111111111111);
+    RING("Integers with '=' length modifiers: %=1d %=2d 0x%=4x 0x%=8x", 11, 1111, 11111111, 1111111111111111);
+    RING("Integer with invalid length modifiers: %=3d %llld %hhhd", 77, 88, 99);
+    
+    RING("Integers with '%%j' and '%%t' modifiers: %jd %td", 111, 222);
+    RING("Integers with flags: %+0d, %'0d, %-'d, %#+d", 111, 222, 333, 444);
+    RING("Formatted integers: %10.5d, %5.10d", 111111111111, 222222222222);
     
     RING("Message with string parameter: (%s)", "String parameter");
     RING("Message with string parameter containing percent: (%s)", "%tring wi% per%ent%");
     
     RING("Memory dump: location %p, length %d\n%Tm", s, sizeof(s), s, sizeof(s));
+
+    RING("Error message: %r", RPC_EINVAL);
+
+    RING("Invalid specifier starting with 'T': %Te");
+    
+    RING("Specifier after double percent is not a specifier: %%d");
     
     RING("File dump: existing file %Tf", "test.txt");
     RING("File dump: non-existing file %Tf", "test2.txt");
