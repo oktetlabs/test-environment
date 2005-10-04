de8b7d40f67d08d4c08a33fc7071fa5346e84f7c
Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
2005-10-04 12:46:42 +0000

    Use interfaces provided by vund model
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@18952 9d14d920-75d5-0310-8bd7-a52572981aee

--- lib/tapi/tapi_cfg_vtund.c
+++ lib/tapi/tapi_cfg_vtund.c
@@ -55,40 +55,122 @@ tapi_cfg_vtund_create_tunnel(const char            *ta_srv,
                              cfg_handle            *ta_srv_if,
                              cfg_handle            *ta_clnt_if)
 {
-    te_errno    rc;
-    uint16_t    srv_port;
+    te_errno        rc;
+    uint16_t        srv_port;
+    cfg_handle      tmp;
+    cfg_val_type    val_type;
+    char           *str;
 
     srv_port = ntohs(sockaddr_get_port(srv_addr));
     if (srv_port == 0)
         srv_port = TAPI_CFG_VTUND_PORT_DEF;
 
-    cfg_add_instance_fmt(NULL, CFG_VAL(INTEGER, 0),
-                         "/agent:%s/vtund:/server:%u",
-                         ta_srv, srv_port);
+    /* Configure tunnel */
 
-    cfg_add_instance_fmt(NULL, CFG_VAL(NONE, 0),
-                         "/agent:%s/vtund:/server:%u/session:%s-%s",
-                         ta_srv, srv_port, ta_srv, ta_clnt);
+    rc = cfg_add_instance_fmt(NULL, CFG_VAL(INTEGER, 0),
+                              "/agent:%s/vtund:/server:%u",
+                              ta_srv, srv_port);
+    if (rc != 0)
+        return rc;
 
-    cfg_add_instance_fmt(NULL, CFG_VAL(INTEGER, 0),
-                         "/agent:%s/vtund:/client:%s-%s",
-                         ta_clnt, ta_srv, ta_clnt);
-    cfg_set_instance_fmt(CFG_VAL(ADDRESS, srv_addr),
-                         "/agent:%s/vtund:/client:%s-%s/server:",
-                         ta_clnt, ta_srv, ta_clnt);
-    cfg_set_instance_fmt(CFG_VAL(INTEGER, srv_port),
-                         "/agent:%s/vtund:/client:%s-%s/port:",
-                         ta_clnt, ta_srv, ta_clnt);
+    rc = cfg_add_instance_fmt(NULL, CFG_VAL(NONE, 0),
+                              "/agent:%s/vtund:/server:%u/session:%s-%s",
+                              ta_srv, srv_port, ta_srv, ta_clnt);
+    if (rc != 0)
+        return rc;
 
-    cfg_set_instance_fmt(CFG_VAL(INTEGER, 1),
-                         "/agent:%s/vtund:/server:%u",
-                         ta_srv, srv_port);
-    cfg_set_instance_fmt(CFG_VAL(INTEGER, 1),
-                         "/agent:%s/vtund:/client:%s-%s",
-                         ta_clnt, ta_srv, ta_clnt);
+    rc = cfg_add_instance_fmt(NULL, CFG_VAL(INTEGER, 0),
+                              "/agent:%s/vtund:/client:%s-%s",
+                              ta_clnt, ta_srv, ta_clnt);
+    if (rc != 0)
+        return rc;
+    
+    rc = cfg_set_instance_fmt(CFG_VAL(ADDRESS, srv_addr),
+                              "/agent:%s/vtund:/client:%s-%s/server:",
+                              ta_clnt, ta_srv, ta_clnt);
+    if (rc != 0)
+        return rc;
 
-    fprintf(stderr, "Sleeping...\n"); fflush(stderr);
-    sleep(30);
+    rc = cfg_set_instance_fmt(CFG_VAL(INTEGER, srv_port),
+                              "/agent:%s/vtund:/client:%s-%s/port:",
+                              ta_clnt, ta_srv, ta_clnt);
+    if (rc != 0)
+        return rc;
 
-    return rc;
+    rc = cfg_set_instance_fmt(CFG_VAL(INTEGER, 1),
+                              "/agent:%s/vtund:/server:%u",
+                              ta_srv, srv_port);
+    if (rc != 0)
+        return rc;
+
+    rc = cfg_set_instance_fmt(CFG_VAL(INTEGER, 1),
+                              "/agent:%s/vtund:/client:%s-%s",
+                              ta_clnt, ta_srv, ta_clnt);
+    if (rc != 0)
+        return rc;
+
+    SLEEP(10);
+
+    /* 
+     * Synchronize configuration trees and get assigned interfaces
+     */
+    
+    rc = cfg_synchronize_fmt(TRUE, "/agent:%s", ta_srv);
+    if (rc != 0)
+        return rc;
+    
+    val_type = CVT_STRING;
+    rc = cfg_get_instance_fmt(&val_type, &str,
+             "/agent:%s/vtund:/server:%u/session:%s-%s/interface:",
+             ta_srv, srv_port, ta_srv, ta_clnt);
+    if (rc != 0)
+    {
+        ERROR("Failed to get name of the network interface created "
+              "by the tunnel on server side: %r", rc);
+        return rc;
+    }
+    
+    rc = cfg_find_fmt(&tmp, "/agent:%s/interface:%s", ta_srv, str);
+    if (rc != 0)
+    {
+        ERROR("Failed to find interface '%s' on TA '%s': %r",
+              str, ta_srv, rc);
+        free(str);
+        return rc;
+    }
+    free(str);
+
+    if (ta_srv_if != NULL)
+        *ta_srv_if = tmp;
+
+
+    rc = cfg_synchronize_fmt(TRUE, "/agent:%s", ta_clnt);
+    if (rc != 0)
+        return rc;
+    
+    val_type = CVT_STRING;
+    rc = cfg_get_instance_fmt(&val_type, &str,
+             "/agent:%s/vtund:/client:%s-%s/interface:",
+             ta_clnt, ta_srv, ta_clnt);
+    if (rc != 0)
+    {
+        ERROR("Failed to get name of the network interface created "
+              "by the tunnel on client side: %r", rc);
+        return rc;
+    }
+    
+    rc = cfg_find_fmt(&tmp, "/agent:%s/interface:%s", ta_clnt, str);
+    if (rc != 0)
+    {
+        ERROR("Failed to find interface '%s' on TA '%s': %r",
+              str, ta_clnt, rc);
+        free(str);
+        return rc;
+    }
+    free(str);
+    
+    if (ta_clnt_if != NULL)
+        *ta_clnt_if = tmp;
+
+    return 0;
 }
