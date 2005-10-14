78fdfb272af40f75ade1d3231da8685123183faf
Elena Vengerova <Elena.Vengerova@oktetlabs.ru>
2005-10-14 15:22:04 +0000

    L5 copyright is added
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@19495 9d14d920-75d5-0310-8bd7-a52572981aee

--- lib/tapi_rpc/tapi_rpc_aio.h
+++ lib/tapi_rpc/tapi_rpc_aio.h
@@ -6,6 +6,7 @@
  *
  * Copyright (C) 2005 Test Environment authors (see file AUTHORS in
  * the root directory of the distribution).
+ * Copyright (c) 2005 Level5 Networks Corp.
  *
  * Test Environment is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
