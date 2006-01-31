b74fce11ab291f633c6060188f687244b583f098
Elena Vengerova <Elena.Vengerova@oktetlabs.ru>
2006-01-31 15:56:56 +0000

    Windows-specific definitions are added
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@23539 9d14d920-75d5-0310-8bd7-a52572981aee

---
+++ include/te_win_defs.h
@@ -0,0 +1,57 @@
+/** @file
+ * @brief Windows support
+ *
+ * Definitions missed in native Windows.
+ *
+ * This file should be used only on pre-processing stage.
+ *
+ * Copyright (C) 2006 Test Environment authors (see file AUTHORS
+ * in the root directory of the distribution).
+ * Copyright (c) 2006 Level5 Networks Corp.
+ *
+ * Test Environment is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * Test Environment is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA  02111-1307  USA
+ *
+ *
+ * @author Elena A. Vengerova <Elena.Vengerova@oktetlabs.ru>
+ *
+ * $Id$
+ */
+
+
+#ifndef __TE_WIN_DEFS_H__
+#define __TE_WIN__DEFS_H__
+
+typedef INT8   int8_t;
+typedef INT16  int16_t;
+typedef INT32  int32_t;
+typedef INT64  int64_t;
+typedef UINT8  uint8_t;
+typedef UINT16 uint16_t;
+typedef UINT32 uint32_t;
+typedef UINT64 uint64_t;
+
+typedef int ssize_t;
+typedef int pid_t;
+
+#define inline
+#define __const
+#undef __stdcall 
+
+#define snprintf                _snprintf
+#define vsnprintf               _vsnprintf
+#define va_copy(_dest, _src)    _dest = _src;
+
+#endif
