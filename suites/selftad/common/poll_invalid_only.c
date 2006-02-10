24c9936f421b5db5718067dc7e6e7e87620efed9
Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
2006-02-10 16:32:47 +0000

    Make for generic test on poll
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@23895 9d14d920-75d5-0310-8bd7-a52572981aee

--- suites/selftad/common/poll_invalid_only.c
+++ suites/selftad/common/poll_invalid_only.c
@@ -24,17 +24,18 @@
  * $Id: example.c 18619 2005-09-22 16:47:08Z artem $
  */
 
-/** @page common-poll_one_invalid Call traffic poll operation for one invalid CSAP
+/** @page common-poll_invalid_only Call traffic poll operation for invalid only CSAPs
  *
- * @objective Check @b rcf_trpoll() behaviour with one invalid CSAP.
+ * @objective Check @b rcf_trpoll() behaviour with only invalid CSAP.
  *
+ * @param n_csaps       Number of CSAPs
  * @param ta_null       @c NULL Test Agent name or not
  * @param zero_timeout  Zero timeout or not
  * @param zero_status   Initialize status as zero or not
  *
  * @par Scenario:
  * 
- * -# Prepare @b rcf_trpoll_csap structure with one entry:
+ * -# Prepare @b rcf_trpoll_csap structure with @a n_csaps entries:
  *      - @a ta is @c NULL or @c not-NULL (unknown TA);
  *      - @a csap is @c CSAP_INVALID_HANDLE;
  *      - @a status is @c 0 or @c -1.
@@ -45,7 +46,7 @@
  * @author Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
  */
 
-#define TE_TEST_NAME    "common/poll_one_invalid"
+#define TE_TEST_NAME    "common/poll_invalid_only"
 
 #include "te_config.h"
 
@@ -55,35 +56,52 @@
 int
 main(int argc, char *argv[])
 {
-    rcf_trpoll_csap     csapd;
+    rcf_trpoll_csap    *csaps = NULL;
+    unsigned int        n_csaps;
     te_bool             ta_null;
     te_bool             zero_timeout;
     te_bool             zero_status;
+    unsigned int        i;
 
     TEST_START;
+    TEST_GET_INT_PARAM(n_csaps);
     TEST_GET_BOOL_PARAM(ta_null);
     TEST_GET_BOOL_PARAM(zero_timeout);
     TEST_GET_BOOL_PARAM(zero_status);
 
-    csapd.ta = ta_null ? NULL : "UnknownTA";
-    csapd.csap_id = CSAP_INVALID_HANDLE;
-    csapd.status = zero_status ? 0 : -1;
+    if (n_csaps == 0)
+        TEST_FAIL("Invalid number of CSAPs as parameter");
 
-    rc = rcf_trpoll(&csapd, 1, zero_timeout ? 0 : rand_range(1, 1000));
+    CHECK_NOT_NULL(csaps = calloc(n_csaps, sizeof(*csaps)));
+    
+    for (i = 0; i < n_csaps; ++i)
+    {
+        csaps[i].ta = ta_null ? NULL : "UnknownTA";
+        csaps[i].csap_id = CSAP_INVALID_HANDLE;
+        csaps[i].status = zero_status ? 0 : -1;
+    }
+
+    rc = rcf_trpoll(csaps, n_csaps, zero_timeout ? 0 : rand_range(1, 1000));
     if (rc != 0)
     {
         TEST_FAIL("rcf_trpoll() with CSAP_INVALID_HANDLE failed: %r",
                   rc);
     }
-    if (TE_RC_GET_ERROR(csapd.status) != TE_ETADCSAPNOTEX)
+    for (i = 0; i < n_csaps; ++i)
     {
-        TEST_FAIL("rcf_trpoll() with CSAP_INVALID_HANDLE set status "
-                  "to %r instead of %r", csapd.status, TE_ETADCSAPNOTEX);
+        if (TE_RC_GET_ERROR(csaps[i].status) != TE_ETADCSAPNOTEX)
+        {
+            TEST_FAIL("rcf_trpoll() with CSAP_INVALID_HANDLE in #%u "
+                      "request set status to %r instead of %r",
+                      i, csaps[i].status, TE_ETADCSAPNOTEX);
+        }
     }
 
     TEST_SUCCESS;
 
 cleanup:
+
+    free(csaps);
 
     TEST_END;
 }
