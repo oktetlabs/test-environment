d6d33c00e8e99885775b6a13b58f4c9fc98ed87a
Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
2006-04-18 05:46:35 +0000

    Fix warning
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@26661 9d14d920-75d5-0310-8bd7-a52572981aee

--- agents/unix/main.c
+++ agents/unix/main.c
@@ -590,7 +590,7 @@ rcf_ch_start_process(pid_t *pid,
             execlp(rtn, rtn, 
                    params[0], params[1], params[2], params[3], params[4], 
                    params[5], params[6], params[7], params[8], params[9],
-                   NULL);
+                   (const char *)NULL);
             exit(0);
         }
         else if (*pid < 0)
