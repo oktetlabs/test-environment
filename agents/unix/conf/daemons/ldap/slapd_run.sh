68aba17ba1d1610bdc2bf71a9b873c9b5f9f148a
Alexandra Kossovsky <Alexandra.Kossovsky@oktetlabs.ru>
2006-06-07 11:21:42 +0000

    [Bug 791]:  fixed Support both Debian and RH in slapd configuration.
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@28928 9d14d920-75d5-0310-8bd7-a52572981aee

--- agents/unix/conf/daemons/ldap/slapd_run.sh
+++ agents/unix/conf/daemons/ldap/slapd_run.sh
@@ -11,17 +11,38 @@ LDIF=$2
 
 SLAPD_DIR=/tmp/te_ldap_$PORT
 
+if [ -d /etc/ldap/schema ]; then
+    # Debian case
+    SCHEMA_DIR=/etc/ldap/schema
+elif [ -d /etc/openldap/schema ]; then
+    # RedHat case
+    SCHEMA_DIR=/etc/openldap/schema
+    REMOVE_MODULES=1
+else
+    echo "Failed to find directory with LDAP schemas."
+    exit 1
+fi
+
 failure()
 {
-    rm -rf $SLAPD_DIR
+    echo "$@"
+#    rm -rf $SLAPD_DIR
     exit 1
 }
 
 rm -rf $SLAPD_DIR || exit 1
 mkdir $SLAPD_DIR || exit 1
-cat `dirname $0`/slapd.conf | sed 's/te_ldap/'te_ldap_$PORT'/g' > \
-    $SLAPD_DIR/slapd.conf || failure
-echo $2 > $SLAPD_DIR/ldif || failure
-/usr/sbin/slapadd -f $SLAPD_DIR/slapd.conf -l $LDIF || failure
-/usr/sbin/slapd -4 -f $SLAPD_DIR/slapd.conf -h ldap://0.0.0.0:$PORT || failure
+sed -e "s/te_ldap/te_ldap_$PORT/g" -e "s?%SCHEMA_DIR%?$SCHEMA_DIR?" \
+    <`dirname $0`/slapd.conf > $SLAPD_DIR/slapd.conf \
+    || failure "Failed to edit slapd.conf"
+if [ ! -z $REMOVE_MODULES ]; then
+    echo sed -i -e 's/moduleload.*//' $SLAPD_DIR/slapd.conf
+    sed -i -e 's/moduleload.*//' $SLAPD_DIR/slapd.conf \
+        || failure "Failed to remove module lines from slapd.conf"
+fi
+echo $2 > $SLAPD_DIR/ldif || failure "Failed to create ldif file"
+/usr/sbin/slapadd -f $SLAPD_DIR/slapd.conf -l $LDIF || \
+    failure "slapadd failed"
+/usr/sbin/slapd -4 -f $SLAPD_DIR/slapd.conf -h ldap://0.0.0.0:$PORT || \
+    failure "slapd start failed"
 exit 0
