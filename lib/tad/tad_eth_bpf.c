8dd2e36884137ef1faeaf3e77f3ce7c5df89e1f2
Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
2006-06-28 07:18:38 +0000

    Added in repository
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@29666 9d14d920-75d5-0310-8bd7-a52572981aee

---
+++ lib/tad/tad_eth_bpf.c
@@ -0,0 +1,35 @@
+/** @file
+ * @brief TAD Data Link Provider Interface
+ *
+ * Implementation routines to access media through BPF (Berkley Packet
+ * Filter).
+ *
+ * Copyright (C) 2006 Test Environment authors (see file AUTHORS
+ * in the root directory of the distribution).
+ *
+ * Test Environment is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * Test Environment is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA  02111-1307  USA
+ *
+ * @author Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
+ *
+ * $Id$
+ */
+
+#define TE_LGR_USER     "TAD BPF"
+
+#include "te_config.h"
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
