bcdea152a8cad1144bb4a9b51675617a5bd073e0
Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
2006-09-01 09:08:19 +0000

    Rename binary TRC tools. Include "TRC Options" in filtered log.
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@31198 9d14d920-75d5-0310-8bd7-a52572981aee

--- tools/trc/te_trc.sh
+++ tools/trc/te_trc.sh
@@ -1,4 +1,31 @@
-#!/bin/sh
+#! /bin/bash
+#
+# Test Environment: Testing Results Comparator
+#
+# Script to process TE raw log and run TRC report generator.
+#
+# Copyright (C) 2004-2006 Test Environment authors (see file AUTHORS
+# in the root directory of the distribution).
+#
+# Test Environment is free software; you can redistribute it and/or 
+# modify it under the terms of the GNU General Public License as 
+# published by the Free Software Foundation; either version 2 of 
+# the License, or (at your option) any later version.
+# 
+# Test Environment is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+# MA  02111-1307  USA
+#
+# Author Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
+#
+# $Id:$
+#
 
 OPTS=""
 while test "${1#-}" != "$1" ; do
@@ -12,28 +39,29 @@ if test -z "$1" -o -n "$2" ; then
     exit 1
 fi
 
-RAW_LOG_FILE=$1
+RAW_LOG_FILE="$1"
 
-RGT_FILTER=`mktemp /tmp/tmp.XXXXXX`
-cat <<EOF >${RGT_FILTER}
+RGT_FILTER="$(mktemp /tmp/tmp.XXXXXX)"
+cat <<EOF >"$RGT_FILTER"
 <?xml version="1.0"?>
 <filters>
   <entity-filter>
     <exclude entity=""/>
     <include entity="">
+        <user name="TRC Options"/>
         <user name="Control"/>
     </include>
   </entity-filter>
 </filters>
 EOF
 
-XML_LOG_FILE=`mktemp /tmp/tmp.XXXXXX`
+XML_LOG_FILE=$(mktemp /tmp/tmp.XXXXXX)
 
-rgt-conv --incomplete-log -m postponed -c $RGT_FILTER -f $RAW_LOG_FILE \
-         -o $XML_LOG_FILE
+rgt-conv --incomplete-log -m postponed -c "$RGT_FILTER" \
+         -f "$RAW_LOG_FILE" -o "$XML_LOG_FILE"
 if test $? -eq 0 ; then
-    te_trc ${OPTS} $XML_LOG_FILE
-    rm $XML_LOG_FILE
+    te-trc-report ${OPTS} "$XML_LOG_FILE"
+    rm "$XML_LOG_FILE"
 fi
 
-rm $RGT_FILTER
+rm "$RGT_FILTER"
