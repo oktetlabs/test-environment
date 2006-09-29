6ba17b8bfb0681c417ea1421c6e7ae14c2b82783
Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
2006-09-29 14:04:55 +0000

    Add TE_TAD_BPF
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@32018 9d14d920-75d5-0310-8bd7-a52572981aee

--- include/te_errno.h
+++ include/te_errno.h
@@ -334,6 +334,7 @@ typedef enum {
     TE_TAD_PF_PACKET,   /**< TAD Ethernet over PF_PACKET sockets */
     TE_TAD_DLPI,        /**< TAD DLPI support */
     TE_TOOL_EXT,        /**< External tools */
+    TE_TAD_BPF,         /**< TAD Ethernet using BPF */
 } te_module;                             
 /*@}*/
 
@@ -404,6 +405,7 @@ te_rc_mod2str(te_errno err)
         MOD2STR(TAD_BPS);
         MOD2STR(TAD_PF_PACKET);
         MOD2STR(TAD_DLPI);
+        MOD2STR(TAD_BPF);
         MOD2STR(TARPC);
         MOD2STR(LOGGER);
         MOD2STR(CS);
