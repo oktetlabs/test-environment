fcc9381691b9bfe02ff497878dc88a4d6ba12b5d
Alexandra Kossovsky <Alexandra.Kossovsky@oktetlabs.ru>
2006-10-20 06:53:32 +0000

    More cleaups in ethtool support
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@32663 9d14d920-75d5-0310-8bd7-a52572981aee

--- include/te_ethtool.h
+++ include/te_ethtool.h
@@ -1,12 +1,11 @@
 #if HAVE_LINUX_ETHTOOL_H
-#include "te_stdint.h"
+#include <asm/types.h> /* For >= 2.6.18 */
+
+#include "te_stdint.h" /* For <= 2.6.17 */
 typedef uint64_t u64;
 typedef uint32_t u32;
 typedef uint16_t u16;
 typedef uint8_t  u8;
-typedef uint64_t __u64;
-typedef uint32_t __u32;
-typedef uint16_t __u16;
-typedef uint8_t  __u8;
+
 #include <linux/ethtool.h>
 #endif
