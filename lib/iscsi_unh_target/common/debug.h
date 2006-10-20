f88f0a7888906d3d33342539068f20871591aa3c
Artem Andreev <Artem.Andreev@oktetlabs.ru>
2006-10-20 11:09:25 +0000

    reverted to standard TE logging
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@32672 9d14d920-75d5-0310-8bd7-a52572981aee

--- lib/iscsi_unh_target/common/debug.h
+++ lib/iscsi_unh_target/common/debug.h
@@ -72,8 +72,7 @@ extern te_bool iscsi_check_verbose(int level);
 
 #define TRACE(level, args...) do {                          \
         if (iscsi_check_verbose(ISCSI_VERBOSITY_ ## level)) \
-        /*    RING(args);    */                              \
-        { fprintf(stderr, "%d: ", ISCSI_VERBOSITY_ ## level); fprintf(stderr, args); fputc('\n', stderr); } \
+             RING(args);                                    \
     } while(0)
 
 #define TRACE_BUFFER(level, buffer, len, args...)               \
@@ -85,15 +84,12 @@ extern te_bool iscsi_check_verbose(int level);
 
 #define TRACE_ERROR(args...) do {                           \
         if (iscsi_check_verbose(ISCSI_VERBOSITY_MINIMAL))   \
-        /*  ERROR(args); */                                   \
-        { fputs("ERROR: ", stderr); \
-          fprintf(stderr, args); fputc('\n', stderr); } \
+            ERROR(args);                                    \
     } while(0)
 
 #define TRACE_WARNING(args...) do {                           \
         if (iscsi_check_verbose(ISCSI_VERBOSITY_MINIMAL))   \
-        /*    WARN(args); */                                    \
-        { fputs("WARNING: ", stderr); fprintf(stderr, args); fputc('\n', stderr); } \
+             WARN(args);                                    \
     } while(0)
 
 
