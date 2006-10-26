ec699e7e4f9ca5736487cd5a924705336513e5e4
Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
2006-10-26 07:09:13 +0000

    Add extremely usefull functions to create connection in TE
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@32804 9d14d920-75d5-0310-8bd7-a52572981aee

---
+++ lib/tapi_rpc/client_server.c
@@ -0,0 +1,372 @@
+/** @file
+ * @brief Test API - RPC to create client-server connections
+ *
+ * Routines to create client-server connections.
+ *
+ * Actually we can't say that in @c SOCK_DGRAM socket type there are
+ * clients and servers, but we will call them so in case each socket
+ * is connected to its peer, so that send() and recv() operations lead
+ * to sending and receiving data to/from the particular remote peer.
+ *
+ *
+ * Copyright (C) 2004-2006 Test Environment authors (see file AUTHORS
+ * in the root directory of the distribution).
+ * Copyright (c) 2004-2006 Level5 Networks Corp.
+ *
+ * Test Environment is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * Test Environment is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA  02111-1307  USA
+ *
+ *
+ * @author Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
+ * @author Oleg Kravtsov <Oleg.Kravtsov@oktetlabs.ru>
+ *
+ * $Id: tapi_rpc.h 22478 2006-01-06 11:58:27Z arybchik $
+ */
+
+/** User name of the library to be used for logging */
+#define TE_LGR_USER "Generic Connection LIB"
+
+#include "te_config.h"
+
+#if HAVE_SYS_TYPES_H
+#include <sys/types.h>
+#endif
+#if HAVE_STRING_H
+#include <string.h>
+#endif
+#if HAVE_SYS_SOCKET_H
+#include <sys/socket.h>
+#endif
+#if HAVE_NETINET_IN_H
+#include <netinet/in.h>
+#endif
+
+#include "logger_api.h"
+#include "te_sockaddr.h"
+#include "tapi_rpc_socket.h"
+#include "tapi_rpc_unistd.h"
+#include "tapi_rpc_client_server.h"
+#include "tapi_rpcsock_macros.h"
+
+#include "tapi_test.h"
+
+
+/** Default backlog of the TCP server */
+#define TAPI_RPC_CLIENT_SERVER_BACKLOG_DEF  1
+
+
+/* See the description in tapi_rpc_client_server.h */
+int
+rpc_create_and_bind_socket(rcf_rpc_server    *rpc,
+                           rpc_socket_type    sock_type,
+                           rpc_socket_proto   proto,
+                           te_bool            wild,
+                           te_bool            set_reuse_addr,
+                           struct sockaddr   *addr)
+{
+    struct sockaddr_storage bind_addr;
+    int                     sockd;
+    int                     result = EXIT_SUCCESS;
+
+    /**
+     * Pointer to the place where the system returns the port socket
+     * is bound to
+     */
+    in_port_t *port_sys = NULL;
+    /** Pointer to the location of port value in user buffer */
+    in_port_t *port_usr = NULL;
+
+
+    if (addr == NULL)
+    {
+        ERROR("%s(): it is not allowed to pass NULL server address",
+               __FUNCTION__);
+        return -1;
+    }
+    if (sizeof(bind_addr) < te_sockaddr_get_size(addr))
+    {
+        ERROR("%s(): it is not support address structures with "
+              "length more than %d", __FUNCTION__, sizeof(bind_addr));
+        return -1;
+    }
+    memcpy(&bind_addr, addr, te_sockaddr_get_size(addr));
+
+    switch (bind_addr.ss_family)
+    {
+        case AF_INET:
+            if (SIN(addr)->sin_addr.s_addr == htonl(INADDR_ANY))
+            {
+                ERROR("%s(): it is not allowed to pass wildcard IPv4 address",
+                      __FUNCTION__);
+                return -1;
+            }
+            if (wild)
+            {
+                SIN(&bind_addr)->sin_addr.s_addr = htonl(INADDR_ANY);
+            }
+            if (SIN(&bind_addr)->sin_port == 0)
+            {
+                port_sys = &(SIN(&bind_addr)->sin_port);
+                port_usr = &(SIN(addr)->sin_port);
+            }
+            break;
+
+        case AF_INET6:
+            if (memcmp(&(SIN6(addr)->sin6_addr),
+                       &in6addr_any, sizeof(in6addr_any)) == 0)
+            {
+                ERROR("%s(): it is not allowed to pass wildcard IPv6 address",
+                      __FUNCTION__);
+                return -1;
+            }
+            if (wild)
+            {
+                memcpy(&(SIN6(&bind_addr)->sin6_addr),
+                       &in6addr_any, sizeof(in6addr_any));
+            }
+            if (SIN6(&bind_addr)->sin6_port == 0)
+            {
+                port_sys = &(SIN6(&bind_addr)->sin6_port);
+                port_usr = &(SIN6(addr)->sin6_port);
+            }
+            break;
+
+        default:
+            ERROR("%s(): Address family %d is not supported yet",
+                  __FUNCTION__, bind_addr.ss_family);
+            return -1;
+    }
+
+    sockd = rpc_socket(rpc, rpc_socket_domain_by_addr(addr), 
+                       sock_type, proto);
+    if (set_reuse_addr)
+    {
+        int on = TRUE;
+
+        rpc_setsockopt(rpc, sockd, RPC_SO_REUSEADDR, &on);
+    }
+    
+    rpc_bind(rpc, sockd, SA(&bind_addr));
+
+    if (port_usr != NULL)
+    {
+        socklen_t addrlen = sizeof(bind_addr);
+
+        /* Determine assigned port */
+        rpc_getsockname(rpc, sockd, SA(&bind_addr), &addrlen);
+        memcpy(port_usr, port_sys, sizeof(*port_usr));
+    }
+
+    if (result != EXIT_SUCCESS)
+    {
+        CLEANUP_RPC_CLOSE(rpc, sockd);
+        sockd = -1;
+    }
+    
+    return sockd;
+}
+
+/* See the description in tapi_rpc_client_server.h */
+int 
+rpc_stream_server(rcf_rpc_server *srvr,
+                  rpc_socket_proto proto, te_bool srvr_wild, 
+                  struct sockaddr *srvr_addr)
+{
+    int sockd = -1;
+    
+    sockd = rpc_create_and_bind_socket(srvr,
+                                       RPC_SOCK_STREAM, proto, srvr_wild,
+                                       FALSE /* Do not set SO_REUSEADDR */,
+                                       srvr_addr);
+    if (sockd < 0)
+    {
+        ERROR("Cannot create server socket of type SOCK_STREAM");
+    }
+    else
+    {
+        rpc_listen(srvr, sockd, TAPI_RPC_CLIENT_SERVER_BACKLOG_DEF);
+    }
+
+    return sockd;
+}
+
+/* See the description in tapi_rpc_client_server.h */
+int
+rpc_stream_client(rcf_rpc_server *clnt,
+                  rpc_socket_domain domain, rpc_socket_proto proto,
+                  const struct sockaddr *clnt_addr)
+{
+    int sockd = -1;
+    
+    sockd = rpc_socket(clnt, domain, RPC_SOCK_STREAM, proto);
+
+    if (clnt_addr != NULL)
+    {
+         rpc_bind(clnt, sockd, clnt_addr);
+    }
+
+    return sockd;
+}
+
+/* See the description in tapi_rpc_client_server.h */
+int
+rpc_stream_connection(rcf_rpc_server *srvr, rcf_rpc_server *clnt,
+                      rpc_socket_proto proto,
+                      const struct sockaddr *srvr_addr,
+                      const struct sockaddr *clnt_addr,
+                      int *srvr_s, int *clnt_s)
+{
+    int result        = EXIT_SUCCESS;
+    int srvr_sock     = -1;
+    int clnt_sock     = -1;
+    int accepted_sock = -1;
+    
+    if ((srvr_sock = rpc_stream_server(srvr, 
+                                       proto, FALSE,
+                                       SA(srvr_addr) /* FIXME */)) < 0)
+    {
+        ERROR("%s(): Cannot create server socket of type SOCK_STREAM",
+              __FUNCTION__);
+        return -1;
+    }
+
+    if ((clnt_sock = rpc_stream_client(clnt,
+                         rpc_socket_domain_by_addr(clnt_addr),
+                         proto, clnt_addr)) < 0)
+    {
+        ERROR("%s(): Cannot create clent socket of type SOCK_STREAM",
+              __FUNCTION__);
+        result = EXIT_FAILURE;
+        goto cleanup;
+    }
+    
+    rpc_connect(clnt, clnt_sock, srvr_addr);
+    accepted_sock = rpc_accept(srvr, srvr_sock, NULL, NULL);
+    
+cleanup:
+    /*
+     * We should close server socket anyway because we've already had a
+     * connection "clnt_sock" <-> "accepted_sock"
+     */
+    CLEANUP_RPC_CLOSE(srvr, srvr_sock);
+
+    if (result == EXIT_FAILURE)
+    {
+        CLEANUP_RPC_CLOSE(srvr, accepted_sock);
+        CLEANUP_RPC_CLOSE(clnt, clnt_sock);
+    }
+    else
+    {
+        *srvr_s = accepted_sock;
+        *clnt_s = clnt_sock;
+    }
+
+    return ((result == EXIT_SUCCESS) ? 0 : -1);
+}
+
+
+/* See the description in tapi_rpc_client_server.h */
+int
+rpc_dgram_connection(rcf_rpc_server *srvr, rcf_rpc_server *clnt,
+                     rpc_socket_proto proto,
+                     const struct sockaddr *srvr_addr,
+                     const struct sockaddr *clnt_addr,
+                     int *srvr_s, int *clnt_s)
+{
+    int srvr_sock = -1;
+    int clnt_sock = -1;
+    int result = EXIT_SUCCESS;
+
+    if (srvr_addr == NULL || clnt_addr == NULL)
+    {
+        ERROR("%s(): srvr_addr and clnt_addr parameters must be not NULL");
+        return -1;
+    }
+
+    if ((srvr_sock = rpc_create_and_bind_socket(srvr,
+                         RPC_SOCK_DGRAM, proto,
+                         FALSE, /* Do not bind to 
+                                   wildcard address */
+                         FALSE, /* Do not set
+                                   SO_REUSEADDR */
+                         SA(srvr_addr) /* FIXME */)) < 0)
+    {
+        ERROR("Cannot create socket of type SOCK_DGRAM");
+        result = EXIT_FAILURE;
+        goto cleanup;
+    }
+    if ((clnt_sock = rpc_create_and_bind_socket(clnt,
+                         RPC_SOCK_DGRAM, proto,
+                         FALSE, /* Do not bind to
+                                   wildcard address */
+                         FALSE, /* Do not set
+                                   SO_REUSEADDR */
+                         SA(clnt_addr) /* FIXME */)) < 0)
+    {
+        ERROR("Cannot create socket of type SOCK_DGRAM");
+        result = EXIT_FAILURE;
+        goto cleanup;
+    }
+
+    rpc_connect(clnt, clnt_sock, srvr_addr);
+    rpc_connect(srvr, srvr_sock, clnt_addr);
+
+cleanup:
+
+    if (result != EXIT_SUCCESS)
+    {
+        CLEANUP_RPC_CLOSE(clnt, clnt_sock);
+        CLEANUP_RPC_CLOSE(srvr, srvr_sock);
+        return -1;
+    }
+
+    *srvr_s = srvr_sock;
+    *clnt_s = clnt_sock;
+
+    return 0;
+}
+
+
+/* See the description in tapi_rpc_client_server.h */
+int
+rpc_gen_connection(rcf_rpc_server *srvr, rcf_rpc_server *clnt,
+                   rpc_socket_type sock_type,
+                   rpc_socket_proto proto,
+                   const struct sockaddr *srvr_addr,
+                   const struct sockaddr *clnt_addr,
+                   int *srvr_s, int *clnt_s)
+{
+    switch (sock_type)
+    {
+        case RPC_SOCK_STREAM:
+            return rpc_stream_connection(srvr, clnt, proto,
+                                         srvr_addr, clnt_addr, 
+                                         srvr_s, clnt_s);
+            break;
+
+        case RPC_SOCK_DGRAM:
+            return rpc_dgram_connection(srvr, clnt, proto,
+                                        srvr_addr, clnt_addr,
+                                        srvr_s, clnt_s);
+            break;
+
+        default:
+            ERROR("%s(): Socket type %d is not supported",
+                  __FUNCTION__, sock_type);
+            return -1;
+    }
+
+    return -1;
+}
---
+++ lib/tapi_rpc/tapi_rpc_client_server.h
@@ -0,0 +1,341 @@
+/** @file
+ * @brief Test API - RPC to create client-server connections
+ *
+ * Routines to create client-server connections.
+ *
+ * Actually we can't say that in @c SOCK_DGRAM socket type there are
+ * clients and servers, but we will call them so in case each socket
+ * is connected to its peer, so that send() and recv() operations lead
+ * to sending and receiving data to/from the particular remote peer.
+ *
+ *
+ * Copyright (C) 2004-2006 Test Environment authors (see file AUTHORS
+ * in the root directory of the distribution).
+ * Copyright (c) 2004-2006 Level5 Networks Corp.
+ *
+ * Test Environment is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * Test Environment is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA  02111-1307  USA
+ *
+ *
+ * @author Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
+ * @author Oleg Kravtsov <Oleg.Kravtsov@oktetlabs.ru>
+ *
+ * $Id: tapi_rpc.h 22478 2006-01-06 11:58:27Z arybchik $
+ */
+
+#ifndef __TE_TAPI_RPC_CLIENT_SERVER_H__
+#define __TE_TAPI_RPC_CLIENT_SERVER_H__
+
+#if HAVE_SYS_TYPES_H
+#include <sys/types.h>
+#endif
+#if HAVE_SYS_SOCKET_H
+#include <sys/socket.h>
+#endif
+
+#include "te_defs.h"
+#include "te_errno.h"
+#include "te_rpc_sys_socket.h"
+#include "rcf_rpc.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Create socket of type @p sock_type from @p domain domain and bind it
+ * to  the specified address
+ *
+ * @param rpc             PCO where a socket is created
+ * @param sock_type       Type of socket
+ * @param proto           Protocol used for the socket
+ * @param wild            Whether to bind socket to wildcard address
+ * @param set_reuse_addr  Whether to set SO_REUSEADDR socket option on 
+ *                        the socket
+ * @param addr            Address the socket should be bound to 
+ *                        (note that it is not allowed to have network 
+ *                        address part of address set to wildcard, but
+ *                        if you want to bind the socket to wildcard
+ *                        address you should set @p wild parameter to
+ *                        @c TRUE)
+ *
+ * @return Descriptor of created socket
+ *
+ * @retval @c -1 Creating or binding socket failed
+ *
+ * @note When the function returns @c -1 it reports the reason of the
+ * failure with ERROR() macro.
+ */
+extern int rpc_create_and_bind_socket(rcf_rpc_server    *rpc,
+                                      rpc_socket_type   sock_type,
+                                      rpc_socket_proto  proto,
+                                      te_bool           wild,
+                                      te_bool           set_reuse_addr,
+                                      struct sockaddr  *addr);
+
+
+/** @page lib-stream_server-alg Algorithm of creating a server socket of type @c SOCK_STREAM
+ *
+ * -# Call @b socket() on @p srvr PCO with the following parameters:
+ *    @p domain, @c SOCK_STREAM, @p proto.
+ *    Created socket is referred as @p srvr_s below;
+ * -# If @p srvr_wild is true, fill in network address part of 
+ *    @p srvr_bind_addr with wildcard network address;
+ * -# Copy port part of @p srvr_addr to port part of @p srvr_bind_addr
+ *    address;
+ * -# Bind @p srvr_s socket to @p srvr_bind_addr address.
+ * -# If port part of @p srvr_addr is zero (not specified), then call
+ *    @b getsockname() on @p srvr_s socket to obtain the assigned port 
+ *    and set it to the port part of @p srvr_addr.
+ * -# Call @b listen() for @p srvr_s socket with default @a backlog.
+ */
+
+/**
+ * Create a listening socket of type @c SOCK_STREAM on a particular PCO.
+ *
+ * @param srvr          PCO
+ * @param domain        Domain for the socket
+ * @param proto         Protocol for the socket
+ * @param srvr_wild     Whether to bind server to wildcard address or not
+ * @param srvr_addr     Server address to be used as a template 
+ *                      for @b bind() on server side (IN/OUT)
+ *
+ * @return Created socket or -1.
+ *
+ * @copydoc lib-stream_server-alg
+ */
+extern int rpc_stream_server(rcf_rpc_server *srvr,
+                             rpc_socket_proto proto, te_bool srvr_wild,
+                             struct sockaddr *srvr_addr);
+
+/** @page lib-stream_client-alg Algorithm of creating a client socket of type
+ * @c SOCK_STREAM
+ *
+ * -# Call @b socket() on @a clnt PCO with the following parameters:
+ *    @p domain, @c SOCK_STREAM, @p proto.
+ *    Created socket is referred as @p clnt_s below.
+ * -# If @p clnt_addr is not equal to @c NULL, @b bind() @p clnt_s socket
+ *    to @p clnt_addr address.
+ */
+
+/**
+ * Create a client socket of type @c SOCK_STREAM ready to connect to
+ * a remote peer (some listening socket)
+ *
+ * @param clnt          PCO where socket will be opened on
+ * @param domain        Domain for the socket
+ * @param proto         Protocol for the socket
+ * @param clnt_addr     Address to bind client to or @c NULL
+ * 
+ * @return Created socket or -1.
+ *
+ * @copydoc lib-stream_client-alg
+ */
+extern int rpc_stream_client(rcf_rpc_server *clnt,
+                             rpc_socket_domain domain,
+                             rpc_socket_proto proto,
+                             const struct sockaddr *clnt_addr);
+
+/** @page lib-stream_client_server Create a connection with connection oriented sockets
+ *
+ * @param srvr          PCO for server
+ * @param clnt          PCO for client
+ * @param domain        Domain used in the connection
+ * @param proto         Protocol used in the connection
+ * @param srvr_addr     server address (cannot be @c NULL) to be used as 
+ *                      a template for @b bind() on server side and for 
+ *                      @b connect() on client side. Network address part of
+ *                      the @p srvr_addr must be specified, but it is
+ *                      allowed to left port part of @p srvr_addr
+ *                      unspecified, which means we do not mind which
+ *                      address the server is bound to (on return the actual
+ *                      port used in established connection is set to the
+ *                      port part of @p srvr_addr).
+ * @param srvr_wild     bind server to wildcard address or not (although we
+ *                      must specify network address in @p srvr_addr
+ *                      parameter, it is still allowed to bind server socket
+ *                      to the wildcard address)
+ * @param clnt_addr     address to bind client to or @c NULL
+ *
+ * @par Step 1: Open @c SOCK_STREAM server socket
+ * @copydoc lib-stream_server-alg
+ *
+ * @par Step 2: Open @c SOCK_STREAM client socket
+ * @copydoc lib-stream_client-alg
+ *
+ * @par Step 3: Open connection
+ * -# Initiate @b accept() for @p srvr_s socket;
+ * -# Call @b connect() to connect client socket @p clnt_s to server
+ *    with @p srvr_addr address;
+ * -# Wait for @b accept() completion to get @p accepted_s socket;
+ * -# Close srvr_s socket
+ * -# Set @p accepted_s to @p srvr_s variable.
+ *
+ * @retval srvr_s       @c SOCK_STREAM socket reside on @p srvr
+ * @retval clnt_s       @c SOCK_STREAM socket reside on @p clnt
+ *
+ * @author Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
+ */
+
+/**
+ * Create a connection of type SOCK_STREAM between two PCO
+ *
+ * @param srvr          PCO where server socket is created
+ * @param clnt          PCO where client socket is created
+ * @param proto         Protocol for the connection
+ * @param srvr_addr     Server address to be used as a template 
+ *                      for @b bind() on server side (IN/OUT)
+ * @param clnt_addr     Address to bind client to or @c NULL
+ * @param srvr_s        Descriptor of the socket reside on @p srvr (OUT)
+ * @param clnt_s        Descriptor of the socket reside on @p clnt (OUT)
+ *
+ * @return Status of the operation
+ *
+ * @retval @c  0 Connection successfully created
+ * @retval @c -1 Creating connection failed
+ *
+ * @note When the function returns @c -1 it reports the reason of the
+ * failure with ERROR() macro.
+ */
+extern int rpc_stream_connection(rcf_rpc_server *srvr,
+                                 rcf_rpc_server *clnt,
+                                 rpc_socket_proto proto,
+                                 const struct sockaddr *srvr_addr,
+                                 const struct sockaddr *clnt_addr,
+                                 int *srvr_s, int *clnt_s);
+
+/** @page lib-dgram_client_server Create a connectionless pair of sockets that can communicate with each other without specifying any addresses in their I/O operations 
+ *
+ * @param srvr          PCO for server part of connection
+ * @param clnt          PCO for client part of connection
+ * @param domain        Domain used in the connection
+ * @param proto         Protocol used in the connection
+ * @param srvr_addr     server address (cannot be @c NULL) to be used as 
+ *                      a template for @b bind() on server side and for 
+ *                      @b connect() on client side.
+ * @param clnt_addr     address to bind client to (cannot be @c NULL)
+ *
+ * -# Open @c SOCK_DGRAM socket @p srvr_s on @p srvr and bind it 
+ *    to @p srvr_addr address;
+ * -# Open @c SOCK_DGRAM socket @p clnt_s on @p clnt and bind it 
+ *    to to @p clnt_addr address;
+ * -# @b connect() @p clnt_s socket to @p srvr_s socket;
+ * -# @b connect() @p srvr_s socket to @p clnt_s socket.
+ *
+ * @retval srvr_s       @c SOCK_DGRAM socket reside on @p srvr
+ * @retval clnt_s       @c SOCK_DGRAM socket reside on @p clnt
+ *
+ * @author Oleg Kravtsov <Oleg.Kravtsov@oktetlabs.ru>
+ */
+
+/**
+ * Create a connection of type @c SOCK_DGRAM between two PCO
+ *
+ * @param srvr          PCO where server socket is created
+ * @param clnt          PCO where client socket is created
+ * @param proto         Protocol for the connection
+ * @param srvr_addr     Server address to be used as a template 
+ *                      for @b bind() on server side
+ * @param clnt_addr     Address to bind client to
+ * @param srvr_s        Descriptor of the socket reside on @p srvr (OUT)
+ * @param clnt_s        Descriptor of the socket reside on @p clnt (OUT)
+ *
+ * @return Status of the operation
+ *
+ * @retval @c  0 Connection successfully created
+ * @retval @c -1 Creating connection failed
+ *
+ * @note When the function returns @c -1 it reports the reason of the
+ * failure with ERROR() macro.
+ */
+extern int rpc_dgram_connection(rcf_rpc_server *srvr,
+                                rcf_rpc_server *clnt,
+                                rpc_socket_proto proto,
+                                const struct sockaddr *srvr_addr,
+                                const struct sockaddr *clnt_addr,
+                                int *srvr_s, int *clnt_s);
+
+
+/** @page lib-gen_connection Create a connection of an arbitrary type
+ *
+ * @objective Provide a generic way to create a connection of an arbitrary
+ * type and from a particular domain
+ *
+ * @param srvr          PCO where server socket is created
+ * @param clnt          PCO where client socket is created
+ * @param sock_type     Socket type used in the connection
+ * @param proto         Protocol for the connection
+ * @param srvr_addr     Server address to be used as a template
+ *                      for @b bind() on server side
+ * @param clnt_addr     Address to bind client to
+ * @param srvr_s        Descriptor of the socket reside on @p srvr
+ *                      (accepted socket in the case of stream connection)
+ *                      (OUT)
+ * @param clnt_s        Descriptor of the socket reside on @p clnt (OUT)
+ *
+ * @note Division of two peers on server and client is purely abstract,
+ *       because actually just after creating a connection of type 
+ *       @c SOCK_STREAM we close real server socket and associate its
+ *       child socket, with @p srvr_s parameter of the function.
+ *
+ * For connection of type @c SOCK_STREAM use algorithm
+ * @ref lib-stream_client_server.
+ *
+ * For connection of type @c SOCK_DGRAM use algorithm
+ * @ref lib-dgram_client_server.
+ */
+
+/**
+ * Create a connection of an arbitrary type between two PCO.
+ *
+ * @copydoc lib-gen_connection
+ *
+ * @return Status of the operation
+ *
+ * @retval @c  0 Connection successfully created
+ * @retval @c -1 Creating connection failed
+ *
+ * @note When the function returns @c -1 it reports the reason of the
+ * failure with ERROR() macro.
+ */
+extern int rpc_gen_connection(rcf_rpc_server *srvr, rcf_rpc_server *clnt,
+                              rpc_socket_type sock_type,
+                              rpc_socket_proto proto,
+                              const struct sockaddr *srvr_addr,
+                              const struct sockaddr *clnt_addr,
+                              int *srvr_s, int *clnt_s);
+
+/** 
+ * The macro is a wrapper over gen_connection function.
+ * In case of failure it calls TEST_FAIL() macro, so that it should be
+ * called in test context only.
+ */
+#define GEN_CONNECTION(srvr_, clnt_, sock_type_, proto_,                \
+                       srvr_addr_, clnt_addr_, srvr_s_, clnt_s_)        \
+    do {                                                                \
+        if (rpc_gen_connection(srvr_, clnt_,                            \
+                               sock_type_, proto_,                      \
+                               srvr_addr_, clnt_addr_,                  \
+                               srvr_s_, clnt_s_) != 0)                  \
+        {                                                               \
+            TEST_FAIL("Cannot create a connection of type %s",          \
+                       socktype_rpc2str(sock_type_));                   \
+        }                                                               \
+    } while (0)
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+#endif /* __TE_TAPI_RPC_CLIENT_SERVER_H__ */
