55a9d1ed40df24b650d215bcbace4edabb4c2f31
Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
2006-11-03 08:18:30 +0000

    Fix warnings
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@33175 9d14d920-75d5-0310-8bd7-a52572981aee

--- lib/tapi_iscsi/tapi_iscsi.c
+++ lib/tapi_iscsi/tapi_iscsi.c
@@ -273,7 +273,9 @@ tapi_iscsi_recv_pkt(const char *ta_name, int sid, csap_handle_t csap,
     asn_value *pattern = NULL;
     struct iscsi_data_message msg;
 
-    int rc = 0, syms, num;
+    te_errno        rc;
+    int             syms;
+    unsigned int    num;
 
     if (ta_name == NULL)
         return TE_EWRONGPTR;
