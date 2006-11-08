cd85fd3c1b051f72df527efd194d1ac0ae4327f9
Alexander Kukuta <Alexander.Kukuta@oktetlabs.ru>
2006-11-08 15:29:57 +0000

    Added descriptive comments for functions, variables and definitions
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@33314 9d14d920-75d5-0310-8bd7-a52572981aee

--- engine/tester/tests/scen_op_test.c
+++ engine/tester/tests/scen_op_test.c
@@ -1,3 +1,29 @@
+/** @file
+ * @brief Tester Engine scenario operaions test
+ *
+ * Test Example
+ *
+ * Copyright (C) 2003 Test Environment authors (see file AUTHORS in the
+ * root directory of the distribution).
+ *
+ * Test Environment is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * Test Environment is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA  02111-1307  USA
+ *
+ *
+ * @author  Alexander Kukuta <Alexander.Kukuta@oktetlabs.ru>
+ */
 #include "te_config.h"
 
 #include "logger_api.h"
@@ -15,10 +41,21 @@ DEFINE_LGR_ENTITY("scen_op_test");
         fprintf(stdout, "\n");  \
     } while (0)
 
+#define DBG_ERR(args...) \
+    do {                        \
+        fprintf(stderr, args);  \
+        fprintf(stderr, "\n");  \
+    } while (0)
+
 #ifdef ERROR
 #undef ERROR
 #endif
-#define ERROR(args...) DBG_MSG("ERROR: " args);
+#define ERROR(args...) DBG_ERR("ERROR: " args);
+
+#ifdef WARN
+#undef WARN
+#endif
+#define WARN(args...) DBG_ERR("WARN: " args);
 
 #ifdef RING
 #undef RING
@@ -41,19 +78,31 @@ typedef enum {
     TESTING_ACT_EXCLUDE,
 } testing_act_op;
 
+/* String representation of supported operations */
 static const char *op_names[] = {"OR", "SUB", "EX"};
 
+/* Flags naming table */
 static char *flag_names[FLAG_NAMES_TABLE_SIZE];
+
+/* Amount of known flags */
 static int   known_flags = 0;
 
+/* see the description in scenario.c */
 extern te_errno testing_scenarios_op(testing_scenario *h0,
                                      testing_scenario *h1,
                                      testing_scenario *h_rslt_p,
                                      testing_act_op op_code);
 
-/*
+
+/**
  * Parse flags in string representation (like "flag1+flag2+flag3...")
- * 
+ * and produce uint32_t result.
+ *
+ * If unknown flag is encounted, this flag is added to the known flags table
+ *
+ * @param buf           String buffer with flags to parse
+ *
+ * @return bit-field corresponding to the flags described in the buffer
  */
 unsigned int
 parse_flags(const char *buf)
@@ -67,6 +116,11 @@ parse_flags(const char *buf)
     while (ptr && (*ptr != '\0'))
     {
         VERB("Parse flags: \"%s\"", ptr);
+
+        /* remove trailing spaces */
+        while (*ptr == ' ')
+            ptr++;
+
         next = strchr(ptr, '+');
         len = (next == NULL) ? (int)strlen(ptr) : (next - ptr);
         VERB("len=%d", len);
@@ -97,6 +151,10 @@ parse_flags(const char *buf)
             /* Add flag to flags table */
             known_flags++;
 
+            /* remove trailing spaces */
+            while (ptr[len - 1] == ' ')
+                len--;
+
             flag_names[flag_no] = (char *)malloc(len + 1);
             memcpy(flag_names[flag_no], ptr, len);
             flag_names[flag_no][len] = '\0';
@@ -112,25 +170,54 @@ parse_flags(const char *buf)
     return flags;
 }
 
+/**
+ * Convert flags bit-field to the string representation
+ * (only known flags are printed)
+ *
+ * @param buf           Storage buffer to print to
+ * @param size          Storage buffer size
+ * @param flags         Flags bit-field to print
+ *
+ * @return amount of symbols printed to the storage buffer
+ */
 int
 sprint_flags(char *buf, int size, unsigned int flags)
 {
     char *ptr = buf;
     int   flag_no;
 
+    if (flags == 0)
+    {
+        ptr += snprintf(ptr, size - (ptr - buf), "0");
+        return ptr - buf;
+    }
+
     for (flag_no = 0; flag_no < known_flags; flag_no++)
     {
         if ((flags & 0x1) != 0)
-        ptr += snprintf(ptr, size - (ptr - buf), "%s", flag_names[flag_no]);
-        if ((flags >>= 1) != 0)
-            ptr += snprintf(ptr, size - (ptr - buf), "+");
+        {
+            ptr += snprintf(ptr, size - (ptr - buf), "%s",
+                            flag_names[flag_no]);
+            if ((flags >> 1) != 0)
+                ptr += snprintf(ptr, size - (ptr - buf), "+");
+        }
+        flags >>= 1;
     }
 
     return ptr - buf;
 }
 
+/** Local storage buffer for printing flags values */
 #define FLAGS_TO_STR_BUF_SIZE   1024
 
+/**
+ * sprint flags bit-field in the string representation to the local
+ * buffer and return it
+ *
+ * @param flags         Flags bit-field to print
+ *
+ * @return local buffer with printed flags bit-field
+ */
 const char *
 flags_to_str(unsigned int flags)
 {
@@ -141,6 +228,15 @@ flags_to_str(unsigned int flags)
     return flags_str_buf;
 }
 
+/**
+ * Convert Testing Act structure to the string representation
+ *
+ * @param buf           Storage buffer to print to
+ * @param size          Storage buffer size
+ * @param act           Testing Act structure to print
+ *
+ * @return amount of symbols printed to the storage buffer
+ */
 int
 sprint_testing_act(char *buf, int size, const testing_act *act)
 {
@@ -155,8 +251,17 @@ sprint_testing_act(char *buf, int size, const testing_act *act)
     return (ptr - buf);
 }
 
+/** Local storage buffer for printing Testing Act structures */
 #define SCENARIO_ACT_TO_STR_BUF_SIZE    1024
 
+/**
+ * sprint Testing Act structure in the string representation to the local
+ * buffer and return it.
+ *
+ * @param act           Testing Act structure to print
+ *
+ * @return local buffer with printed flags bit-field
+ */
 const char *
 testing_act_to_str(const testing_act *act)
 {
@@ -167,6 +272,15 @@ testing_act_to_str(const testing_act *act)
     return scenario_act_buf;
 }
 
+/**
+ * Convert Testing Scenario structure to the string representation
+ *
+ * @param buf           Storage buffer to print to
+ * @param size          Storage buffer size
+ * @param scenario      Testing Scenario to print
+ *
+ * @return amount of symbols printed to the storage buffer
+ */
 int
 sprint_testing_scenario(char *buf, int size, const testing_scenario *scenario)
 {
@@ -190,8 +304,17 @@ sprint_testing_scenario(char *buf, int size, const testing_scenario *scenario)
     return ptr - buf;
 }
 
+/** Local storage buffer for printing Testing Scenario list */
 #define SCENARIO_TO_STR_BUF_SIZE   4096
 
+/**
+ * sprint Testing Act structure in the string representation to the local
+ * buffer and return it.
+ *
+ * @param act           Testing Act structure to print
+ *
+ * @return local buffer with printed flags bit-field
+ */
 const char *
 testing_scenario_to_str(const testing_scenario *scenario)
 {
@@ -203,8 +326,15 @@ testing_scenario_to_str(const testing_scenario *scenario)
     return scenario_str_buf;
 }
 
-
-
+/**
+ * Parse Testing Act structure from the string representation
+ *
+ * @param buf   String buffer to parse
+ * @param ts    Testing Scenario sequence to add parsed Testing Act to
+ *
+ * @return pointer to the first unparsed symbol
+ *         (next to Testing Act description)
+ */
 char *
 parse_scenario_act(const char *buf, testing_scenario *ts)
 {
@@ -269,7 +399,16 @@ parse_scenario_act(const char *buf, testing_scenario *ts)
     return ptr;    
 }
 
-
+/**
+ * Parse Testing Scenario sequence from the string representation
+ *
+ * @param buf   String buffer to parse
+ * @param ts    Uninitialised Testing Scenario sequence
+ *              to fill with parsed data
+ *
+ * @return pointer to the first unparsed symbol
+ *         (next to Testing Scenario sequence description)
+ */
 char *
 parse_scenario(const char *buf, testing_scenario *ts)
 {
@@ -305,6 +444,30 @@ parse_scenario(const char *buf, testing_scenario *ts)
     return ++ptr;
 }
 
+/**
+ * Parse test input data: 2 testing scenario sequences, operation code and
+ * expected testing scenario sequence produced by this operation.
+ * Format of the input data:
+ *   <scenario1> <op> <scenario2> = <expected_scenario>
+ * Format of the scenario data:
+ *   [<act1>,<act2>,...]
+ * Format of the testing act data:
+ *   [first,last,<flag1>+<flag2>+...]
+ * If there are no flags, then use 0 instead of flags field ([fist,last,0])
+ * Supported operations are: 
+ *   'or'  - OR operation
+ *   'sub' - Subtrate operation
+ *   'ex'  - Exclude operation
+ *
+ * @param buf   String buffer with input data to parse
+ * @param ts1   Parsed first Testing Scenario sequence involved in operation
+ * @param ts2   Parsed second Testing Scenario sequence involved in operation
+ * @param ts_r  Parsed Testing Scenario sequence that is expected
+ *              as an operation result
+ * @param op    Parsed operation code
+ *
+ * @return 0, on success, -1 on failure
+ */
 int
 parse_input(const char *buf,
             testing_scenario *ts1,
@@ -374,6 +537,14 @@ parse_input(const char *buf,
     return 0;    
 }
 
+/**
+ * Compare two testing scenario sequences, and print the difference
+ *
+ * @param ts1   First Testing Scenario sequence to compare
+ * @param ts2   Sarsed Testing Scenario sequence to compare
+ *
+ * @return 0, if equal, -1 if not
+ */
 int
 compare_scenarios(const testing_scenario *ts1, const testing_scenario *ts2)
 {
@@ -424,6 +595,11 @@ compare_scenarios(const testing_scenario *ts1, const testing_scenario *ts2)
     return 0;
 }
 
+/**
+ * Main test body.
+ *
+ * @return 0, if success, -1 if not
+ */
 int
 main(void)
 {
@@ -443,10 +619,10 @@ main(void)
         return -1;
     }
 
-    RING("First scenario: %s", scenario_to_str(&ts1));
-    RING("Second scenario: %s", scenario_to_str(&ts2));
+    RING("First scenario: %s", testing_scenario_to_str(&ts1));
+    RING("Second scenario: %s", testing_scenario_to_str(&ts2));
     RING("Operation: %s", op_names[op]);
-    RING("Expected result: %s", scenario_to_str(&ts_expected));
+    RING("Expected result: %s", testing_scenario_to_str(&ts_expected));
 
     if ((err = testing_scenarios_op(&ts1, &ts2, &ts_result, op)) != 0)
     {
@@ -454,7 +630,7 @@ main(void)
         return -1;
     }
 
-    RING("Calculated result: %s", scenario_to_str(&ts_result));
+    RING("Calculated result: %s", testing_scenario_to_str(&ts_result));
 
     if (compare_scenarios(&ts_expected, &ts_result) != 0)
     {
