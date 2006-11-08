fd11242511ce8f6c9f8c23939d1899be1645cbef
Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
2006-11-08 06:57:04 +0000

    [Bug 1011]:  Add the first version of testing scenario operations
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@33296 9d14d920-75d5-0310-8bd7-a52572981aee

--- engine/tester/scenario.c
+++ engine/tester/scenario.c
@@ -54,6 +54,267 @@
 #include "tester_run.h"
 
 
+/**
+ * By example: Given two lists L0 and L1 each consisting of intervals
+ * defined by testing_act:
+ * L0: [1 AB 5] [ 8 B 13]  (A, B, etc. are flags-properties of intervals)
+ * L1: [3 A  9] [11 A 15]
+ * operation OR(merge) will produce the followint list:
+ * [1 AB 5] [6 A 7] [8 AB 9] [10 B 10] [11 AB 13] [14 A 15]
+ * As one can see, the gaps are "implied" intervals with property 0;
+ *
+ * Subtract: 0-flag=0; flag-0=flag; AB-B=A, thus: flag0^(flag0&flag1)
+ * The result:
+ * [1 AB 2] [3 B 5] [8 B 13]
+ *
+ * @todo: Definition of exclude.
+ */
+typedef enum {
+    /* operation codes */
+    TESTING_ACT_OR,
+    TESTING_ACT_SUBTRACT,
+    TESTING_ACT_EXCLUDE,
+} testing_act_op;
+
+#ifndef INTRVL_TQ_POSTV_BIG
+#define INTRVL_TQ_POSTV_BIG  (1 << (8 * sizeof(int) - 2))
+#endif
+
+
+/**
+ * ibar: Please, provide function description.
+ */
+static void
+get_operation_result(testing_act *seg0, testing_act *seg1,
+                     testing_act *overlap, testing_act_op op_code)
+{
+    unsigned int    len_sum;
+    int             twice_centr_dist; /* double the distance between
+                                         the 2 centers */
+
+    len_sum = (seg0->last - seg0->first) + (seg1->last - seg1->first);
+
+    twice_centr_dist =
+        (seg0->last + seg0->first) - (seg1->last + seg1->first);
+
+    twice_centr_dist =
+        (twice_centr_dist >= 0) ? twice_centr_dist : -twice_centr_dist;
+
+    /* there have to be an overlap */
+    assert((unsigned)twice_centr_dist <= len_sum);
+
+    overlap->first = (seg0->first < seg1->first) ?
+                         seg1->first : seg0->first;
+    overlap->last = (seg0->last > seg1->last) ?
+                        seg1->last : seg0->last;
+
+    switch (op_code)
+    {
+        case TESTING_ACT_OR:
+            overlap->flags = seg0->flags | seg1->flags;
+            break;
+
+        case TESTING_ACT_SUBTRACT:
+            overlap->flags = seg0->flags ^ (seg0->flags & seg1->flags);
+            break;
+
+        default:
+            assert(FALSE);
+    }
+}
+
+/**
+ * ibar: Please, provide function description.
+ */
+static testing_act *
+get_next_with_gaps(testing_act **elm_p, testing_act *gap,
+                   unsigned int *the_end_p)
+{
+    unsigned int    the_end = *the_end_p;
+    testing_act    *elm = *elm_p;
+    testing_act    *next = NULL;
+
+
+    if (elm == NULL)
+        return NULL;
+
+    if (the_end < elm->first) /* pre-gap */
+    {
+        gap->first = the_end;
+        gap->last = elm->first - 1;
+        next = gap;
+    }
+    else if (the_end == elm->first) /* elm itself */
+    {
+        next = elm;
+    }
+    else /* move to the next elm and get either pre-gap or elm itself */
+    {
+        elm = elm->links.tqe_next;
+        next = get_next_with_gaps(&elm, gap, &the_end);
+    }
+
+    if (next == NULL )
+    {   
+        /* final gap to "infinity" */
+        gap->first = the_end;
+        gap->last = INTRVL_TQ_POSTV_BIG - 1;
+        next = gap;
+    }
+
+    *the_end_p  = next->last + 1;    
+    *elm_p      = elm;
+
+    return next;
+}
+
+/**
+ * Given two lists of intervals (h0 and h1), the function produces
+ * a list *h_rslt_p  which is the result of (h0 operation h1).
+ * If h0 address is passed as h_rslt_p, elements of h0 are removed
+ * and freed as soon as they are not needed for further processing.
+ * At the end h0 (head itself) is also freed. On exit, *h_rslt_p
+ * will contain the head of the new list (the result of the operation).
+ *
+ * @param h0        head of list0
+ * @param h1        head of list1
+ * @param h_rslt_p  an address to store a head of the resulting list.
+ *
+ * @return Status code.
+ */
+te_errno
+testing_scenarios_op(testing_scenario *h0, testing_scenario *h1,
+                     testing_scenario *h_rslt_p,
+                     testing_act_op op_code)
+{
+    testing_act    *elm0_prev, *elm0, *elm1; /* real list entries */
+    /* pseudo entries for gaps between the real intervals: */
+    testing_act    *gap0, *gap1;
+    testing_act    *next0, *next1;   /* it points to gap or elm */
+    unsigned int    the_end_0, the_end_1; /* how far we've already gone */
+    unsigned int    need_get_next1 = 1;   /* flag */
+    testing_act    *overlap;         /* overlap of two intervals */
+    testing_act    *overlap_grow;    /* cumulative overlap */
+
+    testing_scenario    h_rslt; /* resulting list */
+    te_bool             result_replace; /* put result into h0 */
+
+
+    result_replace = (h_rslt_p == h0); /* result should replace h0 */
+
+    TAILQ_INIT(&h_rslt);
+
+    /* a single obj to hold parameters of all list0 gaps */
+    if ((gap0 = calloc(1, sizeof(*gap0))) == NULL)
+        return TE_RC(TE_TESTER, TE_ENOMEM);
+    the_end_0 = 0;
+    elm0 = h0->tqh_first;
+    next0 = gap0;
+
+    /* a single obj to hold parameters of all list1 gaps */
+    if ((gap1 = calloc(1, sizeof(*gap1))) == NULL)
+        return TE_RC(TE_TESTER, TE_ENOMEM);
+    the_end_1 = 0;
+    elm1 = h1->tqh_first;
+    next1 = gap1;
+
+    /* a single obj to pass parameters of all overlaps */
+    if ((overlap = calloc(1, sizeof(*overlap))) == NULL)
+        return TE_RC(TE_TESTER, TE_ENOMEM);
+    
+    /*
+     * will be allocated again each time the current one is finished
+     * and inserted at the end of the resulting list
+     */
+    if ((overlap_grow = calloc(1, sizeof(*overlap_grow))) == NULL)
+        return TE_RC(TE_TESTER, TE_ENOMEM);
+    
+    
+    while (1) /* interate the fist set of intervals */
+    {
+        elm0_prev = elm0;
+        next0 = get_next_with_gaps(&elm0, gap0, &the_end_0);
+        if (result_replace && elm0_prev != elm0)
+        {   
+            /* we empty list0 one entry at a time as we can */
+            TAILQ_REMOVE(h0, elm0_prev, links);
+        }
+        if (next0 == NULL)
+            break;
+
+        while (1) /* interate the second set of intervals */
+        {
+            if (need_get_next1 == 0)
+            {
+                need_get_next1 = 1; /* reset on each pass */
+            }
+            else
+            {
+                next1 = get_next_with_gaps(&elm1, gap1, &the_end_1);
+                if (next1 == NULL)
+                    break;
+            }
+
+            get_operation_result(next0, next1, overlap, op_code);
+
+            if (overlap_grow->flags != overlap->flags)
+            {   
+                /* a different overlap, push the previous into the list */
+                if (overlap_grow->flags != 0)
+                {   
+                    /* insert only non-blank intervals */
+                    TAILQ_INSERT_TAIL(&h_rslt, overlap_grow, links);
+                    /* need to alloc a new obj to accumulate overlaps: */
+                    if ((overlap_grow = malloc(sizeof(*overlap_grow)))
+                        == NULL)
+                        return TE_RC(TE_TESTER, TE_ENOMEM);
+                }
+                /* start a new accumulation: */
+                memcpy(overlap_grow, overlap, sizeof(*overlap_grow));
+            }
+            else /* extend the cumulative interval */
+            {
+                overlap_grow->last = overlap->last;
+            }
+
+            if (next1->last >= next0->last)
+            {
+                if (next1->last > next0->last)
+                {
+                    /* still have to process this next1 and new next0: */
+                    need_get_next1 = 0;
+                }
+                break; /* need to step forward next0 now */
+            }           
+        }
+    }
+
+    /* Append the last overlap_grow if non-blank */
+    if (overlap_grow->flags != 0)
+    {
+        TAILQ_INSERT_TAIL(&h_rslt, overlap_grow, links);
+    }
+    else
+    {
+        free(overlap_grow);
+    }
+
+    free(gap0);
+    free(gap1);
+    free(overlap);
+
+    if (result_replace)
+    {   
+        assert(h0->tqh_first == NULL);
+        free(h0);
+    }
+    
+    *h_rslt_p = h_rslt;
+
+    return 0;
+}
+
+
 /* See the description in tester_run.h */
 void
 scenario_act_free(testing_act *act)
@@ -303,9 +564,13 @@ scenario_glue(testing_scenario *scenario)
 void
 scenario_exclude(testing_scenario *scenario, testing_scenario *exclude)
 {
+#if 0
+    rc = testing_scenarios_op(scenario, exclude, scenario,
+                              TESTER_ACT_EXCLUDE);
+#else
     UNUSED(scenario);
     UNUSED(exclude);
-    assert(FALSE);
+#endif
 }
 
 /* See the description in tester_run.h */
