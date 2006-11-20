e532aa4ba846e1dae66f5a2e05a5203ec9c7bca0
Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
2006-11-20 13:45:20 +0000

    Add LLC sublayer support in repository
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@33674 9d14d920-75d5-0310-8bd7-a52572981aee

---
+++ lib/tad/llc/tad_llc.h
@@ -0,0 +1,33 @@
+/** @file
+ * @brief TAD IEEE 802.2 LLC sublayer
+ *
+ * TAD support for IEEE Std 802.2 Logical Link Control sublayer
+ * including SubNetwokr Attachment Point.
+ *
+ * Copyright (C) 2006 Test Environment authors (see file AUTHORS
+ * in the root directory of the distribution).
+ *
+ * Test Environment is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * Test Environment is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA  02111-1307  USA
+ *
+ * @author Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
+ *
+ * $Id: tad_bps.h 29265 2006-06-16 09:16:55Z helen $
+ */
+
+#ifndef __TE_TAD_LLC_H__
+#define __TE_TAD_LLC_H__
+
+#endif /* !__TE_TAD_LLC_H__ */
