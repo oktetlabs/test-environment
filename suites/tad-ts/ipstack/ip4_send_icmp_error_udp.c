df63732d5c580a8a0e3b008d384b557317558709
Konstantin Petrov <Konstantin.Petrov@oktetlabs.ru>
2006-11-23 10:41:49 +0000

    CR notes were fixed
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@33751 9d14d920-75d5-0310-8bd7-a52572981aee

--- suites/tad-ts/ipstack/ip4_send_icmp_error_udp.c
+++ suites/tad-ts/ipstack/ip4_send_icmp_error_udp.c
@@ -61,6 +61,9 @@
 #define TEST_START_SPECIFIC     TEST_START_ENV
 #define TEST_END_SPECIFIC       TEST_END_ENV
 
+#define TST_CMSG_LEN   300
+#define TST_VEC        1
+
 #include "te_config.h"
 
 #if HAVE_STRING_H
@@ -74,6 +77,9 @@
 #if HAVE_NETINET_IP_ICMP_H
 #include <netinet/ip_icmp.h>
 #endif
+
+#include <linux/types.h>
+#include <linux/errqueue.h>
 
 #include "tad_common.h"
 #include "rcf_rpc.h"
@@ -114,6 +120,19 @@ main(int argc, char *argv[])
     int                         set_option;
     int                         sock_error;
 
+    uint8_t                     rx_buf[100];
+    size_t                      rx_buf_len = sizeof(rx_buf);
+    struct rpc_iovec            rx_vector;
+    
+    struct sockaddr_storage     msg_name;
+    socklen_t                   msg_namelen = sizeof(struct sockaddr_storage);
+    uint8_t                     cmsg_buf[TST_CMSG_LEN];
+    struct cmsghdr             *cmsg;
+    
+    int                         received;
+    rpc_msghdr                  rx_msghdr;
+    struct sock_extended_err   *optptr;
+
     TEST_START; 
 
     TEST_GET_HOST(host_csap);
@@ -127,58 +146,50 @@ main(int argc, char *argv[])
     TEST_GET_INT_PARAM(code);
     TEST_GET_ERRNO_PARAM(exp_errno);
 
+    rx_vector.iov_base = rx_buf;
+    rx_vector.iov_len = rx_vector.iov_rlen = rx_buf_len;
+
+    rx_msghdr.msg_iovlen = rx_msghdr.msg_riovlen = TST_VEC;
+    rx_msghdr.msg_iov = &rx_vector;
+    rx_msghdr.msg_control = cmsg_buf;
+    rx_msghdr.msg_controllen = TST_CMSG_LEN;
+    rx_msghdr.msg_cmsghdr_num = 1;
+    rx_msghdr.msg_name = &msg_name;
+    rx_msghdr.msg_namelen = rx_msghdr.msg_rnamelen = msg_namelen;
+    rx_msghdr.msg_flags = 0;
+    
     /* Create UDP socket */
     recv_socket = rpc_socket(pco, RPC_PF_INET, 
                              RPC_SOCK_DGRAM, RPC_IPPROTO_UDP);
     /* Bind socket to the local IP/port */
-    CHECK_RC(rpc_bind(pco, recv_socket, sock_addr));
+    if (rpc_bind(pco, recv_socket, sock_addr) == -1)
+        TEST_FAIL("Function rpc_bind() unexpetctedly failed");
 
     /* 
      * Prepare socket to receive ICMP error
      * messages
      */
     set_option = 1;
-    CHECK_RC(rpc_setsockopt(pco, recv_socket,
+    if (rpc_setsockopt(pco, recv_socket,
                             RPC_IP_RECVERR,
-                            &set_option));
-    
-    /* Add udp layer to the CSAP (to send UDP error message) */
-    CHECK_RC(tapi_tad_csap_add_layer(&csap_spec, 
-                                     ndn_udp_csap,
-                                     "#udp",
-                                     NULL));
-    /* Add ip4 layer to the CSAP (to send UDP error message) */
-    CHECK_RC(tapi_tad_csap_add_layer(&csap_spec, 
-                                     ndn_ip4_csap,
-                                     "#ip4",
-                                     NULL));
-    /* Add icmp4 layer to the CSAP */
-    CHECK_RC(tapi_tad_csap_add_layer(&csap_spec, 
-                                     ndn_icmp4_csap,
-                                     "#icmp4",
-                                     NULL));
-    /* Add ip4 layer to the CSAP */
-    CHECK_RC(tapi_ip4_add_csap_layer(&csap_spec, 
-                                     SIN(csap_addr)->sin_addr.s_addr, 
-                                     SIN(sock_addr)->sin_addr.s_addr,
-                                     IPPROTO_ICMP,
-                                     -1,
-                                     -1));
-    /* Add ethernet layer to the CSAP*/
-    CHECK_RC(tapi_eth_add_csap_layer(&csap_spec,
-                                     csap_if->if_name,
-                                     TAD_ETH_RECV_NO,
-                                     sock_hwaddr->sa_data,
-                                     csap_hwaddr->sa_data,
-                                     &ip_eth,
-                                     TE_BOOL3_ANY,
-                                     TE_BOOL3_ANY));
-    /* Create CSAP */
-    CHECK_RC(tapi_tad_csap_create(host_csap->ta, 0,
-                                  "udp.ip4.icmp4.ip4.eth",
-                                  csap_spec,
-                                  &send_csap));
+                            &set_option) == -1)
+        TEST_FAIL("Function rpc_setsockopt() unexpectedly failed");
 
+    /* Create CSAP */
+    CHECK_RC(tapi_udp_ip4_icmp_ip4_eth_csap_create(
+                                  host_csap->ta, 0,
+                                  csap_if->if_name,
+                                  TAD_ETH_RECV_NO,
+                                  sock_hwaddr->sa_data,
+                                  csap_hwaddr->sa_data,
+                                  SIN(csap_addr)->sin_addr.s_addr,
+                                  SIN(sock_addr)->sin_addr.s_addr,
+                                  SIN(sock_addr)->sin_addr.s_addr,
+                                  SIN(csap_addr)->sin_addr.s_addr,
+                                  htons(SIN(sock_addr)->sin_port),
+                                  htons(SIN(csap_addr)->sin_port),
+                                  &send_csap));
+    
     /* Prepare data-sending template */
     /* 
      * Prepare UDP error message's UDP header, src_port being
@@ -229,13 +240,80 @@ main(int argc, char *argv[])
     CHECK_RC(rpc_getsockopt(pco, recv_socket,
                             RPC_SO_ERROR,
                             &sock_error));
+    
     /* Check socket error */
     if (sock_error != (int)exp_errno)
         TEST_FAIL("SO_ERROR is set to %s "
                   "instead of expected %s",
                   errno_rpc2str(sock_error),
                   errno_rpc2str(exp_errno));
+
+    /*
+     * Check that socket error was reset in previous
+     * getsockopt() call
+     */
+    CHECK_RC(rpc_getsockopt(pco, recv_socket,
+                            RPC_SO_ERROR,
+                            &sock_error));
+    if (sock_error != 0)
+        TEST_FAIL("Socket error was unexpectedly not reset in "
+                  "previous getsockopt() call");
+
+    /*
+     * Receive icmp messages pending on socket
+     */
+    received = rpc_recvmsg(pco, recv_socket,
+                            &rx_msghdr, RPC_MSG_ERRQUEUE);
+
+    /* 
+     * Check that protocol name is equal to the
+     * destination IP
+     */
+    if (te_sockaddrcmp(SA(&msg_name), rx_msghdr.msg_namelen,
+                       csap_addr, te_sockaddr_get_size(csap_addr)) != 0)
+        TEST_FAIL("Returned message name:%s is not the same as "
+             "destination addr:%s reside in ICMP message payload",
+             te_sockaddr2str(SA(&msg_name)), te_sockaddr2str(csap_addr));
+
+    /* Check that message's flag is set to MSG_ERRQUEUE */
+    if (!(rx_msghdr.msg_flags & RPC_MSG_ERRQUEUE))
+    {
+        TEST_FAIL("Unexpected msghdr.msg_flags value returned %s, "
+                  "expected MSG_ERRQUEUE",
+                  send_recv_flags_rpc2str(rx_msghdr.msg_flags));
+    }
+
+    /* Get returned ancillary data */
+    cmsg = CMSG_FIRSTHDR(&rx_msghdr);
+    if (cmsg == NULL) 
+        TEST_FAIL("Ancillary data on pco_iut socket "
+                  "is not recieved");
+
+    /* Check ancilliary data */
+    optptr = (struct sock_extended_err *) CMSG_DATA(cmsg);
+    if (cmsg->cmsg_level != SOL_IP ||
+        cmsg->cmsg_type != IP_RECVERR ||
+        (errno_h2rpc(optptr->ee_errno) != exp_errno) ||
+        (optptr->ee_origin != SO_EE_ORIGIN_ICMP) ||
+        (optptr->ee_type != type) ||
+        (optptr->ee_code != code) ||
+        (optptr->ee_pad != 0))
+        TEST_FAIL("Returned unexpected values of ancillary data");
     
+    /* Check that error queue is empty */
+    RPC_AWAIT_IUT_ERROR(pco);
+    received = rpc_recvmsg(pco, recv_socket,
+                           &rx_msghdr, RPC_MSG_ERRQUEUE);
+    if (received != -1)
+    {
+        TEST_FAIL("recvmsg() return %d, "
+                  "but it is expected to return -1, because error queue "
+                  "is empty", received);
+    }
+    CHECK_RPC_ERRNO(pco, RPC_EAGAIN,
+                    "recvmsg() "
+                    "returns -1, but");
+
     TEST_SUCCESS;
 
 cleanup:
