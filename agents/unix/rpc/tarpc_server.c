a37d85437c4781c84cd0c28d86bbf32106f7ee12
Konstantin Petrov <Konstantin.Petrov@oktetlabs.ru>
2006-12-15 12:40:00 +0000

    To fix level5-tot failures
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@34258 9d14d920-75d5-0310-8bd7-a52572981aee

--- agents/unix/rpc/tarpc_server.c
+++ agents/unix/rpc/tarpc_server.c
@@ -4618,9 +4618,6 @@ socket_to_file(tarpc_socket_to_file_in *in)
     }
     timeout.tv_sec += time2run;
 
-    call_timeout.tv_sec = time2run;
-    call_timeout.tv_usec = 0;
-
     INFO("%s(): time2run=%ld, timeout timestamp=%ld.%06ld", __FUNCTION__,
          time2run, (long)timeout.tv_sec, (long)timeout.tv_usec);
 
