7e36b6b8773d9410f6e1eb017401e9530366cfae
Artem Andreev <Artem.Andreev@oktetlabs.ru>
2006-12-18 16:47:52 +0000

    moved conditional out of macro call
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@34319 9d14d920-75d5-0310-8bd7-a52572981aee

--- agents/unix/rpc/tarpc_server.c
+++ agents/unix/rpc/tarpc_server.c
@@ -3468,16 +3468,16 @@ TARPC_FUNC(sysinfo,
 },
 {
     INIT_CHECKED_ARG(out->buf.buf_val, out->buf.buf_len, in->count);
-    MAKE_CALL(
 #if HAVE_SYS_SYSTEMINFO_H
+    MAKE_CALL(
         out->retval = func(sysinfo_command_rpc2h(in->command),
                            out->buf.buf_val,
-                           in->count);
+                           in->count));
 #else
+    MAKE_CALL(
         errno = ENOSYS;
-        out->retval = -1
+        out->retval = -1);
 #endif
-    );
 }
 )
 
