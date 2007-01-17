60b6701d5f93e9d173fee328d87918c0c5118a01
Artem Andreev <Artem.Andreev@oktetlabs.ru>
2007-01-17 21:56:08 +0000

    fixed conditional compilation inside a macro
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@35219 9d14d920-75d5-0310-8bd7-a52572981aee

--- agents/unix/rpc/tarpc_server.c
+++ agents/unix/rpc/tarpc_server.c
@@ -3470,24 +3470,32 @@ finish:
 #undef PUT_STR
 
 /*-------------- sysinfo() -------------------------------*/
+#if HAVE_SYS_SYSTEMINFO_H
 TARPC_FUNC(sysinfo, 
 {
     COPY_ARG(buf);
 },
 {
     INIT_CHECKED_ARG(out->buf.buf_val, out->buf.buf_len, in->count);
-#if HAVE_SYS_SYSTEMINFO_H
     MAKE_CALL(
         out->retval = func(sysinfo_command_rpc2h(in->command),
                            out->buf.buf_val,
                            in->count));
+}
+)
 #else
+TARPC_FUNC(sysinfo, 
+{
+    COPY_ARG(buf);
+},
+{
+    INIT_CHECKED_ARG(out->buf.buf_val, out->buf.buf_len, in->count);
     MAKE_CALL(
         errno = ENOSYS;
         out->retval = -1);
-#endif
 }
 )
+#endif
 
 
 
