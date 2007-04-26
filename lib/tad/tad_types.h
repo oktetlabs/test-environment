8cad22f3961db83c4f22df4cac84602e219e56c3
Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
2007-04-26 14:34:52 +0000

    Change license from GPL to LGPL
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@38839 9d14d920-75d5-0310-8bd7-a52572981aee

--- lib/tad/tad_types.h
+++ lib/tad/tad_types.h
@@ -6,24 +6,24 @@
  * Copyright (C) 2006 Test Environment authors (see file AUTHORS
  * in the root directory of the distribution).
  *
- * Test Environment is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1 of
  * the License, or (at your option) any later version.
  *
- * Test Environment is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
  * MA  02111-1307  USA
  *
  * @author Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
  *
- * $Id: tad_pkt.h 22602 2006-01-09 13:12:42Z arybchik $
+ * $Id$
  */
 
 #ifndef __TE_LIB_TAD_TYPES_H__
