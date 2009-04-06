ae399aee70a9b949dc2d46d7719b94f23ec56507
Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
2009-04-06 12:09:42 +0000

    Add stubs
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@59587 9d14d920-75d5-0310-8bd7-a52572981aee

---
+++ lib/hg_cn/hg_cn.c
@@ -0,0 +1,48 @@
+/** @file
+ * @brief Home Gateway Configuration Notation
+ *
+ * Defition of Home Gateway configuration using ASN.1 syntax.
+ *
+ *
+ * Copyright (C) 2009 Test Environment authors (see file AUTHORS
+ * in the root directory of the distribution).
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA  02111-1307  USA
+ *
+ *
+ * @author Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
+ *
+ * $Id$
+ */
+
+#define TE_LGR_USER     "HG CN"
+
+#include "te_config.h"
+
+#include <stdio.h>
+#ifdef STDC_HEADERS
+#include <stdlib.h>
+#else
+#if HAVE_STDLIB_H
+#include <stdlib.h>
+#endif
+#endif
+
+#include "te_defs.h"
+#include "logger_api.h"
+#include "asn_usr.h"
+#include "hg_cn.h"
+
