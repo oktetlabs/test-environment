5ecf9152d63ec813f4c35950963e7be4bc6a382f
Edward Makarov <Edward.Makarov@oktetlabs.ru>
2009-07-06 14:12:56 +0000

    Lists of methods are completed, excess warnings are removed.
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@60691 9d14d920-75d5-0310-8bd7-a52572981aee

--- lib/acse/acse_cwmp.c
+++ lib/acse/acse_cwmp.c
@@ -37,6 +37,7 @@
 #include <stddef.h>
 #include<string.h>
 
+#include "te_defs.h"
 #include "te_errno.h"
 #include "te_queue.h"
 #include "logger_api.h"
@@ -48,10 +49,54 @@
 
 SOAP_FMAC5 int SOAP_FMAC6 __cwmp__GetRPCMethods(struct soap *soap, struct _cwmp__GetRPCMethods *cwmp__GetRPCMethods, struct _cwmp__GetRPCMethodsResponse *cwmp__GetRPCMethodsResponse)
 {
+    UNUSED(soap);
+    UNUSED(cwmp__GetRPCMethods);
+    UNUSED(cwmp__GetRPCMethodsResponse);
+
     return 0;
 }
 
 SOAP_FMAC5 int SOAP_FMAC6 __cwmp__Inform(struct soap *soap, struct _cwmp__Inform *cwmp__Inform, struct _cwmp__InformResponse *cwmp__InformResponse)
 {
+    UNUSED(soap);
+    UNUSED(cwmp__Inform);
+    UNUSED(cwmp__InformResponse);
+
+    return 0;
+}
+
+SOAP_FMAC5 int SOAP_FMAC6 __cwmp__TransferComplete(struct soap *soap, struct _cwmp__TransferComplete *cwmp__TransferComplete, struct _cwmp__TransferCompleteResponse *cwmp__TransferCompleteResponse)
+{
+    UNUSED(soap);
+    UNUSED(cwmp__TransferComplete);
+    UNUSED(cwmp__TransferCompleteResponse);
+
+    return 0;
+}
+
+SOAP_FMAC5 int SOAP_FMAC6 __cwmp__AutonomousTransferComplete(struct soap *soap, struct _cwmp__AutonomousTransferComplete *cwmp__AutonomousTransferComplete, struct _cwmp__AutonomousTransferCompleteResponse *cwmp__AutonomousTransferCompleteResponse)
+{
+    UNUSED(soap);
+    UNUSED(cwmp__AutonomousTransferComplete);
+    UNUSED(cwmp__AutonomousTransferCompleteResponse);
+
+    return 0;
+}
+
+SOAP_FMAC5 int SOAP_FMAC6 __cwmp__RequestDownload(struct soap *soap, struct _cwmp__RequestDownload *cwmp__RequestDownload, struct _cwmp__RequestDownloadResponse *cwmp__RequestDownloadResponse)
+{
+    UNUSED(soap);
+    UNUSED(cwmp__RequestDownload);
+    UNUSED(cwmp__RequestDownloadResponse);
+
+    return 0;
+}
+
+SOAP_FMAC5 int SOAP_FMAC6 __cwmp__Kicked(struct soap *soap, struct _cwmp__Kicked *cwmp__Kicked, struct _cwmp__KickedResponse *cwmp__KickedResponse)
+{
+    UNUSED(soap);
+    UNUSED(cwmp__Kicked);
+    UNUSED(cwmp__KickedResponse);
+
     return 0;
 }
