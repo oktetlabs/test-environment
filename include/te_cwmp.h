909c521af900bffb1464b667d242a05072a25b6b
Konstantin Abramenko <Konstantin.Abramenko@oktetlabs.ru>
2009-12-22 21:22:21 +0000

    Add a bit.
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@62428 9d14d920-75d5-0310-8bd7-a52572981aee

--- include/te_cwmp.h
+++ include/te_cwmp.h
@@ -31,7 +31,6 @@
 #ifndef __TE_CWMP_H__
 #define __TE_CWMP_H__
 
-#include "te_defs.h"
 #include "te_stdint.h"
 
 /**
@@ -105,18 +104,21 @@ typedef enum {
 } te_cwmp_event_code_t;
 
 /**
- * This structure contains types of Events, which 
- * may occur in Inform from CPE to ACS, according with
- * Table 7 in [TR-069].
+ * This structure contains data in ParameterValue struct
+ * according with Table 14 in [TR-069].
  */
 typedef struct {
     const char         *name;
     union {
         const char *s;
         uint32_t    u;
-    }                   value;
+    }                   value; /* TODO: this is anySimpleType value */
 } te_cwmp_param_value_t;
 
+/**
+ * This structure contains data in DeviceIdStruct
+ * according with Table 36 in [TR-069].
+ */
 typedef struct {
     char  manufacturer[64];
     char  oui[6];
@@ -124,21 +126,40 @@ typedef struct {
     char  serial_number[64];
 } te_cwmp_device_id_struct_t;
 
+/**
+ * This structure contains data in EventStruct
+ * according with Table 37 in [TR-069].
+ */
 typedef struct {
     te_cwmp_event_code_t    event_code;
     char                    command_key[32];
 } te_cwmp_event_struct_t;
 
 
+/**
+ * This structure contains data of Inform arguments
+ * according with Table 34 in [TR-069].
+ */
 typedef struct {
     te_cwmp_device_id_struct_t  device_id;
     uint32_t                    events_len;
     te_cwmp_event_struct_t     *events;
-                                current_time;
+                                current_time; /* TODO: what type? */
     uint32_t                    retry_count;
     uint32_t                    parameter_list_len;
     te_cwmp_param_value_t      *parameter_list;
 } te_cwmp_inform_t;
 
+/* TODO: investigate in standard, think about it. */
+typedef enum {
+    CWMP_SS_NO_STATE,
+    CWMP_SS_DISCONNECTED,
+    CWMP_SS_CONNECTED,
+    CWMP_SS_AUTHENTICATED,
+    CWMP_SS_,
+    CWMP_SS_,
+    CWMP_SS_,
+    CWMP_SS_,
+} te_cwmp_session_state_t;
 
 #endif /*__TE_CWMP_H__ */
