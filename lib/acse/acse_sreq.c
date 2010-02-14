55cfc49a2a531307a63129e72c0cdd07f41ef4cd
Konstantin Abramenko <Konstantin.Abramenko@oktetlabs.ru>
2010-02-14 20:18:17 +0000

    async connection request
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@63052 9d14d920-75d5-0310-8bd7-a52572981aee

--- lib/acse/acse_sreq.c
+++ lib/acse/acse_sreq.c
@@ -38,6 +38,7 @@
 #include<string.h>
 
 #include "acse_internal.h"
+#include "httpda.h"
 
 #include "te_errno.h"
 #include "te_queue.h"
@@ -47,11 +48,10 @@
 typedef struct srec_data_t {
     LIST_ENTRY(srec_data_t) links;
 
-    int              socket;  /**< TCP listening socket. */
-    struct sockaddr *addr;    /**< Network TCP address, which @p socket 
-                                      is bound. */
+    struct soap     m_soap;
 
     cpe_t       *cpe_item;    /**< CPE - object of Connection Request */
+
 } sreq_data_t;
 
 static LIST_HEAD(sreq_list_t, sreq_data_t)
@@ -63,7 +63,7 @@ sreq_before_poll(void *data, struct pollfd *pfd)
 {
     sreq_data_t *sreq = data;
 
-    pfd->fd = sreq->socket;
+    pfd->fd = sreq->m_soap.socket;
     pfd->events = POLLIN;
     pfd->revents = 0;
 
@@ -73,11 +73,47 @@ sreq_before_poll(void *data, struct pollfd *pfd)
 te_errno
 sreq_after_poll(void *data, struct pollfd *pfd)
 {
+    sreq_data_t *sreq = data;
+    struct soap *soap;
+    struct http_da_info info;
+
     if (!(pfd->revents & POLLIN))
         return 0;
-    /* TODO */
 
-    return 0;
+    soap = &(sreq->m_soap);
+    /* should not block after poll() */
+    if (soap_begin_recv(soap))
+    {
+        if (soap->error == 401)
+        {
+            RING("ConnectionRequest, attempt failed, realm: '%s'\n",
+                  soap->authrealm);
+            /* save userid and passwd for basic or digest authentication */
+            http_da_save(soap, &info, soap->authrealm,
+                         sreq->cpe_item->username,
+                         sreq->cpe_item->password);
+            soap_begin_count(soap);
+            soap_end_count(soap);
+
+            info.qop = soap_strdup(soap, "auth");
+            http_da_restore(soap, &info);
+            soap_connect_command(soap, SOAP_GET, sreq->cpe_item->url, "");
+            soap_end_send(soap);
+            return 0;
+        }
+        else
+        {
+            ERROR("Recv after Conn.Req., soap error %d", soap->error);
+        }
+    }
+    else
+        soap_end_recv(soap);
+
+
+    RING("Recv after Conn req, status %d", soap->error);
+
+    soap_closesock(soap);
+    return TE_ENOTCONN;
 }
 
 
@@ -87,6 +123,7 @@ sreq_destroy(void *data)
     sreq_data_t *sreq = data;
     /* TODO: release all */
 
+    soap_end(&(sreq->m_soap));
     free(sreq);
     return 0;
 }
@@ -96,14 +133,37 @@ extern te_errno
 acse_init_connection_request(cpe_t *cpe_item)
 {
     sreq_data_t *sreq_data = malloc(sizeof(*sreq_data));
+    struct soap *soap;
+    channel_t   *channel = malloc(sizeof(*channel)); 
 
-    channel_t *channel;
+    struct http_da_info info;
+
+    if (sreq_data == NULL || channel == NULL)
+        return TE_ENOMEM;
+
+    soap_init(&(sreq_data->m_soap));
+    soap = &(sreq_data->m_soap);
+
+    soap_register_plugin(soap, http_da);
+
+    soap_begin(soap);
 
     sreq_data->cpe_item = cpe_item;
-    sreq_data->addr = malloc(sizeof(struct sockaddr_in));
-    sreq_data->socket = -1; /* TODO !! */
 
-    channel = malloc(sizeof(*channel)); 
+
+    soap_begin_count(soap);
+    soap_end_count(soap);
+
+    if (soap_connect_command(soap, SOAP_GET, cpe_item->url, ""))
+    {
+        soap_print_fault(soap, stderr); 
+        ERROR("%s failed, soap error %d", __FUNCTION__, soap->error);
+        soap_end(soap);
+        free(sreq_data);
+        free(channel);
+        return TE_EFAIL;
+    } 
+
     channel->data = sreq_data;
     channel->before_poll = sreq_before_poll;
     channel->after_poll = sreq_after_poll;
@@ -111,7 +171,61 @@ acse_init_connection_request(cpe_t *cpe_item)
 
     acse_add_channel(channel);
 
-
     return 0;
 }
 
+
+int
+cwmp_SendConnectionRequest(const char *endpoint,
+                           const char *username, 
+                           const char *password)
+{
+    struct soap *soap = soap_new();
+    struct http_da_info info;
+
+    soap_register_plugin(soap, http_da);
+
+    soap_begin(soap);
+    soap_begin_count(soap);
+    soap_end_count(soap);
+
+    if (soap_connect_command(soap, SOAP_GET, endpoint, "")
+        || soap_end_send(soap))
+    {
+        soap_print_fault(soap, stderr); 
+        return 1;
+    } 
+    if (soap_begin_recv(soap))
+    {
+        if (soap->error == 401)
+        {
+            char local_realm[100];
+            // soap_end_recv(soap);
+            printf("UUUUUUUU first attempt failed, AUTH, realm: '%s'\n",
+                  soap->authrealm);
+            strcpy(local_realm, soap->authrealm);
+            /* save userid and passwd for basic or digest authentication */
+            http_da_save(soap, &info, local_realm, username, password);
+#if 1
+            soap_begin_count(soap);
+            soap_end_count(soap);
+#endif
+            info.qop = soap_strdup(soap, "auth");
+            http_da_restore(soap, &info);
+            soap_connect_command(soap, SOAP_GET, endpoint, "");
+            soap_end_send(soap);
+            soap_begin_recv(soap);
+            soap_end_recv(soap);
+            printf("UUUUUUUUU second attempt result %d\n", soap->error);
+            if (soap->error != SOAP_OK)
+                soap_print_fault(soap, stderr);
+        }
+        else 
+            soap_print_fault(soap, stderr); 
+        return 1;
+    } 
+
+    soap_done(soap);
+    soap_end(soap); 
+    return 0;
+}
