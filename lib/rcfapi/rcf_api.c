51838e5aeb64b08d964ec456d535f4d9c80addca
Alexander Kukuta <Alexander.Kukuta@oktetlabs.ru>
2010-03-14 16:57:01 +0000

    Ring about traffic operations logging enabling/disabling
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@63358 9d14d920-75d5-0310-8bd7-a52572981aee

--- lib/rcfapi/rcf_api.c
+++ lib/rcfapi/rcf_api.c
@@ -2088,6 +2088,8 @@ rcf_ta_del_file(const char *ta_name, int session, const char *rfile)
 te_errno
 rcf_tr_op_log(te_bool ring)
 {
+    RING("Turn RCF traffic operations logging %s",
+         ring ? "ON" : "OFF");
     rcf_tr_op_ring = ring;
     return 0;
 }
