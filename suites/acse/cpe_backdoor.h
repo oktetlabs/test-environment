a26e31840c59e8204a3206bab779db6acd653c73
Konstantin Abramenko <Konstantin.Abramenko@oktetlabs.ru>
2010-06-04 08:22:00 +0000

    some more
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@64584 9d14d920-75d5-0310-8bd7-a52572981aee

--- suites/acse/cpe_backdoor.h
+++ suites/acse/cpe_backdoor.h
@@ -15,15 +15,66 @@
 /* TODO: invent actual and portable ID for box, which will 
    help background implementation to connect with it.
    This is temporary. */
-typedef const char * board_id_t;
+typedef struct board_id_s {
+    const char * ta;
+    struct sockaddr_storage addr;
+} board_id_t;
 
-extern te_errno cpe_get_cr_url(board_id_t cpe, char *buf, size_t bufsize);
+/**
+ * Get URL for ConnectionRequest from CPE.
+ *
+ * @param cpe           ID of board with CPE.
+ * @param cr_url        location for URL.
+ * @param bufsize       size of location buffer.
+ *
+ * @return status
+ */
+extern te_errno cpe_get_cr_url(board_id_t cpe,
+                               char *cr_url, size_t bufsize);
 
-extern te_errno cpe_get_acs_url(board_id_t cpe, char *buf, size_t bufsize);
+/**
+ * Get URL of ACS from CPE.
+ *
+ * @param cpe           ID of board with CPE.
+ * @param acs_url       location for URL.
+ * @param bufsize       size of location buffer.
+ *
+ * @return status
+ */
+extern te_errno cpe_get_acs_url(board_id_t cpe,
+                                char *acs_url, size_t bufsize);
 
+/**
+ * Set URL of ACS on CPE.
+ *
+ * @param cpe           ID of board with CPE.
+ * @param acs_url       string with URL.
+ *
+ * @return status
+ */
 extern te_errno cpe_set_acs_url(board_id_t cpe, char *acs_url);
 
 extern te_errno cpe_activate_tr069_mgmt(board_id_t cpe, char *acs_url);
 
-#endif /* __CPE_BACKDOOR__H__ */
+extern te_errno cpe_set_cr_login(board_id_t cpe,  const char *cr_login);
 
+extern te_errno cpe_set_cr_passwd(board_id_t cpe, const char *cr_passwd);
+
+extern te_errno cpe_get_cr_login(board_id_t cpe,
+                                 char *cr_login, size_t bufsize);
+
+extern te_errno cpe_get_cr_passwd(board_id_t cpe,
+                                  char *cr_passwd, size_t bufsize);
+
+extern te_errno cpe_set_acs_login(board_id_t cpe,  const char *acs_login);
+
+extern te_errno cpe_set_acs_passwd(board_id_t cpe, const char *acs_passwd);
+
+extern te_errno cpe_get_acs_login(board_id_t cpe,
+                                  char *acs_login, size_t bufsize);
+
+extern te_errno cpe_get_acs_passwd(board_id_t cpe,
+                                   char *acs_passwd, size_t bufsize);
+
+
+#endif /* __CPE_BACKDOOR__H__ */ 
