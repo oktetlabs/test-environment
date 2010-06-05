dee016dc5dc45e5aec07e97a0777a7506637b59a
Konstantin Abramenko <Konstantin.Abramenko@oktetlabs.ru>
2010-06-05 09:32:31 +0000

    set properties, add descriptions
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@64601 9d14d920-75d5-0310-8bd7-a52572981aee

--- suites/acse/cpe_backdoor.h
+++ suites/acse/cpe_backdoor.h
@@ -7,7 +7,7 @@
  *
  * @author Konstantin Abramenko <Konstantin.Abramenko@oktetlabs.ru>
  * 
- * $Id: $
+ * $Id$
  */
 #ifndef __CPE_BACKDOOR__H__
 #define __CPE_BACKDOOR__H__
@@ -54,25 +54,104 @@ extern te_errno cpe_get_acs_url(board_id_t cpe,
  */
 extern te_errno cpe_set_acs_url(board_id_t cpe, char *acs_url);
 
+/**
+ * Activate TR069 management protocol on CPE. 
+ * Set URL of ACS on CPE, if specified.
+ *
+ * @param cpe           ID of board with CPE.
+ * @param acs_url       string with URL, may be NULL.
+ *
+ * @return status
+ */
 extern te_errno cpe_activate_tr069_mgmt(board_id_t cpe, char *acs_url);
 
+/**
+ * Set ConnectionRequest login username on CPE, that is, login for
+ * authenticate ConnectionRequest from ACS to CPE.
+ *
+ * @param cpe           ID of board with CPE.
+ * @param cr_login      username.
+ *
+ * @return status
+ */
 extern te_errno cpe_set_cr_login(board_id_t cpe,  const char *cr_login);
 
+/**
+ * Set ConnectionRequest login password on CPE.
+ *
+ * @param cpe           ID of board with CPE.
+ * @param cr_passwd     password.
+ *
+ * @return status
+ */
 extern te_errno cpe_set_cr_passwd(board_id_t cpe, const char *cr_passwd);
 
+/**
+ * Get ConnectionRequest login username on CPE.
+ *
+ * @param cpe           ID of board with CPE.
+ * @param cr_login      location for username.
+ * @param bufsize       size of location buffer.
+ *
+ * @return status
+ */
 extern te_errno cpe_get_cr_login(board_id_t cpe,
                                  char *cr_login, size_t bufsize);
 
+/**
+ * Get ConnectionRequest login password on CPE.
+ *
+ * @param cpe           ID of board with CPE.
+ * @param cr_passwd     location for password.
+ * @param bufsize       size of location buffer.
+ *
+ * @return status
+ */
 extern te_errno cpe_get_cr_passwd(board_id_t cpe,
                                   char *cr_passwd, size_t bufsize);
 
+/**
+ * Set ASC login username on CPE, that is login for authenticate
+ * CPE while starting CWMP session (CPE to ACS).
+ *
+ * @param cpe           ID of board with CPE.
+ * @param acs_login     username.
+ *
+ * @return status
+ */
 extern te_errno cpe_set_acs_login(board_id_t cpe,  const char *acs_login);
 
+/**
+ * Set ACS password on CPE for CWMP sessions.
+ *
+ * @param cpe           ID of board with CPE.
+ * @param acs_passwd    password.
+ *
+ * @return status
+ */
 extern te_errno cpe_set_acs_passwd(board_id_t cpe, const char *acs_passwd);
 
+/**
+ * Get ACS login username on CPE.
+ *
+ * @param cpe           ID of board with CPE.
+ * @param acs_login     location for username.
+ * @param bufsize       size of location buffer.
+ *
+ * @return status
+ */
 extern te_errno cpe_get_acs_login(board_id_t cpe,
                                   char *acs_login, size_t bufsize);
 
+/**
+ * Get ACS login password on CPE.
+ *
+ * @param cpe           ID of board with CPE.
+ * @param acs_passwd    location for password.
+ * @param bufsize       size of location buffer.
+ *
+ * @return status
+ */
 extern te_errno cpe_get_acs_passwd(board_id_t cpe,
                                    char *acs_passwd, size_t bufsize);
 
