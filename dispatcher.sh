5e5a5269a84271ce0b701776d4d9d12ddf0b8024
Elena Vengerova <Elena.Vengerova@oktetlabs.ru>
2010-06-21 16:34:55 +0000

    Work-around for latest libtool is added
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@64874 9d14d920-75d5-0310-8bd7-a52572981aee

--- dispatcher.sh
+++ dispatcher.sh
@@ -589,6 +589,10 @@ if test -n "${TRC_TAGS}" ; then
 fi
 
 # Build Test Environment
+
+# Workaround for bug in latest libtool: libtool defines ECHO, but uses $echo
+export echo=echo
+
 TE_BUILD_LOG="${TE_RUN_DIR}/build.log"
 if test -n "$BUILDER" ; then
     pushd "${TE_BASE}" >/dev/null
