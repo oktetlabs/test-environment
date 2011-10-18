632a1413b7edae2700619d355a99ef92129b764a
Yurij Plotnikov <Yurij.Plotnikov@oktetlabs.ru>
2011-10-18 10:43:59 +0000

    Fix for rpc_if_nametoindex() function
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@72127 9d14d920-75d5-0310-8bd7-a52572981aee

--- lib/tapi_rpc/ifnameindex.c
+++ lib/tapi_rpc/ifnameindex.c
@@ -68,8 +68,11 @@ rpc_if_nametoindex(rcf_rpc_server *rpcs,
 
     rcf_rpc_call(rpcs, "if_nametoindex", &in, &out);
 
-    CHECK_RETVAL_VAR(if_nametoindex, out.ifindex, FALSE, 0);
-    TAPI_RPC_LOG(rpcs, if_nametoindex, "%s", "%u", 
+    if (rpcs->errno_change_check && out.common.errno_changed)
+        ERROR("Function if_nametoindex() changed errno to %s",
+              errno_rpc2str(RPC_ERRNO(rpcs)));
+    CHECK_RETVAL_VAR(if_nametoindex, out.ifindex, FALSE, out.ifindex);
+    TAPI_RPC_LOG(rpcs, if_nametoindex, "%s", "%u",
                  ifname == NULL ? "" : ifname, out.ifindex);
     RETVAL_INT(if_nametoindex, out.ifindex);
 }
