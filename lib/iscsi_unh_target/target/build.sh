e849b676a7a1d19291bda3ec398958365d73fb00
Konstantin Ushakov <Konstantin.Ushakov@oktetlabs.ru>
2013-12-02 21:07:34 +0000

    [Bug 8302]: Remove the target!
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@84972 9d14d920-75d5-0310-8bd7-a52572981aee

--- lib/iscsi_unh_target/target/build.sh
+++
@@ -1,4 +0,0 @@
-make -C ../security clean all
-make -C ../common clean all
-cc -c my_login.c
-cc -o my_login my_login.o iscsi_portal_group.o ../common/common_functions.o ../security/security_functions.o ../common/target_negotiate.o -lpthread
