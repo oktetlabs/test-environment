484e3b1f3182986d0095c2e9c6394d8941d4e84d
Albert Podusenko <Albert.Podusenko@oktetlabs.ru>
2015-12-17 11:26:25 +0000

    little
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@92095 9d14d920-75d5-0310-8bd7-a52572981aee

--- agents/unix/conf/daemons/l2tp/l2tp_server.c
+++ agents/unix/conf/daemons/l2tp/l2tp_server.c
@@ -23,7 +23,7 @@
 #define PPP_OPTIONS           "pppoptfile"
 
 /** Default buffer size for command-line construction */
-#define L2TP_SERVER_LIST_SIZE 1024
+#define L2TP_SERVER_LIST_SIZE 2048
 
 /**
  * Default amount of memory allocated for list methods
