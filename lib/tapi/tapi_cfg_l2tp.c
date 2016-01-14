db660f31ae9434dc8eabde302f6803758be48b57
Albert Podusenko <Albert.Podusenko@oktetlabs.ru>
2016-01-14 16:18:46 +0000

    bug 8602: all APIs are written
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@92267 9d14d920-75d5-0310-8bd7-a52572981aee

--- lib/tapi/tapi_cfg_l2tp.c
+++ lib/tapi/tapi_cfg_l2tp.c
@@ -20,13 +20,17 @@
 /** Max length of the instance /ip_range: */
 #define L2TP_IP_RANGE_INST 35
 
+/** Max length of the secret instance  */
+#define L2TP_SECRET_INST 128
+
+
 /* All descriptions are in tapi_cfg_l2tp.h */
 
 te_errno
 tapi_cfg_l2tp_listen_ip_set(const char *ta, const char *lns,
                             struct sockaddr_in *local)
 {
-
+    UNUSED(lns);
     return cfg_set_instance_fmt(CFG_VAL(ADDRESS, local),
                                 TE_CFG_TA_L2TP_SERVER "/listen:", ta);
 }
@@ -36,6 +40,7 @@ te_errno
 tapi_cfg_l2tp_ip_get(const char *ta, const char *lns,
                      struct sockaddr_in *local)
 {
+    UNUSED(lns);
     return cfg_get_instance_fmt((cfg_val_type *) CVT_ADDRESS, local,
                                 TE_CFG_TA_L2TP_SERVER "/listen:", ta);
 }
@@ -94,7 +99,51 @@ tapi_cfg_l2tp_lns_range_del(const char *ta, const char *lns,
     return cfg_del_instance_fmt(FALSE, TE_CFG_TA_L2TP_SERVER
                                 "/lns:%s/%s_range:%s", ta, 
                                 lns, kind == L2TP_IP_RANGE_CLASS_IP ? 
-                                "ip" : "lac", l2tp_add_range(iprange))
+                                "ip" : "lac", l2tp_add_range(iprange));
+}
+
+te_errno
+tapi_cfg_l2tp_lns_connected_get(const char *ta, const char *lns,
+                                struct sockaddr_in ***connected)
+{
+    UNUSED(ta);
+    UNUSED(lns);
+    if (*connected != NULL)
+    {
+        return -1;
+    }
+    struct ifaddrs         *tmp;
+    struct sockaddr_in    **connected_mem;
+    struct sockaddr_in     *sin;
+    int    counter = 0;
+    if (getifaddrs(&tmp))
+    {
+        ERROR("getifaddrs error");
+        return -1;
+    }
+    else
+    {
+        for (tmp; tmp != NULL; tmp = tmp->ifa_next)
+        {
+
+            if (tmp->ifa_addr && tmp->ifa_addr->sa_family == AF_INET
+                && strstr(tmp->ifa_name, "ppp") != NULL
+                && tmp->ifa_ifu.ifu_dstaddr->sa_family == AF_INET)
+            {
+                counter++;
+                sin = (struct sockaddr_in *) tmp->ifa_ifu.ifu_dstaddr;
+                connected_mem = (struct sockaddr_in **) realloc(*connected,
+                                 counter * sizeof(struct sockaddr_in *));
+                if (connected_mem != NULL)
+                {
+                    connected_mem[counter - 1] = sin;
+                   *connected = connected_mem;
+
+                }
+            }
+        }
+    }
+    return 0;
 }
 
 te_errno
@@ -104,18 +153,18 @@ tapi_cfg_l2tp_lns_bit_set(const char *ta, const char *lns,
     return cfg_set_instance_fmt(CFG_VAL(INTEGER, selector),
                                 TE_CFG_TA_L2TP_SERVER "/lns:%s/bit:%s",
                                 ta, lns, bit == L2TP_BIT_HIDDEN ?
-                                "hidden" : mybit == L2TP_BIT_LENGTH ?
+                                "hidden" : bit == L2TP_BIT_LENGTH ?
                                 "length" : "flow");
 }
 
 te_errno
 tapi_cfg_l2tp_lns_bit_get(const char *ta, const char *lns,
-                          enum l2tp_bit *bit_name, bool *selector)
+                          enum l2tp_bit *bit, bool *selector)
 {
     return cfg_get_instance_fmt((cfg_val_type *) CVT_INTEGER, selector,
                                 TE_CFG_TA_L2TP_SERVER "/lns:%s/bit:%s",
-                                ta, lns, bit == L2TP_BIT_HIDDEN ?
-                                "hidden" : bit == L2TP_BIT_LENGTH ?
+                                ta, lns, *bit == L2TP_BIT_HIDDEN ?
+                                "hidden" : *bit == L2TP_BIT_LENGTH ?
                                 "length" : "flow");
 }
 
@@ -147,58 +196,80 @@ tapi_cfg_l2tp_lns_get_auth(const char *ta, const char *lns,
                                 "require" : "refuse");
 }
 
-/* It seems to be bad solution */
+/* It seems to be bad solution
+ * BEGIN */
+te_errno
+l2tp_add_secret_part(char *client, char *part, int8_t selector)
+{
+    cfg_handle  handle;
+
+    return cfg_add_instance_fmt(&handle, CFG_VAL(NONE, NULL),
+                                strcat(client, "%s:%s"),
+                                selector == L2TP_SECRET_PART_SERVER ?
+                                "server" : selector ==
+                                           L2TP_SECRET_PART_SECRET ?
+                                "secret" : "ipv4", part);
+};
+
+
 te_errno
 tapi_cfg_l2tp_lns_secret_add(const char *ta, const char *lns,
                              const l2tp_ppp_secret *new_secret)
 {
-    cfg_handle  handle;
 
-    char buffer[64] = {};
+    char buffer[L2TP_SECRET_INST] = {};
+
     TE_SPRINTF(buffer, TE_CFG_TA_L2TP_SECRET,
-               ta, lns, new_secret->is_chap == L2TP_AUTH_PROT_CHAP ? 
-               "chap" : "pap", new_secret->client)
+               ta, lns, new_secret->is_chap == L2TP_AUTH_PROT_CHAP ?
+                        "chap" : "pap", new_secret->client);
 
-    cfg_add_instance_fmt(&handle, CFG_VAL(NONE, NULL), 
-                         TE_CFG_TA_L2TP_SECRET "server:%s", 
-                         new_secret->server);
+    l2tp_add_secret_part(buffer, new_secret->server,
+                         L2TP_SECRET_PART_SERVER);
+    l2tp_add_secret_part(buffer, new_secret->secret,
+                         L2TP_SECRET_PART_SECRET);
+    l2tp_add_secret_part(buffer,
+                         inet_ntoa(new_secret->sipv4.sin_addr),
+                         L2TP_SECRET_PART_IPV4);
 
-    cfg_add_instance_fmt(&handle, CFG_VAL(NONE, NULL), 
-                         TE_CFG_TA_L2TP_SECRET "secret:%s", 
-                         new_secret->secret);
-
-    cfg_add_instance_fmt(&handle, CFG_VAL(NONE, NULL), 
-                         TE_CFG_TA_L2TP_SECRET "ipv4:%s", 
-                         inet_ntoa(new_secret->ipv4->sin_addr));
     return 0;
 
 }
 
 te_errno
-tapi_cfg_l2tp_lns_secret_delete(const char *ta, const char *lns,
-                                const ppp_secret *prev_secret)
+l2tp_delete_secret_part(char *client, char *part, int8_t selector)
 {
-    return cfg_del_instance_fmt(FALSE, TE_CFG_TA_L2TP_SERVER
-                                "/lns:%s/%s_range:%s", ta, 
-                                lns, kind == L2TP_IP_RANGE_CLASS_IP ? 
-                                "ip" : "lac", l2tp_add_range(iprange))
 
-    char buffer[64] = {};
+    return cfg_del_instance_fmt(FALSE, strcat(client, "%s:%s"),
+                                selector == L2TP_SECRET_PART_SERVER ?
+                                "server" : selector ==
+                                           L2TP_SECRET_PART_SECRET ?
+                                           "secret" : "ipv4", part);
+};
+
+te_errno
+tapi_cfg_l2tp_lns_secret_delete(const char *ta, const char *lns,
+                                const l2tp_ppp_secret *prev_secret)
+{
+
+    char buffer[L2TP_SECRET_INST] = {};
+
     TE_SPRINTF(buffer, TE_CFG_TA_L2TP_SECRET,
-               ta, lns, new_secret->is_chap == L2TP_AUTH_PROT_CHAP ? 
-               "chap" : "pap", prev_secret->client)
+               ta, lns, prev_secret->is_chap == L2TP_AUTH_PROT_CHAP ?
+                        "chap" : "pap", prev_secret->client);
 
-    cfg_add_instance_fmt(FALSE, TE_CFG_TA_L2TP_SECRET "server:%s", 
-                         new_secret->server);
+    l2tp_delete_secret_part(buffer, prev_secret->server,
+                            L2TP_SECRET_PART_SERVER);
+    l2tp_delete_secret_part(buffer, prev_secret->secret,
+                            L2TP_SECRET_PART_SECRET);
+    l2tp_delete_secret_part(buffer,
+                            inet_ntoa(prev_secret->sipv4.sin_addr),
+                            L2TP_SECRET_PART_IPV4);
 
-    cfg_add_instance_fmt(FALSE, TE_CFG_TA_L2TP_SECRET "secret:%s", , 
-                         new_secret->secret);
-
-    cfg_add_instance_fmt(FALSE, TE_CFG_TA_L2TP_SECRET "ipv4:%s", , 
-                         inet_ntoa(new_secret->ipv4->sin_addr));
     return 0;
 
 }
+/* It seems to be bad solution
+ * END */
 
 te_errno
 tapi_cfg_l2tp_lns_set_use_challenge(const char *ta, const char *lns,
@@ -213,7 +284,7 @@ te_errno
 tapi_cfg_l2tp_lns_get_use_challenge(const char *ta, const char *lns,
                                     bool value)
 {
-    return cfg_get_instance_fmt((cfg_val_type *) CVT_INTEGER, value,
+    return cfg_get_instance_fmt((cfg_val_type *) CVT_INTEGER, &value,
                                 TE_CFG_TA_L2TP_SERVER "/lns:%s/use_challenge:",
                                 ta, lns);
 }
@@ -231,7 +302,7 @@ te_errno
 tapi_cfg_l2tp_lns_get_unix_auth(const char *ta, const char *lns,
                                 bool value)
 {
-    return cfg_get_instance_fmt((cfg_val_type *) CVT_INTEGER, value,
+    return cfg_get_instance_fmt((cfg_val_type *) CVT_INTEGER, &value,
                                 TE_CFG_TA_L2TP_SERVER "/lns:%s/unix_auth:",
                                 ta, lns);
 }
@@ -324,9 +395,9 @@ te_errno
 tapi_cfg_l2tp_lns_pppopt_del(const char *ta, const char *lns,
                              const char *pparam)
 {
-    return cfg_del_instance_fmt(FALSE, TTE_CFG_TA_L2TP_SERVER
+    return cfg_del_instance_fmt(FALSE, TE_CFG_TA_L2TP_SERVER
                                 "/pppopt/option:%s", 
-                                ta, lns, pparam)
+                                ta, lns, pparam);
 }
 
 
