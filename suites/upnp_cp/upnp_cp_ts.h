9166251cd84b5e4055771faff17960e33b81ed5f
Ivan Melnikov <Ivan.Melnikov@oktetlabs.ru>
2016-03-01 10:10:55 +0000

    [Bug 8645] Test examples to test UPnP Control Point Framework
    git-svn-id: https://oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/branches/upnp_cp@92808 9d14d920-75d5-0310-8bd7-a52572981aee

---
+++ suites/upnp_cp/upnp_cp_ts.h
@@ -0,0 +1,26 @@
+/** @file
+ * @brief UPnP Control Point Test Suite
+ *
+ * Common includes and definitions.
+ *
+ * Copyright (C) 2016 OKTET Labs Ltd., St.-Petersburg, Russia
+ *
+ * @author Ivan Melnikov <Ivan.Melnikov@oktetlabs.ru>
+ *
+ * $Id$
+ */
+
+#ifndef __UPNP_CP_TS_H__
+#define __UPNP_CP_TS_H__
+
+#include "tapi_env.h"
+
+#ifndef TEST_START_VARS
+#define TEST_START_VARS TEST_START_ENV_VARS
+#endif /* TEST_START_VARS */
+
+#ifndef TEST_START_SPECIFIC
+#define TEST_START_SPECIFIC TEST_START_ENV;
+#endif /* TEST_START_SPECIFIC */
+
+#endif /* __UPNP_CP_TS_H__ */
