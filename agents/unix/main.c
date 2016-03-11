1d8994ad92dd67de13da2d893ae036203f182e48
Yurij Plotnikov <Yurij.Plotnikov@oktetlabs.ru>
2016-03-11 13:59:20 +0000

    [Bug 8621]:  "obsoleted waitpid entry with the same pid" message has been moved from INFO to WARN.
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@92909 9d14d920-75d5-0310-8bd7-a52572981aee

--- agents/unix/main.c
+++ agents/unix/main.c
@@ -1389,7 +1389,7 @@ ta_sigchld_handler(void)
             oldest = dead; /* Oldest entry is always the last */
             if ((pid == dead->pid) && dead->valid)
             {
-                INFO("Removing obsoleted entry with the same pid = %d, "
+                WARN("Removing obsoleted entry with the same pid = %d, "
                      "status = 0x%x from the list of dead children.", 
                      dead->pid, dead->status);
                 SLIST_REMOVE(&ta_children_dead_list, dead,
