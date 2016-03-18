f75fc24adfa559d39ad9caf42f84de8bafb31af8
Dmitry Izbitsky <Dmitry.Izbitsky@oktetlabs.ru>
2016-03-18 07:22:34 +0000

    [Bug 8692]:  Fixing file2file()
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@92977 9d14d920-75d5-0310-8bd7-a52572981aee

--- tools/rgt/rgt-log-bundle/rgt_log_bundle_common.c
+++ tools/rgt/rgt-log-bundle/rgt_log_bundle_common.c
@@ -29,13 +29,6 @@
  * $Id$
  */
 
-#ifndef __TE_RGT_LOG_BUNDLE_COMMON_H__
-#define __TE_RGT_LOG_BUNDLE_COMMON_H__
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <popt.h>
@@ -44,43 +37,43 @@ extern "C" {
 #include "te_defs.h"
 #include "logger_api.h"
 
-/**
- * Copy data from one file to another.
- *
- * @param out_f       Destination file pointer
- * @param in_ f       Source file pointer
- * @param out_offset  At which offset to write data in
- *                    destination file (if < 0, then at
- *                    current positon)
- * @param in_offset   At which offset to read data from
- *                    source file (if < 0, then at
- *                    current positon)
- * @param length      Length of data to be copied
- *
- * @return 0 on success, -1 on failure
- */
-extern int file2file(FILE *out_f, FILE *in_f,
-                     off_t out_offset,
-                     off_t in_offset, off_t length);
-
-static inline void
-usage(poptContext optCon, int exitcode, char *error, char *addl)
+/* See the description in rgt_log_bundle_common.h */
+int
+file2file(FILE *out_f, FILE *in_f,
+          off_t out_offset,
+          off_t in_offset, off_t length)
 {
-    poptPrintUsage(optCon, stderr, 0);
-    if (error)
+#define BUF_SIZE  4096
+    char   buf[BUF_SIZE];
+    size_t bytes_read;
+
+    if (out_offset >= 0)
+        fseeko(out_f, out_offset, SEEK_SET);
+    if (in_offset >= 0)
+        fseeko(in_f, in_offset, SEEK_SET);
+
+    while (length > 0)
     {
-        fprintf(stderr, "%s", error);
-        if (addl != NULL)
-            fprintf(stderr, ": %s", addl);
-        fprintf(stderr, "\n");
+        bytes_read = (length > BUF_SIZE ? BUF_SIZE : length);
+        bytes_read = fread(buf, 1, bytes_read, in_f);
+
+        if (bytes_read > 0)
+        {
+            fwrite(buf, 1, bytes_read, out_f);
+            length -= bytes_read;
+        }
+
+        if (feof(in_f))
+            break;
     }
 
-    poptFreeContext(optCon);
+    if (length > 0)
+    {
+        ERROR("Failed to copy last %llu bytes to file",
+              (long long unsigned int)length);
+        return -1;
+    }
 
-    exit(exitcode);
+    return 0;
+#undef BUF_SIZE
 }
-
-#ifdef __cplusplus
-} /* extern "C" */
-#endif
-#endif /* __TE_RGT_LOG_BUNDLE_COMMON_H__ */
