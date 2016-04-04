fc6dee7e24e53df8e6abdc103f87d78b81358847
Ivan Melnikov <Ivan.Melnikov@oktetlabs.ru>
2016-04-04 11:12:29 +0000

    [Bug 8657] Implemented a test API to extract local files list from configurator
    git-svn-id: https://oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/branches/upnp_cp@93198 9d14d920-75d5-0310-8bd7-a52572981aee

---
+++ lib/tapi_storage/tapi_local_fs.c
@@ -0,0 +1,386 @@
+/** @file
+ * @brief Test API to local file system routines
+ *
+ * Functions for convenient work with the file system mapped on configurator
+ * tree.
+ *
+ * Copyright (C) 2016 Test Environment authors (see file AUTHORS
+ * in the root directory of the distribution).
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA  02111-1307  USA
+ *
+ *
+ * @author Ivan Melnikov <Ivan.Melnikov@oktetlabs.ru>
+ *
+ * $Id$
+ */
+
+#define TE_LGR_USER     "TAPI Local File System"
+
+#include "tapi_local_fs.h"
+
+#ifdef HAVE_STRING_H
+#include <string.h>
+#endif
+#if HAVE_ERRNO_H
+#include <errno.h>
+#endif
+
+#include "te_string.h"
+#include "te_alloc.h"
+#include "logger_api.h"
+#include "conf_api.h"
+
+
+/* Format string for local file system entry in Configuration tree. */
+#define TE_CFG_LOCAL_FS_FMT   "/local:/fs:"
+/* Subid of directory item in Configurator tree. */
+#define SUBID_DIR   "/directory:"
+/* Subid of file item in Configurator tree. */
+#define SUBID_FILE  "/file:"
+/* Subid of file property item in Configurator tree. */
+#define SUBID_FILE_PROPERTY "/property:"
+/* Subid of file metadata item in Configurator tree. */
+#define SUBID_FILE_METADATA "/metadata:"
+
+
+
+/**
+ * Make pathname (concatenate name with path).
+ * Return value should be freed with free when it is no longer needed.
+ *
+ * @param path          Path to the file.
+ * @param name          Name of file.
+ *
+ * @return Pathname.
+ */
+static char *
+make_pathname(const char *path, const char *name)
+{
+    char   *pathname;
+    size_t  l;
+
+    assert(path != NULL && name != NULL);
+    l = strlen(path);
+    pathname = TE_ALLOC(l + strlen(name) + 2); /* +2: '/' and '\0' */
+    if (pathname != NULL)
+    {
+        /* Make pathname as 'path/name'. */
+        strcpy(pathname, path);
+        pathname[l] = '/';
+        strcpy(&pathname[l + 1], name);
+    }
+    return pathname;
+}
+
+/**
+ * Get properties of local file from configurator.
+ *
+ * @param[in]    localfs_path   Local file system pathname.
+ * @param[in]    name           Name of file.
+ * @param[inout] file           Local file.
+ *
+ * @return Status code.
+ */
+static te_errno
+get_local_file_properties(const char      *localfs_path,
+                          const char      *name,
+                          tapi_local_file *file)
+{
+    te_errno  rc;
+    char     *property;
+
+    errno = 0;
+    /* Retrieve a size property. */
+    rc = cfg_get_instance_fmt(NULL, &property, "%s" SUBID_FILE
+                              "%s" SUBID_FILE_PROPERTY "size",
+                              localfs_path, name);
+    if (rc != 0)
+        return rc;
+
+    file->property.size = strtoull(property, NULL, 10);
+    rc = TE_OS_RC(TE_TAPI, errno);
+    free(property);
+    if (rc != 0)
+        return rc;
+
+    /* Retrieve a date property. */
+    rc = cfg_get_instance_fmt(NULL, &property, "%s" SUBID_FILE
+                              "%s" SUBID_FILE_PROPERTY "date",
+                              localfs_path, name);
+    if (rc != 0)
+        return rc;
+
+    file->property.date.tv_sec = strtol(property, NULL, 10);
+    rc = TE_OS_RC(TE_TAPI, errno);
+    free(property);
+    if (rc != 0)
+        return rc;
+
+    return 0;
+}
+
+/**
+ * Free local file list entry.
+ *
+ * @param file      Local file list entry.
+ */
+static void
+free_local_file_le(tapi_local_file_le *file)
+{
+    free((char *)file->file.pathname);
+    free(file);
+}
+
+/**
+ * Convert regular pathname to local file system pathname.
+ * For example, transform directory '/foo' to '/local:/fs:/directory:foo',
+ * but file '/foo/bar' to '/local:/fs:/directory:foo/file:bar'
+ * Return value should be freed with free when it is no longer needed.
+ *
+ * @param type          Type of local file.
+ * @param pathname      Regular pathname.
+ *
+ * @return Local file system pathname, of @c NULL in case of failure.
+ *
+ * FIXME: Add escaping since configurator will get ability to escape special
+ * symbols.
+ */
+static char *
+local_fs_get_pathname(tapi_local_file_type type, const char *pathname)
+{
+    te_errno    rc;
+    te_string   localfs = TE_STRING_INIT;
+    char       *str;
+    char       *token;
+    char       *basename = NULL;
+
+    str = TE_ALLOC(strlen(pathname) + 1);
+    if (str == NULL)
+        return NULL;
+    strcpy(str, pathname);
+    if (type == TAPI_FILE_TYPE_FILE)
+    {
+        basename = strrchr(str, '/');
+        if (basename != NULL)
+            *basename++ = '\0';
+    }
+
+    rc = te_string_append(&localfs, "%s", TE_CFG_LOCAL_FS_FMT);
+    if (rc != 0)
+    {
+        free(str);
+        te_string_free(&localfs);
+        return NULL;
+    }
+
+    for (token = strtok(str, "/");
+         token != NULL;
+         token = strtok(NULL, "/"))
+    {
+        rc = te_string_append(&localfs, "%s%s", SUBID_DIR, token);
+        if (rc != 0)
+        {
+            free(str);
+            te_string_free(&localfs);
+            return NULL;
+        }
+    }
+    if (type == TAPI_FILE_TYPE_FILE && basename != NULL &&
+        *basename != '\0')
+    {
+        rc = te_string_append(&localfs, "%s%s", SUBID_FILE, basename);
+        if (rc != 0)
+        {
+            free(str);
+            te_string_free(&localfs);
+            return NULL;
+        }
+    }
+    free(str);
+    return localfs.ptr;
+}
+
+/**
+ * Get list of local files from particular path of configurator.
+ *
+ * @param[in]  type             Local file type.
+ * @param[in]  localfs_path     Local file system pathname.
+ * @param[in]  path             Regular path to the file, it needed for
+ *                              creation of pathname to avoid extract
+ *                              regular path from @p localfs_path.
+ * @param[out] files            Files list.
+ *
+ * @return Status code.
+ *
+ * FIXME: Add unescaping since configurator will get ability to escape
+ * special symbols.
+ */
+static te_errno
+get_local_files(tapi_local_file_type  type,
+                const char           *localfs_path,
+                const char           *path,
+                tapi_local_file_list *files)
+{
+    te_errno     rc;
+    cfg_handle  *handles;
+    unsigned int n_handles;
+    char        *name;
+    size_t       i;
+    tapi_local_file_le *file;
+
+    if (type == TAPI_FILE_TYPE_DIRECTORY)
+        rc = cfg_find_pattern_fmt(&n_handles, &handles, "%s" SUBID_DIR "*",
+                                  localfs_path);
+    else if (type == TAPI_FILE_TYPE_FILE)
+        rc = cfg_find_pattern_fmt(&n_handles, &handles, "%s" SUBID_FILE "*",
+                                  localfs_path);
+    else
+        return TE_RC(TE_TAPI, TE_EINVAL);
+
+    if (rc != 0)
+        return rc;
+
+    for (i = 0; i < n_handles; i++)
+    {
+        rc = cfg_get_inst_name(handles[i], &name);
+        if (rc != 0)
+            return rc;
+        file = TE_ALLOC(sizeof(*file));
+        if (file == NULL)
+            return TE_RC(TE_TAPI, TE_ENOMEM);
+        file->file.type = type;
+        file->file.pathname = make_pathname(path, name);
+        if (file->file.pathname == NULL)
+        {
+            free_local_file_le(file);
+            return TE_RC(TE_TAPI, TE_ENOMEM);
+        }
+        if (type == TAPI_FILE_TYPE_FILE)
+        {
+            rc = get_local_file_properties(localfs_path, name, &file->file);
+            if (rc != 0)
+            {
+                ERROR("Failed to get local file properties");
+                free_local_file_le(file);
+                return rc;
+            }
+        }
+        SLIST_INSERT_HEAD(files, file, next);
+    }
+    return 0;
+}
+
+
+/* See description in tapi_local_fs.h. */
+te_errno
+tapi_local_fs_ls(const char *pathname, tapi_local_file_list *files)
+{
+    te_errno  rc;
+    char     *localfs_path;
+
+    assert(pathname != NULL);
+    SLIST_INIT(files);
+    localfs_path = local_fs_get_pathname(TAPI_FILE_TYPE_DIRECTORY,
+                                         pathname);
+    rc = get_local_files(TAPI_FILE_TYPE_DIRECTORY, localfs_path, pathname,
+                         files);
+    if (rc == 0)
+        rc = get_local_files(TAPI_FILE_TYPE_FILE, localfs_path, pathname,
+                             files);
+    if (rc != 0)
+        tapi_local_fs_list_free(files);
+
+    free(localfs_path);
+    return rc;
+}
+
+/* See description in tapi_local_fs.h. */
+void
+tapi_local_fs_list_free(tapi_local_file_list *files)
+{
+    tapi_local_file_le *file;
+
+    while (!SLIST_EMPTY(files))
+    {
+        file = SLIST_FIRST(files);
+        SLIST_REMOVE_HEAD(files, next);
+        free_local_file_le(file);
+    }
+}
+
+/* See description in tapi_local_fs.h. */
+te_errno
+tapi_local_fs_traverse(const char                *pathname,
+                       tapi_local_fs_traverse_cb  cb_pre,
+                       tapi_local_fs_traverse_cb  cb_post,
+                       void                      *user_data)
+{
+    tapi_local_file_list  files;
+    tapi_local_file_le   *f;
+    te_errno              rc = 0;
+
+    rc = tapi_local_fs_ls(pathname, &files);
+    if (rc == 0)
+        SLIST_FOREACH(f, &files, next)
+        {
+            if (cb_pre != NULL)
+            {
+                rc = cb_pre(&f->file, user_data);
+                if (rc != 0)
+                    break;
+            }
+
+            if (f->file.type == TAPI_FILE_TYPE_DIRECTORY)
+            {
+                rc = tapi_local_fs_traverse(f->file.pathname, cb_pre,
+                                            cb_post, user_data);
+                if (rc != 0)
+                    break;
+            }
+
+            if (cb_post != NULL)
+            {
+                rc = cb_post(&f->file, user_data);
+                if (rc != 0)
+                    break;
+            }
+        }
+    tapi_local_fs_list_free(&files);
+    return rc;
+}
+
+/* See description in tapi_local_fs.h. */
+te_errno
+tapi_local_fs_get_file_metadata(const char  *pathname,
+                                const char  *metaname,
+                                char       **metadata)
+{
+    te_errno  rc;
+    char     *localfs_path;
+
+    assert(pathname != NULL && metaname != NULL);
+    localfs_path = local_fs_get_pathname(TAPI_FILE_TYPE_FILE, pathname);
+    if (localfs_path == NULL)
+        return TE_RC(TE_TAPI, TE_ENOMEM);
+
+    rc = cfg_get_instance_fmt(NULL, metadata, "%s" SUBID_FILE_METADATA
+                              "%s", localfs_path, metaname);
+    if (rc != 0)
+        ERROR("Failed to get metadata \"%s\": %s",
+              metaname, te_rc_err2str(rc));
+    return rc;
+}
--- lib/tapi_storage/tapi_local_fs.h
+++ lib/tapi_storage/tapi_local_fs.h
@@ -1,7 +1,7 @@
 /** @file
  * @brief Test API to local file system routines
  *
- * Functions for convinient work with the file system mapped on configurator
+ * Functions for convenient work with the file system mapped on configurator
  * tree.
  *
  * Copyright (C) 2016 Test Environment authors (see file AUTHORS
@@ -31,7 +31,10 @@
 #ifndef __TAPI_LOCAL_FS_H__
 #define __TAPI_LOCAL_FS_H__
 
+#include "te_errno.h"
+#include "te_queue.h"
 #include "tapi_local_file.h"
+
 
 #ifdef __cplusplus
 extern "C" {
@@ -59,7 +62,7 @@ typedef struct tapi_local_file_list tapi_local_file_list;
  * @param var_      Pointer to the files list entry.
  * @param head_     Pointer to the head of the list.
  * @param file_     Pointer to the file which is represented by
- *                  @b tapi_local_file structure.
+ *                  @p tapi_local_file structure.
  *
  * @sa tapi_local_file_le, tapi_local_file
  */
@@ -74,7 +77,7 @@ typedef struct tapi_local_file_list tapi_local_file_list;
  * @param var_      Pointer to the files list entry.
  * @param head_     Pointer to the head of the list.
  * @param file_     Pointer to the file which is represented by
- *                  @b tapi_local_file structure.
+ *                  @p tapi_local_file structure.
  *
  * @sa tapi_local_file_le, tapi_local_file
  */
@@ -90,7 +93,7 @@ typedef struct tapi_local_file_list tapi_local_file_list;
  * @param var_      Pointer to the files list entry.
  * @param head_     Pointer to the head of the list.
  * @param dir_      Pointer to the directory which is represented by
- *                  @b tapi_local_file structure.
+ *                  @p tapi_local_file structure.
  *
  * @sa tapi_local_file_le, tapi_local_file
  */
@@ -101,68 +104,118 @@ typedef struct tapi_local_file_list tapi_local_file_list;
         if (tapi_local_file_is_dir(dir_))
 
 /**
- * Recursive loop 'for' to iterate through all file list entries.
+ * Recursive loop 'for' to iterate through all local file system files.
  *
- * @param var_      Pointer to the files list entry.
- * @param head_     Pointer to the head of the list.
- * @param file_     Pointer to the file which is represented by
- *                  @b tapi_local_file structure.
+ * @param pathname_     Pathname to start traverse from.
+ * @param cb_func_      Callback function which is called on each element of
+ *                      local file system before traverse the next elements.
+ * @param user_data_    Additional data to post to @p cb_func_ function,
+ *                      or @c NULL.
  *
- * @sa tapi_local_file_le, tapi_local_file
- *
- * @todo
+ * @sa tapi_local_fs_traverse, tapi_local_fs_traverse_cb
  */
-#define TAPI_LOCAL_FS_FOREACH_RECURSIVE(var_, head_, file_)
-
-/**
- * Recursive loop 'for' to iterate through all file list entries of regular
- * file type.
- *
- * @param var_      Pointer to the files list entry.
- * @param head_     Pointer to the head of the list.
- * @param file_     Pointer to the file which is represented by
- *                  @b tapi_local_file structure.
- *
- * @sa tapi_local_file_le, tapi_local_file
- *
- * @todo
- */
-#define TAPI_LOCAL_FS_FOREACH_FILE_RECURSIVE(var_, head_, file_)
-
-/**
- * Recursive loop 'for' to iterate through all file list entries of
- * directory type.
- *
- * @param var_      Pointer to the files list entry.
- * @param head_     Pointer to the head of the list.
- * @param dir_      Pointer to the directory which is represented by
- *                  @b tapi_local_file structure.
- *
- * @sa tapi_local_file_le, tapi_local_file
- *
- * @todo
- */
-#define TAPI_LOCAL_FS_FOREACH_DIR_RECURSIVE(var_, head_, dir_)
+#define TAPI_LOCAL_FS_FOREACH_RECURSIVE(pathname_, cb_func_, user_data_) \
+    tapi_local_fs_traverse(pathname_, cb_func_, NULL, user_data_)
 
 
 /**
- * Get files list from /local/fs configurator tree. Files should be released
- * with @sa tapi_local_fs_list_free when they are no longer needed.
+ * Prototype of handler for using in @p tapi_local_fs_traverse.
+ *
+ * @param file         Local file which is represented by @p tapi_local_file
+ *                     structure.
+ * @param user_data    Opaque user data.
+ *
+ * @return Status code.
+ *
+ * @sa tapi_local_fs_traverse
+ */
+typedef te_errno (*tapi_local_fs_traverse_cb)(tapi_local_file *file,
+                                              void            *user_data);
+
+/**
+ * Traverse the local file system. It calls @p tapi_local_fs_ls to obtain
+ * subitems of @p files to recursive traverse of files tree and then
+ * @p tapi_local_fs_list_free to free obtained items. This function is not
+ * suitable to change the file system tree structure but only to manipulate
+ * on it elements.
+ *
+ * @param pathname      Pathname to start traverse from.
+ * @param cb_pre        Callback function which is called on each element of
+ *                      local file system before traverse the next elements,
+ *                      or @c NULL if it is @p cb_post only needed.
+ * @param cb_post       Callback function which is called on each element of
+ *                      local file system after traverse the next elements,
+ *                      or @c NULL if it is @p cb_pre only needed.
+ * @param user_data     Additional data to post to @p cb_pre and
+ *                      @p cb_post handlers, or @c NULL.
+ *
+ * @return Status code.
+ *
+ * @sa tapi_local_fs_ls, tapi_local_fs_list_free
+ */
+extern te_errno tapi_local_fs_traverse(
+                                    const char                *pathname,
+                                    tapi_local_fs_traverse_cb  cb_pre,
+                                    tapi_local_fs_traverse_cb  cb_post,
+                                    void                      *user_data);
+
+/**
+ * Get root files list from /local/fs configurator tree.
+ * It calls @p tapi_local_fs_ls to get the files. @p files should be
+ * released with @p tapi_local_fs_list_free when they are no longer needed.
+ *
+ * @param[out] files        Files list.
+ *
+ * @return Status code
+ *
+ * @sa tapi_local_fs_ls, tapi_local_fs_list_free
+ */
+static inline te_errno
+tapi_local_fs_ls_root(tapi_local_file_list *files)
+{
+    te_errno tapi_local_fs_ls(const char           *pathname,
+                              tapi_local_file_list *files);
+
+    return tapi_local_fs_ls("", files);
+}
+
+
+/**
+ * Get files list from /local/fs configurator tree. @p files should be
+ * released with @p tapi_local_fs_list_free when they are no longer needed.
  *
  * @param[in]  pathname     Pathname of file list.
  * @param[out] files        Files list.
  *
  * @return Status code
+ *
+ * @sa tapi_local_fs_list_free
  */
 extern te_errno tapi_local_fs_ls(const char           *pathname,
                                  tapi_local_file_list *files);
 
 /**
- * Free files list that was obtained with @sa tapi_local_fs_ls.
+ * Free files list that was obtained with @p tapi_local_fs_ls.
  *
  * @param files     Files list.
+ *
+ * @sa tapi_local_fs_ls
  */
 extern void tapi_local_fs_list_free(tapi_local_file_list *files);
+
+/**
+ * Get string representation of metadata of local file from configurator.
+ * @p metadata should be freed with free when it is no longer needed.
+ *
+ * @param[in]  pathname     Pathname of file which metadata should be get.
+ * @param[in]  metaname     Metadata name.
+ * @param[out] metadata     Metadata value.
+ *
+ * @return Status code.
+ */
+extern te_errno tapi_local_fs_get_file_metadata(const char  *pathname,
+                                                const char  *metaname,
+                                                char       **metadata);
 
 #ifdef __cplusplus
 } /* extern "C" */
