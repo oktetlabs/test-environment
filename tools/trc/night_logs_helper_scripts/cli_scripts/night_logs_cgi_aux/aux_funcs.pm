fe68e79765246249471d33699c538bc460d6b993
Dmitry Izbitsky <Dmitry.Izbitsky@oktetlabs.ru>
2016-05-03 11:40:45 +0000

    [Bug 8640]:  Partial implementation of interface for ignoring iterations
    git-svn-id: https://svn.oktetlabs.ru/svnroot/prj/oktetlabs/test-environment/src/trunk@93446 9d14d920-75d5-0310-8bd7-a52572981aee

--- tools/trc/night_logs_helper_scripts/cli_scripts/night_logs_cgi_aux/aux_funcs.pm
+++ tools/trc/night_logs_helper_scripts/cli_scripts/night_logs_cgi_aux/aux_funcs.pm
@@ -1,7 +1,13 @@
 use strict;
+use warnings;
+use Fcntl qw/:flock/;
+use Storable qw/nstore_fd fd_retrieve/;
+use Data::Dumper;
+
 package night_logs_cgi_aux::aux_funcs;
 use base 'Exporter';
-our @EXPORT = qw(load_caches_list get_cli_path);
+our @EXPORT = qw(load_caches_list get_cli_path
+                 get_compromised compromise_iter uncompromise_iter);
 
 sub get_cli_path
 {
@@ -27,3 +33,127 @@ sub load_caches_list
     return @caches;
 }
 
+sub get_cache_by_id
+{
+    my $cache_id = $_[0];
+    my @caches = load_caches_list();
+    my $cache;
+
+    foreach my $c (@caches)
+    {
+        if ($c->{"id"} eq $cache_id)
+        {
+            $cache = $c;
+            last;
+        }
+    }
+
+    return $cache;
+}
+
+sub get_compromised
+{
+    my $cache_path = $_[0];
+    my $compromised = { "logs" => {}, "iters" => {} };
+
+    if (!(-e $cache_path."/compromised"))
+    {
+        return $compromised;
+    }
+
+    open (my $fh, '<', $cache_path."/compromised") or
+      (print "Cannot open $cache_path/compromised: $!" and return undef);
+    flock($fh, Fcntl::LOCK_SH);
+    $compromised = Storable::fd_retrieve($fh);
+    flock($fh, Fcntl::LOCK_UN);
+    close($fh);
+
+    return $compromised;
+}
+
+sub proc_iter
+{
+    my $cache_path = $_[0];
+    my $iter = $_[1];
+    my $result = $_[2];
+    my $logs = $_[3];
+    my $compromise = $_[4];
+    my $compromised;
+    my $iters;
+
+    $compromised = get_compromised($cache_path) or return -1;
+    $iters = $compromised->{"iters"};
+
+    if ($compromise > 0)
+    {
+        if (!defined($iters->{$iter}))
+        {
+            $iters->{$iter} = {};
+        }
+        if (!defined($iters->{$iter}->{$result}))
+        {
+            $iters->{$iter}->{$result} = {};
+        }
+
+        foreach my $log (@{$logs})
+        {
+            $iters->{$iter}->{$result}->{$log} = 1;
+        }
+    }
+    else
+    {
+        if (defined($iters->{$iter}) && defined($iters->{$iter}->{$result}))
+        {
+            foreach my $log (@{$logs})
+            {
+                delete $iters->{$iter}->{$result}->{$log};
+            }
+
+            if (scalar(keys %{$iters->{$iter}->{$result}}) == 0)
+            {
+                delete $iters->{$iter}->{$result};
+            }
+            if (scalar(keys %{$iters->{$iter}}) == 0)
+            {
+                delete $iters->{$iter};
+            }
+        }
+    }
+
+    if (!(-e $cache_path."/compromised"))
+    {
+        system("touch ".$cache_path."/compromised");
+    }
+
+    open(my $fh, '+<', $cache_path."/compromised") or
+        (print "Cannot open $cache_path/compromised: $!\n" and
+         return -1);
+    flock($fh, Fcntl::LOCK_EX);
+    Storable::nstore($compromised, $cache_path."/compromised_tmp");
+    system("mv ".$cache_path."/compromised_tmp ".
+           $cache_path."/compromised");
+    flock($fh, Fcntl::LOCK_UN);
+    close($fh);
+
+    return 0;
+}
+
+sub compromise_iter
+{
+    my $cache_path = $_[0];
+    my $iter = $_[1];
+    my $result = $_[2];
+    my $logs = $_[3];
+
+    return proc_iter($cache_path, $iter, $result, $logs, 1);
+}
+
+sub uncompromise_iter
+{
+    my $cache_path = $_[0];
+    my $iter = $_[1];
+    my $result = $_[2];
+    my $logs = $_[3];
+
+    return proc_iter($cache_path, $iter, $result, $logs, 0);
+}
