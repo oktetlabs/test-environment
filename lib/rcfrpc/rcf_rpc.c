a7f11c8bfebf3f3b83b9d23d63f3c00eaf0f7037
Oleg Sadakov <Oleg.Sadakov@oktetlabs.ru>
2016-08-08 17:25:06 +0300

    bug8917: save and restore the field op in a rcf_rpc_server
    Previously, functions rcf_rpc_server_is_op_done and rcf_rpc_setlibname
    update the field op in a rcf_rpc_server. It causes that the next rpc_call
    is incorrect.

--- lib/rcfrpc/rcf_rpc.c
+++ lib/rcfrpc/rcf_rpc.c
@@ -688,6 +688,7 @@ rcf_rpc_server_is_op_done(rcf_rpc_server *rpcs, te_bool *done)
 {
     tarpc_rpc_is_op_done_in  in;
     tarpc_rpc_is_op_done_out out;
+    rcf_rpc_op               old_op;
 
     if (rpcs == NULL || done == NULL)
         return TE_RC(TE_RCF, TE_EINVAL);
@@ -695,8 +696,10 @@ rcf_rpc_server_is_op_done(rcf_rpc_server *rpcs, te_bool *done)
     memset(&in, 0, sizeof(in));
     memset(&out, 0, sizeof(out));
 
+    old_op = rpcs->op;
     rpcs->op = RCF_RPC_CALL_WAIT;
     rcf_rpc_call(rpcs, "rpc_is_op_done", &in, &out);
+    rpcs->op = old_op;
 
     if (rpcs->_errno != 0)
     {
@@ -747,6 +750,7 @@ rcf_rpc_setlibname(rcf_rpc_server *rpcs, const char *libname)
 {
     tarpc_setlibname_in  in;
     tarpc_setlibname_out out;
+    rcf_rpc_op           old_op;
 
     memset(&in, 0, sizeof(in));
     memset(&out, 0, sizeof(out));
@@ -760,8 +764,10 @@ rcf_rpc_setlibname(rcf_rpc_server *rpcs, const char *libname)
     in.libname.libname_len = (libname == NULL) ? 0 : (strlen(libname) + 1);
     in.libname.libname_val = (char *)libname;
 
+    old_op = rpcs->op;
     rpcs->op = RCF_RPC_CALL_WAIT;
     rcf_rpc_call(rpcs, "setlibname", &in, &out);
+    rpcs->op = old_op;
 
     if (!RPC_IS_CALL_OK(rpcs))
         out.retval = -1;
