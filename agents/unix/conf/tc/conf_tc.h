f8e87f1caed818041155928f611622b88a8e01c2
Nikita Somenkov <Nikita.Somenkov@oktetlabs.ru>
2018-05-22 17:32:02 +0300

    [2/7] Bug 9673: Add Traffic Control subtree

---
+++ agents/unix/conf/tc/conf_tc.h
@@ -0,0 +1,28 @@
+/** @file
+ * @brief Unix TA Traffic Control configuration support
+ *
+ * Implementation of configuration nodes traffic control support
+ *
+ * Copyright (C) 2018 OKTET Labs. All rights reserved.
+ *
+ * @author Nikita Somenkov <Nikita.Somenkov@oktetlabs.ru>
+ */
+
+#ifndef __TE_AGENTS_UNIX_CONF_CONF_TC_H_
+#define __TE_AGENTS_UNIX_CONF_CONF_TC_H_
+
+#include "te_errno.h"
+
+/**
+ * Initialization of Traffic Control configuration
+ *
+ * @return Status code
+ */
+extern te_errno ta_unix_conf_tc_init(void);
+
+/**
+ * Clean up of Traffic Control configuration
+ */
+extern void ta_unix_conf_tc_fini(void);
+
+#endif /* __TE_AGENTS_UNIX_CONF_CONF_TC_H_ */
