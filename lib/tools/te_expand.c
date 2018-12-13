116b3751e73c7bd05630b6e6446c1db8040c2e27
Denis Pryazhennikov <Denis.Pryazhennikov@oktetlabs.ru>
2018-12-13 19:52:12 +0300

    Bug 9823: expand te_expand lib to handle custom parameters

--- lib/tools/te_expand.c
+++ lib/tools/te_expand.c
@@ -5,8 +5,9 @@
  *
  * Copyright (C) 2018 OKTET Labs. All rights reserved.
  *
- * @author Artem Andreev <Artem.Andreev@oktetlabs.ru>
+ * @author Denis Pryazhennikov <Denis.Pryazhennikov@oktetlabs.ru>
  */
+
 #include "te_defs.h"
 
 #ifdef STDC_HEADERS
@@ -19,21 +20,52 @@
 #include <sys/types.h>
 #endif
 
-#include "logger_api.h"
 #include "te_expand.h"
 
+/**
+ * Get parameter value by name from environment.
+ *
+ * @param param_name    Name of required value
+ *
+ * @retval   Requested @p param_name value if found, otherwise @c NULL
+ */
+static char *
+te_get_env_value(const char *param_name, void *params_ctx)
+{
+    UNUSED(params_ctx);
+
+    return getenv(param_name);
+}
+
+/**
+ * Get parameter value by name from queue of key-value pairs.
+ *
+ * @param param_name    Name of required value
+ * @param params_ctx    Head of queue of key-value pairs
+ *
+ * @retval   Requested @p param_name value if found, otherwise @c NULL
+ */
+static char *
+te_get_kvpairs_value(const char *param_name, void *params_ctx)
+{
+    return te_kvpairs_get((te_kvpair_h *)params_ctx, param_name);
+}
+
+/* See description in te_expand.h */
 int
-te_expand_env_vars(const char *src, char **posargs, char **retval)
+te_expand_parameters(const char *src, char **posargs,
+                     te_param_value_getter get_param_value,
+                     void *params_ctx, char **retval)
 {
     const char *next = NULL;
     char       *result = NULL;
     int         result_len = 0;
     int         len;
-    char        env_var_name[128];
+    char        param_name[TE_EXPAND_PARAM_NAME_LEN];
     char       *default_value;
-    char       *env_var;
+    char       *param_var;
     int         brace_level = 0;
-    te_bool     need_free_env = FALSE;
+    te_bool     need_param_free = FALSE;
 
     for (;;)
     {
@@ -74,15 +106,15 @@ te_expand_env_vars(const char *src, char **posargs, char **retval)
             }
         }
 
-        if ((unsigned)(src - next - 1) > sizeof(env_var_name))
+        if ((unsigned)(src - next - 1) > sizeof(param_name))
         {
             free(result);
             *retval = NULL;
             return ENOBUFS;
         }
-        memcpy(env_var_name, next, src - next - 1);
-        env_var_name[src - next - 1] = '\0';
-        default_value = strchr(env_var_name, ':');
+        memcpy(param_name, next, src - next - 1);
+        param_name[src - next - 1] = '\0';
+        default_value = strchr(param_name, ':');
         if (default_value != NULL)
         {
             if (default_value[1] == '+' || default_value[1] == '-')
@@ -94,22 +126,23 @@ te_expand_env_vars(const char *src, char **posargs, char **retval)
                 default_value = NULL;
             }
         }
-        if (isdigit(*env_var_name) && env_var_name[1] == '\0')
+        if (isdigit(*param_name) && param_name[1] == '\0')
         {
-            env_var = (posargs == NULL ? NULL :
-                       posargs[*env_var_name - '0']);
+            param_var = (posargs == NULL ? NULL :
+                         posargs[*param_name - '0']);
         }
         else
         {
-            env_var = getenv(env_var_name);
+            param_var = get_param_value(param_name, params_ctx);
         }
         if (default_value != NULL)
         {
-            if ((*default_value == '+' && env_var != NULL) ||
-                (*default_value == '-' && env_var == NULL))
+            if ((*default_value == '+' && param_var != NULL) ||
+                (*default_value == '-' && param_var == NULL))
             {
-                int rc = te_expand_env_vars(default_value + 1, NULL,
-                                            &env_var);
+                int rc = te_expand_parameters(default_value + 1, NULL,
+                                              get_param_value, params_ctx,
+                                              &param_var);
 
                 if (rc != 0)
                 {
@@ -117,24 +150,41 @@ te_expand_env_vars(const char *src, char **posargs, char **retval)
                     *retval = NULL;
                     return rc;
                 }
-                need_free_env = TRUE;
+                need_param_free = TRUE;
             }
         }
-        if (env_var != NULL)
+        if (param_var != NULL)
         {
-            len = strlen(env_var);
+            len = strlen(param_var);
             result = realloc(result, result_len + len);
             if (result == NULL)
                 return errno;
-            memcpy(result + result_len, env_var, len);
+            memcpy(result + result_len, param_var, len);
             result_len += len;
-            if (need_free_env)
+            if (need_param_free)
             {
-                free(env_var);
-                need_free_env = FALSE;
+                free(param_var);
+                need_param_free = FALSE;
             }
         }
     }
     *retval = result;
     return 0;
+}
+
+/* See description in te_expand.h */
+int
+te_expand_env_vars(const char *src, char **posargs, char **retval)
+{
+    return te_expand_parameters(src, posargs, &te_get_env_value,
+                                NULL, retval);
+}
+
+/* See description in te_expand.h */
+int
+te_expand_kvpairs(const char *src, char **posargs, te_kvpair_h *head,
+                  char **retval)
+{
+    return te_expand_parameters(src, posargs, &te_get_kvpairs_value,
+                                head, retval);
 }
--- lib/tools/te_expand.h
+++ lib/tools/te_expand.h
@@ -18,6 +18,44 @@
 #include <libxml/parser.h>
 #endif
 
+#include "te_kvpair.h"
+
+/** Maximal length of parameter name */
+#define TE_EXPAND_PARAM_NAME_LEN 128
+
+/**
+ * A function type for getting value by name from given data.
+ */
+typedef char *(*te_param_value_getter)(const char *, void *);
+
+/**
+ * Expands parameters in a string.
+ * Parameter values will be obtained with @p get_param_value callback.
+ * The parameter names must be between ${ and }.
+ * Conditional expansion is supported:
+ * - ${NAME:-VALUE} is expanded into VALUE if NAME variable is not set,
+ * otherwise to its value
+ * - ${NAME:+VALUE} is expanded into VALUE if NAME variable is set,
+ * otherwise to an empty string.
+ *
+ * @note The length of anything between ${...} must be less than
+ *       @c TE_EXPAND_PARAM_NAME_LEN
+ *
+ * @param src               Source string
+ * @param posargs           Positional parameters (expandable via ${[0-9]})
+ *                          (may be NULL)
+ * @param get_param_value   Function to get param value by name
+ * @param params_ctx        Context for parameters
+ * @param retval            Resulting string (OUT)
+ *
+ * @return 0 if success, an error code otherwise
+ * @retval ENOBUFS The variable name is longer than @c TE_EXPAND_PARAM_NAME_LEN
+ * @retval EINVAL Unmatched ${ found
+ */
+extern int te_expand_parameters(const char *src, char **posargs,
+                                te_param_value_getter get_param_value,
+                                void *params_ctx, char **retval);
+
 /**
  * Expands environment variables in a string.
  * The variable names must be between ${ and }.
@@ -27,7 +65,8 @@
  * - ${NAME:+VALUE} is expanded into VALUE if NAME variable is set,
  * otherwise to an empty string.
  *
- * @note The length of anything between ${...} must be less than 128
+ * @note The length of anything between ${...} must be less than
+ *       @c TE_EXPAND_PARAM_NAME_LEN
  *
  * @param src     Source string
  * @param posargs Positional parameters (expandable via ${[0-9]})
@@ -35,11 +74,36 @@
  * @param retval  Resulting string (OUT)
  *
  * @return 0 if success, an error code otherwise
- * @retval ENOBUFS The variable name is longer than 128
+ * @retval ENOBUFS The variable name is longer than @c TE_EXPAND_PARAM_NAME_LEN
  * @retval EINVAL Unmatched ${ found
  */
 extern int te_expand_env_vars(const char *src, char **posargs,
                               char **retval);
+
+/**
+ * Expands key-value pairs in a string.
+ * The variable names must be between ${ and }.
+ * Conditional expansion is supported:
+ * - ${NAME:-VALUE} is expanded into VALUE if NAME variable is not found,
+ * otherwise to its value
+ * - ${NAME:+VALUE} is expanded into VALUE if NAME variable is found,
+ * otherwise to an empty string.
+ *
+ * @note The length of anything between ${...} must be less than
+ *       @c TE_EXPAND_PARAM_NAME_LEN
+ *
+ * @param src     Source string
+ * @param posargs Positional parameters (expandable via ${[0-9]})
+ *                (may be NULL)
+ * @param head    Head of queue of key-value pairs
+ * @param retval  Resulting string (OUT)
+ *
+ * @return 0 if success, an error code otherwise
+ * @retval ENOBUFS The variable name is longer than @c TE_EXPAND_PARAM_NAME_LEN
+ * @retval EINVAL Unmatched ${ found
+ */
+extern int te_expand_kvpairs(const char *src, char **posargs,
+                             te_kvpair_h *head, char **retval);
 
 
 #ifdef TE_EXPAND_XML
