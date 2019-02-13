56ae85ea82988908d0569ee910a98da5e51efcf3
Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
2019-02-13 13:10:50 +0300

    Bug 9804: remove PATH substitution escaping in the case of local TA
    There is no extra expansion in the case of local TA.

--- lib/rcfunix/rcfunix.c
+++ lib/rcfunix/rcfunix.c
@@ -619,8 +619,8 @@ rcfunix_start(const char *ta_name, const char *ta_type,
     /*
      * Add directory with agent to the PATH
      */
-    sprintf(cmd + strlen(cmd), "PATH=\\$PATH:/tmp/%s%s ", ta_type,
-            ta->postfix);
+    sprintf(cmd + strlen(cmd), "PATH=%s$PATH:/tmp/%s%s ",
+            ta->is_local ? "" : "\\", ta_type, ta->postfix);
 
     if ((shell != NULL) && (strlen(shell) > 0))
     {
