f4bff2a7d19bcf5ff450e59ae2489700caf8e9d5
Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
2019-03-18 11:00:35 +0300

    Bug 8744: fallback to IPv4 when checking for port free if IPv6 is not supported
    The function which checks if port is free was switched
    to use IPv6, but it is bad if IPv6 is not supported.

--- lib/rpcserver/tarpc_imp.c
+++ lib/rpcserver/tarpc_imp.c
@@ -1080,11 +1080,17 @@ TARPC_FUNC(bind, {},
 te_bool
 check_port_is_free(uint16_t port)
 {
-    int fd = socket(PF_INET6, SOCK_STREAM, 0);
+    int pf = PF_INET6;
+    int fd;
     int rc;
+    struct sockaddr_storage addr;
+    socklen_t addr_len;
 
-    struct sockaddr_in6 addr;
-
+    fd = socket(pf, SOCK_STREAM, 0);
+    if (fd < 0 && errno == EAFNOSUPPORT) {
+        pf = PF_INET;
+        fd = socket(pf, SOCK_STREAM, 0);
+    }
     if (fd < 0)
     {
         ERROR("Failed to create TCP socket");
@@ -1092,9 +1098,19 @@ check_port_is_free(uint16_t port)
     }
 
     memset(&addr, 0, sizeof(addr));
-    addr.sin6_family = AF_INET6;
-    addr.sin6_port = htons(port);
-    rc = bind(fd, SA(&addr), sizeof(addr));
+    if (pf == PF_INET6)
+    {
+        addr_len = sizeof(struct sockaddr_in6);
+        SIN6(&addr)->sin6_family = AF_INET6;
+        SIN6(&addr)->sin6_port = htons(port);
+    }
+    else
+    {
+        addr_len = sizeof(struct sockaddr_in);
+        SIN(&addr)->sin_family = AF_INET;
+        SIN(&addr)->sin_port = htons(port);
+    }
+    rc = bind(fd, SA(&addr), addr_len);
     if (rc != 0)
     {
         close(fd);
@@ -1102,14 +1118,14 @@ check_port_is_free(uint16_t port)
     }
 
     close(fd);
-    fd = socket(PF_INET6, SOCK_DGRAM, 0);
+    fd = socket(pf, SOCK_DGRAM, 0);
     if (fd < 0)
     {
         ERROR("Failed to create UDP socket");
         return FALSE;
     }
 
-    rc = bind(fd, SA(&addr), sizeof(addr));
+    rc = bind(fd, SA(&addr), addr_len);
     if (rc != 0)
     {
         close(fd);
