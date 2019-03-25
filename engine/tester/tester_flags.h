bb17513c334929a6f25838af0ee9ba14c81bb1a8
Dmitry Izbitsky <Dmitry.Izbitsky@oktetlabs.ru>
2019-03-25 03:34:32 +0300

    Bug 9567: use omitted 15th bit in tester_flags
    It will make searching for a free bit easier when adding
    new flags.

--- engine/tester/tester_flags.h
+++ engine/tester/tester_flags.h
@@ -64,37 +64,37 @@ typedef uint64_t tester_flags;
 /** Don't use TRC */
 #define TESTER_NO_TRC                 (1LLU << 14)
 /** Run scripts under valgrind */
-#define TESTER_VALGRIND               (1LLU << 16)
+#define TESTER_VALGRIND               (1LLU << 15)
 /** Run scripts under GDB in interactive mode */
-#define TESTER_GDB                    (1LLU << 17)
+#define TESTER_GDB                    (1LLU << 16)
 /** Flag to be used on run path only */
-#define TESTER_RUNPATH                (1LLU << 18)
+#define TESTER_RUNPATH                (1LLU << 17)
 /** End of run path */
-#define TESTER_RUN                    (1LLU << 19)
+#define TESTER_RUN                    (1LLU << 18)
 /** Run tests regardless requested path */
-#define TESTER_FORCE_RUN              (1LLU << 20)
+#define TESTER_FORCE_RUN              (1LLU << 19)
 
 /*
  * These seems to be not used currently.
  */
-#define TESTER_MIX_VALUES             (1LLU << 21)
-#define TESTER_MIX_ARGS               (1LLU << 22)
-#define TESTER_MIX_TESTS              (1LLU << 23)
-#define TESTER_MIX_ITERS              (1LLU << 24)
-#define TESTER_MIX_SESSIONS           (1LLU << 25)
+#define TESTER_MIX_VALUES             (1LLU << 20)
+#define TESTER_MIX_ARGS               (1LLU << 21)
+#define TESTER_MIX_TESTS              (1LLU << 22)
+#define TESTER_MIX_ITERS              (1LLU << 23)
+#define TESTER_MIX_SESSIONS           (1LLU << 24)
 
 /** Force testing flow logging to ignore run item name */
-#define TESTER_LOG_IGNORE_RUN_NAME    (1LLU << 26)
+#define TESTER_LOG_IGNORE_RUN_NAME    (1LLU << 25)
 /** Log list of known requirements to the log file */
-#define TESTER_LOG_REQS_LIST          (1LLU << 27)
+#define TESTER_LOG_REQS_LIST          (1LLU << 26)
 /** Is in prologue/epilogue */
-#define TESTER_INLOGUE                (1LLU << 28)
+#define TESTER_INLOGUE                (1LLU << 27)
 /** Interactive mode */
-#define TESTER_INTERACTIVE            (1LLU << 29)
+#define TESTER_INTERACTIVE            (1LLU << 28)
 /** Shutdown test scenario */
-#define TESTER_SHUTDOWN               (1LLU << 30)
+#define TESTER_SHUTDOWN               (1LLU << 29)
 /** Break test session on Ctrl-C */
-#define TESTER_BREAK_SESSION          (1LLU << 31)
+#define TESTER_BREAK_SESSION          (1LLU << 30)
 
 #ifdef __cplusplus
 } /* extern "C" */
