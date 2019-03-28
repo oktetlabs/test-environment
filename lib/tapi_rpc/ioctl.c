385a4bd0910b24c081a71e4d35478d8b3f8f8c43
Konstantin Ushakov <Konstantin.Ushakov@oktetlabs.ru>
2019-03-28 11:12:56 +0300

    bug9302: don't fallthrough where it's not needed
    I think this code worked "by mistake". default was doing the final
    break and if we're asking for ifindex it was overwriting ifname.
    
    It was causing compilation warnings on gcc (Ubuntu
    7.3.0-27ubuntu1~18.04) 7.3.0.

--- lib/tapi_rpc/ioctl.c
+++ lib/tapi_rpc/ioctl.c
@@ -614,12 +614,12 @@ rpc_ioctl(rcf_rpc_server *rpcs,
 
                     case RPC_SIOCGIFINDEX:
                         ifreq->ifr_ifindex = rpc_ifreq->rpc_ifr_ifindex;
-
+                        break;
                     case RPC_SIOCGIFNAME:
                         memcpy(ifreq->ifr_name,
                                rpc_ifreq->rpc_ifr_name.rpc_ifr_name_val,
                                sizeof(ifreq->ifr_name));
-
+                        break;
                     default:
                         break;
                 }
