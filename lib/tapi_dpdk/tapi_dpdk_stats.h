5b72d86dd53b90793ef99ab90e51a6529ee72f9d
Igor Romanov <Igor.Romanov@oktetlabs.ru>
2019-05-21 15:28:51 +0100

    Bug 8744: tapi_dpdk: calculate and report link usage

--- lib/tapi_dpdk/tapi_dpdk_stats.h
+++ lib/tapi_dpdk/tapi_dpdk_stats.h
@@ -17,6 +17,7 @@
 
 #include "te_defs.h"
 #include "te_stdint.h"
+#include "te_errno.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -41,6 +42,19 @@ uint64_t tapi_dpdk_stats_calculate_l1_bitrate(uint64_t pps,
                                               unsigned int packet_size);
 
 /**
+ * Calculate layer 1 link usage from layer 1 bitrate and link speed.
+ *
+ * @param      l1_bitrate     Layer 1 bits per second
+ * @param      link_speed     Link speed in Mbps
+ * @param[out] l1_link_usage  Layer 1 link usage ratio
+ *
+ * @return  Status code
+ */
+te_errno tapi_dpdk_stats_calculate_l1_link_usage(uint64_t l1_bitrate,
+                                                 unsigned int link_speed,
+                                                 double *l1_link_usage);
+
+/**
  * Report packet per second statistics as a test artifact.
  *
  * @param l1_bitrate    Layer 1 bits per second
@@ -48,12 +62,22 @@ uint64_t tapi_dpdk_stats_calculate_l1_bitrate(uint64_t pps,
 void tapi_dpdk_stats_l1_bitrate_artifact(uint64_t l1_bitrate);
 
 /**
- * Report rates corresponding to PPS and packet_size as test artifacts.
+ * Report layer 1 link usage statistics as a test artifact.
+ *
+ * @param l1_link_usage    Layer 1 link usage ratio
+ */
+void tapi_dpdk_stats_l1_link_usage_artifact(double l1_link_usage);
+
+/**
+ * Report rates corresponding to PPS, packet_size and link speed
+ * as test artifacts.
  *
  * @param pps          Packets per second
  * @param packet_size  Packet size in bytes (without l1 and FCS)
+ * @param link_speed   Link speed in Mbps
  */
-void tapi_dpdk_stats_log_rates(uint64_t pps, unsigned int packet_size);
+void tapi_dpdk_stats_log_rates(uint64_t pps, unsigned int packet_size,
+                               unsigned int link_speed);
 
 #ifdef __cplusplus
 } /* extern "C" */
