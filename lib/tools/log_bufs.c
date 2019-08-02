00b1b84b1938f04f1050f53e4c4b2825c2b50fc4
Maxim Menshikov <Maxim.Menshikov@oktetlabs.ru>
2019-08-02 19:18:33 +0300

    Bug 10294: fix crash with a large number of verdicts by using automatically expandable te_string instead of raw buffer

--- lib/tools/log_bufs.c
+++ lib/tools/log_bufs.c
@@ -38,6 +38,7 @@
 #endif
 
 #include "te_defs.h"
+#include "te_string.h"
 #include "logger_api.h"
 #include "log_bufs.h"
 
@@ -49,9 +50,8 @@
 
 /** Internal presentation of log buffer. */
 struct te_log_buf {
-    te_bool used; /**< Whether this buffer is already in use */
-    char    ptr[LOG_BUF_LEN]; /**< Buffer data */
-    size_t  cur_len; /**< The number of bytes currently used in buffer */
+    te_bool     used;   /**< Whether this buffer is already in use */
+    te_string   str;    /**< Buffer data */
 };
 
 /** Statically allocated pool of log buffers. */
@@ -85,11 +85,12 @@ te_log_buf_alloc()
     if (te_log_buf_last_freed != -1)
     {
         assert(te_log_bufs[te_log_buf_last_freed].used == FALSE &&
-               te_log_bufs[te_log_buf_last_freed].cur_len == 0);
+               te_log_bufs[te_log_buf_last_freed].str.len == 0);
 
         id = te_log_buf_last_freed;
         te_log_buf_last_freed = -1;
         te_log_bufs[id].used = TRUE;
+        te_log_bufs[id].str = (te_string)TE_STRING_INIT_RESERVE(LOG_BUF_LEN);
         pthread_mutex_unlock(&te_log_buf_mutex);
 
         return &te_log_bufs[id];
@@ -99,9 +100,10 @@ te_log_buf_alloc()
     {
         if (!te_log_bufs[i].used)
         {
-            assert(te_log_bufs[i].cur_len == 0);
+            assert(te_log_bufs[i].str.len == 0);
 
             te_log_bufs[i].used = TRUE;
+            te_log_bufs[i].str = (te_string)TE_STRING_INIT_RESERVE(LOG_BUF_LEN);
             pthread_mutex_unlock(&te_log_buf_mutex);
 
             return &te_log_bufs[i];
@@ -115,11 +117,13 @@ te_log_buf_alloc()
         if (te_log_buf_last_freed != -1)
         {
             assert(te_log_bufs[te_log_buf_last_freed].used == FALSE &&
-                   te_log_bufs[te_log_buf_last_freed].cur_len == 0);
+                   te_log_bufs[te_log_buf_last_freed].str.len == 0);
 
             id = te_log_buf_last_freed;
             te_log_buf_last_freed = -1;
             te_log_bufs[id].used = TRUE;
+            te_log_bufs[id].str = (te_string)TE_STRING_INIT_RESERVE(
+                LOG_BUF_LEN);
             pthread_mutex_unlock(&te_log_buf_mutex);
 
             return &te_log_bufs[id];
@@ -138,22 +142,22 @@ te_log_buf_alloc()
 int
 te_log_buf_append(te_log_buf *buf, const char *fmt, ...)
 {
-    int      rc;
+    te_errno rc;
+    size_t   old_len = buf->str.len;
     va_list  ap;
-    char    *ptr;
-    size_t   size;
 
     VALIDATE_LOG_BUF(buf);
 
-    ptr = buf->ptr + buf->cur_len;
-    size = sizeof(buf->ptr) - buf->cur_len;
-
     va_start(ap, fmt);
-    rc = vsnprintf(ptr, size, fmt, ap);
-    buf->cur_len += rc;
+    rc = te_string_append_va(&buf->str, fmt, ap);
     va_end(ap);
+    if (rc != 0)
+        return -1;
 
-    return rc;
+    /* Must be at worst the same */
+    assert(buf->str.len >= old_len);
+
+    return buf->str.len - old_len;
 }
 
 const char *
@@ -161,10 +165,14 @@ te_log_buf_get(te_log_buf *buf)
 {
     VALIDATE_LOG_BUF(buf);
 
-    if (buf->cur_len == 0)
-        buf->ptr[0] = '\0';
+    if (buf->str.len == 0)
+    {
+        te_string_free(&buf->str);
+        buf->str = (te_string)TE_STRING_INIT_RESERVE(LOG_BUF_LEN);
+        assert(te_string_append(&buf->str, "") == 0);
+    }
 
-    return buf->ptr;
+    return buf->str.ptr;
 }
 
 /* See description in log_bufs.h */
@@ -178,7 +186,7 @@ te_log_buf_free(te_log_buf *buf)
 
     pthread_mutex_lock(&te_log_buf_mutex);
     buf->used = FALSE;
-    buf->cur_len = 0;
+    te_string_free(&buf->str);
     te_log_buf_last_freed =
         (buf - te_log_bufs) / sizeof(te_log_bufs[0]);
     pthread_mutex_unlock(&te_log_buf_mutex);
