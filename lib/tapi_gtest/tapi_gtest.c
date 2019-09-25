aaa3b56cdf025db8abe40bf99ec92775e25faf53
Nikita Somenkov <Nikita.Somenkov@oktetlabs.ru>
2019-09-25 14:46:10 +0300

    Bug 10479: Add tapi_gtest for able run Google Tests

---
+++ lib/tapi_gtest/tapi_gtest.c
@@ -0,0 +1,188 @@
+/** @file
+ * @brief Wrapper for Google Test
+ *
+ * Test API for run Google Test binaries
+ *
+ * Copyright (C) 2019 OKTET Labs. All rights reserved.
+ *
+ * @author Nikita Somenkov <Nikita.Somenkov@oktetlabs.ru>
+ */
+
+#define TE_LGR_USER "TAPI GTest"
+
+#include "te_config.h"
+#include "tapi_test_log.h"
+#include "te_vector.h"
+
+#include "tapi_gtest.h"
+
+static void
+gtest_args_free(te_vec *gtest_args)
+{
+    char **arg = NULL;
+
+    TE_VEC_FOREACH(gtest_args, arg)
+        free(*arg);
+
+    te_vec_free(gtest_args);
+}
+
+static te_errno
+gtest_build_command(tapi_gtest *gtest, te_vec *args)
+{
+    te_errno rc;
+    te_vec result = TE_VEC_INIT(const char *);
+    const char *end_arg = NULL;
+
+#define ADD_ARG(cmd_fmt...)                                             \
+    do {                                                                \
+        te_string buf = TE_STRING_INIT;                                 \
+                                                                        \
+        rc = te_string_append(&buf, cmd_fmt);                           \
+                                                                        \
+        if (rc != 0 || (rc = TE_VEC_APPEND(&result, buf.ptr)) != 0)     \
+        {                                                               \
+            te_string_free(&buf);                                       \
+            gtest_args_free(&result);                                   \
+            return rc;                                                  \
+        }                                                               \
+    } while(0)
+
+    ADD_ARG("%s", gtest->bin);
+    ADD_ARG("--gtest_filter=%s.%s", gtest->group, gtest->name);
+    ADD_ARG("--gtest_random_seed=%d", gtest->rand_seed);
+    ADD_ARG("--gtest_color=no");
+    if (gtest->run_disabled)
+        ADD_ARG("--gtest_also_run_disabled_tests");
+
+#undef ADD_ARG
+
+    if ((rc = TE_VEC_APPEND(&result, end_arg)) != 0)
+    {
+        gtest_args_free(&result);
+        return rc;
+    }
+
+    *args = result;
+    return 0;
+}
+
+/* See description in tapi_gtest.h */
+te_errno
+tapi_gtest_init(tapi_gtest *gtest, rcf_rpc_server *rpcs)
+{
+    te_errno rc;
+    te_vec gtest_args = TE_VEC_INIT(const char *);
+    tapi_job_simple_desc_t desc;
+
+    assert(gtest != NULL);
+    assert(gtest->bin != NULL);
+    assert(gtest->name != NULL);
+    assert(gtest->group != NULL);
+
+    desc = (tapi_job_simple_desc_t) {
+        .program = gtest->bin,
+        .job_loc = &gtest->impl.job,
+        .stdout_loc = &gtest->impl.out[0],
+        .stderr_loc = &gtest->impl.out[1],
+        .filters = TAPI_JOB_SIMPLE_FILTERS(
+            {
+                .use_stdout = TRUE,
+                .filter_name = "stdout-gtest",
+                .log_level = TE_LL_RING,
+            },
+            {
+                .use_stderr = TRUE,
+                .filter_name = "stderr-gtest",
+                .log_level = TE_LL_ERROR,
+            }
+        ),
+    };
+
+    if ((rc = gtest_build_command(gtest, &gtest_args)) != 0)
+        return rc;
+
+    desc.argv = (const char **) gtest_args.data.ptr;
+    rc = tapi_job_rpc_simple_create(rpcs, &desc);
+
+    gtest_args_free(&gtest_args);
+    return rc;
+}
+
+/* See description in tapi_gtest.h */
+te_errno
+tapi_gtest_start(tapi_gtest *gtest)
+{
+    assert(gtest != NULL);
+    assert(gtest->name != NULL);
+    assert(gtest->group != NULL);
+    assert(gtest->impl.job != NULL);
+
+    VERB("GTest '%s.%s' start", gtest->group, gtest->name);
+
+    return tapi_job_start(gtest->impl.job);
+}
+
+/* See description in tapi_gtest.h */
+te_errno
+tapi_gtest_wait(tapi_gtest *gtest, int timeout_ms)
+{
+    te_errno rc;
+    tapi_job_status_t status;
+
+    assert(gtest != NULL);
+    assert(gtest->name != NULL);
+    assert(gtest->group != NULL);
+    assert(gtest->impl.job != NULL);
+
+    if ((rc = tapi_job_wait(gtest->impl.job, timeout_ms, &status)) != 0)
+        return rc;
+
+    if (status.value == 0 && status.type == TAPI_JOB_STATUS_EXITED)
+        return 0;
+
+    switch(status.type)
+    {
+        case TAPI_JOB_STATUS_EXITED:
+            ERROR_ARTIFACT("GTest '%s.%s' exited with exit code %d",
+                           gtest->group, gtest->name, status.value);
+            return TE_EFAIL;
+        case TAPI_JOB_STATUS_SIGNALED:
+            ERROR_ARTIFACT("GTest '%s.%s' got signal %d: %s",
+                           gtest->group, gtest->name, status.value,
+                           strsignal(status.value));
+            return TE_EFAIL;
+        case TAPI_JOB_STATUS_UNKNOWN:
+            ERROR_ARTIFACT("GTest '%s.%s': status unknown",
+                           gtest->group, gtest->name);
+            return TE_EFAIL;
+    }
+
+    /* unreachable */
+    return TE_EFAIL;
+}
+
+te_errno
+tapi_gtest_stop(tapi_gtest *gtest)
+{
+    assert(gtest != NULL);
+    assert(gtest->impl.job != NULL);
+
+    return tapi_job_kill(gtest->impl.job, SIGINT);
+}
+
+/* See description in tapi_gtest.h */
+te_errno
+tapi_gtest_fini(tapi_gtest *gtest)
+{
+    te_errno rc;
+    const int term_timeout_ms = -1;
+
+    if (gtest == NULL)
+        return 0;
+
+    if ((rc = tapi_gtest_stop(gtest)) != 0)
+        return rc;
+
+    return tapi_job_destroy(gtest->impl.job, term_timeout_ms);
+}
---
+++ lib/tapi_gtest/tapi_gtest.h
@@ -0,0 +1,155 @@
+/** @file
+ * @brief Wrapper for Google Test
+ *
+ * @defgroup tapi_gtest GTest support
+ * @ingroup te_ts_tapi
+ * @{
+ *
+ * Test API for run Google Test binaries
+ *
+ * Copyright (C) 2019 OKTET Labs. All rights reserved.
+ *
+ * @author Nikita Somenkov <Nikita.Somenkov@oktetlabs.ru>
+ */
+
+#ifndef __TAPI_GTEST_H__
+#define __TAPI_GTEST_H__
+
+#include "tapi_job.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** Internal implementation struct */
+typedef struct tapi_gtest_impl {
+    tapi_job_t *job;                /**< GTest job handler */
+    tapi_job_channel_t *out[2];     /**< GTest stdour/stderr channels */
+} tapi_gtest_impl;
+
+/** Defaults for implementation struct */
+#define TAPI_GTEST_IMPL_DEFAULTS (tapi_gtest_impl)  \
+{                                                   \
+    .job = NULL,                                    \
+    .out = {0},                                     \
+}
+
+/** GTest handler */
+typedef struct tapi_gtest {
+    const char *bin;        /**< Path to GTest binary */
+    const char *group;      /**< Group name in GTest */
+    const char *name;       /**< Test name in GTest */
+
+    te_bool run_disabled;   /**< Force run disabled test */
+    int rand_seed;          /**< Random seed */
+
+    tapi_gtest_impl impl;   /**< Internal implementation struct */
+} tapi_gtest;
+
+/** Defaults for implementation for GTest handler */
+#define TAPI_GTEST_DEFAULTS (tapi_gtest)    \
+{                                           \
+    .bin = NULL,                            \
+    .group = NULL,                          \
+    .name = NULL,                           \
+    .run_disabled = FALSE,                  \
+    .impl = TAPI_GTEST_IMPL_DEFAULTS,       \
+}
+
+/** A way for read gtest option from test arguments */
+#define TEST_GTEST_PARAM(_gtest) (tapi_gtest)    \
+{                                               \
+    .bin = TEST_STRING_PARAM(_gtest##_bin),      \
+    .group = TEST_STRING_PARAM(_gtest##_group),  \
+    .name = TEST_STRING_PARAM(_gtest##_name),    \
+    .rand_seed = TEST_INT_PARAM(te_rand_seed),  \
+    .run_disabled = FALSE,                      \
+    .impl = TAPI_GTEST_IMPL_DEFAULTS,           \
+}
+
+/** A way for read gtest option from test arguments */
+#define TEST_GET_GTEST_PARAM(_gtest) \
+    (_gtest) = TEST_GTEST_PARAM(_gtest)
+
+/**
+ * Create GTest
+ *
+ * @param gtest     GTest handler
+ * @param rpcs      RPC Server
+ *
+ * @return Status code
+ */
+extern te_errno tapi_gtest_init(tapi_gtest *gtest,  rcf_rpc_server *rpcs);
+
+/**
+ * Start GTest
+ *
+ * @param gtest     GTest handler
+ *
+ * @return Status code
+ */
+extern te_errno tapi_gtest_start(tapi_gtest *gtest);
+
+/**
+ * Stop GTest
+ *
+ * @param gtest     GTest handler
+ *
+ * @return Status code
+ */
+extern te_errno tapi_gtest_stop(tapi_gtest *gtest);
+
+/**
+ * Wait GTest and get result
+ *
+ * @param gtest         GTest handler
+ * @param timeout_ms    Timeout for wait
+ *
+ * @return Status code
+ */
+extern te_errno tapi_gtest_wait(tapi_gtest *gtest, int timeout_ms);
+
+/**
+ * Cleanup GTest
+ *
+ * @param gtest         GTest handler
+ *
+ * @return Status code
+ */
+extern te_errno tapi_gtest_fini(tapi_gtest *gtest);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+/**
+ * @page tapi-gtest-scenarios GTest TAPI usage scenarios
+ *
+ * Wrap GTest
+ * ------------
+ *
+ * \code{.c}
+ * int
+ * main(int argc, char *argv[])
+ * {
+ *     tapi_gtest gtest;
+ *
+ *     TEST_START;
+ *     TEST_GET_GTEST_PARAM(gtest);
+ *
+ *     CHECK_RC(tapi_gtest_init(&gtest, rpcs));
+ *     CHECK_RC(tapi_gtest_start(&gtest));
+ *     CHECK_RC(tapi_gtest_wait(&gtest, TE_SEC2MS(10)));
+ *
+ *     TEST_SUCCESS;
+ *
+ * cleanup:
+ *     CLEANUP_CHECK_RC(tapi_gtest_fini(&gtest));
+ *     TEST_END;
+ * }
+ * \endcode
+ */
+
+#endif /* __TAPI_GTEST_H__ */
+
+/**@} <!-- END tapi_gtest --> */
