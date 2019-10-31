bfdfdb3a841139d7b677340f95f921c8a44335b5
Igor Romanov <Igor.Romanov@oktetlabs.ru>
2019-10-31 10:59:37 +0000

    Bug 8744: add TAPI to insert flow API QUEUE action to list

---
+++ lib/tapi_dpdk/tapi_rte_flow.c
@@ -0,0 +1,31 @@
+/** @file
+ * @brief DPDK RTE flow helper functions TAPI
+ *
+ * RTE flow helper functions TAPI (implementation)
+ *
+ * Copyright (C) 2019 OKTET Labs. All rights reserved.
+ *
+ * @author Igor Romanov <Igor.Romanov@oktetlabs.ru>
+ */
+
+#include "te_config.h"
+
+#include "tapi_test.h"
+#include "rte_flow_ndn.h"
+#include "tapi_rte_flow.h"
+
+void
+tapi_rte_flow_add_ndn_action_queue(asn_value *ndn_actions, int action_index,
+                                   uint16_t queue)
+{
+    asn_value *queue_action;
+
+    queue_action = asn_init_value(ndn_rte_flow_action);
+    CHECK_NOT_NULL(queue_action);
+
+    CHECK_RC(asn_write_int32(queue_action, NDN_FLOW_ACTION_TYPE_QUEUE,
+                             "type"));
+    CHECK_RC(asn_write_int32(queue_action, queue, "conf.#index"));
+
+    CHECK_RC(asn_insert_indexed(ndn_actions, queue_action, action_index, ""));
+}
---
+++ lib/tapi_dpdk/tapi_rte_flow.h
@@ -0,0 +1,44 @@
+/** @file
+ * @brief DPDK RTE flow helper functions TAPI
+ *
+ * @defgroup tapi_rte_flow DPDK RTE flow helper functions TAPI
+ * @ingroup te_ts_tapi
+ * @{
+ *
+ * RTE flow helper functions TAPI
+ *
+ * Copyright (C) 2019 OKTET Labs. All rights reserved.
+ *
+ * @author Igor Romanov <Igor.Romanov@oktetlabs.ru>
+ */
+
+#ifndef __TAPI_RTE_FLOW_H__
+#define __TAPI_RTE_FLOW_H__
+
+#include "te_defs.h"
+#include "te_stdint.h"
+#include "te_errno.h"
+#include "asn_usr.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Add a QUEUE action to an action list at specified index.
+ *
+ * @param[inout]  ndn_actions   Action list
+ * @param[in]     action_index  Index at which QUEUE action is put to list
+ * @param[in]     queue         Queue index of QUEUE action
+ */
+extern void tapi_rte_flow_add_ndn_action_queue(asn_value *ndn_actions,
+                                               int action_index,
+                                               uint16_t queue);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* __TAPI_RTE_FLOW_H__ */
+
+/**@} <!-- END tapi_rte_flow --> */
