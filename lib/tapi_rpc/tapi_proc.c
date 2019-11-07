50721b13557e9820c03998b8b0bb13056630313c
Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
2019-11-07 09:44:54 +0300

    Bug 9682: move RPC-dependent proc TAPI to tapi_rpc
    lib/tapi must not depend on lib/tapi_rpc since it makes
    cross-dependency.

---
+++ lib/tapi_rpc/tapi_proc.c
@@ -0,0 +1,51 @@
+/** @file
+ * @brief API to configure some system options via /proc/sys
+ *
+ * Implementation of API for access to some system options via /proc/sys.
+ *
+ *
+ * Copyright (C) 2003-2018 OKTET Labs. All rights reserved.
+ *
+ * 
+ *
+ *
+ * @author Andrey Dmitrov <Andrey.Dmitrov@oktetlabs.ru>
+ *
+ * $Id$
+ */
+
+#define TE_LGR_USER     "TAPI RPC Proc"
+
+#include "te_config.h"
+#include "tapi_proc.h"
+#include "tapi_rpc_unistd.h"
+
+/* See description in tapi_proc.h */
+int
+tapi_cfg_net_route_flush(rcf_rpc_server *rpcs)
+{
+    int     fd;
+    te_bool wait_err = FALSE;
+
+    if (RPC_AWAITING_ERROR(rpcs))
+        wait_err = TRUE;
+
+    if ((fd = rpc_open(rpcs, "/proc/sys/net/ipv4/route/flush",
+                       RPC_O_WRONLY, 0)) < 0)
+        return -1;
+
+    if (wait_err)
+        RPC_AWAIT_IUT_ERROR(rpcs);
+    if (rpc_write(rpcs, fd, "1", 1) < 0)
+    {
+        rpc_close(rpcs, fd);
+        return -1;
+    }
+
+    if (wait_err)
+        RPC_AWAIT_IUT_ERROR(rpcs);
+    if (rpc_close(rpcs, fd) != 0)
+        return -1;
+
+    return 0;
+}
