5dd309d15f0b7984e76343e75eb6945cefb6ead6
Nikita Somenkov <Nikita.Somenkov@oktetlabs.ru>
2019-11-12 17:24:45 +0300

    Bug 10048: Add oktetlabs logo in sphinx documetation

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -48,6 +48,12 @@ exclude_patterns = ['generated/_build']
 # a list of builtin themes.
 html_theme = 'sphinx_rtd_theme'
 
+html_logo = 'static/image/te-logo.png'
+
+html_theme_options = {
+    'logo_only': True,
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
