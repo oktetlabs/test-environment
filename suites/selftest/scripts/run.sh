3ba96b3031fd679968cfcfc734474ea7ceeabe65
Nikita Somenkov <Nikita.Somenkov@oktetlabs.ru>
2019-12-06 11:55:30 +0300

    Bug 10692: Add run.sh into sefltest
    Patch demonstrate template for run.sh - helper for run tests.

---
+++ suites/selftest/scripts/run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+export TS_TOPDIR="$(cd "$(dirname "$(which "$0")")"; pwd -P)"
+if [ "$(basename ${TS_TOPDIR})" == "scripts" ]; then
+    export TS_TOPDIR=$(dirname ${TS_TOPDIR})
+fi
+export TE_TS_DIR=$TS_TOPDIR/ts
+
+TS_OPTS=
+for opt ; do
+    case "${opt}" in
+        *)
+            TS_OPTS+="${opt} ";;
+    esac
+    shift 1
+done
+
+TS_CONF_DIRS=
+TS_CONF_DIRS+="\"${TS_TOPDIR}\"/conf:"
+
+TS_DEFAULT_OPTS=
+
+TS_DEFAULT_OPTS+="--conf-dirs=${TS_CONF_DIRS} "
+TS_DEFAULT_OPTS+="--build-parallel "
+TS_DEFAULT_OPTS+="--build-meson "
+
+eval "${TE_BASE}/dispatcher.sh ${TS_DEFAULT_OPTS} ${TS_OPTS}"
+TS_RESULT=$?
+
+if test ${TS_RESULT} -ne 0 ; then
+    echo FAIL
+    echo ""
+    exit ${TS_RESULT}
+fi
