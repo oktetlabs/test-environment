627c3f21a29a7627e803ee981e7dd23602fa976d
Konstantin Ushakov <Konstantin.Ushakov@oktetlabs.ru>
2020-03-03 17:59:32 +0300

    Bug 10692: add script to deploy doxyrest or log how to deploy it
    Doxyrest is required to build both TS and TE Core documentation. Also
    help printed about doxy-rest is copy-pasted all over the place, so one
    place for instructions location is required.

---
+++ scripts/doxyrest_deploy.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Test Environment:
+#
+# Script to deploy doxyrest that is required for doxygen->RST->SPHINX
+# documentation conversion.
+#
+# Copyright (C) 2020 OKTET Labs.
+#
+# Author Konstantin.Ushakov <Konstantin.Ushakov@oktetlabs.ru>
+
+help() {
+    cat <<EOF
+Usage: doxyrest_deploy.sh [options]
+
+  --how            : print info about doxyrest installation
+  --put            : put doxygest in directory that is $TE_BASE/../
+  --help           : print this help
+EOF
+}
+
+doxyrest_put() {
+    action=$1
+
+    ${action} pushd ${TE_BASE}/..
+    ${action} wget https://github.com/vovkos/doxyrest/releases/download/doxyrest-2.0.0/doxyrest-2.0.0-linux-amd64.tar.xz
+    ${action} tar -xf doxyrest-2.0.0-linux-amd64.tar.xz
+    ${action} export DOXYREST_PREFIX=$PWD/doxyrest-2.0.0-linux-amd64
+    ${action} popd
+}
+
+while test -n "$1" ; do
+    case "$1" in
+        --how)
+            echo "Please set DOXYREST_PREFIX before:"
+            echo "Last release: https://github.com/vovkos/doxyrest/releases"
+            echo "------"
+            doxyrest_put echo
+            ;;
+        --put)
+            echo "Putting doxyrest"
+            doxyrest_put
+            ;;
+        --help)
+            help
+            exit 0
+            ;;
+        *)
+            help
+            exit 1
+    esac
+
+    shift
+done
