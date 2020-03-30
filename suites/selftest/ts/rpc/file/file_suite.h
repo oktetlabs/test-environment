e2c875fae18baf69c02e07bd4a8da0ed5dc917e7
Eugene Suslov <Eugene.Suslov@oktetlabs.ru>
2020-03-30 17:49:06 +0300

    Bug 10903: add tests for file read, write, append
    Add tests for file read, write, append.

--- suites/selftest/ts/rpc/file/file_suite.h
+++ suites/selftest/ts/rpc/file/file_suite.h
@@ -14,10 +14,103 @@
 #include "te_defs.h"
 #include "tapi_test.h"
 #include "tapi_file.h"
+#include "tapi_mem.h"
 #include "tapi_rpc_unistd.h"
+
+#define AGT_A    "Agt_A"
 
 #define TMP_DIR    "/tmp"
 
-#define AGT_A    "Agt_A"
+#define BUFSIZE 64
+#define RW_MAX_RETRY 3
+
+/**
+ * Write the whole buffer.
+ *
+ * @param pco_        PCO where to send from
+ * @param fd_         File descriptor at PCO
+ * @param buf_        Pointer to a buffer
+ * @param buflen_     Buffer size
+ */
+#define WRITE_WHOLE_BUF(pco_, fd_, buf_, buflen_)                       \
+    do {                                                                \
+        size_t total_ = 0;  /* Total bytes has been written */          \
+        int current_;  /* Written by a single write() */                \
+        int retry_ = 0;                                                 \
+                                                                        \
+        while(total_ < buflen_)                                         \
+        {                                                               \
+            RPC_AWAIT_ERROR(pco_);                                      \
+            current_ = rpc_write(pco_, fd_, (void *)buf_ + total_,      \
+                                 buflen_);                              \
+            if (current_ <= 0 && RPC_ERRNO(pco_) != 0)                  \
+            {                                                           \
+                TEST_VERDICT("rpc_write() unexpectedly returned %d,"    \
+                             "errno=%r", current_, RPC_ERRNO(pco_));    \
+            }                                                           \
+            else if (current_ == 0)                                     \
+            {                                                           \
+                if (retry_++ == RW_MAX_RETRY)                           \
+                {                                                       \
+                    TEST_VERDICT("rpc_write() maximum re-try reached"); \
+                }                                                       \
+                else                                                    \
+                {                                                       \
+                    VSLEEP(1, "rpc_write() returned 0. Retry writing"); \
+                    continue;                                           \
+                }                                                       \
+            }                                                           \
+            else                                                        \
+            {                                                           \
+                retry_ = 0;                                             \
+            }                                                           \
+            total_ += current_;                                         \
+        }                                                               \
+    } while(0)
+
+/**
+ * Read the whole buffer.
+ *
+ * @param pco_        PCO where to send from
+ * @param fd_         File descriptor at PCO
+ * @param buf_        Pointer to a buffer
+ * @param buflen_     Buffer size
+ */
+#define READ_WHOLE_BUF(pco_, fd_, buf_, buflen_)                        \
+    do {                                                                \
+        size_t total_ = 0;  /* Total bytes has been read */             \
+        int current_;  /* Read by a single read() */                    \
+        int retry_ = 0;                                                 \
+                                                                        \
+        while(total_ < buflen_)                                         \
+        {                                                               \
+            RPC_AWAIT_ERROR(pco_);                                      \
+            current_ = rpc_read(pco_, fd_, (void *)buf_ + total_,       \
+                                buflen_);                               \
+                                                                        \
+            if (current_ <= 0 && RPC_ERRNO(pco_) != 0)                  \
+            {                                                           \
+                TEST_VERDICT("rpc_read() unexpectedly returned %d,"     \
+                             "errno=%r", current_, RPC_ERRNO(pco_));    \
+            }                                                           \
+            else if (current_ == 0)                                     \
+            {                                                           \
+                if (retry_++ == RW_MAX_RETRY)                           \
+                {                                                       \
+                    TEST_VERDICT("rpc_read() maximum re-try reached");  \
+                }                                                       \
+                else                                                    \
+                {                                                       \
+                    VSLEEP(1, "rpc_read() returned 0. Retry reading");  \
+                    continue;                                           \
+                }                                                       \
+            }                                                           \
+            else                                                        \
+            {                                                           \
+                retry_ = 0;                                             \
+            }                                                           \
+            total_ += current_;                                         \
+        }                                                               \
+    } while(0)
 
 #endif /* __FILE_SUITE_H__ */
---
+++ suites/selftest/ts/rpc/file/file_write.c
@@ -0,0 +1,98 @@
+/** @file
+ * @brief RPC Test Suite
+ *
+ * Write file to Agent.
+ *
+ * Copyright (C) 2020 OKTET Labs. All rights reserved.
+ */
+
+/** @page file_write Test for writing file
+ *
+ * @objective Demo of TAPI/RPC file writing test
+ *
+ * @par Scenario:
+ *
+ * @author Eugene Suslov <Eugene.Suslov@oktetlabs.ru>
+ */
+
+#define TE_TEST_NAME    "file_write"
+
+#include "file_suite.h"
+
+int
+main(int argc, char **argv)
+{
+    char           *rfile = NULL;
+    uint8_t        *buf = NULL;
+    rcf_rpc_server *rpcs = NULL;
+    int             fd = -1;
+    uint8_t        *data = NULL;
+    char           *data_str = NULL;
+    const size_t    data_size = BUFSIZE;
+
+    TEST_START;
+    TEST_GET_RPCS(AGT_A, "rpcs", rpcs);
+
+    TEST_STEP("Write data to a file on TA");
+    data = te_make_buf_by_len(BUFSIZE);
+    rfile = tapi_file_generate_name();
+    if (tapi_file_create_ta(rpcs->ta, rfile, "") != 0)
+    {
+        TEST_VERDICT("tapi_file_create_ta() failed");
+    }
+    RPC_AWAIT_ERROR(rpcs);
+    fd = rpc_open(rpcs, rfile, RPC_O_WRONLY | RPC_O_CREAT, 0);
+    if (fd == -1)
+    {
+        TEST_VERDICT("rpc_open() for writing data failed");
+    }
+    WRITE_WHOLE_BUF(rpcs, fd, data, data_size);
+    RPC_AWAIT_ERROR(rpcs);
+    if (rpc_close(rpcs, fd) != 0)
+    {
+        TEST_VERDICT("rpc_close() failed");
+    }
+
+    TEST_STEP("Read content from the file on TA");
+    buf = (uint8_t *)tapi_calloc(1, data_size);
+    RPC_AWAIT_ERROR(rpcs);
+    fd = rpc_open(rpcs, rfile, RPC_O_RDONLY | RPC_O_CREAT, 0);
+    if (fd == -1)
+    {
+        TEST_VERDICT("rpc_open() for reading data failed");
+    }
+    READ_WHOLE_BUF(rpcs, fd, buf, data_size);
+    RPC_AWAIT_ERROR(rpcs);
+    if (rpc_close(rpcs, fd) != 0)
+    {
+        TEST_VERDICT("rpc_close() failed");
+    }
+
+    TEST_STEP("Print data");
+    TEST_SUBSTEP("Print expected data");
+    data_str = raw2string(buf, data_size);
+    RING("%s", data_str);
+    free(data_str);
+
+    TEST_SUBSTEP("Print received data");
+    data_str = raw2string(data, data_size);
+    RING("%s", data_str);
+    free(data_str);
+
+    TEST_STEP("Check if the buffer matches initial data");
+    if (memcmp(data, buf, data_size) != 0)
+    {
+        TEST_VERDICT("Received data doesn't match");
+    }
+
+    TEST_SUCCESS;
+
+cleanup:
+
+    CLEANUP_CHECK_RC(rcf_ta_del_file(rpcs->ta, 0, rfile));
+
+    free(buf);
+    free(data);
+
+    TEST_END;
+}
