1b8158afc75a1ee01f3859e04c7b56f4ca276459
Eugene Suslov <Eugene.Suslov@oktetlabs.ru>
2020-03-30 17:49:06 +0300

    Bug 10903: add rpc file package into selftest suite
    Add tests for file creation and deletion.

---
+++ suites/selftest/ts/rpc/file/file_suite.h
@@ -0,0 +1,21 @@
+/** @file
+ * @brief RPC Test Suite
+ *
+ * Common definitions for rpc/file test suite.
+ *
+ * Copyright (C) 2020 OKTET Labs. All rights reserved.
+ *
+ * @author Eugene Suslov <Eugene.Suslov@oktetlabs.ru>
+ */
+
+#ifndef __FILE_SUITE_H__
+#define __FILE_SUITE_H__
+
+#include "te_defs.h"
+#include "tapi_test.h"
+#include "tapi_file.h"
+#include "tapi_rpc_unistd.h"
+
+#define AGT_A    "Agt_A"
+
+#endif /* __FILE_SUITE_H__ */
