a8fe4ecd29bd8c52fcfbc67a942da1b867692f77
Igor Romanov <Igor.Romanov@oktetlabs.ru>
2020-04-13 09:40:49 +0100

    Bug 8744: fix DPDK version dependend API call
    Device information API has changed to return an error code, but
    in the old DPDK version this API returns void. Don't check return
    code and instead check the populated structure.
    Also add conditional compilation on define that exists only if
    representors are supported by DPDK.

--- lib/rpcs_dpdk/dpdk.c
+++ lib/rpcs_dpdk/dpdk.c
@@ -25,8 +25,23 @@
 #include "rpc_server.h"
 #include "rpcs_dpdk.h"
 
+static int
+dpdk_rte_eth_dev_info_get(uint16_t port, struct rte_eth_dev_info *dev_info)
+{
+    int rc = 0;
+
+#if HAVE_RTE_DEV_INFO_GET_RETURN_VOID
+    rte_eth_dev_info_get(port, dev_info);
+#else
+    rc = rte_eth_dev_info_get(port, dev_info);
+#endif
+
+    return rc;
+}
+
 TARPC_FUNC_STANDALONE(dpdk_find_representors, {},
 {
+#ifdef RTE_ETH_DEV_REPRESENTOR
     uint16_t port;
     struct rte_eth_dev_info info;
     unsigned int n_rep = 0;
@@ -34,12 +49,16 @@ TARPC_FUNC_STANDALONE(dpdk_find_representors, {},
 
     RTE_ETH_FOREACH_DEV(port)
     {
-        rc = rte_eth_dev_info_get(port, &info);
+        memset(&info, 0, sizeof(info));
+        rc = dpdk_rte_eth_dev_info_get(port, &info);
         if (rc != 0)
             goto exit;
 
-        if (*info.dev_flags & RTE_ETH_DEV_REPRESENTOR)
+        if (info.dev_flags != NULL &&
+            (*info.dev_flags & RTE_ETH_DEV_REPRESENTOR))
+        {
             n_rep++;
+        }
     }
 
 
@@ -59,12 +78,16 @@ TARPC_FUNC_STANDALONE(dpdk_find_representors, {},
         i = 0;
         RTE_ETH_FOREACH_DEV(port)
         {
-            rc = rte_eth_dev_info_get(port, &info);
+            memset(&info, 0, sizeof(info));
+            rc = dpdk_rte_eth_dev_info_get(port, &info);
             if (rc != 0)
                 goto exit;
 
-            if (*info.dev_flags & RTE_ETH_DEV_REPRESENTOR)
+            if (info.dev_flags != NULL &&
+                (*info.dev_flags & RTE_ETH_DEV_REPRESENTOR))
+            {
                 out->rep_port_ids.rep_port_ids_val[i++] = port;
+            }
         }
     }
 
@@ -75,4 +98,9 @@ exit:
         free(out->rep_port_ids.rep_port_ids_val);
     out->retval = rc;
     neg_errno_h2rpc(&out->retval);
+#else
+    out->n_rep = 0;
+    out->rep_port_ids.rep_port_ids_len = 0;
+    out->retval = 0;
+#endif /* RTE_ETH_DEV_REPRESENTOR */
 })
