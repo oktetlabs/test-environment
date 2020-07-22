d7f49ff3420efaed2d35d08e076e6fcb1fe937f7
Georgiy Levashov <Georgiy.Levashov@oktetlabs.ru>
2020-07-22 15:28:32 +0300

    cleanup: Fix TA tmp directory path
    To follow ta_dir conventions to have trailing slash in the directory
    path, path was changed from...//tmp to .../tmp/

--- agents/unix/main.c
+++ agents/unix/main.c
@@ -1621,7 +1621,7 @@ main(int argc, char **argv)
      * working directory to it in order to create all temporary files there
      * by default. It will be removed together with TA directory on TA shutdown.
      */
-    TE_SPRINTF(ta_tmp_dir, "%s/tmp", ta_dir);
+    TE_SPRINTF(ta_tmp_dir, "%stmp/", ta_dir);
     if (mkdir(ta_tmp_dir, S_IRWXU | S_IRWXG | S_IRWXO | S_ISVTX) != 0 &&
         errno != EEXIST)
     {
