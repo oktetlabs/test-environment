12191d139d136f7c91475183e6fafea53fc65a34
Viacheslav Galaktionov <Viacheslav.Galaktionov@oktetlabs.ru>
2020-08-05 15:31:49 +0300

    Bug 11132: lib/log_proc: load filters from YAML files
    This functionality is necessary since currently YAML configs are a primary
    target for live results configuration.

---
+++ lib/log_proc/log_filters_yaml.h
@@ -0,0 +1,37 @@
+/** @file
+ * @brief Raw log filter
+ *
+ * YAML parsing for log filters.
+ *
+ * Copyright (C) 2020 OKTET Labs. All rights reserved.
+ *
+ * @author Viacheslav Galaktionov <Viacheslav.Galaktionov@oktetlabs.ru>
+ */
+
+#ifndef __TE_LOG_FILTERS_YAML_H__
+#define __TE_LOG_FILTERS_YAML_H__
+
+#include "log_msg_filter.h"
+#include <yaml.h>
+
+#ifdef _cplusplus
+extern "C" {
+#endif
+
+/**
+ * Load a message filter from a YAML node.
+ *
+ * @param filter        branch filter
+ * @param doc           YAML document
+ * @param node          root YAML node for this filter
+ *
+ * @returns Status code
+ */
+extern te_errno log_msg_filter_load_yaml(log_msg_filter *filter,
+                                         yaml_document_t *doc,
+                                         yaml_node_t *node);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+#endif /* __TE_LOG_FILTERS_YAML_H__ */
