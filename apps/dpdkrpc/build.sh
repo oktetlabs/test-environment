f8b9f7496e6a0acb4f0a2f296b7ae2635d431262
Andrew Rybchenko <Andrew.Rybchenko@oktetlabs.ru>
2020-08-30 17:41:58 +0300

    Bug 8744: app/dpdkrpc: prepare to support meson build
    Make-based build is deprecated in DPDK and will be removed in 20.11.
    Therefore, it will be impossible to build the application using DPDK
    make helpers.

---
+++ apps/dpdkrpc/build.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+#
+# Helper script to build dpdkrpc application.
+#
+# Copyright (C) 2020 OKTET Labs. All rights reserved.
+
+set -e
+
+build_using_make() {
+    cp -a "${EXT_SOURCES}"/Makefile .
+    make V=1 RTE_SDK_BIN="${RTE_SDK_BIN}" O="$(pwd -P)"
+    # DPDK install rules for make do not fit us
+    for ta_type in ${TE_TA_TYPES} ; do
+        cp -t "${TE_AGENTS_INST}/${ta_type}/" dpdkrpc
+    done
+}
+
+RTE_SDK_BIN="$(pwd -P)/../../../platforms/$(basename "${TE_PREFIX}")/lib/dpdk"
+if test -f "${RTE_SDK_BIN}/.config" ; then
+    build_using_make
+else
+    echo "Failed to build dpdkrpc - no .config file" >&2
+    exit 1
+fi
