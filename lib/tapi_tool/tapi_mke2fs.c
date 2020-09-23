447702f3186d23c1af145980ea0aa7b752ac3204
Andrey Izrailev <Andrey.Izrailev@oktetlabs.ru>
2020-09-23 14:09:46 +0300

    Bug 11257: add TAPI to control mke2fs tool (tapi_mke2fs)
    The TAPI is intended to create filesystems with different parameters.
    It would be useful for virtblk-ts.

---
+++ lib/tapi_tool/tapi_mke2fs.c
@@ -0,0 +1,149 @@
+/** @file
+ * @brief mke2fs tool TAPI
+ *
+ * TAPI to handle mke2fs tool.
+ *
+ * Copyright (C) 2020 OKTET Labs. All rights reserved.
+ *
+ * @author Andrey Izrailev <Andrey.Izrailev@oktetlabs.ru>
+ */
+
+#define TE_LGR_USER "TAPI MKE2FS"
+
+#include <signal.h>
+
+#include "tapi_mke2fs.h"
+#include "tapi_job_opt.h"
+#include "te_alloc.h"
+
+#define TAPI_MKE2FS_TERM_TIMEOUT_MS 1000
+
+struct tapi_mke2fs_app {
+    /* TAPI job handle */
+    tapi_job_t *job;
+    /* Output channels handles */
+    tapi_job_channel_t *out_chs[2];
+};
+
+const tapi_mke2fs_opt tapi_mke2fs_default_opt = {
+    .block_size   = TAPI_JOB_OPT_OMIT_UINT,
+    .use_journal  = FALSE,
+    .fs_type      = NULL,
+    .device       = NULL,
+};
+
+static const tapi_job_opt_bind mke2fs_binds[] = TAPI_JOB_OPT_SET(
+    TAPI_JOB_OPT_UINT_OMITTABLE("-b", FALSE, NULL, tapi_mke2fs_opt, block_size),
+    TAPI_JOB_OPT_BOOL("-j", tapi_mke2fs_opt, use_journal),
+    TAPI_JOB_OPT_STRING("-t", FALSE, tapi_mke2fs_opt, fs_type),
+    TAPI_JOB_OPT_STRING(NULL, FALSE, tapi_mke2fs_opt, device)
+);
+
+te_errno
+tapi_mke2fs_create(tapi_job_factory_t *factory, const tapi_mke2fs_opt *opt,
+                   tapi_mke2fs_app **app)
+{
+    te_errno          rc;
+    tapi_mke2fs_app  *result;
+    const char       *path = "mke2fs";
+    te_vec            mke2fs_args = TE_VEC_INIT(char *);
+
+    result = TE_ALLOC(sizeof(*result));
+    if (result == NULL)
+        return TE_RC(TE_TAPI, TE_ENOMEM);
+
+    rc = tapi_job_opt_build_args(path, mke2fs_binds, opt, &mke2fs_args);
+    if (rc != 0)
+    {
+        ERROR("Failed to build mke2fs options");
+        goto out;
+    }
+
+    rc = tapi_job_simple_create(factory,
+                        &(tapi_job_simple_desc_t){
+                            .program    = path,
+                            .argv       = (const char **)mke2fs_args.data.ptr,
+                            .job_loc    = &result->job,
+                            .stdout_loc = &result->out_chs[0],
+                            .stderr_loc = &result->out_chs[1],
+                            .filters    = TAPI_JOB_SIMPLE_FILTERS(
+                                {
+                                    .use_stdout  = TRUE,
+                                    .readable    = FALSE,
+                                    .log_level   = TE_LL_RING,
+                                    .filter_name = "mke2fs stdout",
+                                },
+                                {
+                                    .use_stderr  = TRUE,
+                                    .readable    = FALSE,
+                                    .log_level   = TE_LL_ERROR,
+                                    .filter_name = "mke2fs stderr",
+                                }
+
+                            )
+                        });
+    if (rc != 0)
+    {
+        ERROR("Failed to create job instance for mke2fs tool");
+        goto out;
+    }
+
+    *app = result;
+
+out:
+    te_vec_deep_free(&mke2fs_args);
+    if (rc != 0)
+        free(result);
+
+    return rc;
+}
+
+te_errno
+tapi_mke2fs_start(tapi_mke2fs_app *app)
+{
+    return tapi_job_start(app->job);
+}
+
+te_errno
+tapi_mke2fs_wait(tapi_mke2fs_app *app, int timeout_ms)
+{
+    te_errno          rc;
+    tapi_job_status_t status;
+
+    rc = tapi_job_wait(app->job, timeout_ms, &status);
+    if (rc != 0)
+        return rc;
+
+    TAPI_JOB_CHECK_STATUS(status);
+
+    return 0;
+}
+
+te_errno
+tapi_mke2fs_kill(tapi_mke2fs_app *app, int signum)
+{
+    return tapi_job_kill(app->job, signum);
+}
+
+te_errno
+tapi_mke2fs_stop(tapi_mke2fs_app *app)
+{
+    return tapi_job_stop(app->job, SIGTERM, TAPI_MKE2FS_TERM_TIMEOUT_MS);
+}
+
+te_errno
+tapi_mke2fs_destroy(tapi_mke2fs_app *app)
+{
+    te_errno rc;
+
+    if (app == NULL)
+        return 0;
+
+    rc = tapi_job_destroy(app->job, TAPI_MKE2FS_TERM_TIMEOUT_MS);
+    if (rc != 0)
+        ERROR("Failed to destroy mke2fs job");
+
+    free(app);
+
+    return rc;
+}
---
+++ lib/tapi_tool/tapi_mke2fs.h
@@ -0,0 +1,115 @@
+/** @file
+ * @brief mke2fs tool TAPI
+ *
+ * @defgroup tapi_mke2fs mke2fs tool tapi (tapi_mke2fs)
+ * @ingroup te_ts_tapi
+ * @{
+ *
+ * TAPI to handle mke2fs tool.
+ *
+ * Copyright (C) 2020 OKTET Labs. All rights reserved.
+ *
+ * @author Andrey Izrailev <Andrey.Izrailev@oktetlabs.ru>
+ */
+
+#ifndef __TE_TAPI_MKE2FS_H__
+#define __TE_TAPI_MKE2FS_H__
+
+#include "tapi_job.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** mke2fs tool specific command line options. */
+typedef struct tapi_mke2fs_opt {
+    /**
+     * Size of blocks in bytes. If omitted with TAPI_JOB_OPT_OMIT_UINT,
+     * block-size is heuristically determined by the filesystem size and
+     * the expected usage of the filesystem.
+     */
+    unsigned int block_size;
+    /** Create the filesystem with an ext3 journal. */
+    te_bool use_journal;
+    /** The filesystem type that is to be created. */
+    const char *fs_type;
+    /** The device name on which to create the filesystem (mandatory). */
+    const char *device;
+} tapi_mke2fs_opt;
+
+/** Default options initializer. */
+extern const tapi_mke2fs_opt tapi_mke2fs_default_opt;
+
+/** mke2fs tool information. */
+typedef struct tapi_mke2fs_app tapi_mke2fs_app;
+
+/**
+ * Create mke2fs app.
+ *
+ * @param[in]  factory         Job factory.
+ * @param[in]  opt             mke2fs tool options.
+ * @param[out] app             mke2fs app handle.
+ *
+ * @return     Status code.
+ */
+extern te_errno tapi_mke2fs_create(tapi_job_factory_t *factory,
+                                   const tapi_mke2fs_opt *opt,
+                                   tapi_mke2fs_app **app);
+
+/**
+ * Start mke2fs tool.
+ *
+ * @param      app             mke2fs app handle.
+ *
+ * @return     Status code.
+ */
+extern te_errno tapi_mke2fs_start(tapi_mke2fs_app *app);
+
+/**
+ * Wait for mke2fs tool completion.
+ *
+ * @param      app             mke2fs app handle.
+ * @param      timeout_ms      Wait timeout in milliseconds.
+ *
+ * @return     Status code.
+ * @retval     TE_EINPROGRESS  mke2fs is still running.
+ * @retval     TE_ESHCMD       mke2fs was never started or returned
+ *                             non-zero exit status.
+ */
+extern te_errno tapi_mke2fs_wait(tapi_mke2fs_app *app, int timeout_ms);
+
+/**
+ * Send a signal to mke2fs tool.
+ *
+ * @param      app             mke2fs app handle.
+ * @param      signum          Signal to send.
+ *
+ * @return     Status code.
+ */
+extern te_errno tapi_mke2fs_kill(tapi_mke2fs_app *app, int signum);
+
+/**
+ * Stop mke2fs tool. It can be started over with tapi_mke2fs_start().
+ *
+ * @param      app             mke2fs app handle.
+ *
+ * @return     Status code.
+ */
+extern te_errno tapi_mke2fs_stop(tapi_mke2fs_app *app);
+
+/**
+ * Destroy ping app. The app cannot be used after calling this function.
+ *
+ * @param      app             mke2fs app handle.
+ *
+ * @return     Status code.
+ */
+extern te_errno tapi_mke2fs_destroy(tapi_mke2fs_app *app);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* __TE_TAPI_MKE2FS_H__ */
+
+/** @} <!-- END tapi_mke2fs --> */
