oktetlabs/test-environment

commit 3e6849076cf6e3b9e5370179426f8e3e49082a97

cleanup: lib/add: avoid usage of one more hardcode as offset
We have data_off which tracks current position in segment to
write to.

lib/tad/tad_eth_sap.c
@@ -929,7 +929,7 @@ tad_eth_sap_pkt_rx_ring_recv(tad_eth_sap        *sap,
     {
         struct tad_vlan_tag *tag;
 
-        tag = (struct tad_vlan_tag *)(seg_data + (2 * ETHER_ADDR_LEN));
+        tag = (struct tad_vlan_tag *)(seg_data + data_off);
 
 #ifdef TP_STATUS_VLAN_TPID_VALID
         tag->vlan_tpid = htons((ph->tp_status & TP_STATUS_VLAN_TPID_VALID) ?

commit 73400aa6c266a34cc11aeb1936cff2afe7d585cc

cleanup: lib/tad: fix bug with reading after packet data
remaining_len contains VLAN header space, but it is not actually
present packet data if VLAN stripping is offloaded.

Fixes: 3e6849076cf6 ("cleanup: lib/add: avoid usage of one more hardcode as offset")

lib/tad/tad_eth_sap.c
@@ -925,7 +925,7 @@ tad_eth_sap_pkt_rx_ring_recv(tad_eth_sap        *sap,
     data_off = copy_len;
     remaining_len -= copy_len;
 
-    if (vlan_tag_valid)
+    if (vlan_tag_valid && remaining_len >= TAD_VLAN_TAG_LEN)
     {
         struct tad_vlan_tag *tag;
 
@@ -940,6 +940,7 @@ tad_eth_sap_pkt_rx_ring_recv(tad_eth_sap        *sap,
         tag->vlan_tci = htons(ph->tp_vlan_tci);
 
         data_off += TAD_VLAN_TAG_LEN;
+        remaining_len -= TAD_VLAN_TAG_LEN;
     }
 
     memcpy(seg_data + data_off, (uint8_t *)ph + ph->tp_mac + (2 * ETHER_ADDR_LEN),