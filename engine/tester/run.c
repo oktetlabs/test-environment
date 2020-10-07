oktetlabs/test-environment

commit 3fbf80b99797e2a1f4eb4eb94fc9aa48e333b727

Bug 11083: reference execution plan items in Tester Control messages
This patch adds a `plan_id` field to MI messages. The value of this field
provides a sequential number of the run item in the execution plan.

The implemented approach handles skips caused by prologue failures. Support
for exceptions is "best-effort", same for the `repeat` attribute.

`skip_start` and `skip_end` may be nested. It's possible to avoid this, but
that would mean more changes to the configuration walking code.

engine/tester/run.c
@@ -191,6 +191,12 @@ typedef struct tester_run_data {
                                                  control data */
 
     tester_plan                 plan;       /**< Execution plan */
+    int                         force_skip; /**< Current skip nesting level
+                                                 due to prologue failure */
+    int                         exception;  /**< Current exception handling
+                                                 nesting level */
+    int                         plan_id;    /**< ID of the next run item in
+                                                 the plan */
 
 #if WITH_TRC
     const te_trc_db            *trc_db;     /**< TRC database handle */
@@ -1477,9 +1483,10 @@ log_test_start(unsigned int flags,
      * jansson-2.10, which is currently the newest version available on
      * CentOS/RHEL-7.x
      */
-    result = json_pack("{s:i, s:i, s:o?}",
+    result = json_pack("{s:i, s:i, s:i, s:o?}",
                        "id", test,
                        "parent", parent,
+                       "plan_id", ri->plan_id,
                        "params", test_params_to_json(ri->n_args, ctx->args));
     if (result == NULL)
     {
@@ -1720,7 +1727,7 @@ pack_test_result(const te_test_result *result)
  * @param error         Reason of failure
  */
 static void
-log_test_result(test_id parent, tester_test_result *result)
+log_test_result(test_id parent, tester_test_result *result, int plan_id)
 {
     json_t      *json;
     json_t      *obtained = NULL;
@@ -1796,9 +1803,10 @@ log_test_result(test_id parent, tester_test_result *result)
      * jansson-2.10, which is currently the newest version available on
      * CentOS/RHEL-7.x
      */
-    json = json_pack("{s:i, s:i, s:s?, s:o?, s:s?, s:o?}",
+    json = json_pack("{s:i, s:i, s:i, s:s?, s:o?, s:s?, s:o?}",
                      "id", result->id,
                      "parent", parent,
+                     "plan_id", plan_id,
                      "error", result->error,
                      "obtained", obtained,
                      "tags_expr", tags,
@@ -3112,6 +3120,7 @@ run_exception_start(run_item *ri, unsigned int cfg_id_off, void *opaque)
     }
 
     VERB("Running test session exception handler...");
+    gctx->exception++;
 
     EXIT("CONT");
     return TESTER_CFG_WALK_CONT;
@@ -3134,6 +3143,8 @@ run_exception_end(run_item *ri, unsigned int cfg_id_off, void *opaque)
         EXIT("CONT");
         return TESTER_CFG_WALK_CONT;
     }
+
+    gctx->exception--;
 
     ctx = SLIST_FIRST(&gctx->ctxs);
     assert(ctx != NULL);
@@ -3609,6 +3620,7 @@ run_repeat_start(run_item *ri, unsigned int cfg_id_off, unsigned int flags,
         required = tester_is_run_required(ctx->targets, &ctx->reqs,
                                           ri, ctx->args, ctx->flags,
                                           TRUE);
+        ri->plan_id = -1;
         if (required || !quiet_skip)
         {
             rc = tester_plan_register_run_item(&gctx->plan, ri, ctx);
@@ -3669,12 +3681,37 @@ run_repeat_start(run_item *ri, unsigned int cfg_id_off, unsigned int flags,
         }
     }
 
+    /*
+     * Increment current plan reference ID if not handling any exceptions
+     * and current item is mentioned in the plan
+     */
+    if (gctx->exception == 0 &&
+        ((ctx->flags & TESTER_VERB_SKIP) ||
+         (~ctx->flags & TESTER_QUIET_SKIP) ||
+         tester_is_run_required(ctx->targets, &ctx->reqs,
+                                ri, ctx->args, ctx->flags,
+                                TRUE)))
+        gctx->plan_id++;
+
+    /* Go inside skipped packages and sessions */
+    if (gctx->force_skip > 0 && run_item_container(ri) &&
+        tester_is_run_required(ctx->targets, &ctx->reqs,
+                                    ri, ctx->args, ctx->flags,
+                                    TRUE))
+    {
+        ctx->current_result.status = TESTER_TEST_INCOMPLETE;
+        ctx->group_step = TRUE;
+        EXIT("CONT");
+        return TESTER_CFG_WALK_CONT;
+    }
+
     /* FIXME: Optimize */
     /* verb overwrites quiet */
-    if ((~ctx->flags & TESTER_VERB_SKIP) &&
-        (ctx->flags & TESTER_QUIET_SKIP) &&
-        !tester_is_run_required(ctx->targets, &ctx->reqs,
-                                ri, ctx->args, ctx->flags, TRUE))
+    if ((gctx->force_skip > 0) ||
+        ((~ctx->flags & TESTER_VERB_SKIP) &&
+         (ctx->flags & TESTER_QUIET_SKIP) &&
+         !tester_is_run_required(ctx->targets, &ctx->reqs,
+                                 ri, ctx->args, ctx->flags, TRUE)))
     {
         /* Silently skip without any logs */
         ctx->current_result.status = TESTER_TEST_INCOMPLETE;
@@ -3684,6 +3721,8 @@ run_repeat_start(run_item *ri, unsigned int cfg_id_off, unsigned int flags,
     }
 
     ctx->current_result.id = tester_get_id();
+
+    ri->plan_id = gctx->exception == 0 ? gctx->plan_id - 1 : -1;
 
     tin = (ctx->flags & TESTER_INLOGUE || ri->type != RUN_ITEM_SCRIPT) ?
               TE_TIN_INVALID : cfg_id_off;
@@ -3788,7 +3827,8 @@ run_repeat_end(run_item *ri, unsigned int cfg_id_off, unsigned int flags,
     assert(ctx != NULL);
     LOG_WALK_ENTRY(cfg_id_off, gctx);
 
-    if (ctx->current_result.status == TESTER_TEST_INCOMPLETE)
+    if (gctx->force_skip > 0 ||
+        ctx->current_result.status == TESTER_TEST_INCOMPLETE)
     {
         ctx->current_result.status = TESTER_TEST_EMPTY;
     }
@@ -3901,7 +3941,7 @@ run_repeat_end(run_item *ri, unsigned int cfg_id_off, unsigned int flags,
 
         tin = (ctx->flags & TESTER_INLOGUE || ri->type != RUN_ITEM_SCRIPT) ?
                   TE_TIN_INVALID : cfg_id_off;
-        log_test_result(ctx->group_result.id, &ctx->current_result);
+        log_test_result(ctx->group_result.id, &ctx->current_result, ri->plan_id);
 
         tester_term_out_done(ctx->flags, ri->type, run_item_name(ri), tin,
                              ctx->group_result.id,
@@ -4073,6 +4113,22 @@ run_repeat_end(run_item *ri, unsigned int cfg_id_off, unsigned int flags,
     }
 }
 
+static void
+run_skip_start(void *opaque)
+{
+    tester_run_data *gctx = opaque;
+
+    gctx->force_skip++;
+}
+
+static void
+run_skip_end(void *opaque)
+{
+    tester_run_data *gctx = opaque;
+
+    gctx->force_skip--;
+}
+
 /**
  * Check whether preparatory walk over testing configuration
  * tree can help to improve scenario.
@@ -4145,6 +4201,8 @@ tester_run(testing_scenario   *scenario,
         run_repeat_start,
         run_repeat_end,
         run_script,
+        run_skip_start,
+        run_skip_end,
     };
 
     testing_act   *act;

engine/tester/config_prepare.c
@@ -604,6 +604,8 @@ tester_prepare_configs(tester_cfgs *cfgs)
         NULL, /* repeat_start */
         NULL, /* repeat_end */
         prepare_script,
+        NULL, /* skip_start */
+        NULL, /* skip_end */
     };
 
     ENTRY();

engine/tester/config_walk.c
@@ -208,6 +208,8 @@ walk_test_session(const tester_cfg_walk *walk, const void *opaque,
 {
     tester_cfg_walk_ctl ctl;
     tester_cfg_walk_ctl ctl_tmp;
+    te_bool             skip_items = FALSE;
+    te_bool             have_skip_cb;
 
     ENTRY("run=%s id_off=%u flags=%#x", run_item_name(ri), id_off, flags);
 
@@ -225,13 +227,20 @@ walk_test_session(const tester_cfg_walk *walk, const void *opaque,
                                walk->prologue_start, walk->prologue_end);
         }
 
-        if (ctl == TESTER_CFG_WALK_CONT)
-        {
+        skip_items = ctl != TESTER_CFG_WALK_CONT;
+        have_skip_cb = walk->skip_start != NULL && walk->skip_end != NULL;
+
+        if (skip_items && have_skip_cb)
+            walk->skip_start((void *)opaque);
+
+        if (!skip_items || have_skip_cb)
             ctl = walk_run_items(walk, (void *)opaque, id_off, flags,
                                  &session->run_items,
                                  session->keepalive,
                                  session->exception);
-        }
+
+        if (skip_items && have_skip_cb)
+            walk->skip_end((void *)opaque);
 
         /*
          * Global context flag TESTER_BREAK_SESION is 0 on default.
@@ -289,6 +298,8 @@ walk_test_package(const tester_cfg_walk *walk, const void *opaque,
 {
     tester_cfg_walk_ctl  ctl;
     tester_cfg_walk_ctl  ctl_tmp;
+    te_bool              skip_items;
+    te_bool              have_skip_cb;
 
     ENTRY("run=%s id_off=%u flags=%#x", run_item_name(ri), id_off, flags);
 
@@ -298,11 +309,18 @@ walk_test_package(const tester_cfg_walk *walk, const void *opaque,
     else
         ctl = TESTER_CFG_WALK_CONT;
 
-    if (ctl == TESTER_CFG_WALK_CONT)
-    {
+    skip_items = ctl != TESTER_CFG_WALK_CONT;
+    have_skip_cb = walk->skip_start != NULL && walk->skip_end != NULL;
+
+    if (skip_items && have_skip_cb)
+        walk->skip_start((void *)opaque);
+
+    if (!skip_items || have_skip_cb)
         ctl = walk_test_session(walk, opaque, id_off, flags, ri,
                                 &pkg->session);
-    }
+
+    if (skip_items && have_skip_cb)
+        walk->skip_end((void *)opaque);
 
     if (walk->pkg_end != NULL)
     {

engine/tester/test_path.c
@@ -803,6 +803,8 @@ process_test_path(const tester_cfgs *cfgs, test_path *path)
         NULL, /* repeat_start */
         NULL, /* repeat_end */
         NULL, /* script */
+        NULL, /* skip_start */
+        NULL, /* skip_end */
     };
 
     ENTRY("path=%s type=%d", path->str, path->type);

engine/tester/tester_conf.h
@@ -266,6 +266,7 @@ struct run_item {
 
     const test_session *context;    /**< Parent session */
 
+    int                 plan_id;    /**< Plan ID */
     char               *name;       /**< Name or NULL */
     char               *objective;  /**< Objective, if NULL - taken from script */
     char               *page;       /**< Page, if NULL - taken from script */
@@ -692,6 +693,8 @@ typedef struct tester_cfg_walk {
                                        unsigned int, void *);
     tester_cfg_walk_ctl (* script)(run_item *, test_script *,
                                    unsigned int, void *);
+    void                (* skip_start)(void *);
+    void                (* skip_end)(void *);
 } tester_cfg_walk;
 
 /**

tools/rgt/rgt-core/log_msg.c
@@ -220,7 +220,7 @@ rgt_process_tester_control_message_json(log_msg *msg)
          * values and type-checked manually.
          */
         err_code = json_unpack_ex(msg_json, &json_error, JSON_STRICT,
-                                  "{s:i, s:i, s?s, s?o, s?o, s?o, s?o}",
+                                  "{s:i, s:i, s?s, s?o, s?o, s?o, s?o, s?o}",
                                   "id",        &node_id,
                                   "parent",    &parent_id,
                                   "status",    &status,
@@ -231,6 +231,7 @@ rgt_process_tester_control_message_json(log_msg *msg)
                                    * function, they are only mentioned here
                                    * for schema validation.
                                    */
+                                  "plan_id",   &ignored,
                                   "expected",  &ignored,
                                   "tags_expr", &ignored);
         if (err_code != 0)
@@ -736,6 +737,7 @@ create_node_by_msg_json(json_t *msg, uint32_t *ts)
     json_t     *page_opt = NULL;
     json_t     *hash_opt = NULL;
     json_t     *tin_opt = NULL;
+    json_t     *ignored = NULL;
     const char *type = NULL;
     const char *name = NULL;
     const char *objective = NULL;
@@ -761,7 +763,7 @@ create_node_by_msg_json(json_t *msg, uint32_t *ts)
      * values and type-checked manually.
      */
     ret = json_unpack_ex(msg, &err, JSON_STRICT,
-                         "{s:i, s:i, s:s, s?o, s?o, s?o, s?o, s?o, s?o, s?o}",
+                         "{s:i, s:i, s:s, s?o, s?o, s?o, s?o, s?o, s?o, s?o, s?o}",
                          "id", &node->node_id,
                          "parent", &node->parent_id,
                          "node_type", &type,
@@ -771,7 +773,8 @@ create_node_by_msg_json(json_t *msg, uint32_t *ts)
                          "hash", &hash_opt,
                          "tin", &tin_opt,
                          "authors", &authors,
-                         "params", &params);
+                         "params", &params,
+                         "plan_id", &ignored);
     if (ret != 0)
     {
         FMT_TRACE("Error unpacking JSON log message: %s (line %d, column %d)",

tools/rgt/rgt-format/mi_msg.c
@@ -787,10 +787,12 @@ te_rgt_parse_mi_test_start_message(te_rgt_mi *mi)
         goto cleanup;
     }
 
+    data->plan_id = -1;
     ret = json_unpack_ex(root, &err, JSON_STRICT,
-                         "{s:i, s:i, s:s, s?o, s?o, s?o, s?o, s?o, s?o, s?o}",
+                         "{s:i, s:i, s?i, s:s, s?o, s?o, s?o, s?o, s?o, s?o, s?o}",
                          "id", &data->node_id,
                          "parent", &data->parent_id,
+                         "plan_id", &data->plan_id,
                          "node_type", &data->node_type,
                          "name", &name,
                          "params", &params,
@@ -1023,10 +1025,12 @@ te_rgt_parse_mi_test_end_message(te_rgt_mi *mi)
         goto cleanup;
     }
 
+    data->plan_id = -1;
     ret = json_unpack_ex(root, &err, JSON_STRICT,
-                         "{s:i, s:i, s?s, s?o, s?o, s?o, s?o}",
+                         "{s:i, s:i, s?i, s?s, s?o, s?o, s?o, s?o}",
                          "id", &data->node_id,
                          "parent", &data->parent_id,
+                         "plan_id", &data->plan_id,
                          "status", &data->obtained.status,
                          "obtained", &obtained,
                          "expected", &expected,

tools/rgt/rgt-format/mi_msg.h
@@ -130,6 +130,7 @@ typedef struct te_rgt_mi_person {
 typedef struct te_rgt_mi_test_start {
     int               node_id;    /**< Node ID */
     int               parent_id;  /**< Parent ID */
+    int               plan_id;    /**< Plan ID */
     const char       *node_type;  /**< PACKAGE, SESSION or TEST */
     const char       *name;       /**< Name */
     te_rgt_mi_kv     *params;     /**< Array of parameters */
@@ -155,6 +156,7 @@ typedef struct te_rgt_mi_test_result {
 typedef struct te_rgt_mi_test_end {
     int                    node_id;      /**< Node ID */
     int                    parent_id;    /**< Parent ID */
+    int                    plan_id;      /**< Plan ID */
     const char            *error;        /**< TRC error message */
     const char            *tags_expr;    /**< Matched tag expression */
     te_rgt_mi_test_result  obtained;     /**< Obtained result */

commit dd4afe5412289c372564fbc99d7a6b5bc13a70e7

Bug 11083: fix --tester-run-while option
Fixes: 3fbf80b99 Bug 11083: reference execution plan items in Tester Control messages

After changes introdused by the patch,
result given when --tester-run-while fails becomes tester_configs_walk() result.
That is why it have to be changed from TESTER_CFG_WALK_STOP to
TESTER_CFG_FIN, so it would pass preparatory tree walk.

engine/tester/run.c
@@ -3989,24 +3989,24 @@ run_repeat_end(run_item *ri, unsigned int cfg_id_off, unsigned int flags,
             if ((ctx->flags & TESTER_RUN_WHILE_PASSED) &&
                 (ctx->current_result.status != TESTER_TEST_PASSED))
             {
-                return TESTER_CFG_WALK_STOP;
+                return TESTER_CFG_WALK_FIN;
             }
             if ((ctx->flags & TESTER_RUN_WHILE_FAILED) &&
                 (ctx->current_result.status != TESTER_TEST_FAILED))
             {
-                return TESTER_CFG_WALK_STOP;
+                return TESTER_CFG_WALK_FIN;
             }
 #if WITH_TRC
             if ((ctx->flags & TESTER_RUN_WHILE_EXPECTED) &&
                 (ctx->current_result.exp_status != TRC_VERDICT_EXPECTED))
             {
-                return TESTER_CFG_WALK_STOP;
+                return TESTER_CFG_WALK_FIN;
             }
             if ((ctx->flags & TESTER_RUN_WHILE_UNEXPECTED) &&
                 (ctx->current_result.exp_status != TRC_VERDICT_UNEXPECTED &&
                  ctx->current_result.exp_status != TRC_VERDICT_UNKNOWN))
             {
-                return TESTER_CFG_WALK_STOP;
+                return TESTER_CFG_WALK_FIN;
             }
 #endif
         }