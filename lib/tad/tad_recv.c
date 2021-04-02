oktetlabs/test-environment

commit d0a5685053d221eadb62e9d8d400c6dce3768b44

Bug 11411: lib/tad: fill in mismatch packet payload
If mismatch packets are requested by the test, mismatch data should
be reported in payload. In theory, mismatch layer still could be
filled in, but in theory data could be too short for the layer and
it requires careful handling. Don't care about it for now. It means
that only match layers are filled in and nicely reported in NDS.

lib/tad/tad_recv.c
@@ -952,8 +952,22 @@ tad_recv_match_with_unit(csap_p csap, tad_recv_ptrn_unit_data *unit_data,
         VERB("match cb 0x%x for layer %u sdu_len=%u returned %r",
              csap_spt_descr->match_do_cb, layer,
              (unsigned)tad_pkt_len(sdu), rc);
-        if (rc != 0)
-            return rc;
+        switch (rc)
+        {
+            case 0:
+                break;
+            case TE_ETADNOTMATCH:
+                /* Move everything starting from mismatch layer to payload */
+                if (csap->state & CSAP_STATE_RECV_MISMATCH)
+                {
+                    (void)tad_pkt_get_frag(&meta_pkt->payload, pdu, 0,
+                                           tad_pkt_len(pdu),
+                                           TAD_PKT_GET_FRAG_ERROR);
+                }
+                /* FALLTHROUGH */
+            default:
+                return rc;
+        }
     } while (layer-- > 0);
 
     /* Match payload */

commit aa43138e56c9a092178798c4fb3796510e97d44d

Bug 11411: lib/tad: fix mismatch packet payload reporting
Error without domain should be used as a switch key.

Fixes: d0a5685053d2 ("Bug 11411: lib/tad: fill in mismatch packet payload")

lib/tad/tad_recv.c
@@ -957,7 +957,7 @@ tad_recv_match_with_unit(csap_p csap, tad_recv_ptrn_unit_data *unit_data,
         VERB("match cb 0x%x for layer %u sdu_len=%u returned %r",
              csap_spt_descr->match_do_cb, layer,
              (unsigned)tad_pkt_len(sdu), rc);
-        switch (rc)
+        switch (TE_RC_GET_ERROR(rc))
         {
             case 0:
                 break;