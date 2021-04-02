oktetlabs/test-environment

commit 89415f609789fe6ac39770337e6e8a9e6a3baa9c

Bug 11411: lib/tad: fix crash on mismatch packet reporting
If a packet does not match pattern, mismatch layers data are not
filled in by match callback. So, it cannot be convered to NDS -
just skip it.

lib/tad/tad_recv.c
@@ -1473,6 +1473,13 @@ tad_recv_get_packets(csap_p csap, tad_reply_context *reply_ctx, te_bool wait,
             ERROR("ERROR: %s:%u", __FILE__, __LINE__);
         for (layer = 0; layer < csap->depth; ++layer)
         {
+            /*
+             * Handle mismatch packet: silently skip layer if match packet
+             * is empty.
+             */
+            if (tad_pkt_len(tad_pkts_first_pkt(&pkt->layers[layer].pkts)) == 0)
+                continue;
+
             if (csap_get_proto_support(csap, layer)->match_post_cb != NULL)
             {
                 rc = csap_get_proto_support(csap, layer)->match_post_cb(

commit fd2a1b1e740ae172e269e2fdd06f5bef0a67c934

Bug 11411: lib/tad: fix mismatch packet conversion to ASN.1
asn_insert_indexed() fails if too big index is requested.
It could happen if one of layers is skipped. Use -1 to
insert into the tail.

Fixes: 89415f609789 ("Bug 11411: lib/tad: fix crash on mismatch packet reporting")

lib/tad/tad_recv.c
@@ -1512,7 +1512,7 @@ tad_recv_get_packets(csap_p csap, tad_reply_context *reply_ctx, te_bool wait,
             if (asn_put_child_value(pdu, pkt->layers[layer].nds, PRIVATE,
                                     csap->layers[layer].proto_tag) != 0)
                 ERROR("ERROR: %s:%u", __FILE__, __LINE__);
-            if (asn_insert_indexed(pdus, pdu, layer, "") != 0)
+            if (asn_insert_indexed(pdus, pdu, -1, "") != 0)
                 ERROR("ERROR: %s:%u", __FILE__, __LINE__);
         }
 