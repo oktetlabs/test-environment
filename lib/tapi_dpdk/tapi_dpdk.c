oktetlabs/test-environment

commit cfc3390987ff03d45bcf3716e2797da59006cb9f

Bug 8744: tapi_dpdk: add Rx/Tx doorbells filters

lib/tapi_dpdk/tapi_dpdk.c
@@ -543,6 +543,27 @@ append_testpmd_cmdline_from_args(te_kvpair_h *test_args,
     }
 }
 
+static te_errno
+tapi_dpdk_attach_filters(tapi_job_simple_desc_t *desc)
+{
+    te_errno rc;
+    tapi_job_simple_filter_t *filter;
+
+    for (filter = desc->filters;
+         filter->use_stdout || filter->use_stderr;
+         filter++)
+    {
+        if ((rc = tapi_job_attach_simple_filter(desc, filter)) != 0)
+        {
+            tapi_job_destroy(*desc->job_loc, -1);
+            *desc->job_loc = NULL;
+            return rc;
+        }
+    }
+
+    return 0;
+}
+
 te_errno
 tapi_dpdk_grab_cpus(const char *ta,
                     unsigned int n_cpus_preferred,
@@ -1248,4 +1269,54 @@ tapi_dpdk_mbuf_size_by_pkt_size(unsigned int packet_size,
     *mbuf_size = minimal_mbuf_size;
 
     return TRUE;
+}
+
+te_errno
+tapi_dpdk_attach_dbells_filter_rx(tapi_dpdk_testpmd_job_t *testpmd_job)
+{
+    tapi_job_simple_desc_t desc = {
+        .job_loc = &testpmd_job->job,
+        .stdout_loc = &testpmd_job->out_channels[0],
+        .filters = TAPI_JOB_SIMPLE_FILTERS(
+            {.use_stdout = TRUE,
+             .readable = TRUE,
+             .re = "(?m)rx_dbells\\s*([0-9]+)\\s*([0-9]+)",
+             .extract = 2,
+             .filter_var = &testpmd_job->rx_dbells_filter,
+            },
+            {.use_stdout = TRUE,
+             .readable = TRUE,
+             .re = "(?m)No\\sxstat\\s'rx_dbells'",
+             .extract = 0,
+             .filter_var = &testpmd_job->rx_dbells_skip_filter,
+            }
+        )
+    };
+
+    return tapi_dpdk_attach_filters(&desc);
+}
+
+te_errno
+tapi_dpdk_attach_dbells_filter_tx(tapi_dpdk_testpmd_job_t *testpmd_job)
+{
+    tapi_job_simple_desc_t desc = {
+        .job_loc = &testpmd_job->job,
+        .stdout_loc = &testpmd_job->out_channels[0],
+        .filters = TAPI_JOB_SIMPLE_FILTERS(
+            {.use_stdout = TRUE,
+             .readable = TRUE,
+             .re = "(?m)tx_dbells\\s*([0-9]+)\\s*([0-9]+)",
+             .extract = 2,
+             .filter_var = &testpmd_job->tx_dbells_filter,
+            },
+            {.use_stdout = TRUE,
+             .readable = TRUE,
+             .re = "(?m)No\\sxstat\\s'tx_dbells'",
+             .extract = 0,
+             .filter_var = &testpmd_job->tx_dbells_skip_filter,
+            }
+        )
+    };
+
+    return tapi_dpdk_attach_filters(&desc);
 }

lib/tapi_dpdk/tapi_dpdk.h
@@ -49,6 +49,10 @@ typedef struct tapi_dpdk_testpmd_job_t {
     tapi_job_channel_t *tx_pps_filter;
     tapi_job_channel_t *rx_pps_filter;
     tapi_job_channel_t *link_speed_filter;
+    tapi_job_channel_t *tx_dbells_filter;
+    tapi_job_channel_t *tx_dbells_skip_filter;
+    tapi_job_channel_t *rx_dbells_filter;
+    tapi_job_channel_t *rx_dbells_skip_filter;
 } tapi_dpdk_testpmd_job_t;
 
 
@@ -258,6 +262,27 @@ extern const char *tapi_dpdk_get_vdev_eal_argument(int eal_argc,
  */
 extern te_errno tapi_dpdk_get_vdev_port_number(const char *vdev,
                                                unsigned int *port_number);
+
+/**
+ * Attach filters to catch Rx doorbells rate and if they are supported.
+ *
+ * @param testpmd_job           Handle of running test-pmd job
+ *
+ * @return Status code
+ */
+extern te_errno tapi_dpdk_attach_dbells_filter_rx(
+                                        tapi_dpdk_testpmd_job_t *testpmd_job);
+
+/**
+ * Attach filters to catch Tx doorbells rate and if they are supported.
+ *
+ * @param testpmd_job           Handle of running test-pmd job
+ *
+ * @return Status code
+ */
+extern te_errno tapi_dpdk_attach_dbells_filter_tx(
+                                        tapi_dpdk_testpmd_job_t *testpmd_job);
+
 
 #ifdef __cplusplus
 } /* extern "C" */

commit cbc2d8761e18fb796c635f5282d5679d7c05b58c

Bug 8744: tapi_dpdk: fix filters checking for doorbells support to use stderr
Fixes: cfc3390987 ("Bug 8744: tapi_dpdk: add Rx/Tx doorbells filters")

lib/tapi_dpdk/tapi_dpdk.c
@@ -1474,6 +1474,7 @@ tapi_dpdk_attach_dbells_filter_rx(tapi_dpdk_testpmd_job_t *testpmd_job)
     tapi_job_simple_desc_t desc = {
         .job_loc = &testpmd_job->job,
         .stdout_loc = &testpmd_job->out_channels[0],
+        .stderr_loc = &testpmd_job->out_channels[1],
         .filters = TAPI_JOB_SIMPLE_FILTERS(
             {.use_stdout = TRUE,
              .readable = TRUE,
@@ -1481,7 +1482,7 @@ tapi_dpdk_attach_dbells_filter_rx(tapi_dpdk_testpmd_job_t *testpmd_job)
              .extract = 2,
              .filter_var = &testpmd_job->rx_dbells_filter,
             },
-            {.use_stdout = TRUE,
+            {.use_stderr = TRUE,
              .readable = TRUE,
              .re = "(?m)No\\sxstat\\s'rx_dbells'",
              .extract = 0,
@@ -1499,6 +1500,7 @@ tapi_dpdk_attach_dbells_filter_tx(tapi_dpdk_testpmd_job_t *testpmd_job)
     tapi_job_simple_desc_t desc = {
         .job_loc = &testpmd_job->job,
         .stdout_loc = &testpmd_job->out_channels[0],
+        .stderr_loc = &testpmd_job->out_channels[1],
         .filters = TAPI_JOB_SIMPLE_FILTERS(
             {.use_stdout = TRUE,
              .readable = TRUE,
@@ -1506,7 +1508,7 @@ tapi_dpdk_attach_dbells_filter_tx(tapi_dpdk_testpmd_job_t *testpmd_job)
              .extract = 2,
              .filter_var = &testpmd_job->tx_dbells_filter,
             },
-            {.use_stdout = TRUE,
+            {.use_stderr = TRUE,
              .readable = TRUE,
              .re = "(?m)No\\sxstat\\s'tx_dbells'",
              .extract = 0,