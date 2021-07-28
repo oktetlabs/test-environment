oktetlabs/test-environment

commit 78e4b91cee5ff29de992ec4537fe9ebad0356f55

Bug 8744: te_mi_log: add events per sec and per another events meas units
Add generic measurement units events per seconds (Hz) and events per
another event. This might be used for example for doorbells statistics
such as doorbells rate and packets per doorbell.

lib/tools/te_mi_log.c
@@ -108,6 +108,7 @@ typedef enum te_mi_meas_base_unit_type {
     TE_MI_MEAS_BASE_UNIT_BPS, /**< Bits per second */
     TE_MI_MEAS_BASE_UNIT_CELSIUS, /**< Degrees of celsius */
     TE_MI_MEAS_BASE_UNIT_RPS, /**< Requests per second */
+    TE_MI_MEAS_BASE_UNIT_HZ, /**< Events per seconds (Hz) */
 } te_mi_meas_base_unit_type;
 
 static te_mi_meas_base_unit_type meas_base_unit_by_type_map[] = {
@@ -119,6 +120,8 @@ static te_mi_meas_base_unit_type meas_base_unit_by_type_map[] = {
     [TE_MI_MEAS_RPS] = TE_MI_MEAS_BASE_UNIT_RPS,
     [TE_MI_MEAS_RTT] = TE_MI_MEAS_BASE_UNIT_SECOND,
     [TE_MI_MEAS_RETRANS] = TE_MI_MEAS_BASE_UNITLESS,
+    [TE_MI_MEAS_FREQ] = TE_MI_MEAS_BASE_UNIT_HZ,
+    [TE_MI_MEAS_EPE] = TE_MI_MEAS_BASE_UNITLESS,
 };
 
 static const char *meas_base_unit_names[] = {
@@ -128,6 +131,7 @@ static const char *meas_base_unit_names[] = {
     [TE_MI_MEAS_BASE_UNIT_BPS] =  "bps",
     [TE_MI_MEAS_BASE_UNIT_CELSIUS] = "degrees celsius",
     [TE_MI_MEAS_BASE_UNIT_RPS] = "rps",
+    [TE_MI_MEAS_BASE_UNIT_HZ] = "Hz",
 };
 
 static const char *mi_type_names[] = {

lib/tools/te_mi_log.h
@@ -237,6 +237,8 @@ typedef enum te_mi_meas_type {
     TE_MI_MEAS_RPS, /**< Requests per second */
     TE_MI_MEAS_RTT, /**< Rount trip time in seconds */
     TE_MI_MEAS_RETRANS, /**< TCP retransmissons */
+    TE_MI_MEAS_FREQ, /**< Events per seconds (Hz) */
+    TE_MI_MEAS_EPE, /**< Events per another event */
 
     TE_MI_MEAS_END, /**< End marker for a measurement vector.
                          Also is one past last valid type */

commit c189419f700137d44b2acbc2038405f1e1504342

Bug 8744: add strings for events per sec and per another events
Add string representations for for events per sec and per another
events measurement units to have human-readable reports.

Fixes: 78e4b91cee5f ("Bug 8744: te_mi_log: add events per sec and per another events meas units")

lib/tools/te_mi_log.c
@@ -162,6 +162,8 @@ static const char *meas_type_names[] = {
     [TE_MI_MEAS_RPS] = "rps",
     [TE_MI_MEAS_RTT] = "rtt",
     [TE_MI_MEAS_RETRANS] = "TCP retransmissions",
+    [TE_MI_MEAS_FREQ] = "events-per-second",
+    [TE_MI_MEAS_EPE] = "events-per-event",
 };
 
 /** Array for storing string representations of view types */
@@ -858,6 +860,12 @@ te_mi_meas_type2descr(te_mi_meas_type type)
 
         case TE_MI_MEAS_RETRANS:
             return "TCP retransmissions";
+
+        case TE_MI_MEAS_FREQ:
+            return "Events per second";
+
+        case TE_MI_MEAS_EPE:
+            return "Events per another event";
 
         default:
             return "Unknown type";