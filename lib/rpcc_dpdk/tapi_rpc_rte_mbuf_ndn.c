oktetlabs/test-environment

commit 4a1a888768305da88be48e022156d96b62c5ee4b

Bug 8744: add a means to match Rx burst on TST with DPDK
Explain the principles of operation in multiple comments.

lib/rpcc_dpdk/tapi_rpc_rte_mbuf_ndn.c
@@ -193,3 +193,65 @@ rpc_rte_mbuf_match_tx_rx_pre(rcf_rpc_server *rpcs,
 
     RETVAL_ZERO_INT(rte_mbuf_match_tx_rx_pre, out.retval);
 }
+
+static const char *
+tarpc_rte_mbuf_ol_status2str(enum tarpc_rte_mbuf_ol_status ol_status)
+{
+    switch (ol_status)
+    {
+        case TARPC_RTE_MBUF_OL_NA:
+            return "NA";
+            break;
+        case TARPC_RTE_MBUF_OL_DONE:
+            return "DONE";
+            break;
+        case TARPC_RTE_MBUF_OL_NOT_DONE:
+            return "NOT_DONE";
+            break;
+        default:
+            return "__CORRUPTED";
+            break;
+    }
+}
+
+int
+rpc_rte_mbuf_match_tx_rx(rcf_rpc_server               *rpcs,
+                         rpc_rte_mbuf_p                m_tx,
+                         rpc_rte_mbuf_p               *rx_burst,
+                         unsigned int                  nb_rx,
+                         struct tarpc_rte_mbuf_report *reportp)
+{
+    tarpc_rte_mbuf_match_tx_rx_in in;
+    tarpc_rte_mbuf_match_tx_rx_out out;
+    te_log_buf *tlbp;
+
+    memset(&in, 0, sizeof(in));
+    memset(&out, 0, sizeof(out));
+
+    in.m_tx = (tarpc_rte_mbuf)m_tx;
+    in.rx_burst.rx_burst_val = tapi_memdup(rx_burst, nb_rx * sizeof(*rx_burst));
+    in.rx_burst.rx_burst_len = nb_rx;
+
+    rcf_rpc_call(rpcs, "rte_mbuf_match_tx_rx", &in, &out);
+
+    tlbp = te_log_buf_alloc();
+    TAPI_RPC_LOG(rpcs, rte_mbuf_match_tx_rx,
+                 "m_tx = " RPC_PTR_FMT "; rx_burst = %s", "offloads = { "
+                 "vlan = %s; outer_ip_cksum = %s; outer_udp_cksum = %s; "
+                 "innermost_ip_cksum = %s; innermost_l4_cksum = %s }; "
+                 NEG_ERRNO_FMT, RPC_PTR_VAL(in.m_tx),
+                 rpc_rte_mbufs2str(tlbp, in.rx_burst.rx_burst_val,
+                                   in.rx_burst.rx_burst_len, rpcs),
+                 tarpc_rte_mbuf_ol_status2str(out.report.ol_vlan),
+                 tarpc_rte_mbuf_ol_status2str(out.report.ol_outer_ip_cksum),
+                 tarpc_rte_mbuf_ol_status2str(out.report.ol_outer_udp_cksum),
+                 tarpc_rte_mbuf_ol_status2str(out.report.ol_innermost_ip_cksum),
+                 tarpc_rte_mbuf_ol_status2str(out.report.ol_innermost_l4_cksum),
+                 NEG_ERRNO_ARGS(out.retval));
+    te_log_buf_free(tlbp);
+
+    if (reportp != NULL)
+        memcpy(reportp, &out.report, sizeof(*reportp));
+
+    RETVAL_ZERO_INT(rte_mbuf_match_tx_rx, out.retval);
+}

include/te_defs.h
@@ -507,6 +507,8 @@ te_round_to_zero(unsigned int n, unsigned int m)
     return (n / m) * m;
 }
 
+#define TE_DIV_ROUND_UP(_n, _d) (((_n) + (_d) - 1) / (_d))
+
 #ifdef __cplusplus
 } /* extern "C" */
 #endif

lib/rpcc_dpdk/tapi_rpc_rte_mbuf_ndn.h
@@ -106,6 +106,46 @@ extern int tapi_rte_mbuf_match_pattern_seq(rcf_rpc_server    *rpcs,
 extern int rpc_rte_mbuf_match_tx_rx_pre(rcf_rpc_server *rpcs,
                                         rpc_rte_mbuf_p  m);
 
+/**
+ * Ensure that the given Tx mbuf and Rx burst match. If
+ * they do, provide status of HW offloads in the report.
+ *
+ * @param[in]  m_tx     Tx mbuf
+ * @param[in]  rx_burst Rx burst
+ * @param[in]  nb_rx    Rx burst size
+ * @param[out] reportp  Report location; can be @c NULL
+ *
+ * @note Usage instructions for future test maintainers:
+ *       1) Construct the mbuf by means of rte_mbuf SAP
+ *          in the assumption that payload size is zero;
+ *       2) Remove any padding at the end of the packet
+ *          and append the actual payload to the packet;
+ *       3) Adjust the mbuf to enable hardware offloads;
+ *       4) Invoke rpc_rte_mbuf_match_tx_rx_pre() on it;
+ *       5) If the packet is supposed to be received on
+ *          a different test agent than the one used to
+ *          transmit it, clone the mbuf between the two;
+ *       6) If required, make @p m_tx a multi-seg chain;
+ *       7) Carry out transmit and receive transactions;
+ *       8) Invoke rpc_rte_mbuf_match_tx_rx() on the Rx
+ *          burst and pass the mbuf from step (5) to it.
+ *
+ * @note @p m tx must abide by prerequisites imposed by
+ *       @c rpc_rte_mbuf_match_tx_rx_pre().
+ *
+ * @note For correct Tx VLAN insertion status discovery,
+ *       Rx VLAN stripping must be enabled on Rx device.
+ *
+ * @note The mbufs will be modified and can't be reused.
+ *
+ * @return @c 0 on success; jumps out on failure
+ */
+extern int rpc_rte_mbuf_match_tx_rx(rcf_rpc_server               *rpcs,
+                                    rpc_rte_mbuf_p                m_tx,
+                                    rpc_rte_mbuf_p               *rx_burst,
+                                    unsigned int                  nb_rx,
+                                    struct tarpc_rte_mbuf_report *reportp);
+
 /**@} <!-- END te_lib_rpc_rte_mbuf_ndn --> */
 
 #ifdef __cplusplus

lib/rpcxdr/tarpc_dpdk.x.m4
@@ -719,6 +719,35 @@ struct tarpc_rte_mbuf_match_pattern_out {
 typedef struct tarpc_mbuf_in tarpc_rte_mbuf_match_tx_rx_pre_in;
 typedef struct tarpc_int_retval_out tarpc_rte_mbuf_match_tx_rx_pre_out;
 
+/** rte_mbuf_match_tx_rx() */
+struct tarpc_rte_mbuf_match_tx_rx_in {
+    struct tarpc_in_arg common;
+    tarpc_rte_mbuf      m_tx;
+    tarpc_rte_mbuf      rx_burst<>;
+};
+
+enum tarpc_rte_mbuf_ol_status {
+    TARPC_RTE_MBUF_OL_NA = 0,
+    TARPC_RTE_MBUF_OL_DONE,
+    TARPC_RTE_MBUF_OL_NOT_DONE,
+
+    TARPC_RTE_MBUF_OL__UNKNOWN
+};
+
+struct tarpc_rte_mbuf_report {
+    enum tarpc_rte_mbuf_ol_status ol_vlan;
+    enum tarpc_rte_mbuf_ol_status ol_outer_ip_cksum;
+    enum tarpc_rte_mbuf_ol_status ol_outer_udp_cksum;
+    enum tarpc_rte_mbuf_ol_status ol_innermost_ip_cksum;
+    enum tarpc_rte_mbuf_ol_status ol_innermost_l4_cksum;
+};
+
+struct tarpc_rte_mbuf_match_tx_rx_out {
+    struct tarpc_out_arg         common;
+    struct tarpc_rte_mbuf_report report;
+    tarpc_int                    retval;
+};
+
 /*
  * rte_eth_dev API
  */
@@ -2024,6 +2053,7 @@ program dpdk
         RPC_DEF(rte_mk_mbuf_from_template)
         RPC_DEF(rte_mbuf_match_pattern)
         RPC_DEF(rte_mbuf_match_tx_rx_pre)
+        RPC_DEF(rte_mbuf_match_tx_rx)
 
         RPC_DEF(rte_eth_stats_get)
         RPC_DEF(rte_eth_xstats_get)

commit a86467103d1b3616c18abc0413f0cecd3a4b18a5

Bug 8744: fix memory leak and take care of style consistency
Fixes: 4a1a88876830 ("Bug 8744: add a means to match Rx burst on TST with DPDK")

lib/rpcc_dpdk/tapi_rpc_rte_mbuf_ndn.c
@@ -221,9 +221,9 @@ rpc_rte_mbuf_match_tx_rx(rcf_rpc_server               *rpcs,
                          unsigned int                  nb_rx,
                          struct tarpc_rte_mbuf_report *reportp)
 {
-    tarpc_rte_mbuf_match_tx_rx_in in;
-    tarpc_rte_mbuf_match_tx_rx_out out;
-    te_log_buf *tlbp;
+    tarpc_rte_mbuf_match_tx_rx_in   in;
+    tarpc_rte_mbuf_match_tx_rx_out  out;
+    te_log_buf                     *tlbp;
 
     memset(&in, 0, sizeof(in));
     memset(&out, 0, sizeof(out));
@@ -250,6 +250,8 @@ rpc_rte_mbuf_match_tx_rx(rcf_rpc_server               *rpcs,
                  tarpc_rte_mbuf_ol_status2str(out.report.ol_innermost_l4_cksum),
                  out.report.tso_cutoff_barrier, NEG_ERRNO_ARGS(out.retval));
     te_log_buf_free(tlbp);
+
+    free(in.rx_burst.rx_burst_val);
 
     if (reportp != NULL)
         memcpy(reportp, &out.report, sizeof(*reportp));