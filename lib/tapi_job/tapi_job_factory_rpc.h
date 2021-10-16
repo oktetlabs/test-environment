496d770a793d4155917af753dcf8a6b784e1c948
Andrey Izrailev <Andrey.Izrailev@oktetlabs.ru>
2021-10-16 17:15:02 +0300

    cleanup: tapi_job_factory_rpc: add a file header, fix a typo
    Each .h file must have an appropriate header.

--- lib/tapi_job/tapi_job_factory_rpc.h
+++ lib/tapi_job/tapi_job_factory_rpc.h
@@ -1,3 +1,17 @@
+/** @file
+ * @brief TAPI to handle RPC server job factory
+ *
+ * @defgroup tapi_job_factory_rpc RPC server job factory control (tapi_job_factory_rpc)
+ * @ingroup tapi_job
+ * @{
+ *
+ * TAPI to handle RPC server job factory.
+ *
+ * Copyright (C) 2019 OKTET Labs. All rights reserved.
+ *
+ * @author Igor Romanov <Igor.Romanov@oktetlabs.ru>
+ */
+
 #ifndef __TAPI_JOB_FACTORY_RPC_H__
 #define __TAPI_JOB_FACTORY_RPC_H__
 
@@ -10,7 +24,7 @@ extern "C" {
 struct tapi_job_factory_t;
 
 /**
- * Create a job factory by the RPC server @p rcps.
+ * Create a job factory by the RPC server @p rpcs.
  *
  * @param rpcs          RPC server
  * @param[out] factory  Job factory handle
@@ -25,3 +39,5 @@ extern te_errno tapi_job_factory_rpc_create(rcf_rpc_server *rpcs,
 #endif
 
 #endif /* __TAPI_JOB_FACTORY_RPC_H__ */
+
+/** @} <!-- END tapi_job_factory_rpc --> */
