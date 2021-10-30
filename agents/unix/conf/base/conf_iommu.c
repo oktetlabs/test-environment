oktetlabs/test-environment

commit 019ae580f1203c0496305ff4896c997ad81a0c40

Bug 8744: add node indicating if IOMMU is enabled
It's useful to determine if a host has IOMMU disabled.
For example to use vfio-pci module that requires a proper
option to be passed in this case.

agents/unix/conf/base/conf_iommu.c
@@ -1,0 +1,58 @@
+/** @file
+ * @brief IOMMU status
+ *
+ * IOMMU status
+ *
+ * Copyright (C) 2003-2021 OKTET Labs. All rights reserved.
+ *
+ * @author Ivan Ilchenko <Ivan.Ilchenko@oktetlabs.ru>
+ */
+
+#include <sys/types.h>
+#include <dirent.h>
+
+#include "rcf_ch_api.h"
+#include "rcf_pch.h"
+
+static te_errno
+pci_iommu_get(unsigned int gid, const char *oid, char *value,
+              const char *unused1, const char *unused2, const char *unused3)
+{
+    const char *dirname = "/sys/class/iommu";
+    DIR *dir;
+    int dirs_nb;
+
+    UNUSED(gid);
+    UNUSED(oid);
+    UNUSED(unused1);
+    UNUSED(unused2);
+    UNUSED(unused3);
+
+    /* Check if directory is empty, i.e. contains only '.' and '..' entries */
+    dir = opendir(dirname);
+    if (dir == NULL)
+        return TE_OS_RC(TE_TA_UNIX, errno);
+    dirs_nb = 0;
+    while (readdir(dir) != NULL)
+    {
+        dirs_nb++;
+        if (dirs_nb > 2)
+            break;
+    }
+
+    if (dirs_nb > 2)
+        strcpy(value, "on");
+    else
+        strcpy(value, "off");
+
+    return 0;
+}
+
+RCF_PCH_CFG_NODE_RO_COLLECTION(node_pci_iommu, "iommu", NULL, NULL,
+                               pci_iommu_get, NULL);
+
+te_errno
+ta_unix_conf_iommu_init(void)
+{
+    return rcf_pch_add_node("/agent/hardware", &node_pci_iommu);
+}

agents/unix/conf/base/conf.c
@@ -273,6 +273,7 @@ extern te_errno ta_unix_conf_sys_tree_fini(void);
 extern te_errno ta_unix_conf_phy_init(void);
 extern te_errno ta_unix_conf_if_coalesce_init(void);
 extern te_errno ta_unix_conf_eth_init(void);
+extern te_errno ta_unix_conf_iommu_init(void);
 extern te_errno ta_unix_conf_macvlan_init(void);
 extern te_errno ta_unix_conf_ipvlan_init(void);
 extern te_errno ta_unix_conf_module_init(void);
@@ -1345,6 +1346,9 @@ rcf_ch_conf_init(void)
             goto fail;
 
         if (ta_unix_conf_eth_init() != 0)
+            goto fail;
+
+        if (ta_unix_conf_iommu_init() != 0)
             goto fail;
 
         rcf_pch_rsrc_init();

agents/unix/conf/meson.build
@@ -19,6 +19,7 @@ sources += files(
     'base/conf_configfs.c',
     'base/conf_cpu.c',
     'base/conf_eth.c',
+    'base/conf_iommu.c',
     'base/conf_ipvlan.c',
     'base/conf_l4_port.c',
     'base/conf_macvlan.c',

doc/cm/cm_system.yml
@@ -389,3 +389,11 @@
              and let the test or some other entity interpret the data,
            - identify some set of important parameters and make a bona fide
              effort to provide platform-independent way to access them.
+
+    - oid: "/agent/hardware/iommu"
+      access: read_only
+      type: string
+      d: |
+         Node indicating if IOMMU is enabled
+         Name: none
+         Value: "on", "off"

commit 37df9de10cf855c3c96af493e98a037c6fafb842

Bug 8744: agents/unix: fix leak in IOMMU get accessor
Both memory and FD leaked.

Fixes: 019ae580f120 ("Bug 8744: add node indicating if IOMMU is enabled")

agents/unix/conf/base/conf_iommu.c
@@ -45,6 +45,8 @@ pci_iommu_get(unsigned int gid, const char *oid, char *value,
     else
         strcpy(value, "off");
 
+    (void)closedir(dir);
+
     return 0;
 }
 