oktetlabs/test-environment

commit aed3658b8fff7f674a64df473e5fdb102256ca13

Bug 11610: conf_netconsole: fix lookup of local IP address and interface
Netconsole configure function picks random IP address and interface as
source for some target host. Fix it using correct procedure of performing
routing table lookup.

Fixes: d38af41c67 ("[Bug 6990]:  Adding configfs and netconsole nodes in Configuration tree")

lib/netconf/route.c
@@ -14,6 +14,7 @@
 #include "netconf.h"
 #include "netconf_internal.h"
 #include "te_sockaddr.h"
+#include <sys/ioctl.h>
 
 /**
  * Callback of routes dump.
@@ -533,4 +534,106 @@ netconf_route_modify(netconf_handle nh, netconf_cmd cmd,
 
     /* Send request and receive ACK */
     return netconf_talk(nh, &req, sizeof(req), NULL, NULL, NULL);
+}
+
+netconf_list *
+netconf_route_get_entry_for_addr(netconf_handle nh,
+                                 const struct sockaddr *dst_addr)
+{
+    char                req[NETCONF_MAX_REQ_LEN];
+    struct nlmsghdr    *h;
+    struct rtmsg       *rtm;
+    netconf_list       *list;
+
+    if (nh == NULL)
+    {
+        errno = EINVAL;
+        return NULL;
+    }
+
+    if (dst_addr->sa_family != AF_INET)
+    {
+        ERROR("%s(): failed, IPv6 is not supported", __FUNCTION__);
+        errno = ENOTSUP;
+        return NULL;
+    }
+
+    memset(&req, 0, sizeof(req));
+
+    h = (struct nlmsghdr *)req;
+    h->nlmsg_len = NLMSG_LENGTH(sizeof(struct rtmsg));
+    h->nlmsg_type = RTM_GETROUTE;
+    h->nlmsg_flags = NLM_F_REQUEST;
+    h->nlmsg_seq = ++nh->seq;
+
+    rtm = NLMSG_DATA(h);
+    rtm->rtm_family = AF_INET;
+    rtm->rtm_dst_len = sizeof(in_addr_t);
+    rtm->rtm_src_len = 0;
+    rtm->rtm_tos = 0;
+    rtm->rtm_table = RT_TABLE_UNSPEC;
+    rtm->rtm_protocol = RTPROT_UNSPEC;
+    rtm->rtm_scope = RT_SCOPE_UNIVERSE;
+    rtm->rtm_type = RTN_UNSPEC;
+    rtm->rtm_flags = RTM_F_LOOKUP_TABLE;
+
+    netconf_append_rta(h, &(SIN(dst_addr)->sin_addr.s_addr),
+                       sizeof(in_addr_t), RTA_DST);
+
+    list = malloc(sizeof(*list));
+    if (list == NULL)
+        return NULL;
+
+    memset(list, 0, sizeof(netconf_list));
+
+    if (netconf_talk(nh, &req, h->nlmsg_len, route_list_cb, NULL, list) != 0)
+    {
+        int err = errno;
+
+        netconf_list_free(list);
+        errno = err;
+        return NULL;
+    }
+
+    return list;
+}
+
+int
+netconf_route_get_src_addr_and_iface(netconf_handle         nh,
+                                     const struct sockaddr  *dst_addr,
+                                     const struct sockaddr  *src_addr,
+                                     char                   *ifname)
+{
+    netconf_list    *l;
+    netconf_route   *route;
+    int             rc;
+
+    l = netconf_route_get_entry_for_addr(nh, dst_addr);
+    if (l == NULL)
+    {
+        ERROR("%s(): failed to get entry in routing table for remote "
+              "IP address", __FUNCTION__);
+        return errno;
+    }
+
+    route = &(l->tail->data.route);
+    if (route->family != AF_INET)
+    {
+        ERROR("%s(): failed, IPv6 is not supported", __FUNCTION__);
+        rc = ENOTSUP;
+        goto out;
+    }
+    SIN(src_addr)->sin_family = route->family;
+    memcpy(&(SIN(src_addr)->sin_addr.s_addr), route->src, sizeof(in_addr_t));
+
+    if (if_indextoname(route->oifindex, ifname) == NULL)
+    {
+        rc = errno;
+        goto out;
+    }
+    rc = 0;
+
+out:
+    netconf_list_free(l);
+    return rc;
 }

agents/unix/conf/base/conf_netconsole.c
@@ -34,6 +34,8 @@
 #include "ta_common.h"
 #include "unix_internal.h"
 #include "te_shell_cmd.h"
+#include "netconf.h"
+#include "conf_netconf.h"
 
 #if HAVE_SYS_IOCTL_H
 #include <sys/ioctl.h>
@@ -131,20 +133,13 @@ configure_netconsole(in_port_t local_port, const char *remote_host_name,
 #define IFS_BUF_SIZE 1024
 #define MAX_STR 1024
 #if defined(SIOCGARP) && defined(SIOCGIFCONF)
-    char            local_host_name[HOST_NAME_MAX];
 
     struct sockaddr_in  local_ipv4_addr;
     struct sockaddr_in  remote_ipv4_addr;
-    te_bool             local_ipv4_found = FALSE;
     te_bool             remote_ipv4_found = FALSE;
     int                 rc;
     int                 s = -1;
     struct arpreq       remote_hwaddr_req;
-
-    char            buf[IFS_BUF_SIZE];
-    struct ifconf   ifc;
-    int             i;
-    int             ifs_count;
 
     char    cmdline[MAX_STR];
     int     tmp_err;
@@ -153,21 +148,7 @@ configure_netconsole(in_port_t local_port, const char *remote_host_name,
     char    remote_ip_str[RCF_MAX_VAL];
     char    remote_hwaddr_str[RCF_MAX_VAL];
 
-    if (gethostname(local_host_name, HOST_NAME_MAX) < 0)
-    {
-        tmp_err = errno;
-        ERROR("%s(): failed to obtain host name", __FUNCTION__);
-        return te_rc_os2te(tmp_err);
-    }
-
-    rc = te_get_host_addrs(local_host_name, &local_ipv4_addr,
-                           &local_ipv4_found, NULL, NULL);
-    if (rc != 0)
-    {
-        ERROR("%s(): failed to obtain addresses of local host",
-              __FUNCTION__);
-        return rc;
-    }
+    char ifname[IF_NAMESIZE];
 
     rc = te_get_host_addrs(remote_host_name, &remote_ipv4_addr,
                            &remote_ipv4_found, NULL, NULL);
@@ -178,11 +159,20 @@ configure_netconsole(in_port_t local_port, const char *remote_host_name,
         return rc;
     }
 
-    if (!local_ipv4_found || !remote_ipv4_found)
+    if (!remote_ipv4_found)
     {
-        ERROR("%s(): failed to find IPv4 address for local and/or "
-              "remote host", __FUNCTION__);
+        ERROR("%s(): failed to find IPv4 address for remote host",
+              __FUNCTION__);
         return TE_EADDRNOTAVAIL;
+    }
+
+    rc = netconf_route_get_src_addr_and_iface(nh, SA(&remote_ipv4_addr),
+                                              SA(&local_ipv4_addr), ifname);
+    if (rc != 0)
+    {
+        ERROR("%s(): failed to get source address and interface, errno '%s'",
+              __FUNCTION__, strerror(rc));
+        return te_rc_os2te(rc);
     }
 
     s = socket(AF_INET, SOCK_DGRAM, 0);
@@ -192,35 +182,6 @@ configure_netconsole(in_port_t local_port, const char *remote_host_name,
         ERROR("%s(): failed to create datagram socket, errno '%s'",
               __FUNCTION__, strerror(errno));
         return te_rc_os2te(tmp_err);
-    }
-
-    memset(&ifc, 0, sizeof(ifc));
-    ifc.ifc_len = sizeof(buf);
-    ifc.ifc_buf = buf;
-
-    if (ioctl(s, SIOCGIFCONF, &ifc) < 0)
-    {
-        tmp_err = errno;
-        ERROR("%s(): ioctl(SIOCGIFCONF) failed, errno '%s'",
-              __FUNCTION__, strerror(errno));
-        close(s);
-        return te_rc_os2te(tmp_err);
-    }
-
-    ifs_count = ifc.ifc_len / sizeof (struct ifreq);
-
-    for (i = 0; i < ifs_count; i++)
-    {
-        if (memcmp(&ifc.ifc_req[i].ifr_addr, &local_ipv4_addr,
-                   sizeof(local_ipv4_addr)) == 0)
-            break;
-    }
-
-    if (i == ifs_count)
-    {
-        ERROR("%s(): local interface not found", __FUNCTION__); 
-        close(s);
-        return TE_ENOENT;
     }
 
     local_ipv4_addr.sin_port = htons(local_port);
@@ -255,7 +216,7 @@ configure_netconsole(in_port_t local_port, const char *remote_host_name,
            sizeof(remote_ipv4_addr));
     ((struct sockaddr_in *)&remote_hwaddr_req.arp_pa)->sin_port = 0;
 #ifdef HAVE_STRUCT_ARPREQ_ARP_DEV
-    te_strlcpy(remote_hwaddr_req.arp_dev, ifc.ifc_req[i].ifr_name,
+    te_strlcpy(remote_hwaddr_req.arp_dev, ifname,
                sizeof(remote_hwaddr_req.arp_dev));
 #endif
     ((struct sockaddr_in *)&remote_hwaddr_req.arp_ha)->sin_family =
@@ -315,7 +276,7 @@ configure_netconsole(in_port_t local_port, const char *remote_host_name,
                  "/sbin/modprobe netconsole netconsole=%d@%s/%s,"
                  "%d@%s/%s",
                  (int)local_port, local_ip_str,
-                 ifc.ifc_req[i].ifr_name, (int)remote_port,
+                 ifname, (int)remote_port,
                  remote_ip_str, remote_hwaddr_str);
 
         if (ta_system("/sbin/modprobe -r netconsole") != 0)
@@ -369,7 +330,7 @@ configure_netconsole(in_port_t local_port, const char *remote_host_name,
                  "echo %d > remote_port && echo %s > local_ip && "
                  "echo %s > remote_ip && echo %s > remote_mac &&"
                  "echo 1 > enabled || exit 1",
-                 tmp_path, ifc.ifc_req[i].ifr_name, (int)local_port,
+                 tmp_path, ifname, (int)local_port,
                  (int)remote_port, local_ip_str, remote_ip_str,
                  remote_hwaddr_str);
 

lib/netconf/netconf.h
@@ -451,6 +451,32 @@ netconf_list *netconf_route_dump(netconf_handle nh,
                                  unsigned char family);
 
 /**
+ * Get list with routing table entry for specified destination address.
+ *
+ * @param nh            Netconf session handle.
+ * @param dst_addr      Destination address, now only IPv4 is supported.
+ *
+ * @return List, or NULL in case of error (check errno for details).
+ */
+netconf_list *netconf_route_get_entry_for_addr(netconf_handle nh,
+                                               const struct sockaddr *dst_addr);
+
+/**
+ * Get source address and interface for specified destination address.
+ *
+ * @param nh           Netconf session handle
+ * @param dst_addr     Destination address
+ * @param src_addr     Source address (OUT)
+ * @param ifname       Source interface. Must be at least IF_NAMESIZE. (OUT)
+ *
+ * @return Status code
+ */
+int netconf_route_get_src_addr_and_iface(netconf_handle         nh,
+                                         const struct sockaddr  *dst_addr,
+                                         const struct sockaddr  *src_addr,
+                                         char                   *ifname);
+
+/**
  * Set default values to fields in rule struct.
  *
  * @param rule          Rule entry struct

commit 6c2a7cc86475ae5074edbd98ee60bbf06db5f6af

Bug 11610: fix get routing table entry function for older kernels
The function uses an unspecified routing table and
RTM_F_LOOKUP_TABLE flag not supported in older kernels.
Fix it by specifying explicitly table main.

Fixes: aed3658b8fff ("Bug 11610: conf_netconsole: fix lookup of local IP address and interface")

lib/netconf/route.c
@@ -571,11 +571,11 @@ netconf_route_get_entry_for_addr(netconf_handle nh,
     rtm->rtm_dst_len = sizeof(in_addr_t);
     rtm->rtm_src_len = 0;
     rtm->rtm_tos = 0;
-    rtm->rtm_table = RT_TABLE_UNSPEC;
+    rtm->rtm_table = RT_TABLE_MAIN;
     rtm->rtm_protocol = RTPROT_UNSPEC;
     rtm->rtm_scope = RT_SCOPE_UNIVERSE;
     rtm->rtm_type = RTN_UNSPEC;
-    rtm->rtm_flags = RTM_F_LOOKUP_TABLE;
+    rtm->rtm_flags = 0;
 
     netconf_append_rta(h, &(SIN(dst_addr)->sin_addr.s_addr),
                        sizeof(in_addr_t), RTA_DST);