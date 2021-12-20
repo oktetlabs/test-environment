oktetlabs/test-environment

commit 147ba1b58a0801adccde7ba144c3406ce5d87a1c

Bug 11411: Add API to get unmatched packets from CSAP
Now user (test) will receive unmatched packets (payload only) as well
as matched packets using existing API.

Unmathched packets will be received `stealthily` - they will not be
count, but will be received together with matched packets (with special
"mismatch" flag) until required number of matched packets will be received.

lib/tad/tad_recv.c
@@ -632,6 +632,9 @@ tad_recv_start_prepare(csap_p csap, const char *ptrn_str,
     if (flags & RCF_CH_TRRECV_PACKETS_SEQ_MATCH)
         csap->state |= CSAP_STATE_RECV_SEQ_MATCH;
 
+    if (flags & RCF_CH_TRRECV_MISMATCH)
+        csap->state |= CSAP_STATE_RECV_MISMATCH;
+
     csap->first_pkt = csap->last_pkt = tad_tv_zero;
 
     CSAP_UNLOCK(csap);
@@ -1261,11 +1264,20 @@ tad_recv_do(csap_p csap)
         if (TE_RC_GET_ERROR(rc) == TE_ETADNOTMATCH)
         {
             context->no_match_pkts++;
+            if (csap->state & CSAP_STATE_RECV_MISMATCH)
+            {
+                meta_pkt->match_unit = -1;
+                tad_recv_pkt_enqueue(csap, &context->packets, meta_pkt);
+                meta_pkt = NULL;
+            }
+            else
+            {
+                /* Nothing is owned by match routine */
+                tad_recv_pkt_cleanup(csap, meta_pkt);
+            }
 
             VERB(CSAP_LOG_FMT "received packet does not match",
                  CSAP_LOG_ARGS(csap));
-            /* Nothing is owned by match routine */
-            tad_recv_pkt_cleanup(csap, meta_pkt);
             continue;
         }
         if (TE_RC_GET_ERROR(rc) == TE_ETADLESSDATA)
@@ -1300,6 +1312,8 @@ tad_recv_do(csap_p csap)
 
         if ((csap->state & CSAP_STATE_RESULTS) && !no_report)
         {
+            meta_pkt->match_unit = context->ptrn_data.cur_unit;
+
             F_VERB(CSAP_LOG_FMT "put packet into the queue",
                    CSAP_LOG_ARGS(csap));
             tad_recv_pkt_enqueue(csap, &context->packets, meta_pkt);
@@ -1435,11 +1449,22 @@ tad_recv_get_packets(csap_p csap, tad_reply_context *reply_ctx, te_bool wait,
 
     while ((rc = tad_recv_get_packet(csap, wait, &pkt)) == 0)
     {
-        (*got)++;
 
         /* Process packet */
         pkt->nds = asn_init_value(ndn_raw_packet);
         /* FIXME: Check pkt->nds */
+
+        asn_write_int32(pkt->nds, pkt->match_unit, "match-unit");
+
+        /*
+         * Number of match packets must be reported here for backward
+         * compatibility. If caller wants to know number of got mismatch
+         * packets, it should be calculated on test side in packet
+         * handling callback
+         */
+        if (pkt->match_unit != -1)
+            (*got)++;
+
         asn_write_int32(pkt->nds, pkt->ts.tv_sec,
                         "received.seconds");
         asn_write_int32(pkt->nds, pkt->ts.tv_usec,

lib/ndn/ndn.h
@@ -171,6 +171,7 @@ typedef enum {
     NDN_PKT_TIMESTAMP,
     NDN_PKT_PDUS,
     NDN_PKT_PAYLOAD,
+    NDN_PKT_MATCH_UNIT,
 } ndn_raw_packet_tags_t;
 
 typedef enum {

lib/ndn/ndn_generic.c
@@ -416,17 +416,19 @@ const asn_type * const  ndn_time_stamp = &ndn_time_stamp_s;
 /*
 Raw-Packet ::= SEQUENCE -- values of this type are passed from CSAP to test
 {
-    received    NDN-TimeStamp,
-    pdus        SEQUENCE (SIZE (1..max-pdus)) OF Generic-PDU,
-    payload     Payload OPTIONAL
+    received        NDN-TimeStamp,
+    pdus            SEQUENCE (SIZE (1..max-pdus)) OF Generic-PDU,
+    payload         Payload OPTIONAL
+    match-unit      Index of matched pattern unit, -1 for mismatch packets
 }
 */
 
 
 static asn_named_entry_t _ndn_raw_packet_ne_array[] = {
-    { "received",  &ndn_time_stamp_s, {PRIVATE, NDN_PKT_TIMESTAMP} },
-    { "pdus",      &ndn_generic_pdu_sequence_s, {PRIVATE, NDN_PKT_PDUS} },
-    { "payload",   &ndn_payload_s,    {PRIVATE, NDN_PKT_PAYLOAD} },
+    { "received",      &ndn_time_stamp_s, {PRIVATE, NDN_PKT_TIMESTAMP} },
+    { "pdus",          &ndn_generic_pdu_sequence_s, {PRIVATE, NDN_PKT_PDUS} },
+    { "payload",       &ndn_payload_s,    {PRIVATE, NDN_PKT_PAYLOAD} },
+    { "match-unit",    &asn_base_integer_s, {PRIVATE, NDN_PKT_MATCH_UNIT} }
 };
 
 asn_type ndn_raw_packet_s = {

lib/rcfapi/rcf_api.h
@@ -65,9 +65,11 @@ typedef enum rcf_call_mode {
 typedef enum rcf_trrecv_mode {
     RCF_TRRECV_COUNT,              /**< Count received packets only */
     RCF_TRRECV_NO_PAYLOAD,         /**< Get packet headers only, do not get
-                                        payload */
+                                       payload */
     RCF_TRRECV_PACKETS,            /**< Store packets to get from test later */
     RCF_TRRECV_SEQ_MATCH = 0x04,   /**< Pattern sequence matching */
+    RCF_TRRECV_MISMATCH = 0x08,    /**< Store mismatch packets
+                                        to get from test later */
 } rcf_trrecv_mode;
 
 /**

lib/rcfpch/rcf_ch_api.h
@@ -486,8 +486,11 @@ extern int rcf_ch_trsend_stop(struct rcf_comm_connection *handle,
 typedef enum rcf_ch_trrecv_flags {
     RCF_CH_TRRECV_PACKETS = 1,           /**< Receive and report packets */
     RCF_CH_TRRECV_PACKETS_NO_PAYLOAD = 2,/**< Do not report packets
-                                             payload */
-    RCF_CH_TRRECV_PACKETS_SEQ_MATCH = 4, /**< Use pattern sequence for matching */
+                                              payload */
+    RCF_CH_TRRECV_PACKETS_SEQ_MATCH = 4, /**< Use pattern sequence for
+                                              matching */
+    RCF_CH_TRRECV_MISMATCH = 8,          /**< Store mismatch packets
+                                              to get from test later */
 } rcf_ch_trrecv_flags;
 
 /**

lib/rcfpch/rcf_pch.c
@@ -1083,6 +1083,13 @@ rcf_pch_run(const char *confstr, const char *info)
                     SKIP_SPACES(ptr);
                 }
 
+                if (strncmp(ptr, "mismatch", strlen("mismatch")) == 0)
+                {
+                    mode |= RCF_CH_TRRECV_MISMATCH;
+                    ptr += strlen("mismatch");
+                    SKIP_SPACES(ptr);
+                }
+
                 if (*ptr != 0)
                     goto bad_protocol;
 

lib/tad/tad_ch.c
@@ -974,6 +974,9 @@ rcf_ch_trsend_recv(struct rcf_comm_connection *rcfc,
             csap->state |= CSAP_STATE_PACKETS_NO_PAYLOAD;
     }
 
+    if (flags & RCF_CH_TRRECV_MISMATCH)
+        csap->state |= CSAP_STATE_RECV_MISMATCH;
+
     csap->first_pkt = tad_tv_zero;
     csap->last_pkt  = tad_tv_zero;
 

lib/tad/tad_csap_inst.h
@@ -123,27 +123,30 @@ typedef struct csap_layer_t {
 
 /** @name CSAP processing flags */
 enum {
-    CSAP_STATE_IDLE       = 0x0001, /**< CSAP is idle */
-    CSAP_STATE_SEND       = 0x0002, /**< CSAP is sending or idle after
+    CSAP_STATE_IDLE       = 0x00001, /**< CSAP is idle */
+    CSAP_STATE_SEND       = 0x00002, /**< CSAP is sending or idle after
                                          the send operation */
-    CSAP_STATE_RECV       = 0x0004, /**< CSAP is receiving or idle after
+    CSAP_STATE_RECV       = 0x00004, /**< CSAP is receiving or idle after
                                          the receive operation */
-    CSAP_STATE_DONE       = 0x0010, /**< Processing has been finished */
-    CSAP_STATE_SEND_DONE  = 0x0020, /**< Send has been finished */
-    CSAP_STATE_RECV_DONE  = 0x0040, /**< Receive has been finished */
-    CSAP_STATE_COMPLETE   = 0x0100, /**< Receive operation complete */
+    CSAP_STATE_DONE       = 0x00010, /**< Processing has been finished */
+    CSAP_STATE_SEND_DONE  = 0x00020, /**< Send has been finished */
+    CSAP_STATE_RECV_DONE  = 0x00040, /**< Receive has been finished */
+    CSAP_STATE_COMPLETE   = 0x00100, /**< Receive operation complete */
 
-    CSAP_STATE_RECV_SEQ_MATCH =     0x0200, /**< Pattern sequence matching */
+    CSAP_STATE_RECV_SEQ_MATCH =     0x00200, /**< Pattern sequence matching */
 
-    CSAP_STATE_PACKETS_NO_PAYLOAD = 0x0400, /**< Do not report payload of
+    CSAP_STATE_RECV_MISMATCH =      0x00400, /**< Store mismatch packets
+                                                  to get from test later */
+
+    CSAP_STATE_PACKETS_NO_PAYLOAD = 0x00800, /**< Do not report payload of
                                                  received packets */
-    CSAP_STATE_RESULTS    = 0x0800, /**< Receive results are required */
+    CSAP_STATE_RESULTS    = 0x01000, /**< Receive results are required */
 
-    CSAP_STATE_FOREGROUND = 0x1000, /**< RCF answer is pending */
-    CSAP_STATE_WAIT       = 0x2000, /**< User request to wait for
+    CSAP_STATE_FOREGROUND = 0x02000, /**< RCF answer is pending */
+    CSAP_STATE_WAIT       = 0x04000, /**< User request to wait for
                                          end of processing */
-    CSAP_STATE_STOP       = 0x4000, /**< User request to stop */
-    CSAP_STATE_DESTROY    = 0x8000, /**< CSAP is being destroyed */
+    CSAP_STATE_STOP       = 0x08000, /**< User request to stop */
+    CSAP_STATE_DESTROY    = 0x10000, /**< CSAP is being destroyed */
 };
 /*@}*/
 

commit f360249d12fd8be39ed3ee4248050c77c85397a9

Bug 11411: lib/tad: set unit number on non-sequence multi-pattern match
Since it sets what pattern unit packets matched, this allows
the user (i.e. test) to know what patterns have been matched.

Fixes: 147ba1b58a08 ("Bug 11411: Add API to get unmatched packets from CSAP")

lib/tad/tad_recv.c
@@ -1035,7 +1035,8 @@ tad_recv_match(csap_p csap, tad_recv_pattern_data *ptrn_data,
         return rc;
     }
 
-    if (ptrn_data->cur_unit == ptrn_data->n_units)
+    if ((csap->state & CSAP_STATE_RECV_SEQ_MATCH) &&
+        ptrn_data->cur_unit == ptrn_data->n_units)
     {
         F_VERB(CSAP_LOG_FMT "The matching of pattern sequence is finished",
                CSAP_LOG_ARGS(csap), unit, rc);
@@ -1057,6 +1058,10 @@ tad_recv_match(csap_p csap, tad_recv_pattern_data *ptrn_data,
 
                 if (csap->state & CSAP_STATE_RECV_SEQ_MATCH)
                     ptrn_data->cur_unit++;
+                else
+                    /* Let this packet know what unit it matched */
+                    ptrn_data->cur_unit = unit;
+
                 /*@fallthrough@*/
 
             case TE_ETADLESSDATA: