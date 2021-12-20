oktetlabs/test-environment

commit d0a5685053d221eadb62e9d8d400c6dce3768b44

Bug 11411: lib/tad: fill in mismatch packet payload
If mismatch packets are requested by the test, mismatch data should
be reported in payload. In theory, mismatch layer still could be
filled in, but in theory data could be too short for the layer and
it requires careful handling. Don't care about it for now. It means
that only match layers are filled in and nicely reported in NDS.

lib/tad/tad_recv.c
@@ -952,8 +952,22 @@ tad_recv_match_with_unit(csap_p csap, tad_recv_ptrn_unit_data *unit_data,
         VERB("match cb 0x%x for layer %u sdu_len=%u returned %r",
              csap_spt_descr->match_do_cb, layer,
              (unsigned)tad_pkt_len(sdu), rc);
-        if (rc != 0)
-            return rc;
+        switch (rc)
+        {
+            case 0:
+                break;
+            case TE_ETADNOTMATCH:
+                /* Move everything starting from mismatch layer to payload */
+                if (csap->state & CSAP_STATE_RECV_MISMATCH)
+                {
+                    (void)tad_pkt_get_frag(&meta_pkt->payload, pdu, 0,
+                                           tad_pkt_len(pdu),
+                                           TAD_PKT_GET_FRAG_ERROR);
+                }
+                /* FALLTHROUGH */
+            default:
+                return rc;
+        }
     } while (layer-- > 0);
 
     /* Match payload */

commit 956e2f0753f367df881b75d71d4681362166afe7

Bug 11433: fix non-sequence multi-pattern matching
Non-sequence multi-pattern routines iterate every received packet
over patterns list trying to match them against each other. If
matching on bottom layer (e.g. Ethernet) fails, the content moved
to payload field and the layer itself cleaned. This doesn't allow
further matching on second and so on iterations. Preserve bottom
layer on such occasions and clean it later if no match occured.

Fixes: d0a5685053 ("Bug 11411: lib/tad: fill in mismatch packet payload")

lib/tad/tad_recv.c
@@ -898,9 +898,12 @@ tad_recv_match_payload(tad_payload_spec_t *pattern, const tad_pkt *payload)
  * Try match binary data with Traffic-Pattern-Unit and prepare ASN value
  * with packet if it satisfies to the pattern unit.
  *
- * @param csap          CSAP instance
- * @param unit_data     Unit data prepared during preprocessing
- * @param meta_pkt      Receiver meta packet
+ * @param csap                  CSAP instance
+ * @param unit_data             Unit data prepared during preprocessing
+ * @param meta_pkt              Receiver meta packet
+ * @param clean_bottom_layer    Shows if mismatch happened on bottom layer
+ *                              (e.g. Ethernet). Caller is responsible for
+ *                              cleanup.
  *
  * @return Status code.
  * @retval 0                Received packet match, meta_pkt is owned and
@@ -913,7 +916,7 @@ tad_recv_match_payload(tad_payload_spec_t *pattern, const tad_pkt *payload)
  */
 static te_errno
 tad_recv_match_with_unit(csap_p csap, tad_recv_ptrn_unit_data *unit_data,
-                         tad_recv_pkt *meta_pkt)
+                         tad_recv_pkt *meta_pkt, te_bool *clean_bottom_layer)
 {
     const asn_value    *pattern_unit = unit_data->nds;
     unsigned int        layer;
@@ -968,7 +971,22 @@ tad_recv_match_with_unit(csap_p csap, tad_recv_ptrn_unit_data *unit_data,
                     (void)tad_pkt_get_frag(&meta_pkt->payload, pdu, 0,
                                            tad_pkt_len(pdu),
                                            TAD_PKT_GET_FRAG_ERROR);
-                    tad_pkt_cleanup(pdu);
+                    if (layer == csap->depth - 1)
+                    {
+                        /*
+                         * Don't clean this layer since packet content
+                         * would be lost. It is caller responsibility to do
+                         * it when needed, i.e. matching packet against
+                         * pattern units is over and cleaning is required
+                         * to invalidate Ethernet for mismatch reporting.
+                         */
+                        *clean_bottom_layer = TRUE;
+                    }
+                    else
+                    {
+                        tad_pkt_cleanup(pdu);
+                        *clean_bottom_layer = FALSE;
+                    }
                 }
                 /* FALLTHROUGH */
             default:
@@ -1018,6 +1036,7 @@ static te_errno
 tad_recv_match(csap_p csap, tad_recv_pattern_data *ptrn_data,
                tad_recv_pkt *meta_pkt, size_t pkt_len, te_bool *no_report)
 {
+    te_bool         clean_bottom_layer = FALSE;
     unsigned int    unit;
     te_errno        rc;
 
@@ -1049,7 +1068,7 @@ tad_recv_match(csap_p csap, tad_recv_pattern_data *ptrn_data,
         tad_recv_pkt_cleanup_upper(csap, meta_pkt);
 
         rc = tad_recv_match_with_unit(csap, ptrn_data->units + unit,
-                                      meta_pkt);
+                                      meta_pkt, &clean_bottom_layer);
         switch (TE_RC_GET_ERROR(rc))
         {
             case 0: /* received data matches to this pattern unit */
@@ -1075,7 +1094,7 @@ tad_recv_match(csap_p csap, tad_recv_pattern_data *ptrn_data,
                        CSAP_LOG_ARGS(csap), unit, rc);
 
                 if (csap->state & CSAP_STATE_RECV_SEQ_MATCH)
-                    return rc;
+                    goto out;
 
                 continue;
 
@@ -1089,6 +1108,14 @@ tad_recv_match(csap_p csap, tad_recv_pattern_data *ptrn_data,
 
     } while (++unit < ptrn_data->n_units);
 
+out:
+    if (rc == TE_ETADNOTMATCH && clean_bottom_layer)
+    {
+        tad_pkt *p;
+
+        p = tad_pkts_first_pkt(&meta_pkt->layers[csap->depth - 1].pkts);
+        tad_pkt_cleanup(p);
+    }
     return rc;
 }
 