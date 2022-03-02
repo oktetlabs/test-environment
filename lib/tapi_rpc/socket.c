oktetlabs/test-environment

commit e400315548c5e9787ccc5c217cd15eeb519fcaa0

Bug 11721: rpc: do not check errno after check_port_is_free()
This RPC call may change errno to EAFNOSUPPORT if system does
not support IPv6. It is acceptable, so ignore errno changing
in rpc_check_port_is_free().

lib/tapi_rpc/socket.c
@@ -364,6 +364,12 @@ rpc_check_port_is_free(rcf_rpc_server *rpcs, uint16_t port)
 
     in.port = port;
 
+    /*
+     * Bug 11721: check_port_is_free() may change errno to EAFNOSUPPORT on
+     * systems without IPv6 support, so errno checking should be disabled.
+     */
+    rpcs->errno_change_check = 0;
+
     rcf_rpc_call(rpcs, "check_port_is_free", &in, &out);
     CHECK_RETVAL_VAR(check_port_is_free, out.retval,
                      (out.retval != TRUE && out.retval != FALSE), FALSE);

commit 74dadadb8b5098995031ab59ceca7c6cdea1e763

Bug 11721: rpc_check_port_is_free: restore previous errno_change_check value
errno_change_check field must be restored to the previous state in order
not to affect subsequent RPC calls.

Fixes: e400315548c5 ("Bug 11721: rpc: do not check errno after check_port_is_free()")

lib/tapi_rpc/socket.c
@@ -352,6 +352,7 @@ rpc_check_port_is_free(rcf_rpc_server *rpcs, uint16_t port)
 {
     tarpc_check_port_is_free_in  in;
     tarpc_check_port_is_free_out out;
+    te_bool errno_change_check_prev = rpcs->errno_change_check;
 
     memset(&in, 0, sizeof(in));
     memset(&out, 0, sizeof(out));
@@ -375,6 +376,7 @@ rpc_check_port_is_free(rcf_rpc_server *rpcs, uint16_t port)
                      (out.retval != TRUE && out.retval != FALSE), FALSE);
     TAPI_RPC_LOG(rpcs, check_port_is_free, "%d", "%d",
                  (int)port, out.retval);
+    rpcs->errno_change_check = errno_change_check_prev;
     TAPI_RPC_OUT(check_port_is_free, FALSE);
     return out.retval; /* no jumps! */
 }