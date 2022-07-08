6dc58a9e1508d6d5426715de807ff7941392738a
Artem V. Andreev <Artem.Andreev@oktetlabs.ru>
2022-07-08 18:11:46 +0300

    Task 12166: allow to use site-specific configs in selftests

--- suites/selftest/scripts/run.sh
+++ suites/selftest/scripts/run.sh
@@ -14,16 +14,55 @@ for keys in ${TE_TS_SSH_KEY_DIR}/*.id_rsa; do
     chmod 600 $keys;
 done
 
+TS_SITE=auto
 TS_OPTS=
+TS_CFG=
 for opt ; do
     case "${opt}" in
         --cfg=*)
-            TS_CFG=${opt#--cfg=};;
+            TS_CFG=${opt#--cfg=}
+            ;;
+        --site=*)
+            TS_SITE=${opt#--site=}
+            ;;
         *)
-            TS_OPTS+="${opt} ";;
+            TS_OPTS+="${opt} "
+            ;;
     esac
     shift 1
 done
+
+# FIXME: that gimli1 is explicitly listed below is a hack.
+# Technically it's a site-specific configuration, but
+# it is different from the others, so it behaves more like
+# a 'local' one.
+
+case "${TS_SITE}" in
+    none)
+        ;;
+    auto)
+        case "${TS_CFG}" in
+            "" | localhost | gimli1)
+                TS_SITE=none
+                ;;
+            *)
+                TS_SITE="$(hostname -d)"
+                echo "Site is auto-detected as ${TS_SITE}"
+                ;;
+        esac
+        ;;
+    *)
+        case "${TS_CFG}" in
+            "" | localhost | gimli1)
+                echo "Site-specific options do not make sense" \
+                     "with local configurations" >&2
+                exit 1
+                ;;
+            *)
+                ;;
+        esac
+        ;;
+esac
 
 test -z "${TS_CFG}" || TS_OPTS+="--opts=run/\"${TS_CFG}\" "
 
@@ -31,6 +70,14 @@ TS_CONF_DIRS=
 TS_CONF_DIRS+="\"${TS_TOPDIR}\"/conf:"
 
 TS_DEFAULT_OPTS=
+
+if test "$TS_SITE" != "none" ; then
+    if ! test -f "${TS_TOPDIR}/conf/site/${TS_SITE}"; then
+        echo "Unknown site '${TS_SITE}'" >&2
+        exit 1
+    fi
+    source "${TS_TOPDIR}/conf/site/${TS_SITE}"
+fi
 
 TS_DEFAULT_OPTS+="--conf-dirs=${TS_CONF_DIRS} "
 TS_DEFAULT_OPTS+="--build-parallel "
