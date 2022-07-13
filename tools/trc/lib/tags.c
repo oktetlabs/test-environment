oktetlabs/test-environment

commit 704d66f4b7e35b7517a0a42c5cb783cc07a3afbb

Task 11894: add TRC tags JSON string parsing from MI messages
Tester now generates uniform MI messages which contain properly formatted tag
info as JSON objects. So TRC log parser should be reworked to use those.

tools/trc/lib/tags.c
@@ -21,12 +21,14 @@
 #include <stdlib.h>
 #include <string.h>
 #endif
+#include <jansson.h>
 
 #include "te_queue.h"
 #include "logger_api.h"
 
 #include "trc_tags.h"
 #include "te_trc.h"
+#include "te_string.h"
 
 
 /**
@@ -52,5 +54,113 @@ trc_tags_str_to_list(tqh_strings *tags, char *tags_str)
             *space++ = '\0';
         rc = trc_add_tag(tags, tag);
     }
+    return rc;
+}
+
+/* See the description in trc_tags.h */
+te_errno
+trc_tags_json_to_list(tqh_strings *parsed_tags, char *json_buf)
+{
+    int       ret;
+    size_t    i;
+    te_errno  rc = 0;
+    json_t   *type = NULL;
+    json_t   *version = 0;
+
+    json_t *root = NULL;
+    json_t *tags = NULL;
+    json_t *tag = NULL;
+
+    json_error_t  err;
+    const char   *cur_tag = NULL;
+    const char   *cur_tag_value = NULL;
+    te_string     tag_combine = TE_STRING_INIT;
+
+    root = json_loadb(json_buf, strlen(json_buf), 0, &err);
+    if (root == NULL)
+    {
+        ERROR("Incorrect json_buf: %s", err.text);
+        return TE_EINVAL;
+    }
+
+    type = json_object_get(root, "type");
+    if (!json_is_string(type))
+    {
+        ERROR("Failed to get the \"type\" field from the trc_tags message");
+        rc = TE_EFAIL;
+        goto cleanup;
+    }
+    if (strcmp(json_string_value(type), "trc_tags") != 0)
+    {
+        ERROR("Wrong MI type");
+        free(type);
+        rc = TE_EINVAL;
+        goto cleanup;
+    }
+    free(type);
+
+    version = json_object_get(root, "version");
+    if (!json_is_integer(version))
+    {
+        ERROR("Failed to get the \"version\" field from the trc_tags message");
+        rc = TE_EFAIL;
+        goto cleanup;
+    }
+    if (json_integer_value(version) != 1)
+    {
+        ERROR("Wrong TRC tags version");
+        rc = TE_EINVAL;
+        goto cleanup;
+    }
+
+    tags = json_object_get(root, "tags");
+    if (!json_is_array(tags))
+    {
+        ERROR("Failed to get the \"tags\" field from the trc_tags message");
+        rc = TE_EFAIL;
+        goto cleanup;
+    }
+
+    json_array_foreach(tags, i, tag)
+    {
+        ret = json_unpack_ex(tag, &err, JSON_STRICT, "{s:s, s?s}",
+                             "name", &cur_tag, "value", &cur_tag_value);
+        if (ret != 0)
+        {
+            ERROR("Error unpacking trc_tags JSON log message: %s (line %d, "
+                  "column %d)",
+                  err.text, err.line, err.column);
+            json_decref(tag);
+            rc = TE_EFAIL;
+            goto cleanup;
+        }
+
+        if (cur_tag_value != NULL)
+            rc = te_string_append(&tag_combine, "%s:%s", cur_tag, cur_tag_value);
+        else
+            rc = te_string_append(&tag_combine, "%s", cur_tag);
+
+        json_decref(tag);
+
+        if (rc != 0)
+        {
+            ERROR("Failed to combine tag name and tag value");
+            goto cleanup;
+        }
+
+        rc = trc_add_tag(parsed_tags, te_string_value(&tag_combine));
+        te_string_reset(&tag_combine);
+        if (rc != 0)
+        {
+            ERROR("Failed to add TRC tag to tags list");
+            goto cleanup;
+        }
+    }
+
+cleanup:
+    json_decref(root);
+    json_decref(tags);
+    te_string_free(&tag_combine);
+
     return rc;
 }

tools/trc/lib/log_parse.c
@@ -920,8 +920,7 @@ trc_log_parse_start_element(void *user_data,
                     if (!entity_match &&
                         xmlStrcmp(attrs[0], CONST_CHAR2XML("entity")) == 0)
                     {
-                        if (xmlStrcmp(attrs[1],
-                                      CONST_CHAR2XML("Dispatcher")) != 0)
+                        if (xmlStrcmp(attrs[1], CONST_CHAR2XML("Tester")) != 0)
                             break;
                         entity_match = TRUE;
                     }
@@ -1254,7 +1253,8 @@ trc_log_parse_end_element(void *user_data, const xmlChar *name)
 
         case TRC_LOG_PARSE_TAGS:
             assert(strcmp(tag, "msg") == 0);
-            trc_tags_str_to_list(ctx->tags, ctx->str);
+            if (trc_tags_json_to_list(ctx->tags, ctx->str) != 0)
+                ERROR("TRC tags parse failure");
             free(ctx->str);
             ctx->str = NULL;
             ctx->state = TRC_LOG_PARSE_LOGS;

tools/trc/lib/meson.build
@@ -20,14 +20,14 @@ libtrc_diff_sources = files(
 libtrc_report = static_library(
     'libtrc_report',
     ['log_parse.c', 'report_html.c', common_sources],
-    dependencies: [dep_common],
+    dependencies: [dep_common, dependency('jansson')],
     c_args: c_args,
 )
 
 libtrc_diff = static_library(
     'libtrc_diff',
     [libtrc_diff_sources, common_sources],
-    dependencies : [dep_common],
+    dependencies : [dep_common, dependency('jansson')],
     c_args: c_args,
 )
 

tools/trc/lib/trc_tags.h
@@ -34,6 +34,16 @@ extern "C" {
  */
 extern te_errno trc_tags_str_to_list(tqh_strings *tags, char *tags_str);
 
+/**
+ * Parse JSON string with TRC tags and add them into the list.
+ *
+ * @param json_buf      JSON string with TRC tags.
+ * @param parsed_tags   List to add tags.
+ *
+ * @return Status code.
+ */
+extern te_errno trc_tags_json_to_list(tqh_strings *parsed_tags, char *json_buf);
+
 #ifdef __cplusplus
 } /* extern "C" */
 #endif

tools/trc/trc-log/te-trc-log
@@ -91,7 +91,7 @@ cat <<EOF >"$rgt_filter"
 <filters>
   <entity-filter>
     <exclude entity=""/>
-    <include entity="Dispatcher">
+    <include entity="Tester">
         <user name="TRC tags"/>
     </include>
     <include entity="Tester">

commit 542c167ceeaf985e5cc6410d7e677ec7931ebe6b

Task 11894: fix a regression introduced in 704d66f4b7e35b7517a0a42c5cb783cc07a3afbb
's?' specifier in json_unpack_ex does __not__ put a NULL in its argument
if a key is not found, so a location for an optional value should be manually
cleared beforehand.

Fixes: 704d66f4b7e35b7517a0a42c5cb783cc07a3afbb

tools/trc/lib/tags.c
@@ -123,6 +123,7 @@ trc_tags_json_to_list(tqh_strings *parsed_tags, char *json_buf)
 
     json_array_foreach(tags, i, tag)
     {
+        cur_tag_value = NULL;
         ret = json_unpack_ex(tag, &err, JSON_STRICT, "{s:s, s?s}",
                              "name", &cur_tag, "value", &cur_tag_value);
         if (ret != 0)