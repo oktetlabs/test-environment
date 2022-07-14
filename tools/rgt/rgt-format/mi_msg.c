oktetlabs/test-environment

commit 31fca45f688b40acb5cfc40d1ec5f3b09f508ebc

Task 11894: add processing of trc_tags MI messages by RGT
Report generator tool proces raw logs and write to text format. Now it can
parse trc_tags MI messages in JSON format and correctly print in text format.

tools/rgt/rgt-format/mi_msg.c
@@ -198,6 +198,25 @@ te_rgt_mi_test_end_clean(te_rgt_mi_test_end *data)
     }
 }
 
+/**
+ * Release memory allocated for storing data for MI message
+ * of type "trc_tags".
+ *
+ * @param tags      Pointer to te_rgt_mi_trc_tags structure.
+ */
+static void
+te_rgt_mi_trc_tags_clean(te_rgt_mi_trc_tags *data)
+{
+    te_rgt_mi_trc_tag_entry *tag;
+
+    TE_VEC_FOREACH(&data->tags, tag)
+    {
+        free(tag->name);
+        free(tag->value);
+    }
+    te_vec_free(&data->tags);
+}
+
 /* See description in mi_msg.h */
 void
 te_rgt_mi_clean(te_rgt_mi *mi)
@@ -212,6 +231,9 @@ te_rgt_mi_clean(te_rgt_mi *mi)
             break;
         case TE_RGT_MI_TYPE_TEST_END:
             te_rgt_mi_test_end_clean(&mi->data.test_end);
+            break;
+        case TE_RGT_MI_TYPE_TRC_TAGS:
+            te_rgt_mi_trc_tags_clean(&mi->data.trc_tags);
             break;
         default:;
     }
@@ -1088,6 +1110,82 @@ cleanup:
         te_rgt_mi_clean(mi);
 }
 
+/** Parse "trc_tags" MI message */
+static void
+te_rgt_parse_mi_trc_tags_message(te_rgt_mi *mi)
+{
+    int      ret;
+    size_t   i;
+    te_errno rc;
+
+    json_t *root;
+    json_t *tags = NULL;
+    json_t *tag = NULL;
+
+    json_error_t err;
+
+    te_rgt_mi_trc_tags *data;
+    const char         *tag_name;
+    const char         *tag_value;
+
+    mi->type = TE_RGT_MI_TYPE_TRC_TAGS;
+    data = &mi->data.trc_tags;
+
+    root = (json_t *)(mi->json_obj);
+
+    tags = json_object_get(root, "tags");
+    if (tags == NULL)
+    {
+        te_rgt_mi_parse_error(mi, TE_EINVAL,
+                              "Failed to get the \"tags\" field from the "
+                              "trc_tags message");
+        goto cleanup;
+    }
+
+    if (!check_json_type(mi, &tags, JSON_ARRAY, "tags"))
+        goto cleanup;
+
+    data->tags = TE_VEC_INIT(te_rgt_mi_trc_tag_entry);
+
+    if (tags != NULL)
+    {
+        json_array_foreach(tags, i, tag)
+        {
+            te_rgt_mi_trc_tag_entry tag_entry;
+
+            ret = json_unpack_ex(tag, &err, JSON_STRICT, "{s:s, s?s}",
+                                 "name", &tag_name, "value", &tag_value);
+            if (ret != 0)
+            {
+                te_rgt_mi_parse_error(mi, TE_EINVAL,
+                                      "Error unpacking trc_tags JSON log "
+                                      "message: %s (line %d, column %d)",
+                                      err.text, err.line, err.column);
+                goto cleanup;
+            }
+
+            tag_entry.name = strdup(tag_name);
+            tag_entry.value = (tag_value != NULL ? strdup(tag_value) : NULL);
+
+            rc = TE_VEC_APPEND(&data->tags, tag_entry);
+            if (rc != 0)
+            {
+                te_rgt_mi_parse_error(mi, TE_EINVAL,
+                                      "Error appending unpacked trc_tag: %s",
+                                      tag_name);
+                free(tag_entry.name);
+                free(tag_entry.value);
+                goto cleanup;
+            }
+        }
+    }
+
+cleanup:
+    json_decref(tags);
+    if (mi->rc != 0)
+        te_rgt_mi_clean(mi);
+}
+
 #endif /* HAVE_LIBJANSSON */
 
 /* See description in mi_msg.h */
@@ -1136,6 +1234,10 @@ te_rgt_parse_mi_message(const char *json_buf, size_t buf_len,
     else if (strcmp(type, "test_end") == 0)
     {
         te_rgt_parse_mi_test_end_message(mi);
+    }
+    else if (strcmp(type, "trc_tags") == 0)
+    {
+        te_rgt_parse_mi_trc_tags_message(mi);
     }
     else
     {

tools/rgt/rgt-format/mi_msg.h
@@ -29,6 +29,7 @@ extern "C" {
 
 #include "te_defs.h"
 #include "te_errno.h"
+#include "te_vector.h"
 
 /** Maximum length of error description from JSON parser */
 #define TE_RGT_MI_MAX_ERR_LEN 1024
@@ -164,11 +165,23 @@ typedef struct te_rgt_mi_test_end {
     size_t                 expected_num; /**< Number of expected results */
 } te_rgt_mi_test_end;
 
+/** Description of a TRC tag */
+typedef struct te_rgt_mi_trc_tag_entry {
+    char *name;   /**< Tag name */
+    char *value;  /**< Tag value */
+} te_rgt_mi_trc_tag_entry;
+
+/** Description of MI message of type "trc_tags" */
+typedef struct te_rgt_mi_trc_tags {
+    te_vec tags; /**< Vector of TRC tags */
+} te_rgt_mi_trc_tags;
+
 /** Types of MI message */
 typedef enum {
     TE_RGT_MI_TYPE_MEASUREMENT = 0,   /**< Measurement */
     TE_RGT_MI_TYPE_TEST_START,        /**< Package/Session/Test start */
     TE_RGT_MI_TYPE_TEST_END,          /**< Package/Session/Test end */
+    TE_RGT_MI_TYPE_TRC_TAGS,          /**< TRC tags */
     TE_RGT_MI_TYPE_UNKNOWN            /**< Unknown type */
 } te_rgt_mi_type;
 
@@ -191,6 +204,8 @@ typedef struct te_rgt_mi {
                                          message */
         te_rgt_mi_test_start test_start; /**< Data for test_start MI message */
         te_rgt_mi_test_end   test_end;   /**< Data for test_end MI message */
+
+        te_rgt_mi_trc_tags trc_tags; /**< Data for trc_tags MI message */
     } data; /**< Data obtained from JSON object */
 } te_rgt_mi;
 

tools/rgt/rgt-format/xml2text/xml2text.c
@@ -790,6 +790,31 @@ log_mi_test_end(gen_ctx_user_t *ctx, te_rgt_mi *mi)
 }
 
 /**
+ * Log MI trc tags message.
+ *
+ * @param ctx     Logging context.
+ * @param mi      Structure with data from parsed MI artifact.
+ */
+static void
+log_mi_trc_tags(gen_ctx_user_t *ctx, te_rgt_mi *mi)
+{
+    te_rgt_mi_trc_tags      *data = &mi->data.trc_tags;
+    te_rgt_mi_trc_tag_entry *tag;
+
+    if (te_vec_size(&data->tags) > 0)
+    {
+        fprintf_log(ctx, "TRC tags:");
+        TE_VEC_FOREACH(&data->tags, tag)
+        {
+            if (tag->value == NULL)
+                fprintf_log(ctx, "\n %s", tag->name);
+            else
+                fprintf_log(ctx, "\n %s:%s", tag->name, tag->value);
+        }
+    }
+}
+
+/**
  * Log MI artifact.
  *
  * @param ctx     Logging context.
@@ -903,6 +928,10 @@ log_mi_artifact(gen_ctx_user_t *ctx, te_rgt_mi *mi)
     else if (mi->type == TE_RGT_MI_TYPE_TEST_END)
     {
         log_mi_test_end(ctx, mi);
+    }
+    else if (mi->type == TE_RGT_MI_TYPE_TRC_TAGS)
+    {
+        log_mi_trc_tags(ctx, mi);
     }
 }
 

commit f0dc4235a91c60b5881cf1741a6d2e9b03fb3829

Task 11894: fix a regression introduced in 31fca45f688b40acb5cfc40d1ec5f3b09f508ebc
The same regression has been fixed in 542c167ceeaf985e5cc6410d7e677ec7931ebe6b.

Fixes: 31fca45f688b40acb5cfc40d1ec5f3b09f508ebc

tools/rgt/rgt-format/mi_msg.c
@@ -1153,6 +1153,7 @@ te_rgt_parse_mi_trc_tags_message(te_rgt_mi *mi)
         {
             te_rgt_mi_trc_tag_entry tag_entry;
 
+            tag_value = NULL;
             ret = json_unpack_ex(tag, &err, JSON_STRICT, "{s:s, s?s}",
                                  "name", &tag_name, "value", &tag_value);
             if (ret != 0)