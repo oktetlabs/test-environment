887809df83fa83a0fb4656d3ffb3bc3a4ed0d489
Artem V. Andreev <Artem.Andreev@oktetlabs.ru>
2022-07-19 20:08:18 +0300

    agents/unix: implement config node to check for loop device
    OL-Redmine-Id: 12196
    Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
    Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
    Reviewed-by: Daniil Byshenko <daniilbyshenko@oktetlabs.ru>

--- agents/unix/conf/base/conf_block.c
+++ agents/unix/conf/base/conf_block.c
@@ -17,6 +17,12 @@
 #include <stdarg.h>
 #endif
 
+#include <unistd.h>
+
+#if HAVE_LINUX_MAJOR_H
+#include <linux/major.h>
+#endif
+
 #include<string.h>
 
 #include "te_stdint.h"
@@ -64,8 +70,76 @@ block_dev_list(unsigned int gid, const char *oid,
     return 0;
 }
 
+#if HAVE_LINUX_MAJOR_H
+static te_errno
+check_block_loop(const char *block_name)
+{
+    te_errno rc = 0;
+    char buf[64];
+
+    rc = read_sys_value(buf, sizeof(buf), FALSE, "/sys/block/%s/dev",
+                        block_name);
+    if (rc != 0)
+    {
+        /* if we cannot read device numbers, assume it's not a loop device */
+        if (TE_RC_GET_ERROR(rc) == TE_ENOENT)
+            rc = TE_RC(TE_TA_UNIX, TE_ENOTBLK);
+    }
+    else
+    {
+        unsigned major, minor;
+
+        if (sscanf(buf, "%u:%u", &major, &minor) != 2)
+        {
+            ERROR("Invalid contents of /sys/block/%s/dev: %s", block_name,
+                  buf);
+            return TE_RC(TE_TA_UNIX, TE_EBADMSG);
+        }
+        UNUSED(minor);
+
+        if (major != LOOP_MAJOR)
+            rc = TE_RC(TE_TA_UNIX, TE_ENOTBLK);
+    }
+
+    return rc;
+}
+#endif
+
+static te_errno
+block_dev_loop_get(unsigned int gid, const char *oid, char *value,
+                   const char *block_name)
+{
+    te_errno rc = TE_RC(TE_TA_UNIX, TE_ENOTBLK);
+
+    if (!ta_block_is_mine(block_name, NULL))
+        return TE_RC(TE_TA_UNIX, TE_EPERM);
+
+#if HAVE_LINUX_MAJOR_H
+    rc  = check_block_loop(block_name);
+#else
+    UNUSED(block_name);
+#endif
+    UNUSED(gid);
+    UNUSED(oid);
+
+    if (TE_RC_GET_ERROR(rc) == TE_ENOTBLK)
+    {
+        strcpy(value, "0");
+        rc = 0;
+    }
+    else if (rc == 0)
+    {
+        strcpy(value, "1");
+    }
+
+    return rc;
+}
+
+RCF_PCH_CFG_NODE_RO(node_block_dev_loop, "loop", NULL, NULL,
+                    block_dev_loop_get);
+
 RCF_PCH_CFG_NODE_COLLECTION(node_block_dev, "block",
-                            NULL, NULL,
+                            &node_block_dev_loop, NULL,
                             NULL, NULL, block_dev_list, NULL);
 
 te_errno
