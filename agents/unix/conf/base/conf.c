oktetlabs/test-environment

commit eda7c985033cc21542ca8f2cedb8f2ea7afe4907

cleanup: remove DLPI usage
Corresponding code is dead and not used/tested for a long-long time.

agents/unix/conf/base/conf.c
@@ -67,10 +67,6 @@
 #include <dirent.h>
 #endif
 
-#ifdef HAVE_LIBDLPI
-#include <libdlpi.h>
-#endif
-
 #ifdef _NET_TIMESTAMPING_H
 #include <linux/net_tstamp.h>
 #endif
@@ -174,7 +170,6 @@ typedef struct pam_message const pam_message_t;
 #include "rcf_pch_ta_cfg.h"
 #include "logger_api.h"
 #include "unix_internal.h"
-#include "conf_dlpi.h"
 #include "conf_ovs.h"
 #include "conf_route.h"
 #include "conf_rule.h"
@@ -553,9 +548,6 @@ typedef struct mma_list_el {
 
 typedef struct ifs_list_el {
     char                ifname[IFNAMSIZ];
-#ifdef HAVE_LIBDLPI
-    dlpi_handle_t       fd;
-#endif
     struct mma_list_el *mcast_addresses;
     struct ifs_list_el *next;
 } ifs_list_el;
@@ -3294,7 +3286,7 @@ ifindex_get(unsigned int gid, const char *oid, char *value,
     return 0;
 }
 
-#if ((defined(USE_LIBNETCONF)) || (defined(HAVE_SYS_DLPI_H)))
+#if (defined(USE_LIBNETCONF)
 /*
  * Next functions are pulled out from iproute internals
  * to be accessible here and renamed.
@@ -3536,51 +3528,6 @@ iface_kind_get(unsigned int gid, const char *oid, char *value,
 #endif
 }
 
-#ifdef HAVE_LIBDLPI
-static te_errno
-mcast_link_addr_change_dlpi(dlpi_handle_t hnd, const char *addr, int op)
-{
-    uint8_t     mac_addr[ETHER_ADDR_LEN];
-    int         i;
-    char       *p;
-    te_errno    rc;
-
-    for (i = 0, p = addr; i < ETHER_ADDR_LEN; i++, p++)
-    {
-        unsigned tmp = strtoul(p, (char **)&p, 16);
-
-        if (tmp > UCHAR_MAX)
-            return TE_RC(TE_TA_UNIX, TE_EINVAL);
-        if (*p != ':' && (*p != '\0' || i < ETHER_ADDR_LEN - 1))
-            return TE_RC(TE_TA_UNIX, TE_EINVAL);
-        mac_addr[i] = tmp;
-    }
-    if (op == SIOCADDMULTI)
-    {
-        if ((rc = dlpi_enabmulti(hnd, mac_addr, ETHER_ADDR_LEN)) !=
-            DLPI_SUCCESS)
-        {
-            ERROR("dlpi_enabmulti() failed, rc = %x", rc);
-            return TE_EINVAL;
-        }
-    }
-    else if (op == SIOCDELMULTI)
-    {
-        if ((rc = dlpi_disabmulti(hnd, mac_addr, ETHER_ADDR_LEN)) !=
-            DLPI_SUCCESS)
-        {
-            ERROR("dlpi_disabmulti() failed, rc = %x", rc);
-            return TE_EINVAL;
-        }
-    }
-    else
-    {
-        ERROR("Invalid operation: %d", op);
-        return TE_EINVAL;
-    }
-    return 0;
-}
-#endif
 static te_errno
 mcast_link_addr_change_ioctl(const char *ifname, const char *addr, int op)
 {
@@ -3653,9 +3600,6 @@ mcast_link_addr_add(unsigned int gid, const char *oid,
         strcpy(p->ifname, ifname);
         p->mcast_addresses = NULL;
         p->next = interface_stream_list;
-#ifdef HAVE_LIBDLPI
-        dlpi_open(ifname, &p->fd, DLPI_NATIVE);
-#endif
         interface_stream_list = p;
     }
 
@@ -3666,10 +3610,6 @@ mcast_link_addr_add(unsigned int gid, const char *oid,
         q = (mma_list_el *)malloc(sizeof(mma_list_el));
         /* Against setting too long value for MAC address */
         te_strlcpy(q->value, addr, sizeof(q->value));
-#ifdef HAVE_LIBDLPI
-        /* Adding the address via DLPI */
-        rc = mcast_link_addr_change_dlpi(p->fd, addr, SIOCADDMULTI);
-#endif
         q->next = p->mcast_addresses;
         p->mcast_addresses = q;
     }
@@ -3711,18 +3651,11 @@ mcast_link_addr_del(unsigned int gid, const char *oid, const char *ifname,
     }
     if (strcmp(p->mcast_addresses->value, addr) == 0)
     {
-#ifdef HAVE_LIBDLPI
-        /* Deleting address via DLPI */
-        rc = mcast_link_addr_change_dlpi(p->fd, addr, SIOCDELMULTI);
-#endif
         q = p->mcast_addresses->next;
         free(p->mcast_addresses);
         p->mcast_addresses = q;
         if (q == NULL)
         {
-#ifdef HAVE_LIBDLPI
-            dlpi_close(p->fd);
-#endif
             if (p == interface_stream_list)
             {
                 interface_stream_list = p->next;
@@ -3751,10 +3684,6 @@ mcast_link_addr_del(unsigned int gid, const char *oid, const char *ifname,
         else
         {
             mma_list_el *tmp = q->next->next;
-#ifdef HAVE_LIBDLPI
-            /* Deleting address via DLPI */
-            rc = mcast_link_addr_change_dlpi(p->fd, addr, SIOCDELMULTI);
-#endif
             free(q->next);
             q->next = tmp;
         }
@@ -5371,30 +5300,6 @@ link_addr_get(unsigned int gid, const char *oid, char *value,
     else
         ptr = (const uint8_t *)my_ifr_hwaddr_data(req);
 
-#elif HAVE_SYS_DLPI_H
-    do {
-        size_t  len = sizeof(buf);
-
-        rc = ta_unix_conf_dlpi_phys_addr_get(ifname, buf, &len);
-        if (TE_RC_GET_ERROR(rc) == TE_ENOENT)
-        {
-            /* No link-layer or address */
-            break;
-        }
-        else if (rc != 0)
-        {
-            ERROR("Failed to get interface link-layer address using "
-                  "DLPI: %r", rc);
-            return TE_RC(TE_TA_UNIX, rc);
-        }
-        if (len != ETHER_ADDR_LEN)
-        {
-            ERROR("%s(): Unsupported link-layer address length %u",
-                  __FUNCTION__, (unsigned)len);
-            return TE_RC(TE_TA_UNIX, TE_ENOSYS);
-        }
-        ptr = (const uint8_t *)buf;
-    } while (0);
 #elif defined(__FreeBSD__)
 
     void           *ifconf_buf = NULL;
@@ -5483,14 +5388,6 @@ link_addr_set(unsigned int gid, const char *oid, const char *value,
     memcpy(my_ifr_hwaddr_data(req), link_addr, sizeof(link_addr));
 
     CFG_IOCTL(cfg_socket, SIOCSIFHWADDR, &req);
-#elif HAVE_SYS_DLPI_H
-    rc = ta_unix_conf_dlpi_phys_addr_set(ifname,
-                                         link_addr, sizeof(link_addr));
-    if (rc != 0)
-    {
-        ERROR("Failed to set interface link-layer address using "
-              "DLPI: %r", rc);
-    }
 #else
     ERROR("Set of link-layer address is not supported");
     rc = TE_RC(TE_TA_UNIX, TE_EOPNOTSUPP);
@@ -5593,26 +5490,6 @@ bcast_link_addr_get(unsigned int gid, const char *oid,
 #if defined(USE_LIBNETCONF)
     return iface_get_property_netconf(ifname, value,
                                       IF_PROP_BCAST_ADDR);
-#elif HAVE_SYS_DLPI_H
-    do {
-        size_t  len = sizeof(buf);
-
-        rc = ta_unix_conf_dlpi_phys_bcast_addr_get(ifname, buf, &len);
-        if (TE_RC_GET_ERROR(rc) == TE_ENOENT)
-        {
-            /* No link-layer or broadcast address */
-            break;
-        }
-        else if (rc != 0)
-        {
-            ERROR("Failed to get interface link-layer broadcast address "
-                  "using DLPI: %r", rc);
-            break;
-        }
-        link_addr_n2a((unsigned char *)buf, len, value, RCF_MAX_VAL);
-    } while (0);
-
-    return rc;
 #else
     return TE_RC(TE_TA_UNIX, TE_ENOENT);
 #endif

agents/unix/conf/meson.build
@@ -44,7 +44,6 @@ sources += files(
     'route/conf_route_socket.c',
     'rule/conf_rule.c',
     'util/conf_cmd_monitor.c',
-    'util/conf_dlpi.c',
     'util/conf_getmsg.c',
     'util/te_cmd_monitor.c',
     'vm/conf_vm.c',

agents/unix/conf/util/conf_dlpi.c
@@ -1,580 +1,0 @@
-/** @file
- * @brief Unix Test Agent
- *
- * Unix TA configuring support using DLPI (implementation).
- *
- * Copyright (C) 2004-2022 OKTET Labs Ltd. All rights reserved.
- */
-
-#define TE_LGR_USER     "Unix Conf DLPI"
-
-#include "te_config.h"
-#if HAVE_CONFIG_H
-#include <config.h>
-#endif
-
-#if HAVE_SYS_DLPI_H
-#include <sys/dlpi.h>
-
-#if HAVE_STROPTS_H
-#include <stropts.h>
-#else
-#error DLPI cannot be used without STREAMS
-#endif
-
-#include <stdio.h>
-#if HAVE_FCNTL_H
-#include <fcntl.h>
-#endif
-#if HAVE_ASSERT_H
-#include <assert.h>
-#endif
-
-#include "te_defs.h"
-#include "te_errno.h"
-#include "logger_api.h"
-
-
-#define  MAXADDRLEN      64
-#define  MAXSAPLEN       64
-
-#define  ETHER_ADDR_LEN  6
-
-/**
- * Open STREAM.
- *
- * @param ifname        Interface name
- *
- * @return Status code.
- */
-static te_errno
-dlpi_open(const char *ifname, int *fd)
-{
-    const char *fpath[] = {
-        "/dev/%s", /* Some old Solaris version worked with this path */
-        "/dev/net/%s", /* Solaris 5.11 requires /dev/net prefix */
-    };
-    size_t      i;
-    char        path[32];
-    te_errno    rc = TE_EOPNOTSUPP;
-
-    assert(ifname != NULL);
-    assert(fd != NULL);
-
-    for (i = 0; i < sizeof(fpath) / sizeof(fpath[0]); i++)
-    {
-        int len;
-
-        len = snprintf(path, sizeof(path), fpath[i], ifname);
-        if (len >= (int)sizeof(path))
-        {
-            ERROR("%s:%u: Too small buffer", __FUNCTION__, __LINE__);
-            return TE_RC(TE_TA_UNIX, TE_ESMALLBUF);
-        }
-
-        *fd = open(path, O_RDWR);
-        if (*fd >= 0)
-        {
-            VERB("DLPI: Opened '%s'", path);
-            return 0;
-        }
-
-        rc = te_rc_os2te(errno);
-        if (rc != TE_ENOENT)
-        {
-            ERROR("%s(): Failed to open device '%s': %r",
-                  __FUNCTION__, path, rc);
-        }
-    }
-
-    return TE_RC(TE_TA_UNIX, rc);
-}
-
-/* See the description in conf_dlpi.h */
-te_errno
-ta_unix_conf_dlpi_phys_addr_get(const char *name, void *addr,
-                                size_t *addrlen)
-{
-    te_errno                rc;
-    int                     res;
-    int                     fd = -1;
-    size_t                  size;
-    union DL_primitives    *prim = NULL;
-    dl_info_req_t           info_req = { DL_INFO_REQ };
-    dl_info_ack_t          *info_ack;
-    dl_phys_addr_req_t      pa_req = { DL_PHYS_ADDR_REQ,
-                                       DL_CURR_PHYS_ADDR };
-    dl_phys_addr_ack_t     *pa_ack;
-    dl_error_ack_t         *err;
-    struct strbuf           msg;
-    int                     msg_flags = 0;
-
-
-    if (name == NULL || addrlen == NULL)
-    {
-        ERROR("%s(): Invalid arguments", __FUNCTION__);
-        return TE_RC(TE_TA_UNIX, TE_EFAULT);
-    }
-
-    rc = dlpi_open(name, &fd);
-    if (rc != 0)
-    {
-        /** Zero ethernet address emulation for 'lo0' */
-        if ((strcmp(name, "lo0") == 0) &&
-            (TE_RC_GET_ERROR(rc) == TE_ENOENT))
-        {
-            memset(addr, 0, (*addrlen = ETHER_ADDR_LEN));
-            return 0;
-        }
-
-        return rc;
-    }
-
-    size = DL_INFO_ACK_SIZE;
-    size += sizeof(union DL_qos_types) + sizeof(union DL_qos_types);
-    size += MAXADDRLEN + MAXSAPLEN;
-    size += MAXADDRLEN;
-    size = MAX(size, DL_PHYS_ADDR_ACK_SIZE + MAXADDRLEN);
-    size = MAX(size, DL_ERROR_ACK_SIZE);
-
-    if ((prim = malloc(size)) == NULL)
-    {
-        rc = TE_ENOMEM;
-        goto exit;
-    }
-
-    /* Send DL_INFO_REQ */
-    msg.buf = (char *)&info_req;
-    msg.len = sizeof(info_req);
-    if (putmsg(fd, &msg, NULL, RS_HIPRI) < 0)
-    {
-        rc = te_rc_os2te(errno);
-        assert(rc != 0);
-        ERROR("%s(): putmsg() failed: %r", __FUNCTION__, rc);
-        goto exit;
-    }
-
-    /* Receive reply */
-    msg.buf = (char *)prim;
-    msg.len = 0;
-    msg.maxlen = size;
-    if ((res = getmsg(fd, &msg, NULL, &msg_flags)) < 0)
-    {
-        rc = te_rc_os2te(errno);
-        assert(rc != 0);
-        ERROR("%s(): getmsg() failed: %r", __FUNCTION__, rc);
-        goto exit;
-    }
-    if (res & (MORECTL | MOREDATA))
-    {
-        ERROR("%s(): Invalid reply for DL_INFO_REQ", __FUNCTION__);
-        rc = TE_EPROTO;
-        goto exit;
-    }
-
-    if (msg.len < (int)DL_INFO_ACK_SIZE)
-    {
-        ERROR("%s(): Reply for DL_INFO_REQ is too short", __FUNCTION__);
-        rc = TE_EBADMSG;
-        goto exit;
-    }
-
-    info_ack = &prim->info_ack;
-    if (info_ack->dl_primitive != DL_INFO_ACK ||
-        info_ack->dl_version != DL_VERSION_2)
-    {
-        ERROR("%s(): Unexpected reply for DL_INFO_REQ", __FUNCTION__);
-        rc = TE_EPROTO;
-        goto exit;
-    }
-
-    if (info_ack->dl_addr_length == 0)
-    {
-        ERROR("%s(): Zero address length in DL_INFO_ACK", __FUNCTION__);
-        rc = TE_EPROTO;
-        goto exit;
-    }
-    if (info_ack->dl_addr_length <= abs(info_ack->dl_sap_length))
-    {
-        ERROR("%s(): Invalid address length in DL_INFO_ACK", __FUNCTION__);
-        rc = TE_EPROTO;
-        goto exit;
-    }
-    if (*addrlen < info_ack->dl_addr_length - abs(info_ack->dl_sap_length))
-    {
-        ERROR("%s(): Too small buffer for physical address",
-              __FUNCTION__);
-        rc = TE_ESMALLBUF;
-        goto exit;
-    }
-    *addrlen = info_ack->dl_addr_length - abs(info_ack->dl_sap_length);
-
-    if (addr != NULL)
-    {
-        if (info_ack->dl_addr_offset != 0)
-        {
-            /* Address is present in DL_INFO_ACK */
-            memcpy(addr, (char *)prim + info_ack->dl_addr_offset,
-                   *addrlen);
-        }
-        else
-        {
-            /* Send DL_PHYS_ADDR_REQ */
-            msg.buf = (char *)&pa_req;
-            msg.len = sizeof(pa_req);
-            if (putmsg(fd, &msg, NULL, 0) < 0)
-            {
-                rc = te_rc_os2te(errno);
-                assert(rc != 0);
-                goto exit;
-            }
-
-            /* Receive reply */
-            msg.buf = (char *)prim;
-            msg.len = 0;
-            msg.maxlen = size;
-            if ((res = getmsg(fd, &msg, NULL, &msg_flags)) < 0)
-            {
-                rc = te_rc_os2te(errno);
-                assert(rc != 0);
-                goto exit;
-            }
-            if (res & (MORECTL | MOREDATA))
-            {
-                rc = TE_EPROTO;
-                goto exit;
-            }
-            size = msg.len;
-
-            if (size < sizeof(t_uscalar_t))
-            {
-                rc = TE_EBADMSG;
-                goto exit;
-            }
-
-            switch (prim->dl_primitive)
-            {
-                case DL_PHYS_ADDR_ACK:
-                    if (size < DL_PHYS_ADDR_ACK_SIZE)
-                    {
-                        rc = TE_EBADMSG;
-                        break;
-                    }
-
-                    pa_ack = &prim->physaddr_ack;
-                    if (pa_ack->dl_addr_offset != 0)
-                    {
-                        if (pa_ack->dl_addr_length != *addrlen)
-                        {
-                            ERROR("%s(): Unexpected length (%u vs %u) of "
-                                  "address in DL_PHYS_ADDR_ACK",
-                                  __FUNCTION__,
-                                  (unsigned)pa_ack->dl_addr_length,
-                                  (unsigned)*addrlen);
-                            rc = TE_EPROTO;
-                            break;
-                        }
-                        memcpy(addr,
-                               (char *)prim + pa_ack->dl_addr_offset,
-                               *addrlen);
-                    }
-                    else
-                    {
-                        ERROR("%s(): No address in DL_PHYS_ADDR_ACK",
-                              __FUNCTION__);
-                        rc = TE_ENOENT;
-                    }
-                    break;
-
-                case DL_ERROR_ACK:
-                    if (size < DL_ERROR_ACK_SIZE)
-                    {
-                        rc = TE_EBADMSG;
-                        break;
-                    }
-
-                    err = &prim->error_ack;
-                    switch (err->dl_errno)
-                    {
-                        case DL_SYSERR:
-                            rc = te_rc_os2te(err->dl_unix_errno);
-                            break;
-
-                        default:
-                            rc = TE_EPROTO;
-                            break;
-                    }
-                    break;
-
-                default:
-                    rc = TE_EBADMSG;
-            }
-        }
-    }
-
-exit:
-    free(prim);
-    close(fd);
-
-    return TE_RC(TE_TA_UNIX, rc);
-}
-
-/* See the description in conf_dlpi.h */
-te_errno
-ta_unix_conf_dlpi_phys_addr_set(const char *name, const void *addr,
-                                size_t addrlen)
-{
-    te_errno                rc;
-    int                     res;
-    int                     fd = -1;
-    size_t                  size;
-    union DL_primitives    *prim = NULL;
-    dl_set_phys_addr_req_t *req;
-    dl_error_ack_t         *err;
-    struct strbuf           msg;
-    int                     msg_flags = 0;
-
-
-    if (name == NULL || addrlen == NULL)
-    {
-        ERROR("%s(): Invalid arguments", __FUNCTION__);
-        return TE_RC(TE_TA_UNIX, TE_EFAULT);
-    }
-
-    rc = dlpi_open(name, &fd);
-    if (rc != 0)
-        return rc;
-
-    size = DL_SET_PHYS_ADDR_REQ_SIZE + addrlen;
-    size = MAX(size, DL_OK_ACK_SIZE);
-    size = MAX(size, DL_ERROR_ACK_SIZE);
-
-    if ((prim = malloc(size)) == NULL)
-    {
-        rc = TE_ENOMEM;
-        goto exit;
-    }
-
-    /* Prepare DL_SET_PHYS_ADDR_REQ */
-    req = &prim->set_physaddr_req;
-    req->dl_primitive = DL_SET_PHYS_ADDR_REQ;
-    req->dl_addr_length = addrlen;
-    req->dl_addr_offset = DL_SET_PHYS_ADDR_REQ_SIZE;
-    memcpy((char *)prim + DL_SET_PHYS_ADDR_REQ_SIZE, addr, addrlen);
-
-    /* Send DL_SET_PHYS_ADDR_REQ */
-    msg.buf = (char *)prim;
-    msg.len = DL_SET_PHYS_ADDR_REQ_SIZE + addrlen;
-    if (putmsg(fd, &msg, NULL, 0) < 0)
-    {
-        rc = te_rc_os2te(errno);
-        assert(rc != 0);
-        ERROR("%s(): putmsg() failed: %r", __FUNCTION__, rc);
-        goto exit;
-    }
-
-    /* Receive reply */
-    msg.buf = (char *)prim;
-    msg.len = 0;
-    msg.maxlen = size;
-    if ((res = getmsg(fd, &msg, NULL, &msg_flags)) < 0)
-    {
-        rc = te_rc_os2te(errno);
-        assert(rc != 0);
-        ERROR("%s(): getmsg() failed: %r", __FUNCTION__, rc);
-        goto exit;
-    }
-    if (res & (MORECTL | MOREDATA))
-    {
-        ERROR("%s(): Invalid reply for DL_INFO_REQ", __FUNCTION__);
-        rc = TE_EPROTO;
-        goto exit;
-    }
-
-    if (size < sizeof(t_uscalar_t))
-    {
-        rc = TE_EBADMSG;
-        goto exit;
-    }
-
-    switch (prim->dl_primitive)
-    {
-        case DL_OK_ACK:
-            if (size < DL_OK_ACK_SIZE)
-            {
-                rc = TE_EBADMSG;
-                break;
-            }
-            break;
-
-        case DL_ERROR_ACK:
-            if (size < DL_ERROR_ACK_SIZE)
-            {
-                rc = TE_EBADMSG;
-                break;
-            }
-
-            err = &prim->error_ack;
-            switch (err->dl_errno)
-            {
-                case DL_BADADDR:
-                     rc = TE_EINVAL;
-                     break;
-
-                case DL_NOTSUPPORTED:
-                     rc = TE_EOPNOTSUPP;
-                     break;
-
-                case DL_SYSERR:
-                    rc = te_rc_os2te(err->dl_unix_errno);
-                    break;
-
-                default:
-                    rc = TE_EPROTO;
-                    break;
-            }
-            break;
-
-        default:
-            rc = TE_EBADMSG;
-    }
-
-exit:
-    free(prim);
-    close(fd);
-
-    return TE_RC(TE_TA_UNIX, rc);
-}
-
-
-/* See the description in conf_dlpi.h */
-te_errno
-ta_unix_conf_dlpi_phys_bcast_addr_get(const char *name, void *addr,
-                                      size_t *addrlen)
-{
-    te_errno                rc;
-    int                     res;
-    int                     fd = -1;
-    size_t                  size;
-    union DL_primitives    *prim = NULL;
-    dl_info_req_t           info_req = { DL_INFO_REQ };
-    dl_info_ack_t          *info_ack;
-    struct strbuf           msg;
-    int                     msg_flags = 0;
-
-
-    if (name == NULL || addrlen == NULL)
-    {
-        ERROR("%s(): Invalid arguments", __FUNCTION__);
-        return TE_RC(TE_TA_UNIX, TE_EFAULT);
-    }
-
-    rc = dlpi_open(name, &fd);
-    if (rc != 0)
-    {
-        /** Zero ethernet broadcast address emulation for 'lo0' */
-        if ((strcmp(name, "lo0") == 0) &&
-            (TE_RC_GET_ERROR(rc) == TE_ENOENT))
-        {
-            memset(addr, 0, (*addrlen = ETHER_ADDR_LEN));
-            return 0;
-        }
-
-        return rc;
-    }
-
-    size = DL_INFO_ACK_SIZE;
-    size += sizeof(union DL_qos_types) + sizeof(union DL_qos_types);
-    size += MAXADDRLEN + MAXSAPLEN;
-    size += MAXADDRLEN;
-
-    if ((prim = malloc(size)) == NULL)
-    {
-        rc = TE_ENOMEM;
-        goto exit;
-    }
-
-    /* Send DL_INFO_REQ */
-    msg.buf = (char *)&info_req;
-    msg.len = sizeof(info_req);
-    if (putmsg(fd, &msg, NULL, RS_HIPRI) < 0)
-    {
-        rc = te_rc_os2te(errno);
-        assert(rc != 0);
-        ERROR("%s(): putmsg() failed: %r", __FUNCTION__, rc);
-        goto exit;
-    }
-
-    /* Receive reply */
-    msg.buf = (char *)prim;
-    msg.len = 0;
-    msg.maxlen = size;
-    if ((res = getmsg(fd, &msg, NULL, &msg_flags)) < 0)
-    {
-        rc = te_rc_os2te(errno);
-        assert(rc != 0);
-        ERROR("%s(): getmsg() failed: %r", __FUNCTION__, rc);
-        goto exit;
-    }
-    if (res & (MORECTL | MOREDATA))
-    {
-        ERROR("%s(): Invalid reply for DL_INFO_REQ", __FUNCTION__);
-        rc = TE_EPROTO;
-        goto exit;
-    }
-
-    if (msg.len < (int)DL_INFO_ACK_SIZE)
-    {
-        ERROR("%s(): Reply for DL_INFO_REQ is too short", __FUNCTION__);
-        rc = TE_EBADMSG;
-        goto exit;
-    }
-
-    info_ack = &prim->info_ack;
-    if (info_ack->dl_primitive != DL_INFO_ACK ||
-        info_ack->dl_version != DL_VERSION_2)
-    {
-        ERROR("%s(): Unexpected reply for DL_INFO_REQ", __FUNCTION__);
-        rc = TE_EPROTO;
-        goto exit;
-    }
-
-    if (info_ack->dl_brdcst_addr_length == 0)
-    {
-        VERB("%s(): Zero broadcat address length in DL_INFO_ACK",
-             __FUNCTION__);
-
-        /*
-         * From man page of DL_INFO_ACK:
-         * When the Stream is unattached, or when the PPA does not support
-         * broadcast, 'dl_brdcst_addr_length' will be set to zero (0).
-         */
-        memset(addr, 0, (*addrlen = ETHER_ADDR_LEN));
-        goto exit;
-    }
-    *addrlen = info_ack->dl_brdcst_addr_length;
-
-    if (addr != NULL)
-    {
-        if (info_ack->dl_brdcst_addr_offset != 0)
-        {
-            /* Address is present in DL_INFO_ACK */
-            memcpy(addr, (char *)prim + info_ack->dl_brdcst_addr_offset,
-                   *addrlen);
-        }
-        else
-        {
-            ERROR("%s(): Physical broadcast address does not present "
-                  "in DL_INFO_ACK", __FUNCTION__);
-            rc = TE_EPROTO;
-        }
-    }
-
-exit:
-    free(prim);
-    close(fd);
-
-    return TE_RC(TE_TA_UNIX, rc);
-}
-
-#endif /* HAVE_SYS_DLPI_H */

agents/unix/conf/util/conf_dlpi.h
@@ -1,55 +1,0 @@
-/** @file
- * @brief Unix Test Agent
- *
- * Unix TA configuring support using DLPI (definitions).
- *
- * Copyright (C) 2004-2022 OKTET Labs Ltd. All rights reserved.
- */
-
-#ifndef __TE_TA_UNIX_CONF_DLPI_H__
-#define __TE_TA_UNIX_CONF_DLPI_H__
-
-#if HAVE_SYS_TYPES_H
-#include <sys/types.h>
-#endif
-
-/**
- * Get physical address using DLPI.
- *
- * @param name          Interface name
- * @param addr          Buffer for address
- * @param addrlen       Buffer size (in), address length (out)
- *
- * @return Status code.
- */
-extern te_errno ta_unix_conf_dlpi_phys_addr_get(const char *name,
-                                                void       *addr,
-                                                size_t     *addrlen);
-
-/**
- * Set physical address using DLPI.
- *
- * @param name          Interface name
- * @param addr          Address
- * @param addrlen       Address length
- *
- * @return Status code.
- */
-extern te_errno ta_unix_conf_dlpi_phys_addr_set(const char *name,
-                                                void       *addr,
-                                                size_t      addrlen);
-
-/**
- * Get physical broadcast address using DLPI.
- *
- * @param name          Interface name
- * @param addr          Buffer for address
- * @param addrlen       Buffer size (in), address length (out)
- *
- * @return Status code.
- */
-extern te_errno ta_unix_conf_dlpi_phys_bcast_addr_get(const char *name,
-                                                      void       *addr,
-                                                      size_t     *addrlen);
-
-#endif /* !__TE_TA_UNIX_CONF_DLPI_H__ */

agents/unix/meson.build
@@ -140,19 +140,6 @@ if not cc.has_function('sendfile', args: te_cflags)
 endif
 
 #
-# libdlpi is provided on Solaris
-#
-dep_dlpi = cc.find_library('dlpi', required: false)
-if dep_dlpi.found() and cc.has_function('dlpi_open', args: te_cflags,
-                                        dependencies: dep_dlpi)
-    deps += [ dep_dlpi ]
-    h = 'libdlpi.h'
-    if cc.has_header(h)
-        c_args += [ '-DHAVE_' + h.to_upper().underscorify() ]
-    endif
-endif
-
-#
 # Dynamic linker functions are provided by:
 #  - -ldl in Linux;
 #  - -lc in FreeBSD;

include/meson.build
@@ -80,7 +80,6 @@ check_headers = [
     'strings.h',
     'stropts.h',
     'sys/cdefs.h',
-    'sys/dlpi.h',
     'sys/errno.h',
     'sys/ethernet.h',
     'sys/filio.h',

include/te_config_internal.h.meson
@@ -225,9 +225,6 @@
 /* Define to 1 if you have the <sys/cdefs.h> header file. */
 #mesondefine HAVE_SYS_CDEFS_H
 
-/* Define to 1 if you have the <sys/dlpi.h> header file. */
-#mesondefine HAVE_SYS_DLPI_H
-
 /* Define to 1 if you have the <sys/errno.h> header file. */
 #mesondefine HAVE_SYS_ERRNO_H
 

include/te_errno.h
@@ -347,7 +347,6 @@ typedef enum {
                              via RPC */
     TE_ISCSI_TARGET,    /**< Error related to iSCSI target */
     TE_TAD_PF_PACKET,   /**< TAD Ethernet over PF_PACKET sockets */
-    TE_TAD_DLPI,        /**< TAD DLPI support */
     TE_TOOL_EXT,        /**< External tools */
     TE_TAD_BPF,         /**< TAD Ethernet using BPF */
     TE_TRC,             /**< Testing Results Comparator library */
@@ -437,7 +436,6 @@ te_rc_mod2str(te_errno err)
         MOD2STR(TAD_PKT);
         MOD2STR(TAD_BPS);
         MOD2STR(TAD_PF_PACKET);
-        MOD2STR(TAD_DLPI);
         MOD2STR(TAD_BPF);
         MOD2STR(TARPC);
         MOD2STR(LOGGER);

commit 64a1863c502ea7b825addd42c6bff243890b2daa

cleanup: agent/unix: fix broken build
Fixes: eda7c985033c ("cleanup: remove DLPI usage")

agents/unix/conf/base/conf.c
@@ -3286,7 +3286,7 @@ ifindex_get(unsigned int gid, const char *oid, char *value,
     return 0;
 }
 
-#if (defined(USE_LIBNETCONF)
+#if (defined(USE_LIBNETCONF))
 /*
  * Next functions are pulled out from iproute internals
  * to be accessible here and renamed.