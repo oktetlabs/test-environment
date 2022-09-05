c02f8ac0ebbc7c0c20727048a30171dd01889738
Artem V. Andreev <Artem.Andreev@oktetlabs.ru>
2022-09-05 19:19:12 +0300

    suites/selftest: add selftest for loop block dev management
    OL-Redmine-Id: 12459
    Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
    Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

---
+++ suites/selftest/ts/cs/loop.c
@@ -0,0 +1,53 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (C) 2022 OKTET Labs Ltd. All rights reserved. */
+/** @file
+ * @brief Testing Block Device Configuration TAPI
+ *
+ * Testing Block Device Configuration TAPI.
+ */
+
+/** @page cs-loop Testing Loop Block Device Configuration TAPI
+ *
+ * @objective Check that Loop Block Device Configuration TAPI works properly.
+ *
+ * @par Scenario:
+ *
+ */
+
+#define TE_TEST_NAME "cs/loop"
+
+#ifndef TEST_START_VARS
+#define TEST_START_VARS TEST_START_ENV_VARS
+#endif
+
+#ifndef TEST_START_SPECIFIC
+#define TEST_START_SPECIFIC TEST_START_ENV
+#endif
+
+#ifndef TEST_END_SPECIFIC
+#define TEST_END_SPECIFIC TEST_END_ENV
+#endif
+
+#include "te_config.h"
+#include "tapi_cfg_block.h"
+#include "tapi_test.h"
+#include "tapi_env.h"
+
+int
+main(int argc, char **argv)
+{
+    rcf_rpc_server *pco_iut = NULL;
+
+    TEST_START;
+
+    TEST_GET_PCO(pco_iut);
+
+    TEST_STEP("Initialize loop devices");
+    CHECK_RC(tapi_cfg_block_initialize_loop(pco_iut->ta));
+
+    TEST_SUCCESS;
+
+cleanup:
+
+    TEST_END;
+}
