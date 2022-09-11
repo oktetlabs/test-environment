1904b4152256319bcdba1e0c7c9bb62dad440035
Boleslav Stankevich <Boleslav.Stankevich@oktetlabs.ru>
2022-09-11 14:45:36 +0300

    engine/builder: refactor creating TCE workspace directory
    The RCF controller will save the TCE information fetched from TA agents
    within the TCE workspace directory.
    
    Further commits will use this text to create TCE workspace before the RCF
    controller is started.
    
    OL-Redmine-Id: 11766
    Signed-off-by: Boleslav Stankevich <boleslav.stankevich@oktetlabs.ru>
    Acked-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
    Acked-by: Artem Andreev <artem.andreev@oktetlabs.ru>

--- engine/builder/te_tce_functions.sh
+++ engine/builder/te_tce_functions.sh
@@ -6,6 +6,11 @@
 # Internal functions for TCE Processing
 # Do not use it directly!
 
+. "${TE_BUILD}/builder.conf.processed"
+. "${SCRIPT_DIR}/te_functions"
+
+TCE_WS="${TE_BUILD}/tce_workspace"
+
 #######################################
 # Die with a message.
 # Arguments:
@@ -18,4 +23,19 @@
 function tce_die() {
     echo "ERROR: TCE: $*" >&2
     exit 1
+}
+
+#######################################
+# Provide directory to process TCE.
+# Globals:
+#   TCE_WS
+# Arguments:
+#   None
+# Returns:
+#   0 if succeeds, non-zero on error.
+#######################################
+function tce_clean_workspace() {
+    [[ -d "${TCE_WS}" ]] \
+        && rm --interactive=never -r "${TCE_WS}" 2> /dev/null
+    mkdir "${TCE_WS}" 2> /dev/null
 }
