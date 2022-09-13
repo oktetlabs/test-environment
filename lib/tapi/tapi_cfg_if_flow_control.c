2dbcf284ef96a8f3cefeaf31758c6a030472f55b
Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
2022-09-13 16:41:38 +0300

    lib/tapi: support interface flow control configuration
    Make it simpler to get and set it from tests.
    
    Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
    Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

---
+++ lib/tapi/tapi_cfg_if_flow_control.c
@@ -0,0 +1,168 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (C) 2022 OKTET Labs Ltd. All rights reserved. */
+/** @file
+ * @brief Basic Configuration Model TAPI
+ *
+ * Implementation of test API for Network Interface flow control
+ * configuration (doc/cm/cm_base.xml).
+ */
+
+#define TE_LGR_USER "TAPI CFG IF FC"
+
+#include "te_config.h"
+#include "te_defs.h"
+#include "conf_api.h"
+#include "tapi_cfg_if_flow_control.h"
+
+#define COMMON_FMT "/agent:%s/interface:%s/flow_control:"
+
+/* See description in the tapi_cfg_if_flow_control.h */
+te_errno
+tapi_cfg_if_fc_autoneg_get(const char *ta, const char *ifname,
+                           int *autoneg)
+{
+    cfg_val_type type = CVT_INTEGER;
+
+    return cfg_get_instance_fmt(&type, autoneg,
+                                COMMON_FMT "/autoneg:",
+                                ta, ifname);
+}
+
+/* See description in the tapi_cfg_if_flow_control.h */
+te_errno
+tapi_cfg_if_fc_autoneg_set(const char *ta, const char *ifname,
+                           int autoneg)
+{
+    return cfg_set_instance_fmt(CFG_VAL(INTEGER, autoneg),
+                                COMMON_FMT "/autoneg:",
+                                ta, ifname);
+}
+
+/* See description in the tapi_cfg_if_flow_control.h */
+te_errno
+tapi_cfg_if_fc_autoneg_set_local(const char *ta,
+                                 const char *ifname,
+                                 int autoneg)
+{
+    return cfg_set_instance_local_fmt(CFG_VAL(INTEGER, autoneg),
+                                      COMMON_FMT "/autoneg:",
+                                      ta, ifname);
+}
+
+/* See description in the tapi_cfg_if_flow_control.h */
+te_errno
+tapi_cfg_if_fc_rx_get(const char *ta, const char *ifname, int *rx)
+{
+    cfg_val_type type = CVT_INTEGER;
+
+    return cfg_get_instance_fmt(&type, rx,
+                                COMMON_FMT "/rx:",
+                                ta, ifname);
+}
+
+/* See description in the tapi_cfg_if_flow_control.h */
+te_errno
+tapi_cfg_if_fc_rx_set(const char *ta, const char *ifname, int rx)
+{
+    return cfg_set_instance_fmt(CFG_VAL(INTEGER, rx),
+                                COMMON_FMT "/rx:",
+                                ta, ifname);
+}
+
+/* See description in the tapi_cfg_if_flow_control.h */
+te_errno
+tapi_cfg_if_fc_rx_set_local(const char *ta, const char *ifname,
+                            int rx)
+{
+    return cfg_set_instance_local_fmt(CFG_VAL(INTEGER, rx),
+                                      COMMON_FMT "/rx:",
+                                      ta, ifname);
+}
+
+/* See description in the tapi_cfg_if_flow_control.h */
+te_errno
+tapi_cfg_if_fc_tx_get(const char *ta, const char *ifname, int *tx)
+{
+    cfg_val_type type = CVT_INTEGER;
+
+    return cfg_get_instance_fmt(&type, tx,
+                                COMMON_FMT "/tx:",
+                                ta, ifname);
+}
+
+/* See description in the tapi_cfg_if_flow_control.h */
+te_errno
+tapi_cfg_if_fc_tx_set(const char *ta, const char *ifname, int tx)
+{
+    return cfg_set_instance_fmt(CFG_VAL(INTEGER, tx),
+                                COMMON_FMT "/tx:",
+                                ta, ifname);
+}
+
+/* See description in the tapi_cfg_if_flow_control.h */
+te_errno
+tapi_cfg_if_fc_tx_set_local(const char *ta, const char *ifname,
+                            int tx)
+{
+    return cfg_set_instance_local_fmt(CFG_VAL(INTEGER, tx),
+                                      COMMON_FMT "/tx:",
+                                      ta, ifname);
+}
+
+/* See description in the tapi_cfg_if_flow_control.h */
+te_errno
+tapi_cfg_if_fc_commit(const char *ta, const char *if_name)
+{
+    return cfg_commit_fmt(COMMON_FMT, ta, if_name);
+}
+
+/* See description in the tapi_cfg_if_flow_control.h */
+te_errno
+tapi_cfg_if_fc_get(const char *ta, const char *ifname,
+                   tapi_cfg_if_fc *params)
+{
+    te_errno rc;
+
+    rc = tapi_cfg_if_fc_autoneg_get(ta, ifname,
+                                    &params->autoneg);
+    if (rc != 0)
+        return rc;
+
+    rc = tapi_cfg_if_fc_rx_get(ta, ifname, &params->rx);
+    if (rc != 0)
+        return rc;
+
+    return tapi_cfg_if_fc_tx_get(ta, ifname, &params->tx);
+}
+
+/* See description in the tapi_cfg_if_flow_control.h */
+te_errno
+tapi_cfg_if_fc_set(const char *ta, const char *ifname,
+                   tapi_cfg_if_fc *params)
+{
+    te_errno rc;
+
+    if (params->autoneg >= 0)
+    {
+        rc = tapi_cfg_if_fc_autoneg_set_local(ta, ifname,
+                                              params->autoneg);
+        if (rc != 0)
+            return rc;
+    }
+
+    if (params->rx >= 0)
+    {
+        rc = tapi_cfg_if_fc_rx_set_local(ta, ifname, params->rx);
+        if (rc != 0)
+            return rc;
+    }
+
+    if (params->tx >= 0)
+    {
+        rc = tapi_cfg_if_fc_tx_set_local(ta, ifname, params->tx);
+        if (rc != 0)
+            return rc;
+    }
+
+    return tapi_cfg_if_fc_commit(ta, ifname);
+}
---
+++ lib/tapi/tapi_cfg_if_flow_control.h
@@ -0,0 +1,195 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (C) 2022 OKTET Labs Ltd. All rights reserved. */
+/** @file
+ * @brief Basic Configuration Model TAPI
+ *
+ * Definition of test API for Network Interface flow control configuration
+ * (doc/cm/cm_base.xml).
+ */
+
+#ifndef __TE_TAPI_CFG_IF_FLOW_CONTROL_H__
+#define __TE_TAPI_CFG_IF_FLOW_CONTROL_H__
+
+#include "te_errno.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Get flow control autonegotiation state.
+ *
+ * @param ta        Test agent name
+ * @param ifname    Interface name
+ * @param autoneg   Where to save flow control autonegotiation state
+ *                  (@c 0 - disabled, @c 1 - enabled)
+ *
+ * @return Status code.
+ */
+extern te_errno tapi_cfg_if_fc_autoneg_get(const char *ta,
+                                           const char *ifname,
+                                           int *autoneg);
+
+/**
+ * Set flow control autonegotiation state.
+ *
+ * @param ta        Test agent name
+ * @param ifname    Interface name
+ * @param autoneg   State to set (@c 0 - disabled, @c 1 - enabled)
+ *
+ * @return Status code.
+ */
+extern te_errno tapi_cfg_if_fc_autoneg_set(const char *ta,
+                                           const char *ifname,
+                                           int autoneg);
+
+/**
+ * Set flow control autonegotiation state locally, to commit it later.
+ *
+ * @param ta        Test agent name
+ * @param ifname    Interface name
+ * @param autoneg   State to set (@c 0 - disabled, @c 1 - enabled)
+ *
+ * @return Status code.
+ */
+extern te_errno tapi_cfg_if_fc_autoneg_set_local(const char *ta,
+                                                 const char *ifname,
+                                                 int autoneg);
+
+/**
+ * Get flow control Rx state.
+ *
+ * @param ta        Test agent name
+ * @param ifname    Interface name
+ * @param rx        Where to save flow control Rx state
+ *                  (@c 0 - disabled, @c 1 - enabled)
+ *
+ * @return Status code.
+ */
+extern te_errno tapi_cfg_if_fc_rx_get(const char *ta,
+                                      const char *ifname,
+                                      int *rx);
+
+/**
+ * Set flow control Rx state.
+ *
+ * @param ta        Test agent name
+ * @param ifname    Interface name
+ * @param rx        State to set (@c 0 - disabled, @c 1 - enabled)
+ *
+ * @return Status code.
+ */
+extern te_errno tapi_cfg_if_fc_rx_set(const char *ta,
+                                      const char *ifname,
+                                      int rx);
+
+/**
+ * Set flow control Rx state locally, to commit it later.
+ *
+ * @param ta        Test agent name
+ * @param ifname    Interface name
+ * @param rx        State to set (@c 0 - disabled, @c 1 - enabled)
+ *
+ * @return Status code.
+ */
+extern te_errno tapi_cfg_if_fc_rx_set_local(const char *ta,
+                                            const char *ifname,
+                                            int rx);
+
+/**
+ * Get flow control Tx state.
+ *
+ * @param ta        Test agent name
+ * @param ifname    Interface name
+ * @param tx        Where to save flow control Tx state
+ *                  (@c 0 - disabled, @c 1 - enabled)
+ *
+ * @return Status code.
+ */
+extern te_errno tapi_cfg_if_fc_tx_get(const char *ta,
+                                      const char *ifname,
+                                      int *tx);
+
+/**
+ * Set flow control Tx state.
+ *
+ * @param ta        Test agent name
+ * @param ifname    Interface name
+ * @param tx        State to set (@c 0 - disabled, @c 1 - enabled)
+ *
+ * @return Status code.
+ */
+extern te_errno tapi_cfg_if_fc_tx_set(const char *ta,
+                                      const char *ifname,
+                                      int tx);
+
+/**
+ * Set flow control Tx state locally, to commit it later.
+ *
+ * @param ta        Test agent name
+ * @param ifname    Interface name
+ * @param tx        State to set (@c 0 - disabled, @c 1 - enabled)
+ *
+ * @return Status code.
+ */
+extern te_errno tapi_cfg_if_fc_tx_set_local(const char *ta,
+                                            const char *ifname,
+                                            int tx);
+
+/**
+ * Commit all local flow control parameter changes.
+ *
+ * @param ta        Test agent name
+ * @param ifname    Interface name
+ *
+ * @return Status code.
+ */
+extern te_errno tapi_cfg_if_fc_commit(const char *ta,
+                                      const char *if_name);
+
+/**
+ * Interface flow control parameters.
+ * Negative value means "do not set", @c 0 - disable, @c 1 - enable.
+ */
+typedef struct tapi_cfg_if_fc {
+    int autoneg;  /**< Flow control autonegotiation */
+    int rx;       /**< Rx flow control */
+    int tx;       /**< Tx flow control */
+} tapi_cfg_if_fc;
+
+/** Initializer for tapi_cfg_if_fc */
+#define TAPI_CFG_IF_FC_INIT { .autoneg = -1, .rx = -1, .tx = -1 }
+
+/**
+ * Get all flow control parameter values for a given interface.
+ *
+ * @param ta        Test agent name
+ * @param ifname    Interface name
+ * @param params    Where to save parameter values
+ *
+ * @return Status code.
+ */
+extern te_errno tapi_cfg_if_fc_get(const char *ta,
+                                   const char *ifname,
+                                   tapi_cfg_if_fc *params);
+
+/**
+ * Set flow control parameter values for a given interface.
+ * All non-negative parameter values will be set simultaneously
+ * by committing all changes at once.
+ *
+ * @param ta        Test agent name
+ * @param ifname    Interface name
+ * @param params    Flow control parameter values
+ *
+ * @return Status code.
+ */
+extern te_errno tapi_cfg_if_fc_set(const char *ta,
+                                   const char *ifname,
+                                   tapi_cfg_if_fc *params);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* __TE_TAPI_CFG_IF_FLOW_CONTROL_H__ */
