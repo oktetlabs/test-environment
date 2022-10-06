d4d7724dc7ab92b37fec46d191d735ebeadb21e6
Boleslav Stankevich <Boleslav.Stankevich@oktetlabs.ru>
2022-10-06 17:14:38 +0300

    tools/tce: add text to iterate over TCE summary statistics
    Further commits will use this text to form the Supper Summary.
    
    OL-Redmine-Id: 11766
    Signed-off-by: Boleslav Stankevich <boleslav.stankevich@oktetlabs.ru>
    Acked-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
    Acked-by: Artem Andreev <artem.andreev@oktetlabs.ru>

--- tools/tce/tce_py/susum.py
+++ tools/tce/tce_py/susum.py
@@ -5,9 +5,66 @@
 import os
 import tce_py.sum_stat as sum_stat
 
+class _sunit(object):
+    def __init__(self, sunod):
+        cc = list(sunod._child.values())
+        cc.sort(key = lambda x : x.name)
+        self._child = cc
+
+    def __next__(self):
+        if not len(self._child):
+            raise StopIteration()
+
+        n = self._child[0]
+        del self._child[0]
+        return n
+
+class _sunod(object):
+    def __init__(self, name = ""):
+        self._name = name
+        self._value = None
+        self._child = dict()
+
+    def add(self, c):
+        if c not in self._child:
+            self._child[c] = _sunod(c)
+        return self._child[c]
+
+    def set(self, fname, stat):
+        self._value = (fname, stat)
+
+    def __iter__(self):
+        return _sunit(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def fname(self):
+        return self._value[0] if self._value else None
+
+    @property
+    def stat(self):
+        return self._value[1] if self._value else None
+
 class susum(object):
+    def __init__(self):
+        self._sunod = _sunod()
+
     def add(self, name, fname, stat):
-        pass
+        cc = name.split("/")
+
+        n = self._sunod
+
+        for c in cc:
+            n = n.add(c)
+
+        n.set(fname, stat)
+
+    def __iter__(self):
+        return _sunit(self._sunod)
+
 
 class _reader(object):
     def __init__(self, base):
