oktetlabs/test-environment

commit b538ac442a23156cd67a82e63b90252e404a6274

agents/unix: revise Configurator accessors for NIC PHY attrs
1. If possible, use modern ETHTOOL_GLINKSETTINGS/ETHTOOL_SLINKSETTINGS
which allows to set larger speed and supports extended set of link
modes. Use deprecated ETHTOOL_GSET/ETHTOOL_SSET only if the modern
commands are not supported.
2. Use the same API that other configuration nodes implemented via
SIOCETHTOOL use. Rewrite commit() so that it works in the same way as
for other nodes.
3. Update configuration model to show all supported link modes,
as well as all link modes advertised by link partner.
Replace autoneg_oper/autoneg_admin with simple autoneg node.
Remove mention of not implemented agent/interface/phy/reset.

Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

agents/unix/conf/base/conf.c
@@ -254,7 +254,7 @@ extern te_errno ta_unix_conf_netconsole_init(void);
 extern te_errno ta_unix_conf_sys_init(void);
 extern te_errno ta_unix_conf_sys_tree_init(void);
 extern te_errno ta_unix_conf_sys_tree_fini(void);
-extern te_errno ta_unix_conf_phy_init(void);
+extern te_errno ta_unix_conf_if_phy_init(void);
 extern te_errno ta_unix_conf_if_coalesce_init(void);
 extern te_errno ta_unix_conf_if_flow_ctrl_init(void);
 extern te_errno ta_unix_conf_if_rss_init(void);
@@ -1357,7 +1357,7 @@ rcf_ch_conf_init(void)
             goto fail;
 
         /* Initialize configurator PHY support */
-        if (ta_unix_conf_phy_init() != 0)
+        if (ta_unix_conf_if_phy_init() != 0)
             goto fail;
 
         if (ta_unix_conf_if_coalesce_init() != 0)

doc/cm/cm_base.yml
@@ -472,81 +472,54 @@
     - oid: "/agent/interface/phy"
       access: read_only
       type: none
+      unit: true
+      depends:
+        - oid: "/agent/interface/status"
       d: |
          Network interface PHY parameters
          Name: none
          Value: none
 
-    - oid: "/agent/interface/phy/autoneg_admin"
+    - oid: "/agent/interface/phy/autoneg"
       access: read_write
       type: int32
       d: |
          Network interface autonegotiation state control
          Name: none
-         Value: -1 - Not supported
-                 0 - ON
-                 1 - OFF
-
-    - oid: "/agent/interface/phy/autoneg_oper"
-      access: read_only
-      type: int32
-      volatile: true
-      d: |
-         Network interface autonegotiation state oper value
-         Name: none
-         Value: -1 - Not supported
-                 0 - ON
-                 1 - OFF
-
-    - oid: "/agent/interface/phy/autoneg_lp_adv"
-      access: read_only
-      type: int32
-      volatile: true
-      d: |
-         Network interface link partner advertised autonegotiation state value
-         Name: none
-         Value:  off (0)
-                 on (1)
-                 unknown (2)
-
-    - oid: "/agent/interface/phy/pause_lp_adv"
-      access: read_only
-      type: int32
-      volatile: true
-      d: |
-         Network interface link partner advertised pause frame use value
-         Name: none
-         Value: none (0)              - no pause frame use
-                Tx only (1)           - transmit only
-                symmetric (2)         - transmit and receive
-                symmetric Rx only (3) - transmit and receive OR receive only
-                unknown (4)           - not supported
+         Value: 0 - OFF
+                1 - ON
 
     - oid: "/agent/interface/phy/duplex_admin"
       access: read_write
       type: string
+      depends:
+        - oid: "/agent/interface/phy/autoneg"
       d: |
          Network interface duplex control; set value denied when autonegotiation
          state on
          Name: none
          Value: half          - half duplex
                 full          - full duplex
-                not supported - not supported
+                unknown       - unknown
 
     - oid: "/agent/interface/phy/duplex_oper"
       access: read_only
       type: string
       volatile: true
+      depends:
+        - oid: "/agent/interface/phy/autoneg"
       d: |
          Network interface duplex oper value
          Name: none
          Value: half          - half duplex
                 full          - full duplex
-                not supported - not supported
+                unknown       - unknown
 
     - oid: "/agent/interface/phy/speed_admin"
       access: read_write
       type: int32
+      depends:
+        - oid: "/agent/interface/phy/autoneg"
       d: |
          Network interface speed control; set value denied when autonegotiation
          state on
@@ -558,34 +531,30 @@
       access: read_only
       type: int32
       volatile: true
+      depends:
+        - oid: "/agent/interface/phy/autoneg"
       d: |
          Network interface speed oper value
          Name: none
          Value: integer equal PHY speed in Mbit/sec or -1
                 if not supported
 
-    - oid: "/agent/interface/phy/modes"
-      access: read_only
-      type: none
-      d: |
-         PHY supported mode list
-         Name: none
-
-    - oid: "/agent/interface/phy/modes/speed"
-      access: read_only
-      type: none
-      d: |
-         PHY supported mode speed
-         Name: integer equal PHY speed in Mbit/sec
-
-    - oid: "/agent/interface/phy/modes/speed/duplex"
+    - oid: "/agent/interface/phy/mode"
       access: read_write
       type: int32
       d: |
-         PHY supported mode duplex state
-         Name: half/full
+         Supported link mode
+         Name: Link mode name
          Value: 0 - mode not advertised
                 1 - mode advertised
+
+    - oid: "/agent/interface/phy/lp_advertised"
+      access: read_only
+      type: none
+      volatile: true
+      d: |
+         Link mode advertised by link partner
+         Name: Link mode name
 
     - oid: "/agent/interface/phy/state"
       access: read_only
@@ -597,14 +566,6 @@
          Value: -1 - not supported
                  0 - link down
                  1 - link up
-
-    - oid: "/agent/interface/phy/reset"
-      access: read_write
-      type: int32
-      d: |
-         Link reset
-         Name: none
-         Value: any value means that link should be reseted
 
     - oid: "/agent/interface/feature"
       access: read_write

include/te_ethernet_phy.h
@@ -47,14 +47,25 @@
 /* PHY duplex states */
 #define TE_PHY_DUPLEX_HALF            (0)       /* Half duplex */
 #define TE_PHY_DUPLEX_FULL            (1)       /* Full duplex */
-#define TE_PHY_DUPLEX_UNKNOWN         (2)       /* Unknown duplex state */
+#define TE_PHY_DUPLEX_UNKNOWN         (0xff)    /* Unknown duplex state */
 
 /* PHY speed values */
-#define TE_PHY_SPEED_UNKNOWN          (0)       /* Unknown speed value */
+#define TE_PHY_SPEED_UNKNOWN          (-1)      /* Unknown speed value */
 #define TE_PHY_SPEED_10               (10)      /* 10 Mb/sec */
 #define TE_PHY_SPEED_100              (100)     /* 100 Mb/sec */
 #define TE_PHY_SPEED_1000             (1000)    /* 1000 Mb/sec */
+#define TE_PHY_SPEED_2500             (2500)    /* 2500 Mb/sec */
+#define TE_PHY_SPEED_5000             (5000)    /* 5000 Mb/sec */
 #define TE_PHY_SPEED_10000            (10000)   /* 10000 Mb/sec */
+#define TE_PHY_SPEED_14000            (14000)   /* 14000 Mb/sec */
+#define TE_PHY_SPEED_20000            (20000)   /* 20000 Mb/sec */
+#define TE_PHY_SPEED_25000            (25000)   /* 25000 Mb/sec */
+#define TE_PHY_SPEED_40000            (40000)   /* 40000 Mb/sec */
+#define TE_PHY_SPEED_50000            (50000)   /* 50000 Mb/sec */
+#define TE_PHY_SPEED_56000            (56000)   /* 56000 Mb/sec */
+#define TE_PHY_SPEED_100000           (100000)  /* 100000 Mb/sec */
+#define TE_PHY_SPEED_200000           (200000)  /* 200000 Mb/sec */
+#define TE_PHY_SPEED_400000           (400000)  /* 400000 Mb/sec */
 
 /* PHY pause frame use */
 #define TE_PHY_PAUSE_NONE                   (0)     /* No pause frame use */

commit 449d7703970e48f20cb2b67ae085d71bb1d03d9f

agents/unix: improve handling of link settings
Some types of interfaces (bond, team, vlan) support reading link
settings but do not support changing them. If settings change,
Configurator tries to restore them and fails.

To avoid this, administrative values should always be set to
unknown values when settings cannot be changed. Configurator
cares only about them, not about operative values.

OL-Redmine-Id: 12380
Fixes: b538ac442a23 ("agents/unix: revise Configurator accessors for NIC PHY attrs"
Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>
Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>

agents/unix/conf/base/conf.c
@@ -3504,6 +3504,19 @@ iface_parent_get(unsigned int gid, const char *oid, char *value,
 #endif
 }
 
+/* See description in conf_common.h */
+te_errno
+get_interface_kind(const char *ifname, char *value)
+{
+    *value = '\0';
+
+#ifdef USE_LIBNETCONF
+    return iface_get_property_netconf(ifname, value, IF_PROP_KIND);
+#else
+    return TE_RC(TE_TA_UNIX, TE_ENOENT);
+#endif
+}
+
 /**
  * Get kind of an interface (whether it is vlan, macvlan, ipvlan, etc).
  *
@@ -3526,11 +3539,7 @@ iface_kind_get(unsigned int gid, const char *oid, char *value,
     if ((rc = CHECK_INTERFACE(ifname)) != 0)
         return TE_RC(TE_TA_UNIX, rc);
 
-#ifdef USE_LIBNETCONF
-    return iface_get_property_netconf(ifname, value, IF_PROP_KIND);
-#else
-    return TE_RC(TE_TA_UNIX, TE_ENOENT);
-#endif
+    return get_interface_kind(ifname, value);
 }
 
 static te_errno

agents/unix/conf/base/conf_common.h
@@ -116,6 +116,17 @@ extern te_errno string_replace(char **dst, const char *src);
  */
 extern te_errno string_empty_list(char **list);
 
+/**
+ * Get kind of interface ("bond", "vlan", "team", etc).
+ *
+ * @param ifname      Interface name
+ * @param value       Where to save interface kind
+ *                    (should be of @c RCF_MAX_VAL length).
+ *
+ * @return Status code.
+ */
+extern te_errno get_interface_kind(const char *ifname, char *value);
+
 #ifdef __cplusplus
 } /* extern "C" */
 #endif

agents/unix/conf/base/conf_ethtool.c
@@ -33,6 +33,7 @@
 
 #include "unix_internal.h"
 #include "conf_ethtool.h"
+#include "conf_common.h"
 
 #if defined (__linux__) && HAVE_LINUX_ETHTOOL_H
 
@@ -290,6 +291,105 @@ call_ethtool_ioctl(const char *if_name, int cmd, void *value)
     return call_ioctl(&ifr, if_name, cmd);
 }
 
+#ifdef ETHTOOL_SLINKSETTINGS
+/*
+ * Check whether ETHTOOL_SLINKSETTINGS is supported for
+ * an interface.
+ */
+static void
+check_slinksettings_support(const char *if_name,
+                            ta_ethtool_lsets *lsets)
+{
+    struct ifreq ifr;
+    struct ethtool_link_settings native_lsets;
+    int rc;
+    te_errno te_rc;
+
+    /*
+     * This produces incorrectly filled settings which
+     * will be rejected by Linux kernel. Interface should
+     * not be affected in any way. If driver does not
+     * provide set_link_ksettings() callback, EOPNOTSUPP
+     * is obtained; otherwise - EINVAL.
+     */
+    memset(&native_lsets, 0, sizeof(native_lsets));
+    native_lsets.cmd = ETHTOOL_SLINKSETTINGS;
+
+    te_rc = prepare_ifr(&ifr, if_name);
+    if (te_rc != 0)
+        return;
+
+    ifr.ifr_data = (caddr_t)&native_lsets;
+
+    rc = ioctl(cfg_socket, SIOCETHTOOL, &ifr);
+    if (rc < 0 && errno == EOPNOTSUPP)
+    {
+        lsets->set_supported = FALSE;
+    }
+    else if (rc >= 0)
+    {
+        ERROR("%s(if_name=%s): ioctl() succeeded with incorrect "
+              "request", __FUNCTION__, if_name);
+    }
+}
+#endif
+
+/*
+ * Check whether ETHTOOL_SSET is supported for an interface.
+ */
+static void
+check_sset_support(const char *if_name, ta_ethtool_lsets *lsets)
+{
+    te_errno rc;
+    char value[RCF_MAX_VAL] = "";
+    static const char *known_kinds[] = { "vlan", "bond", "team",
+                                         "ipvlan", "macvlan" };
+    unsigned int i;
+
+    /*
+     * There is no good way to check whether deprecated ETHTOOL_SSET
+     * works without potentially affecting network interface.
+     * So just check for known interface kinds here.
+     */
+
+    rc = get_interface_kind(if_name, value);
+    if (rc == 0)
+    {
+        for (i = 0; i < TE_ARRAY_LEN(known_kinds); i++)
+        {
+            if (strcmp(value, known_kinds[i]) == 0)
+            {
+                lsets->set_supported = FALSE;
+                break;
+            }
+        }
+    }
+}
+
+/*
+ * Check whether changing link settings is supported by the driver.
+ */
+static void
+lsets_check_set_support(const char *if_name,
+                        ta_ethtool_lsets *lsets)
+{
+    lsets->set_supported = TRUE;
+
+    if (lsets->use_xlinksettings)
+    {
+#ifdef ETHTOOL_SLINKSETTINGS
+        check_slinksettings_support(if_name, lsets);
+#else
+        ERROR("%s(if_name=%s): ETHTOOL_SLINKSETTINGS should be used but "
+              "it is not defined", __FUNCTION__, if_name);
+#endif
+    }
+    else
+    {
+        check_sset_support(if_name, lsets);
+    }
+}
+
 /*
  * Try to fill link settings structure with ETHTOOL_GLINKSETTINGS.
  * If that command is not defined or fails, use ETHTOOL_GSET instead.
@@ -440,7 +540,11 @@ get_ethtool_value(const char *if_name, unsigned int gid,
         }
 
         if (cmd == TA_ETHTOOL_LINKSETTINGS)
+        {
             rc = get_ethtool_lsets(if_name, *ptr_out);
+            if (rc == 0)
+                lsets_check_set_support(if_name, *ptr_out);
+        }
         else
             rc = call_ethtool_ioctl(if_name, native_cmd, *ptr_out);
 

agents/unix/conf/base/conf_ethtool.h
@@ -79,6 +79,8 @@ typedef struct ta_ethtool_lsets {
     te_bool use_xlinksettings;  /**< If @c TRUE, link settings were obtained
                                      via ETHTOOL_GLINKSETTINGS, otherwise
                                      ETHTOOL_GSET was used */
+    te_bool set_supported;      /**< If @c TRUE, changing link settings is
+                                     supported */
 } ta_ethtool_lsets;
 
 /** Link settings field IDs */

agents/unix/conf/base/conf_phy.c
@@ -74,6 +74,10 @@ phy_field_get(unsigned int gid, const char *if_name,
          * to unknown value so that Configurator will not try to set
          * specific speed/duplex when trying to restore configuration
          * from backup.
+         *
+         * When driver does not support changing link settings,
+         * administrative speed/duplex should be set to unknown values
+         * for the same reason.
          */
 
         rc = ta_ethtool_lsets_field_get(lsets_ptr,
@@ -82,7 +86,7 @@ phy_field_get(unsigned int gid, const char *if_name,
         if (rc != 0)
             return rc;
 
-        if (autoneg != 0)
+        if (autoneg != 0 || !lsets_ptr->set_supported)
         {
             if (field == TA_ETHTOOL_LSETS_SPEED)
             {
@@ -236,6 +240,40 @@ phy_duplex_admin_get(unsigned int gid, const char *oid, char *value,
                      const char *if_name)
 {
     return phy_duplex_get_common(gid, oid, value, if_name, TRUE);
+}
+
+/*
+ * Check whether changing link settings is supported for the interface.
+ */
+static te_errno
+phy_set_supported_get(unsigned int gid, const char *oid, char *value,
+                      const char *if_name)
+{
+    te_string str_val = TE_STRING_EXT_BUF_INIT(value, RCF_MAX_VAL);
+    ta_ethtool_lsets *lsets_ptr = NULL;
+    te_errno rc;
+
+    UNUSED(oid);
+
+    rc = get_ethtool_value(if_name, gid, TA_ETHTOOL_LINKSETTINGS,
+                           (void **)&lsets_ptr);
+    if (rc != 0)
+    {
+        if (rc == TE_RC(TE_TA_UNIX, TE_EOPNOTSUPP))
+        {
+            /*
+             * This will cause Configurator to ignore absence of
+             * value silently; it simply will not show not supported
+             * node in the tree.
+             */
+            return TE_RC(TE_TA_UNIX, TE_ENOENT);
+        }
+
+        return rc;
+    }
+
+    return te_string_append(&str_val, "%d",
+                            lsets_ptr->set_supported ? 1 : 0);
 }
 
 /* Common function to set link settings structure field */
@@ -628,7 +666,10 @@ RCF_PCH_CFG_NODE_RWC(node_phy_duplex_admin, "duplex_admin", NULL,
 RCF_PCH_CFG_NODE_RO(node_phy_duplex_oper, "duplex_oper", NULL,
                     &node_phy_duplex_admin, phy_duplex_oper_get);
 
-RCF_PCH_CFG_NODE_NA_COMMIT(node_phy, "phy", &node_phy_duplex_oper, NULL,
+RCF_PCH_CFG_NODE_RO(node_phy_set_supported, "set_supported", NULL,
+                    &node_phy_duplex_oper, phy_set_supported_get);
+
+RCF_PCH_CFG_NODE_NA_COMMIT(node_phy, "phy", &node_phy_set_supported, NULL,
                            phy_commit);
 
 /**

doc/cm/cm_base.yml
@@ -567,6 +567,16 @@
                  0 - link down
                  1 - link up
 
+    - oid: "/agent/interface/phy/set_supported"
+      access: read_only
+      type: int32
+      volatile: true
+      d: |
+         Can link settings be changed?
+         Name: none
+         Value: 0 - Changing is not supported
+                1 - Changing is supported
+
     - oid: "/agent/interface/feature"
       access: read_write
       type: int32