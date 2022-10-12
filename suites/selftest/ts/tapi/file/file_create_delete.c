oktetlabs/test-environment

commit f33836d43ac2899ca0f94b4d2bf1934fb2dc3064

lib/tapi: use printf attributes as needed in File TAPI
Note that this patch may break build of test suites if they
erroneously pass a random string as a format string.

OL-Redmine-Id: 12117
Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>

suites/selftest/ts/tapi/file/file_create_delete.c
@@ -30,7 +30,7 @@ main(int argc, char **argv)
 
     TEST_STEP("Create a file on TA");
     filename = tapi_file_generate_name();
-    if (tapi_file_create_ta(pco_iut->ta, filename, "") != 0)
+    if (tapi_file_create_ta(pco_iut->ta, filename, "%s", "") != 0)
     {
         TEST_VERDICT("tapi_file_create_ta() failed");
     }

lib/tapi/tapi_file.h
@@ -113,7 +113,8 @@ extern char *tapi_file_create(size_t len, char *buf, te_bool random);
  * @note the function is not thread-safe
  */
 extern te_errno tapi_file_create_ta(const char *ta, const char *filename,
-                                    const char *fmt, ...);
+                                    const char *fmt, ...)
+    __attribute__((format(printf, 3, 4)));
 
 /**
  * Create local file, copy it to TA, remove local file.
@@ -132,7 +133,8 @@ extern te_errno tapi_file_create_ta(const char *ta, const char *filename,
 extern te_errno tapi_file_create_ta_r(const char *ta,
                                       const char *lfile,
                                       const char *rfile,
-                                      const char *fmt, ...);
+                                      const char *fmt, ...)
+    __attribute__((format(printf, 4, 5)));;
 
 /**
  * Read file content from the TA.
@@ -163,7 +165,8 @@ extern te_errno tapi_file_read_ta(const char *ta, const char *filename,
  * @note the function is not thread-safe
  */
 extern te_errno tapi_file_append_ta(const char *ta, const char *filename,
-                                    const char *fmt, ...);
+                                    const char *fmt, ...)
+    __attribute__((format(printf, 3, 4)));
 
 
 
@@ -189,7 +192,8 @@ extern te_errno tapi_file_copy_ta(const char *ta_src, const char *src,
  * @return Status code.
  */
 extern te_errno tapi_file_ta_unlink_fmt(const char *ta,
-                                        const char *path_fmt, ...);
+                                        const char *path_fmt, ...)
+    __attribute__((format(printf, 2, 3)));
 
 #ifdef __cplusplus
 } /* extern "C" */

suites/selftest/ts/tapi/file/file_directory.c
@@ -32,7 +32,7 @@ create_files(size_t nfiles, rcf_rpc_server *rpcs, const char *path)
     {
         te_string_reset(&filename);
         CHECK_RC(te_string_append(&filename, "%s/%zu", path, i));
-        if (tapi_file_create_ta(rpcs->ta, filename.ptr, "") != 0)
+        if (tapi_file_create_ta(rpcs->ta, filename.ptr, "%s", "") != 0)
             break;
         ncreated++;
         filename.len = 0;

suites/selftest/ts/tapi/file/file_write.c
@@ -36,7 +36,7 @@ main(int argc, char **argv)
     TEST_STEP("Write data to a file on TA");
     data = te_make_buf_by_len(BUFSIZE);
     rfile = tapi_file_generate_name();
-    if (tapi_file_create_ta(pco_iut->ta, rfile, "") != 0)
+    if (tapi_file_create_ta(pco_iut->ta, rfile, "%s", "") != 0)
     {
         TEST_VERDICT("tapi_file_create_ta() failed");
     }

commit f973f9b4583be371ab4583bdba179e2bc06e0e11

suites/selftest: use proper format string
The previous patch accidentally did not fix some uses of
tapi_file_ta_unlink_fmt(), resulting in a build error on
some flavours of GCC.

OL-Redmine-Id: 12117
Fixes: f33836d43ac2 ("lib/tapi: use printf attributes as needed in File TAPI")
Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>

suites/selftest/ts/tapi/file/file_create_delete.c
@@ -39,7 +39,7 @@ main(int argc, char **argv)
     file_check_exist(pco_iut, filename);
 
     TEST_STEP("Delete the file from TA");
-    if (tapi_file_ta_unlink_fmt(pco_iut->ta, filename) != 0)
+    if (tapi_file_ta_unlink_fmt(pco_iut->ta, "%s", filename) != 0)
     {
         TEST_VERDICT("tapi_file_ta_unlink_fmt() failed");
     }

suites/selftest/ts/tapi/tool/torrent/ctorrent.c
@@ -64,7 +64,7 @@ check_dest_content(const char *dest_ta, const char *dest_file,
 void
 cleanup_unlink_file(const char *ta, const char *file)
 {
-    if (tapi_file_ta_unlink_fmt(ta, file) != 0)
+    if (tapi_file_ta_unlink_fmt(ta, "%s", file) != 0)
         ERROR("Failed to remove file '%s' from TA '%s'", file, ta);
 }
 
@@ -154,7 +154,7 @@ main(int argc, char **argv)
     {
         WARN("The file already exists");
         TEST_SUBSTEP("Remove the file");
-        if (tapi_file_ta_unlink_fmt(pco_iut->ta, metainfo_iut) != 0)
+        if (tapi_file_ta_unlink_fmt(pco_iut->ta, "%s", metainfo_iut) != 0)
             TEST_FAIL("Failed to remove the file");
 
         TEST_SUBSTEP("Create the metainfo file again");