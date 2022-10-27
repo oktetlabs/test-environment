oktetlabs/test-environment

commit f64fce721745bbf37d00d72d88ed864767ed7f1d

include: add a macro to report fatal errors
OL-Redmine-Id: 12077
Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>

include/logger_api.h
@@ -289,4 +289,20 @@
     } while (0)
 
 
+/**
+ * Log an error and then immediately terminate the program.
+ *
+ * @note A log message may be lost in this case. A future version
+ *       may use a more sophisticated mechanism of exception handling.
+ *
+ * @param _fmt   format string
+ * @param ...    arguments as specified by @p _fmt
+ */
+#define TE_FATAL_ERROR(_fmt, ...) \
+    do {                                                        \
+        ERROR("%s() at %s:%d" _fmt, __func__, __FILE__, __LINE__,   \
+              ##__VA_ARGS__);                                       \
+        abort();                                                    \
+    } while (0)
+
 #endif /* !__TE_LOGGER_API_H__ */

commit dd9dc1ceb4a5151104cd4c8948f3211ba621294e

include: fix error message formatting
The fatal error reporting macro lacked a separator
between the source location and the message.

OL-Redmine-Id: 12077
Fixes: f64fce7217 ("include: add a macro to report fatal errors")
Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

include/logger_api.h
@@ -299,8 +299,8 @@
  * @param ...    arguments as specified by @p _fmt
  */
 #define TE_FATAL_ERROR(_fmt, ...) \
-    do {                                                        \
-        ERROR("%s() at %s:%d" _fmt, __func__, __FILE__, __LINE__,   \
+    do {                                                            \
+        ERROR("%s() at %s:%d: " _fmt, __func__, __FILE__, __LINE__, \
               ##__VA_ARGS__);                                       \
         abort();                                                    \
     } while (0)