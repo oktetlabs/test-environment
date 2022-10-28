oktetlabs/test-environment

commit 601efe4b007821236c3e057e98a2713c0aa7e093

lib/tapi: provide more robust version of filename generators
The old functions used a static buffer which is highly unreliable.
They are still retained though but marked as deprecated.

OL-Redmine-Id: 12117
Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>

lib/tapi/tapi_file.c
@@ -41,13 +41,44 @@
 #include "logger_api.h"
 #include "tapi_file.h"
 
+void
+tapi_file_make_name(te_string *dest)
+{
+    static unsigned int seq = 0;
+
+    te_string_append(dest, "te_tmp_%u_%u_%u", (unsigned)time(NULL), getpid(),
+                     seq++);
+}
+
+void
+tapi_file_make_custom_pathname(te_string *dest, const char *dirname,
+                               const char *suffix)
+{
+    if (dirname != NULL)
+        te_string_append(dest, "%s/", dirname);
+    tapi_file_make_name(dest);
+    if (suffix != NULL)
+        te_string_append(dest, "%s", suffix);
+}
+
+void
+tapi_file_make_pathname(te_string *dest)
+{
+    const char *te_tmp = getenv("TE_TMP");
+
+    if (te_tmp == NULL)
+        TE_FATAL_ERROR("TE_TMP is empty");
+
+    tapi_file_make_custom_pathname(dest, te_tmp, NULL);
+}
+
 char *
 tapi_file_generate_name(void)
 {
-    static int  num = 0;
     static char buf[RCF_MAX_PATH];
+    te_string str = TE_STRING_BUF_INIT(buf);
 
-    TE_SPRINTF(buf, "te_tmp_%u_%u_%u", (uint32_t)time(NULL), getpid(), num++);
+    tapi_file_make_name(&str);
 
     return buf;
 }
@@ -55,18 +86,12 @@ tapi_file_generate_name(void)
 char *
 tapi_file_generate_pathname(void)
 {
-    static char  pathname[RCF_MAX_PATH];
-    static char *te_tmp;
+    static char buf[RCF_MAX_PATH];
+    te_string str = TE_STRING_BUF_INIT(buf);
 
-    if (te_tmp == NULL && (te_tmp = getenv("TE_TMP")) == NULL)
-    {
-        ERROR("TE_TMP is empty");
-        return NULL;
-    }
+    tapi_file_make_pathname(&str);
 
-    TE_SPRINTF(pathname, "%s/%s", te_tmp, tapi_file_generate_name());
-
-    return pathname;
+    return buf;
 }
 
 static te_errno
@@ -150,15 +175,17 @@ close_and_check(FILE *f, const char *filename)
 char *
 tapi_file_create_pattern(size_t len, char c)
 {
-    char *pathname = tapi_file_generate_pathname();
+    te_string pathname = TE_STRING_INIT;
     char  buf[1024];
     FILE *f;
 
-    if (pathname == NULL)
-        return NULL;
+    tapi_file_make_pathname(&pathname);
 
-    if (open_and_check(pathname, "w", &f) != 0)
+    if (open_and_check(pathname.ptr, "w", &f) != 0)
+    {
+        te_string_free(&pathname);
         return NULL;
+    }
 
     memset(buf, c, sizeof(buf));
 
@@ -166,46 +193,56 @@ tapi_file_create_pattern(size_t len, char c)
     {
         size_t chunk = len > sizeof(buf) ? sizeof(buf) : len;
 
-        if (write_and_check(f, pathname, buf, chunk) != 0)
+        if (write_and_check(f, pathname.ptr, buf, chunk) != 0)
         {
             fclose(f);
+            te_string_free(&pathname);
             return NULL;
         }
         len -= chunk;
     }
 
-    if (close_and_check(f, pathname) != 0)
+    if (close_and_check(f, pathname.ptr) != 0)
+    {
+        te_string_free(&pathname);
         return NULL;
+    }
 
-    return strdup(pathname);
+    return pathname.ptr;
 }
 
 /* See description in tapi_file.h */
 char *
 tapi_file_create(size_t len, char *buf, te_bool random)
 {
-    char *pathname = tapi_file_generate_pathname();
+    te_string pathname = TE_STRING_INIT;
     FILE *f;
 
-    if (pathname == NULL)
-        return NULL;
+    tapi_file_make_pathname(&pathname);
 
     if (random)
         te_fill_buf(buf, len);
 
-    if (open_and_check(pathname, "w", &f) != 0)
-        return NULL;
-
-    if (write_and_check(f, pathname, buf, len) != 0)
+    if (open_and_check(pathname.ptr, "w", &f) != 0)
     {
-        fclose(f);
+        te_string_free(&pathname);
         return NULL;
     }
 
-    if (close_and_check(f, pathname) != 0)
+    if (write_and_check(f, pathname.ptr, buf, len) != 0)
+    {
+        fclose(f);
+        te_string_free(&pathname);
         return NULL;
+    }
 
-    return strdup(pathname);
+    if (close_and_check(f, pathname.ptr) != 0)
+    {
+        te_string_free(&pathname);
+        return NULL;
+    }
+
+    return pathname.ptr;
 }
 
 /**
@@ -228,27 +265,31 @@ tapi_file_create_ta_gen(const char *ta,
                         const char *header,
                         const char *fmt, va_list ap)
 {
+    te_string lfile_name = TE_STRING_INIT;
     FILE *f;
     te_errno rc;
 
     if (lfile == NULL)
-        lfile = tapi_file_generate_pathname();
+        tapi_file_make_pathname(&lfile_name);
+    else
+        te_string_append(&lfile_name, "%s", lfile);
 
-    if ((rc = open_and_check(lfile, "w", &f)) != 0)
+    if ((rc = open_and_check(lfile_name.ptr, "w", &f)) != 0)
         return rc;
 
     if (header != NULL)
         fputs(header, f);
     vfprintf(f, fmt, ap);
 
-    rc = close_and_check(f, lfile);
+    rc = close_and_check(f, lfile_name.ptr);
     if (rc == 0)
     {
-        rc = rcf_ta_put_file(ta, 0, lfile, rfile);
+        rc = rcf_ta_put_file(ta, 0, lfile_name.ptr, rfile);
         if (rc != 0)
             ERROR("Cannot put file %s on TA %s: %r", rfile, ta, rc);
     }
-    unlink(lfile);
+    unlink(lfile_name.ptr);
+    te_string_free(&lfile_name);
 
     return rc;
 }
@@ -290,7 +331,7 @@ te_errno
 tapi_file_copy_ta(const char *ta_src, const char *src,
                   const char *ta_dst, const char *dst)
 {
-    const char *pathname;
+    te_string pathname = TE_STRING_INIT;
     te_errno rc = 0;
     struct stat st;
     te_bool need_unlink = FALSE;
@@ -301,52 +342,58 @@ tapi_file_copy_ta(const char *ta_src, const char *src,
         {
             ERROR("%s(): copying between local files is not supported",
                   __func__);
+            te_string_free(&pathname);
+
             return TE_RC(TE_TAPI, TE_EOPNOTSUPP);
         }
-        pathname = src;
+        te_string_append(&pathname, "%s", src);
     }
     else if (ta_dst == NULL)
     {
-        pathname = dst;
+        te_string_append(&pathname, "%s", dst);
     }
     else
     {
-        pathname = tapi_file_generate_pathname();
+        tapi_file_make_pathname(&pathname);
         need_unlink = TRUE;
     }
 
     if (ta_src != NULL)
     {
-        if ((rc = rcf_ta_get_file(ta_src, 0, src, pathname)) != 0)
+        if ((rc = rcf_ta_get_file(ta_src, 0, src, pathname.ptr)) != 0)
         {
             ERROR("Cannot get file %s from TA %s: %r", src, ta_src, rc);
+            te_string_free(&pathname);
+
             return rc;
         }
     }
 
     if (ta_dst != NULL)
     {
-        if ((rc = rcf_ta_put_file(ta_dst, 0, pathname, dst)) != 0)
+        if ((rc = rcf_ta_put_file(ta_dst, 0, pathname.ptr, dst)) != 0)
         {
             ERROR("Cannot put file %s to TA %s: %r", dst, ta_dst, rc);
             goto cleanup;
         }
     }
 
-    if (stat(pathname, &st) != 0)
+    if (stat(pathname.ptr, &st) != 0)
     {
         rc = TE_OS_RC(TE_TAPI, errno);
-        ERROR("Cannot stat local file %s: %r", pathname, rc);
+        ERROR("Cannot stat local file %s: %r", pathname.ptr, rc);
         goto cleanup;
     }
 
     RING("Copy file %s:%s to %s:%s using local %s size %lld",
-         ta_src, src, ta_dst, dst, pathname,
+         ta_src, src, ta_dst, dst, pathname.ptr,
          (long long)st.st_size);
 
 cleanup:
     if (need_unlink)
-        unlink(pathname);
+        unlink(pathname.ptr);
+    te_string_free(&pathname);
+
     return rc;
 }
 
@@ -354,7 +401,7 @@ static te_errno
 tapi_file_read_ta_gen(const char *ta, const char *filename,
                       te_bool may_not_exist, char **pbuf)
 {
-    char *pathname = tapi_file_generate_pathname();
+    te_string pathname = TE_STRING_INIT;
     te_errno rc = 0;
     char *buf = NULL;
     FILE *f = NULL;
@@ -362,8 +409,11 @@ tapi_file_read_ta_gen(const char *ta, const char *filename,
     struct stat st;
     size_t to_read;
 
-    if ((rc = rcf_ta_get_file(ta, 0, filename, pathname)) != 0)
+    tapi_file_make_pathname(&pathname);
+    if ((rc = rcf_ta_get_file(ta, 0, filename, pathname.ptr)) != 0)
     {
+        te_string_free(&pathname);
+
         if (may_not_exist && TE_RC_GET_ERROR(rc) == TE_ENOENT)
         {
             *pbuf = strdup("");
@@ -373,10 +423,12 @@ tapi_file_read_ta_gen(const char *ta, const char *filename,
         ERROR("Cannot get file %s from TA %s: %r", filename, ta, rc);
         return rc;
     }
-    if (stat(pathname, &st) != 0)
+    if (stat(pathname.ptr, &st) != 0)
     {
         rc = TE_OS_RC(TE_TAPI, errno);
-        ERROR("Failed to stat file %s: %r", pathname, rc);
+        ERROR("Failed to stat file %s: %r", pathname.ptr, rc);
+        te_string_free(&pathname);
+
         return rc;
     }
     to_read = st.st_size;
@@ -388,14 +440,15 @@ tapi_file_read_ta_gen(const char *ta, const char *filename,
     }
     else
     {
-        rc = open_and_check(pathname, "r", &f);
+        rc = open_and_check(pathname.ptr, "r", &f);
         if (rc == 0)
-            rc = read_and_check(f, pathname, buf, to_read);
+            rc = read_and_check(f, pathname.ptr, buf, to_read);
     }
 
     if (f != NULL)
         fclose(f);
-    unlink(pathname);
+    unlink(pathname.ptr);
+    te_string_free(&pathname);
 
     if (rc == 0)
         *pbuf = buf;

lib/tapi/tapi_file.h
@@ -16,31 +16,74 @@
 #define __TE_TAPI_FILE_H__
 
 #include "te_defs.h"
+#include "te_string.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 /**
+ * Generate a unique basename for a file.
+ *
+ * @param[out] dest  The string to hold the name.
+ *
+ * @note The name is appended to the string contents.
+ *       That way it is easier to construct derived pathnames
+ *       and similiar stuff.
+ */
+extern void tapi_file_make_name(te_string *dest);
+
+/**
+ * Generate a unique pathname for a file on the Engine side.
+ *
+ * @param[out] dest    The string to hold the name.
+ * @param[in]  dirname Directory component (may be @c NULL,
+ *                     in which case a relative name is generated)
+ * @param[in]  suffix  A custom suffix to add to a generated
+ *                     pathname (may be @c NULL).
+ *
+ * @note The name is appended to the string contents.
+ */
+extern void tapi_file_make_custom_pathname(te_string *dest,
+                                           const char *dirname,
+                                           const char *suffix);
+
+
+/**
+ * Generate a unique pathname for a file on the Engine side.
+ *
+ * @param[out] dest  The string to hold the name.
+ *
+ * @note The name is appended to the string contents.
+ * @note @c TE_TMP env variable must be set.
+ */
+extern void tapi_file_make_pathname(te_string *dest);
+
+/**
  * Generate unique basename for file.
  *
  * @return generated name
  *
- * @note The function returns a pointer to 
- *       a static buffer, so the caller should
- *       in most cases use strdup() on it or
- *       something similiar.
+ * @deprecated
+ * The function returns a pointer to
+ * a static buffer, so it is inherently unreliable.
+ * Use tapi_file_make_name() instead.
  */
-extern char *tapi_file_generate_name(void);
+extern char *tapi_file_generate_name(void)
+    __attribute__((deprecated));
 
 /**
  * Generate unique pathname for file on the engine.
  *
  * @return generated name
  *
- * @note the function is not thread-safe
+ * @deprecated
+ * The function returns a pointer to
+ * a static buffer, so it is inherently unreliable.
+ * Use tapi_file_make_pathname() instead.
  */
-extern char *tapi_file_generate_pathname(void);
+extern char *tapi_file_generate_pathname(void)
+    __attribute__((deprecated));
 
 /**
  * Create file in the TE temporary directory.
@@ -49,9 +92,7 @@ extern char *tapi_file_generate_pathname(void);
  * @param c     file content pattern
  *
  * @return name (memory is allocated) of the file or
- *         NULL in the case of failure
- *
- * @note The function is not thread-safe
+ *         @c NULL in the case of failure
  */
 extern char *tapi_file_create_pattern(size_t len, char c);
 
@@ -63,9 +104,7 @@ extern char *tapi_file_create_pattern(size_t len, char c);
  * @param random  if TRUE, fill buffer with random data
  *
  * @return name (memory is allocated) of the file or
- *         NULL in the case of failure
- *
- * @note The function is not thread-safe
+ *         @c NULL in the case of failure
  */
 extern char *tapi_file_create(size_t len, char *buf, te_bool random);
 
@@ -77,8 +116,6 @@ extern char *tapi_file_create(size_t len, char *buf, te_bool random);
  * @param fmt           format string for the file content
  *
  * @return Status code
- *
- * @note the function is not thread-safe
  */
 extern te_errno tapi_file_create_ta(const char *ta, const char *filename,
                                     const char *fmt, ...)
@@ -88,8 +125,7 @@ extern te_errno tapi_file_create_ta(const char *ta, const char *filename,
  * Create local file, copy it to TA, remove local file.
  * The function does the same thing as tapi_file_create_ta(),
  * but it creates local file with specified name instead of
- * using automatically generated name. This is useful when
- * you need to create files in a thread safe manner.
+ * using automatically generated name.
  *
  * @param ta            Test Agent name
  * @param lfile         pathname of the local file
@@ -112,8 +148,6 @@ extern te_errno tapi_file_create_ta_r(const char *ta,
  * @param pbuf          location for buffer allocated by the routine
  *
  * @return Status code
- *
- * @note the function is not thread-safe
  */
 extern te_errno tapi_file_read_ta(const char *ta, const char *filename,
                                   char **pbuf);
@@ -129,8 +163,6 @@ extern te_errno tapi_file_read_ta(const char *ta, const char *filename,
  * @param fmt           format string for the file content
  *
  * @return Status code
- *
- * @note the function is not thread-safe
  */
 extern te_errno tapi_file_append_ta(const char *ta, const char *filename,
                                     const char *fmt, ...)

suites/selftest/ts/rpc/unistd/append.c
@@ -24,7 +24,7 @@
 int
 main(int argc, char **argv)
 {
-    char           *rfile = NULL;
+    te_string rfile = TE_STRING_INIT;
     uint8_t        *buf = NULL;
     rcf_rpc_server *pco_iut = NULL;
     int             fd = -1;
@@ -37,17 +37,17 @@ main(int argc, char **argv)
     data = te_make_buf_by_len(BUFSIZE);
 
     TEST_STEP("Create a file with content on TA");
-    rfile = tapi_file_generate_name();
-    fd = rpc_open(pco_iut, rfile, RPC_O_WRONLY | RPC_O_CREAT, 0);
+    tapi_file_make_name(&rfile);
+    fd = rpc_open(pco_iut, rfile.ptr, RPC_O_WRONLY | RPC_O_CREAT, 0);
     CHECK_LENGTH(rpc_write_and_close(pco_iut, fd, data, BUFSIZE), BUFSIZE);
 
     TEST_STEP("Append data to the file on TA");
-    fd = rpc_open(pco_iut, rfile, RPC_O_WRONLY | RPC_O_APPEND, 0);
+    fd = rpc_open(pco_iut, rfile.ptr, RPC_O_WRONLY | RPC_O_APPEND, 0);
     CHECK_LENGTH(rpc_write_and_close(pco_iut, fd, data, BUFSIZE), BUFSIZE);
 
     TEST_STEP("Read content from the file on TA");
     buf = tapi_calloc(1, data_size);
-    fd = rpc_open(pco_iut, rfile, RPC_O_RDONLY, 0);
+    fd = rpc_open(pco_iut, rfile.ptr, RPC_O_RDONLY, 0);
 
     CHECK_LENGTH(rpc_read(pco_iut, fd, buf, data_size), data_size);
     rpc_close(pco_iut, fd);
@@ -59,10 +59,11 @@ main(int argc, char **argv)
 
 cleanup:
 
-    CLEANUP_CHECK_RC(tapi_file_ta_unlink_fmt(pco_iut->ta, "%s", rfile));
+    CLEANUP_CHECK_RC(tapi_file_ta_unlink_fmt(pco_iut->ta, "%s", rfile.ptr));
 
     free(buf);
     free(data);
+    te_string_free(&rfile);
 
     TEST_END;
 }

suites/selftest/ts/rpc/unistd/mkdir.c
@@ -67,7 +67,7 @@ remove_files(rcf_rpc_server *rpcs, const char *path)
 int
 main(int argc, char **argv)
 {
-    char           *rdir = NULL;
+    te_string rdir = TE_STRING_INIT;
     rcf_rpc_server *pco_iut = NULL;
     size_t          nfiles = 0;
 
@@ -76,11 +76,11 @@ main(int argc, char **argv)
     TEST_GET_UINT_PARAM(nfiles);
 
     TEST_STEP("Create a directory on TA");
-    rdir = strdup(tapi_file_generate_name());
-    rpc_mkdir(pco_iut, rdir, 0);
+    tapi_file_make_name(&rdir);
+    rpc_mkdir(pco_iut, rdir.ptr, 0);
 
     TEST_STEP("Create files in the directory");
-    if (create_files(nfiles, pco_iut, rdir) != nfiles)
+    if (create_files(nfiles, pco_iut, rdir.ptr) != nfiles)
         TEST_VERDICT("Files aren't created");
 
     TEST_SUCCESS;
@@ -88,14 +88,14 @@ main(int argc, char **argv)
 cleanup:
 
     TEST_STEP("Remove the directory");
-    if (remove_files(pco_iut, rdir) != 0)
+    if (remove_files(pco_iut, rdir.ptr) != 0)
         TEST_VERDICT("Directory isn't removed");
 
-    rpc_rmdir(pco_iut, rdir);
+    rpc_rmdir(pco_iut, rdir.ptr);
 
     TEST_STEP("Check if the directory is deleted");
     RPC_AWAIT_ERROR(pco_iut);
-    if (rpc_access(pco_iut, rdir, RPC_F_OK) == 0)
+    if (rpc_access(pco_iut, rdir.ptr, RPC_F_OK) == 0)
     {
         TEST_VERDICT("The removed directory still exists");
     }
@@ -104,6 +104,7 @@ cleanup:
         TEST_VERDICT("access() failed with an unexpected error: %r",
                      RPC_ERRNO(pco_iut));
     }
+    te_string_free(&rdir);
 
     TEST_END;
 }

lib/tapi_tool/tapi_wrk.c
@@ -120,27 +120,22 @@ static te_errno
 generate_script_filename(const char *ta, char **file_name)
 {
     char *working_dir = NULL;
-    char *result;
+    te_string result = TE_STRING_INIT;
     te_errno rc;
 
     rc = cfg_get_instance_fmt(NULL, &working_dir, "/agent:%s/dir:", ta);
     if (rc != 0)
     {
         ERROR("Failed to get working directory");
+        te_string_free(&result);
         return rc;
     }
 
-    rc = te_asprintf(&result, "%s/%s_%s", working_dir,
-                     tapi_file_generate_name(),
-                     TAPI_WRK_SCRIPT_FILE_NAME_SUFFIX);
+    tapi_file_make_custom_pathname(&result, working_dir,
+                                   "_" TAPI_WRK_SCRIPT_FILE_NAME_SUFFIX);
     free(working_dir);
-    if (rc < 0)
-    {
-        ERROR("Failed to create wrk script file name");
-        return TE_RC(TE_TAPI, TE_EFAIL);
-    }
 
-    *file_name = result;
+    *file_name = result.ptr;
 
     return 0;
 }

suites/selftest/ts/tapi/file/file_copy.c
@@ -24,8 +24,8 @@ main(int argc, char **argv)
 {
     rcf_rpc_server *pco_iut = NULL;
     rcf_rpc_server *pco_tst = NULL;
-    char     *src = NULL;
-    char     *dst = NULL;
+    te_string src = TE_STRING_INIT;
+    te_string dst = TE_STRING_INIT;
     int             fd = -1;
     uint8_t        *data = NULL;
     size_t          data_size = BUFSIZE;
@@ -36,30 +36,30 @@ main(int argc, char **argv)
 
     TEST_STEP("Generate a file on TA");
     data = te_make_buf_by_len(BUFSIZE);
-    src = strdup(tapi_file_generate_name());
-    fd = rpc_open(pco_iut, src, RPC_O_WRONLY | RPC_O_CREAT, 0);
+    tapi_file_make_name(&src);
+    fd = rpc_open(pco_iut, src.ptr, RPC_O_WRONLY | RPC_O_CREAT, 0);
     CHECK_LENGTH(rpc_write_and_close(pco_iut, fd, data, data_size), data_size);
 
     TEST_STEP("Copy the file from TA to TA");
-    dst = strdup(tapi_file_generate_name());
-    if (tapi_file_copy_ta(pco_iut->ta, src, pco_tst->ta, dst) != 0)
+    tapi_file_make_name(&dst);
+    if (tapi_file_copy_ta(pco_iut->ta, src.ptr, pco_tst->ta, dst.ptr) != 0)
     {
         TEST_VERDICT("tapi_file_copy_ta() failed");
     }
 
     TEST_STEP("Check if the file exists on TA");
-    file_check_exist(pco_tst, dst);
+    file_check_exist(pco_tst, dst.ptr);
 
     TEST_SUCCESS;
 
 cleanup:
 
-    CLEANUP_CHECK_RC(tapi_file_ta_unlink_fmt(pco_iut->ta, "%s", src));
-    CLEANUP_CHECK_RC(tapi_file_ta_unlink_fmt(pco_tst->ta, "%s", dst));
+    CLEANUP_CHECK_RC(tapi_file_ta_unlink_fmt(pco_iut->ta, "%s", src.ptr));
+    CLEANUP_CHECK_RC(tapi_file_ta_unlink_fmt(pco_tst->ta, "%s", dst.ptr));
 
     free(data);
-    free(src);
-    free(dst);
+    te_string_free(&src);
+    te_string_free(&dst);
 
     TEST_END;
 }

suites/selftest/ts/tapi/file/file_create_delete.c
@@ -22,34 +22,35 @@
 int
 main(int argc, char **argv)
 {
-    char           *filename = NULL;
+    te_string filename = TE_STRING_INIT;
     rcf_rpc_server *pco_iut = NULL;
 
     TEST_START;
     TEST_GET_PCO(pco_iut);
 
     TEST_STEP("Create a file on TA");
-    filename = tapi_file_generate_name();
-    if (tapi_file_create_ta(pco_iut->ta, filename, "%s", "") != 0)
+    tapi_file_make_name(&filename);
+    if (tapi_file_create_ta(pco_iut->ta, filename.ptr, "%s", "") != 0)
     {
         TEST_VERDICT("tapi_file_create_ta() failed");
     }
 
     TEST_STEP("Check if the file exists");
-    file_check_exist(pco_iut, filename);
+    file_check_exist(pco_iut, filename.ptr);
 
     TEST_STEP("Delete the file from TA");
-    if (tapi_file_ta_unlink_fmt(pco_iut->ta, "%s", filename) != 0)
+    if (tapi_file_ta_unlink_fmt(pco_iut->ta, "%s", filename.ptr) != 0)
     {
         TEST_VERDICT("tapi_file_ta_unlink_fmt() failed");
     }
 
     TEST_STEP("Check if the file is really deleted");
-    file_check_not_exist(pco_iut, filename);
+    file_check_not_exist(pco_iut, filename.ptr);
 
     TEST_SUCCESS;
 
 cleanup:
 
+    te_string_free(&filename);
     TEST_END;
 }

suites/selftest/ts/tapi/file/file_get.c
@@ -22,7 +22,7 @@
 int
 main(int argc, char **argv)
 {
-    char           *rfile = NULL;
+    te_string rfile = TE_STRING_INIT;
     rcf_rpc_server *pco_iut = NULL;
     char           *contents = NULL;
     static char     expected[] = "First Second";
@@ -32,13 +32,13 @@ main(int argc, char **argv)
 
     TEST_STEP("Create a file on TA");
 
-    rfile = strdup(tapi_file_generate_name());
-    CHECK_RC(tapi_file_append_ta(pco_iut->ta, rfile, "NULL"));
-    CHECK_RC(tapi_file_create_ta(pco_iut->ta, rfile, "First"));
-    CHECK_RC(tapi_file_append_ta(pco_iut->ta, rfile, " Second"));
+    tapi_file_make_name(&rfile);
+    CHECK_RC(tapi_file_append_ta(pco_iut->ta, rfile.ptr, "NULL"));
+    CHECK_RC(tapi_file_create_ta(pco_iut->ta, rfile.ptr, "First"));
+    CHECK_RC(tapi_file_append_ta(pco_iut->ta, rfile.ptr, " Second"));
 
     TEST_STEP("Get the file from TA");
-    CHECK_RC(tapi_file_read_ta(pco_iut->ta, rfile, &contents));
+    CHECK_RC(tapi_file_read_ta(pco_iut->ta, rfile.ptr, &contents));
 
     TEST_STEP("Check the expected contents");
     if (strcmp(contents, expected) != 0)
@@ -52,8 +52,8 @@ main(int argc, char **argv)
 cleanup:
 
     free(contents);
-    CLEANUP_CHECK_RC(tapi_file_ta_unlink_fmt(pco_iut->ta, "%s", rfile));
-    free(rfile);
+    CLEANUP_CHECK_RC(tapi_file_ta_unlink_fmt(pco_iut->ta, "%s", rfile.ptr));
+    te_string_free(&rfile);
 
     TEST_END;
 }

commit 487322c1270964bda167b2c61ae185438567b051

lib/tools: make new filename functions more similiar to old
It was observed that the new functions do not provide a smooth
upgrade path from the old, so their signature is changed to
make it easier.

OL-Redmine-Id: 12416
Fixes: 601efe4b0078 ("lib/tapi: provide more robust version of filename generators")
Suggested-by: Ivan Melnikov <ivan.melnikov@oktetlabs.ru>
Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Ivan Melnikov <ivan.melnikov@oktetlabs.ru>

lib/tapi/tapi_file.c
@@ -41,27 +41,40 @@
 #include "logger_api.h"
 #include "tapi_file.h"
 
-void
+char *
 tapi_file_make_name(te_string *dest)
 {
     static unsigned int seq = 0;
+    te_string tmp = TE_STRING_INIT;
+
+    if (dest == NULL)
+        dest = &tmp;
 
     te_string_append(dest, "te_tmp_%u_%u_%u", (unsigned)time(NULL), getpid(),
                      seq++);
+
+    return dest->ptr;
 }
 
-void
+char *
 tapi_file_make_custom_pathname(te_string *dest, const char *dirname,
                                const char *suffix)
 {
+    te_string tmp = TE_STRING_INIT;
+
+    if (dest == NULL)
+        dest = &tmp;
+
     if (dirname != NULL)
         te_string_append(dest, "%s/", dirname);
     tapi_file_make_name(dest);
     if (suffix != NULL)
         te_string_append(dest, "%s", suffix);
+
+    return dest->ptr;
 }
 
-void
+char *
 tapi_file_make_pathname(te_string *dest)
 {
     const char *te_tmp = getenv("TE_TMP");
@@ -69,7 +82,7 @@ tapi_file_make_pathname(te_string *dest)
     if (te_tmp == NULL)
         TE_FATAL_ERROR("TE_TMP is empty");
 
-    tapi_file_make_custom_pathname(dest, te_tmp, NULL);
+    return tapi_file_make_custom_pathname(dest, te_tmp, NULL);
 }
 
 char *

lib/tapi/tapi_file.h
@@ -25,26 +25,34 @@ extern "C" {
 /**
  * Generate a unique basename for a file.
  *
- * @param[out] dest  The string to hold the name.
+ * If @p dest is @c NULL, a fresh string is allocated and returned.
+ *
+ * @param[out] dest  The string to hold the name or @c NULL.
  *
  * @note The name is appended to the string contents.
  *       That way it is easier to construct derived pathnames
  *       and similiar stuff.
+ *
+ * @return the pointer to the contents of @p dest or a heap-allocated buffer
  */
-extern void tapi_file_make_name(te_string *dest);
+extern char *tapi_file_make_name(te_string *dest);
 
 /**
  * Generate a unique pathname for a file on the Engine side.
  *
- * @param[out] dest    The string to hold the name.
+ * If @p dest is @c NULL, a fresh string is allocated and returned.
+ *
+ * @param[out] dest    The string to hold the name or @c NULL.
  * @param[in]  dirname Directory component (may be @c NULL,
  *                     in which case a relative name is generated)
  * @param[in]  suffix  A custom suffix to add to a generated
  *                     pathname (may be @c NULL).
  *
  * @note The name is appended to the string contents.
+ *
+ * @return the pointer to the contents of @p dest or a heap-allocated buffer
  */
-extern void tapi_file_make_custom_pathname(te_string *dest,
+extern char *tapi_file_make_custom_pathname(te_string *dest,
                                            const char *dirname,
                                            const char *suffix);
 
@@ -52,12 +60,16 @@ extern void tapi_file_make_custom_pathname(te_string *dest,
 /**
  * Generate a unique pathname for a file on the Engine side.
  *
- * @param[out] dest  The string to hold the name.
+ * If @p dest is @c NULL, a fresh string is allocated and returned.
+ *
+ * @param[out] dest  The string to hold the name or @c NULL.
  *
  * @note The name is appended to the string contents.
  * @note @c TE_TMP env variable must be set.
+ *
+ * @return the pointer to the contents of @p dest or a heap-allocated buffer
  */
-extern void tapi_file_make_pathname(te_string *dest);
+extern char *tapi_file_make_pathname(te_string *dest);
 
 /**
  * Generate unique basename for file.

suites/selftest/ts/rpc/unistd/append.c
@@ -24,7 +24,7 @@
 int
 main(int argc, char **argv)
 {
-    te_string rfile = TE_STRING_INIT;
+    char *rfile = NULL;
     uint8_t        *buf = NULL;
     rcf_rpc_server *pco_iut = NULL;
     int             fd = -1;
@@ -37,17 +37,17 @@ main(int argc, char **argv)
     data = te_make_buf_by_len(BUFSIZE);
 
     TEST_STEP("Create a file with content on TA");
-    tapi_file_make_name(&rfile);
-    fd = rpc_open(pco_iut, rfile.ptr, RPC_O_WRONLY | RPC_O_CREAT, 0);
+    rfile = tapi_file_make_name(NULL);
+    fd = rpc_open(pco_iut, rfile, RPC_O_WRONLY | RPC_O_CREAT, 0);
     CHECK_LENGTH(rpc_write_and_close(pco_iut, fd, data, BUFSIZE), BUFSIZE);
 
     TEST_STEP("Append data to the file on TA");
-    fd = rpc_open(pco_iut, rfile.ptr, RPC_O_WRONLY | RPC_O_APPEND, 0);
+    fd = rpc_open(pco_iut, rfile, RPC_O_WRONLY | RPC_O_APPEND, 0);
     CHECK_LENGTH(rpc_write_and_close(pco_iut, fd, data, BUFSIZE), BUFSIZE);
 
     TEST_STEP("Read content from the file on TA");
     buf = tapi_calloc(1, data_size);
-    fd = rpc_open(pco_iut, rfile.ptr, RPC_O_RDONLY, 0);
+    fd = rpc_open(pco_iut, rfile, RPC_O_RDONLY, 0);
 
     CHECK_LENGTH(rpc_read(pco_iut, fd, buf, data_size), data_size);
     rpc_close(pco_iut, fd);
@@ -59,11 +59,11 @@ main(int argc, char **argv)
 
 cleanup:
 
-    CLEANUP_CHECK_RC(tapi_file_ta_unlink_fmt(pco_iut->ta, "%s", rfile.ptr));
+    CLEANUP_CHECK_RC(tapi_file_ta_unlink_fmt(pco_iut->ta, "%s", rfile));
 
     free(buf);
     free(data);
-    te_string_free(&rfile);
+    free(rfile);
 
     TEST_END;
 }

suites/selftest/ts/rpc/unistd/mkdir.c
@@ -67,7 +67,7 @@ remove_files(rcf_rpc_server *rpcs, const char *path)
 int
 main(int argc, char **argv)
 {
-    te_string rdir = TE_STRING_INIT;
+    char *rdir = NULL;
     rcf_rpc_server *pco_iut = NULL;
     size_t          nfiles = 0;
 
@@ -76,11 +76,11 @@ main(int argc, char **argv)
     TEST_GET_UINT_PARAM(nfiles);
 
     TEST_STEP("Create a directory on TA");
-    tapi_file_make_name(&rdir);
-    rpc_mkdir(pco_iut, rdir.ptr, 0);
+    rdir = tapi_file_make_name(NULL);
+    rpc_mkdir(pco_iut, rdir, 0);
 
     TEST_STEP("Create files in the directory");
-    if (create_files(nfiles, pco_iut, rdir.ptr) != nfiles)
+    if (create_files(nfiles, pco_iut, rdir) != nfiles)
         TEST_VERDICT("Files aren't created");
 
     TEST_SUCCESS;
@@ -88,14 +88,14 @@ main(int argc, char **argv)
 cleanup:
 
     TEST_STEP("Remove the directory");
-    if (remove_files(pco_iut, rdir.ptr) != 0)
+    if (remove_files(pco_iut, rdir) != 0)
         TEST_VERDICT("Directory isn't removed");
 
-    rpc_rmdir(pco_iut, rdir.ptr);
+    rpc_rmdir(pco_iut, rdir);
 
     TEST_STEP("Check if the directory is deleted");
     RPC_AWAIT_ERROR(pco_iut);
-    if (rpc_access(pco_iut, rdir.ptr, RPC_F_OK) == 0)
+    if (rpc_access(pco_iut, rdir, RPC_F_OK) == 0)
     {
         TEST_VERDICT("The removed directory still exists");
     }
@@ -104,7 +104,7 @@ cleanup:
         TEST_VERDICT("access() failed with an unexpected error: %r",
                      RPC_ERRNO(pco_iut));
     }
-    te_string_free(&rdir);
+    free(rdir);
 
     TEST_END;
 }