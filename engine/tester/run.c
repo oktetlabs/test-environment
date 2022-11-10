oktetlabs/test-environment

commit fa9d07e59cce7250c99d49b17f2edfb0ba6f3c48

Task 11659: add support for adding TRC tags via TAPI
It is necessary to add new TRC tags via TAPI using the configurator. This will
allow tests to add new tags inside the prologue that the tester will see.

engine/tester/run.c
@@ -49,6 +49,7 @@
 #include "te_alloc.h"
 #include "conf_api.h"
 #include "log_bufs.h"
+#include "te_trc.h"
 
 #include "tester_conf.h"
 #include "tester_term.h"
@@ -58,6 +59,8 @@
 #include "tester_flags.h"
 #include "tester_serial_thread.h"
 #include "te_shell_cmd.h"
+#include "tester.h"
+#include "tester_msg.h"
 
 /** Define it to enable support of timeouts in Tester */
 #undef TESTER_TIMEOUT_SUPPORT
@@ -2479,6 +2482,85 @@ run_test_script(test_script *script, const char *run_name, test_id exec_id,
     return 0;
 }
 
+#ifdef WITH_TRC
+/**
+ * This function reads TRC tags from Configurator tree and appends them to the
+ * list.
+ *
+ * @param[out]  trc_tags  List of TRC tags to append to.
+ *
+ * @return      Status code.
+ */
+static te_errno
+get_trc_tags(tqh_strings *trc_tags)
+{
+    te_errno      rc = 0;
+    te_string     tag = TE_STRING_INIT;
+    char         *tag_name = NULL;
+    char         *tag_value = NULL;
+    unsigned int  num;
+    cfg_handle   *handles = NULL;
+    cfg_val_type  type = CVT_STRING;
+    int           i;
+
+    if (trc_tags == NULL)
+        return TE_RC(TE_TAPI, TE_EINVAL);
+
+    rc = cfg_find_pattern_fmt(&num, &handles, TE_CFG_TRC_TAGS_FMT, "*");
+    if (rc != 0)
+    {
+        ERROR("%s(): Cannot get the list of TRC tags: %r", __FUNCTION__, rc);
+        return rc;
+    }
+
+    for (i = 0; i < num; i++)
+    {
+        rc = cfg_get_inst_name(handles[i], &tag_name);
+        if (rc != 0)
+        {
+            ERROR("%s(): Cannot get TRC tag name by its handle %d: %r",
+                  __FUNCTION__, handles[i], rc);
+            goto cleanup;
+        }
+
+        rc = cfg_get_instance(handles[i], &type, &tag_value);
+        if (rc != 0)
+        {
+            ERROR("%s(): Cannot get TRC tag value by its handle %d: %r",
+                  __FUNCTION__, handles[i], rc);
+            free(tag_name);
+            goto cleanup;
+        }
+
+        if (strlen(tag_value) > 0)
+            rc = te_string_append(&tag, "%s:%s", tag_name, tag_value);
+        else
+            rc = te_string_append(&tag, "%s", tag_name);
+        free(tag_value);
+        free(tag_name);
+        if (rc != 0)
+        {
+            ERROR("%s(): Failed to construct TRC tag: %r", __FUNCTION__, rc);
+            rc = TE_RC(TE_TESTER, TE_EFAULT);
+            goto cleanup;
+        }
+
+        rc = trc_add_tag(trc_tags, te_string_value(&tag));
+        te_string_reset(&tag);
+        if (rc != 0)
+        {
+            ERROR("%s(): Failed to add TRC tag: %r", __FUNCTION__, rc);
+            goto cleanup;
+        }
+    }
+
+cleanup:
+    free(handles);
+    te_string_free(&tag);
+
+    return rc;
+}
+#endif /* WITH_TRC */
 
 static tester_cfg_walk_ctl
 run_script(run_item *ri, test_script *script,
@@ -3068,6 +3150,49 @@ run_prologue_end(run_item *ri, unsigned int cfg_id_off, void *opaque)
         {
             char *reqs = NULL;
 
+#ifdef WITH_TRC
+            if (id == TE_TEST_ID_ROOT_PROLOGUE)
+            {
+                tqh_strings new_tags;
+
+                TAILQ_INIT(&new_tags);
+
+                rc = get_trc_tags(&new_tags);
+                if (rc != 0)
+                {
+                    ERROR("Get new TRC tags failed: %r", rc);
+                    EXIT("FAULT");
+                    return TESTER_CFG_WALK_FAULT;
+                }
+
+                if (!TAILQ_EMPTY(&new_tags))
+                {
+                    tqe_string *cur_tag;
+
+                    rc = tester_log_trc_tags(&new_tags);
+                    if (rc != 0)
+                    {
+                        ERROR("Logging of TRC tags failed: %r", rc);
+                        EXIT("FAULT");
+                        tq_strings_free(&new_tags, free);
+                        return TESTER_CFG_WALK_FAULT;
+                    }
+
+                    TAILQ_FOREACH(cur_tag, &new_tags, links)
+                    {
+                        rc = trc_add_tag(gctx->trc_tags, cur_tag->v);
+                        if (rc != 0)
+                        {
+                            ERROR("Update of TRC tags failed: %r", rc);
+                            EXIT("FAULT");
+                            tq_strings_free(&new_tags, free);
+                            return TESTER_CFG_WALK_FAULT;
+                        }
+                    }
+                }
+                tq_strings_free(&new_tags, free);
+            }
+#endif
             rc = cfg_get_instance_string_fmt(&reqs, "/local:/reqs:%u", id);
 
             if (rc == 0)

doc/cm/cm_local.yml
@@ -26,6 +26,14 @@
            Name: test ID
            Value: requirements expression
 
+    - oid: "/local/trc_tags"
+      access: read_create
+      type: string
+      d: |
+         Names and values of TRC tags (used by tapi_tags_add_tag()).
+           Name: TRC tag name
+           Value: TRC tag value
+
     - oid: "/local/ip4_alien"
       access: read_write
       type: address

engine/tester/tester.c
@@ -885,17 +885,8 @@ tester_log_reqs(void)
 }
 
 #ifdef WITH_TRC
-/**
- * Log TRC tags as an MI message.
- *
- * Tag names and values are split at the first colon, no additional checks
- * are performed.
- *
- * @param trc_tags          TRC tags
- *
- * @returns Status code
- */
-static te_errno
+/* See the description in tester.h */
+te_errno
 tester_log_trc_tags(const tqh_strings *trc_tags)
 {
     char         *text;

engine/tester/tester.h
@@ -52,4 +52,16 @@ typedef struct tester_global {
 
 extern tester_global tester_global_context;
 
+/**
+ * Log TRC tags as an MI message.
+ *
+ * Tag names and values are split at the first colon, no additional checks
+ * are performed.
+ *
+ * @param trc_tags          TRC tags.
+ *
+ * @return Status code.
+ */
+extern te_errno tester_log_trc_tags(const tqh_strings *trc_tags);
+
 #endif  /* __TE_TESTER_H__ */

include/tester_msg.h
@@ -17,6 +17,23 @@
 /** Type of IPC used by Tester IPC */
 #define TESTER_IPC              (TRUE)  /** Connection-oriented */
 
+
+/** Initial test ID (assigned to root package) */
+#define TE_TEST_ID_INIT 1
+
+/**
+ * Test ID of the root prologue if the prologue exists.
+ *
+ * Root package gets the initial ID. The next one is as typically root prologue.
+ */
+#define TE_TEST_ID_ROOT_PROLOGUE (TE_TEST_ID_INIT + 1)
+
+/**
+ * @def TE_CFG_TRC_TAGS_FMT
+ * Macro for working with a branch of the configurator object tree for TRC tags.
+ */
+#define TE_CFG_TRC_TAGS_FMT "/local:/trc_tags:%s"
+
 /**
  * Types of messages which tests send to Tester.
  */

lib/tapi/meson.build
@@ -51,6 +51,7 @@ headers += files(
     'tapi_sfptpd.h',
     'tapi_sh_env.h',
     'tapi_sniffer.h',
+    'tapi_tags.h',
     'tapi_test.h',
     'tapi_test_log.h',
     'tapi_test_behaviour.h',
@@ -103,6 +104,7 @@ sources += files(
     'tapi_serial_parse.c',
     'tapi_sh_env.c',
     'tapi_sniffer.c',
+    'tapi_tags.c',
     'tapi_test_behaviour.c',
     'tapi_test_run_status.c',
     'test_params.c',

lib/tapi/tapi_tags.c
@@ -1,0 +1,59 @@
+/** @file
+ * @brief API to modify TRC tags from prologues
+ *
+ * Implementation of API to modify TRC tags from prologues.
+ *
+ * Copyright (C) 2022 OKTET Labs. All rights reserved.
+ *
+ * @author Ivan Khodyrev <Ivan.Khodyrev@oktetlabs.ru>
+ */
+
+#define TE_LGR_USER "TAPI Tags"
+
+#include "te_config.h"
+
+#if HAVE_STDLIB_H
+#include <stdlib.h>
+#endif
+
+#include "te_errno.h"
+#include "conf_api.h"
+#include "tapi_test.h"
+
+/* See the description from tapi_tags.h */
+te_errno
+tapi_tags_add_tag(const char *tag, const char *value)
+{
+    te_errno rc;
+
+    if (tag == NULL)
+        return TE_RC(TE_TAPI, TE_EINVAL);
+
+    if (strcspn(tag, "/:") < strlen(tag))
+    {
+        ERROR("TRC tag name contains invalid characters");
+        return TE_RC(TE_TAPI, TE_EINVAL);
+    }
+
+    /*
+     * The check does not guarantee that it is root prologue, but it should
+     * filter out almost all misuses.
+     */
+    if (te_test_id != TE_TEST_ID_ROOT_PROLOGUE)
+    {
+        ERROR("The root prologue only may modify TRC tags: %d", te_test_id);
+        return TE_RC(TE_TAPI, TE_EPERM);
+    }
+
+    if (value == NULL)
+        value = "";
+
+    rc = cfg_add_instance_fmt(NULL, CFG_VAL(STRING, value), TE_CFG_TRC_TAGS_FMT,
+                              tag);
+    if (rc != 0)
+    {
+        ERROR("%s(): cfg_add_instance_fmt(" TE_CFG_TRC_TAGS_FMT ") failed: %r",
+              __FUNCTION__, tag, rc);
+    }
+    return rc;
+}

lib/tapi/tapi_tags.h
@@ -1,0 +1,44 @@
+/** @file
+ * @brief API to modify TRC tags from prologues
+ *
+ * @defgroup ts_tapi_tags TRC tags modification
+ * @ingroup te_ts_tapi
+ * @{
+ *
+ * Declaration of API to modify TRC tags from prologues.
+ *
+ *
+ * Copyright (C) 2022 OKTET Labs. All rights reserved.
+ *
+ * @author Ivan Khodyrev <Ivan.Khodyrev@oktetlabs.ru>
+ */
+
+#ifndef __TE_TAPI_TAGS_H__
+#define __TE_TAPI_TAGS_H__
+
+#include "te_errno.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define TE_CFG_TRC_TAGS_FMT "/local:/trc_tags:%s"
+
+/**
+ * Modify the set of defined TRC tags for tests by adding a new tag to the
+ * configurator database. This function should be used to pass the TRC tag from
+ * the root prologue only to the tester.
+ *
+ * @param tag       Tag name.
+ * @param value     Tag value for TRC tags expressions (can be @c NULL).
+ *
+ * @return          Status code.
+ */
+extern te_errno tapi_tags_add_tag(const char *tag, const char *value);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+#endif /* !__TE_TAPI_TAGS_H__ */
+
+/**@} <!-- END ts_tapi_tags --> */

commit 525e33d3883d4d53bf07cc1a94c1e3b2a12061f6

engine/tester: make array of TRC tags mutable
A set of TRC tags may change in the course of a Tester run.
The input set of TRC tags will not be changed.

Fixes: fa9d07e59cce ("Task 11659: add support for adding TRC tags via TAPI")
OL-Redmine-Id: 12184
Signed-off-by: Ivan Khodyrev <ivan.khodyrev@oktetlabs.ru>
Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>

engine/tester/run.c
@@ -207,7 +207,7 @@ typedef struct tester_run_data {
 
 #if WITH_TRC
     const te_trc_db            *trc_db;     /**< TRC database handle */
-    const tqh_strings          *trc_tags;   /**< TRC tags */
+    tqh_strings                 trc_tags;   /**< TRC tags */
 #endif
 
     SLIST_HEAD(, tester_ctx)    ctxs;       /**< Stack of contexts */
@@ -3215,7 +3215,7 @@ run_prologue_end(run_item *ri, unsigned int cfg_id_off, void *opaque)
 
                     TAILQ_FOREACH(cur_tag, &new_tags, links)
                     {
-                        rc = trc_add_tag(gctx->trc_tags, cur_tag->v);
+                        rc = trc_add_tag(&gctx->trc_tags, cur_tag->v);
                         if (rc != 0)
                         {
                             ERROR("Update of TRC tags failed: %r", rc);
@@ -3908,7 +3908,7 @@ run_iter_start(run_item *ri, unsigned int cfg_id_off, unsigned int flags,
         ctx->do_trc_walker = TRUE;
 
         ctx->current_result.exp_result =
-            trc_db_walker_get_exp_result(ctx->trc_walker, gctx->trc_tags);
+            trc_db_walker_get_exp_result(ctx->trc_walker, &gctx->trc_tags);
     }
     else
     {
@@ -4635,7 +4635,10 @@ tester_run(testing_scenario   *scenario,
     data.direction = TESTING_FORWARD;
 #if WITH_TRC
     data.trc_db = trc_db;
-    data.trc_tags = trc_tags;
+    TAILQ_INIT(&data.trc_tags);
+    rc = tq_strings_copy(&data.trc_tags, trc_tags);
+    if (rc != 0)
+        TE_FATAL_ERROR("Failed to copy the list of trc_tags: %r", rc);
 #else
     UNUSED(trc_db);
     UNUSED(trc_tags);
@@ -4770,6 +4773,9 @@ tester_run(testing_scenario   *scenario,
 
     tester_run_destroy_ctx(&data);
     scenario_free(&data.fixed_scen);
+#if WITH_TRC
+    tq_strings_free(&data.trc_tags, free);
+#endif
 
     rc2 = tester_test_msg_listener_stop(&data.vl);
     if (rc2 != 0)