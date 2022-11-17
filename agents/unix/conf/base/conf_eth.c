oktetlabs/test-environment

commit 3087b883d1fbddbad8408c6d6eaac9de531d4b99

agents/unix: support private flags for network interfaces
RSS tests need to disable flow-director-atr private flag for
Intel X710 NICs to check RSS hash indirection for TCP connections.

OL-Redmine-Id: 12496
Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>

agents/unix/conf/base/conf_eth.c
@@ -47,6 +47,7 @@
 #include "te_rpc_sys_socket.h"
 
 #include "conf_oid.h"
+#include "conf_ethtool.h"
 
 #ifdef HAVE_LINUX_ETHTOOL_H
 #define E_BITS_PER_DWORD ((unsigned)(sizeof(uint32_t) * CHAR_BIT))
@@ -547,6 +548,117 @@ eth_feature_readonly_get(unsigned int gid,
     value[1] = '\0';
 
     return 0;
+}
+
+/* list all private flags */
+static te_errno
+eth_priv_flags_list(unsigned int gid, const char *oid_str,
+                    const char *sub_id, char **list_out,
+                    const char *if_name)
+{
+    UNUSED(oid_str);
+    UNUSED(sub_id);
+
+    return ta_ethtool_get_strings_list(gid, if_name,
+                                       ETH_SS_PRIV_FLAGS, list_out);
+}
+
+/* common code for getting and setting private flag */
+static te_errno
+eth_priv_flags_common(unsigned int gid, const char *oid_str,
+                      const char *if_name, const char *flags_name,
+                      const char *flag_name, unsigned int *idx,
+                      uint32_t **data)
+{
+    te_errno rc;
+    struct ethtool_value *pflags = NULL;
+
+    UNUSED(oid_str);
+    UNUSED(flags_name);
+
+    rc = ta_ethtool_get_string_idx(gid, if_name, ETH_SS_PRIV_FLAGS,
+                                   flag_name, idx);
+    if (rc != 0)
+        return rc;
+
+    rc = get_ethtool_value(if_name, gid, TA_ETHTOOL_PFLAGS,
+                           (void **)&pflags);
+    if (rc != 0)
+        return rc;
+
+    *data = &pflags->data;
+    return 0;
+}
+
+/* get state of private flag */
+static te_errno
+eth_priv_flags_get(unsigned int gid, const char *oid_str,
+                   char *value, const char *if_name,
+                   const char *flags_name, const char *flag_name)
+{
+    te_errno rc;
+    unsigned int idx;
+    uint32_t *data;
+
+    te_string str_val = TE_STRING_EXT_BUF_INIT(value, RCF_MAX_VAL);
+
+    rc = eth_priv_flags_common(gid, oid_str, if_name, flags_name,
+                               flag_name, &idx, &data);
+    if (rc != 0)
+        return rc;
+
+    return te_string_append_chk(&str_val, "%u",
+                                (*data & (1 << idx)) ? 1 : 0);
+}
+
+/* set state of private flag */
+static te_errno
+eth_priv_flags_set(unsigned int gid, const char *oid_str,
+                   const char *value, const char *if_name,
+                   const char *flags_name, const char *flag_name)
+{
+    te_errno rc;
+    unsigned int idx;
+    uint32_t *data;
+    unsigned int int_val;
+
+    rc = te_strtoui(value, 10, &int_val);
+    if (rc != 0 || (int_val != 0 && int_val != 1))
+    {
+        ERROR("%s(): cannot parse private flag value '%s'", __FUNCTION__,
+              value);
+        return TE_RC(TE_TA_UNIX, TE_EINVAL);
+    }
+
+    rc = eth_priv_flags_common(gid, oid_str, if_name, flags_name,
+                               flag_name, &idx, &data);
+    if (rc != 0)
+        return rc;
+
+    if (idx > sizeof(uint32_t) * 8)
+    {
+        ERROR("%s(): index of private flag '%s' is too big (%u), cannot "
+              "access it", __FUNCTION__, flag_name, idx);
+        return TE_RC(TE_TA_UNIX, TE_EINVAL);
+    }
+
+    if (int_val == 1)
+        *data = *data | (1 << idx);
+    else
+        *data = *data & ~(1 << idx);
+
+    return 0;
+}
+
+/* commit private flags */
+static te_errno
+eth_priv_flags_commit(unsigned int gid, const cfg_oid *oid)
+{
+    const char *if_name;
+
+    if_name = CFG_OID_GET_INST_NAME(oid, 2);
+
+    return commit_ethtool_value(if_name, gid, TA_ETHTOOL_PFLAGS);
 }
 
 /**
@@ -1169,6 +1281,23 @@ static rcf_pch_cfg_object eth_feature = {
     (rcf_ch_cfg_commit)eth_feature_commit, NULL, NULL
 };
 
+static rcf_pch_cfg_object eth_priv_flags;
+
+static rcf_pch_cfg_object eth_priv_flag = {
+    .sub_id = "flag",
+    .get = (rcf_ch_cfg_get)eth_priv_flags_get,
+    .set = (rcf_ch_cfg_set)eth_priv_flags_set,
+    .list = (rcf_ch_cfg_list)eth_priv_flags_list,
+    .commit_parent = &eth_priv_flags
+};
+
+static rcf_pch_cfg_object eth_priv_flags = {
+    .sub_id = "private",
+    .son = &eth_priv_flag,
+    .brother = &eth_feature,
+    .commit = (rcf_ch_cfg_commit)eth_priv_flags_commit
+};
+
 RCF_PCH_CFG_NODE_RO(eth_ring_tx_max, "max", NULL, NULL,
                     eth_ring_tx_max_get);
 
@@ -1185,7 +1314,7 @@ RCF_PCH_CFG_NODE_RW(eth_ring_rx_current, "current", NULL, &eth_ring_rx_max,
 
 RCF_PCH_CFG_NODE_NA(eth_ring_rx, "rx", &eth_ring_rx_current, &eth_ring_tx);
 
-RCF_PCH_CFG_NODE_NA(eth_ring, "ring", &eth_ring_rx, &eth_feature);
+RCF_PCH_CFG_NODE_NA(eth_ring, "ring", &eth_ring_rx, &eth_priv_flags);
 
 RCF_PCH_CFG_NODE_RO(eth_channels_tx_maximum, "maximum",
                     NULL, NULL, eth_channels_get);

agents/unix/conf/base/conf_ethtool.c
@@ -221,6 +221,8 @@ ethtool_cmd2str(int cmd)
 #ifdef ETHTOOL_SRSSH
         CASE_CMD(ETHTOOL_SRSSH);
 #endif
+        CASE_CMD(ETHTOOL_GPFLAGS);
+        CASE_CMD(ETHTOOL_SPFLAGS);
     }
 
     return "<UNKNOWN>";
@@ -521,6 +523,12 @@ get_ethtool_value(const char *if_name, unsigned int gid,
             val_size = sizeof(ta_ethtool_lsets);
             break;
 
+        case TA_ETHTOOL_PFLAGS:
+            obj_type = TA_OBJ_TYPE_IF_PFLAGS;
+            val_size = sizeof(struct ethtool_value);
+            native_cmd = ETHTOOL_GPFLAGS;
+            break;
+
         default:
             ERROR("%s(): unknown ethtool command %d", __FUNCTION__, cmd);
             return TE_RC(TE_TA_UNIX, TE_EINVAL);
@@ -594,6 +602,11 @@ commit_ethtool_value(const char *if_name, unsigned int gid,
 
         case TA_ETHTOOL_LINKSETTINGS:
             obj_type = TA_OBJ_TYPE_IF_LINK_SETS;
+            break;
+
+        case TA_ETHTOOL_PFLAGS:
+            obj_type = TA_OBJ_TYPE_IF_PFLAGS;
+            native_cmd = ETHTOOL_SPFLAGS;
             break;
 
         default:
@@ -714,6 +727,60 @@ ta_ethtool_get_strings(unsigned int gid, const char *if_name,
 
     *strs = result;
     return rc;
+}
+
+/* see description in conf_ethtool.h */
+te_errno
+ta_ethtool_get_strings_list(unsigned int gid, const char *if_name,
+                            unsigned int set_id, char **list_out)
+{
+    const struct ta_ethtool_strings *sset = NULL;
+    unsigned int i;
+    te_errno rc;
+    te_string str = TE_STRING_INIT;
+
+    rc = ta_ethtool_get_strings(gid, if_name, set_id, &sset);
+    if (rc != 0)
+        return rc;
+
+    for (i = 0; i < sset->num; i++)
+    {
+        rc = te_string_append_chk(&str, "%s ", sset->strings[i]);
+        if (rc != 0)
+        {
+            te_string_free(&str);
+            return rc;
+        }
+    }
+
+    *list_out = str.ptr;
+    return 0;
+}
+
+/* see description in conf_ethtool.h */
+te_errno
+ta_ethtool_get_string_idx(unsigned int gid, const char *if_name,
+                          unsigned int set_id, const char *target,
+                          unsigned int *idx)
+{
+    const ta_ethtool_strings *sset = NULL;
+    unsigned int i;
+    te_errno rc;
+
+    rc = ta_ethtool_get_strings(gid, if_name, set_id, &sset);
+    if (rc != 0)
+        return rc;
+
+    for (i = 0; i < sset->num; i++)
+    {
+        if (strcmp(target, sset->strings[i]) == 0)
+        {
+            *idx = i;
+            return 0;
+        }
+    }
+
+    return TE_RC(TE_TA_UNIX, TE_ENOENT);
 }
 
 #ifdef ETHTOOL_GRSSH

agents/unix/conf/base/conf_ethtool.h
@@ -30,6 +30,7 @@ typedef enum ta_ethtool_cmd {
     TA_ETHTOOL_PAUSEPARAM,    /**< ETHTOOL_[G|S]PAUSEPARAM */
     TA_ETHTOOL_LINKSETTINGS,  /**< ETHTOOL_[G|S]LINKSETTINGS if supported,
                                    or ETHTOOL_[G|S]SET otherwise */
+    TA_ETHTOOL_PFLAGS,        /**< ETHTOOL_[G|S]PFLAGS */
 } ta_ethtool_cmd;
 
 /**
@@ -376,6 +377,40 @@ extern te_errno ta_ethtool_get_strings(unsigned int gid,
                                        const char *if_name,
                                        unsigned int set_id,
                                        const ta_ethtool_strings **strs);
+
+/*
+ * Same as ta_ethtool_get_strings(), but obtained set of strings is
+ * returned as a space-separated list suitable for list() method of
+ * configuration object.
+ *
+ * @param gid         Request group ID
+ * @param if_name     Interface name
+ * @param set_id      String set ID
+ * @param list_out    Where to save pointer to the list
+ *
+ * @return Status code.
+ */
+extern te_errno ta_ethtool_get_strings_list(unsigned int gid,
+                                            const char *if_name,
+                                            unsigned int set_id,
+                                            char **list_out);
+
+/**
+ * Get index of a given string in a set of strings.
+ *
+ * @param gid         Request group ID
+ * @param if_name     Interface name
+ * @param set_id      String set ID
+ * @param target      Target string
+ * @param idx         Where to save index of the target string
+ *
+ * @return Status code.
+ */
+extern te_errno ta_ethtool_get_string_idx(unsigned int gid,
+                                          const char *if_name,
+                                          unsigned int set_id,
+                                          const char *target,
+                                          unsigned int *idx);
 
 #ifdef ETHTOOL_GRSSH
 /**

doc/cm/cm_base.yml
@@ -595,6 +595,23 @@
          Value: 0 - feature can be changed
                 1 - feature is read-only
 
+    - oid: "/agent/interface/private"
+      access: read_only
+      type: none
+      d: |
+         Network interface private flags
+         Name: none
+         Value: none
+
+    - oid: "/agent/interface/private/flag"
+      access: read_write
+      type: bool
+      d: |
+         Network interface private flag
+         Name:  Flag name
+         Value: 0 - disabled
+                1 - enabled
+
     - oid: "/agent/interface/channels"
       access: read_only
       type: none

lib/rcfpch/rcf_pch_ta_cfg.h
@@ -313,6 +313,12 @@ extern void ta_cfg_obj_log(const ta_cfg_obj_t *obj);
  */
 #define TA_OBJ_TYPE_IF_RSSH "if_rssh"
 
+/**
+ * Name for the object type containing private flags for network
+ * interface.
+ */
+#define TA_OBJ_TYPE_IF_PFLAGS "if_pflags"
+
 /** System-independent route types */
 typedef enum ta_route_type {
     TA_RT_TYPE_UNSPECIFIED,

commit 5c9113c771911fd62649c9aac87fb28e839e487b

agent/unix: avoid unnecessary error messages
Return empty list instead of error if interface private flags are
not supported to avoid printing error messages in log.

Fixes: 3087b883d1fb ("agents/unix: support private flags for network interfaces")
OL-Redmine-Id: 12496
Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
Reviewed-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

agents/unix/conf/base/conf_eth.c
@@ -556,11 +556,24 @@ eth_priv_flags_list(unsigned int gid, const char *oid_str,
                     const char *sub_id, char **list_out,
                     const char *if_name)
 {
+    te_errno rc;
+
     UNUSED(oid_str);
     UNUSED(sub_id);
 
-    return ta_ethtool_get_strings_list(gid, if_name,
-                                       ETH_SS_PRIV_FLAGS, list_out);
+    rc = ta_ethtool_get_strings_list(gid, if_name,
+                                     ETH_SS_PRIV_FLAGS, list_out);
+    /*
+     * If private flags are not supported, let Configurator
+     * think they are not present to avoid error messages.
+     */
+    if (rc == TE_RC(TE_TA_UNIX, TE_EOPNOTSUPP))
+    {
+        *list_out = NULL;
+        rc = 0;
+    }
+
+    return rc;
 }
 
 /* common code for getting and setting private flag */