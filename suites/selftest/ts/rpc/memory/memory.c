oktetlabs/test-environment

commit 529e57f7c1a9c7e5274ae4b29623580b68112146

lib/rpcserve: add posix_memalign() RPC call
It is required to remove obsolete memalign() from RPC server open.

OL-Redmine-Id: 12186
Signed-off-by: Nikolai Kosovskii <nikolai.kosovskii@oktetlabs.ru>

suites/selftest/ts/rpc/memory/memory.c
@@ -107,6 +107,35 @@ main(int argc, char **argv)
     }
     rpc_free(pco_iut, ptr);
 
+    TEST_STEP("Allocate memory on TA using posix_memalign several steps");
+    alignment = sizeof(void *);
+    for (i = 0; i++ < number_of_alignments; alignment *= 2)
+    {
+        for (size = size_interval_begin; size < size_interval_end; size ++)
+        {
+            RPC_AWAIT_ERROR(pco_iut);
+
+            rc = rpc_posix_memalign(pco_iut, &ptr, alignment, size);
+            if (RPC_ERRNO(pco_iut) != TE_RC(TE_TA_UNIX, 0))
+                TEST_VERDICT("rpc_malloc_misaligned() failed via errno");
+            if (rc != TE_RC(TE_TA_UNIX, 0))
+                TEST_VERDICT("rpc_malloc_misaligned() failed via rc");
+            rpc_free(pco_iut, ptr);
+        }
+    }
+
+    TEST_STEP("Try to allocate memory on TA using posix_memalign "
+              "with wrong alignment");
+    alignment = sizeof(void *) + 5; /* to be not power of 2 */
+    size = size_interval_begin;
+    RPC_AWAIT_ERROR(pco_iut);
+
+    rc = rpc_posix_memalign(pco_iut, &ptr, alignment, size);
+
+    if (rc != TE_RC(TE_TA_UNIX, EINVAL))
+        TEST_VERDICT("rpc_malloc_misaligned() should fail with another error");
+    rpc_free(pco_iut, ptr);
+
     TEST_SUCCESS;
 
 cleanup:

lib/rpcserver/tarpc_imp.c
@@ -10312,6 +10312,21 @@ TARPC_FUNC_DYNAMIC_UNSAFE(memalign, {},
 }
 )
 
+
+/*-------------- posix_memalign() ------------------------------*/
+
+TARPC_FUNC(posix_memalign, {},
+{
+    void *buf;
+    int rc;
+
+    rc = func_ret_ptr(&buf, in->alignment, in->size);
+    out->retval = TE_RC(TE_TA_UNIX, rc);
+    if (rc == 0)
+        out->ptr = rcf_pch_mem_alloc(buf);
+}
+)
+
 /*-------------- mmap() ------------------------------*/
 
 TARPC_FUNC(mmap, {},

lib/rpcxdr/tarpc_base.x.m4
@@ -2185,6 +2185,21 @@ struct tarpc_malloc_misaligned_out {
 	tarpc_ptr retval;
 };
 
+/* posix_memalign */
+struct tarpc_posix_memalign_in {
+    struct tarpc_in_arg common;
+    tarpc_size_t alignment;     /**< Alignment of a block */
+    tarpc_size_t size;          /**< Bytes to allocate */
+};
+typedef struct tarpc_posix_memalign_in tarpc_posix_memalign_in;
+
+struct tarpc_posix_memalign_out {
+    struct tarpc_out_arg common;
+    int                  retval;/**< Code of error */
+    tarpc_ptr            ptr;   /**< A pointer in the TA address space */
+};
+typedef struct tarpc_posix_memalign_out tarpc_posix_memalign_out;
+
 /* raw2integer */
 struct tarpc_raw2integer_in {
     struct tarpc_in_arg     common;
@@ -5804,5 +5819,6 @@ program tarpc
 
         RPC_DEF(remove_dir_with_files)
         RPC_DEF(malloc_misaligned)
+        RPC_DEF(posix_memalign)
     } = 1;
 } = 1;

lib/tapi_rpc/tapi_rpc_unistd.h
@@ -1211,6 +1211,19 @@ extern rpc_ptr rpc_memalign(rcf_rpc_server *rpcs,
                             size_t alignment,
                             size_t size);
 
+/**
+ * Allocate a buffer of specified size in the TA address space
+ * aligned at a specified boundary.
+ *
+ * @param rpcs          RPC server handle
+ * @param ptr           allocated buffer identifier
+ * @param alignment     alignment of the buffer
+ * @param size          size of the buffer to be allocated
+ *
+ * @return  @c 0 on success or code of the error on failure
+ */
+extern int rpc_posix_memalign(rcf_rpc_server *rpcs, rpc_ptr *ptr,
+                                  size_t alignment, size_t size);
 
 /**
  * Free memory allocated on RPC server in cleanup part of the test.

lib/tapi_rpc/unistd.c
@@ -2608,6 +2608,35 @@ rpc_memalign(rcf_rpc_server *rpcs, size_t alignment, size_t size)
     RETVAL_RPC_PTR(memalign, (rpc_ptr)out.retval);
 }
 
+/* See description in 'tapi_rpc_unistd.h' */
+extern int
+rpc_posix_memalign(rcf_rpc_server *rpcs, rpc_ptr *ptr,
+                   size_t alignment, size_t size)
+{
+    tarpc_posix_memalign_in in;
+    tarpc_posix_memalign_out out;
+
+    memset(&in, 0, sizeof(in));
+    memset(&out, 0, sizeof(out));
+
+    if (rpcs == NULL)
+    {
+        ERROR("%s(): Invalid RPC server handle", __FUNCTION__);
+        RETVAL_RPC_PTR(posix_memalign, 0);
+    }
+
+    in.alignment = alignment;
+    in.size = size;
+
+    rcf_rpc_call(rpcs, "posix_memalign", &in, &out);
+    *ptr = out.ptr;
+
+    TAPI_RPC_LOG(rpcs, posix_memalign, "%p{%u} ""%" TE_PRINTF_SIZE_T "u "
+                 "%" TE_PRINTF_SIZE_T "u", "%u",
+                 ptr, ptr == NULL ? 0 : *ptr, alignment, size, out.retval);
+    RETVAL_RPC_PTR(posix_memalign, out.retval);
+}
+
 int
 rpc_setrlimit(rcf_rpc_server *rpcs,
               int resource, const tarpc_rlimit *rlim)

commit 9edb3c91e52f322d2d15831c065f1dbe1496bc30

suites/selftest: correct function name in VERDICT
Fixes: 529e57f7c1a9 ("lib/rpcserve: add posix_memalign() RPC call")
OL-Redmine-Id: 12186
Signed-off-by: Nikolai Kosovskii <nikolai.kosovskii@oktetlabs.ru>
Reviewed-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>

suites/selftest/ts/rpc/memory/memory.c
@@ -117,9 +117,9 @@ main(int argc, char **argv)
 
             rc = rpc_posix_memalign(pco_iut, &ptr, alignment, size);
             if (RPC_ERRNO(pco_iut) != TE_RC(TE_TA_UNIX, 0))
-                TEST_VERDICT("rpc_malloc_misaligned() failed via errno");
+                TEST_VERDICT("rpc_posix_memalign() failed via errno");
             if (rc != TE_RC(TE_TA_UNIX, 0))
-                TEST_VERDICT("rpc_malloc_misaligned() failed via rc");
+                TEST_VERDICT("rpc_posix_memalign() failed via rc");
             rpc_free(pco_iut, ptr);
         }
     }
@@ -133,7 +133,7 @@ main(int argc, char **argv)
     rc = rpc_posix_memalign(pco_iut, &ptr, alignment, size);
 
     if (rc != TE_RC(TE_TA_UNIX, EINVAL))
-        TEST_VERDICT("rpc_malloc_misaligned() should fail with another error");
+        TEST_VERDICT("rpc_posix_memalign() should fail with another error");
     rpc_free(pco_iut, ptr);
 
     TEST_SUCCESS;