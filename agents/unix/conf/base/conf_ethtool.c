oktetlabs/test-environment

commit e1450671f4553f824da934b38eb35e80d6e13e30

agents/unix: implement API for Rx classification rules
This API will be used to add Rx classification rules in configuration
tree for Unix TA.

OL-Redmine-Id: 11880
Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

agents/unix/conf/meson.build
@@ -275,3 +275,18 @@ if unix_daemons.length() != 0
         )
     endif
 endif
+
+flow_spec_types = [
+    'struct ethtool_tcpip4_spec',
+    'struct ethtool_ah_espip4_spec',
+    'struct ethtool_usrip4_spec',
+    'struct ethtool_tcpip6_spec',
+    'struct ethtool_ah_espip6_spec',
+    'struct ethtool_usrip6_spec',
+]
+
+foreach t : flow_spec_types
+    if cc.has_type(t, prefix: '#include <linux/ethtool.h>')
+        c_args += [ '-DHAVE_' + t.to_upper().underscorify() ]
+    endif
+endforeach

agents/unix/conf/base/conf_ethtool.h
@@ -16,6 +16,13 @@
 #include "config.h"
 #endif
 
+#ifdef HAVE_SYS_SOCKET_H
+#include <sys/socket.h>
+#endif
+#ifdef HAVE_NETINET_IN_H
+#include <netinet/in.h>
+#endif
+
 #include "te_defs.h"
 #include "te_errno.h"
 #include "te_string.h"
@@ -460,5 +467,149 @@ extern te_errno ta_ethtool_commit_rssh(unsigned int gid,
 #endif
 
 #endif
+
+#ifdef ETHTOOL_GRXCLSRLALL
+
+/** Information about Rx rules */
+typedef struct ta_ethtool_rx_cls_rules {
+    unsigned int table_size;  /**< Size of rules table */
+    unsigned int rule_cnt;    /**< Current number of rules */
+    te_bool spec_loc_flag;    /**< If @c TRUE, special insert locations
+                                   for rules are supported */
+    unsigned int *locs;       /**< Locations of existing rules in rules
+                                   table */
+} ta_ethtool_rx_cls_rules;
+
+/* Maximum size of level 3 (IP) address */
+#define TA_MAX_L3_ADDR sizeof(struct in6_addr)
+
+/* Fields defining network flow for Rx classification rule */
+typedef struct ta_ethtool_rx_cls_rule_fields {
+    /** Source MAC address */
+    uint8_t src_mac[ETH_ALEN];
+    /** Destination MAC address */
+    uint8_t dst_mac[ETH_ALEN];
+    /** EtherType */
+    uint16_t ether_type;
+    /** VLAN tag protocol identifier */
+    uint16_t vlan_tpid;
+    /** VLAN tag control information */
+    uint16_t vlan_tci;
+    /** First number of user-defined data */
+    uint32_t data0;
+    /** Second number of user-defined data */
+    uint32_t data1;
+
+    /** Level 3 (IP) source address */
+    uint8_t src_l3_addr[TA_MAX_L3_ADDR];
+    /** Level 3 (IP) destination address */
+    uint8_t dst_l3_addr[TA_MAX_L3_ADDR];
+    /** Source port (TCP, UDP) */
+    uint16_t src_port;
+    /** Destination port (TCP, UDP) */
+    uint16_t dst_port;
+    /** IPv4 TOS or IPv6 traffic class */
+    uint8_t tos_or_tclass;
+    /** Security Parameters Index */
+    uint32_t spi;
+    /** First four bytes of L4 (transport) header */
+    uint32_t l4_4_bytes;
+    /** Transport protocol number */
+    uint8_t l4_proto;
+} ta_ethtool_rx_cls_rule_fields;
+
+/** Description of Rx classification rule */
+typedef struct ta_ethtool_rx_cls_rule {
+    /** Location in rules table */
+    uint32_t location;
+
+    /** Network flow type (TCP_V4_FLOW, ETHER_FLOW, etc) */
+    uint32_t flow_type;
+    /** RSS context (negative means not set) */
+    intmax_t rss_context;
+    /** Rx queue */
+    uintmax_t rx_queue;
+
+    /** Values of network flow fields */
+    ta_ethtool_rx_cls_rule_fields field_values;
+    /** Masks of network flow fields */
+    ta_ethtool_rx_cls_rule_fields field_masks;
+} ta_ethtool_rx_cls_rule;
+
+/**
+ * Get information about Rx rules.
+ *
+ * @param gid         Request group ID
+ * @param if_name     Interface name
+ * @param rules_data  Where to save pointer to ta_ethtool_rx_cls_rules
+ *
+ * @return Status code.
+ */
+extern te_errno ta_ethtool_get_rx_cls_rules(
+                                      unsigned int gid, const char *if_name,
+                                      ta_ethtool_rx_cls_rules **rules_data);
+
+/**
+ * Get information about specific Rx classification rule.
+ *
+ * @param gid             Request group ID
+ * @param if_name         Interface name
+ * @param location        Rule location
+ * @param rule_out        Where to save pointer to rule data
+ *
+ * @return Status code.
+ */
+extern te_errno ta_ethtool_get_rx_cls_rule(
+                                      unsigned int gid, const char *if_name,
+                                      unsigned int location,
+                                      ta_ethtool_rx_cls_rule **rule_out);
+
+/**
+ * Add a new Rx classification rule (should be committed later).
+ *
+ * @param gid             Request group ID
+ * @param if_name         Interface name
+ * @param location        Rule location
+ * @param rule_out        Where to save pointer to rule data
+ *
+ * @return Status code.
+ */
+extern te_errno ta_ethtool_add_rx_cls_rule(
+                                      unsigned int gid, const char *if_name,
+                                      unsigned int location,
+                                      ta_ethtool_rx_cls_rule **rule_out);
+
+/**
+ * Commit changes made to existing Rx classification rule or insert a new
+ * rule.
+ *
+ * @param gid             Request group ID
+ * @param if_name         Interface name
+ * @param location        Rule location (may be one of special rule
+ *                        insert location values like @c RX_CLS_LOC_ANY)
+ * @param ret_location    Resulting rule location (may be different from
+ *                        requested @p location if special value was
+ *                        passed)
+ *
+ * @return Status code.
+ */
+extern te_errno ta_ethtool_commit_rx_cls_rule(
+                                            unsigned int gid,
+                                            const char *if_name,
+                                            unsigned int location,
+                                            unsigned int *ret_location);
+
+/**
+ * Remove existing Rx classification rule.
+ *
+ * @param if_name         Interface name
+ * @param location        Rule location
+ *
+ * @return Status code.
+ */
+extern te_errno ta_ethtool_del_rx_cls_rule(const char *if_name,
+                                           unsigned int location);
+
+#endif /* ifdef ETHTOOL_GRXCLSRLALL */
 
 #endif /* !__TE_AGENTS_UNIX_CONF_BASE_CONF_ETHTOOL_H_ */

lib/rcfpch/rcf_pch_ta_cfg.h
@@ -319,6 +319,17 @@ extern void ta_cfg_obj_log(const ta_cfg_obj_t *obj);
  */
 #define TA_OBJ_TYPE_IF_PFLAGS "if_pflags"
 
+/*
+ * Name for the object type containing list of RX classification
+ * rules.
+ */
+#define TA_OBJ_TYPE_IF_RX_CLS_RULES "if_rx_cls_rules"
+
+/**
+ * Name for the object type containing RX classification rule.
+ */
+#define TA_OBJ_TYPE_IF_RX_CLS_RULE "if_rx_cls_rule"
+
 /** System-independent route types */
 typedef enum ta_route_type {
     TA_RT_TYPE_UNSPECIFIED,

commit 2f8b083926e334e56840431a7d78f54205b4b47b

agents/unix: check whether a structure field is present
ethtool_rxnfc structure has no rss_context member on older distributions
such as Debian 9. TE should check for its presence to avoid breaking
build.

Fixes: e1450671f455 ("agents/unix: implement API for Rx classification rules")
OL-Redmine-Id: 11880
Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>

agents/unix/conf/base/conf_ethtool.c
@@ -1933,9 +1933,11 @@ rule_h2ta(const struct ethtool_rxnfc *h_rule,
 
     ta_rule->rx_queue = h_rule->fs.ring_cookie;
 
+#ifdef HAVE_STRUCT_ETHTOOL_RXNFC_RSS_CONTEXT
     if (h_rule->fs.flow_type & FLOW_RSS)
         ta_rule->rss_context = h_rule->rss_context;
     else
+#endif
         ta_rule->rss_context = -1;
 
     rc = rule_fields_h2ta(ta_rule->flow_type, &h_rule->fs.h_u,
@@ -2224,8 +2226,14 @@ rule_ta2h(const ta_ethtool_rx_cls_rule *ta_rule,
 
     if (ta_rule->rss_context >= 0)
     {
+#ifdef HAVE_STRUCT_ETHTOOL_RXNFC_RSS_CONTEXT
         h_rule->fs.flow_type |= FLOW_RSS;
         h_rule->rss_context = ta_rule->rss_context;
+#else
+        ERROR("%s(): setting RSS context for Rx rules is not supported",
+              __FUNCTION__);
+        return TE_RC(TE_TA_UNIX, TE_EOPNOTSUPP);
+#endif
     }
 
     rc = rule_fields_ta2h(ta_rule->flow_type, FALSE, &ta_rule->field_values,

agents/unix/conf/meson.build
@@ -291,3 +291,9 @@ foreach t : flow_spec_types
         c_args += [ '-DHAVE_' + t.to_upper().underscorify() ]
     endif
 endforeach
+
+t = 'struct ethtool_rxnfc'
+m = 'rss_context'
+if cc.has_member(t, m, prefix: '#include <linux/ethtool.h>')
+    c_args += [ '-DHAVE_' + (t + '_' + m).to_upper().underscorify() ]
+endif