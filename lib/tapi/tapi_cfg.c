oktetlabs/test-environment

commit 67bfc8d4bdc038592657a7397382e512b35b850a

agents/unix: support unreachable, prohibit and throw routes
Add support for unusual routes such as unreachable, prohibit and throw.
This feature will also be useful to block packets in various ways
from one TA to another.

OL-Redmine-Id: 12539
Signed-off-by: Daniil Byshenko <daniil.byshenko@oktetlabs.ru>
Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>

lib/tapi/tapi_cfg.c
@@ -681,10 +681,11 @@ tapi_cfg_add_route(const char *ta, int addr_family,
 }
 
 static int
-tapi_cfg_add_blackhole(const char *ta, int addr_family,
+tapi_cfg_add_blocking_routes(const char *ta, int addr_family,
                        const void *dst_addr, int prefix,
-                       cfg_handle *handle)
+                       cfg_handle *handle, const char *route_type)
 {
+    te_errno    rc;
     char        dst_addr_str[INET6_ADDRSTRLEN];
     int         netaddr_size = te_netaddr_get_size(addr_family);
 
@@ -709,9 +710,47 @@ tapi_cfg_add_blackhole(const char *ta, int addr_family,
               "into a character string", __FUNCTION__);
         return TE_OS_RC(TE_TAPI, errno);
     }
-    return cfg_add_instance_fmt(handle, CFG_VAL(NONE, NULL),
-                                "/agent:%s/blackhole:%s|%d",
-                                ta, dst_addr_str, prefix);
+
+    rc = cfg_add_instance_local_fmt(handle, CFG_VAL(NONE, NULL),
+                                    "/agent:%s/blackhole:%s|%d",
+                                    ta, dst_addr_str, prefix);
+    if (rc != 0)
+    {
+        ERROR("%s() fails to add a new blackhole route %s|%d",
+                __func__, dst_addr_str, prefix);
+        return rc;
+    }
+
+    rc = cfg_set_instance_local_fmt(CFG_VAL(STRING, route_type),
+                                    "/agent:%s/blackhole:%s|%d/type:",
+                                    ta, dst_addr_str, prefix);
+    if (rc != 0)
+    {
+        ERROR("%s() fails to add a new blackhole route %s|%d with type %s",
+              __func__, dst_addr_str, prefix, route_type);
+        return rc;
+    }
+
+    rc = cfg_commit_fmt("/agent:%s/blackhole:%s|%d",
+                        ta, dst_addr_str, prefix);
+    if (rc != 0)
+    {
+        ERROR("%s() fails to commit a new blackhole route %s|%d with type %s",
+              __func__, dst_addr_str, prefix, route_type);
+        return rc;
+    }
+
+    return 0;
+}
+
+static int
+tapi_cfg_add_blackhole(const char *ta, int addr_family,
+                       const void *dst_addr, int prefix,
+                       cfg_handle *handle)
+{
+    return tapi_cfg_add_blocking_routes(ta, addr_family,
+                                        dst_addr, prefix,
+                                        handle, "blackhole");
 }
 
 /* See the description in tapi_cfg.h */
@@ -725,10 +764,13 @@ tapi_cfg_add_full_route(const char *ta, int addr_family,
 {
     tapi_cfg_rt_params rt_params;
 
-    if (type != NULL && strcmp(type, "blackhole") == 0)
+    if (type != NULL && (strcmp(type, "blackhole") == 0 ||
+                         strcmp(type, "unreachable") == 0 ||
+                         strcmp(type, "prohibit") == 0 ||
+                         strcmp(type, "throw") == 0))
     {
-        return tapi_cfg_add_blackhole(ta, addr_family, dst_addr,
-                                      prefix, cfg_hndl);
+        return tapi_cfg_add_blocking_routes(ta, addr_family, dst_addr,
+                                            prefix, cfg_hndl, type);
     }
     if (type != NULL &&
         strcmp(type, "unicast") != 0 &&

agents/unix/conf/route/conf_route.c
@@ -598,34 +598,104 @@ static te_errno
 blackhole_add(unsigned int gid, const char *oid, const char *value,
               const char *route)
 {
-    te_errno        rc;
-    ta_rt_info_t    rt_info;
-
-    UNUSED(gid);
     UNUSED(oid);
     UNUSED(value);
 
-    rc = ta_rt_parse_inst_name(route, &rt_info);
-    if (rc != 0)
-        return rc;
-
-    return ta_unix_conf_route_blackhole_add(&rt_info);
+    return ta_obj_add(TA_OBJ_TYPE_BLACKHOLE, route, value, gid,
+                      NULL, NULL, NULL);
 }
 
 static te_errno
 blackhole_del(unsigned int gid, const char *oid, const char *route)
 {
-    te_errno        rc;
-    ta_rt_info_t    rt_info;
-
-    UNUSED(gid);
     UNUSED(oid);
 
-    rc = ta_rt_parse_inst_name(route, &rt_info);
+    return ta_obj_del(TA_OBJ_TYPE_BLACKHOLE, route, NULL,
+                      NULL, gid, route_load_attrs);
+}
+
+static te_errno
+blackhole_commit(unsigned int gid, const cfg_oid *p_oid)
+{
+    const char             *route;
+    ta_cfg_obj_t           *obj;
+    ta_rt_info_t            rt_info;
+    te_errno                rc;
+    ta_cfg_obj_action_e     obj_action;
+
+    route = ((cfg_inst_subid *)(p_oid->ids))[p_oid->len - 1].name;
+    ENTRY("%s", route);
+
+    obj = ta_obj_find(TA_OBJ_TYPE_BLACKHOLE, route, gid);
+    if (obj == NULL)
+    {
+        WARN("Commit for %s route which has not been updated", route);
+        return 0;
+    }
+
+    memset(&rt_info, 0, sizeof(rt_info));
+
+    rc = ta_rt_parse_inst_name(obj->name, &rt_info);
+    if (rc != 0)
+    {
+        ERROR("%s(): ta_rt_parse_inst_name() failed: %r", __func__, rc);
+        ta_obj_free(obj);
+        return rc;
+    }
+
+    rc = ta_rt_parse_attrs(obj->attrs, &rt_info);
+    if (rc != 0)
+    {
+        ERROR("%s(): ta_rt_parse_attrs() failed: %r", __func__, rc);
+        ta_obj_free(obj);
+        return rc;
+    }
+
+    if (rt_info.type != TA_RT_TYPE_BLACKHOLE &&
+        rt_info.type != TA_RT_TYPE_UNREACHABLE &&
+        rt_info.type != TA_RT_TYPE_PROHIBIT &&
+        rt_info.type != TA_RT_TYPE_THROW)
+    {
+        ERROR("Blackhole route type '%s' is not supported yet", rt_info.type);
+        return TE_RC(TE_TAPI, TE_EINVAL);
+    }
+
+    obj_action = obj->action;
+    ta_obj_free(obj);
+
+    rc = ta_unix_conf_route_change(obj_action, &rt_info);
+    ta_rt_info_clean(&rt_info);
+    return rc;
+}
+
+static te_errno
+blackhole_type_get(unsigned int gid, const char *oid,
+                   char *value, const char *route)
+{
+    te_errno rc;
+    ta_rt_info_t *rt_info;
+
+    UNUSED(oid);
+
+    rc = route_find(gid, route, &rt_info);
     if (rc != 0)
         return rc;
 
-    return ta_unix_conf_route_blackhole_del(&rt_info);
+    if (rt_info->type >= TA_RT_TYPE_MAX_VALUE)
+        return TE_RC(TE_TA_UNIX, TE_EINVAL);
+
+    snprintf(value, RCF_MAX_VAL, "%s", ta_rt_type2name(rt_info->type));
+    return 0;
+}
+
+static te_errno
+blackhole_type_set(unsigned int gid, const char *oid,
+                   char *value, const char *route)
+{
+    UNUSED(oid);
+
+    return ta_obj_set(TA_OBJ_TYPE_BLACKHOLE, route, "type",
+                      value, gid, route_load_attrs);
 }
 
 /**
@@ -1170,10 +1240,15 @@ RCF_PCH_CFG_NODE_RO(node_ip4_rt_default_if, "ip4_rt_default_if",
 RCF_PCH_CFG_NODE_RO(node_ip6_rt_default_if, "ip6_rt_default_if",
                     NULL, &node_ip4_rt_default_if, ip6_rt_default_if_get);
 
+static rcf_pch_cfg_object node_blackhole;
+
+RCF_PCH_CFG_NODE_RWC(node_blackhole_type, "type", NULL, NULL,
+                     blackhole_type_get, blackhole_type_set, &node_blackhole);
+
 RCF_PCH_CFG_NODE_COLLECTION(node_blackhole, "blackhole",
-                            NULL, &node_ip6_rt_default_if,
+                            &node_blackhole_type, &node_ip6_rt_default_if,
                             blackhole_add, blackhole_del,
-                            blackhole_list, NULL);
+                            blackhole_list, blackhole_commit);
 
 static rcf_pch_cfg_object node_route;
 

doc/cm/cm_base.yml
@@ -1326,6 +1326,14 @@
          Blackhole route
          Name: address+prefix
 
+    - oid: "/agent/blackhole/type"
+      access: read_write
+      type: string
+      d: |
+         Blackhole route type.
+         Name: empty
+         Value: Type of the blackhole route (e.g. blackhole, throw, prohibit etc)
+
     - oid: "/agent/ip4_rt_default_if"
       access: read_only
       type: string

lib/rcfpch/rcf_pch_ta_cfg.h
@@ -282,6 +282,9 @@ extern void ta_cfg_obj_log(const ta_cfg_obj_t *obj);
 /** Name for object of type route */
 #define TA_OBJ_TYPE_ROUTE "route"
 
+/** Name for object of type blackhole route */
+#define TA_OBJ_TYPE_BLACKHOLE "blackhole"
+
 /** Name for object of type sniffer */
 #define TA_OBJ_TYPE_SNIFFER "sniffer"
 

commit 576a157dde3686188407c2b21b1669af6c7b2596

lib/tapi: remove unused static function
Remove the compiler warning of unused ‘tapi_cfg_add_blackhole’ function.

Fixes: 67bfc8d4bdc0 ("agents/unix: support unreachable, prohibit and throw routes")
OL-Redmine-Id: 12263
Signed-off-by: Daniil Byshenko <daniil.byshenko@oktetlabs.ru>
Reviewed-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>

lib/tapi/tapi_cfg.c
@@ -743,16 +743,6 @@ tapi_cfg_add_blocking_routes(const char *ta, int addr_family,
     return 0;
 }
 
-static int
-tapi_cfg_add_blackhole(const char *ta, int addr_family,
-                       const void *dst_addr, int prefix,
-                       cfg_handle *handle)
-{
-    return tapi_cfg_add_blocking_routes(ta, addr_family,
-                                        dst_addr, prefix,
-                                        handle, "blackhole");
-}
-
 /* See the description in tapi_cfg.h */
 int
 tapi_cfg_add_full_route(const char *ta, int addr_family,