dfd917666d1eac087cd080425ab3e2f0f232fcbb
Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
2023-01-21 10:51:03 +0300

    lib/tapi_bpf: add TAPI support for XDP-related RPC calls
    It will allow to use these calls in AF_XDP tests.
    
    OL-Redmine-Id: 12593
    Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
    Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>
    Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

---
+++ lib/tapi_bpf/tapi_rpc_bpf.c
@@ -0,0 +1,346 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (C) 2023 OKTET Labs Ltd. All rights reserved. */
+/** @file
+ * @brief TAPI RPC for BPF
+ *
+ * Implementation of TAPI RPC for BPF
+ */
+
+#include "te_defs.h"
+#include "rcf_rpc.h"
+#include "tarpc.h"
+#include "tapi_rpc_internal.h"
+#include "te_string.h"
+#include "te_rpc_bpf.h"
+#include "tapi_bpf.h"
+#include "tapi_file.h"
+
+/*
+ * Maximum length of string representation of UMEM or socket
+ * configuration structure.
+ */
+#define CONFIG_STR_LEN 1024
+
+/* Get string representation of tarpc_xsk_umem_config. */
+static void
+xsk_umem_config_tarpc2str(tarpc_xsk_umem_config *config, te_string *str)
+{
+    if (config == NULL)
+    {
+        te_string_append(str, "%s", "(null)");
+    }
+    else
+    {
+        te_string_append(
+                 str,
+                 "{.fill_size=%u, .comp_size=%u, .frame_size=%u, "
+                 ".frame_headroom=%u, .flags=0x%x}",
+                 config->fill_size, config->comp_size, config->frame_size,
+                 config->frame_headroom, config->flags);
+    }
+}
+
+/* See description in tapi_rpc_bpf.h */
+int
+rpc_xsk_umem__create(rcf_rpc_server *rpcs, rpc_ptr umem_area,
+                     uint64_t size, tarpc_xsk_umem_config *config,
+                     rpc_ptr *umem)
+{
+    tarpc_xsk_umem__create_in in;
+    tarpc_xsk_umem__create_out out;
+
+    te_string cfg_str = TE_STRING_INIT_STATIC(CONFIG_STR_LEN);
+
+    memset(&in, 0, sizeof(in));
+    memset(&out, 0, sizeof(out));
+
+    in.umem_area = umem_area;
+    in.size = size;
+
+    if (config != NULL)
+    {
+        in.config.config_val = config;
+        in.config.config_len = 1;
+    }
+
+    rcf_rpc_call(rpcs, "xsk_umem__create", &in, &out);
+    CHECK_RETVAL_VAR_IS_ZERO_OR_MINUS_ONE(xsk_umem__create, out.retval);
+
+    if (RPC_IS_CALL_OK(rpcs) && rpcs->op != RCF_RPC_WAIT)
+        *umem = out.umem_ptr;
+
+    xsk_umem_config_tarpc2str(config, &cfg_str);
+    TAPI_RPC_LOG(rpcs, xsk_umem__create,
+                 "umem_area=" RPC_PTR_FMT ", size=%" TE_PRINTF_64 "u, "
+                 "config=%s", "%d (" RPC_PTR_FMT ")",
+                 RPC_PTR_VAL(umem_area), size,
+                 te_string_value(&cfg_str),
+                 out.retval, RPC_PTR_VAL(out.umem_ptr));
+    RETVAL_INT(xsk_umem__create, out.retval);
+}
+
+/* See description in tapi_rpc_bpf.h */
+int
+rpc_xsk_umem__delete(rcf_rpc_server *rpcs, rpc_ptr umem)
+{
+    tarpc_xsk_umem__delete_in in;
+    tarpc_xsk_umem__delete_out out;
+
+    memset(&in, 0, sizeof(in));
+    memset(&out, 0, sizeof(out));
+
+    in.umem_ptr = umem;
+
+    rcf_rpc_call(rpcs, "xsk_umem__delete", &in, &out);
+    CHECK_RETVAL_VAR_IS_ZERO_OR_MINUS_ONE(xsk_umem__delete, out.retval);
+
+    TAPI_RPC_LOG(rpcs, xsk_umem__delete,
+                 RPC_PTR_FMT, "%d",
+                 RPC_PTR_VAL(umem), out.retval);
+    RETVAL_INT(xsk_umem__delete, out.retval);
+}
+
+/* Get string representation of tarpc_xsk_socket_config. */
+static void
+xsk_socket_config_tarpc2str(tarpc_xsk_socket_config *config, te_string *str)
+{
+    if (config == NULL)
+    {
+        te_string_append(str, "%s", "(null)");
+    }
+    else
+    {
+        te_string_append(
+                 str,
+                 "{.rx_size=%u, .tx_size=%u, .libxdp_flags=%s, "
+                 ".xdp_flags=0x%x, .bind_flags=%s}",
+                 config->rx_size, config->tx_size,
+                 xsk_libxdp_flags_rpc2str(config->libxdp_flags),
+                 config->xdp_flags,
+                 xdp_bind_flags_rpc2str(config->bind_flags));
+    }
+}
+
+/* See description in tapi_rpc_bpf.h */
+int
+rpc_xsk_socket__create(rcf_rpc_server *rpcs, const char *if_name,
+                       uint32_t queue_id, rpc_ptr umem,
+                       te_bool shared_umem,
+                       tarpc_xsk_socket_config *config,
+                       rpc_ptr *sock)
+{
+    tarpc_xsk_socket__create_in in;
+    tarpc_xsk_socket__create_out out;
+
+    te_string cfg_str = TE_STRING_INIT_STATIC(CONFIG_STR_LEN);
+
+    memset(&in, 0, sizeof(in));
+    memset(&out, 0, sizeof(out));
+
+    in.if_name = (char *)if_name;
+    in.queue_id = queue_id;
+    in.umem_ptr = umem;
+    in.shared_umem = shared_umem;
+
+    if (config != NULL)
+    {
+        in.config.config_val = config;
+        in.config.config_len = 1;
+    }
+
+    rcf_rpc_call(rpcs, "xsk_socket__create", &in, &out);
+    CHECK_RETVAL_VAR_IS_GTE_MINUS_ONE(xsk_socket__create, out.retval);
+
+    if (RPC_IS_CALL_OK(rpcs) && rpcs->op != RCF_RPC_WAIT)
+        *sock = out.socket_ptr;
+
+    xsk_socket_config_tarpc2str(config, &cfg_str);
+    TAPI_RPC_LOG(rpcs, xsk_socket__create,
+                 "if_name=%s, queue_id=%u, umem=" RPC_PTR_FMT ", "
+                 "shared_umem=%s, config=%s", "%d (" RPC_PTR_FMT ")",
+                 if_name, queue_id, RPC_PTR_VAL(umem),
+                 shared_umem ? "TRUE" : "FALSE",
+                 te_string_value(&cfg_str),
+                 out.retval, RPC_PTR_VAL(out.socket_ptr));
+    RETVAL_INT(xsk_socket__create, out.retval);
+}
+
+/* See description in tapi_rpc_bpf.h */
+int
+rpc_xsk_socket__delete(rcf_rpc_server *rpcs, rpc_ptr sock)
+{
+    tarpc_xsk_socket__delete_in in;
+    tarpc_xsk_socket__delete_out out;
+
+    memset(&in, 0, sizeof(in));
+    memset(&out, 0, sizeof(out));
+
+    in.socket_ptr = sock;
+
+    rcf_rpc_call(rpcs, "xsk_socket__delete", &in, &out);
+    CHECK_RETVAL_VAR_IS_ZERO_OR_MINUS_ONE(xsk_socket__delete, out.retval);
+
+    TAPI_RPC_LOG(rpcs, xsk_socket__delete,
+                 RPC_PTR_FMT, "%d",
+                 RPC_PTR_VAL(sock), out.retval);
+    RETVAL_INT(xsk_socket__delete, out.retval);
+}
+
+/* See description in tapi_rpc_bpf.h */
+int
+rpc_xsk_map_set(rcf_rpc_server *rpcs, int map_fd,
+                unsigned int key, int fd)
+{
+    tarpc_xsk_map_set_in in;
+    tarpc_xsk_map_set_out out;
+
+    memset(&in, 0, sizeof(in));
+    memset(&out, 0, sizeof(out));
+
+    in.map_fd = map_fd;
+    in.key = key;
+    in.fd = fd;
+
+    rcf_rpc_call(rpcs, "xsk_map_set", &in, &out);
+    CHECK_RETVAL_VAR_IS_ZERO_OR_MINUS_ONE(xsk_map_set,
+                                          out.retval);
+
+    TAPI_RPC_LOG(rpcs, xsk_map_set,
+                 "map_fd=%d, key=%u, fd=%d", "%d",
+                 map_fd, key, fd, out.retval);
+    RETVAL_INT(xsk_map_set, out.retval);
+}
+
+/* See description in tapi_rpc_bpf.h */
+ssize_t
+rpc_xsk_rx_fill_simple(rcf_rpc_server *rpcs, rpc_ptr sock,
+                       size_t frames_cnt)
+{
+    tarpc_xsk_rx_fill_simple_in in;
+    tarpc_xsk_rx_fill_simple_out out;
+
+    memset(&in, 0, sizeof(in));
+    memset(&out, 0, sizeof(out));
+
+    in.socket_ptr = sock;
+    in.frames_cnt = frames_cnt;
+
+    rcf_rpc_call(rpcs, "xsk_rx_fill_simple", &in, &out);
+    CHECK_RETVAL_VAR_IS_GTE_MINUS_ONE(xsk_rx_fill_simple, out.retval);
+
+    TAPI_RPC_LOG(rpcs, xsk_rx_fill_simple,
+                 RPC_PTR_FMT ", frames_cnt=%" TE_PRINTF_SIZE_T "u", "%d",
+                 RPC_PTR_VAL(sock), frames_cnt, out.retval);
+    RETVAL_INT64(xsk_rx_fill_simple, out.retval);
+}
+
+/* See description in tapi_rpc_bpf.h */
+ssize_t
+rpc_xsk_receive_simple(rcf_rpc_server *rpcs, rpc_ptr sock,
+                       uint8_t *buf, size_t len)
+{
+    tarpc_xsk_receive_simple_in in;
+    tarpc_xsk_receive_simple_out out;
+
+    memset(&in, 0, sizeof(in));
+    memset(&out, 0, sizeof(out));
+
+    in.socket_ptr = sock;
+
+    rcf_rpc_call(rpcs, "xsk_receive_simple", &in, &out);
+    CHECK_RETVAL_VAR_IS_GTE_MINUS_ONE(xsk_receive_simple, out.retval);
+
+    if (RPC_IS_CALL_OK(rpcs))
+    {
+        if (buf != NULL && out.data.data_val != NULL)
+            memcpy(buf, out.data.data_val, MIN(len, out.data.data_len));
+    }
+
+    TAPI_RPC_LOG(rpcs, xsk_receive_simple,
+                 RPC_PTR_FMT ", %p, %" TE_PRINTF_SIZE_T "u",
+                 "%" TE_PRINTF_SIZE_T "d",
+                 RPC_PTR_VAL(sock), buf, len, out.retval);
+    RETVAL_INT64(xsk_receive_simple, out.retval);
+}
+
+/* See description in tapi_rpc_bpf.h */
+int
+rpc_xsk_send_simple(rcf_rpc_server *rpcs, rpc_ptr sock,
+                    void *buf, size_t len)
+{
+    tarpc_xsk_send_simple_in in;
+    tarpc_xsk_send_simple_out out;
+
+    memset(&in, 0, sizeof(in));
+    memset(&out, 0, sizeof(out));
+
+    in.socket_ptr = sock;
+    in.data.data_val = buf;
+    in.data.data_len = len;
+
+    rcf_rpc_call(rpcs, "xsk_send_simple", &in, &out);
+    CHECK_RETVAL_VAR_IS_ZERO_OR_MINUS_ONE(xsk_send_simple, out.retval);
+
+    TAPI_RPC_LOG(rpcs, xsk_send_simple,
+                 RPC_PTR_FMT ", %p, %" TE_PRINTF_SIZE_T "u", "%d",
+                 RPC_PTR_VAL(sock), buf, len, out.retval);
+    RETVAL_INT(xsk_send_simple, out.retval);
+}
+
+/* See description in tapi_rpc_bpf.h */
+int
+rpc_bpf_obj_get(rcf_rpc_server *rpcs, const char *path)
+{
+    tarpc_bpf_obj_get_in in;
+    tarpc_bpf_obj_get_out out;
+
+    memset(&in, 0, sizeof(in));
+    memset(&out, 0, sizeof(out));
+
+    in.path = (char *)path;
+
+    rcf_rpc_call(rpcs, "bpf_obj_get", &in, &out);
+    CHECK_RETVAL_VAR_IS_GTE_MINUS_ONE(bpf_obj_get, out.retval);
+
+    TAPI_RPC_LOG(rpcs, bpf_obj_get,
+                 "%s", "%d", path, out.retval);
+    RETVAL_INT(bpf_obj_get, out.retval);
+}
+
+/* See description in tapi_rpc_bpf.h */
+te_errno
+tapi_bpf_map_pin_get_fd(rcf_rpc_server *rpcs, unsigned int bpf_id,
+                        const char *map_name, int *fd)
+{
+    te_string file_path_str = TE_STRING_INIT;
+    char *pin_path = NULL;
+    te_errno rc = 0;
+
+    rc = tapi_bpf_map_get_pin(rpcs->ta, bpf_id, map_name, &pin_path);
+    if (rc != 0)
+        goto finish;
+
+    if (pin_path == NULL)
+    {
+        te_string_append(&file_path_str, "/sys/fs/bpf/");
+        tapi_file_make_name(&file_path_str);
+        te_string_move(&pin_path, &file_path_str);
+
+        rc = tapi_bpf_map_set_pin(rpcs->ta, bpf_id, map_name, pin_path);
+        if (rc != 0)
+            goto finish;
+    }
+
+    RPC_AWAIT_ERROR(rpcs);
+    *fd = rpc_bpf_obj_get(rpcs, pin_path);
+    if (*fd < 0)
+    {
+        rc = RPC_ERRNO(rpcs);
+        goto finish;
+    }
+
+finish:
+
+    free(pin_path);
+    return rc;
+}
---
+++ lib/tapi_bpf/tapi_rpc_bpf.h
@@ -0,0 +1,188 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (C) 2023 OKTET Labs Ltd. All rights reserved. */
+/** @file
+ * @brief TAPI RPC for BPF
+ *
+ * Definitions of TAPI RPC for BPF
+ */
+
+#ifndef __TE_TAPI_RPC_BPF_H__
+#define __TE_TAPI_RPC_BPF_H__
+
+#include "te_config.h"
+
+#include "te_defs.h"
+#include "rcf_rpc.h"
+#include "te_rpc_bpf.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Create UMEM.
+ *
+ * @param rpcs          RPC server
+ * @param umem_area     Memory allocated for UMEM
+ * @param size          Size of the memory
+ * @param config        UMEM configuration parameters
+ *                      (if @c NULL, default values will be used by libxdp)
+ * @param umem          RPC pointer to created UMEM
+ *
+ * @return @c 0 on success, @c -1 on failure.
+ */
+extern int rpc_xsk_umem__create(rcf_rpc_server *rpcs, rpc_ptr umem_area,
+                                uint64_t size,
+                                tarpc_xsk_umem_config *config,
+                                rpc_ptr *umem);
+
+/**
+ * Destroy UMEM.
+ *
+ * @param rpcs          RPC server
+ * @param umem          RPC pointer to UMEM
+ *
+ * @return @c 0 on success, @c -1 on failure.
+ */
+extern int rpc_xsk_umem__delete(rcf_rpc_server *rpcs, rpc_ptr umem);
+
+/**
+ * Create AF_XDP socket.
+ *
+ * @param rpcs          RPC server
+ * @param if_name       Interface name
+ * @param queue_id      Rx queue id
+ * @param umem          RPC pointer to UMEM
+ * @param shared_umem   If @c TRUE, UMEM is shared with other sockets
+ * @oaram config        Configuration parameters
+ * @param sock          RPC pointer to created socket structure on TA
+ *
+ * @return Nonnegative socket FD on success, @c -1 on failure.
+ */
+extern int rpc_xsk_socket__create(rcf_rpc_server *rpcs, const char *if_name,
+                                  uint32_t queue_id, rpc_ptr umem,
+                                  te_bool shared_umem,
+                                  tarpc_xsk_socket_config *config,
+                                  rpc_ptr *sock);
+
+/**
+ * Destroy AF_XDP socket on TA.
+ *
+ * @param rpcs      RPC server
+ * @param sock      RPC pointer to socket structure on TA
+ *
+ * @return @c 0 on success, @c -1 on failure.
+ */
+extern int rpc_xsk_socket__delete(rcf_rpc_server *rpcs, rpc_ptr sock);
+
+/**
+ * Set an entry of XSK map to FD of AF_XDP socket.
+ *
+ * @note This operation cannot be done via Configurator by editing
+ *       BPF map entries in configuration tree. XSKMAP is not supported
+ *       like other map types by Linux kernel. Configurator cannot even
+ *       list values of its entries because map lookup does not work.
+ *       The set values are file descriptors which are valid only in the
+ *       RPC server which created them, not in TA process handling
+ *       Configuration tree changes.
+ *
+ * @sa rpc_bpf_obj_get()
+ *
+ * @param rpcs      RPC server
+ * @param map_fd    File descriptor of the map
+ * @param key       Key in the map
+ * @param fd        Value to set (nonnegative value means FD of
+ *                  AF_XDP socket; negative value means "remove existing
+ *                  entry")
+ *
+ * @return @c 0 on success, @c -1 on failure.
+ */
+extern int rpc_xsk_map_set(rcf_rpc_server *rpcs, int map_fd,
+                           unsigned int key, int fd);
+
+/**
+ * Add specified number of frame buffers to FILL ring of UMEM associated
+ * with a given AF_XDP socket. It will enable receiving data into these
+ * buffers.
+ *
+ * @param rpcs        RPC server
+ * @param sock        RPC pointer to socket structure on TA
+ * @param frames_cnt  Number of frame buffers
+ *
+ * @return Number of added buffers on success, @c -1 on failure.
+ */
+extern ssize_t rpc_xsk_rx_fill_simple(rcf_rpc_server *rpcs, rpc_ptr sock,
+                                      size_t frames_cnt);
+
+/**
+ * Obtain a single packet from RX ring of AF_XDP socket.
+ *
+ * @note This function is meant to be used with rpc_xsk_rx_fill_simple().
+ *       After reading data from frame buffer, the buffer is again added
+ *       to FILL ring so that it can be reused.
+ *
+ * @param rpcs        RPC server
+ * @param sock        RPC pointer to socket structure on TA
+ * @param buf         Buffer for received data
+ * @param len         Buffer length
+ *
+ * @return Actual number of received bytes on success, @c -1 on failure.
+ */
+extern ssize_t rpc_xsk_receive_simple(rcf_rpc_server *rpcs, rpc_ptr sock,
+                                      void *buf, size_t len);
+
+/**
+ * Send a single packet from AF_XDP socket.
+ *
+ * @note This function chooses for sending one of frame buffers
+ *       not added to FILL ring by rpc_xsk_rx_fill_simple().
+ *       After adding packet buffer to TX ring, it waits until
+ *       completion can be read from COMPLETION ring, and marks
+ *       frame buffer as free for future use.
+ *
+ * @param rpcs        RPC server
+ * @param sock        RPC pointer to socket structure on TA
+ * @param buf         Buffer with data to send
+ * @param len         Data length
+ *
+ * @return @c 0 on success, @c -1 on failure.
+ */
+extern int rpc_xsk_send_simple(rcf_rpc_server *rpcs, rpc_ptr sock,
+                               void *buf, size_t len);
+
+/**
+ * Open a file in BPF file system, obtain BPF object file descriptor.
+ *
+ * @note This function can be used to obtain from RPC process FD of XSK map
+ *       pinned via configuration tree from TA process.
+ *
+ * @sa rpc_xsk_map_set()
+ *
+ * @param rpcs        RPC server
+ * @param path        Path to BPF object file
+ *
+ * @return nonnegative FD on success, @c -1 on failure.
+ */
+extern int rpc_bpf_obj_get(rcf_rpc_server *rpcs, const char *path);
+
+/**
+ * Pin BPF map to a file in BPFFS in TA process via configuration tree
+ * (if it is not pinned yet; otherwise use the currently pinned file).
+ * Then open that file with bpf_obj_get() to obtain FD of the map in RPC
+ * server process.
+ *
+ * @param rpcs        RPC server
+ * @param bpf_id      BPF object ID
+ * @param map_name    BPF map name
+ * @param fd          Location for obtained file descriptor
+ *
+ * @return Status code.
+ */
+extern te_errno tapi_bpf_map_pin_get_fd(rcf_rpc_server *rpcs,
+                                        unsigned int bpf_id,
+                                        const char *map_name, int *fd);
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* !__TE_TAPI_RPC_BPF_H__ */
