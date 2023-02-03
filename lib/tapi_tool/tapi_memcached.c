oktetlabs/test-environment

commit 7d6774c5469dd3fc115abca5ad5fcd929692fc57

lib/tapi_tool: let users select different verbosity modes
In tapi_memcached, add an opportunity to select
different verbosity modes using an enum mapping.

OL-Redmine-Id: 11994
Signed-off-by: Daniil Byshenko <daniil.byshenko@oktetlabs.ru>

lib/tapi_tool/tapi_memcached.c
@@ -41,6 +41,15 @@ static const te_enum_map tapi_mamcached_proto_mapping[] = {
     TE_ENUM_MAP_END
 };
 
+/** Mapping of possible values for memcached::verbose option. */
+static const te_enum_map tapi_mamcached_verbose_mapping[] = {
+    {.name = "",        .value = TAPI_MEMCACHED_NONE_VERBOSE},
+    {.name = "-v",      .value = TAPI_MEMCACHED_VERBOSE},
+    {.name = "-vv",     .value = TAPI_MEMCACHED_MORE_VERBOSE},
+    {.name = "-vvv",    .value = TAPI_MEMCACHED_EXTRA_VERBOSE},
+    TE_ENUM_MAP_END
+};
+
 /* Possible memcached command line arguments. */
 static const tapi_job_opt_bind memcached_binds[] = TAPI_JOB_OPT_SET(
     TAPI_JOB_OPT_STRING("--unix-socket=", TRUE, tapi_memcached_opt,
@@ -71,7 +80,8 @@ static const tapi_job_opt_bind memcached_binds[] = TAPI_JOB_OPT_SET(
     TAPI_JOB_OPT_UINT_T("--slab-min-size=", TRUE, NULL, tapi_memcached_opt,
                         slab_min_size),
     TAPI_JOB_OPT_BOOL("--disable-cas", tapi_memcached_opt, disable_cas),
-    TAPI_JOB_OPT_BOOL("-v", tapi_memcached_opt, verbose),
+    TAPI_JOB_OPT_ENUM(NULL, FALSE, tapi_memcached_opt, verbose,
+                      tapi_mamcached_verbose_mapping),
     TAPI_JOB_OPT_UINT_T("--threads=", TRUE, NULL, tapi_memcached_opt,
                         threads),
     TAPI_JOB_OPT_STRING("-D", FALSE, tapi_memcached_opt, delimiter),
@@ -144,7 +154,7 @@ const tapi_memcached_opt tapi_memcached_default_opt = {
     .slab_growth_factor         = TAPI_JOB_OPT_DOUBLE_UNDEF,
     .slab_min_size              = TAPI_JOB_OPT_UINT_UNDEF,
     .disable_cas                = FALSE,
-    .verbose                    = FALSE,
+    .verbose                    = TAPI_MEMCACHED_NONE_VERBOSE,
     .threads                    = TAPI_JOB_OPT_UINT_UNDEF,
     .delimiter                  = NULL,
     .enable_largepages          = FALSE,

lib/tapi_tool/tapi_memcached.h
@@ -46,6 +46,14 @@ typedef enum tapi_mamcached_proto {
     TAPI_MEMCACHED_PROTO_BINARY,
 } tapi_mamcached_proto_t;
 
+/** Representation of possible values for memcached::verbose option. */
+typedef enum tapi_mamcached_verbose {
+    TAPI_MEMCACHED_NONE_VERBOSE,
+    TAPI_MEMCACHED_VERBOSE,
+    TAPI_MEMCACHED_MORE_VERBOSE,
+    TAPI_MEMCACHED_EXTRA_VERBOSE,
+} tapi_mamcached_verbose_t;
+
 /** Specific memcached options. */
 typedef struct tapi_memcached_opt {
     /** Unix socket path to listen on (disables network support). */
@@ -104,8 +112,11 @@ typedef struct tapi_memcached_opt {
     tapi_job_opt_uint_t                 slab_min_size;
     /** Disable the use of CAS (and reduce the per-item size by 8 bytes). */
     te_bool                             disable_cas;
-    /** Be verbose during the event loop, print out errors and warnings. */
-    te_bool                             verbose;
+    /**
+     * Be verbose during the event loop.
+     * Print out errors and warnings (none by default).
+     */
+    tapi_mamcached_verbose_t            verbose;
     /** Number of threads to use to process incoming requests. */
     tapi_job_opt_uint_t                 threads;
     /**

commit de4353283aef365e66a1e0970438c6ce49f59737

tapi/tapi_tool: do not pass empty argument to memcached
The user should be able to run memcached without specifying
the verbose level.

OL-Redmine-Id: 11994
Fixes: 7d6774c5469d ("lib/tapi_tool: let users select different verbosity modes")
Signed-off-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>
Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>

lib/tapi_tool/tapi_memcached.c
@@ -43,7 +43,6 @@ static const te_enum_map tapi_mamcached_proto_mapping[] = {
 
 /** Mapping of possible values for memcached::verbose option. */
 static const te_enum_map tapi_mamcached_verbose_mapping[] = {
-    {.name = "",        .value = TAPI_MEMCACHED_NONE_VERBOSE},
     {.name = "-v",      .value = TAPI_MEMCACHED_VERBOSE},
     {.name = "-vv",     .value = TAPI_MEMCACHED_MORE_VERBOSE},
     {.name = "-vvv",    .value = TAPI_MEMCACHED_EXTRA_VERBOSE},
@@ -184,7 +183,7 @@ const tapi_memcached_opt tapi_memcached_default_opt = {
     .slab_growth_factor         = TAPI_JOB_OPT_DOUBLE_UNDEF,
     .slab_min_size              = TAPI_JOB_OPT_UINT_UNDEF,
     .disable_cas                = FALSE,
-    .verbose                    = TAPI_MEMCACHED_NONE_VERBOSE,
+    .verbose                    = TAPI_MEMCACHED_NOT_VERBOSE,
     .threads                    = TAPI_JOB_OPT_UINT_UNDEF,
     .napi_ids                   = TAPI_JOB_OPT_UINT_UNDEF,
     .delimiter                  = NULL,

lib/tapi_tool/tapi_memcached.h
@@ -48,7 +48,9 @@ typedef enum tapi_mamcached_proto {
 
 /** Representation of possible values for memcached::verbose option. */
 typedef enum tapi_mamcached_verbose {
-    TAPI_MEMCACHED_NONE_VERBOSE,
+    /** Option is omitted */
+    TAPI_MEMCACHED_NOT_VERBOSE = TAPI_JOB_OPT_ENUM_UNDEF,
+
     TAPI_MEMCACHED_VERBOSE,
     TAPI_MEMCACHED_MORE_VERBOSE,
     TAPI_MEMCACHED_EXTRA_VERBOSE,