oktetlabs/test-environment

commit 07cf4fc9e45de284d99d0436abd1f5eb43ff7971

lib/tools: add functions for random buffers with constraints
In many cases it is desired to generate a random sequence subject
to certain constraints, e.g. being a sequence of digits or suitable
for use as a filename etc. The patch extends older buffer-making
API with such functions. Older functions are retained as special-case
instantiations of more generic functions.

OL-Redmine-Id: 12392
Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Tested-by: Pavel Liulchak <pavel.liulchak@oktetlabs.ru>

lib/tools/te_bufs.c
@@ -24,88 +24,292 @@
 #include "te_defs.h"
 #include "logger_api.h"
 #include "te_alloc.h"
+#include "te_intset.h"
+#include "te_bufs.h"
 #include "te_hex_diff_dump.h"
 
-/* See description in te_bufs.h */
-void
-te_fill_buf(void *buf, size_t len)
-{
-    size_t  i;
+#define FILL_SPEC_ESC_CHAR '`'
 
-    for (i = 0; i < len / sizeof(int); ++i)
+static te_errno
+parse_byte_set(const char **spec, te_charset *set)
+{
+    const char *iter = *spec;
+
+    te_charset_clear(set);
+
+    if (*iter == FILL_SPEC_ESC_CHAR)
     {
-        ((int *)buf)[i] = rand();
+        iter++;
+        te_charset_add_range(set, (uint8_t)*iter, (uint8_t)*iter);
     }
-    for (i = (len / sizeof(int)) * sizeof(int); i < len; ++i)
+    else if (*iter != '[')
     {
-        ((unsigned char *)buf)[i] = rand();
+        te_charset_add_range(set, (uint8_t)*iter, (uint8_t)*iter);
+    }
+    else
+    {
+        te_bool except = FALSE;
+        te_bool empty_range = TRUE;
+
+        for (iter++; *iter != ']'; iter++)
+        {
+            uint8_t minch = 0;
+            uint8_t maxch = 0;
+
+            switch (*iter)
+            {
+                case '^':
+                    if (empty_range)
+                    {
+                        te_charset_add_range(set, 0, UINT8_MAX);
+                        empty_range = FALSE;
+                    }
+                    except = !except;
+                    continue;
+                case FILL_SPEC_ESC_CHAR:
+                    iter++;
+                    minch = maxch = *iter;
+                    break;
+                case '\0':
+                    ERROR("Unterminated ']'");
+                    return TE_EILSEQ;
+                default:
+                    if (iter[1] == '-' && iter[2] != ']' && iter[2] != '\0')
+                    {
+                        minch = (uint8_t)*iter;
+                        maxch = (uint8_t)iter[2];
+                        iter += 2;
+                    }
+                    else
+                    {
+                        minch = maxch = (uint8_t)*iter;
+                    }
+                    break;
+            }
+
+            if (except)
+                te_charset_remove_range(set, minch, maxch);
+            else
+                te_charset_add_range(set, minch, maxch);
+            empty_range = FALSE;
+        }
+
+        if (empty_range)
+            te_charset_add_range(set, 0, UINT8_MAX);
+    }
+
+    *spec = iter + 1;
+    return 0;
+}
+
+te_errno
+te_compile_buf_pattern(const char *spec, uint8_t *storage,
+                       size_t max_size,
+                       te_buf_pattern *pattern)
+{
+    size_t remaining = max_size;
+
+    pattern->start = storage;
+    pattern->suffix_len = 0;
+    pattern->repeat = pattern->suffix = pattern->end = NULL;
+
+    while (*spec != '\0')
+    {
+        te_errno rc = 0;
+        te_charset cset;
+        size_t need_space;
+
+        if (*spec == '(')
+        {
+            spec++;
+
+            if (pattern->repeat != NULL)
+            {
+                ERROR("Multiple repeat sections");
+                return TE_EINVAL;
+            }
+            pattern->repeat = storage;
+        }
+        else if (*spec == ')' &&
+                 pattern->repeat != NULL &&
+                 pattern->suffix == NULL)
+        {
+            spec++;
+            pattern->suffix = storage;
+            continue;
+        }
+
+        rc = parse_byte_set(&spec, &cset);
+        if (rc != 0)
+            return rc;
+
+        need_space = cset.n_items;
+        if (need_space == UINT8_MAX + 1)
+            need_space = 1;
+        else
+            need_space++;
+        if (remaining < need_space)
+        {
+            ERROR("Not enough space for compiled pattern, needed %zu",
+                  need_space);
+            return TE_ENOBUFS;
+        }
+
+        *storage++ = (uint8_t)cset.n_items;
+        if (cset.n_items != UINT8_MAX + 1)
+        {
+            te_charset_get_bytes(&cset, storage);
+            storage += cset.n_items;
+        }
+        remaining -= need_space;
+        if (pattern->suffix != NULL)
+            pattern->suffix_len++;
+    }
+
+    pattern->end = storage;
+    if (pattern->repeat == NULL)
+    {
+        pattern->repeat = pattern->start;
+        pattern->suffix = pattern->end;
+    }
+    else if (pattern->suffix == NULL)
+    {
+        ERROR("Unterminated '('");
+        return TE_EILSEQ;
+    }
+
+    if (pattern->end == pattern->start)
+    {
+        ERROR("Empty pattern");
+        return TE_ENODATA;
+    }
+    return 0;
+}
+
+static uint8_t
+fill_pattern_byte(uint8_t **pattern)
+{
+    uint8_t byte;
+
+    if (**pattern == 0)
+    {
+        byte = rand_range(0, UINT8_MAX);
+        (*pattern)++;
+    }
+    else
+    {
+        byte = rand_range(0, **pattern - 1);
+        byte = (*pattern)[byte + 1];
+        (*pattern) += **pattern + 1;
+    }
+
+    return byte;
+}
+
+void
+te_fill_pattern_buf(void *buf, size_t len, const te_buf_pattern *pattern)
+{
+    static uint8_t any_byte[1] = {0};
+    static const te_buf_pattern any_byte_pattern = {
+        .start = any_byte,
+        .end = any_byte + 1,
+        .repeat = any_byte,
+        .suffix = any_byte + 1,
+        .suffix_len = 0,
+    };
+    uint8_t *buf_ptr;
+    uint8_t *pat_ptr;
+    size_t remain;
+
+    assert(buf != NULL);
+
+    if (pattern == NULL)
+        pattern = &any_byte_pattern;
+
+    if (len == 0)
+        return;
+
+    pat_ptr = pattern->start;
+    buf_ptr = buf;
+    for (remain = len; remain > pattern->suffix_len; remain--)
+    {
+        assert(pat_ptr < pattern->end);
+
+        *buf_ptr++ = fill_pattern_byte(&pat_ptr);
+
+        if (pat_ptr == pattern->suffix)
+            pat_ptr = pattern->repeat;
+    }
+    pat_ptr = pattern->suffix;
+    for (; remain > 0; remain--)
+    {
+        assert(pat_ptr < pattern->end);
+
+        *buf_ptr++ = fill_pattern_byte(&pat_ptr);
     }
 }
 
-
-/* See description in te_bufs.h */
 void *
-te_make_buf(size_t min, size_t max, size_t *p_len)
+te_make_pattern_buf(size_t min, size_t max, size_t *p_len,
+                    const te_buf_pattern *pattern)
 {
+    size_t len;
     void *buf;
 
     assert(min <= max);
-    *p_len = rand_range(min, max);
+    len = rand_range(min, max);
 
     /*
      * There is nothing wrong with asking for a zero-length buffer.
      * However, ISO C and POSIX allow NULL or non-NULL to be returned
      * in this case, so we allocate a single-byte buffer instead.
      */
-    buf = TE_ALLOC(*p_len == 0 ? 1 : *p_len);
+    buf = TE_ALLOC(len == 0 ? 1 : len);
     if (buf == NULL)
-    {
-        /* FIXME: see issue #12079 for a consisent solution */
-        ERROR("Memory allocation failure - EXIT");
-        exit(1);
-    }
-    te_fill_buf(buf, *p_len);
-    return buf;
-}
+        TE_FATAL_ERROR("No memory");
 
-/* See description in te_bufs.h */
-void
-te_fill_printable_buf(void *buf, size_t len)
-{
-    size_t i;
+    te_fill_pattern_buf(buf, len, pattern);
 
-    assert(len > 0);
-    for (i = 0; i < len - 1; i++)
-    {
-        /* the range of ASCII printable characters */
-        ((char *)buf)[i] = rand_range(' ', '~');
-    }
-    ((char *)buf)[len - 1] = '\0';
-}
-
-
-/* See description in te_bufs.h */
-char *
-te_make_printable_buf(size_t min, size_t max, size_t *p_len)
-{
-    size_t len;
-    void *buf;
-
-    assert(min > 0);
-    assert(min <= max);
-    len = rand_range(min, max);
-
-    buf = TE_ALLOC(len);
-    if (buf == NULL)
-        TE_FATAL_ERROR("Memory allocation failure");
-
-    te_fill_printable_buf(buf, len);
     if (p_len != NULL)
         *p_len = len;
 
     return buf;
 }
 
+te_errno
+te_fill_spec_buf(void *buf, size_t len, const char *spec)
+{
+    uint8_t pat_storage[1024];
+    te_buf_pattern pattern;
+    te_errno rc;
+
+    rc = te_compile_buf_pattern(spec, pat_storage, sizeof(pat_storage),
+                                &pattern);
+    if (rc != 0)
+        return rc;
+
+    te_fill_pattern_buf(buf, len, &pattern);
+
+    return 0;
+}
+
+void *
+te_make_spec_buf(size_t min, size_t max, size_t *p_len, const char *spec)
+{
+    uint8_t pat_storage[1024];
+    te_buf_pattern pattern;
+    te_errno rc;
+
+    rc = te_compile_buf_pattern(spec, pat_storage, sizeof(pat_storage),
+                                &pattern);
+    if (rc != 0)
+    {
+        ERROR("Invalid pattern spec: %r", rc);
+        return NULL;
+    }
+
+    return te_make_pattern_buf(min, max, p_len, &pattern);
+}
 
 te_bool
 te_compare_bufs(const void *exp_buf, size_t exp_len,

lib/tools/te_bufs.h
@@ -22,56 +22,322 @@
 #endif
 
 #include "te_defs.h"
+#include "te_errno.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 /**
- * Fill buffer by random numbers.
+ * Compiled buffer fill pattern.
  *
- * @param buf      Buffer pointer
- * @param len      Buffer length
+ * The structure should be treated as opaque and
+ * only filled by te_compile_buf_pattern().
+ *
+ * The compiled pattern is a sequence of "possible bytes",
+ * where each "possible byte" is a number followed by
+ * that many byte values. If the number is zero, any byte
+ * is possible at that position.
  */
-extern void te_fill_buf(void *buf, size_t len);
+typedef struct te_buf_pattern {
+    /** Start of the pattern bytes. */
+    uint8_t *start;
+    /** End of the pattern bytes. */
+    uint8_t *end;
+    /** Repeating part of the pattern bytes. */
+    uint8_t *repeat;
+    /** Suffix part of the pattern bytes. */
+    uint8_t *suffix;
+    /** Number of bytes generated by @a suffix. */
+    size_t suffix_len;
+} te_buf_pattern;
 
 /**
- * Allocate buffer of random size from @b min to @b max and preset
- * it by random numbers.
- * Note, that this function will never return NULL and so all function
- * which call it.
+ * Convert a textual pattern spec into a compiled pattern.
  *
- * @param min     minimum size of buffer
- * @param max     maximum size of buffer
- * @param p_len   location for real size of allocated buffer
+ * The pattern spec is a string consisting of three parts:
+ * @e prefix @c ( @e repeat @c ) @e suffix,
+ * where both @e prefix and @e suffix are optional.
  *
- * @return Pointer to allocated buffer or exit(1)!
+ * The resulting byte sequence is produced as follows:
+ * - first @e prefix is generated;
+ * - then @e repeat is repeated until there is only room for @e suffix
+ * - then @e suffix is generated.
+ *
+ * If the buffer is shorter than the prefix and the suffix together, the suffix
+ * is laid over the prefix. This is slightly counter-intuitive, but it e.g.
+ * allows to ensure easily that a string would be zero-terminated.
+ *
+ * Each part is a sequence of byte specs, which are either:
+ * - any verbatim bytes except an open bracket, a backtick and a binary zero;
+ * - a backtick followed by any byte including a binary zero;
+ * - a byte set.
+ *
+ * A byte set is a sequence of elements enclosed in `[ ... ]`,
+ * where each element is either:
+ * - any verbatim byte except a hyphen, a caret, a closing bracket,
+ *   a backtick and a binary zero;
+ * - a backtick followed by any byte including a binary zero;
+ * - a range: a pair of bytes separated by @c -;
+ * - a literal unescaped hyphen if it's the first or the last element
+ *   of a byte set.
+ *
+ * An empty byte set `[]` means any byte.
+ *
+ * A byte set may also contain a right-associative exclusion operator @c ^:
+ * @e A @c ^ @e B @c ^ @e C means "all bytes from @e A except (those in @e B
+ * except those in @e C); see below for examples.
+ *
+ * @note The backtick was chosen as an escape character instead of a more
+ *       common backslash in order to avoid double-escaping in C literals.
+ *
+ * The compiled pattern is a sequence of "possible bytes",
+ * where each "possible byte" is a number followed by
+ * that many byte values. If the number is zero, any byte
+ * is possible at that position.
+ *
+ * The compiled pattern data are stored in
+ * @p storage which should be an array of at least @p max_size bytes.
+ * @p pattern holds pointers inside @p storage, so the latter should not
+ * have a shorter duration that the former.
+ *
+ * @par Examples:
+ *
+ * - @c "[0-9]"                        a sequence of decimal digits
+ *                                     (not zero-terminated)
+ * - @c "[a-zA-Z_]([a-zA-Z0-9_])`\0"   a valid C identifier
+ *                                     (zero-terminated)
+ * - @c "[^`\0\x1-\x1F\x7F-\xFF^\n\t]" a sequence of printable ASCII
+ *                                     characters  + newline and tab
+ * - @c "[ -~\n\t]"                    a shorter version of the previous
+ * - @c "`[([0-9a-f])`]`\0"            a zero-terminated sequence of
+ *                                     hexadecimal digits enclosed in brackets
+ *
+ * @param[in]  spec      pattern spec
+ * @param[out] storage   buffer to hold the compiled pattern
+ * @param[in]  max_size  total size of @p storage
+ * @param[out] pattern   compiled pattern header
+ *                       (the structure is completely initialised by
+ *                        this function)
+ * @return status code
+ * @retval TE_ENOBUFS  There is not enough room in @p storage to hold the data.
+ * @retval TE_EILSEQ   An unmatched @c [ or @c ( is found.
+ * @retval TE_ENODATA  The spec contains no actual byte specs.
+ * @retval TE_EINVAL   The spec contains multiple repeat sections.
  */
-extern void *te_make_buf(size_t min, size_t max, size_t *p_len);
-
+extern te_errno te_compile_buf_pattern(const char *spec,
+                                       uint8_t *storage,
+                                       size_t max_size,
+                                       te_buf_pattern *pattern);
 
 /**
- * Create a buffer of specified size
+ * Fill @p buf of length @p len with random bytes according
+ * to a given @p pattern.
  *
- * @param len    Buffer length
+ * If @p pattern is @c NULL, the buffer is filled with unconstrained
+ * random bytes.
  *
- * @return Pointer to allocated buffer.
+ * @param[out] buf      buffer
+ * @param[in]  len      length of @p buf
+ * @param[in]  pattern  compiled pattern (see te_compile_buf_pattern())
+ */
+extern void te_fill_pattern_buf(void *buf, size_t len,
+                                const te_buf_pattern *pattern);
+
+/**
+ * Allocate a buffer of random size between @p min and @p max
+ * filled with random bytes according to @p pattern.
+ *
+ * If @p pattern is @c NULL, the buffer is filled with unconstrained
+ * random bytes.
+ *
+ * @param[in]  min      minimum allowed size
+ * @param[in]  max      maximum allowed size
+ * @param[out] p_len    actual size of the buffer (may be @c NULL)
+ * @param[in]  pattern  compiled pattern (see te_compile_buf_pattern())
+ *
+ * @return an allocated and filled buffer (never @c NULL)
+ *
+ * @note @p p_len may be @c NULL, if either @p min is equal to @p max,
+ *       or @p spec contains a terminating @c NUL byte, since in this
+ *       case the size may be determined by @c strlen().
+ */
+extern void *te_make_pattern_buf(size_t min, size_t max,
+                                 size_t *p_len, const te_buf_pattern *pattern);
+
+/**
+ * Fill @p buf of length @p len with random bytes according
+ * to a given @p spec.
+ *
+ * @param[out] buf      buffer
+ * @param[in]  len      length of @p buf
+ * @param[in]  spec     pattern spec (see te_compile_buf_pattern())
+ *
+ * @return status code
+ *
+ * @note The function only returns an error if @p spec is malformed.
+ *       So callers that always pass known constant patterns to it,
+ *       need not check for the return value.
+ */
+extern te_errno te_fill_spec_buf(void *buf, size_t len, const char *spec);
+
+/**
+ * Allocate a buffer of random size between @p min and @p max
+ * filled with random bytes according to @p spec.
+ *
+ * @param[in]  min      minimum allowed size
+ * @param[in]  max      maximum allowed size
+ * @param[out] p_len    actual size of the buffer (may be @c NULL)
+ * @param[in]  spec     pattern spec (see te_compile_buf_pattern())
+ *
+ * @return an allocated and filled buffer
+ *
+ * @note @p p_len may be @c NULL, if either @p min is equal to @p max,
+ *       or @p spec contains a terminating @c NUL byte, since in this
+ *       case the size may be determined by @c strlen().
+ *
+ * @note The function only returns @c NULL if @p spec is malformed.
+ *       So callers that always pass known constant patterns to it,
+ *       need not check for the return value being not @c NULL.
+ */
+extern void *te_make_spec_buf(size_t min, size_t max, size_t *p_len,
+                              const char *spec);
+
+/** Common fill patterns specs.
+ *
+ * All sequences except TE_FILL_SPEC_BINARY are zero-terminated.
+ *@{
+ */
+
+/** A sequence of decimal digits. */
+#define TE_FILL_SPEC_DECIMAL "([0-9])`\0"
+
+/** A sequence of hex digits. */
+#define TE_FILL_SPEC_HEX "([0-9A-F])`\0"
+
+/** A sequence of hex digits (lowercase variant). */
+#define TE_FILL_SPEC_HEX_LCASE "([0-9a-f])`\0"
+
+/** A sequence of Latin letters. */
+#define TE_FILL_SPEC_WORD "([a-zA-Z])`\0"
+
+/** A valid C identifier. */
+#define TE_FILL_SPEC_C_ID "[a-zA-Z_]([0-9a-zA-Z_])`\0"
+
+/**
+ * A safe filename.
+ *
+ * A name contains no directory separators,
+ * no wildcards, no spaces and no characters
+ * that may be interpreted by a shell, such as
+ * a dollar sign. A name also does not start
+ * with a dot.
+ */
+#define TE_FILL_SPEC_FILENAME "[0-9a-zA-Z_%+=@`^-]([0-9a-zA-Z_.%+=@`^-])`\0"
+
+/**
+ * A valid URI chunk.
+ *
+ * It contains only RFC 3986 "unreserved" characters:
+ * alphanumerics, underscores, hyphens, periods and tildes.
+ */
+#define TE_FILL_SPEC_URI_CHUNK "([0-9a-zA-Z_.~-])`\0"
+/**
+ * A valid XML name.
+ *
+ * It contains only alphanumerics, underscores, hyphens
+ * and periods, and it starts with either an alphanumeric
+ * or an underscore.
+ */
+#define TE_FILL_SPEC_XML_NAME "[a-zA-Z_]([0-9a-zA-Z_.-])`\0"
+
+/**
+ * A valid XML text.
+ *
+ * It contains any printable characters except quotes,
+ * apostrophes, ampersands and angle brackets. It may
+ * contain newlines.
+ */
+#define TE_FILL_SPEC_XML_TEXT "([\n -~^\"'&<>])`\0"
+
+/**
+ * A valid content of a JSON string literal.
+ *
+ * It contains any printable characters except quotes and
+ * backslashes.
+ */
+#define TE_FILL_SPEC_JSON_STR "([ -~^\\\"])`\0"
+
+/** A sequence of printable characters. */
+#define TE_FILL_SPEC_PRINTABLE "([ -~])`\0"
+
+/** A sequence of printable characters, tabs and newlines. */
+#define TE_FILL_SPEC_TEXT "([\n\t -~])`\0"
+
+/** A random sequence of ASCII characters excluding @c NUL. */
+#define TE_FILL_SPEC_ASCII "([\x1-\x7F])`\0"
+
+/** A random sequence of bytes including @c NULs. */
+#define TE_FILL_SPEC_BINARY "[]"
+/**@}*/
+
+/**
+ * Fill buffer with random bytes.
+ *
+ * @param[out] buf      buffer pointer
+ * @param[in]  len      buffer length
+ *
+ * @sa te_fill_pattern_buf()
+ */
+static inline void
+te_fill_buf(void *buf, size_t len)
+{
+    te_fill_pattern_buf(buf, len, NULL);
+}
+
+/**
+ * Allocate buffer of random size between @p min and @p max and fill
+ * it with random bytes.
+ *
+ * @param[in]  min     minimum allowed size
+ * @param[in]  max     maximum allowed size
+ * @param[out] p_len   actual size of the buffer
+ *                     (may be @c NULL if @p min is equal to @p max)
+ *
+ * @return an allocated and filled buffer
+ *
+ * @sa te_make_pattern_buf()
+ */
+static inline void *
+te_make_buf(size_t min, size_t max, size_t *p_len)
+{
+    return te_make_pattern_buf(min, max, p_len, NULL);
+}
+
+/**
+ * Allocate a buffer of given size @p len and fill it with
+ * random bytes.
+ *
+ * @param len    buffer length
+ *
+ * @return an allocated and filled buffer
+ *
+ * @sa te_make_pattern_buf()
  */
 static inline void *
 te_make_buf_by_len(size_t len)
 {
-    size_t ret_len;
-
-    return te_make_buf(len, len, &ret_len);
+    return te_make_buf(len, len, NULL);
 }
 
 /**
- * Create a buffer not shorter that specified length.
+ * Allocate a buffer of at least @p min bytes and fill it with
+ * random bytes
  *
- * @param min       Minimum buffer length
- * @param p_len     Buffer length (OUT)
+ * @param[in]  min       minimum buffer length
+ * @param[out] p_len     actual buffer length
  *
- * @return Pointer to allocated buffer.
+ * @return an allocated and filled buffer
  */
 static inline void *
 te_make_buf_min(size_t min, size_t *p_len)
@@ -80,47 +346,57 @@ te_make_buf_min(size_t min, size_t *p_len)
 }
 
 /**
- * Fill buffer by random printable characters.
+ * Fill a buffer with random printable characters.
  *
  * The last byte will be zero, so the result may be
  * used as a C string.
  *
- * @param buf      buffer pointer
- * @param len      buffer length (must be greater than @c 0)
+ * @param[out] buf     buffer pointer
+ * @param[in] len      buffer length
+ *
+ * @sa te_fill_spec_buf()
  */
-extern void te_fill_printable_buf(void *buf, size_t len);
+static inline void
+te_fill_printable_buf(void *buf, size_t len)
+{
+    te_fill_spec_buf(buf, len, TE_FILL_SPEC_PRINTABLE);
+}
 
 /**
- * Allocate buffer of random size from @p min to @p max and preset
- * it by random printable characters.
+ * Allocate buffer of random size between @p min and @p max and fill
+ * it with random printable characters. The buffer will be NUL-terminated,
+ * so it can be used as a C string.
  *
- * The last byte will be zero, so the result is actually
- * a valid C string.
+ * @param[in]  min     minimum allowed size
+ * @param[in]  max     maximum allowed size
+ * @param[out] p_len   actual size of the buffer
+ *                     (may be @c NULL)
  *
- * @param[in]  min     minimum size of buffer (must be greater than @c 0)
- * @param[in]  max     maximum size of buffer (must not be less than @p min)
- * @param[out] p_len   location for real size of allocated buffer
- *                     (may be @c NULL, as the real length may always
- *                     be determined by strlen())
+ * @return an allocated and filled buffer
  *
- * @return Pointer to an allocated buffer.
+ * @sa te_make_spec_buf()
  */
-extern char *te_make_printable_buf(size_t min, size_t max, size_t *p_len);
-
+static inline char *
+te_make_printable_buf(size_t min, size_t max, size_t *p_len)
+{
+    return te_make_spec_buf(min, max, p_len, TE_FILL_SPEC_PRINTABLE);
+}
 
 /**
- * Create a buffer of specified size filled with printable characters.
+ * Allocate a buffer of given size @p len and fill it with
+ * random printable characters. The buffer will be NUL-terminated.
  *
- * @param len    Buffer length
+ * @param len    buffer length
  *
- * @return Pointer to allocated buffer (a valid C string).
+ * @return an allocated and filled buffer
+ *
+ * @sa te_make_spec_buf()
  */
 static inline char *
 te_make_printable_buf_by_len(size_t len)
 {
     return te_make_printable_buf(len, len, NULL);
 }
-
 
 /**
  * Allocate memory and fill it with the @p byte

suites/selftest/ts/tools/make_bufs.c
@@ -21,11 +21,50 @@
 #include "te_config.h"
 
 #include <string.h>
-#include <ctype.h>
+#include <regex.h>
 
 #include "tapi_test.h"
 #include "tapi_mem.h"
 #include "te_bufs.h"
+
+static void
+check_valid_buf(size_t min_len, size_t max_len, const char *spec,
+                const char *exp_match)
+{
+    char *buf = NULL;
+    size_t len;
+    regex_t re;
+
+    if (regcomp(&re, exp_match, REG_EXTENDED | REG_NOSUB) != 0)
+        TE_FATAL_ERROR("Cannot compile '%s'", exp_match);
+
+    CHECK_NOT_NULL(buf = te_make_spec_buf(min_len, max_len, &len, spec));
+    if (len < min_len || len > max_len)
+    {
+        TEST_VERDICT("The generated length is outside of "
+                     "the valid range: %zu", len);
+    }
+
+    if (buf[len - 1] != '\0')
+        TEST_VERDICT("The buffer is not zero terminated");
+    if (strlen(buf) + 1 != len)
+    {
+        TEST_VERDICT("The reported length %zu is incorrect: should be %zu",
+                     len, strlen(buf) + 1);
+    }
+
+    if (len > 1)
+    {
+        if (regexec(&re, buf, 0, NULL, 0) != 0)
+        {
+            ERROR("'%s' does not match RE '%s'", buf, exp_match);
+            TEST_VERDICT("The buffer contains invalid characters");
+        }
+    }
+
+    free(buf);
+    regfree(&re);
+}
 
 int
 main(int argc, char **argv)
@@ -34,7 +73,28 @@ main(int argc, char **argv)
     unsigned int min_len;
     unsigned int max_len;
     unsigned int n_iterations;
-    char *buf = NULL;
+    te_bool embedded_zeroes = FALSE;
+
+    static const char *specs[][3] = {
+        {"digits", TE_FILL_SPEC_DECIMAL, "^[[:digit:]]+$"},
+        {"hex digits", TE_FILL_SPEC_HEX, "^[[:xdigit:]]+$"},
+        {"letters", TE_FILL_SPEC_WORD, "^[[:alpha:]]+$"},
+        {"C id", TE_FILL_SPEC_C_ID, "^[[:alpha:]_][[:alnum:]_]*$"},
+        {"filename", TE_FILL_SPEC_FILENAME,
+         "^[[:alnum:]_%+=@^-][[:alnum:]_%+=@^.-]*$"},
+        {"URI chunk", TE_FILL_SPEC_URI_CHUNK, "^[[:alnum:]_.~-]+$"},
+        {"XML name", TE_FILL_SPEC_XML_NAME, "^[[:alpha:]_][[:alnum:]_.-]*"},
+        {"JSON literal", TE_FILL_SPEC_JSON_STR, "^[][ !#-Z^-~]+$"},
+        {"XML text", TE_FILL_SPEC_XML_TEXT, "^[][\n !#-%(-;=?-~]+$"},
+        {"printable characters", TE_FILL_SPEC_PRINTABLE, "^[[:print:]]+$"},
+        {"text", TE_FILL_SPEC_TEXT, "^[\n\t[:print:]]+$"},
+        {"ASCII characters", TE_FILL_SPEC_ASCII, "^[\x1-\x7F]+$"},
+        {"([a-z^d-x^k-p])\\0", "([a-z^d-x^k-p])`\0", "^[abcklmnopyz]+$"},
+        {"([^`\\0\\x1-\\x1F\\x7F-\\xFF^\\n\\t])",
+         "([^`\0\x1-\x1F\x7F-\xFF^\n\t])`\0", "^[\n\t[:print:]]+$"},
+        {"x(yz)`\\0", "x(yz)`\0", "^x(yz)*(yz?)?$"},
+    };
+    unsigned int specno;
 
     TEST_START;
 
@@ -42,38 +102,41 @@ main(int argc, char **argv)
     TEST_GET_UINT_PARAM(max_len);
     TEST_GET_UINT_PARAM(n_iterations);
 
-    TEST_STEP("Generate random printable buffers");
-    for (i = 0; i < n_iterations; i++)
+
+    TEST_STEP("Generate random buffers");
+    for (specno = 0; specno < TE_ARRAY_LEN(specs); specno++)
+    {
+        TEST_SUBSTEP("Checking buffer constraint: %s", specs[specno][0]);
+        for (i = 0; i < n_iterations; i++)
+        {
+            check_valid_buf(min_len, max_len, specs[specno][1],
+                            specs[specno][2]);
+        }
+    }
+
+    TEST_STEP("Check embedded zeroes");
+    for (i = 0; i < n_iterations && !embedded_zeroes; i++)
     {
         size_t len;
-        const char *iter;
+        char *buf = te_make_buf(min_len, max_len, &len);
+        size_t j;
 
-        CHECK_NOT_NULL(buf = te_make_printable_buf(min_len, max_len, &len));
-        if (len < min_len || len > max_len)
+        for (j = 0; j < len - 1; j++)
         {
-            TEST_VERDICT("The generated length is outside of the valid range: "
-                         "%zu", len);
-        }
-        if (buf[len - 1] != '\0')
-            TEST_VERDICT("The buffer is not zero terminated");
-        if (strlen(buf) + 1 != len)
-            TEST_VERDICT("The reported length %zu is incorrect", len);
-        for (iter = buf; *iter != '\0'; iter++)
-        {
-            if (!isprint(*iter))
+            if (buf[j] == '\0')
             {
-                TEST_VERDICT("%u'th character is not printable, code %02x",
-                             (unsigned)(iter - buf), (unsigned)*iter);
+                embedded_zeroes = TRUE;
+                break;
             }
         }
         free(buf);
-        buf = NULL;
     }
+    if (!embedded_zeroes)
+        TEST_VERDICT("No embedded zeroes after %zu iterations", n_iterations);
 
     TEST_SUCCESS;
 
 cleanup:
-    free(buf);
 
     TEST_END;
 }

commit 97959ea74ddc3325096380b77cfe0464a11cebd4

lib/tools: do not try to fill a buffer with zero length
Just use assert only when it is really necessary.

OL-Redmine-Id: 12392
Fixes: 07cf4fc9e45d ("lib/tools: add functions for random buffers with constraints")
Signed-off-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>
Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>

lib/tools/te_bufs.c
@@ -221,13 +221,13 @@ te_fill_pattern_buf(void *buf, size_t len, const te_buf_pattern *pattern)
     uint8_t *pat_ptr;
     size_t remain;
 
+    if (len == 0)
+        return;
+
     assert(buf != NULL);
 
     if (pattern == NULL)
         pattern = &any_byte_pattern;
-
-    if (len == 0)
-        return;
 
     pat_ptr = pattern->start;
     buf_ptr = buf;