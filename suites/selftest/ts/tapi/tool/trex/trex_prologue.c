a733525e21ac8dd1eec377b08041b15b5f9106d2
Daniil Byshenko <daniil.byshenko@oktetlabs.ru>
2023-03-24 23:38:36 +0300

    suites/selftest: add TAPI TRex selftest prologue
    Implement selftest prologue for TAPI TRex for future selftests.
    
    OL-Redmine-Id: 12780
    Signed-off-by: Daniil Byshenko <daniil.byshenko@oktetlabs.ru>
    Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>
    Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

---
+++ suites/selftest/ts/tapi/tool/trex/trex_prologue.c
@@ -0,0 +1,26 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (C) 2023 OKTET Labs Ltd. All rights reserved. */
+/** @file
+ * @brief TRex Test Suite
+ *
+ * Test Suite prologue.
+ */
+
+#define TE_TEST_NAME "trex_prologue"
+
+#include "tapi_cfg_net.h"
+#include "tapi_test.h"
+#include "tapi_cfg.h"
+
+int
+main(int argc, char **argv)
+{
+    TEST_START;
+
+    CHECK_RC(tapi_cfg_net_all_up(FALSE));
+    CHECK_RC(tapi_cfg_net_all_assign_ip(AF_INET));
+
+    TEST_SUCCESS;
+cleanup:
+    TEST_END;
+}
