77898d92215e5bb000ee7b5e9681c44a00a44f0e
Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
2023-04-21 03:35:43 +0300

    tools/rgt: improve nesting level computation
    Do not ignore log messages with "undefined" log ID when computing
    nesting level. Such messages come from TE or test agents (while
    messages from tests have tests IDs as their log IDs). It does not
    seem useful to exclude them from messages hierarchy specified
    by TEST_STEPs and other such macros, and it creates strange nesting
    level "jumps" in log which are hard to handle in RGT tools, like
    
    message N: nesting level 2
    message N+1: nesting level 0
    message N+2: nesting level 2
    
    OL-Redmine-Id: 12861
    Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
    Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>

--- tools/rgt/rgt-core/flow_tree.c
+++ tools/rgt/rgt-core/flow_tree.c
@@ -1464,49 +1464,48 @@ wrapper_process_regular_msg(gpointer data, gpointer user_data)
     if (reg_msg_proc != NULL)
     {
         msg = log_msg_read(msg_ptr);
-        if (msg->id != TE_LOG_ID_UNDEFINED)
+
+        if (~msg->level & TE_LL_CONTROL)
         {
-            if (~msg->level & TE_LL_CONTROL)
+            msg->nest_lvl = rgt_ctx.current_nest_lvl;
+        }
+        else
+        {
+            if (strcmp(msg->user, TE_USER_STEP) == 0)
+            {
+                msg->nest_lvl = 0;
+                rgt_ctx.current_nest_lvl = 1;
+            }
+            else if (strcmp(msg->user, TE_USER_SUBSTEP) == 0)
+            {
+                msg->nest_lvl = 1;
+                rgt_ctx.current_nest_lvl = 2;
+            }
+            else if (strcmp(msg->user, TE_USER_STEP_PUSH) == 0)
             {
                 msg->nest_lvl = rgt_ctx.current_nest_lvl;
+                rgt_ctx.current_nest_lvl++;
+            }
+            else if (strcmp(msg->user, TE_USER_STEP_POP) == 0)
+            {
+                rgt_ctx.current_nest_lvl--;
+                msg->nest_lvl = rgt_ctx.current_nest_lvl;
+            }
+            else if (strcmp(msg->user, TE_USER_STEP_NEXT) == 0)
+            {
+                msg->nest_lvl = rgt_ctx.current_nest_lvl - 1;
+            }
+            else if (strcmp(msg->user, TE_USER_STEP_RESET) == 0)
+            {
+                msg_visible = FALSE;
+                rgt_ctx.current_nest_lvl = 0;
             }
             else
             {
-                if (strcmp(msg->user, TE_USER_STEP) == 0)
-                {
-                    msg->nest_lvl = 0;
-                    rgt_ctx.current_nest_lvl = 1;
-                }
-                else if (strcmp(msg->user, TE_USER_SUBSTEP) == 0)
-                {
-                    msg->nest_lvl = 1;
-                    rgt_ctx.current_nest_lvl = 2;
-                }
-                else if (strcmp(msg->user, TE_USER_STEP_PUSH) == 0)
-                {
-                    msg->nest_lvl = rgt_ctx.current_nest_lvl;
-                    rgt_ctx.current_nest_lvl++;
-                }
-                else if (strcmp(msg->user, TE_USER_STEP_POP) == 0)
-                {
-                    rgt_ctx.current_nest_lvl--;
-                    msg->nest_lvl = rgt_ctx.current_nest_lvl;
-                }
-                else if (strcmp(msg->user, TE_USER_STEP_NEXT) == 0)
-                {
-                    msg->nest_lvl = rgt_ctx.current_nest_lvl - 1;
-                }
-                else if (strcmp(msg->user, TE_USER_STEP_RESET) == 0)
-                {
-                    msg_visible = FALSE;
-                    rgt_ctx.current_nest_lvl = 0;
-                }
-                else
-                {
-                    msg->nest_lvl = rgt_ctx.current_nest_lvl;
-                }
+                msg->nest_lvl = rgt_ctx.current_nest_lvl;
             }
         }
+
         if (msg_visible)
             reg_msg_proc(msg);
         free_log_msg(msg);
