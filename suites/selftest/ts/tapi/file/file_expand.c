oktetlabs/test-environment

commit 1630e130316b20238b870176882e9a07335fe916

suites/selftest: add a self test for variable expansion
The test is needed to ensure correctness of the forthcoming
refactoring.

OL-Redmine-Id: 12845
Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>

suites/selftest/conf/trc/tools/expand.trc.xml
@@ -1,0 +1,13 @@
+<?xml version="1.0"?>
+<!-- SPDX-License-Identifier: Apache-2.0 -->
+<!-- Copyright (C) 2023 OKTET Labs Ltd. All rights reserved. -->
+<test name="expand" type="script">
+    <objective>Testing variable expansion routines.</objective>
+    <notes/>
+    <iter result="PASSED">
+        <arg name="var1" />
+        <arg name="var2" />
+        <arg name="posarg" />
+        <arg name="expanded" />
+    </iter>
+</test>

suites/selftest/ts/tools/expand.c
@@ -1,0 +1,77 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (C) 2023 OKTET Labs Ltd. All rights reserved. */
+/** @file
+ * @brief Test for te_expand.h functions
+ *
+ * Testing variable expansion routines.
+ */
+
+/** @page tools_expand te_expand.h test
+ *
+ * @objective Testing variable expansion routines.
+ *
+ * Test the correctness of variable expanding functions.
+ *
+ * @par Test sequence:
+ */
+
+/** Logging subsystem entity name */
+#define TE_TEST_NAME    "tools/expand"
+
+#include <string.h>
+#include "te_config.h"
+
+#include "tapi_test.h"
+#include "te_kvpair.h"
+#include "te_expand.h"
+#include "te_str.h"
+
+/*
+ * FIXME: the template should a parameter too, but
+ * due to OL bug #12847 a parameter inside package.xml
+ * cannot contain shell-like variable references
+ */
+static const char template[] = "var1=${var1} var2=${var2:-unknown} "
+    "pos=${0} unknown=${unknown:-${var2}} known=${var2:+known} "
+    "nested=${var2:+${unknown:-${var1}}} end";
+
+int
+main(int argc, char **argv)
+{
+    const char *var1;
+    const char *var2;
+    const char *posarg;
+    const char *expanded;
+    te_kvpair_h kvpairs;
+    const char *posargs[10] = {NULL,};
+    char *actual = NULL;
+
+    te_kvpair_init(&kvpairs);
+
+    TEST_START;
+    TEST_GET_OPT_STRING_PARAM(var1);
+    TEST_GET_OPT_STRING_PARAM(var2);
+    TEST_GET_STRING_PARAM(posarg);
+    TEST_GET_STRING_PARAM(expanded);
+    posargs[0] = posarg;
+    CHECK_RC(te_kvpair_add(&kvpairs, "var1", "%s",
+                           te_str_empty_if_null(var1)));
+    CHECK_RC(te_kvpair_add(&kvpairs, "var2", "%s",
+                           te_str_empty_if_null(var2)));
+
+    if (te_expand_kvpairs(template, posargs, &kvpairs, &actual) != 0)
+        TEST_VERDICT("te_expand_kvpairs() failed");
+    if (strcmp(expanded, actual) != 0)
+    {
+        ERROR("Expected '%s', got '%s'", expanded, actual);
+        TEST_VERDICT("Unexpected expansion");
+    }
+
+    TEST_SUCCESS;
+
+cleanup:
+
+    te_kvpair_fini(&kvpairs);
+    free(actual);
+    TEST_END;
+}

suites/selftest/ts/tools/package.xml
@@ -1,6 +1,6 @@
 <?xml version="1.0"?>
 <!-- SPDX-License-Identifier: Apache-2.0 -->
-<!-- Copyright (C) 2022 OKTET Labs Ltd. All rights reserved. -->
+<!-- Copyright (C) 2023 OKTET Labs Ltd. All rights reserved. -->
 <package version="1.0">
     <description>Package for self-tests of lib/tool functions</description>
     <author mailto="Artem.Andreev@oktetlabs.ru"/>
@@ -37,6 +37,25 @@
 
         <run>
             <script name="enum_map"/>
+        </run>
+
+        <run>
+            <script name="expand"/>
+            <arg name="var1" list="">
+                <value>value1</value>
+                <value>-</value>
+            </arg>
+            <arg name="var2" list="">
+                <value>value2</value>
+                <value>-</value>
+            </arg>
+            <arg name="posarg" list="">
+                <value>positional</value>
+            </arg>
+            <arg name="expanded" list="">
+                <value>var1=value1 var2=value2 pos=positional unknown=value2 known=known nested=value1 end</value>
+                <value>var1= var2= pos=positional unknown= known=known nested= end</value>
+            </arg>
         </run>
 
         <run>

suites/selftest/conf/trc/tools.trc.xml
@@ -13,6 +13,8 @@
         <xi:include xmlns:xi="http://www.w3.org/2003/XInclude"
                     href="tools/enum_map.trc.xml" parse="xml"/>
         <xi:include xmlns:xi="http://www.w3.org/2003/XInclude"
+                    href="tools/expand.trc.xml" parse="xml"/>
+        <xi:include xmlns:xi="http://www.w3.org/2003/XInclude"
                     href="tools/extract_glob.trc.xml" parse="xml"/>
         <xi:include xmlns:xi="http://www.w3.org/2003/XInclude"
                     href="tools/hexdump.trc.xml" parse="xml"/>

suites/selftest/ts/tools/meson.build
@@ -1,9 +1,10 @@
 # SPDX-License-Identifier: Apache-2.0
-# Copyright (C) 2022-2022 OKTET Labs Ltd. All rights reserved.
+# Copyright (C) 2022-2023 OKTET Labs Ltd. All rights reserved.
 
 tests = [
     'compare_bufs',
     'enum_map',
+    'expand',
     'extract_glob',
     'hexdump',
     'intset',

commit 1f0dc4571f8bcf3359608acaa9616d62d4817034

suites/selftest: remove hardcoded expansion templates
Expansion patterns were hardcoded in tests for variable
expansion tests due to a bug that prevented the use of dollar
signs in package.xml parameters. Now the bug is fixed and
proper parameterisation can be used.

OL-Redmine-Id: 12845
Fixes: 1630e130316b ("suites/selftest: add a self test for variable expansion")
Fixes: 019c34af42ef ("lib/tapi: implement API to generate files from templates")
Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>

suites/selftest/conf/trc/tapi/file.trc.xml
@@ -29,6 +29,7 @@
             <notes/>
             <iter result="PASSED">
                 <arg name="env"/>
+                <arg name="template"/>
                 <arg name="varname"/>
                 <arg name="value"/>
                 <arg name="expected"/>

suites/selftest/conf/trc/tools/expand.trc.xml
@@ -5,6 +5,7 @@
     <objective>Testing variable expansion routines.</objective>
     <notes/>
     <iter result="PASSED">
+        <arg name="template" />
         <arg name="var1" />
         <arg name="var2" />
         <arg name="posarg" />

suites/selftest/ts/tapi/file/file_expand.c
@@ -23,10 +23,10 @@
 int
 main(int argc, char **argv)
 {
-    te_string template = TE_STRING_INIT;
     te_string filename = TE_STRING_INIT;
     te_string local = TE_STRING_INIT;
     rcf_rpc_server *pco_iut = NULL;
+    const char *template;
     const char *varname;
     const char *value;
     const char *expected;
@@ -39,23 +39,18 @@ main(int argc, char **argv)
     te_kvpair_init(&kvpairs);
 
     TEST_START;
+    TEST_GET_STRING_PARAM(template);
     TEST_GET_STRING_PARAM(varname);
     TEST_GET_OPT_STRING_PARAM(value);
     TEST_GET_OPT_STRING_PARAM(expected);
     TEST_GET_PCO(pco_iut);
 
-    /*
-     * FIXME: the template should actually be a parameter, but
-     * due to OL bug #12847 a parameter inside package.xml
-     * cannot contain shell-like variable references
-     */
-    te_string_append(&template, "%s=${%s}", varname, varname);
     CHECK_RC(te_kvpair_add(&kvpairs, varname, "%s",
                            te_str_empty_if_null(value)));
     tapi_file_make_name(&filename);
 
     TEST_STEP("Testing remote file expansion");
-    CHECK_RC(tapi_file_expand_kvpairs(pco_iut->ta, template.ptr, NULL, &kvpairs,
+    CHECK_RC(tapi_file_expand_kvpairs(pco_iut->ta, template, NULL, &kvpairs,
                                       "%s", filename.ptr));
     CHECK_RC(tapi_file_read_ta(pco_iut->ta, filename.ptr, &remote));
     if (strcmp(expected, remote) != 0)
@@ -65,7 +60,7 @@ main(int argc, char **argv)
     }
 
     TEST_STEP("Testing local file expansion");
-    CHECK_RC(tapi_file_expand_kvpairs(NULL, template.ptr, NULL, &kvpairs,
+    CHECK_RC(tapi_file_expand_kvpairs(NULL, template, NULL, &kvpairs,
                                       "%s/%s", tmpdir, filename.ptr));
     CHECK_RC(te_file_read_string(&local, FALSE, 0, "%s/%s", tmpdir,
                                  filename.ptr));
@@ -83,7 +78,6 @@ cleanup:
     CLEANUP_CHECK_RC(te_unlink_fmt("%s/%s", tmpdir, filename.ptr));
     free(remote);
     te_string_free(&filename);
-    te_string_free(&template);
     te_string_free(&local);
     te_kvpair_fini(&kvpairs);
     TEST_END;

suites/selftest/ts/tapi/file/package.xml
@@ -29,6 +29,9 @@
         <run>
             <script name="file_expand"/>
             <arg name="env" ref="env" />
+            <arg name="template" list="">
+                <value>var=${var}</value>
+            </arg>
             <arg name="varname" list="">
                 <value>var</value>
             </arg>

suites/selftest/ts/tools/expand.c
@@ -27,18 +27,10 @@
 #include "te_str.h"
 #include "te_string.h"
 
-/*
- * FIXME: the template should a parameter too, but
- * due to OL bug #12847 a parameter inside package.xml
- * cannot contain shell-like variable references
- */
-static const char template[] = "var1=${var1} var2=${var2:-unknown} "
-    "pos=${0} unknown=${unknown:-${var2}} known=${var2:+known} "
-    "nested=${var2:+${unknown:-${var1}}} notempty=${var2|notempty:-empty} end";
-
 int
 main(int argc, char **argv)
 {
+    const char *template;
     const char *var1;
     const char *var2;
     const char *posarg;
@@ -50,6 +42,7 @@ main(int argc, char **argv)
     te_kvpair_init(&kvpairs);
 
     TEST_START;
+    TEST_GET_STRING_PARAM(template);
     TEST_GET_OPT_STRING_PARAM(var1);
     TEST_GET_OPT_STRING_PARAM(var2);
     TEST_GET_STRING_PARAM(posarg);

suites/selftest/ts/tools/package.xml
@@ -60,6 +60,9 @@
             <session>
                 <run>
                     <script name="expand"/>
+                    <arg name="template">
+                        <value>var1=${var1} var2=${var2:-unknown} pos=${0} unknown=${unknown:-${var2}} known=${var2:+known} nested=${var2:+${unknown:-${var1}}} notempty=${var2|notempty:-empty} end</value>
+                    </arg>
                     <arg name="var1" list="">
                         <value>value1</value>
                         <value>-</value>