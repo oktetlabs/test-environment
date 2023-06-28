e81e68573be43ea35775b50b3c52bf6d7cd92aa4
Ivan Khodyrev <ivan.khodyrev@oktetlabs.ru>
2023-06-28 12:53:55 +0300

    engine/logger: add parsing structured logs for TE Tee
    te_tee has always output logs with the specified user and
    entity, which are not suitable in some cases. This patch
    adds a parser that takes the necessary data from a logs
    message.
    
    OL-Redmine-Id: 13022
    Signed-off-by: Ivan Khodyrev <ivan.khodyrev@oktetlabs.ru>
    Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>

--- engine/logger/te_tee.c
+++ engine/logger/te_tee.c
@@ -57,6 +57,8 @@
 #include "logger_ten.h"
 #include "te_raw_log.h"
 #include "te_str.h"
+#include "te_enum.h"
+#include "regex.h"
 
 #define DEFAULT_LL TE_LL_WARN
 #define DEFAULT_LOG_USER default_log_user
@@ -76,13 +78,72 @@ typedef struct log_msg {
 #define LOG_MESSAGE_INIT \
     {.level = 0, .entity = {0,}, .user = {0,}, .msg = TE_STRING_INIT}
 
+static const te_enum_map log_level_map[] = {
+    {.name = "ERROR",   .value = TE_LL_ERROR},
+    {.name = "HALT",    .value = TE_LL_ERROR},
+    {.name = "WARNING", .value = TE_LL_WARN},
+    {.name = "INFO",    .value = TE_LL_INFO},
+    TE_ENUM_MAP_END
+};
+
+static regex_t log_msg_sep_regex;
+static const char regex_str[] = "^([^-]+)-([^:]+): "
+                                "(HALT|ERROR|WARNING|INFO): "
+                                "(.*)$";
+
+static void
+parse_log_msg(const char *buffer, log_msg *parsed_msg)
+{
+    regmatch_t pmatch[5];
+    int res = 0;
+
+    res = regexec(&log_msg_sep_regex, buffer, TE_ARRAY_LEN(pmatch), pmatch, 0);
+    if (res != 0)
+    {
+        te_strlcpy(parsed_msg->entity, TE_LGR_ENTITY,
+                   sizeof(parsed_msg->entity));
+        te_strlcpy(parsed_msg->user, DEFAULT_LOG_USER,
+                   sizeof(parsed_msg->user));
+        parsed_msg->level = DEFAULT_LL;
+        te_string_append(&parsed_msg->msg, "%s", buffer);
+
+        return;
+    }
+
+    te_strlcpy(parsed_msg->entity, buffer + pmatch[1].rm_so,
+               MIN(pmatch[1].rm_eo - pmatch[1].rm_so + 1,
+                   sizeof(parsed_msg->entity)));
+
+    te_strlcpy(parsed_msg->user, buffer + pmatch[2].rm_so,
+               MIN(pmatch[2].rm_eo - pmatch[2].rm_so + 1,
+                   sizeof(parsed_msg->user)));
+
+    parsed_msg->level = te_enum_parse_longest_match(log_level_map, DEFAULT_LL,
+                                                    FALSE,
+                                                    buffer + pmatch[3].rm_so,
+                                                    NULL);
+
+    te_string_append_buf(&parsed_msg->msg, buffer + pmatch[4].rm_so,
+                         pmatch[4].rm_eo - pmatch[4].rm_so);
+}
+
+static inline te_bool
+is_log_msg_sender_equal(const log_msg *left, const log_msg *right)
+{
+    return (left->level == right->level) &&
+           (strcmp(left->entity, right->entity) == 0) &&
+           (strcmp(left->user, right->user) == 0);
+}
+
 static inline void
 flush_msg_buffer(log_msg* msg)
 {
-    if (msg->msg.len > 0)
+    if (*msg->entity != '\0')
     {
         TE_LOG(msg->level, msg->entity, msg->user, "%s", msg->msg.ptr);
 
+        memset(msg->entity, 0, sizeof(msg->entity));
+        memset(msg->user, 0, sizeof(msg->user));
         te_string_reset(&msg->msg);
     }
 }
@@ -91,9 +152,21 @@ static te_errno
 line_handler(char *line, void *user_data)
 {
     log_msg *cur_msg = (log_msg *)user_data;
+    log_msg new_msg = LOG_MESSAGE_INIT;
 
-    te_string_append(&cur_msg->msg, "%s\n", line);
-    flush_msg_buffer(cur_msg);
+    parse_log_msg(line, &new_msg);
+
+    if (is_log_msg_sender_equal(cur_msg, &new_msg))
+    {
+        te_string_append(&cur_msg->msg, "\n%s", new_msg.msg.ptr);
+        te_string_free(&new_msg.msg);
+    }
+    else
+    {
+        flush_msg_buffer(cur_msg);
+
+        *cur_msg = new_msg;
+    }
 
     return 0;
 }
@@ -108,6 +181,7 @@ main (int argc, char *argv[])
     struct pollfd  poller;
     te_string buffer_str = TE_STRING_INIT;
     log_msg cur_msg = LOG_MESSAGE_INIT;
+    size_t rc = 0;
 
 
     te_log_init("(Tee)", ten_log_message);
@@ -129,14 +203,12 @@ main (int argc, char *argv[])
 
     fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK | fcntl(STDIN_FILENO, F_GETFL));
 
-    te_strlcpy(cur_msg.entity, TE_LGR_ENTITY, sizeof(cur_msg.entity));
-    te_strlcpy(cur_msg.user, DEFAULT_LOG_USER, sizeof(cur_msg.user));
-    cur_msg.level = DEFAULT_LL;
+    rc = regcomp(&log_msg_sep_regex, regex_str, REG_EXTENDED);
+    if (rc != 0)
+        TE_FATAL_ERROR("Failed to compile a regular expression");
 
     for (;;)
     {
-        int rc;
-
         poller.fd = STDIN_FILENO;
         poller.revents = 0;
         poller.events = POLLIN;
@@ -192,6 +264,7 @@ main (int argc, char *argv[])
     }
 
     te_string_process_lines(&buffer_str, FALSE, line_handler, &cur_msg);
+    regfree(&log_msg_sep_regex);
 
     return EXIT_SUCCESS;
 }
