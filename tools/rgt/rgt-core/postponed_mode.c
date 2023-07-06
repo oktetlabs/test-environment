oktetlabs/test-environment

commit 451d0d6c68cb56809913f3e0ab0315f1e42a7496

tools/rgt: improve handling of MI artifacts
MI artifacts are not human-readable and often contain a lot of raw data.
It seems better not to include them into places like <meta> sections
in XML logs or "gist logs" in RAW log bundle and list only
human-readable plain text artifacts there.

OL-Redmine-Id: 12861
Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
Acked-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

tools/rgt/rgt-core/postponed_mode.c
@@ -215,10 +215,13 @@ process_result_msg(gpointer data, gpointer user_data,
     UNUSED(user_data);
 
     msg = log_msg_read(msg_ptr);
-    fprintf(rgt_ctx.out_fd, "<%s level=\"%s\">", tag, msg->level_str);
-    output_regular_log_msg(msg);
+    if (~msg->level & TE_LL_MI)
+    {
+        fprintf(rgt_ctx.out_fd, "<%s level=\"%s\">", tag, msg->level_str);
+        output_regular_log_msg(msg);
+        fprintf(rgt_ctx.out_fd, "</%s>\n", tag);
+    }
     free_log_msg(msg);
-    fprintf(rgt_ctx.out_fd, "</%s>\n", tag);
 }
 
 /*
@@ -350,7 +353,7 @@ postponed_process_start_event(node_info_t *node, const char *node_name,
             fputs("</verdicts>\n", rgt_ctx.out_fd);
         }
 
-        if (!msg_queue_is_empty(&data->artifacts))
+        if (!msg_queue_is_empty(&data->artifacts) && data->not_mi_artifacts)
         {
             fputs("<artifacts>", rgt_ctx.out_fd);
             msg_queue_foreach(&data->artifacts, process_artifact_cb, NULL);

tools/rgt/rgt-core/flow_tree.c
@@ -247,6 +247,7 @@ ctrl_msg_data_init(ctrl_msg_data *data)
 {
     msg_queue_init(&data->verdicts);
     msg_queue_init(&data->artifacts);
+    data->not_mi_artifacts = FALSE;
 }
 
 /**
@@ -1436,6 +1437,8 @@ flow_tree_attach_message(log_msg *msg)
             {
                 msg_queue_attach(&cur_node->ctrl_data.artifacts,
                                  log_msg_ref(msg));
+                if (~msg->level & TE_LL_MI)
+                    cur_node->ctrl_data.not_mi_artifacts = TRUE;
             }
             else
             {

tools/rgt/rgt-core/index_mode.c
@@ -188,7 +188,15 @@ index_process_regular_msg(log_msg *msg)
 
     UNUSED(msg);
 
-    if ((msg->flags & (RGT_MSG_FLG_VERDICT | RGT_MSG_FLG_ARTIFACT)) ||
+    /*
+     * Do not add MI artifacts to the start fragment - they are not
+     * human-readable and can contain a lot of data.
+     */
+    if ((msg->flags & RGT_MSG_FLG_ARTIFACT) &&
+        (~msg->level & TE_LL_MI))
+        to_start_frag = 1;
+
+    if ((msg->flags & RGT_MSG_FLG_VERDICT) ||
         strcmp(msg->user, "TRC tags") == 0)
         to_start_frag = 1;
 

tools/rgt/rgt-core/log_msg.h
@@ -187,6 +187,12 @@ typedef struct node_info {
 typedef struct ctrl_msg_data {
     msg_queue verdicts;    /**< Test verdicts */
     msg_queue artifacts;   /**< Test artifacts */
+
+    /**
+     * If TRUE, indicates that at least some human readable
+     * artifacts are present.
+     */
+    te_bool not_mi_artifacts;
 } ctrl_msg_data;
 
 /**

tools/rgt/rgt-format/xml2html-multi/xml2html-multi.c
@@ -78,11 +78,6 @@ typedef struct depth_ctx_user {
                                       value - array of user names */
     uint32_t linum; /**< Line number in HTML */
 
-    te_bool ignore_artifact; /**< If @c TRUE, the currently processed
-                                  artifact should be ignored - this
-                                  is used for filtering out MI
-                                  artifacts in summary */
-
     te_dbuf json_data;    /**< Buffer for collecting JSON before it can
                                be parsed */
 
@@ -1662,14 +1657,11 @@ DEF_FUNC_WITHOUT_ATTRS(proc_log_msg_file_end, LOG_MSG_FILE_END)
 RGT_DEF_FUNC(proc_meta_artifact_start)
 {
     depth_ctx_user_t *depth_user = (depth_ctx_user_t *)depth_ctx->user_data;
-    const char *level = rgt_tmpls_xml_attrs_get(xml_attrs, "level");
     FILE *fd = depth_user->fd;
 
     RGT_FUNC_UNUSED_PRMS();
 
-    if (level != NULL && strcmp(level, "MI") == 0)
-        depth_user->ignore_artifact = TRUE;
-    else if (fd != NULL)
+    if (fd != NULL)
         rgt_tmpls_output(fd, &xml2fmt_tmpls[META_ARTIFACT_START], NULL);
 }
 
@@ -1680,10 +1672,8 @@ RGT_DEF_FUNC(proc_meta_artifact_end)
 
     RGT_FUNC_UNUSED_PRMS();
 
-    if (!(depth_user->ignore_artifact) && fd != NULL)
+    if (fd != NULL)
         rgt_tmpls_output(fd, &xml2fmt_tmpls[META_ARTIFACT_END], NULL);
-
-    depth_user->ignore_artifact = FALSE;
 }
 
 /**
@@ -1753,9 +1743,6 @@ proc_chars(rgt_gen_ctx_t *ctx, rgt_depth_ctx_t *depth_ctx,
     TE_COMPILE_TIME_ASSERT(sizeof(rgt_xmlChar) == 1);
 
     if (fd == NULL)
-        return;
-
-    if (depth_user->ignore_artifact)
         return;
 
     if (depth_user->log_level != NULL &&
@@ -1915,7 +1902,6 @@ alloc_depth_user_data(uint32_t depth)
     assert(g_ptr_array_index(depth_array, depth - 1) != NULL);
 
     depth_user = g_ptr_array_index(depth_array, depth - 1);
-    depth_user->ignore_artifact = FALSE;
     depth_user->log_level = NULL;
     depth_user->json_data = (te_dbuf)TE_DBUF_INIT(0);
     depth_user->no_logs = TRUE;

commit 88620c4921231e3ae5e9fcd0a4d9f66b9371596c

tools: add option to include MI artifacts in XML <meta>
Bublik expects MI artifacts to be present in <meta> section
of XML log generated by te-trc-log. By default they are not
included there because they can be lengthy and are not meant
for human users of TRC reports.

Fixes: 451d0d6c68cb ("tools/rgt: improve handling of MI artifacts")
OL-Redmine-Id: 13060
Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

tools/rgt/rgt-core/postponed_mode.c
@@ -215,7 +215,7 @@ process_result_msg(gpointer data, gpointer user_data,
     UNUSED(user_data);
 
     msg = log_msg_read(msg_ptr);
-    if (~msg->level & TE_LL_MI)
+    if (~msg->level & TE_LL_MI || rgt_ctx.mi_meta)
     {
         fprintf(rgt_ctx.out_fd, "<%s level=\"%s\">", tag, msg->level_str);
         output_regular_log_msg(msg);
@@ -353,7 +353,8 @@ postponed_process_start_event(node_info_t *node, const char *node_name,
             fputs("</verdicts>\n", rgt_ctx.out_fd);
         }
 
-        if (!msg_queue_is_empty(&data->artifacts) && data->not_mi_artifacts)
+        if (!msg_queue_is_empty(&data->artifacts) &&
+            (data->not_mi_artifacts || rgt_ctx.mi_meta))
         {
             fputs("<artifacts>", rgt_ctx.out_fd);
             msg_queue_foreach(&data->artifacts, process_artifact_cb, NULL);

tools/rgt/rgt-core/rgt-conv.in
@@ -21,6 +21,7 @@ prog_name=`basename $0`
 # This script gets the following parameters:
 #   -m MODE, --mode=MODE        Choose mode of RGT opertation.
 #   --no-cntrl-msg              Process Tester control messages as ordinary
+#   --mi-meta                   Include MI artifacts in <meta>
 #   -c FILE, --cfg-filter=FILE  Specify XMl filter file name.
 #   -f FILE, --raw-log=FILE     Specify Raw Log file name.
 #   -o FILE, --output=FILE      Output file name.
@@ -43,6 +44,7 @@ Usage: $prog_name [OPTION...] [<Raw Log file> [<Output file>]]
                            (postponed by default).
 
   --no-cntrl-msg           Process Tester control messages as ordinary.
+  --mi-meta                Include MI artifacts in <meta>
 
   --incomplete-log         Do not shout on truncated log report, but complete
                            it automatically.
@@ -172,6 +174,11 @@ do
     --no-cntrl-msg)
         extra_flags="$extra_flags $1"
         ;;
+
+    --mi-meta)
+        extra_flags="$extra_flags $1"
+        ;;
+
     --incomplete-log)
         extra_flags="$extra_flags $1"
         ;;

tools/rgt/rgt-core/rgt_common.h
@@ -208,6 +208,9 @@ typedef struct rgt_gen_ctx {
     te_bool         proc_cntrl_msg; /**< Whether Rgt should process control
                                          messages or not */
 
+    /** Include MI artifacts in <meta> section */
+    te_bool mi_meta;
+
     te_bool         proc_incomplete; /**< Whether Rgt should process
                                           incomplete log reports as normal
                                           or give error message */

tools/rgt/rgt-core/rgt_core.c
@@ -136,6 +136,10 @@ process_cmd_line_opts(int argc, char **argv, rgt_gen_ctx_t *ctx)
           "Process TESTER control messages as ordinary: do not process "
           "test flow structure.", NULL },
 
+        { "mi-meta", '\0', POPT_ARG_NONE, NULL, 'e',
+          "Include MI artifacts in <meta> section of XML log",
+          NULL },
+
         { "incomplete-log", '\0', POPT_ARG_NONE, NULL, 'i',
           "Do not shout on truncated log report, but complete it "
           "automatically.", NULL },
@@ -220,6 +224,10 @@ process_cmd_line_opts(int argc, char **argv, rgt_gen_ctx_t *ctx)
             case 'n':
                 /* User do not want us to process control messages */
                 ctx->proc_cntrl_msg = FALSE;
+                break;
+
+            case 'e':
+                ctx->mi_meta = TRUE;
                 break;
 
             case 'i':
@@ -519,6 +527,7 @@ rgt_ctx_set_defaults(rgt_gen_ctx_t *ctx)
     ctx->op_mode = RGT_OP_MODE_DEFAULT;
     ctx->op_mode_str = RGT_OP_MODE_DEFAULT_STR;
     ctx->proc_cntrl_msg = TRUE;
+    ctx->mi_meta = FALSE;
     ctx->proc_incomplete = FALSE;
     ctx->verb = FALSE;
     ctx->tmp_dir = NULL;

tools/trc/trc-log/te-trc-log
@@ -27,6 +27,7 @@ OPTIONS:
   -o <output-file>          Specify output file name
   -z                        Compress output using gzip
   -j                        Compress output using bzip2
+  --mi-meta                 Include MI artifacts in log
 
   --help                    Show this help message
 
@@ -53,6 +54,9 @@ while test -z "$end_of_options" -a -n "$1" ; do
                 ;;
         -o)     shift 1
                 output_file="$1"
+                ;;
+
+        --mi-meta) rgt_opts+=("--mi-meta")
                 ;;
 
         --)     end_of_options=yes