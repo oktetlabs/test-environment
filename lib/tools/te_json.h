oktetlabs/test-environment

commit d0ca768b54df8fa87a60ed45422541b4c573baa2

lib/tools: implement simple JSON serializing functions
te_mi_logger have a lot of ad hoc JSON formatting which
is hard to maintain and is not robust. This patch provides
a consistent API to generate JSON values, which is going
to be used by te_mi_logger later. The API can also be used
to support JSON generation in tapi_tool for tools that use
JSON config format.

The API is not intended to replace a full-fledges JSON library,
its primary usecase is serialization of relatively simple C
structures where using e.g. Jansson would lead to a lot of
unnecesary boilerplate.

OL-Redmine-Id: 12514
Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Ivan Melnikov <ivan.melnikov@oktetlabs.ru>
Reviewed-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>

lib/tools/te_json.h
@@ -1,0 +1,280 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (C) 2023 OKTET Labs Ltd. All rights reserved. */
+/** @file
+ * @brief JSON-handling routines.
+ *
+ * @defgroup te_tools_te_json JSON routines.
+ * @ingroup te_tools
+ * @{
+ *
+ * These functions provide an easy but robust way to convert
+ * native data to JSON format.
+ *
+ * Note, however, that it is not a full-fledged JSON serializer,
+ * so it is still possible to generate invalid JSON if the API is
+ * used carelessly.
+ *
+ * Since they are intended to be used with C data of known layout,
+ * they do not report errors: if anything goes wrong, it's considered
+ * API contract violation and an assertion fires.
+ */
+
+#ifndef __TE_JSON_H__
+#define __TE_JSON_H__
+
+#include <inttypes.h>
+#include <math.h>
+
+#include "te_errno.h"
+#include "te_defs.h"
+#include "te_string.h"
+#include "te_kvpair.h"
+#include "te_enum.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** The kind of JSON compound. */
+typedef enum te_json_compound {
+    TE_JSON_COMPOUND_TOPLEVEL,     /**< toplevel */
+    TE_JSON_COMPOUND_ARRAY,        /**< array */
+    TE_JSON_COMPOUND_OBJECT,       /**< object (dictionary) */
+    TE_JSON_COMPOUND_OBJECT_VALUE, /**< object value */
+} te_json_compound;
+
+/** Maximum nesting level for JSON serialization */
+#define TE_JSON_MAX_NEST 16
+
+/** One level of JSON value nesting */
+typedef struct te_json_level_t {
+    /** the kind of compound at this level  */
+    te_json_compound kind;
+    /** number of items already added at this level */
+    size_t n_items;
+} te_json_level_t;
+
+/**
+ * The context for JSON serialization.
+ *
+ * @note While the structure is declared public to make on-stack variables
+ *       possible, it shall be treated as an opaque and only initialized
+ *       with TE_JSON_INIT_STR() and then passed to the API from this group.
+ */
+typedef struct te_json_ctx_t {
+    /** JSON */
+    te_string *dest;
+
+    /** stack of nested JSON compounds */
+    te_json_level_t nesting[TE_JSON_MAX_NEST];
+
+    /** current nesting depth */
+    size_t current_level;
+} te_json_ctx_t;
+
+/**
+ * Initializer for a JSON context.
+ *
+ * @param str_   destination string
+ */
+#define TE_JSON_INIT_STR(str_) \
+    {                                                               \
+        .dest = (str_),                                             \
+        .nesting = {[0] = {                                         \
+                .kind = TE_JSON_COMPOUND_TOPLEVEL,                  \
+                .n_items = 0}},                                     \
+        .current_level = 0                                          \
+    }
+
+/**
+ * Serialize a simple JSON value formatted according to @p fmt.
+ *
+ * @attention
+ * This function does no escaping, so is not intended for general usage.
+ * Normally one should use one of type-specific functions.
+ *
+ * @param ctx JSON context
+ * @param fmt format string
+ * @param ... arguments
+ *
+ * @sa te_json_add_null, te_json_add_integer,
+ *     te_json_add_float, te_json_add_string
+ */
+extern void te_json_add_simple(te_json_ctx_t *ctx,
+                               const char *fmt, ...)
+    __attribute__((format(printf, 2, 3)));
+
+/**
+ * Serialize JSON null.
+ *
+ * @param ctx JSON context
+ */
+static inline void
+te_json_add_null(te_json_ctx_t *ctx)
+{
+    te_json_add_simple(ctx, "null");
+}
+
+/**
+ * Serialize a JSON boolean.
+ *
+ * @param ctx JSON context
+ * @param val boolean value
+ */
+static inline void
+te_json_add_bool(te_json_ctx_t *ctx, te_bool val)
+{
+    te_json_add_simple(ctx, val ? "true" : "false");
+}
+
+
+/**
+ * Serialize a JSON integer value.
+ *
+ * @param ctx JSON context
+ * @param val integral value
+ */
+static inline void
+te_json_add_integer(te_json_ctx_t *ctx, intmax_t val)
+{
+    te_json_add_simple(ctx, "%jd", val);
+}
+
+/**
+ * Serialize a JSON floating value.
+ *
+ * If @p val is not finite (infinity or NaN), @c null is serialized.
+ *
+ * @param ctx       JSON context
+ * @param val       Value
+ * @param precision Floating-point precision
+ */
+static inline void
+te_json_add_float(te_json_ctx_t *ctx, double val, unsigned int precision)
+{
+    if (!isfinite(val))
+        te_json_add_null(ctx);
+    else
+        te_json_add_simple(ctx, "%.*g", precision, val);
+}
+
+/**
+ * Serialize a string value escaping it if necessary.
+ *
+ * Namely, double quotes, backslashes and control characters are
+ * properly escaped. No special provision is made for Unicode non-ASCII
+ * characters, though. Embedded zeroes are not supported.
+ *
+ * Double quotes are added around the value.
+ *
+ * @param ctx      JSON context
+ * @param fmt      format string
+ * @param ...      arguments
+ */
+extern void te_json_add_string(te_json_ctx_t *ctx,
+                               const char *fmt, ...)
+    __attribute__((format(printf, 2, 3)));;
+
+/**
+ * Start serializing a JSON array.
+ *
+ * te_json_end() shall be called after all items are serialized.
+ *
+ * @param ctx JSON context
+ */
+extern void te_json_start_array(te_json_ctx_t *ctx);
+
+/**
+ * Start serializing a JSON object.
+ *
+ * te_json_end() shall be called after all items are serialized.
+ *
+ * @param ctx JSON context
+ */
+extern void te_json_start_object(te_json_ctx_t *ctx);
+
+/**
+ * Finalize the current JSON value nesting.
+ *
+ * This function must be called after te_json_start_object()
+ * or te_json_start_array(). It may be called at the toplevel
+ * and has no effect in this case.
+ *
+ * @param ctx JSON context
+ */
+extern void te_json_end(te_json_ctx_t *ctx);
+
+/**
+ * Mark the beginning of a new key in an object.
+ *
+ * @param ctx  JSON context
+ * @param key  key (it is assumed that it does not need escaping);
+ *             @c NULL value is treated as an empty string
+ */
+extern void te_json_add_key(te_json_ctx_t *ctx, const char *key);
+
+/**
+ * Output a new key of an object with a given string value.
+ *
+ * If @p val is @c NULL, nothing is added, not a null value.
+ *
+ * @param ctx  JSON context
+ * @param key  key
+ * @param val  string value
+ */
+static inline void
+te_json_add_key_str(te_json_ctx_t *ctx, const char *key, const char *val)
+{
+    if (val != NULL)
+    {
+        te_json_add_key(ctx, key);
+        te_json_add_string(ctx, "%s", val);
+    }
+}
+
+/**
+ * Output a new key with an enumeration-like value.
+ *
+ * The string representation of a value is produced by applying @p map
+ * to @p val. If @p val is not found in @p map or if it maps to @c NULL
+ * explicitly, the key is omitted.
+ *
+ * @param ctx      JSON context
+ * @param map      enum-to-string mapping
+ * @param key      key
+ * @param val      enum value
+ */
+static inline void
+te_json_add_key_enum(te_json_ctx_t *ctx, const te_enum_map *map,
+                     const char *key, int val)
+{
+    te_json_add_key_str(ctx, key, te_enum_map_from_any_value(map, val, NULL));
+}
+
+/**
+ * Serialize an array of strings.
+ *
+ * If @p skip_null is @c TRUE, @c NULL values in @p strs
+ * will be skipped, otherwise serialized as JSON @c null.
+ *
+ * @param ctx       JSON context
+ * @param skip_null null handling mode
+ * @param n_strs    number of elements in @p strs
+ * @param strs      array of strings
+ */
+extern void te_json_add_array_str(te_json_ctx_t *ctx, te_bool skip_null,
+                                  size_t n_strs, const char *strs[n_strs]);
+
+/**
+ * Serialize a list of kv_pairs as a JSON object.
+ *
+ * @param ctx  JSON context
+ * @param head Key-value list
+ */
+extern void te_json_add_kvpair(te_json_ctx_t *ctx, const te_kvpair_h *head);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+#endif /* !__TE_JSON_H__ */
+/**@} <!-- END te_tools_te_json --> */

lib/tools/meson.build
@@ -24,6 +24,7 @@ headers += files(
     'te_intset.h',
     'te_ipstack.h',
     'te_iscsi.h',
+    'te_json.h',
     'te_kernel_log.h',
     'te_kvpair.h',
     'te_log_fmt.h',
@@ -64,6 +65,7 @@ sources += files(
     'te_format.c',
     'te_hex_diff_dump.c',
     'te_intset.c',
+    'te_json.c',
     'te_ipstack.c',
     'te_kernel_log.c',
     'te_kvpair.c',

lib/tools/te_json.c
@@ -1,0 +1,192 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (C) 2023 OKTET Labs Ltd. All rights reserved. */
+/** @file
+ * @brief JSON routines.
+ *
+ * Routines to serialize data as JSON.
+ */
+
+#include "te_config.h"
+
+#include <assert.h>
+#include <ctype.h>
+
+#include "logger_api.h"
+
+#include "te_defs.h"
+#include "te_str.h"
+#include "te_json.h"
+
+static void
+maybe_add_separator(te_json_ctx_t *ctx)
+{
+    te_json_level_t *level = &ctx->nesting[ctx->current_level];
+
+    if (level->kind == TE_JSON_COMPOUND_OBJECT_VALUE)
+    {
+        level->kind = TE_JSON_COMPOUND_OBJECT;
+    }
+    else
+    {
+        if (level->n_items > 0)
+            te_string_append(ctx->dest, ",");
+        level->n_items++;
+    }
+}
+
+void
+te_json_add_simple(te_json_ctx_t *ctx, const char *fmt, ...)
+{
+    va_list args;
+
+    maybe_add_separator(ctx);
+
+    va_start(args, fmt);
+    te_string_append_va(ctx->dest, fmt, args);
+    va_end(args);
+}
+
+void
+te_json_add_string(te_json_ctx_t *ctx, const char *fmt, ...)
+{
+    va_list args;
+    te_string inner = TE_STRING_INIT;
+    const char *iter;
+
+    maybe_add_separator(ctx);
+
+    va_start(args, fmt);
+    te_string_append_va(&inner, fmt, args);
+    va_end(args);
+
+    te_string_append(ctx->dest, "%s", "\"");
+    for (iter = inner.ptr; *iter != '\0'; iter++)
+    {
+        switch (*iter)
+        {
+            case '\\':
+            case '\"':
+            case '/':
+                te_string_append(ctx->dest, "\\%c", *iter);
+                break;
+            case '\b':
+                te_string_append(ctx->dest, "\\b");
+                break;
+            case '\f':
+                te_string_append(ctx->dest, "\\f");
+                break;
+            case '\n':
+                te_string_append(ctx->dest, "\\n");
+                break;
+            case '\r':
+                te_string_append(ctx->dest, "\\r");
+                break;
+            case '\t':
+                te_string_append(ctx->dest, "\\t");
+                break;
+            default:
+                if (iscntrl(*iter))
+                {
+                    te_string_append(ctx->dest, "\\u%4.4" PRIx8,
+                                     (uint8_t)*iter);
+                }
+                else
+                {
+                    te_string_append(ctx->dest, "%c", *iter);
+                }
+                break;
+        }
+    }
+    te_string_append(ctx->dest, "%s", "\"");
+    te_string_free(&inner);
+}
+
+static void
+push_json_level(te_json_ctx_t *ctx, te_json_compound new_kind)
+{
+    assert(ctx->current_level + 1 < TE_ARRAY_LEN(ctx->nesting));
+    ctx->current_level++;
+    ctx->nesting[ctx->current_level].kind = new_kind;
+    ctx->nesting[ctx->current_level].n_items = 0;
+}
+
+void
+te_json_start_array(te_json_ctx_t *ctx)
+{
+    maybe_add_separator(ctx);
+    push_json_level(ctx, TE_JSON_COMPOUND_ARRAY);
+    te_string_append(ctx->dest, "[");
+}
+
+void
+te_json_start_object(te_json_ctx_t *ctx)
+{
+    maybe_add_separator(ctx);
+    push_json_level(ctx, TE_JSON_COMPOUND_OBJECT);
+    te_string_append(ctx->dest, "{");
+}
+
+void
+te_json_end(te_json_ctx_t *ctx)
+{
+    switch (ctx->nesting[ctx->current_level].kind)
+    {
+        case TE_JSON_COMPOUND_TOPLEVEL:
+            /* do nothing */
+            break;
+        case TE_JSON_COMPOUND_ARRAY:
+            te_string_append(ctx->dest, "]");
+            break;
+        case TE_JSON_COMPOUND_OBJECT:
+            te_string_append(ctx->dest, "}");
+            break;
+        case TE_JSON_COMPOUND_OBJECT_VALUE:
+            TE_FATAL_ERROR("Incomplete object value");
+            break;
+    }
+    if (ctx->current_level != 0)
+        ctx->current_level--;
+}
+
+void
+te_json_add_key(te_json_ctx_t *ctx, const char *key)
+{
+    assert(ctx->nesting[ctx->current_level].kind == TE_JSON_COMPOUND_OBJECT);
+    maybe_add_separator(ctx);
+
+    te_string_append(ctx->dest, "\"%s\":", te_str_empty_if_null(key));
+    ctx->nesting[ctx->current_level].kind = TE_JSON_COMPOUND_OBJECT_VALUE;
+}
+
+void
+te_json_add_array_str(te_json_ctx_t *ctx, te_bool skip_null,
+                      size_t n_strs, const char *strs[n_strs])
+{
+    size_t i;
+
+    te_json_start_array(ctx);
+    for (i = 0; i < n_strs; i++)
+    {
+        if (strs[i] != NULL)
+            te_json_add_string(ctx, "%s", strs[i]);
+        else if (!skip_null)
+            te_json_add_null(ctx);
+    }
+    te_json_end(ctx);
+}
+
+void
+te_json_add_kvpair(te_json_ctx_t *ctx, const te_kvpair_h *head)
+{
+    te_kvpair *p;
+
+    assert(head != NULL);
+
+    te_json_start_object(ctx);
+    TAILQ_FOREACH(p, head, links)
+    {
+        te_json_add_key(ctx, p->key);
+        te_json_add_string(ctx, "%s", p->value);
+    }
+    te_json_end(ctx);
+}

suites/selftest/conf/trc/tools.trc.xml
@@ -19,6 +19,8 @@
         <xi:include xmlns:xi="http://www.w3.org/2003/XInclude"
                     href="tools/intset.trc.xml" parse="xml"/>
         <xi:include xmlns:xi="http://www.w3.org/2003/XInclude"
+                    href="tools/json.trc.xml" parse="xml"/>
+        <xi:include xmlns:xi="http://www.w3.org/2003/XInclude"
                     href="tools/make_bufs.trc.xml" parse="xml"/>
         <xi:include xmlns:xi="http://www.w3.org/2003/XInclude"
                     href="tools/scandir.trc.xml" parse="xml"/>

suites/selftest/conf/trc/tools/json.trc.xml
@@ -1,0 +1,6 @@
+<test name="json" type="script">
+    <objective>Testing JSON generating routines.</objective>
+    <notes/>
+    <iter result="PASSED">
+    </iter>
+</test>

suites/selftest/ts/tools/json.c
@@ -1,0 +1,269 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (C) 2023 OKTET Labs Ltd. All rights reserved. */
+/** @file
+ * @brief Test for JSON generating functions
+ *
+ * Testing JSON generating routines.
+ */
+
+/** @page tools_json JSON generation test
+ *
+ * @objective Testing JSON generating routines.
+ *
+ * Test the correctness of JSON generating functions.
+ *
+ * @par Test sequence:
+ */
+
+/** Logging subsystem entity name */
+#define TE_TEST_NAME    "tools/json"
+
+#include "te_config.h"
+
+#include "tapi_test.h"
+#include "te_json.h"
+
+static void
+do_json_string(te_json_ctx_t *ctx, void *val)
+{
+    te_json_add_string(ctx, "%s", (const char *)val);
+}
+
+static void
+do_json_int(te_json_ctx_t *ctx, void *val)
+{
+    te_json_add_integer(ctx, *(intmax_t *)val);
+}
+
+static void
+do_json_float(te_json_ctx_t *ctx, void *val)
+{
+    te_json_add_float(ctx, *(double *)val, 6);
+}
+
+static void
+do_json_array(te_json_ctx_t *ctx, void *val)
+{
+    const char **strs = val;
+    unsigned int i;
+
+    te_json_start_array(ctx);
+    for (i = 0; strs[i] != NULL; i++)
+        te_json_add_string(ctx, "%s", strs[i]);
+
+    te_json_end(ctx);
+}
+
+typedef struct key_value {
+    const char *key;
+    const char *value;
+} key_value;
+
+static void
+do_json_object(te_json_ctx_t *ctx, void *val)
+{
+    key_value *kv = val;
+    unsigned int i;
+
+    te_json_start_object(ctx);
+    for (i = 0; kv[i].key != NULL; i++)
+    {
+        te_json_add_key(ctx, kv[i].key);
+        te_json_add_string(ctx, "%s", kv[i].value);
+    }
+    te_json_end(ctx);
+}
+
+static void
+do_json_optkeys(te_json_ctx_t *ctx, void *val)
+{
+    key_value *kv = val;
+    unsigned int i;
+
+    te_json_start_object(ctx);
+    for (i = 0; kv[i].key != NULL; i++)
+        te_json_add_key_str(ctx, kv[i].key, kv[i].value);
+    te_json_end(ctx);
+}
+
+static void
+do_json_kvpair(te_json_ctx_t *ctx, void *val)
+{
+    key_value *kv = val;
+    unsigned int i;
+    te_kvpair_h kvp;
+
+    te_kvpair_init(&kvp);
+    for (i = 0; kv[i].key != NULL; i++)
+        te_kvpair_add(&kvp, kv[i].key, "%s", kv[i].value);
+    te_json_add_kvpair(ctx, &kvp);
+    te_kvpair_fini(&kvp);
+}
+
+static void
+do_json_array_of_arrays(te_json_ctx_t *ctx, void *val)
+{
+    int **rows = val;
+    unsigned int i;
+
+    te_json_start_array(ctx);
+    for (i = 0; rows[i] != NULL; i++)
+    {
+        unsigned int j;
+
+        te_json_start_array(ctx);
+
+        for (j = 0; rows[i][j] != 0; j++)
+            te_json_add_integer(ctx, rows[i][j]);
+
+        te_json_end(ctx);
+    }
+    te_json_end(ctx);
+}
+
+typedef struct array_of_str {
+    te_bool skip_null;
+    unsigned int n_strs;
+    const char **strs;
+} array_of_str;
+
+static void
+do_json_array_of_str(te_json_ctx_t *ctx, void *val)
+{
+    array_of_str *array = val;
+
+    te_json_add_array_str(ctx, array->skip_null, array->n_strs, array->strs);
+}
+
+static void
+check_json(void *val, void (*func)(te_json_ctx_t *ctx, void *),
+           const char *expected)
+{
+    te_string dest = TE_STRING_INIT;
+    te_json_ctx_t ctx = TE_JSON_INIT_STR(&dest);
+
+    func(&ctx, val);
+
+    if (ctx.current_level != 0)
+        TEST_VERDICT("Invalid JSON nesting");
+
+    if (strcmp(dest.ptr, expected) != 0)
+    {
+        ERROR("Unexpected JSON escaping: %s (expected %s)", dest.ptr, expected);
+        TEST_VERDICT("JSON escaping is wrong");
+    }
+
+    te_string_free(&dest);
+}
+
+int
+main(int argc, char **argv)
+{
+    TEST_START;
+
+    TEST_STEP("Checking JSON integers");
+    check_json((intmax_t[]){0}, do_json_int, "0");
+    check_json((intmax_t[]){INT_MAX}, do_json_int, "2147483647");
+    check_json((intmax_t[]){-1}, do_json_int, "-1");
+
+    TEST_STEP("Checking JSON floats");
+    check_json((double[]){0.0}, do_json_float, "0");
+    check_json((double[]){0.5}, do_json_float, "0.5");
+    check_json((double[]){-1.0}, do_json_float, "-1");
+    check_json((double[]){1e6}, do_json_float, "1e+06");
+    check_json((double[]){INFINITY}, do_json_float, "null");
+    check_json((double[]){NAN}, do_json_float, "null");
+
+    TEST_STEP("Checking JSON string escaping");
+    check_json("", do_json_string, "\"\"");
+    check_json("abc def", do_json_string, "\"abc def\"");
+    check_json("\x1\a\b\f\n\r\t\v\\/\"\x7F",
+               do_json_string,
+               "\"\\u0001\\u0007\\b\\f\\n\\r\\t\\u000b"
+               "\\\\\\/\\\"\\u007f\"");
+
+    TEST_STEP("Checking JSON arrays");
+    check_json((const char *[]){NULL}, do_json_array, "[]");
+    check_json((const char *[]){"a", NULL}, do_json_array, "[\"a\"]");
+    check_json((const char *[]){"a", "b", NULL}, do_json_array,
+               "[\"a\",\"b\"]");
+
+    TEST_STEP("Checking JSON objects");
+    check_json((key_value []){{.key = NULL}}, do_json_object, "{}");
+    check_json((key_value []){{.key = "a", .value = "b"}, {.key = NULL}},
+               do_json_object, "{\"a\":\"b\"}");
+    check_json((key_value []){{.key = "a", .value = "b"},
+                              {.key = "c", .value = "d"},
+                              {.key = NULL}},
+               do_json_object, "{\"a\":\"b\",\"c\":\"d\"}");
+
+    TEST_STEP("Checking JSON objects with optional keys");
+    check_json((key_value []){{.key = NULL}}, do_json_optkeys, "{}");
+    check_json((key_value []){{.key = "a", .value = "b"}, {.key = NULL}},
+               do_json_optkeys, "{\"a\":\"b\"}");
+    check_json((key_value []){{.key = "c", .value = NULL}, {.key = NULL}},
+               do_json_optkeys, "{}");
+    check_json((key_value []){{.key = "a", .value = "b"},
+                              {.key = "c", .value = "\n"},
+                              {.key = NULL}},
+               do_json_optkeys, "{\"a\":\"b\",\"c\":\"\\n\"}");
+    check_json((key_value []){{.key = "a", .value = NULL},
+                              {.key = "c", .value = "\n"},
+                              {.key = NULL}},
+               do_json_optkeys, "{\"c\":\"\\n\"}");
+
+    TEST_STEP("Checking JSON arrays of arrays");
+    check_json((int *[]){NULL}, do_json_array_of_arrays, "[]");
+    check_json((int *[]){(int[]){1, 0}, NULL},
+               do_json_array_of_arrays, "[[1]]");
+    check_json((int *[]){(int[]){0}, NULL},
+               do_json_array_of_arrays, "[[]]");
+    check_json((int *[]){(int[]){1, 2, 0}, NULL},
+               do_json_array_of_arrays, "[[1,2]]");
+    check_json((int *[]){(int[]){1, 2, 0}, (int[]){3, 4, 0}, NULL},
+               do_json_array_of_arrays, "[[1,2],[3,4]]");
+
+    TEST_STEP("Checking JSON arrays of strings");
+    check_json((array_of_str []){{.skip_null = TRUE,
+                    .n_strs = 0, .strs = NULL}},
+        do_json_array_of_str, "[]");
+    check_json((array_of_str []){{.skip_null = TRUE,
+                    .n_strs = 1,
+                    .strs = (const char *[]){"abc"}}},
+        do_json_array_of_str, "[\"abc\"]");
+    check_json((array_of_str []){{.skip_null = TRUE,
+                    .n_strs = 2,
+                    .strs = (const char *[]){"abc", "def"}}},
+        do_json_array_of_str, "[\"abc\",\"def\"]");
+    check_json((array_of_str []){{.skip_null = TRUE,
+                    .n_strs = 1,
+                    .strs = (const char *[]){NULL}}},
+        do_json_array_of_str, "[]");
+    check_json((array_of_str []){{.skip_null = TRUE,
+                    .n_strs = 2,
+                    .strs = (const char *[]){NULL, "abc"}}},
+        do_json_array_of_str, "[\"abc\"]");
+    check_json((array_of_str []){{.skip_null = FALSE,
+                    .n_strs = 1,
+                    .strs = (const char *[]){NULL}}},
+        do_json_array_of_str, "[null]");
+    check_json((array_of_str []){{.skip_null = FALSE,
+                    .n_strs = 2,
+                    .strs = (const char *[]){"abc", NULL}}},
+        do_json_array_of_str, "[\"abc\",null]");
+
+    TEST_STEP("Checking conversion of kvpairs");
+    check_json((key_value []){{.key = NULL}}, do_json_kvpair, "{}");
+    check_json((key_value []){{.key = "a", .value = "b"}, {.key = NULL}},
+               do_json_kvpair, "{\"a\":\"b\"}");
+    check_json((key_value []){{.key = "a", .value = "b"},
+                              {.key = "c", .value = "d"},
+                              {.key = NULL}},
+               do_json_kvpair, "{\"a\":\"b\",\"c\":\"d\"}");
+
+    TEST_SUCCESS;
+
+cleanup:
+
+    TEST_END;
+}

suites/selftest/ts/tools/meson.build
@@ -7,6 +7,7 @@ tests = [
     'extract_glob',
     'hexdump',
     'intset',
+    'json',
     'make_bufs',
     'readlink',
     'resolvepath',

suites/selftest/ts/tools/package.xml
@@ -100,6 +100,11 @@
         <run>
             <script name="intset"/>
         </run>
+
+        <run>
+            <script name="json"/>
+        </run>
+
         <run>
             <script name="scandir"/>
             <arg name="n_files">

commit 7f2cadff590821b6ceb6ec0bde726cbd843c7eaf

lib/tools: allow unconstrained precision for JSON floats
This was impossible due to unfortunate argument type choice.

Fixes: d0ca768b54df ("lib/tools: implement simple JSON serializing functions")
OL-Redmine-Id: 12514
Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>

lib/tools/te_json.h
@@ -183,10 +183,10 @@ te_json_add_integer(te_json_ctx_t *ctx, intmax_t val)
  *
  * @param ctx       JSON context
  * @param val       Value
- * @param precision Floating-point precision
+ * @param precision Floating-point precision (unlimited if negative)
  */
 static inline void
-te_json_add_float(te_json_ctx_t *ctx, double val, unsigned int precision)
+te_json_add_float(te_json_ctx_t *ctx, double val, int precision)
 {
     if (!isfinite(val))
         te_json_add_null(ctx);