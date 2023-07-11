oktetlabs/test-environment

commit 2bd667e9e6acd25b60d771a61a46f939f50b03fe

engine/tester: add opt for tester to fail on memory leaks
At the moment, it is possible to track memory leaks for a
specific test only by viewing the valgrind log files for
each test, which is quite time-consuming. The patch adds a
flag that causes the tests in which a memory leak occurs to
fail.

OL-Redmine-Id: 12963
Signed-off-by: Ivan Khodyrev <ivan.khodyrev@oktetlabs.ru>
Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>

engine/tester/tester.c
@@ -1,11 +1,9 @@
 /* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (C) 2004-2023 OKTET Labs Ltd. All rights reserved. */
 /** @file
  * @brief Tester Subsystem
  *
  * Application main file
- *
- *
- * Copyright (C) 2004-2022 OKTET Labs Ltd. All rights reserved.
  */
 
 #include "te_config.h"
@@ -198,6 +196,7 @@ process_cmd_line_opts(tester_global *global, int argc, char **argv)
 
         TESTER_OPT_VALGRIND,
         TESTER_OPT_GDB,
+        TESTER_OPT_FAIL_ON_LEAK,
 
         TESTER_OPT_MIX,
         TESTER_OPT_MIX_VALUES,
@@ -315,6 +314,9 @@ process_cmd_line_opts(tester_global *global, int argc, char **argv)
         { "gdb", '\0', POPT_ARG_STRING, NULL, TESTER_OPT_GDB,
           "Run test scripts under specified path using gdb.",
           "<testpath>" },
+        { "fail-on-leak", 'v', POPT_ARG_NONE, NULL, TESTER_OPT_FAIL_ON_LEAK,
+          "Test scripts fail if valgrind detects a memory leak (must be "
+          "specified together with tester-vg).", NULL },
 
         { "run-while", '\0', POPT_ARG_STRING, NULL, TESTER_OPT_RUN_WHILE,
           "Run tests while they produce a given result.",
@@ -507,6 +509,10 @@ process_cmd_line_opts(tester_global *global, int argc, char **argv)
 
             case TESTER_OPT_INTERACTIVE:
                 global->flags |= TESTER_INTERACTIVE;
+                break;
+
+            case TESTER_OPT_FAIL_ON_LEAK:
+                global->flags |= TESTER_FAIL_ON_LEAK;
                 break;
 
             case TESTER_OPT_SUITE_PATH:

dispatcher.sh
@@ -169,6 +169,9 @@ Generic options:
   --tester-gdb=<testpath>       Run test scripts under specified path using
                                 gdb.
 
+  --tester-fail-on-leak         Test scripts fail if valgrind detects a memory
+                                leak (must be specified together with tester-vg).
+
   --tester-random-seed=<number> Random seed to initialize pseudo-random number
                                 generator
   --tester-verbose              Increase verbosity of the Tester (the first

engine/tester/run.c
@@ -1,11 +1,9 @@
 /* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (C) 2004-2023 OKTET Labs Ltd. All rights reserved. */
 /** @file
  * @brief Tester Subsystem
  *
  * Code dealing with running of the requested configuration.
- *
- *
- * Copyright (C) 2004-2022 OKTET Labs Ltd. All rights reserved.
  */
 
 #define TE_LGR_USER "Run"
@@ -2158,15 +2156,24 @@ run_test_script(test_script *script, const char *run_name, test_id exec_id,
     }
     else if (flags & TESTER_VALGRIND)
     {
-        if (snprintf(shell, sizeof(shell),
-                     "valgrind --num-callers=16 --leak-check=yes "
-                     "--show-reachable=yes --tool=memcheck ") >=
-                (int)sizeof(shell))
+        te_string shell_str = TE_STRING_INIT;
+
+        te_string_append(&shell_str, "valgrind --num-callers=16 "
+                         "--leak-check=yes --show-reachable=yes "
+                         "--tool=memcheck ");
+        if (flags & TESTER_FAIL_ON_LEAK)
+            te_string_append(&shell_str, "--error-exitcode=%d ", EXIT_FAILURE);
+
+        if (shell_str.len >= sizeof(shell))
         {
             ERROR("Too short buffer is reserved for shell command prefix");
             te_string_free(&params_str);
+            te_string_free(&shell_str);
             return TE_RC(TE_TESTER, TE_ESMALLBUF);
         }
+        strcpy(shell, shell_str.ptr);
+        te_string_free(&shell_str);
+
         if (snprintf(vg_filename, sizeof(vg_filename),
                      TESTER_VG_FILENAME_FMT, exec_id) >=
                 (int)sizeof(vg_filename))
@@ -2499,6 +2506,9 @@ run_script(run_item *ri, test_script *script,
         ctx->current_result.status = TESTER_TEST_PASSED;
         return TESTER_CFG_WALK_CONT;
     }
+
+    if (ctx->flags & TESTER_FAIL_ON_LEAK)
+        def_flags |= TESTER_FAIL_ON_LEAK;
 
     assert(ri != NULL);
     assert(ri->n_args == ctx->n_args);

engine/tester/tester_flags.h
@@ -1,11 +1,9 @@
 /* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (C) 2004-2023 OKTET Labs Ltd. All rights reserved. */
 /** @file
  * @brief Tester Subsystem
  *
  * Tester flags definitions.
- *
- *
- * Copyright (C) 2004-2022 OKTET Labs Ltd. All rights reserved.
  */
 
 #ifndef __TE_TESTER_FLAGS_H__
@@ -120,6 +118,9 @@ typedef uint64_t tester_flags;
 
 /** Gather the execution plan */
 #define TESTER_ASSEMBLE_PLAN          (1LLU << 38)
+
+/** Fail test scripts if valgrind detects a memory leak. */
+#define TESTER_FAIL_ON_LEAK           (1LLU << 39)
 
 #ifdef __cplusplus
 } /* extern "C" */

commit a2c50198cc17132b6a380c1257a337080f05c25b

engine/tester: fix cmd line options order
Fixes: 2bd667e9e6ac ("engine/tester: add opt for tester to fail on memory leaks")
GitHub-TE-Id: 17
OL-Redmine-Id: 13065
Signed-off-by: Ivan Khodyrev <ivan.khodyrev@oktetlabs.ru>
Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>

engine/tester/tester.c
@@ -196,7 +196,6 @@ process_cmd_line_opts(tester_global *global, int argc, char **argv)
 
         TESTER_OPT_VALGRIND,
         TESTER_OPT_GDB,
-        TESTER_OPT_FAIL_ON_LEAK,
 
         TESTER_OPT_MIX,
         TESTER_OPT_MIX_VALUES,
@@ -211,6 +210,7 @@ process_cmd_line_opts(tester_global *global, int argc, char **argv)
         /*
          * End of list corresponding to test_path_type.
          */
+        TESTER_OPT_FAIL_ON_LEAK,
 
         TESTER_OPT_RUN_WHILE,
         TESTER_OPT_RUN_UNTIL_VERDICT,