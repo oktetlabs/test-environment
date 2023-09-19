oktetlabs/test-environment

commit bdef4010ee3a99dea5fd88b02b5288f80ad18ce3

lib/tools: abort on failed memory alloc
On modern POSIX systems malloc cannot normally
return an error, and in marginal cases where it
can happen, the only sensible way to deal with such
errors is to abort the program immediately.

OL-Redmine-Id: 12079
Breaks: allocating zero-size buffers now fails, but
        no sane program should do that anyway.
Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>

lib/tools/te_alloc.c
@@ -1,48 +1,42 @@
 /* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (C) 2004-2023 OKTET Labs Ltd. All rights reserved. */
 /** @file
  * @brief API to safely allocate memory
  *
- * Safe memory allocation
- *
- *
- * Copyright (C) 2004-2022 OKTET Labs Ltd. All rights reserved.
+ * Safe memory allocation.
  */
 
 #define TE_LGR_USER     "TE Alloc"
 
 #include "te_config.h"
 
-#ifndef WINDOWS
-#ifdef STDC_HEADERS
 #include <stdlib.h>
-#include <stdio.h>
-#include <errno.h>
-#include <string.h>
-#endif
-#endif
 
 #include "logger_api.h"
 #include "te_alloc.h"
-
-#if SIZEOF_SIZE_T > SIZEOF_LONG
-#define SIZE_T_FMT "%Lu"
-#define SIZE_T_CAST(x) ((unsigned long long)(x))
-#else
-#define SIZE_T_FMT "%lu"
-#define SIZE_T_CAST(x) ((unsigned long)(x))
-#endif
 
 /* See description in te_alloc.h */
 void *
 te_alloc_internal(size_t size, const char *filename, int line)
 {
-    void *result = calloc(1, size);
+    void *result;
 
+    /*
+     * Allocating zero bytes is formally permitted by ISO C but
+     * it may either return NULL or some magic non-null pointer
+     * that cannot be dereferenced.
+     */
+    if (size == 0)
+    {
+        TE_FATAL_ERROR("Attempted to allocate a zero buffer at %s:%d",
+                       filename, line);
+    }
+
+    result = calloc(1, size);
     if (result == NULL)
     {
-        ERROR("Cannot allocate memory of size " SIZE_T_FMT " at %s:%d",
-              SIZE_T_CAST(size),
-              filename, line);
+        TE_FATAL_ERROR("Cannot allocate memory of size %zu at %s:%d",
+                       size, filename, line);
     }
     return result;
 }

lib/tools/te_alloc.h
@@ -1,4 +1,5 @@
 /* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (C) 2004-2023 OKTET Labs Ltd. All rights reserved. */
 /** @file
  * @brief API to safely allocate memory
  *
@@ -6,10 +7,7 @@
  * @ingroup te_tools
  * @{
  *
- * Safe memory allocation
- *
- *
- * Copyright (C) 2004-2022 OKTET Labs Ltd. All rights reserved.
+ * Safe memory allocation.
  */
 
 #ifndef __TE_TOOLS_ALLOC_H__
@@ -17,13 +15,7 @@
 
 #include "te_config.h"
 
-#ifdef HAVE_STDDEF_H
 #include <stddef.h>
-#endif
-#ifdef HAVE_SYS_TYPES_H
-#include <sys/types.h>
-#endif
-
 
 #ifdef __cplusplus
 extern "C" {
@@ -31,19 +23,27 @@ extern "C" {
 
 /**
  * Allocate @p size bytes and fill allocated memory with zeroes.
- * Logs an error if the memory cannot be allocated.
- * This function should never be called directly,
- * use TE_ALLOC() macro instead
  *
- * @param size     Number of bytes to allocate
- * @param filename Caller's filename
- * @param line     Caller's line
+ * This function should never be called directly,
+ * use TE_ALLOC() macro instead.
+ *
+ * @param size     Number of bytes to allocate.
+ * @param filename Caller's filename.
+ * @param line     Caller's line.
+ *
+ * @return A pointer to a fresh memory block (never @c NULL).
+ *
+ * @exception TE_FATAL_ERROR in an unlikely case of a memory allocation
+ *            failure.
+ *
+ * @note Unlike system malloc(), this function does not permit allocating
+ *       zero-sized memory blocks.
  */
 extern void *te_alloc_internal(size_t size, const char *filename, int line);
 
 
 /**
- * It is a wrapper for te_alloc_internal()
+ * A wrapper for te_alloc_internal() tracking source location.
  *
  * @param _size     Number of bytes to allocate
  */

commit 70d9f5474e90500cd13838ab864f07db6bfe92b3

lib/tools: do not abort on zero byte allocations
It turns out there are existing cases of allocating
zero bytes that expect it not to fail and return
a non-null pointer. This patch ensures such behaviour,
but prints a warning as such cases are still a source
of potential bugs.

OL-Redmine-Id: 13202
Fixes: bdef4010ee3a ("lib/tools: abort on failed memory alloc")
Reported-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

lib/tools/te_alloc.c
@@ -24,12 +24,13 @@ te_alloc_internal(size_t size, const char *filename, int line)
     /*
      * Allocating zero bytes is formally permitted by ISO C but
      * it may either return NULL or some magic non-null pointer
-     * that cannot be dereferenced.
+     * that cannot be dereferenced. We force the latter variant
+     * by actually allocating a single byte.
      */
     if (size == 0)
     {
-        TE_FATAL_ERROR("Attempted to allocate a zero buffer at %s:%d",
-                       filename, line);
+        size = 1;
+        WARN("Attempted to allocate a zero buffer at %s:%d", filename, line);
     }
 
     result = calloc(1, size);