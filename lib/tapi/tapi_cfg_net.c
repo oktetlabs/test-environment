oktetlabs/test-environment

commit 63d5b9fc326a1c2a21f0de660184e294dd216684

lib/tapi: support multiple ports for a single PCI function
In practice, there are NICs whose ports sit on the same PCI
function: prior to this patch, TE had the ability to work
with only one port on one PCI function.

OL-Redmine-Id: 13145
Signed-off-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>
Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

lib/tapi/tapi_cfg_net.c
@@ -74,6 +74,11 @@ tapi_cfg_net_get_node_rsrc_type(cfg_net_node_t *node)
     {
         node->rsrc_type = NET_NODE_RSRC_TYPE_PCI_FN;
     }
+    else if (strcmp(obj_oid,
+                    "/agent/hardware/pci/vendor/device/instance/netdev") == 0)
+    {
+        node->rsrc_type = NET_NODE_RSRC_TYPE_PCI_FN_NETDEV;
+    }
     else if (strcmp(obj_oid, "/local/dpdk/vdev") == 0)
     {
         node->rsrc_type = NET_NODE_RSRC_TYPE_RTE_VDEV;
@@ -791,6 +796,7 @@ tapi_cfg_net_mk_node_rsrc_desc_iface(const cfg_oid         *oid,
 
 static te_errno
 tapi_cfg_net_mk_node_rsrc_desc_pci_fn(const cfg_oid         *oid,
+                                      enum net_node_rsrc_type rsrc_type,
                                       net_node_rsrc_desc_t **rsrc_descp)
 {
     net_node_rsrc_desc_t *d = tapi_cfg_net_node_rsrc_desc_alloc(1);
@@ -805,7 +811,22 @@ tapi_cfg_net_mk_node_rsrc_desc_pci_fn(const cfg_oid         *oid,
         return TE_ENOMEM;
     }
 
-    d->rsrc_names[0] = tapi_cfg_pci_rsrc_name(oid);
+    switch (rsrc_type)
+    {
+        case NET_NODE_RSRC_TYPE_PCI_FN:
+            d->rsrc_names[0] = tapi_cfg_pci_rsrc_name(oid);
+            break;
+
+        case NET_NODE_RSRC_TYPE_PCI_FN_NETDEV:
+            d->rsrc_names[0] = tapi_cfg_pci_fn_netdev_rsrc_name(oid);
+            break;
+
+        default:
+            ERROR("%s() unexpected rsrc_type value: %d", __func__, rsrc_type);
+            tapi_cfg_net_node_rsrc_desc_free(d);
+            return TE_EINVAL;
+    }
+
     if (d->rsrc_names[0] == NULL)
     {
         tapi_cfg_net_node_rsrc_desc_free(d);
@@ -910,7 +931,8 @@ tapi_cfg_net_mk_node_rsrc_names_vals(enum net_node_rsrc_type    rsrc_type,
             break;
 
         case NET_NODE_RSRC_TYPE_PCI_FN:
-            rc = tapi_cfg_net_mk_node_rsrc_desc_pci_fn(oid, rsrc_descp);
+        case NET_NODE_RSRC_TYPE_PCI_FN_NETDEV:
+            rc = tapi_cfg_net_mk_node_rsrc_desc_pci_fn(oid, rsrc_type, rsrc_descp);
             break;
 
         case NET_NODE_RSRC_TYPE_RTE_VDEV:
@@ -1229,6 +1251,23 @@ tapi_cfg_net_bind_driver_by_node(enum net_node_type node_type,
     return tapi_cfg_net_foreach_node(tapi_cfg_net_node_bind_driver, &cfg);
 }
 
+static char *
+make_pci_fn_oid_str_by_pci_fn_netdev_oid(cfg_oid *oid)
+{
+    unsigned int i;
+    te_string str_inst_oid = TE_STRING_INIT;
+
+    /*
+     * Build an inst_oid for pci instance without the last ('netdev') item.
+     */
+    for (i = 1; i < oid->len - 1; i++)
+    {
+        te_string_append(&str_inst_oid, "/%s:%s",
+                         cfg_oid_inst_subid(oid, i),
+                         CFG_OID_GET_INST_NAME(oid, i));
+    }
+    return str_inst_oid.ptr;
+}
 
 /**
  * Callback to switch network node specified using PCI function to
@@ -1246,6 +1285,8 @@ switch_agent_pci_fn_to_interface(cfg_net_t *net, cfg_net_node_t *node,
     const char *agent;
     char *pci_path = NULL;
     te_errno rc = 0;
+    char obj_oid[CFG_OID_MAX];
+    te_bool pci_with_netdev = FALSE;
 
     UNUSED(net);
 
@@ -1259,14 +1300,38 @@ switch_agent_pci_fn_to_interface(cfg_net_t *net, cfg_net_node_t *node,
         goto out;
     }
 
-    rc = cfg_get_instance_str(NULL, &pci_path, oid_str);
+    cfg_oid_inst2obj(oid_str, obj_oid);
+    if (strcmp(obj_oid,
+               "/agent/hardware/pci/vendor/device/instance/netdev") == 0)
+    {
+        char *pci_fn_oid_str;
+
+        pci_with_netdev = TRUE;
+        pci_fn_oid_str = make_pci_fn_oid_str_by_pci_fn_netdev_oid(oid);
+        rc = cfg_get_instance_str(NULL, &pci_path, pci_fn_oid_str);
+        free(pci_fn_oid_str);
+    }
+    else
+    {
+        rc = cfg_get_instance_str(NULL, &pci_path, oid_str);
+    }
+
     if (rc != 0)
     {
         ERROR("Failed to get PCI device path: %r", rc);
         goto out;
     }
 
-    rc = tapi_cfg_pci_get_net_if(pci_path, &interface);
+    if (pci_with_netdev)
+    {
+        rc = tapi_cfg_pci_fn_netdev_get_net_if(pci_path,
+                    CFG_OID_GET_INST_NAME(oid, oid->len - 1), &interface);
+    }
+    else
+    {
+        rc = tapi_cfg_pci_get_net_if(pci_path, &interface);
+    }
+
     if (rc != 0)
     {
         if (TE_RC_GET_ERROR(rc) == TE_ENOENT)

agents/unix/conf/base/conf_pci.c
@@ -3432,6 +3432,10 @@ ta_unix_conf_pci_init()
                       pci_device_grab,
                       pci_device_release);
 
+    rcf_pch_rsrc_info("/agent/hardware/pci/vendor/device/instance/netdev",
+                      pci_device_grab,
+                      pci_device_release);
+
     return rcf_pch_add_node("/agent/hardware", &node_pci);
 }
 

lib/tapi/tapi_cfg_net.h
@@ -41,6 +41,7 @@ enum net_node_rsrc_type {
     NET_NODE_RSRC_TYPE_UNKNOWN = 0, /**< Unknown type of the resource */
     NET_NODE_RSRC_TYPE_INTERFACE,   /**< Network interface */
     NET_NODE_RSRC_TYPE_PCI_FN,      /**< PCI function */
+    NET_NODE_RSRC_TYPE_PCI_FN_NETDEV,   /**< PCI function with port (net) */
     NET_NODE_RSRC_TYPE_RTE_VDEV,    /**< RTE vdev */
 };
 

lib/tapi/tapi_cfg_pci.c
@@ -277,23 +277,35 @@ out:
     return rc;
 }
 
-char *
-tapi_cfg_pci_rsrc_name(const cfg_oid *pci_instance)
+static char *
+tapi_cfg_pci_rsrc_name_gen(const cfg_oid *oid, const char *rsrc_pfx)
 {
     te_string rsrc_name = TE_STRING_INIT;
     unsigned int i;
     char *inst_name;
 
-    te_string_append(&rsrc_name, "%s", "pci_fn");
+    te_string_append(&rsrc_name, "%s", rsrc_pfx);
     /* The agent's name (1) is not interesting on the agent itself */
-    for (i = 2; i < pci_instance->len; i++)
+    for (i = 2; i < oid->len; i++)
     {
-        inst_name = CFG_OID_GET_INST_NAME(pci_instance, i);
+        inst_name = CFG_OID_GET_INST_NAME(oid, i);
         if (*inst_name != '\0')
             te_string_append(&rsrc_name, ":%s", inst_name);
     }
 
     return rsrc_name.ptr;
+}
+
+char *
+tapi_cfg_pci_rsrc_name(const cfg_oid *pci_instance)
+{
+    return tapi_cfg_pci_rsrc_name_gen(pci_instance, "pci_fn");
+}
+
+char *
+tapi_cfg_pci_fn_netdev_rsrc_name(const cfg_oid *oid)
+{
+    return tapi_cfg_pci_rsrc_name_gen(oid, "pci_fn_netdev");
 }
 
 te_errno
@@ -431,12 +443,15 @@ tapi_cfg_pci_get_ta_driver(const char *ta,
     return 0;
 }
 
-te_errno
-tapi_cfg_pci_get_net_if(const char *pci_oid, char **interface)
+static te_errno
+tapi_cfg_pci_get_net_if_gen(const char *pci_oid, const char *netdev,
+                            char **interface)
 {
     te_errno rc;
 
-    rc = cfg_get_instance_string_fmt(interface, "%s/net:", pci_oid);
+    rc = cfg_get_instance_string_fmt(interface, "%s/net:%s", pci_oid,
+                             te_str_is_null_or_empty(netdev) ? "" : netdev);
+
     if (rc != 0 && rc != TE_RC(TE_CS, TE_ENOENT))
     {
         ERROR("Failed to get the only interface of a PCI device %s: %r",
@@ -444,6 +459,19 @@ tapi_cfg_pci_get_net_if(const char *pci_oid, char **interface)
     }
 
     return rc;
+}
+
+te_errno
+tapi_cfg_pci_fn_netdev_get_net_if(const char *pci_fn_oid, const char *netdev,
+                                  char **interface)
+{
+    return tapi_cfg_pci_get_net_if_gen(pci_fn_oid, netdev, interface);
+}
+
+te_errno
+tapi_cfg_pci_get_net_if(const char *pci_oid, char **interface)
+{
+    return tapi_cfg_pci_get_net_if_gen(pci_oid, NULL, interface);
 }
 
 /* See description in tapi_cfg_pci.h */

lib/tapi/tapi_cfg_pci.h
@@ -159,6 +159,16 @@ extern te_errno tapi_cfg_pci_addr_by_oid_array(unsigned int n_devices,
 extern char * tapi_cfg_pci_rsrc_name(const cfg_oid *pci_instance);
 
 /**
+ * Allocate a string with resource name for grabbing a PCI instance with netdev.
+ *
+ * @param[in]  pci_instance PCI instance OID
+ *                          (/agent/hardware/pci/vendor/device/instance/netdev)
+ *
+ * @return Allocated resource name string
+ */
+extern char * tapi_cfg_pci_fn_netdev_rsrc_name(const cfg_oid *oid);
+
+/**
  * Grab a PCI device as a resource.
  *
  * @param[in]  pci_instance     PCI instance OID
@@ -242,6 +252,19 @@ extern te_errno tapi_cfg_pci_bind_driver(const char *pci_oid,
 extern te_errno tapi_cfg_pci_bind_ta_driver_on_device(const char *ta,
                                               enum tapi_cfg_driver_type type,
                                               const char *pci_addr);
+
+/**
+ * Get the network interface associated with a PCI device with port.
+ *
+ * @param[in]  pci_fn_oid   PCI device OID (/agent/hardware/pci/device/net)
+ * @param[in]  netdev       Port (net) instance name
+ * @param[out] interface    Network interface name (must not be @c NULL)
+ *
+ * @return Status code
+ */
+extern te_errno tapi_cfg_pci_fn_netdev_get_net_if(const char *pci_fn_oid,
+                                                  const char *netdev,
+                                                  char **interface);
 
 /**
  * Get the first network interface associated with a PCI device.

commit 85689c82d0d05891886c860f4a70e1e148c05445

lib/tapi: improve port support on a single PCI function
When binding the driver, it must be taken into account
that there may be several ports on one PCI function

OL-Redmine-Id: 13145
Fixes: 63d5b9fc326a ("lib/tapi: support multiple ports for a single PCI function")
Signed-off-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>
Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

lib/tapi/tapi_cfg_net.c
@@ -1125,6 +1125,24 @@ struct bind_cfg_t {
     enum tapi_cfg_driver_type driver;
 };
 
+static char *
+make_pci_fn_oid_str_by_pci_fn_netdev_oid(cfg_oid *oid)
+{
+    unsigned int i;
+    te_string str_inst_oid = TE_STRING_INIT;
+
+    /*
+     * Build an inst_oid for pci instance without the last ('netdev') item.
+     */
+    for (i = 1; i < oid->len - 1; i++)
+    {
+        te_string_append(&str_inst_oid, "/%s:%s",
+                         cfg_oid_inst_subid(oid, i),
+                         CFG_OID_GET_INST_NAME(oid, i));
+    }
+    return str_inst_oid.ptr;
+}
+
 static tapi_cfg_net_node_cb tapi_cfg_net_node_bind_driver;
 static te_errno
 tapi_cfg_net_node_bind_driver(cfg_net_t *net, cfg_net_node_t *node,
@@ -1169,10 +1187,24 @@ tapi_cfg_net_node_bind_driver(cfg_net_t *net, cfg_net_node_t *node,
         }
         else if (strcmp(cfg_oid_inst_subid(oid, 2), "hardware") == 0)
         {
+            char obj_oid[CFG_OID_MAX];
+
             n_pci_fns = 1;
             pci_fns = TE_ALLOC(sizeof(*pci_fns));
 
-            rc = cfg_get_instance_str(NULL, &pci_fns[0], oid_str);
+            cfg_oid_inst2obj(oid_str, obj_oid);
+            if (strcmp(obj_oid, TAPI_CFG_NET_OID_NETDEV) == 0)
+            {
+                char *pci_fn_oid_str;
+
+                pci_fn_oid_str = make_pci_fn_oid_str_by_pci_fn_netdev_oid(oid);
+                rc = cfg_get_instance_str(NULL, &pci_fns[0], pci_fn_oid_str);
+                free(pci_fn_oid_str);
+            }
+            else
+            {
+                rc = cfg_get_instance_str(NULL, &pci_fns[0], oid_str);
+            }
             if (rc != 0)
             {
                 ERROR("Failed to get PCI device path of an agent");
@@ -1228,24 +1260,6 @@ tapi_cfg_net_bind_driver_by_node(enum net_node_type node_type,
     };
 
     return tapi_cfg_net_foreach_node(tapi_cfg_net_node_bind_driver, &cfg);
-}
-
-static char *
-make_pci_fn_oid_str_by_pci_fn_netdev_oid(cfg_oid *oid)
-{
-    unsigned int i;
-    te_string str_inst_oid = TE_STRING_INIT;
-
-    /*
-     * Build an inst_oid for pci instance without the last ('netdev') item.
-     */
-    for (i = 1; i < oid->len - 1; i++)
-    {
-        te_string_append(&str_inst_oid, "/%s:%s",
-                         cfg_oid_inst_subid(oid, i),
-                         CFG_OID_GET_INST_NAME(oid, i));
-    }
-    return str_inst_oid.ptr;
 }
 
 /**