oktetlabs/test-environment

commit b354354f15bbcdb2a1a499e86377a5b22cc7df91

lib/tools: implement abort on failure strdup replacement
These functions shall eventually replace all uses of
strdup() and strndup(), getting rid of extra ENOMEM check.
Also a generic memory block duplication function is
implemented similiar to memdup() found in some other OSes.

OL-Redmine-Id: 12711
Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Anatoly Mirin <anatoly.mirin@oktetlabs.ru>

suites/selftest/ts/tools/alloc.c
@@ -1,0 +1,116 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (C) 2023 OKTET Labs. All rights reserved. */
+/** @file
+ * @brief Test for allocator functions.
+ *
+ * Testing allocator functions.
+ */
+
+/** @page tools_alloc te_alloc.h test
+ *
+ * @objective Testing allocator functions.
+ *
+ * Test the correctness of allocator functions.
+ *
+ * The test is more useful when run under valgrind.
+ *
+ * @par Test sequence:
+ */
+
+/** Logging subsystem entity name */
+#define TE_TEST_NAME    "tools/alloc"
+
+#include "te_config.h"
+
+#include <string.h>
+
+#include "tapi_test.h"
+#include "te_bufs.h"
+#include "te_alloc.h"
+
+int
+main(int argc, char **argv)
+{
+    unsigned int i;
+    unsigned int min_len;
+    unsigned int max_len;
+    unsigned int n_iterations;
+
+    TEST_START;
+
+    TEST_GET_UINT_PARAM(min_len);
+    TEST_GET_UINT_PARAM(max_len);
+    TEST_GET_UINT_PARAM(n_iterations);
+
+    sleep(10);
+    TEST_STEP("Testing memdup");
+    for (i = 0; i < n_iterations; i++)
+    {
+        size_t len;
+        void *buf = te_make_buf(min_len, max_len, &len);
+        void *copy;
+
+        copy = TE_MEMDUP(buf, len);
+        if (!te_compare_bufs(buf, len, 1, copy, len, TE_LL_ERROR))
+            TEST_VERDICT("The copy differs from the original");
+        free(copy);
+        free(buf);
+    }
+
+    TEST_STEP("Testing strdup");
+    for (i = 0; i < n_iterations; i++)
+    {
+        size_t len;
+        char *buf = te_make_printable_buf(min_len, max_len, &len);
+        char *copy;
+        size_t copy_len;
+
+        copy = TE_STRDUP(buf);
+        copy_len = strlen(copy) + 1;
+        if (!te_compare_bufs(buf, len, 1, copy, copy_len, TE_LL_ERROR))
+            TEST_VERDICT("The copy differs from the original");
+        free(copy);
+        free(buf);
+    }
+
+    TEST_STEP("Testing strndup");
+    for (i = 0; i < n_iterations; i++)
+    {
+        size_t len;
+        char *buf = te_make_printable_buf(min_len, max_len, &len);
+        char *copy;
+        size_t copy_len;
+        size_t trunc_at = rand_range(0, 10);
+
+        copy = TE_STRNDUP(buf, len + trunc_at);
+        copy_len = strlen(copy) + 1;
+        if (!te_compare_bufs(buf, len, 1, copy, copy_len, TE_LL_ERROR))
+            TEST_VERDICT("The copy differs from the original");
+        free(copy);
+        free(buf);
+    }
+
+    TEST_STEP("Testing strndup with limit");
+    for (i = 0; i < n_iterations; i++)
+    {
+        size_t len;
+        char *buf = te_make_printable_buf(min_len, max_len, &len);
+        char *copy;
+        size_t trunc_at;
+
+        trunc_at = rand_range(0, len - 1);
+        copy = TE_STRNDUP(buf, trunc_at);
+        if (copy[trunc_at] != '\0')
+            TEST_VERDICT("The copy is not null terminated");
+        if (!te_compare_bufs(buf, trunc_at, 1, copy, trunc_at, TE_LL_ERROR))
+            TEST_VERDICT("The copy differs from the original");
+        free(copy);
+        free(buf);
+    }
+
+    TEST_SUCCESS;
+
+cleanup:
+
+    TEST_END;
+}

lib/tools/meson.build
@@ -125,6 +125,10 @@ if cc.has_function('getaddrinfo')
     c_args += [ '-DHAVE_GETADDRINFO' ]
 endif
 
+if cc.has_function('strnlen')
+    c_args += [ '-DHAVE_STRNLEN' ]
+endif
+
 deps += [
     cc.find_library('m'),
     cc.find_library('rt'),

lib/tools/te_alloc.c
@@ -11,6 +11,7 @@
 #include "te_config.h"
 
 #include <stdlib.h>
+#include <string.h>
 
 #include "logger_api.h"
 #include "te_alloc.h"
@@ -44,4 +45,36 @@ te_alloc_internal(size_t size, te_bool initialize,
                        size, filename, line);
     }
     return result;
+}
+
+/* See description in te_alloc.h */
+void *
+te_memdup_internal(const void *src, te_bool zero_terminated,
+                   size_t maxsize, const char *filename, int line)
+{
+    void *copy;
+    size_t copy_size = maxsize;
+
+    if (src == NULL)
+        return NULL;
+
+    if (zero_terminated)
+    {
+#if defined(HAVE_STRNLEN)
+        copy_size = strnlen(src, maxsize);
+#else
+        for (copy_size = 0;
+             copy_size < maxsize && ((const char *)src)[copy_size] != '\0';
+             copy_size++)
+            ;
+#endif
+        maxsize = copy_size + 1;
+    }
+
+    copy = te_alloc_internal(maxsize, FALSE, filename, line);
+    memcpy(copy, src, copy_size);
+    if (zero_terminated)
+        ((char *)copy)[copy_size] = '\0';
+
+    return copy;
 }

lib/tools/te_alloc.h
@@ -80,6 +80,83 @@ extern void *te_alloc_internal(size_t size, te_bool initialize,
 #define TE_ALLOC_UNINITIALIZED(size_) \
     (te_alloc_internal((size_), FALSE, __FILE__, __LINE__))
 
+/**
+ * Copy a block of memory @p src.
+ *
+ * This function should never be called directly,
+ *
+ * @param src                Source buffer (may be @c NULL).
+ * @param zero_terminated    If @c TRUE, only copy bytes up to the first
+ *                           binary zero.
+ * @param maxsize            Maximum number of bytes to copy
+ *                           (may be less if @p zero_terminated is @c TRUE).
+ * @param filename           Source filename.
+ * @param line               Source line.
+ *
+ * @return A pointer to a fresh copy of @p src.
+ * @retval @c NULL  if and only if @p src is @c NULL.
+ *
+ * @exception TE_FATAL_ERROR in an unlikely case of a memory allocation
+ *            failure.
+ */
+extern void *te_memdup_internal(const void *src, te_bool zero_terminated,
+                                size_t maxsize, const char *filename, int line);
+
+/**
+ * Make a copy of @p size_ bytes of memory starting from @p src_.
+ *
+ * This macro is similiar to @c memdup function found in OpenBSD
+ * and some other OSes.
+ *
+ * @param src_  Source buffer (may be @c NULL).
+ * @param size_ Size of the source buffer.
+ *
+ * @return A pointer to a fresh copy of @p src_.
+ * @retval @c NULL  if and only if @p src_ is @c NULL.
+ *
+ * @exception TE_FATAL_ERROR in an unlikely case of a memory allocation
+ *            failure.
+ *
+ * @note Refer to TE_ALLOC() on behaviour when @p size_ is zero.
+ */
+#define TE_MEMDUP(src_, size_) \
+    (te_memdup_internal((src_), FALSE, (size_), __FILE__, __LINE__))
+
+/**
+ * Make a copy of zero-terminated @p src_ like system @c strdup.
+ *
+ * @param src_  Zero-terminated string (may be @c NULL).
+ *
+ * @return A pointer to a fresh copy of @p src_.
+ * @retval @c NULL  if and only if @p src_ is @c NULL.
+ *
+ * @exception TE_FATAL_ERROR in an unlikely case of a memory allocation
+ *            failure.
+ */
+#define TE_STRDUP(src_) \
+    ((char *)te_memdup_internal((src_), TRUE, SIZE_MAX, __FILE__, __LINE__))
+
+/**
+ * Make a copy of possibly zero-terminated @p src_ like system @c strndup.
+ *
+ * The macro copies the content of @p src_ up to and including the first
+ * binary zero or the first @p maxsize_ bytes, whichever comes first.
+ *
+ * The result is always zero-terminated.
+ *
+ * @param src_      A possibly zero-terminated string (may be @c NULL).
+ * @param maxsize_  Max number of bytes to search for a trailing zero.
+ *
+ * @return A pointer to a fresh copy of @p src_.
+ * @retval @c NULL  if and only if @p src_ is @c NULL.
+ *
+ * @exception TE_FATAL_ERROR in an unlikely case of a memory allocation
+ *            failure.
+ */
+#define TE_STRNDUP(src_, maxsize_) \
+    ((char *)te_memdup_internal((src_), TRUE, (maxsize_), __FILE__, __LINE__))
+
+
 #ifdef __cplusplus
 } /* extern "C" */
 #endif

suites/selftest/conf/trc/tools.trc.xml
@@ -5,6 +5,8 @@
     <objective>Self-tests for lib/tools functions</objective>
     <iter result="PASSED">
         <xi:include xmlns:xi="http://www.w3.org/2003/XInclude"
+                    href="tools/alloc.trc.xml" parse="xml"/>
+        <xi:include xmlns:xi="http://www.w3.org/2003/XInclude"
                     href="tools/base64.trc.xml" parse="xml"/>
         <xi:include xmlns:xi="http://www.w3.org/2003/XInclude"
                     href="tools/readlink.trc.xml" parse="xml"/>

suites/selftest/conf/trc/tools/alloc.trc.xml
@@ -1,0 +1,12 @@
+<?xml version="1.0"?>
+<!-- SPDX-License-Identifier: Apache-2.0 -->
+<!-- Copyright (C) 2023 OKTET Labs Ltd. All rights reserved. -->
+<test name="alloc" type="script">
+    <objective>Testing allocator functions.</objective>
+    <notes/>
+    <iter result="PASSED">
+        <arg name="n_iterations" />
+        <arg name="min_len" />
+        <arg name="max_len" />
+    </iter>
+</test>

suites/selftest/ts/minimal/package.xml.orig
@@ -1,0 +1,28 @@
+<?xml version="1.0"?>
+<!-- SPDX-License-Identifier: Apache-2.0 -->
+<!-- Copyright (C) 2019-2022 OKTET Labs Ltd. All rights reserved. -->
+<package version="1.0">
+    <description>Package for demonstrating minimal tests</description>
+    <author mailto="te-maint@oktetlabs.ru"/>
+
+    <session>
+        <run>
+            <script name="helloworld"/>
+        </run>
+        <run>
+            <script name="artifact"/>
+        </run>
+        <run>
+            <script name="verdict"/>
+        </run>
+        <run>
+            <script name="mi_meas_line_graph"/>
+        </run>
+        <run>
+            <script name="env"/>
+            <arg name="env">
+                <value>{{{'pco_iut':IUT},if:'iut_if'}}</value>
+            </arg>
+        </run>
+    </session>
+</package>

suites/selftest/ts/minimal/package.xml.rej
@@ -1,0 +1,15 @@
+--- suites/selftest/ts/minimal/package.xml
++++ suites/selftest/ts/minimal/package.xml
+@@ -74,6 +74,12 @@
+             <arg name="opt_uint_val_param">
+                 <value>42</value>
+             </arg>
++            <arg name="opt_uint64_none_param">
++                <value>-</value>
++            </arg>
++            <arg name="opt_uint64_val_param">
++                <value>42</value>
++            </arg>
+             <arg name="opt_dbl_none_param">
+                 <value>-</value>
+             </arg>

commit ef334b14bd80184e0937500c3848a23e3c0c2b6b

suites/selftest: remove debug remnant
A sleep was added solely for the debugging
purpose, it is meaningless otherwise.

OL-Redmine-Id: 12711
Fixes: b354354f15bb ("lib/tools: implement abort on failure strdup replacement")
Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>

suites/selftest/ts/tools/alloc.c
@@ -42,7 +42,6 @@ main(int argc, char **argv)
     TEST_GET_UINT_PARAM(max_len);
     TEST_GET_UINT_PARAM(n_iterations);
 
-    sleep(10);
     TEST_STEP("Testing memdup");
     for (i = 0; i < n_iterations; i++)
     {