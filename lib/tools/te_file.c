oktetlabs/test-environment

commit 19eab56b47a252e1cdecd037b7a9dabdb5a42f49

lib/tools: add API to read/write from/into dynamic strings
OL-Redmine-Id: 12863
Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>
Reviewed-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>

lib/tools/te_file.c
@@ -4,7 +4,7 @@
  *
  * Functions to operate the files.
  *
- * Copyright (C) 2018-2022 OKTET Labs Ltd. All rights reserved.
+ * Copyright (C) 2018-2023 OKTET Labs Ltd. All rights reserved.
  */
 
 #include "te_config.h"
@@ -12,6 +12,7 @@
 #include "te_defs.h"
 #include "te_errno.h"
 #include "te_file.h"
+#include "te_str.h"
 #include "te_string.h"
 #include "te_dbuf.h"
 #include "te_alloc.h"
@@ -400,71 +401,165 @@ te_unlink_fmt(const char *fmt, ...)
 }
 
 te_errno
-te_file_read_text(const char *path, char *buffer, size_t bufsize)
+te_file_read_string(te_string *dest, te_bool binary,
+                    size_t maxsize, const char *path_fmt, ...)
 {
-    int fd = open(path, O_RDONLY);
-    off_t size;
+    te_string pathname = TE_STRING_INIT;
+    va_list args;
+    struct stat st;
+    te_errno rc = 0;
+    int fd = -1;
     ssize_t actual;
-    ssize_t i;
-    int saved_errno;
 
+    va_start(args, path_fmt);
+    te_string_append_va(&pathname, path_fmt, args);
+    va_end(args);
+
+    if (stat(pathname.ptr, &st))
+    {
+        rc = te_rc_os2te(errno);
+        ERROR("Cannot stat '%s': %r", pathname.ptr, rc);
+        goto out;
+    }
+
+    if (!S_ISREG(st.st_mode))
+    {
+        WARN("'%s' is not a regular file or symlink, "
+             "%s() may not return the expected data",
+             pathname.ptr, __func__);
+    }
+
+    if (maxsize != 0 && st.st_size > maxsize)
+    {
+        ERROR("File %s's size (%zu) is larger than %zu",
+              pathname.ptr, (size_t)st.st_size, maxsize);
+        rc = TE_EFBIG;
+        goto out;
+    }
+    te_string_reserve(dest, dest->len + st.st_size + 1);
+
+    fd  = open(pathname.ptr, O_RDONLY);
     if (fd < 0)
-        return TE_OS_RC(TE_MODULE_NONE, errno);
-    size = lseek(fd, 0, SEEK_END);
-    if (size == (off_t)-1)
     {
-        saved_errno = errno;
-        close(fd);
-        return TE_OS_RC(TE_MODULE_NONE, saved_errno);
+        rc = te_rc_os2te(errno);
+        ERROR("Cannot open '%s' for reading: %r",
+              pathname.ptr, rc);
+        goto out;
     }
 
-    if (size >= bufsize)
-    {
-        ERROR("File %s's size (%zu) is larger than %zu", path,
-              (size_t)size, bufsize);
-        close(fd);
-        return TE_RC(TE_MODULE_NONE, TE_EFBIG);
-    }
-
-    if (lseek(fd, 0, SEEK_SET) == (off_t)-1)
-    {
-        saved_errno = errno;
-        close(fd);
-        return TE_OS_RC(TE_MODULE_NONE, saved_errno);
-    }
-
-    actual = read(fd, buffer, size);
+    actual = read(fd, dest->ptr + dest->len, st.st_size);
     if (actual < 0)
     {
-        saved_errno = errno;
-        close(fd);
-        return TE_OS_RC(TE_MODULE_NONE, saved_errno);
-    }
-    close(fd);
-
-    if (actual != size)
-    {
-        ERROR("Could not read %zu bytes from %s, only %zu were read", size,
-              path, (size_t)actual);
-        return TE_RC(TE_MODULE_NONE, TE_EIO);
+        rc = te_rc_os2te(errno);
+        ERROR("Cannot read from '%s': %r", pathname.ptr, rc);
+        goto out;
     }
 
-    for (i = 0; i < actual; i++)
+    if ((size_t)actual != (size_t)st.st_size)
     {
-        if (buffer[i] == '\0')
+        ERROR("Could not read %zu bytes from %s, only %zu were read",
+              (size_t)st.st_size, pathname.ptr, (size_t)actual);
+        rc = TE_EIO;
+        goto out;
+    }
+
+    dest->len += actual;
+    dest->ptr[dest->len] = '\0';
+
+    if (!binary)
+    {
+        ssize_t i;
+
+        for (i = 0; i < actual; i++)
         {
-            ERROR("File '%s' contains an embedded zero at %zu", path, actual);
-            return TE_RC(TE_MODULE_NONE, TE_EILSEQ);
+            if (dest->ptr[dest->len - actual + i] == '\0')
+            {
+                ERROR("File '%s' contains an embedded zero at %zu",
+                      pathname.ptr, (size_t)i);
+                te_string_cut(dest, actual);
+                rc = TE_EILSEQ;
+                goto out;
+            }
+        }
+
+        te_string_chop(dest, "\n");
+    }
+
+out:
+    if (fd >= 0)
+        close(fd);
+    te_string_free(&pathname);
+
+    return rc;
+}
+
+te_errno
+te_file_write_string(const te_string *src, size_t fitlen,
+                     int flags, mode_t mode,
+                     const char *path_fmt, ...)
+{
+    te_string pathname = TE_STRING_INIT;
+    va_list args;
+    te_errno rc = 0;
+    int fd = -1;
+    ssize_t actual;
+    size_t remaining = fitlen == 0 ? src->len : fitlen;
+
+    va_start(args, path_fmt);
+    te_string_append_va(&pathname, path_fmt, args);
+    va_end(args);
+
+    fd  = open(pathname.ptr, O_WRONLY | flags, mode);
+    if (fd < 0)
+    {
+        rc = te_rc_os2te(errno);
+        ERROR("Cannot open '%s' for writing: %r",
+              pathname.ptr, rc);
+        goto out;
+    }
+
+    while (remaining > 0)
+    {
+        size_t chunk = src->len > remaining ? remaining : src->len;
+
+        actual = write(fd, src->ptr, chunk);
+        if (actual < 0)
+        {
+            rc = te_rc_os2te(errno);
+            ERROR("Cannot write to '%s': %r", pathname.ptr, rc);
+            goto out;
+        }
+
+        if ((size_t)actual != chunk)
+        {
+            ERROR("Could not write %zu bytes to %s, only %zu were written",
+                  chunk, pathname.ptr, (size_t)actual);
+            rc = TE_EIO;
+            goto out;
+        }
+
+        remaining -= chunk;
+    }
+out:
+    if (fd >= 0)
+    {
+        if (close(fd) != 0)
+        {
+            rc = te_rc_os2te(errno);
+            ERROR("Error closing '%s': %r", pathname.ptr, rc);
         }
     }
-    buffer[actual] = '\0';
-    while (actual > 0 && buffer[actual - 1] == '\n')
-    {
-        actual--;
-        buffer[actual] = '\0';
-    }
+    te_string_free(&pathname);
 
-    return 0;
+    return rc;
+}
+
+te_errno
+te_file_read_text(const char *path, char *buffer, size_t bufsize)
+{
+    te_string bufstr = TE_STRING_EXT_BUF_INIT(buffer, bufsize);
+
+    return te_file_read_string(&bufstr, FALSE, bufsize - 1, "%s", path);
 }
 
 static te_errno

lib/tools/te_file.h
@@ -8,7 +8,7 @@
  *
  * Functions to operate the files.
  *
- * Copyright (C) 2018-2022 OKTET Labs Ltd. All rights reserved.
+ * Copyright (C) 2018-2023 OKTET Labs Ltd. All rights reserved.
  */
 
 #ifndef __TE_FILE_H__
@@ -22,6 +22,7 @@
 #endif
 
 #include "te_vector.h"
+#include "te_string.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -227,6 +228,59 @@ extern te_errno te_access_fmt(int mode, const char *fmt, ...)
 extern te_errno te_unlink_fmt(const char *fmt, ...)
     __attribute__((format(printf, 1, 2)));
 
+/**
+ * Read the contents of the file into a dynamic string @p dest.
+ *
+ * If @p binary is @c FALSE, the function ensures that there is no
+ * embedded zeroes in the file content and strips off trailing newlines
+ * if there are any.
+ *
+ * If @p maxsize is not zero, the function ensures that the file size
+ * is not greater than it. That allows to avoid crashes if @p dest
+ * is a statically allocated TE string and in general to prevent memory
+ * bombs if a file comes from an untrusted source.
+ *
+ * @note The file should be random-access, so the function cannot read
+ *       reliably from sockets, named FIFOs and most kinds of character devices.
+ *
+ * @param dest     TE string (the content of the file will be appended to it)
+ * @param binary   reading mode (binary or text mode)
+ * @param maxsize  the maximum allowed file size, if not zero
+ * @param path_fmt pathname format string
+ * @param ...      arguments
+ *
+ * @return status code
+ * @retval TE_EFBIG     The file size is larger than @p maxsize.
+ * @retval TE_EILSEQ    The file contains embedded zeroes and
+ *                      @p binary is @c FALSE.
+ */
+extern te_errno te_file_read_string(te_string *dest, te_bool binary,
+                                    size_t maxsize, const char *path_fmt, ...)
+                                    __attribute__((format(printf, 4, 5)));
+
+/**
+ * Write the contents of the file from a dynamic string @p src.
+ *
+ * The file is opened with POSIX @p flags ORed with @c O_WRONLY
+ * and if the file is created, then access @p mode will be set on it.
+ *
+ * If @p fitlen is not zero, the resulting file will be exactly that long:
+ * - if @p src is longer than @p fitlen, it will be truncated;
+ * - if @p src is shorter, it will be repeated until the length is reached.
+ *
+ * @param src      source TE string
+ * @param fitlen   desired file length (ignored if zero)
+ * @param flags    POSIX open flags, such as @c O_CREAT
+ * @param mode     POSIX access mode for newly created files
+ * @param path_fmt pathname format string
+ * @param ...      arguments
+ *
+ * @return status code
+ */
+extern te_errno te_file_write_string(const te_string *src, size_t fitlen,
+                                     int flags, mode_t mode,
+                                     const char *path_fmt, ...)
+                                     __attribute__((format(printf, 5, 6)));
 
 /**
  * Read the contents of the file @p path into @p buffer.
@@ -245,8 +299,9 @@ extern te_errno te_unlink_fmt(const char *fmt, ...)
  *
  * @return Status code
  * @retval TE_EFBIG     The file size is larger than @p bufsize
- * @retval TE_ESPIPE    The file is not random-access
  * @retval TE_EILSEQ    The file contains embedded zeroes
+ *
+ * @deprecated Prefer te_file_read_string().
  */
 extern te_errno te_file_read_text(const char *path, char *buffer,
                                   size_t bufsize);

suites/selftest/conf/trc/tools.trc.xml
@@ -21,6 +21,8 @@
         <xi:include xmlns:xi="http://www.w3.org/2003/XInclude"
                     href="tools/extract_glob.trc.xml" parse="xml"/>
         <xi:include xmlns:xi="http://www.w3.org/2003/XInclude"
+                    href="tools/file.trc.xml" parse="xml"/>
+        <xi:include xmlns:xi="http://www.w3.org/2003/XInclude"
                     href="tools/hexdump.trc.xml" parse="xml"/>
         <xi:include xmlns:xi="http://www.w3.org/2003/XInclude"
                     href="tools/intset.trc.xml" parse="xml"/>

suites/selftest/ts/tools/file.c
@@ -1,0 +1,132 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (C) 2023 OKTET Labs Ltd. All rights reserved. */
+/** @file
+ * @brief Testing file read-write functions
+ *
+ * Testing te_file_read_string and te_file_write_string correctness.
+ */
+
+/** @page tools_file te_file_read_string and te_file_write_string test
+ *
+ * @objective Testing te_file_read_string and te_file_write_string correctness.
+ *
+ * @par Test sequence:
+ *
+ */
+
+/** Logging subsystem entity name */
+#define TE_TEST_NAME    "tools/file"
+
+#include "te_config.h"
+
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include "tapi_test.h"
+#include "te_file.h"
+
+int
+main(int argc, char **argv)
+{
+    te_string outbuf = TE_STRING_INIT;
+    te_string inbuf = TE_STRING_INIT;
+    te_string padding = TE_STRING_INIT;
+    te_string expected = TE_STRING_INIT;
+    const char *content;
+    te_bool binary;
+    char *path = NULL;
+    te_errno chk_rc;
+
+    TEST_START;
+    TEST_GET_STRING_PARAM(content);
+    TEST_GET_BOOL_PARAM(binary);
+
+    TEST_STEP("Create a temporary file");
+    CHECK_NOT_NULL((path = te_file_create_unique("/tmp/te_file_", NULL)));
+
+    TEST_STEP("Write to a file");
+    te_string_append(&outbuf, "%s", content);
+    CHECK_RC(te_file_write_string(&outbuf, 0, 0, 0, "%s", path));
+
+    TEST_STEP("Append padding to a file");
+    if (binary)
+        te_string_append_buf(&padding, (const char[]){'\0'}, 1);
+    else
+        te_string_append(&padding, "\n\n\n\n");
+    CHECK_RC(te_file_write_string(&padding, 0, O_APPEND, 0, "%s", path));
+
+    TEST_STEP("Reading from a file");
+    CHECK_RC(te_file_read_string(&inbuf, binary, 0, "%s", path));
+    te_string_append_buf(&expected, outbuf.ptr, outbuf.len);
+    if (binary)
+        te_string_append_buf(&expected, padding.ptr, padding.len);
+    if (!te_compare_bufs(expected.ptr, expected.len, 1,
+                         inbuf.ptr, inbuf.len, TE_LL_ERROR))
+    {
+        TEST_VERDICT("The contents of file being read is different "
+                     "from written");
+    }
+
+    TEST_STEP("Checking for maxsize limit");
+    chk_rc = te_file_read_string(&inbuf, binary, strlen(content), "%s", path);
+    if (chk_rc != TE_EFBIG)
+        TEST_VERDICT("The maximum size of a file is not detected: %r", chk_rc);
+
+    if (binary)
+    {
+        TEST_STEP("Checking embedded zero detection");
+        chk_rc = te_file_read_string(&inbuf, FALSE, 0, "%s", path);
+        if (chk_rc != TE_EILSEQ)
+            TEST_VERDICT("Embedded zeroes are not detected: %r", chk_rc);
+    }
+    else
+    {
+        TEST_STEP("Reading text as binary");
+        te_string_reset(&inbuf);
+        te_string_reset(&expected);
+        te_string_append_buf(&expected, outbuf.ptr, outbuf.len);
+        te_string_append_buf(&expected, padding.ptr, padding.len);
+
+        CHECK_RC(te_file_read_string(&inbuf, TRUE, 0, "%s", path));
+        if (!te_compare_bufs(expected.ptr, expected.len, 1,
+                             inbuf.ptr, inbuf.len, TE_LL_ERROR))
+        {
+            TEST_VERDICT("The contents of file being read as binary "
+                         "differs from written");
+        }
+    }
+
+    TEST_STEP("Checking replicated writing");
+    te_string_reset(&inbuf);
+    te_string_reset(&expected);
+    /* Not a typo: indeed append two copies of outbuf */
+    te_string_append_buf(&expected, outbuf.ptr, outbuf.len);
+    te_string_append_buf(&expected, outbuf.ptr, outbuf.len);
+    te_string_append_buf(&expected, outbuf.ptr, 1);
+
+    CHECK_RC(te_file_write_string(&outbuf, expected.len, O_TRUNC, 0,
+                                  "%s", path));
+    CHECK_RC(te_file_read_string(&inbuf, binary, 0, "%s", path));
+    if (!te_compare_bufs(expected.ptr, expected.len, 1,
+                         inbuf.ptr, inbuf.len, TE_LL_ERROR))
+        TEST_VERDICT("The content of file differs from a replicated write");
+
+    TEST_SUCCESS;
+
+cleanup:
+
+    te_string_free(&expected);
+    te_string_free(&outbuf);
+    te_string_free(&inbuf);
+    te_string_free(&padding);
+
+    if (path != NULL)
+    {
+        unlink(path);
+        free(path);
+    }
+
+    TEST_END;
+}

suites/selftest/ts/tools/meson.build
@@ -8,6 +8,7 @@ tests = [
     'expand',
     'expand_filters',
     'extract_glob',
+    'file',
     'hexdump',
     'intset',
     'json',

suites/selftest/ts/tools/package.xml
@@ -197,6 +197,17 @@
         </run>
 
         <run>
+            <script name="file"/>
+            <arg name="content">
+                <value>Test data</value>
+            </arg>
+            <arg name="binary">
+                <value>FALSE</value>
+                <value>TRUE</value>
+            </arg>
+        </run>
+
+        <run>
             <script name="hexdump"/>
         </run>
 

suites/selftest/conf/trc/tools/file.trc.xml
@@ -1,0 +1,11 @@
+<?xml version="1.0"?>
+<!-- SPDX-License-Identifier: Apache-2.0 -->
+<!-- Copyright (C) 2023 OKTET Labs Ltd. All rights reserved. -->
+<test name="file" type="script">
+    <objective>Testing te_file_read_string and te_file_write_string correctness.</objective>
+    <notes/>
+    <iter result="PASSED">
+        <arg name="content" />
+        <arg name="binary" />
+    </iter>
+</test>

commit 2132fbd1750ef1f7f7cc52adefaa963a68bd6506

lib/tools: allow reading files in chunks
One of the initial goals of te_file_read_string
was reading files in /proc filesystem on the agent.
However, it turns out it could not do that, because
the size of such files is always reported as zero.
The patch amends the function to be able to read
such files in chunks.

Fixes: 19eab56b47a2 ("lib/tools: add API to read/write from/into dynamic strings")
OL-Redmine-Id: 13264
Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>

lib/tools/te_file.c
@@ -396,7 +396,7 @@ te_file_read_string(te_string *dest, te_bool binary,
     struct stat st;
     te_errno rc = 0;
     int fd = -1;
-    ssize_t actual;
+    ssize_t actual = 0;
 
     va_start(args, path_fmt);
     te_string_append_va(&pathname, path_fmt, args);
@@ -409,13 +409,6 @@ te_file_read_string(te_string *dest, te_bool binary,
         goto out;
     }
 
-    if (!S_ISREG(st.st_mode))
-    {
-        WARN("'%s' is not a regular file or symlink, "
-             "%s() may not return the expected data",
-             pathname.ptr, __func__);
-    }
-
     if (maxsize != 0 && st.st_size > maxsize)
     {
         ERROR("File %s's size (%zu) is larger than %zu",
@@ -423,7 +416,6 @@ te_file_read_string(te_string *dest, te_bool binary,
         rc = TE_EFBIG;
         goto out;
     }
-    te_string_reserve(dest, dest->len + st.st_size + 1);
 
     fd  = open(pathname.ptr, O_RDONLY);
     if (fd < 0)
@@ -434,20 +426,55 @@ te_file_read_string(te_string *dest, te_bool binary,
         goto out;
     }
 
-    actual = read(fd, dest->ptr + dest->len, st.st_size);
-    if (actual < 0)
+    if (st.st_size > 0)
     {
-        rc = te_rc_os2te(errno);
-        ERROR("Cannot read from '%s': %r", pathname.ptr, rc);
-        goto out;
-    }
+        te_string_reserve(dest, dest->len + st.st_size + 1);
+        actual = read(fd, dest->ptr + dest->len, st.st_size);
+        if (actual < 0)
+        {
+            rc = te_rc_os2te(errno);
+            ERROR("Cannot read from '%s': %r", pathname.ptr, rc);
+            goto out;
+        }
 
-    if ((size_t)actual != (size_t)st.st_size)
+        if ((size_t)actual != (size_t)st.st_size)
+        {
+            ERROR("Could not read %zu bytes from %s, only %zu were read",
+                  (size_t)st.st_size, pathname.ptr, (size_t)actual);
+            rc = TE_EIO;
+            goto out;
+        }
+    }
+    else
     {
-        ERROR("Could not read %zu bytes from %s, only %zu were read",
-              (size_t)st.st_size, pathname.ptr, (size_t)actual);
-        rc = TE_EIO;
-        goto out;
+        /* The value is pretty arbitrary */
+        ssize_t chunk_size = 4096;
+        ssize_t actual_chunk;
+
+        do
+        {
+            if (maxsize != 0 && actual + chunk_size > maxsize)
+            {
+                chunk_size = maxsize - actual;
+                if (chunk_size == 0)
+                {
+                    ERROR("Cannot read more than %zu bytes from %s",
+                          maxsize, pathname.ptr);
+                    rc = TE_EFBIG;
+                    goto out;
+                }
+            }
+            te_string_reserve(dest, dest->len + actual + chunk_size);
+            actual_chunk = read(fd, dest->ptr + dest->len + actual, chunk_size);
+            if (actual_chunk < 0)
+            {
+                rc = te_rc_os2te(errno);
+                ERROR("Cannot read from '%s' after %zd bytes: %r",
+                      pathname.ptr, actual, rc);
+                goto out;
+            }
+            actual += actual_chunk;
+        } while (actual_chunk > 0);
     }
 
     dest->len += actual;

lib/tools/te_file.h
@@ -275,8 +275,10 @@ extern te_errno te_unlink_fmt(const char *fmt, ...)
  * is a statically allocated TE string and in general to prevent memory
  * bombs if a file comes from an untrusted source.
  *
- * @note The file should be random-access, so the function cannot read
- *       reliably from sockets, named FIFOs and most kinds of character devices.
+ * @note The function should be used with caution on non-regular files
+ *       such as named FIFOs or character devices, as it may block
+ *       indefinitely or return partial data. Using it with regular files
+ *       on special filesystems such as @c /proc is completely reliable.
  *
  * @param dest     TE string (the content of the file will be appended to it)
  * @param binary   reading mode (binary or text mode)

suites/selftest/conf/trc/tools.trc.xml
@@ -27,6 +27,8 @@
         <xi:include xmlns:xi="http://www.w3.org/2003/XInclude"
                     href="tools/file.trc.xml" parse="xml"/>
         <xi:include xmlns:xi="http://www.w3.org/2003/XInclude"
+                    href="tools/file_chunked.trc.xml" parse="xml"/>
+        <xi:include xmlns:xi="http://www.w3.org/2003/XInclude"
                     href="tools/hexdump.trc.xml" parse="xml"/>
         <xi:include xmlns:xi="http://www.w3.org/2003/XInclude"
                     href="tools/intset.trc.xml" parse="xml"/>

suites/selftest/conf/trc/tools/file_chunked.trc.xml
@@ -1,0 +1,10 @@
+<?xml version="1.0"?>
+<!-- SPDX-License-Identifier: Apache-2.0 -->
+<!-- Copyright (C) 2023 OKTET Labs Ltd. All rights reserved. -->
+<test name="file_chunked" type="script">
+    <objective>Testing te_file_read_string correctness on chunked reads.</objective>
+    <notes/>
+    <iter result="PASSED">
+        <arg name="pathname" />
+    </iter>
+</test>

suites/selftest/ts/tools/file.c
@@ -73,6 +73,8 @@ main(int argc, char **argv)
     chk_rc = te_file_read_string(&inbuf, binary, strlen(content), "%s", path);
     if (chk_rc != TE_EFBIG)
         TEST_VERDICT("The maximum size of a file is not detected: %r", chk_rc);
+    if (inbuf.len != expected.len)
+        TEST_VERDICT("Buffer not rewound after error");
 
     if (binary)
     {

suites/selftest/ts/tools/file_chunked.c
@@ -1,0 +1,102 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (C) 2023 OKTET Labs Ltd. All rights reserved. */
+/** @file
+ * @brief Testing chunked read from a file
+ *
+ * Testing te_file_read_string chunked read correcteness
+ */
+
+/** @page tools_file_chunked te_file_read_string chunked read test
+ *
+ * @objective Testing te_file_read_string correctness on chunked reads.
+ *
+ * @par Test sequence:
+ *
+ */
+
+/** Logging subsystem entity name */
+#define TE_TEST_NAME    "tools/file_chunked"
+
+#include "te_config.h"
+
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include "tapi_test.h"
+#include "te_file.h"
+
+static void
+system_read_file(const char *pathname, te_string *dest)
+{
+    FILE *f = fopen(pathname, "r");
+    char buf[1024];
+
+    if (f == NULL)
+    {
+        TEST_SKIP("Cannot open '%s': %r",
+                  pathname, te_rc_os2te(errno));
+    }
+
+    while (TRUE)
+    {
+        size_t actual = fread(buf, 1, sizeof(buf), f);
+
+        if (actual == 0)
+        {
+            if (ferror(f))
+            {
+                TEST_VERDICT("Cannot read from '%s': %r",
+                             pathname, te_rc_os2te(errno));
+            }
+            break;
+        }
+
+        te_string_append_buf(dest, buf, actual);
+    }
+    te_string_chop(dest, "\n");
+    fclose(f);
+}
+
+int
+main(int argc, char **argv)
+{
+    te_string inbuf = TE_STRING_INIT;
+    te_string expected = TE_STRING_INIT;
+    const char *pathname = NULL;
+    te_errno chk_rc;
+
+    TEST_START;
+    TEST_GET_STRING_PARAM(pathname);
+
+    TEST_STEP("Reading a file via system functions");
+    system_read_file(pathname, &expected);
+
+    TEST_STEP("Reading from a file via TE");
+    CHECK_RC(te_file_read_string(&inbuf, FALSE, 0, "%s", pathname));
+
+    if (!te_compare_bufs(expected.ptr, expected.len, 1,
+                         inbuf.ptr, inbuf.len, TE_LL_ERROR))
+    {
+        TEST_VERDICT("The contents of file being read is different "
+                     "from written");
+    }
+
+    TEST_STEP("Checking for maxsize limit");
+    chk_rc = te_file_read_string(&inbuf, FALSE, inbuf.len / 2, "%s",
+                                 pathname);
+    if (chk_rc != TE_EFBIG)
+        TEST_VERDICT("The maximum size of a file is not detected: %r", chk_rc);
+    if (inbuf.len != expected.len)
+        TEST_VERDICT("Buffer not rewound after error");
+
+    TEST_SUCCESS;
+
+cleanup:
+
+    te_string_free(&expected);
+    te_string_free(&inbuf);
+
+    TEST_END;
+}

suites/selftest/ts/tools/meson.build
@@ -11,6 +11,7 @@ tests = [
     'expand_list',
     'extract_glob',
     'file',
+    'file_chunked',
     'hexdump',
     'intset',
     'json',

suites/selftest/ts/tools/package.xml
@@ -239,15 +239,29 @@
             </arg>
         </run>
 
-        <run>
-            <script name="file"/>
-            <arg name="content">
-                <value>Test data</value>
-            </arg>
-            <arg name="binary">
-                <value>FALSE</value>
-                <value>TRUE</value>
-            </arg>
+        <run name="files">
+            <session>
+                <run>
+                    <script name="file"/>
+                    <arg name="content">
+                        <value>Test data</value>
+                    </arg>
+                    <arg name="binary">
+                        <value>FALSE</value>
+                        <value>TRUE</value>
+                    </arg>
+                </run>
+                <run>
+                    <script name="file_chunked"/>
+                    <!--
+                        This should be a file on a special filesystem like /proc
+                        but that does not change too frequently
+                    -->
+                    <arg name="pathname">
+                        <value>/proc/self/mounts</value>
+                    </arg>
+                </run>
+            </session>
         </run>
 
         <run>