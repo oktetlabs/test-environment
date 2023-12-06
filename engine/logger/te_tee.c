oktetlabs/test-environment

commit 1755a582f06905b27a40e2819dace29c869d7e1e

engine/logger: use new method to parse logs in TE tee
In the following patches, the processing of structured log
messages will be added, therefore, in this patch, the
processing of the log stream has been replaced with a
similar one, convenient for subsequent processing of
individual log messages.

OL-Redmine-Id: 13022
Signed-off-by: Ivan Khodyrev <ivan.khodyrev@oktetlabs.ru>
Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>

engine/logger/te_tee.c
@@ -1,11 +1,10 @@
 /* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (C) 2004-2023 OKTET Labs Ltd. All rights reserved. */
 /** @file
  * @brief Logging of input through TE Logger
  *
  * Redirection of stdout/stderr in two files: stdout merged with stderr,
  * stderr only.
- *
- * Copyright (C) 2004-2022 OKTET Labs Ltd. All rights reserved.
  */
 
 #include "te_config.h"
@@ -57,51 +56,61 @@
 #include "logger_api.h"
 #include "logger_ten.h"
 #include "te_raw_log.h"
+#include "te_str.h"
 
+#define DEFAULT_LL TE_LL_WARN
+#define DEFAULT_LOG_USER default_log_user
+
+#define LOG_ENTITY_BUF_LEN 20
+#define LOG_USER_BUF_LEN 15
+
+static const char *default_log_user;
+
+typedef struct log_msg {
+    te_log_level level;
+    char entity[LOG_ENTITY_BUF_LEN];
+    char user[LOG_USER_BUF_LEN];
+    te_string msg;
+} log_msg;
+
+#define LOG_MESSAGE_INIT \
+    {.level = 0, .entity = {0,}, .user = {0,}, .msg = TE_STRING_INIT}
+
+static inline void
+flush_msg_buffer(log_msg* msg)
+{
+    if (msg->msg.len > 0)
+    {
+        TE_LOG(msg->level, msg->entity, msg->user, "%s", msg->msg.ptr);
+
+        te_string_reset(&msg->msg);
+    }
+}
+
+static te_errno
+line_handler(char *line, void *user_data)
+{
+    log_msg *cur_msg = (log_msg *)user_data;
+
+    te_string_append(&cur_msg->msg, "%s\n", line);
+    flush_msg_buffer(cur_msg);
+
+    return 0;
+}
 
 int
 main (int argc, char *argv[])
 {
-    char          *buffer;
-    char          *current;
-    char          *fence;
-    char          *newline;
-    char          *rest = NULL;
-    int            interval;
-    int            current_timeout = -1;
-    int            len;
+    static char buffer[TE_LOG_FIELD_MAX + 1];
+    int interval;
+    int current_timeout = -1;
+    int len;
     struct pollfd  poller;
-    static char    buffer1[TE_LOG_FIELD_MAX + 1];
-    static char    buffer2[TE_LOG_FIELD_MAX + 1];
+    te_string buffer_str = TE_STRING_INIT;
+    log_msg cur_msg = LOG_MESSAGE_INIT;
+
 
     te_log_init("(Tee)", ten_log_message);
-
-#define MAYBE_DO_LOG \
-    do {                                                       \
-        if (current != buffer)                                 \
-        {                                                      \
-            *current = '\0';                                   \
-            newline = strrchr(buffer, '\n');                   \
-            if (newline)                                       \
-                *newline = '\0';                               \
-            LGR_MESSAGE(TE_LL_WARN, argv[2], "%s%s",           \
-                        rest ? rest : "",                      \
-                        buffer);                               \
-            if (!newline)                                      \
-            {                                                  \
-                fence = buffer + TE_LOG_FIELD_MAX;             \
-                rest = NULL;                                   \
-            }                                                  \
-            else                                               \
-            {                                                  \
-                rest = newline + 1;                            \
-                buffer = (buffer == buffer1 ? buffer2 : buffer1); \
-                fence = buffer + TE_LOG_FIELD_MAX - (current - rest);   \
-            }                                                  \
-            current_timeout = -1;                              \
-            current = buffer;                                  \
-        }                                                      \
-    } while (0)
 
     if (argc != 4)
     {
@@ -109,6 +118,7 @@ main (int argc, char *argv[])
         return EXIT_FAILURE;
     }
     te_log_init(argv[1], NULL);
+    default_log_user = argv[2];
 
     interval = strtol(argv[3], NULL, 10);
     if (interval <= 0)
@@ -117,11 +127,11 @@ main (int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
-    buffer  = buffer1;
-    current = buffer;
-    fence   = buffer + TE_LOG_FIELD_MAX;
-    *fence  = '\0';
     fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK | fcntl(STDIN_FILENO, F_GETFL));
+
+    te_strlcpy(cur_msg.entity, TE_LGR_ENTITY, sizeof(cur_msg.entity));
+    te_strlcpy(cur_msg.user, DEFAULT_LOG_USER, sizeof(cur_msg.user));
+    cur_msg.level = DEFAULT_LL;
 
     for (;;)
     {
@@ -134,11 +144,10 @@ main (int argc, char *argv[])
         VERB("something is available");
         if (poller.revents & POLLIN)
         {
-            VERB("trying to read %d bytes", fence - current);
-            len = read(poller.fd, current, fence - current);
+            VERB("trying to read %d bytes", sizeof(buffer));
+            len = read(poller.fd, buffer, sizeof(buffer));
             if (len <= 0)
             {
-                MAYBE_DO_LOG;
                 if (len < 0)
                     ERROR("Error reading from stdin: %s", strerror(errno));
                 break;
@@ -151,41 +160,38 @@ main (int argc, char *argv[])
              * if we actually see an error - something is wrong and we should
              * try to stop doing whatever we're doing.
              */
-            rc = write(STDOUT_FILENO, current, len);
+            rc = write(STDOUT_FILENO, buffer, len);
             if (rc < 0)
             {
                 ERROR("Failed to write date stdout: %s", strerror(errno));
                 break;
             }
 
-            current += len;
-            if (current == fence)
-            {
-                MAYBE_DO_LOG;
-            }
-            else
-            {
-                if (current_timeout < 0)
-                    current_timeout = interval;
-            }
+            te_string_append(&buffer_str, "%s", buffer);
+            te_string_process_lines(&buffer_str, TRUE, line_handler, &cur_msg);
+
+            if (current_timeout < 0)
+                current_timeout = interval;
         }
         else if (poller.revents & POLLERR)
         {
-            MAYBE_DO_LOG;
+            flush_msg_buffer(&cur_msg);
             ERROR("Error condition signaled on stdin");
             break;
         }
         else if (poller.revents & POLLHUP)
         {
-            MAYBE_DO_LOG;
+            flush_msg_buffer(&cur_msg);
             break;
         }
         else
         {
             VERB("timeout");
-            MAYBE_DO_LOG;
+            flush_msg_buffer(&cur_msg);
         }
     }
+
+    te_string_process_lines(&buffer_str, FALSE, line_handler, &cur_msg);
 
     return EXIT_SUCCESS;
 }

commit 52e46e58cea1a4be753b53245b827a9c18727dbc

engine/logger: avoid repeating output in tee
Limit the size of data appended to the "buffer string" by the length of
the data chunk actually read from the child process to avoid various
memory-related errors. The most common is repeating output because of
the lack of null terminator in the original buffer.

Fixes: 1755a582f069 ("engine/logger: use new method to parse logs in TE tee")

Signed-off-by: Viacheslav Galaktionov <viacheslav.galaktionov@arknetworks.am>

engine/logger/te_tee.c
@@ -239,7 +239,7 @@ main (int argc, char *argv[])
                 break;
             }
 
-            te_string_append(&buffer_str, "%s", buffer);
+            te_string_append_buf(&buffer_str, buffer, len);
             te_string_process_lines(&buffer_str, TRUE, line_handler, &cur_msg);
 
             if (current_timeout < 0)