oktetlabs/test-environment

commit 616a730861825430db86b57355a9b6de426a5398

lib/tools: implement ring buffers
Ring buffers are a generally useful data structure,
but the immediate goal is to support a list of output
matches for Nginx in the forthcoming patches.

OL-Redmine-Id: 13235
Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>
Reviewed-by: Anatoly Mirin <anatoly.mirin@oktetlabs.ru>

lib/tools/te_ring.h
@@ -1,0 +1,179 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (C) 2023 OKTET Labs Ltd. All rights reserved. */
+/** @file
+ * @brief Ring buffers.
+ *
+ * Ring buffers are an efficient way to store
+ * N last items from a continuous stream of data.
+ *
+ * @defgroup te_tools_te_ring Ring buffers.
+ * @ingroup te_tools
+ * @{
+ */
+
+#ifndef __TE_RING_H__
+#define __TE_RING_H__
+
+#include "te_config.h"
+
+#include "te_defs.h"
+#include "te_errno.h"
+#include "te_vector.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Ring buffer structure.
+ *
+ * All the fields may be inspected but must never
+ * be directly modified by the user.
+ */
+typedef struct te_ring {
+    /** Ring size. */
+    size_t ring_size;
+    /** Underlying data vector. */
+    te_vec data;
+    /** Sequential read pointer. */
+    size_t rptr;
+    /** Number of items in the ring. */
+    size_t fill;
+} te_ring;
+
+/**
+ * Initialize a ring to store objects of a given @p type_.
+ *
+ * @param type_       type of elements
+ * @param destroy_    element destructor
+ * @param ring_size_  initial ring size
+ */
+#define TE_RING_INIT(type_, destroy_, ring_size_) \
+    {                                                       \
+        .ring_size = (ring_size_),                          \
+        .data = TE_VEC_INIT_DESTROY(type_, destroy_)        \
+    }
+
+/**
+ * Put the content of @p element into the @p ring.
+ *
+ * If the ring is already full, the oldest element is discarded.
+ * If the ring has non-null destructor, it is called for the discarded
+ * element.
+ *
+ * @param[in,out] ring     ring buffer
+ * @param[in]     element  new data
+ *
+ * @return status code
+ * @retval TE_ENOBUFS  The ring buffer has overflowed.
+ *                     It is not an error, but the caller may
+ *                     take some appropriate action if needed.
+ */
+extern te_errno te_ring_put(te_ring *ring, const void *element);
+
+/**
+ * Get the oldest element from the @p ring.
+ *
+ * The element is removed from the ring and
+ * the read pointer is moved to the next element.
+ *
+ * The data are put into @p element if it is not @c NULL;
+ * otherwise the data are discared (the element destructor is
+ * called if it's non-null).
+ *
+ * If there is no data in @p ring, @p element is not changed.
+ *
+ * @param[in,out]  ring      ring buffer
+ * @param[out]     element   destination buffer (may be @c NULL)
+ *
+ * @return status code
+ * @retval TE_ENODATA    The ring is empty.
+ */
+extern te_errno te_ring_get(te_ring *ring, void *element);
+
+/**
+ * Get the oldest element from the ring @p keeping it.
+ *
+ * The pointer inside the ring itself is returned, the caller
+ * must treat it as a pointer to local storage.
+ *
+ * The read pointer is not moved.
+ *
+ * @param ring   ring buffer
+ *
+ * @return the pointer to the data or @c NULL if the ring is empty
+ */
+extern const void *te_ring_peek(const te_ring *ring);
+
+/**
+ * Copy at most @p count oldest elements from @p ring to @p dest.
+ *
+ * The read pointer is not moved. The data are appended to @p dest.
+ *
+ * @param[in]  ring    ring buffer
+ * @param[in]  count   maximum number of elements to copy
+ * @param[out] dest    destination vector
+ *
+ * @return the number of elements actually copied (may be 0)
+ *
+ * @pre @p dest must have a null destructor.
+ */
+extern size_t te_ring_copy(const te_ring *ring, size_t count, te_vec *dest);
+
+/**
+ * Put @p count items from @p elements to @p ring.
+ *
+ * The ring is never overran, if there is not enough space
+ * in @p ring, fewer elements are put.
+ *
+ * @param[in,out] ring      ring buffer
+ * @param[in]     count     maximum number of elements to put
+ * @param[in]     elements  new data
+ *
+ * @return the number of elements actually put (may be 0)
+ */
+extern size_t te_ring_put_many(te_ring *ring, size_t count,
+                               const void *elements);
+
+/**
+ * Get at most @p count oldest elements from @p ring and put them to @p dest.
+ *
+ * The elements are removed from the ring and the read pointer is moved
+ * as needed.
+ *
+ * The data are appended to @p dest,
+ * if @p dest is not @c NULL, otherwise the data are discarded (the element
+ * destructor is called if it's not null).
+ *
+ * @param[in,out] ring   ring buffer
+ * @param[in]     count  maximum number of elements to put
+ * @param[out]    dest   destination vector (may be @c NULL)
+ *
+ * @return the number of elements actually got.
+ */
+extern size_t te_ring_get_many(te_ring *ring, size_t count, te_vec *dest);
+
+/**
+ * Change the size of @p ring to @p new_ring_size.
+ *
+ * If @p new_ring_size is less than the current fill of the ring,
+ * oldest redundant items are discarded. In all other cases all the data
+ * are preserved, but the layout may change.
+ *
+ * @param[in,out] ring             ring buffer
+ * @param[in]     new_ring_size    new size of the ring
+ */
+extern void te_ring_resize(te_ring *ring, size_t new_ring_size);
+
+/**
+ * Free any resources associated with @p ring.
+ *
+ * @param ring     ring buffer
+ */
+extern void te_ring_free(te_ring *ring);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+#endif /* !__TE_RING_H__ */
+/**@} <!-- END te_tools --> */

lib/tools/te_vector.h
@@ -123,6 +123,20 @@ typedef struct te_vec {
     TE_VEC_INIT_COMPLETE(type_, TE_DBUF_DEFAULT_GROW_FACTOR, destroy_)
 
 /*
+ * Initialize a vector with the same parameters as @p other_.
+ *
+ * Data are *not* copied.
+ *
+ * @param other_    A pointer to another vector.
+ */
+#define TE_VEC_INIT_LIKE(other_) \
+    (te_vec){                                             \
+        .data = TE_DBUF_INIT((other_)->data.grow_factor), \
+        .element_size = (other_)->element_size,           \
+        .destroy = (other_)->destroy,                     \
+    }
+
+/*
  * Set a vector element destructor with sanity checks.
  *
  * The following rules are used to minimize the risk of

lib/tools/meson.build
@@ -35,6 +35,7 @@ headers += files(
     'te_pci.h',
     'te_pci_ids.h',
     'te_rand.h',
+    'te_ring.h',
     'te_serial.h',
     'te_serial_common.h',
     'te_serial_parser.h',
@@ -78,6 +79,7 @@ sources += files(
     'te_numeric.c',
     'te_pci.c',
     'te_rand.c',
+    'te_ring.c',
     'te_shell_cmd.c',
     'te_sigmap.c',
     'te_sockaddr.c',

lib/tools/te_ring.c
@@ -1,0 +1,185 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (C) 2023 OKTET Labs Ltd. All rights reserved. */
+/** @file
+ * @brief Ring buffers.
+ *
+ * Implementation of ring buffers.
+ */
+
+#define TE_LGR_USER     "TE ring buffers"
+
+#include "te_config.h"
+
+#include <assert.h>
+
+#include "te_defs.h"
+#include "te_errno.h"
+
+#include "te_ring.h"
+
+#include "logger_api.h"
+
+te_errno
+te_ring_put(te_ring *ring, const void *element)
+{
+    size_t wptr = (ring->rptr + ring->fill) % ring->ring_size;
+
+    assert(element != NULL);
+
+    te_vec_replace(&ring->data, wptr, element);
+    if (ring->fill == ring->ring_size)
+    {
+        ring->rptr++;
+        return TE_ENOBUFS;
+    }
+    else
+    {
+        ring->fill++;
+        return 0;
+    }
+}
+
+te_errno
+te_ring_get(te_ring *ring, void *element)
+{
+    if (ring->fill == 0)
+        return TE_ENODATA;
+
+    te_vec_transfer(&ring->data, ring->rptr % ring->ring_size, element);
+    ring->rptr++;
+    ring->fill--;
+
+    return 0;
+}
+
+const void *
+te_ring_peek(const te_ring *ring)
+{
+    if (ring->fill == 0)
+        return NULL;
+
+    return te_vec_get_immutable(&ring->data, ring->rptr % ring->ring_size);
+}
+
+size_t
+te_ring_copy(const te_ring *ring, size_t count, te_vec *dest)
+{
+    size_t index = ring->rptr % ring->ring_size;
+
+    if (count > ring->fill)
+        count = ring->fill;
+
+    if (count == 0)
+        return 0;
+
+    assert(dest->destroy == NULL);
+    assert(dest->element_size == ring->data.element_size);
+
+    if (index + count <= ring->ring_size)
+    {
+        te_vec_append_array(dest, te_vec_get_immutable(&ring->data, index),
+                            count);
+    }
+    else
+    {
+        size_t tail_count = ring->ring_size - index;
+
+        te_vec_append_array(dest, te_vec_get_immutable(&ring->data, index),
+                            tail_count);
+        te_vec_append_array(dest, te_vec_get_immutable(&ring->data, 0),
+                            count - tail_count);
+    }
+
+    return count;
+}
+
+size_t
+te_ring_put_many(te_ring *ring, size_t count, const void *elements)
+{
+    size_t n = 0;
+    const uint8_t *iter = elements;
+
+    assert(count == 0 || elements != NULL);
+
+    while (ring->fill < ring->ring_size && n < count)
+    {
+        te_ring_put(ring, iter);
+        iter += ring->data.element_size;
+        n++;
+    }
+
+    return n;
+}
+
+static void
+ring_linearize(te_ring *ring, size_t count, te_vec *dest)
+{
+    size_t index = ring->rptr % ring->ring_size;
+
+    if (index + count <= ring->ring_size)
+        te_vec_transfer_append(&ring->data, index, count, dest);
+    else
+    {
+        size_t tail_count = ring->ring_size - index;
+
+        te_vec_transfer_append(&ring->data, index, tail_count, dest);
+        te_vec_transfer_append(&ring->data, 0, count - tail_count, dest);
+    }
+}
+
+size_t
+te_ring_get_many(te_ring *ring, size_t count, te_vec *dest)
+{
+    if (count > ring->fill)
+        count = ring->fill;
+
+    if (count == 0)
+        return 0;
+
+    ring_linearize(ring, count, dest);
+
+    ring->rptr += count;
+    ring->fill -= count;
+
+    return count;
+}
+
+void
+te_ring_resize(te_ring *ring, size_t new_ring_size)
+{
+    size_t old_index;
+    size_t new_index;
+
+    if (new_ring_size < ring->fill)
+        te_ring_get_many(ring, ring->fill - new_ring_size, NULL);
+
+    old_index = ring->rptr % ring->ring_size;
+    new_index = ring->rptr % new_ring_size;
+
+    if (old_index == new_index &&
+        old_index + ring->fill <= ring->ring_size &&
+        new_index + ring->fill <= new_ring_size)
+    {
+        ring->ring_size = new_ring_size;
+    }
+    else
+    {
+        te_vec tmp = TE_VEC_INIT_LIKE(&ring->data);
+
+        tmp.destroy = NULL;
+        ring_linearize(ring, ring->fill, &tmp);
+
+        ring->fill = 0;
+        ring->ring_size = new_ring_size;
+        te_ring_put_many(ring, te_vec_size(&tmp), tmp.data.ptr);
+
+        te_vec_free(&tmp);
+    }
+
+}
+
+void
+te_ring_free(te_ring *ring)
+{
+    te_vec_free(&ring->data);
+}

suites/selftest/conf/trc/tools.trc.xml
@@ -41,6 +41,8 @@
         <xi:include xmlns:xi="http://www.w3.org/2003/XInclude"
                     href="tools/make_bufs.trc.xml" parse="xml"/>
         <xi:include xmlns:xi="http://www.w3.org/2003/XInclude"
+                    href="tools/rings.trc.xml" parse="xml"/>
+        <xi:include xmlns:xi="http://www.w3.org/2003/XInclude"
                     href="tools/scalar_types.trc.xml" parse="xml"/>
         <xi:include xmlns:xi="http://www.w3.org/2003/XInclude"
                     href="tools/scandir.trc.xml" parse="xml"/>

suites/selftest/conf/trc/tools/rings.trc.xml
@@ -1,0 +1,8 @@
+<test name="rings" type="script">
+    <objective>Testing ring buffer functions.</objective>
+    <notes/>
+    <iter result="PASSED">
+        <arg name="n_iterations" />
+        <arg name="max_ring_size" />
+    </iter>
+</test>

suites/selftest/ts/tools/meson.build
@@ -19,6 +19,7 @@ tests = [
     'lines',
     'make_bufs',
     'readlink',
+    'rings',
     'resolvepath',
     'scalar_types',
     'scandir',

suites/selftest/ts/tools/package.xml
@@ -295,6 +295,16 @@
         </run>
 
         <run>
+            <script name="rings"/>
+            <arg name="n_iterations">
+                <value>10000</value>
+            </arg>
+            <arg name="max_ring_size">
+                <value>100</value>
+            </arg>
+        </run>
+
+        <run>
             <script name="scalar_types"/>
             <arg name="src_type">
                 <value>te_bool</value>

commit 1d5b7a4b20b7c5896cf27ce2106fd5010b169a9e

lib/tools: add a short initializer for pointer vectors
The case of vectors owning heap-allocated pointers is
supposedly the most common usecase for non-null destructors,
so a special shortcut initializer would make code more
concise and clear.

Fixes: 616a73086182 ("lib/tools: implement ring buffers")
Fixes: 111693d8d5dc ("lib/tools: store element destructors in vectors")
OL-Redmine-Id: 13200
Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>

lib/tools/te_ring.h
@@ -56,6 +56,11 @@ typedef struct te_ring {
         .fill = 0,                                          \
     }
 
+#define TE_RING_INIT_AUTOPTR(type_, ring_size_) \
+    TE_RING_INIT(type_, TE_COMPILE_TIME_ASSERT_EXPR(sizeof(type_) ==   \
+                                                    sizeof(void *)) ?  \
+                 te_vec_item_free_ptr : NULL, ring_size)
+
 /**
  * Put the content of @p element into the @p ring.
  *

lib/tools/te_vector.h
@@ -128,6 +128,21 @@ typedef struct te_vec {
 #define TE_VEC_INIT_DESTROY(type_, destroy_) \
     TE_VEC_INIT_COMPLETE(type_, TE_DBUF_DEFAULT_GROW_FACTOR, destroy_)
 
+
+/**
+ * Vector initializer for heap-allocated pointers.
+ *
+ * The destructor is set to te_vec_item_free_ptr().
+ * Additionally a check is made that @p type_ is at least
+ * the same size as a pointer type.
+ *
+ * @param type_     Element type (which must be a pointer type).
+ */
+#define TE_VEC_INIT_AUTOPTR(type_) \
+    TE_VEC_INIT_DESTROY(type_,                                          \
+                        TE_COMPILE_TIME_ASSERT_EXPR(sizeof(type_) ==    \
+                                                    sizeof(void *)) ?   \
+                        te_vec_item_free_ptr : NULL)
 /*
  * Initialize a vector with the same parameters as @p other_.
  *

suites/selftest/ts/tools/rings.c
@@ -276,7 +276,7 @@ static void
 check_ring_heap_buf(unsigned int max_ring_size)
 {
     unsigned int ring_size = rand_range(1, max_ring_size);
-    te_ring ring = TE_RING_INIT(void *, te_vec_item_free_ptr, ring_size);
+    te_ring ring = TE_RING_INIT_AUTOPTR(void *, ring_size);
     unsigned int i;
 
     for (i = 0; i < ring_size; i++)

suites/selftest/ts/tools/vector_mem.c
@@ -28,6 +28,7 @@
 #include "tapi_test.h"
 #include "te_vector.h"
 
+
 static te_vec_item_destroy_fn count_destroy;
 static unsigned int destroy_cnt;
 static void
@@ -260,6 +261,31 @@ check_transfer_append(unsigned int max_elements)
     te_vec_free(&dest_vec);
 }
 
+static void
+check_vector_heap_buf(unsigned int max_elements)
+{
+    unsigned int n = rand_range(1, max_elements);
+    te_vec vec = TE_VEC_INIT_AUTOPTR(void *);
+    unsigned int i;
+
+    for (i = 0; i < n; i++)
+    {
+        void *mem = TE_ALLOC(1);
+        void *mem1 = TE_ALLOC(1);
+        void *mem2 = TE_ALLOC(1);
+        void *mem3 = NULL;
+
+        TE_VEC_APPEND(&vec, mem);
+        te_vec_replace(&vec, i, &mem1);
+        te_vec_transfer(&vec, i, &mem3);
+        if (mem3 != mem1)
+            TEST_VERDICT("Unexpected contents after transfer");
+        te_vec_replace(&vec, i, &mem2);
+        free(mem1);
+    }
+
+    te_vec_free(&vec);
+}
 
 int
 main(int argc, char **argv)
@@ -295,6 +321,10 @@ main(int argc, char **argv)
     TEST_STEP("Checking element bulk transferral");
     for (i = 0; i < n_iterations; i++)
         check_transfer_append(max_elements);
+
+    TEST_STEP("Checking vector destructors with real heap buffers");
+    for (i = 0; i < n_iterations; i++)
+        check_vector_heap_buf(max_elements);
 
     TEST_SUCCESS;
 