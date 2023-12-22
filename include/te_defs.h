oktetlabs/test-environment

commit a6bb18873f9bb585e62105379b9303a4321a3303

include: add a typeof wrapper
typeof() is going standard in C23 and it has been
supported by gcc and clang for ages. It is a very
useful feature which is already employed by TE,
so it's important to mitigate the difference between
compilers in C23 and C11/C17 mode.

OL-Redmine-Id: 12761
Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>

include/te_defs.h
@@ -482,7 +482,8 @@ te_round_up_pow2(unsigned long long num)
 #define TE_ALIGN_MASK(_v, _m)       (((_v) + (_m)) & ~(_m))
 
 /** Align up a value to a power of two */
-#define TE_ALIGN(_v, _a)            TE_ALIGN_MASK((_v), (typeof(_v))(_a) - 1)
+#define TE_ALIGN(_v, _a) \
+    TE_ALIGN_MASK((_v), TE_TYPEOF_CAST(_v, _a) - 1)
 
 
 /**
@@ -540,7 +541,7 @@ te_round_up_pow2(unsigned long long num)
 #ifdef __GNUC__
 #define TE_CONST_PTR_CAST(_type, _ptr) \
     (TE_COMPILE_TIME_ASSERT_EXPR(                                        \
-        __builtin_types_compatible_p(__typeof__(_ptr), const _type *)) ? \
+        __builtin_types_compatible_p(TE_TYPEOF(_ptr), const _type *)) ? \
      ((_type *)(uintptr_t)(_ptr)) : NULL)
 #else
 #define TE_CONST_PTR_CAST(_type, _ptr) ((_type *)(uintptr_t)(_ptr))

include/te_compiler.h
@@ -81,4 +81,59 @@
 #define TE_DEPRECATED
 #endif
 
+/*
+ * typeof is supported by gcc and clang and clang defines __GNUC__,
+ * so a single check is sufficient.
+ */
+#ifdef __GNUC__
+/**
+ * A feature macro indicating whether @c typeof is supported.
+ *
+ * @todo Add a check for C23's @c typeof when it is standardized.
+ */
+#define TE_HAS_RELIABLE_TYPEOF 1
+#else
+#define TE_HAS_RELIABLE_TYPEOF 0
+#endif
+
+#if TE_HAS_RELIABLE_TYPEOF
+/**
+ * Get a type of @p obj_.
+ *
+ * If @c typeof is not supported on a given platform,
+ * it falls back to @c void which will certainly break
+ * all non-pointer cases, but at least `TE_TYPEOF(obj) *`
+ * would remain compilable.
+ *
+ * For better portability TE_CAST_TYPEOF() should be
+ * used instead of `(TE_TYPEOF(obj))(val)`.
+ */
+#define TE_TYPEOF(obj_) __typeof__(obj_)
+
+/**
+ * Cast @p src_ to the type of @p obj_.
+ *
+ * If @c typeof is not supported, it falls back to no-op.
+ *
+ * @param obj_   Object of target type.
+ * @param src_   Source object.
+ */
+#define TE_CAST_TYPEOF(obj_, src_) ((TE_TYPEOF(obj_))(src_))
+#else
+#define TE_TYPEOF(obj_) void
+#define TE_CAST_TYPEOF(obj_) (obj_)
+#endif
+
+/**
+ * Cast @p src_ to the type of pointer to @p obj_.
+ *
+ * If @c typeof is not supported, it falls back to
+ * a generic `void *`.
+ *
+ * @param obj_   Object of target type.
+ * @param src_   Source pointer.
+ */
+#define TE_CAST_TYPEOF_PTR(obj_, src_) ((TE_TYPEOF(obj_) *)(src_))
+
+
 #endif /* __TE_COMPILER_H__ */

lib/rpcserver/rpc_server.h
@@ -720,14 +720,14 @@ extern void tarpc_generic_service(deferred_call_list *list,
         TE_FUNC_CAST(api_func_ret_int64, func);
 
 #define TARPC_FUNC_DECL_SAFE(_func)                                     \
-    __typeof(_func) * const func =                                      \
-                  TE_FUNC_CAST(__typeof(_func) *, _call->func);         \
-    __typeof(_func) * const func_ptr = func;                            \
-    __typeof(_func) * const func_void =  func;                          \
-    __typeof(_func) * const func_ret_ptr = func;                        \
-    __typeof(_func) * const func_ptr_ret_ptr = func;                    \
-    __typeof(_func) * const func_void_ret_ptr = func;                   \
-    __typeof(_func) * const func_ret_int64 = func;
+    TE_TYPEOF(_func) * const func =                                     \
+        TE_FUNC_CAST(TE_TYPEOF(_func) *, _call->func);                  \
+    TE_TYPEOF(_func) * const func_ptr = func;                           \
+    TE_TYPEOF(_func) * const func_void =  func;                         \
+    TE_TYPEOF(_func) * const func_ret_ptr = func;                       \
+    TE_TYPEOF(_func) * const func_ptr_ret_ptr = func;                   \
+    TE_TYPEOF(_func) * const func_void_ret_ptr = func;                  \
+    TE_TYPEOF(_func) * const func_ret_int64 = func;
 
 #define TARPC_FUNC_DECL_STANDALONE(_func)
 

lib/tools/te_vector.h
@@ -275,7 +275,7 @@ te_vec_size(const te_vec *vec)
 #else
 #define te_vec_get(vec_, index_) \
     (__builtin_choose_expr(                                             \
-        __builtin_types_compatible_p(__typeof__(vec_), const te_vec *), \
+        __builtin_types_compatible_p(TE_TYPEOF(vec_), const te_vec *), \
         te_vec_get_immutable,                                           \
         te_vec_get_mutable)(vec_, index_))
 #endif
@@ -315,7 +315,7 @@ te_vec_get_mutable(te_vec *vec, size_t index)
 #define te_vec_get_safe(vec_, index_, element_size_) \
     (__builtin_choose_expr(                                         \
         __builtin_types_compatible_p(                               \
-            __typeof__(vec_), const te_vec *),                      \
+            TE_TYPEOF(vec_), const te_vec *),                       \
         te_vec_get_safe_immutable,                                  \
         te_vec_get_safe_mutable)(vec_, index_, element_size_))
 #endif

commit 3f462973f49e4b8a94a878b9dc48762ffecdaed7

include: fixed bad macro reference
OL-Redmine-Id: 12761
Fixes: a6bb18873f9b ("include: add a typeof wrapper")
Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

include/te_defs.h
@@ -529,7 +529,7 @@ te_round_up_pow2(unsigned long long num)
 
 /** Align up a value to a power of two */
 #define TE_ALIGN(_v, _a) \
-    TE_ALIGN_MASK((_v), TE_TYPEOF_CAST(_v, _a) - 1)
+    TE_ALIGN_MASK((_v), TE_CAST_TYPEOF(_v, _a) - 1)
 
 
 /**