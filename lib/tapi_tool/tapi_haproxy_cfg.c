oktetlabs/test-environment

commit f21cdd00d9e3fd9ad8c0e1600bbcfa21fc353ca0

lib/tapi_tool: generate cfg file for HAProxy tool
Add TAPI to generate configuration file for HAProxy tool. It will be
used in HAProxy TAPI.

OL-Redmine-Id: 13274
Signed-off-by: Alexander Kuzminov <alexander.kuzminov@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>
Reviewed-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>
Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>

lib/tapi_tool/tapi_haproxy_cfg.c
@@ -1,0 +1,183 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (C) 2023 OKTET Labs Ltd. All rights reserved. */
+/** @file
+ * @brief HAProxy tool config file generation TAPI
+ *
+ * TAPI to handle HAProxy tool config file generation.
+ */
+
+#define TE_LGR_USER "TAPI HAPROXY CFG"
+
+#include "tapi_haproxy_cfg.h"
+#include "te_string.h"
+#include "conf_api.h"
+#include "tapi_file.h"
+
+#define TAPI_HAPROXY_CONF_FILENAME_SUFFIX "haproxy.cfg"
+#define INDENT "    "
+
+const tapi_haproxy_cfg_opt tapi_haproxy_cfg_default_opt = {
+    .nbthread = TAPI_JOB_OPT_UINT_UNDEF,
+    .tune_listener_multi_queue = FALSE,
+    .tune_idle_pool_shared = FALSE,
+    .timeout_client_ms =
+        TAPI_JOB_OPT_UINT_VAL(TAPI_HAPROXY_CONF_DEFAULT_TIMEOUT_MS),
+    .timeout_server_ms =
+        TAPI_JOB_OPT_UINT_VAL(TAPI_HAPROXY_CONF_DEFAULT_TIMEOUT_MS),
+    .timeout_connect_ms =
+        TAPI_JOB_OPT_UINT_VAL(TAPI_HAPROXY_CONF_DEFAULT_TIMEOUT_MS),
+    .frontend.frontend_addr.addr = NULL,
+    .frontend.frontend_addr.port = TAPI_JOB_OPT_UINT_UNDEF,
+    .frontend.shards = TAPI_HAPROXY_CFG_SHARDS_BY_THREAD,
+    .frontend.shards_n = TAPI_JOB_OPT_UINT_UNDEF,
+    .backend.n = 0,
+    .backend.backends = NULL,
+};
+
+static const te_enum_map tapi_haproxy_cfg_shards_mapping[] = {
+    {.name = "", .value = TAPI_HAPROXY_CFG_SHARDS_NUMBER},
+    {.name = " by-thread",  .value = TAPI_HAPROXY_CFG_SHARDS_BY_THREAD},
+    {.name = " by-group",  .value = TAPI_HAPROXY_CFG_SHARDS_BY_GROUP},
+    TE_ENUM_MAP_END
+};
+
+static const te_enum_map tapi_haproxy_cfg_bool_mapping[] = {
+    {.name = "on", .value = TRUE},
+    {.name = "off",  .value = FALSE},
+    TE_ENUM_MAP_END
+};
+
+static const tapi_job_opt_bind haproxy_cfg_global_binds[] = TAPI_JOB_OPT_SET(
+    TAPI_JOB_OPT_UINT_T("nbthread ", TRUE, NULL,
+                        tapi_haproxy_cfg_opt, nbthread),
+    TAPI_JOB_OPT_ENUM_BOOL("tune.listener.multi-queue ", TRUE,
+                            tapi_haproxy_cfg_opt, tune_listener_multi_queue,
+                            tapi_haproxy_cfg_bool_mapping),
+    TAPI_JOB_OPT_ENUM_BOOL("tune.idle-pool.shared ", TRUE,
+                            tapi_haproxy_cfg_opt, tune_idle_pool_shared,
+                            tapi_haproxy_cfg_bool_mapping)
+);
+
+static const tapi_job_opt_bind haproxy_cfg_defaults_binds[] = TAPI_JOB_OPT_SET(
+    TAPI_JOB_OPT_UINT_T("timeout client ", TRUE, NULL, tapi_haproxy_cfg_opt,
+                        timeout_client_ms),
+    TAPI_JOB_OPT_UINT_T("timeout server ", TRUE, NULL, tapi_haproxy_cfg_opt,
+                        timeout_server_ms),
+    TAPI_JOB_OPT_UINT_T("timeout connect ", TRUE, NULL, tapi_haproxy_cfg_opt,
+                        timeout_connect_ms)
+);
+
+static const tapi_job_opt_bind haproxy_cfg_frontend_binds[] = TAPI_JOB_OPT_SET(
+    TAPI_JOB_OPT_STRUCT("bind ", TRUE, " ", NULL,
+        TAPI_JOB_OPT_STRUCT(NULL, FALSE, ":", NULL,
+            TAPI_JOB_OPT_STRING(NULL, FALSE,
+                tapi_haproxy_cfg_opt, frontend.frontend_addr.addr),
+            TAPI_JOB_OPT_UINT_T(NULL, FALSE, NULL,
+                tapi_haproxy_cfg_opt, frontend.frontend_addr.port)),
+        TAPI_JOB_OPT_ENUM("shards", TRUE, tapi_haproxy_cfg_opt,
+                          frontend.shards, tapi_haproxy_cfg_shards_mapping),
+        TAPI_JOB_OPT_UINT_T(NULL, FALSE, NULL, tapi_haproxy_cfg_opt,
+                            frontend.shards_n)),
+    TAPI_JOB_OPT_STRING("default_backend ", TRUE, tapi_haproxy_cfg_opt,
+                        backend.name)
+);
+
+static const tapi_job_opt_bind haproxy_cfg_backends_binds[] = TAPI_JOB_OPT_SET(
+    TAPI_JOB_OPT_DUMMY("mode http"),
+    TAPI_JOB_OPT_DUMMY("balance static-rr"),
+    TAPI_JOB_OPT_ARRAY_PTR(tapi_haproxy_cfg_opt,
+        backend.n, backend.backends,
+        TAPI_JOB_OPT_STRUCT("backend ", TRUE, " ", NULL,
+            TAPI_JOB_OPT_STRING(NULL, FALSE,
+                tapi_haproxy_cfg_backend, name),
+            TAPI_JOB_OPT_STRUCT(NULL, FALSE, ":", NULL,
+                TAPI_JOB_OPT_STRING(NULL, FALSE,
+                    tapi_haproxy_cfg_backend, backend_addr.addr),
+                TAPI_JOB_OPT_UINT_T(NULL, FALSE, NULL,
+                    tapi_haproxy_cfg_backend, backend_addr.port))))
+);
+
+static char *
+generate_conf_path(const char *ta)
+{
+    char *ta_tmp_dir;
+    char *result;
+
+    ta_tmp_dir = tapi_cfg_base_get_ta_dir(ta, TAPI_CFG_BASE_TA_DIR_TMP);
+
+    result = tapi_file_make_custom_pathname(NULL, ta_tmp_dir,
+                "_" TAPI_HAPROXY_CONF_FILENAME_SUFFIX);
+    free(ta_tmp_dir);
+
+    return result;
+}
+
+static void
+build_cfg_group(const char *prefix_base,
+                const char *group_name,
+                const tapi_haproxy_cfg_opt *opt,
+                const tapi_job_opt_bind *binds,
+                const char *sep,
+                te_string *res,
+                te_bool last_group)
+{
+    te_string prefix = TE_STRING_INIT;
+    te_vec args = TE_VEC_INIT(char *);
+
+    te_string_append(&prefix, prefix_base);
+    if (group_name != NULL)
+       te_string_append(&prefix, " %s", group_name);
+
+    tapi_job_opt_build_args(prefix.ptr, binds, opt, &args);
+    te_string_join_vec(res, &args, sep);
+    te_string_append(res, last_group ? "\n" : "\n\n");
+
+    te_string_free(&prefix);
+    te_vec_deep_free(&args);
+}
+
+/* See description in tapi_haproxy_cfg.h */
+te_errno
+tapi_haproxy_cfg_create(const char *ta, const tapi_haproxy_cfg_opt *opt,
+                        char **result_pathname)
+{
+    te_string cfg_data = TE_STRING_INIT;
+    char *path = NULL;
+    te_errno rc;
+
+    assert(result_pathname != NULL);
+
+    if (opt == NULL)
+        opt = &tapi_haproxy_cfg_default_opt;
+
+    build_cfg_group("global", NULL, opt, haproxy_cfg_global_binds,
+                    "\n" INDENT, &cfg_data, FALSE);
+    build_cfg_group("defaults", NULL, opt, haproxy_cfg_defaults_binds,
+                    "\n" INDENT, &cfg_data, FALSE);
+    build_cfg_group("frontend", opt->frontend.name, opt,
+                    haproxy_cfg_frontend_binds, "\n" INDENT, &cfg_data, FALSE);
+    build_cfg_group("backend", opt->backend.name, opt,
+                    haproxy_cfg_backends_binds, "\n" INDENT, &cfg_data, TRUE);
+
+    path = generate_conf_path(ta);
+    rc = tapi_file_create_ta(ta, path, "%s", cfg_data.ptr);
+    te_string_free(&cfg_data);
+    if (rc != 0)
+    {
+        free(path);
+        ERROR("Failed to create HAProxy config file: %r", rc);
+        return rc;
+    }
+
+    *result_pathname = path;
+
+    return 0;
+}
+
+/* See description in tapi_haproxy_cfg.h */
+void
+tapi_haproxy_cfg_destroy(const char *ta, const char *cfg_file)
+{
+    if (cfg_file != NULL)
+        tapi_file_ta_unlink_fmt(ta, cfg_file);
+}

lib/tapi_tool/meson.build
@@ -5,6 +5,7 @@ headers += files(
     'tapi_bttrack.h',
     'tapi_ctorrent.h',
     'tapi_ethtool.h',
+    'tapi_haproxy_cfg.h',
     'tapi_memaslap.h',
     'tapi_memcached.h',
     'tapi_memtier.h',
@@ -24,6 +25,7 @@ sources += files(
     'tapi_bttrack.c',
     'tapi_ctorrent.c',
     'tapi_ethtool.c',
+    'tapi_haproxy_cfg.c',
     'tapi_memaslap.c',
     'tapi_memcached.c',
     'tapi_memtier.c',

lib/tapi_tool/tapi_haproxy_cfg.h
@@ -1,0 +1,143 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (C) 2023 OKTET Labs Ltd. All rights reserved. */
+/** @file
+ * @brief HAProxy tool config file generation TAPI
+ *
+ * @defgroup tapi_haproxy_cfg HAProxy tool config file generation TAPI
+ * @ingroup te_ts_tapi
+ * @{
+ *
+ * TAPI to handle HAProxy tool config file generation.
+ */
+
+#ifndef __TE_TAPI_HAPROXY_CFG_H__
+#define __TE_TAPI_HAPROXY_CFG_H__
+
+#include "tapi_job.h"
+#include "tapi_job_opt.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define TAPI_HAPROXY_CONF_DEFAULT_TIMEOUT_MS 30000
+
+/** Representation of pair of address and port. */
+typedef struct tapi_haproxy_cfg_address {
+    /** Address, interface or host name. */
+    const char *addr;
+    /** Port number. */
+    tapi_job_opt_uint_t port;
+} tapi_haproxy_cfg_address;
+
+/** Representation of possible frontend listener shards option value sources. */
+typedef enum tapi_haproxy_cfg_shards {
+    /** Set shards number manually. */
+    TAPI_HAPROXY_CFG_SHARDS_NUMBER,
+    /** Create as many shards as there are threads on the "bind" line. */
+    TAPI_HAPROXY_CFG_SHARDS_BY_THREAD,
+    /** Create one shard per thread group. */
+    TAPI_HAPROXY_CFG_SHARDS_BY_GROUP,
+} tapi_haproxy_cfg_shards;
+
+/** Backend server representation for HAProxy configuration. */
+typedef struct tapi_haproxy_cfg_backend {
+    /** Server name. */
+    char *name;
+    /** Server address representation. */
+    tapi_haproxy_cfg_address backend_addr;
+} tapi_haproxy_cfg_backend;
+
+/** HAProxy config file options. */
+typedef struct tapi_haproxy_cfg_opt {
+    /** Number of threads to run HAProxy on. */
+    tapi_job_opt_uint_t nbthread;
+    /**
+     * If @c TRUE, HAProxy listener spreads the incoming traffic to all
+     * threads a frontend "bind" line is allowed to run on instead of taking
+     * them for itself.
+     */
+    te_bool tune_listener_multi_queue;
+    /**
+     * If @c TRUE, idle connection pools are shared between threads for a same
+     * server.
+     */
+    te_bool tune_idle_pool_shared;
+
+    /** The maximum inactivity time on the client side (in milliseconds). */
+    tapi_job_opt_uint_t timeout_client_ms;
+    /**
+     * The maximum time for pending data staying into output buffer
+     * (in milliseconds).
+     */
+    tapi_job_opt_uint_t timeout_server_ms;
+    /**
+     * The maximum time to wait for a connection attempt to a server to succeed
+     * (in milliseconds).
+     */
+    tapi_job_opt_uint_t timeout_connect_ms;
+
+    /** Frontend listener bind configuration. */
+    struct {
+        /** Frontend group name in configuration file. */
+        const char *name;
+        /** Listener address representation. */
+        tapi_haproxy_cfg_address frontend_addr;
+        /**
+         * In multi-threaded mode, source of number of listeners
+         * on the same address.
+         * If the value is not #TAPI_HAPROXY_CFG_SHARDS_NUMBER, #shards_n field
+         * should be set to #TAPI_JOB_OPT_UINT_UNDEF.
+         */
+        tapi_haproxy_cfg_shards shards;
+        /**
+         * In multi-threaded mode, number of listeners on the same address.
+         * The field is ommited if it's value is #TAPI_JOB_OPT_UINT_UNDEF.
+        */
+        tapi_job_opt_uint_t shards_n;
+    } frontend;
+
+    /** Array of backend servers representations. */
+    struct {
+        /** Backend group name in configuration file. */
+        const char *name;
+        /** Size of backend servers representations array. */
+        size_t n;
+        /** Pointer to backend servers representations array. */
+        tapi_haproxy_cfg_backend *backends;
+    } backend;
+} tapi_haproxy_cfg_opt;
+
+/** Default options initializer. */
+extern const tapi_haproxy_cfg_opt tapi_haproxy_cfg_default_opt;
+
+/**
+ * Generate config file for HAProxy app. Save it in /tmp subdir
+ * of Test Agent working dir.
+ *
+ * @note @p result_pathname must be free'd.
+ *
+ * @param[in]  ta                 Test Agent name.
+ * @param[in]  opt                Configs for HAProxy tool.
+ * @param[out] result_pathname    Resulting path to the file (must not be @c NULL).
+ *
+ * @return    Status code.
+ */
+extern te_errno tapi_haproxy_cfg_create(const char *ta,
+                                        const tapi_haproxy_cfg_opt *opt,
+                                        char **result_pathname);
+
+/**
+ * Destroy generated config file for HAProxy app.
+ *
+ * @param ta            Test Agent name.
+ * @param cfg_file      Path to generated config file.
+ */
+extern void tapi_haproxy_cfg_destroy(const char *ta, const char *cfg_file);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* __TE_TAPI_HAPROXY_CFG_H__ */
+/** @} <!-- END tapi_haproxy_cfg --> */

commit 533e336ad31f3213310d1eab9f11227fdbd6ed19

lib/tapi_tool: fix incorrect HAProxy backend configuration
Fix incorrect lines adding backend servers in TAPI generating HAProxy
configuration file.

OL-Redmine-Id: 13434
Fixes: f21cdd00d9e3 ("lib/tapi_tool: generate cfg file for HAProxy tool")
Signed-off-by: Alexander Kuzminov <alexander.kuzminov@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>
Reviewed-by: Alexander Dubovoj <alexander.dubovoj@oktetlabs.ru>
Reviewed-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>
Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>

lib/tapi_tool/tapi_haproxy_cfg.c
@@ -87,7 +87,7 @@ static const tapi_job_opt_bind haproxy_cfg_backends_binds[] = TAPI_JOB_OPT_SET(
     TAPI_JOB_OPT_DUMMY("balance static-rr"),
     TAPI_JOB_OPT_ARRAY_PTR(tapi_haproxy_cfg_opt,
         backend.n, backend.backends,
-        TAPI_JOB_OPT_STRUCT("backend ", TRUE, " ", NULL,
+        TAPI_JOB_OPT_STRUCT("server ", TRUE, " ", NULL,
             TAPI_JOB_OPT_STRING(NULL, FALSE,
                 tapi_haproxy_cfg_backend, name),
             TAPI_JOB_OPT_STRUCT(NULL, FALSE, ":", NULL,