04655bd0d013db14efa63d7ad11915e4ec180861
Artem Andreev <artem.andreev@oktetlabs.ru>
2024-01-12 21:28:32 +0300

    lib/rpcs_sai: add dummy library
    This patch creates a dummy SAI RPC library so that
    SAI-using testsuites could be bootstrapped properly.
    The actual RPC code will be provided in future patches.
    
    OL-Redmine-Id: 13417
    Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
    Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

---
+++ lib/rpcs_sai/sai.c
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (C) 2024 OKTET Labs Ltd. All rights reserved. */
+/** @file
+ * @brief RPCs for SAI
+ *
+ * Implementation of RPCs for SAI
+ */
+
+#define TE_LGR_USER "RPC SAI"
+
+#include "te_config.h"
+
+#include <sai.h>
+
+/*
+ * TODO: the actual code
+ */
