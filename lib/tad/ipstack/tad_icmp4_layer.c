oktetlabs/test-environment

commit e421414e1a79f1c768bb270ea9c93bdb0952fcb2

lib/tad: add nexthop-mtu to ICMP4
The Next-Hop MTU field exists in the Destination
Unreachable message of ICMPv4, so it should also
be added to the ICMPv4 CSAP to enable setting it
in packets via ASN representation.

AMD-Jira-Id: ST-2738
Link: https://github.com/oktetlabs/test-environment/pull/34
Signed-off-by: Yurij Plotnikov <yurij.plotnikov@arknetworks.am>
Reviewed-by: Denis Pryazhennikov <denis.pryazhennikov@arknetworks.am>
Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>

lib/tad/ipstack/tad_icmp4_layer.c
@@ -57,6 +57,7 @@ typedef struct tad_icmp4_proto_data {
     tad_bps_pkt_frag_def    redirect;
     tad_bps_pkt_frag_def    echo_info;
     tad_bps_pkt_frag_def    ts;
+    tad_bps_pkt_frag_def    du;
 } tad_icmp4_proto_data;
 
 /**
@@ -70,6 +71,7 @@ typedef struct tad_icmp4_proto_pdu_data {
     tad_bps_pkt_frag_data   redirect;
     tad_bps_pkt_frag_data   echo_info;
     tad_bps_pkt_frag_data   ts;
+    tad_bps_pkt_frag_data   du;
 } tad_icmp4_proto_pdu_data;
 
 
@@ -144,6 +146,16 @@ static const tad_bps_pkt_frag tad_icmp4_ts_bps_hdr[] =
       TAD_DU_I32, FALSE },
 };
 
+/**
+ * Definition of ICMPv4 Destination Unreachable message subheader.
+ */
+static const tad_bps_pkt_frag tad_icmp4_du_bps_hdr[] =
+{
+    { "unused",   16, BPS_FLD_CONST_DEF(NDN_TAG_ICMP4_UNUSED, 0),
+      TAD_DU_I32, FALSE },
+    { "nexthop-mtu", 16, BPS_FLD_NO_DEF(NDN_TAG_ICMP4_NH_MTU),
+      TAD_DU_I32, FALSE },
+};
 
 /* See description tad_ipstack_impl.h */
 te_errno
@@ -195,6 +207,12 @@ tad_icmp4_init_cb(csap_p csap, unsigned int layer)
     if (rc != 0)
         return rc;
 
+    rc = tad_bps_pkt_frag_init(tad_icmp4_du_bps_hdr,
+                               TE_ARRAY_LEN(tad_icmp4_du_bps_hdr),
+                               NULL, &proto_data->du);
+    if (rc != 0)
+        return rc;
+
     return 0;
 }
 
@@ -213,6 +231,7 @@ tad_icmp4_destroy_cb(csap_p csap, unsigned int layer)
     tad_bps_pkt_frag_free(&proto_data->redirect);
     tad_bps_pkt_frag_free(&proto_data->echo_info);
     tad_bps_pkt_frag_free(&proto_data->ts);
+    tad_bps_pkt_frag_free(&proto_data->du);
 
     free(proto_data);
 
@@ -277,6 +296,11 @@ tad_icmp4_nds_to_pdu_data(csap_p csap, tad_icmp4_proto_data *proto_data,
     if (rc != 0)
         return rc;
 
+    rc = tad_bps_nds_to_data_units(&proto_data->du, layer_pdu,
+                                   &pdu_data->du);
+    if (rc != 0)
+        return rc;
+
     return 0;
 }
 
@@ -304,6 +328,8 @@ tad_icmp4_release_pdu_cb(csap_p csap, unsigned int layer, void *opaque)
                                    &pdu_data->echo_info);
         tad_bps_free_pkt_frag_data(&proto_data->ts,
                                    &pdu_data->ts);
+        tad_bps_free_pkt_frag_data(&proto_data->du,
+                                   &pdu_data->du);
         free(pdu_data);
     }
 }
@@ -328,6 +354,9 @@ tad_icmp4_frag_structs_by_type(const unsigned int         type,
 #else
 #error Dont know type for ICMP destination unreachable message
 #endif
+            *def = &proto_data->du;
+            *data = &tmpl_data->du;
+            break;
 #ifdef ICMP_TIME_EXCEEDED
         case ICMP_TIME_EXCEEDED:
 #elif defined(ICMP_TIMXCEED)
@@ -587,6 +616,10 @@ tad_icmp4_match_pre_cb(csap_p              csap,
 
     rc = tad_bps_pkt_frag_match_pre(&proto_data->redirect,
                                     &pkt_data->redirect);
+    if (rc != 0)
+        return rc;
+
+    rc = tad_bps_pkt_frag_match_pre(&proto_data->du, &pkt_data->du);
     if (rc != 0)
         return rc;
 

lib/ndn/ndn_ipstack.c
@@ -440,6 +440,8 @@ static asn_named_entry_t _ndn_icmp4_message_ne_array [] = {
       { PRIVATE, NDN_TAG_ICMP4_RX_TS } },
     { "tx-ts",          &ndn_data_unit_int32_s,
       { PRIVATE, NDN_TAG_ICMP4_TX_TS } },
+    { "nexthop-mtu",    &ndn_data_unit_int16_s,
+      { PRIVATE, NDN_TAG_ICMP4_NH_MTU } },
 };
 
 asn_type ndn_icmp4_message_s = {

lib/ndn/ndn_ipstack.h
@@ -115,6 +115,7 @@ typedef enum {
     NDN_TAG_ICMP4_ORIG_TS,
     NDN_TAG_ICMP4_RX_TS,
     NDN_TAG_ICMP4_TX_TS,
+    NDN_TAG_ICMP4_NH_MTU,
 } ndn_icmp4_tags_t;
 
 typedef enum {

commit 89c594f00c850908e9931c8dcc144224427d1ff2

lib/tad: fix initialization of nexthop-mtu field
Set nexthop-mtu to 0 by default, to prevent problems when this field
is not initialized in tests.

Fixes: e421414e1a79 ("lib/tad: add nexthop-mtu to ICMP4")
AMD-Jira-Id: ST-2738
Link: https://github.com/oktetlabs/test-environment/pull/35
Signed-off-by: Yurij Plotnikov <yurij.plotnikov@arknetworks.am>
Reviewed-by: Denis Pryazhennikov <denis.pryazhennikov@arknetworks.am>
Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>

lib/tad/ipstack/tad_icmp4_layer.c
@@ -153,7 +153,7 @@ static const tad_bps_pkt_frag tad_icmp4_du_bps_hdr[] =
 {
     { "unused",   16, BPS_FLD_CONST_DEF(NDN_TAG_ICMP4_UNUSED, 0),
       TAD_DU_I32, FALSE },
-    { "nexthop-mtu", 16, BPS_FLD_NO_DEF(NDN_TAG_ICMP4_NH_MTU),
+    { "nexthop-mtu", 16, BPS_FLD_CONST_DEF(NDN_TAG_ICMP4_NH_MTU, 0),
       TAD_DU_I32, FALSE },
 };
 