oktetlabs/test-environment

commit a96abf8434c0155200d84a70d4f5bd97d09a4d9c

tools: add examples of logs storage Web server CGI scripts
These scripts help to setup disk space efficient logs storage
since compressed raw logs only may be stored and everything
else generated upon request.

Signed-off-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
Acked-by: Artem Andreev <artem.andreev@oktetlabs.ru>

tools/log_server/te-logs-error404.sh
@@ -1,0 +1,84 @@
+#! /bin/bash
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2007-2023 OKTET Labs Ltd. All rights reserved.
+#
+# CGI script to handle error 404 in log storage directories and
+# generate requested file on fly.
+#
+# Uses the following CGI environment varialbes:
+#   - REQUEST_URI
+#
+
+# FIXME set path to built and installed Test Environment
+#TE_PATH="${TE_INSTALL}/default/bin/"
+
+# Multi-page HTML logs have static part which is the same for instances.
+# It could be stored on logs server in one place and referenced from
+# all generated logs. It allows to use browser cache efficiently.
+# FIXME set URL to shared files
+#SHARED_URL="https://..."
+
+#############################
+# Exit with "Not Found" message.
+# Arguments:
+#   None
+# Outputs:
+#   HTTP response
+#############################
+function not_found_exit() {
+    printf "Content-Type: text/plain\r\n"
+    printf "\r\n"
+    printf "Not Found\r\n"
+    printf "$*\r\n"
+    exit 0
+}
+
+[[ -n "${TE_PATH}" ]] || not_found_exit "TE_PATH is not set"
+
+case "${REQUEST_URI}" in
+    # FIXME Put your logs URL to location mapping here
+    /logs/* )
+        root_dir_uri="/logs"
+        root_dir="/srv/logs"
+        # FIXME Set docs_url to test suite documentation html root to
+        # have links to the test documentation in HTML and JSON logs.
+        ;;
+    * )
+        not_found_exit
+        ;;
+esac
+
+# Requested file
+request_file="${REQUEST_URI/$root_dir_uri/$root_dir}"
+# Substitute %20->'space'
+request_file="${request_file//%20/ }"
+
+get_item_cmd=()
+# Consider to set custom TMPDIR which is big enough for temporary
+# unpacked log files
+#get_item_cmd+=(TMPDIR=/srv/tmp)
+# It is recommended to run it under nice since really many requests
+# could be generated via Web server
+get_item_cmd+=(nice)
+get_item_cmd+=("${TE_PATH}"/rgt-log-get-item)
+[[ -n "${SHARED_URL}" ]] && get_item_cmd+=(--shared-url="${SHARED_URL}")
+[[ -n "${docs_url}" ]] && get_item_cmd+=(--docs-url="${docs_url}")
+# Try to fix generated files permissions
+get_item_cmd+=(--fix-permissions)
+# Try to generate requested log file
+status=$("${get_item_cmd[@]}" --req-path="${request_file}" 2>&1)
+result=$?
+
+if [[ -r "${request_file}" ]] ; then
+    # Now requested file exists, redirect
+    printf "Status: 302 Moved\r\n"
+    printf "Location: %s\r\n" "${REQUEST_URI}"
+    printf "\r\n"
+elif [[ x"$result" = x"2" ]] ; then
+    # Internal error
+    printf "Content-Type: text/plain\r\n"
+    printf "\r\n"
+    printf "ERROR: $status\r\n"
+else
+    not_found_exit
+fi

tools/log_server/te-logs-index.sh
@@ -1,0 +1,106 @@
+#! /bin/bash
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2007-2023 OKTET Labs Ltd. All rights reserved.
+#
+# CGI script to index log storage directories and provide fake entries
+# for log files which may be generated from available raw logs.
+#
+# Uses the following CGI environment varialbes:
+#   - REQUEST_URI
+#   - CONTEXT_PREFIX
+#   - CONTEXT_DOCUMENT_ROOT
+#
+
+#############################
+# Print fake (to be generated) or real directory entry.
+# Arguments:
+#   Directory name
+# Outputs:
+#   HTML fragment to stdout.
+#############################
+function print_dir_entry()
+{
+    if test ! -e "$1" ; then
+        icon="/icons/link.gif"
+    else
+        icon="/icons/folder.gif"
+    fi
+    printf "<img src=\"%s\" alt=\"[DIR]\"> " "$icon"
+    printf "<a href=\"%s/\">%-32s %10s\n" "$1" "$1/</a>" "&lt;DIR&gt;"
+}
+
+#############################
+# Print fake (to be generated) or real file entry.
+# Arguments:
+#   File name
+# Outputs:
+#   HTML fragment to stdout.
+#############################
+function print_file_entry()
+{
+    if test ! -e "$1" ; then
+        icon="/icons/link.gif"
+        size=""
+    else
+        size="$(ls -1hs "$1" | awk '{print $1}')"
+        if file "$1" | grep -qw compressed ; then
+            icon="/icons/compressed.gif"
+        elif file "$1" | grep -qw text ; then
+            icon="/icons/text.gif"
+        else
+            icon="/icons/unknown.gif"
+        fi
+    fi
+
+    printf "<img src=\"%s\" alt=\"[   ]\"> " "$icon"
+    printf "<a href=\"%s\">%-32s %10s\n" "$1" "$1</a>" "$size"
+}
+
+printf "Content-Type: text/html\r\n\r\n"
+
+# Substitute spaces in URI
+REQUEST_URI="${REQUEST_URI//%20/ }"
+
+directory="${REQUEST_URI/#${CONTEXT_PREFIX}/${CONTEXT_DOCUMENT_ROOT}}"
+cd "$directory"
+
+echo "<html><head><title>Index of ${REQUEST_URI}</title></head><body>"
+
+if test -r "HEADER.html" ; then
+    cat "HEADER.html"
+else
+    echo "<h2>Index of ${REQUEST_URI}</h2>"
+fi
+
+echo "<pre><hr />"
+
+print_dir_entry ".."
+if [[ -r log.raw.bz2 || -r log.raw.xz || -r raw_log_bundle.tpxz ]] ; then
+    if test ! -d html ; then
+        print_dir_entry "html"
+    fi
+    if test ! -e log.txt ; then
+        print_file_entry "log.txt"
+    fi
+    if [[ -r raw_log_bundle.tpxz ]] ; then
+        if test ! -e log_gist.raw ; then
+            print_file_entry "log_gist.raw"
+        fi
+        if test ! -e bublik.xml ; then
+            print_file_entry "bublik.xml"
+        fi
+    fi
+fi
+for i in * ; do
+    test "$i" = "." -o "$i" = ".." -o "$i" = "HEADER.html" && continue
+    i="${i/#.\//}"
+    if test -d "$i" ; then
+        print_dir_entry "$i"
+    else
+        print_file_entry "$i"
+    fi
+done
+
+echo "<hr /></pre></body></html>"
+
+exit 0

commit d2e4d1ddf194e136ac8915497c060f9ba0096a11

tools/log_server: add missing slash to shared URL
RGT just appends file names to shared URL. Since we copy these files
as is, shared URL should have slash at the end to produce right path.

Fixes: a96abf8434c0 ("tools: add examples of logs storage Web server CGI scripts")
Signed-off-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>

tools/log_server/te-logs-error404.sh
@@ -64,7 +64,7 @@ get_item_cmd=()
 # could be generated via Web server
 get_item_cmd+=(nice)
 get_item_cmd+=("${TE_PATH}"/rgt-log-get-item)
-[[ -n "${SHARED_URL}" ]] && get_item_cmd+=(--shared-url="${SHARED_URL}")
+[[ -n "${SHARED_URL}" ]] && get_item_cmd+=(--shared-url="${SHARED_URL}/")
 [[ -n "${docs_url}" ]] && get_item_cmd+=(--docs-url="${docs_url}")
 # Try to fix generated files permissions
 get_item_cmd+=(--fix-permissions)