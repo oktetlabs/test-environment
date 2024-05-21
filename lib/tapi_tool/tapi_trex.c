oktetlabs/test-environment

commit a2892e1d0629ece9ddfc0b650ace173f757cfab6

lib/tapi_tool: get the median value from TRex statistics
Sometimes it is necessary (in addition to the average) to obtain
the median value of one or more parameters for a selected period
of time.

OL-Redmine-Id: 13645
Signed-off-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>
Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>

lib/tapi_tool/tapi_trex.c
@@ -1849,3 +1849,83 @@ out:
     *vals = res;
     *n_vals = count;
 }
+
+/* Callback function for qsort. */
+static int
+tapi_trex_compare_doubles_max(const void *a, const void *b)
+{
+    double *val1 = (double *)a;
+    double *val2 = (double *)b;
+
+    if (*val1 < *val2)
+        return 1;
+    else if (*val1 > *val2)
+        return -1;
+    else
+        return 0;
+}
+
+/* See description in tapi_trex.h */
+te_errno
+tapi_trex_port_stat_median_get(tapi_trex_report *report,
+                            tapi_trex_port_stat_enum param, unsigned int index,
+                            double time_start, double time_end, double *median)
+{
+    te_errno rc = 0;
+    double *vals;
+    double *meds;
+    unsigned int i;
+    unsigned int n_vals;
+    unsigned int n_meds;
+    unsigned int i_start = UINT_MAX;
+    unsigned int i_end = UINT_MAX;
+
+    tapi_trex_port_stat_time_series_get(report, &vals, &n_vals);
+    if (vals == NULL)
+    {
+        ERROR("%s() interface statistics do not contain data (is the --iom 1 "
+              "option used?)", __func__);
+        return TE_RC(TE_TAPI, TE_ENODATA);
+    }
+    assert(time_start < time_end);
+
+    for (i = 0; i < n_vals - 1; i++)
+    {
+        if (vals[i] < time_start && time_start < vals[i + 1])
+            i_start = i;
+
+        if (vals[i] < time_end && time_end < vals[i + 1])
+            i_end = i;
+    }
+
+    if (i_start == UINT_MAX)
+    {
+        rc = TE_RC(TE_TAPI, TE_ERANGE);
+        WARN("%s() cannot find start time -gt %f, using first element",
+             __func__, time_start);
+        i_start = 0;
+    }
+    if (i_end == UINT_MAX)
+    {
+        rc = TE_RC(TE_TAPI, TE_ERANGE);
+        WARN("%s() cannot find end time -gt %f, using last element",
+             __func__, time_end);
+        i_end = n_vals - 1;
+    }
+    free(vals);
+
+    n_meds = i_end - i_start + 1;
+    tapi_trex_port_stat_param_series_by_time_get(report, param, index,
+                                                 &vals, &n_vals);
+    assert(n_meds < n_vals);
+
+    meds = TE_ALLOC(sizeof(*meds) * n_meds);
+    memcpy(meds, &vals[i_start], sizeof(*meds) * n_meds);
+    free(vals);
+
+    qsort(meds, n_meds, sizeof(*meds), &tapi_trex_compare_doubles_max);
+    *median = meds[n_meds / 2];
+    free(meds);
+
+    return rc;
+}

lib/tapi_tool/tapi_trex.h
@@ -992,6 +992,27 @@ tapi_trex_port_stat_param_series_by_time_get(tapi_trex_report *report,
     tapi_trex_port_stat_param_series_get(report,
                             param, index, FALSE, TRUE, vals, n_vals);
 }
+
+/**
+ * Get the median for a given parameter from the per-port statistics.
+ *
+ * @param[in]  report       TRex report.
+ * @param[in]  param        Type of param.
+ * @param[in]  index        Port index.
+ * @param[in]  time_start   Use values starting from the specified time
+ *                          (in seconds).
+ * @param[in]  time_end     Use values up to the specified time (in seconds).
+ * @param[out] median       Result.
+ *
+ * @return Status code.
+ * @retval TE_ENODATA       Per-port statistics do not contain data.
+ * @retval TE_ERANGE        The @p time_start is too long, or the @p time_end
+ *                          is too short in the interface statistics.
+ * @retval @c 0             The result is successful.
+ */
+extern te_errno tapi_trex_port_stat_median_get(tapi_trex_report *report,
+                            tapi_trex_port_stat_enum param, unsigned int index,
+                            double time_start, double time_end, double *median);
 /**@}*/
 
 #ifdef __cplusplus

commit d528397c5af67bdeae5308f9d5c4dad9fbab18e2

lib/tapi_tool: fix checking the comparison of values in TRex
To make sure that the desired value belongs to the specified range
of values, it is necessary to check not only that it is located
between the lower and upper points of the range, but also that it
coincides with these points.

Fixes: a2892e1d0629 ("lib/tapi_tool: get the median value from TRex statistics")
Signed-off-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>
Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>

lib/tapi_tool/tapi_trex.c
@@ -8,6 +8,7 @@
 
 #define TE_LGR_USER "TAPI CISCO TREX"
 
+#include <float.h>
 #include <signal.h>
 #include <regex.h>
 #include <netinet/in.h>
@@ -1896,11 +1897,17 @@ tapi_trex_port_stat_median_get(tapi_trex_report *report,
 
     for (i = 0; i < n_vals - 1; i++)
     {
-        if (vals[i] < time_start && time_start < vals[i + 1])
+        if (time_start - vals[i] > -DBL_EPSILON &&
+            vals[i + 1] - time_start > DBL_EPSILON)
+        {
             i_start = i;
+        }
 
-        if (vals[i] < time_end && time_end < vals[i + 1])
+        if (time_end - vals[i] > -DBL_EPSILON &&
+            vals[i + 1] - time_end > DBL_EPSILON)
+        {
             i_end = i;
+        }
     }
 
     if (i_start == UINT_MAX)