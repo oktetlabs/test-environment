9ae216197a41c5f3e4742fd10f00acb3f8c19d46
Oleg Sadakov <Oleg.Sadakov@arknetworks.am>
2024-10-10 16:12:34 +0400

    engine/tester: fix handling of command node
    Handle correctly command node at the end of command_monitor node.
    Before this change we will have:
    > monitor_process(): unexpected node name 'command' encountered
    
    with the following command_monitor node in package.xml:
    > <command_monitor>
    >     <command>.../monitor</command>
    > </command_monitor>
    
    Signed-off-by: Oleg Sadakov <Oleg.Sadakov@arknetworks.am>

--- engine/tester/config_parse.c
+++ engine/tester/config_parse.c
@@ -2162,8 +2162,8 @@ monitors_process(xmlNodePtr *node, run_item *ritem)
                     return rc;
                 }
             }
-            if (xmlStrcmp(p->name,
-                          CONST_CHAR2XML("ta")) == 0)
+            else if (xmlStrcmp(p->name,
+                               CONST_CHAR2XML("ta")) == 0)
             {
                 if ((rc = cmd_monitor_get_prop(&p, &monitor->ta,
                                                "ta")) != 0)
