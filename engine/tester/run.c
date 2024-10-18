oktetlabs/test-environment

commit 139dde992bd993ccb02234968acf8ca03f1ca18e

engine/tester: save requirements in raw log
Save information about test requirements in raw log so that RGT tools
can extract it and add to their log representations.

GitHub-TE-Id: 49
Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

engine/tester/run.c
@@ -1331,6 +1331,106 @@ test_params_to_json(const unsigned int n_args, const test_iter_arg *args)
 }
 
 /**
+ * Add requirements to JSON array.
+ *
+ * @param array   JSON array.
+ * @param reqs    Requirements.
+ * @param n_args  Number of test arguments.
+ * @param args    Test arguments (used when requirement has a
+ *                "ref" attribute).
+ *
+ * @return 0 on success or -1 on failure.
+ */
+static int
+add_test_reqs_to_array(json_t *array, const test_requirements *reqs,
+                       unsigned int n_args, const test_iter_arg *args)
+{
+    json_t *item;
+    test_requirement *p;
+
+    TAILQ_FOREACH(p, reqs, links)
+    {
+        item = json_pack("s", test_req_id(p, n_args, args));
+        if (item == NULL)
+        {
+            ERROR("%s(): json_pack() failed", __FUNCTION__);
+            return -1;
+        }
+
+        if (json_array_append_new(array, item) != 0)
+        {
+            ERROR("%s(): failed to add item to the array", __FUNCTION__);
+            return -1;
+        }
+    }
+
+    return 0;
+}
+
+/**
+ * Convert test requirements to JSON representation.
+ *
+ * @param sticky_reqs     Sticky requirements.
+ * @param ri              Run item.
+ * @param args            Iteration arguments.
+ *
+ * @return JSON array or NULL.
+ */
+static json_t *
+test_reqs_to_json(const test_requirements *sticky_reqs,
+                  const run_item *ri, const test_iter_arg *args)
+{
+    json_t *result;
+    const test_requirements *reqs;
+    int rc;
+    unsigned int i;
+
+    result = json_array();
+    if (result == NULL)
+    {
+        ERROR("%s(): failed to allocate memory for array", __FUNCTION__);
+        return NULL;
+    }
+
+    rc = add_test_reqs_to_array(result, sticky_reqs, ri->n_args, args);
+    if (rc < 0)
+    {
+        json_decref(result);
+        return NULL;
+    }
+
+    reqs = tester_get_ri_reqs(ri);
+    if (reqs != NULL)
+    {
+        rc = add_test_reqs_to_array(result, reqs, ri->n_args, args);
+        if (rc < 0)
+        {
+            json_decref(result);
+            return NULL;
+        }
+    }
+
+    for (i = 0; i < ri->n_args; i++)
+    {
+        rc = add_test_reqs_to_array(result, &args[i].reqs,
+                                    ri->n_args, args);
+        if (rc < 0)
+        {
+            json_decref(result);
+            return NULL;
+        }
+    }
+
+    if (json_array_size(result) == 0)
+    {
+        json_decref(result);
+        return NULL;
+    }
+
+    return result;
+}
+
+/**
  * Normalize parameter value. Remove trailing spaces and newlines.
  *
  * @param param   Parameter value to normalize
@@ -1552,11 +1652,12 @@ log_test_start(unsigned int flags,
      * jansson-2.10, which is currently the newest version available on
      * CentOS/RHEL-7.x
      */
-    result = json_pack("{s:i, s:i, s:i, s:o?}",
+    result = json_pack("{s:i, s:i, s:i, s:o?, s:o?}",
                        "id", test,
                        "parent", parent,
                        "plan_id", ri->plan_id,
-                       "params", test_params_to_json(ri->n_args, ctx->args));
+                       "params", test_params_to_json(ri->n_args, ctx->args),
+                       "reqs", test_reqs_to_json(&ctx->reqs, ri, ctx->args));
     if (result == NULL)
     {
         ERROR("JSON object creation failed in %s", __FUNCTION__);

engine/tester/reqs.c
@@ -158,19 +158,10 @@ test_requirements_free(test_requirements *reqs)
     }
 }
 
-
-/**
- * Get requirement identifier in specified context of parameters.
- *
- * @param req           A requirement
- * @param n_args        Number of arguments
- * @param args          Test iteration arguments
- *
- * @return Requirement ID.
- */
-static const char *
-req_get(const test_requirement *req,
-        unsigned int n_args, const test_iter_arg *args)
+/* See the description in tester_reqs.h */
+const char *
+test_req_id(const test_requirement *req,
+            unsigned int n_args, const test_iter_arg *args)
 {
     assert(req != NULL);
     assert((args == NULL) == (n_args == 0));
@@ -209,7 +200,7 @@ is_req_in_set(const char *req, const test_requirements *set,
          s != NULL;
          s = TAILQ_NEXT(s, links))
     {
-        if (strcmp(req, req_get(s, n_args, args)) == 0)
+        if (strcmp(req, test_req_id(s, n_args, args)) == 0)
         {
             return true;
         }
@@ -450,7 +441,7 @@ reqs_list_to_string_buf(const test_requirements  *reqs,
     {
         out = snprintf(s, *left, "%s%s",
                        (p == TAILQ_FIRST(reqs)) ? "" : ", ",
-                       req_get(p, n_args, args));
+                       test_req_id(p, n_args, args));
     }
     *buf = s;
 }
@@ -524,6 +515,33 @@ params_reqs_list_to_string(const unsigned int   n_args,
     return out_buf;
 }
 
+/* See description in tester_reqs.h */
+const test_requirements *
+tester_get_ri_reqs(const run_item *ri)
+{
+    const test_requirements *reqs;
+
+    switch (ri->type)
+    {
+        case RUN_ITEM_SCRIPT:
+            reqs = &ri->u.script.reqs;
+            break;
+
+        case RUN_ITEM_SESSION:
+            reqs = &ri->u.session.reqs;
+            break;
+
+        case RUN_ITEM_PACKAGE:
+            reqs = &ri->u.package->reqs;
+            break;
+
+        default:
+            assert(false);
+            return NULL;
+    }
+
+    return reqs;
+}
 
 /* See description in tester_reqs.h */
 bool
@@ -538,24 +556,9 @@ tester_is_run_required(const logic_expr        *targets,
     bool force = false;
     const test_requirements    *reqs;
 
-    switch (test->type)
-    {
-        case RUN_ITEM_SCRIPT:
-            reqs = &test->u.script.reqs;
-            break;
-
-        case RUN_ITEM_SESSION:
-            reqs = &test->u.session.reqs;
-            break;
-
-        case RUN_ITEM_PACKAGE:
-            reqs = &test->u.package->reqs;
-            break;
-
-        default:
-            assert(false);
-            return false;
-    }
+    reqs = tester_get_ri_reqs(test);
+    if (reqs == NULL)
+        return false;
 
     if (targets != NULL)
     {

engine/tester/tester_reqs.h
@@ -107,6 +107,28 @@ extern te_errno tester_get_sticky_reqs(test_requirements *sticky_reqs,
                                        const test_requirements *reqs);
 
 /**
+ * Get pointer to requirements for a run item.
+ *
+ * @param ri      Run item.
+ *
+ * @return Pointer to associated requirements.
+ */
+const test_requirements *tester_get_ri_reqs(const struct run_item *ri);
+
+/**
+ * Get requirement identifier in specified context of parameters.
+ *
+ * @param req           A requirement.
+ * @param n_args        Number of arguments.
+ * @param args          Test iteration arguments.
+ *
+ * @return Requirement Id.
+ */
+extern const char *test_req_id(const test_requirement *req,
+                               unsigned int n_args,
+                               const struct test_iter_arg *args);
+
+/**
  * Print requirements expression to static buffer.
  *
  * @param expr      Requirements expression

tools/rgt/rgt-core/log_msg.c
@@ -836,6 +836,7 @@ create_node_by_msg_json(json_t *msg, uint32_t *ts)
 
     json_t     *authors = NULL;
     json_t     *params = NULL;
+    json_t     *reqs = NULL;
     json_t     *name_opt = NULL;
     json_t     *objective_opt = NULL;
     json_t     *page_opt = NULL;
@@ -867,7 +868,7 @@ create_node_by_msg_json(json_t *msg, uint32_t *ts)
      * values and type-checked manually.
      */
     ret = json_unpack_ex(msg, &err, JSON_STRICT,
-                         "{s:i, s:i, s:s, s?o, s?o, s?o, s?o, s?o, s?o, s?o, s?i}",
+                         "{s:i, s:i, s:s, s?o, s?o, s?o, s?o, s?o, s?o, s?o, s?o, s?i}",
                          "id", &node->node_id,
                          "parent", &node->parent_id,
                          "node_type", &type,
@@ -878,6 +879,7 @@ create_node_by_msg_json(json_t *msg, uint32_t *ts)
                          "tin", &tin_opt,
                          "authors", &authors,
                          "params", &params,
+                         "reqs", &reqs,
                          "plan_id", &node->plan_id);
     if (ret != 0)
     {

tools/rgt/rgt-format/mi_msg.c
@@ -781,6 +781,7 @@ te_rgt_parse_mi_test_start_message(te_rgt_mi *mi)
     json_t *root;
     json_t *name = NULL;
     json_t *params = NULL;
+    json_t *reqs = NULL;
     json_t *authors = NULL;
     json_t *objective = NULL;
     json_t *page = NULL;
@@ -807,13 +808,15 @@ te_rgt_parse_mi_test_start_message(te_rgt_mi *mi)
 
     data->plan_id = -1;
     ret = json_unpack_ex(root, &err, JSON_STRICT,
-                         "{s:i, s:i, s?i, s:s, s?o, s?o, s?o, s?o, s?o, s?o, s?o}",
+                         "{s:i, s:i, s?i, s:s, s?o, s?o, s?o, s?o, s?o, "
+                         "s?o, s?o, s?o}",
                          "id", &data->node_id,
                          "parent", &data->parent_id,
                          "plan_id", &data->plan_id,
                          "node_type", &data->node_type,
                          "name", &name,
                          "params", &params,
+                         "reqs", &reqs,
                          "authors", &authors,
                          "objective", &objective,
                          "page", &page,

commit dd36e69247a2e067139402d6e903737531b430be

engine/tester: fix listing argument requirements
Do not include requirements from variables.

Fixes: 139dde992bd9 ("engine/tester: save requirements in raw log")
GitHub-TE-Id: 49
Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

engine/tester/run.c
@@ -1416,6 +1416,9 @@ test_reqs_to_json(const test_requirements *sticky_reqs,
 
     for (i = 0; i < ri->n_args; i++)
     {
+        if (args[i].variable)
+            continue;
+
         add_test_reqs_to_strings(&reqs_unique, &args[i].reqs,
                                  ri->n_args, args);
     }