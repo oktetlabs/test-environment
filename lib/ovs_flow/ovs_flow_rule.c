oktetlabs/test-environment

commit 6c25f113903223636681dc130d7dcfbc6bcc6573

agents/unix: implement OvS flow handling
Tests need a way to set up and modify OvS flow rules, to redirect
or otherwise process traffic.

Processing OvS flow rules isn't trivial: different OvS tools use
slightly differing formats. In order to deal with these discrepancies
and to simplify the implementation in the test agent, most of the logic
was moved to a new library called ovs_flow. In the future, this library
can be extended to allow more complex processing of flow rules.

Link: https://github.com/oktetlabs/test-environment/pull/23
Signed-off-by: Viacheslav Galaktionov <viacheslav.galaktionov@arknetworks.am>
Reviewed-by: Ivan Malov <ivan.malov@arknetworks.am>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
Acked-by: Artem Andreev <artem.andreev@oktetlabs.ru>

lib/ovs_flow/ovs_flow_rule.c
@@ -1,0 +1,287 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (C) 2023 Advanced Micro Devices, Inc. All rights reserved. */
+/** @file
+ * @brief OVS Flow Rule Processing Library
+ *
+ * Definition of the flow rule descriptor structure and the associated
+ * operations.
+ */
+
+#define TE_LGR_USER      "OVS Flow Lib"
+
+#include "te_config.h"
+
+#include <ctype.h>
+#include <assert.h>
+#include <stddef.h>
+
+#include "te_defs.h"
+#include "te_errno.h"
+#include "te_str.h"
+#include "logger_api.h"
+#include "ovs_flow_rule.h"
+
+
+typedef struct ovs_flow_field_desc ovs_flow_field_desc;
+
+/** Flow field parsing function */
+typedef te_errno (*ovs_flow_field_parse)(const char *value_start,
+                                         size_t value_len,
+                                         const ovs_flow_field_desc *field,
+                                         ovs_flow_rule* rule);
+
+/** Flow field formatting function */
+typedef void (*ovs_flow_field_format)(const ovs_flow_field_desc *field,
+                                      te_string *str,
+                                      ovs_flow_rule* rule);
+
+
+typedef enum ovs_flow_field_props {
+    OVS_FIELD_HIDE_ZERO    = 1 << 0,
+    OVS_FIELD_NOT_OPENFLOW = 1 << 1,
+    OVS_FIELD_FORMAT_HEX   = 1 << 2,
+} ovs_flow_field_props;
+
+/** Flow field descriptor structure used by the main flow parsing function */
+typedef struct ovs_flow_field_desc {
+    const char            *name;
+    size_t                 name_len;
+    size_t                 offset;
+    ovs_flow_field_parse   parser;
+    ovs_flow_field_format  formatter;
+    ovs_flow_field_props   props;
+} ovs_flow_field_desc;
+
+static te_errno
+parse_ovs_u64(const char *value_start, size_t value_len,
+              const ovs_flow_field_desc *field, ovs_flow_rule *rule)
+{
+    uint64_t *res;
+    char      buf[72]; /* Enough to fit any 64bit number in base >= 2 */
+
+    if (value_len + 1 > sizeof(buf))
+        return TE_EINVAL;
+
+    memcpy(buf, value_start, value_len);
+    buf[value_len] = '\0';
+
+    res = (uint64_t *)((char *)rule + field->offset);
+
+    return te_str_to_uint64(buf, 0, res);
+}
+
+static void
+format_ovs_u64(const ovs_flow_field_desc *field, te_string *str,
+               ovs_flow_rule *rule)
+{
+    uint64_t *value = (uint64_t *)((char *)rule + field->offset);
+
+    if (((field->props & OVS_FIELD_HIDE_ZERO) != 0) && *value == 0)
+        return;
+
+    if ((field->props & OVS_FIELD_FORMAT_HEX) != 0)
+        te_string_append(str, "%s=0x%"PRIx64, field->name, *value);
+    else
+        te_string_append(str, "%s=%"PRIu64, field->name, *value);
+}
+
+#define OVS_FIELD_DESC(_name, _type, _props) \
+    { #_name, sizeof (#_name) - 1, offsetof(ovs_flow_rule, _name), \
+      parse_ovs_##_type, format_ovs_##_type, _props }
+
+/* All flow rule fields that need to be parsed and formatted */
+static ovs_flow_field_desc field_desc[] = {
+    OVS_FIELD_DESC(cookie, u64, OVS_FIELD_HIDE_ZERO | OVS_FIELD_FORMAT_HEX),
+    OVS_FIELD_DESC(table, u64, OVS_FIELD_HIDE_ZERO),
+    OVS_FIELD_DESC(n_packets, u64, OVS_FIELD_NOT_OPENFLOW),
+    OVS_FIELD_DESC(n_bytes, u64, OVS_FIELD_NOT_OPENFLOW),
+    OVS_FIELD_DESC(n_offload_packets, u64, OVS_FIELD_NOT_OPENFLOW),
+    OVS_FIELD_DESC(n_offload_bytes, u64, OVS_FIELD_NOT_OPENFLOW),
+};
+static const unsigned int field_desc_num = sizeof(field_desc) / sizeof(field_desc[0]);
+#undef OVS_FIELD_DESC
+
+static void
+trim_spaces(const char **start, const char **end)
+{
+    while (*start < *end && isspace(**start))
+        *start += 1;
+
+    while (*start < *end && isspace(*(*end - 1)))
+        *end -= 1;
+}
+
+
+static void
+ovs_flow_rule_str_comma_append(te_string *rule, const char *field)
+{
+    if (field == NULL || *field == '\0')
+        return;
+
+    if (rule->len > 0)
+        te_string_append(rule, ",%s", field);
+    else
+        te_string_append(rule, "%s", field);
+}
+
+te_errno
+ovs_flow_rule_parse(const char *rule_str, ovs_flow_rule *rule)
+{
+    te_errno      rc;
+    te_string     body = TE_STRING_INIT;
+    const char   *next_name_start;
+    const char   *name_start;
+    const char   *name_end;
+    const char   *value_start;
+    const char   *value_end;
+    unsigned int  i;
+
+    memset(rule, 0, sizeof(*rule));
+    name_start = rule_str;
+
+    do {
+        name_end = strpbrk(name_start, ",=");
+        if (name_end == NULL)
+        {
+            /* Field name is the end of the string */
+            ERROR("Name: %s (%p)", name_start, name_start);
+            ERROR("Can't parse OvS flow rule without actions");
+            te_string_free(&body);
+            return TE_RC(TE_TAPI, TE_EINVAL);
+        }
+
+        if (*name_end == ',')
+        {
+            value_start = NULL;
+            value_end = value_start;
+            next_name_start = name_end + 1;
+        }
+        else /* *name_end == '=' */
+        {
+            value_start = name_end + 1;
+        }
+
+        trim_spaces(&name_start, &name_end);
+
+        if (name_end - name_start == sizeof("actions") - 1 &&
+            strncmp(name_start, "actions", sizeof("actions") - 1) == 0)
+        {
+            /* Actions started, dump the rest of the body string and return */
+            ovs_flow_rule_str_comma_append(&body, name_start);
+            break;
+        }
+
+        if (value_start != NULL)
+        {
+            value_end = strpbrk(value_start, ",");
+            if (value_end == NULL)
+            {
+                /* Field value is the end of the string */
+                ERROR("Can't parse OvS flow rule without actions");
+                te_string_free(&body);
+                return TE_RC(TE_TAPI, TE_EINVAL);
+            }
+            trim_spaces(&value_start, &value_end);
+            next_name_start = value_end + 1;
+        }
+
+        /*
+         * Different OvS tools use different field names for table IDs.
+         * Handle "table_id" when parsing, but stick to "table" everywhere else.
+         */
+        if (strncmp(name_start, "table_id", name_end - name_start) == 0)
+        {
+            name_start = "table";
+            name_end = name_start + strlen(name_start);
+        }
+
+        for (i = 0; i < field_desc_num; i++)
+        {
+            ovs_flow_field_desc *desc = &field_desc[i];
+
+            if (name_end - name_start == desc->name_len &&
+                strncmp(name_start, desc->name, desc->name_len) == 0)
+            {
+                rc = desc->parser(value_start, value_end - value_start, desc, rule);
+                if (rc != 0)
+                {
+                    te_string_free(&body);
+                    return rc;
+                }
+                break;
+            }
+        }
+
+        if (i == field_desc_num)
+        {
+            te_string field_str = TE_STRING_INIT;
+
+            te_string_append(&field_str, "%.*s", name_end - name_start,
+                             name_start);
+
+            if (value_start != NULL)
+            {
+                te_string_append(&field_str, "=%.*s",
+                                 value_end - value_start, value_start);
+            }
+
+            ovs_flow_rule_str_comma_append(&body, field_str.ptr);
+
+            te_string_free(&field_str);
+        }
+
+        name_start = next_name_start;
+    } while (true);
+
+    rule->body = body.ptr;
+    return 0;
+}
+
+void
+ovs_flow_rule_fini(ovs_flow_rule *rule)
+{
+    free(rule->body);
+    rule->body = NULL;
+}
+
+static te_errno
+ovs_flow_rule_to_string_impl(ovs_flow_rule *rule, char **rule_str,
+                             te_bool of_only)
+{
+    unsigned int i;
+    te_string    str = TE_STRING_INIT;
+
+    if (rule_str == NULL)
+        return TE_EINVAL;
+
+    for (i = 0; i < field_desc_num; i++)
+    {
+        ovs_flow_field_desc *desc = &field_desc[i];
+        te_string            field_str = TE_STRING_INIT;
+
+        if (of_only && ((desc->props & OVS_FIELD_NOT_OPENFLOW) != 0))
+            continue;
+
+        desc->formatter(desc, &field_str, rule);
+
+        ovs_flow_rule_str_comma_append(&str, field_str.ptr);
+        te_string_free(&field_str);
+    }
+
+    ovs_flow_rule_str_comma_append(&str, rule->body);
+
+    *rule_str = str.ptr;
+    return 0;
+}
+
+te_errno
+ovs_flow_rule_to_string(ovs_flow_rule *rule, char **rule_str)
+{
+    return ovs_flow_rule_to_string_impl(rule, rule_str, FALSE);
+}
+
+te_errno
+ovs_flow_rule_to_ofctl(ovs_flow_rule *rule, char **rule_str)
+{
+    return ovs_flow_rule_to_string_impl(rule, rule_str, TRUE);
+}

agents/unix/conf/ovs/conf_ovs.c
@@ -43,6 +43,7 @@
 
 #include "conf_common.h"
 #include "conf_ovs.h"
+#include "ovs_flow_rule.h"
 
 #define OVS_SLEEP_MS_MAX 256
 
@@ -307,6 +308,108 @@ out:
         ta_waitpid(ret, NULL, 0);
 
     te_string_free(&cmd);
+
+    return rc;
+}
+
+typedef te_errno ovs_command_handler(const char *line,
+                                     void       *data);
+
+__attribute__((format(printf, 4, 5)))
+static te_errno
+ovs_command(ovs_ctx_t           *ctx,
+            ovs_command_handler *handler,
+            void                *data,
+            const char          *cmd_fmt,
+                                 ...)
+{
+    te_string  cmd_str = TE_STRING_INIT;
+    te_errno   rc;
+    char      *user_cmd;
+    int        ret;
+    int        out_fd;
+    int        status;
+
+    va_list va;
+
+    va_start(va, cmd_fmt);
+    ret = te_vasprintf(&user_cmd, cmd_fmt, va);
+    va_end(va);
+    if (ret < 0)
+    {
+        ERROR("Failed to format user command");
+        return TE_RC(TE_TA_UNIX, TE_EFAIL);
+    }
+
+    rc = te_string_append(&cmd_str, "%s %s", ctx->env.ptr, user_cmd);
+    free(user_cmd);
+    if (rc != 0)
+    {
+        ERROR("Failed to format OvS command: %r", rc);
+        te_string_free(&cmd_str);
+        return rc;
+    }
+
+    ret = te_shell_cmd(cmd_str.ptr, -1,
+                       NULL, (handler != NULL ? &out_fd : NULL), NULL);
+    te_string_free(&cmd_str);
+    if (ret == -1)
+    {
+        ERROR("Failed to invoke OvS command");
+        return TE_RC(TE_TA_UNIX, TE_ECHILD);
+    }
+
+    if (handler != NULL)
+    {
+        FILE   *f;
+        char   *buf = NULL;
+        size_t  buf_size = 0;
+
+        f = fdopen(out_fd, "r");
+        if (f == NULL)
+        {
+            rc = TE_OS_RC(TE_TA_UNIX, errno);
+            ERROR("Failed to open OvS command output stream: %r", rc);
+            goto out;
+        }
+
+        while (!feof(f))
+        {
+            ssize_t ret;
+
+            ret = getline(&buf, &buf_size, f);
+            if (ret == -1 && !feof(f))
+            {
+                rc = TE_OS_RC(TE_TA_UNIX, te_rc_os2te(errno));
+                ERROR("Failed to read OvS command output stream: %r", rc);
+                break;
+            }
+            else if (ret < 1)
+            {
+                rc = 0;
+                break;
+            }
+
+            if (buf[ret - 1] == '\n')
+                buf[ret - 1] = '\0';
+
+            rc = handler(buf, data);
+            if (rc != 0)
+                break;
+        }
+
+        fclose(f);
+        free(buf);
+    }
+
+out:
+    ta_waitpid(ret, &status, 0);
+    if (status != 0)
+    {
+        ERROR("OvS command returned %d", status);
+        if (rc == 0)
+            rc = TE_OS_RC(TE_TA_UNIX, TE_EFAIL);
+    }
 
     return rc;
 }
@@ -3832,6 +3935,318 @@ ovs_bridge_port_vlan_trunks_del(unsigned int  gid,
     return 0;
 }
 
+static te_errno
+ovs_bridge_flow_add(unsigned int  gid,
+                    const char   *oid,
+                    const char   *value,
+                    const char   *ovs,
+                    const char   *bridge_name,
+                    const char   *cookie)
+{
+    ovs_ctx_t     *ctx;
+    te_errno       rc;
+    ovs_flow_rule  rule;
+    char          *rule_str = NULL;
+    uint64_t       new_cookie;
+
+    UNUSED(gid);
+    UNUSED(oid);
+
+    if (strcmp_start("0x", cookie) != 0)
+    {
+        ERROR("OvS flow instance names must start with 0x");
+        return TE_RC(TE_TA_UNIX, TE_EINVAL);
+    }
+
+    rc = te_str_to_uint64(cookie, 16, &new_cookie);
+    if (rc != 0)
+    {
+        ERROR("Failed to parse OvS flow cookie: %r", rc);
+        return rc;
+    }
+
+    rc = ovs_flow_rule_parse(value, &rule);
+    if (rc != 0)
+    {
+        ERROR("Failed to parse Open vSwitch flow rule: %r", rc);
+        return rc;
+    }
+
+    if (rule.cookie != 0 && rule.cookie != new_cookie)
+    {
+        ERROR("Cookie specified in flow rule must match the one in instance name");
+        return rc;
+    }
+
+    rule.cookie = new_cookie;
+
+    /*
+     * TE uses cookies to uniquely identify flow rules. However, Open vSwitch
+     * allows multiple rules to share the same cookie. Most hidden flow rules
+     * have their cookie set to 0, which also applies to the "actions=normal"
+     * rule that is set up by default. TE needs to be able to handle this rule,
+     * so it can't simply ignore rules that have 0 for their cookies. The hidden
+     * rules have been observed to be created only in tables other than 0, which
+     * can be used by TE as a filter criterion.
+     */
+    if (rule.cookie == 0 && rule.table != 0)
+    {
+        ERROR("Cookie 0x0 is reserved for table=0 rules");
+        ovs_flow_rule_fini(&rule);
+        return TE_RC(TE_TA_UNIX, TE_EINVAL);
+    }
+
+    rc = ovs_bridge_pick(ovs, bridge_name, &ctx, NULL);
+    if (rc != 0)
+    {
+        ERROR("Failed to pick the bridge entry");
+        ovs_flow_rule_fini(&rule);
+        return rc;
+    }
+
+    /* Remove metadata fields that cannot be interpreted by ovs-ofctl */
+    rc = ovs_flow_rule_to_ofctl(&rule, &rule_str);
+    ovs_flow_rule_fini(&rule);
+    if (rc != 0)
+        return rc;
+
+    rc = ovs_command(ctx, NULL, NULL,
+                     "ovs-ofctl add-flow %s '%s'",
+                     bridge_name, rule_str);
+    free(rule_str);
+
+    return rc;
+}
+
+static te_errno
+ovs_bridge_flow_del(unsigned int  gid,
+                    const char   *oid,
+                    const char   *ovs,
+                    const char   *bridge_name,
+                    const char   *cookie)
+{
+    ovs_ctx_t    *ctx;
+    te_errno      rc;
+
+    UNUSED(gid);
+    UNUSED(oid);
+
+    rc = ovs_bridge_pick(ovs, bridge_name, &ctx, NULL);
+    if (rc != 0)
+    {
+        ERROR("Failed to pick the bridge entry");
+        return rc;
+    }
+
+    if (strcmp(cookie, "0x0") == 0)
+    {
+        return ovs_command(ctx, NULL, NULL,
+                           "ovs-ofctl del-flows %s cookie=0x0/-1,table=0",
+                           bridge_name);
+    }
+
+    return ovs_command(ctx, NULL, NULL,
+                       "ovs-ofctl del-flows %s 'cookie=%s/-1'",
+                       bridge_name, cookie);
+}
+
+typedef struct flow_get_ctx {
+    uint64_t  cookie;
+    char     *result;
+} flow_get_ctx;
+
+static te_errno
+ovs_bridge_flow_get_handler(const char *line,
+                            void       *data)
+{
+    te_errno       rc;
+    flow_get_ctx  *ctx = data;
+    char          *value = ctx->result;
+    ovs_flow_rule  rule;
+    char          *rule_str;
+    size_t         ret;
+
+    /* Skip lines that are not flows */
+    if (strstr(line, "NXST_FLOW") == line || *line == '\0')
+        return 0;
+
+    rc = ovs_flow_rule_parse(line, &rule);
+    if (rc != 0)
+    {
+        ERROR("Failed to parse Open vSwitch flow rule: %r", rc);
+        return rc;
+    }
+
+    if (rule.cookie == 0 && rule.table != 0)
+        return 0;
+
+    if (rule.cookie != ctx->cookie)
+        return 0;
+
+    if (*value != '\0')
+        return TE_RC(TE_TA_UNIX, TE_ETOOMANY);
+
+    rc = ovs_flow_rule_to_string(&rule, &rule_str);
+    ovs_flow_rule_fini(&rule);
+    if (rc != 0)
+        return rc;
+
+    ret = te_strlcpy(value, rule_str, RCF_MAX_VAL);
+    free(rule_str);
+    if (ret == RCF_MAX_VAL)
+        return TE_RC(TE_TA_UNIX, TE_EINVAL);
+
+    return 0;
+}
+
+static te_errno
+ovs_bridge_flow_get(unsigned int  gid,
+                    const char   *oid,
+                    char         *value,
+                    const char   *ovs,
+                    const char   *bridge_name,
+                    const char   *cookie)
+{
+    ovs_ctx_t    *ctx;
+    te_errno      rc;
+    flow_get_ctx  get_ctx = {
+        .result = value,
+    };
+
+    UNUSED(gid);
+    UNUSED(oid);
+
+    INFO("Getting flow with cookie %s from %s", cookie, bridge_name);
+
+    if (strcmp_start("0x", cookie) != 0)
+    {
+        ERROR("OvS flow instance names must start with 0x");
+        return TE_RC(TE_TA_UNIX, TE_EINVAL);
+    }
+
+    rc = te_str_to_uint64(cookie, 16, &get_ctx.cookie);
+    if (rc != 0)
+    {
+        ERROR("Failed to pass flow cookie: %r", rc);
+        return rc;
+    }
+
+    rc = ovs_bridge_pick(ovs, bridge_name, &ctx, NULL);
+    if (rc != 0)
+    {
+        ERROR("Failed to pick the bridge entry");
+        return rc;
+    }
+
+    *value = '\0';
+    rc = ovs_command(ctx, ovs_bridge_flow_get_handler, &get_ctx,
+                     "ovs-appctl bridge/dump-flows --offload-stats %s",
+                     bridge_name);
+
+    return rc;
+}
+
+static te_errno
+ovs_bridge_flow_set(unsigned int  gid,
+                    const char   *oid,
+                    const char   *value,
+                    const char   *ovs,
+                    const char   *bridge_name,
+                    const char   *cookie)
+{
+    te_errno rc;
+    te_errno rc_restore;
+    char     old_val[RCF_MAX_VAL];
+
+    rc = ovs_bridge_flow_get(gid, oid, old_val, ovs, bridge_name, cookie);
+    if (rc != 0)
+    {
+        ERROR("Failed to back up old OvS flow rule");
+        return rc;
+    }
+
+    rc = ovs_bridge_flow_del(gid, oid, ovs, bridge_name, cookie);
+    if (rc != 0)
+    {
+        ERROR("Failed to install new OvS flow rule");
+        return rc;
+    }
+
+    rc = ovs_bridge_flow_add(gid, oid, value, ovs, bridge_name, cookie);
+    if (rc != 0)
+    {
+        ERROR("Failed to install new OvS flow rule");
+        rc_restore = ovs_bridge_flow_add(gid, oid, old_val, ovs, bridge_name,
+                                         cookie);
+        if (rc_restore != 0)
+        {
+            ERROR("Failed to restore the old rule");
+            return rc_restore;
+        }
+
+        return rc;
+    }
+
+    return 0;
+}
+
+static te_errno
+ovs_bridge_flow_list_handler(const char *line,
+                             void *data)
+{
+    te_string     *result = data;
+    te_errno       rc;
+    ovs_flow_rule  rule;
+
+    /* Skip lines that are not flows */
+    if (strstr(line, "NXST_FLOW") == line || *line == '\0')
+        return 0;
+
+    rc = ovs_flow_rule_parse(line, &rule);
+    if (rc != 0)
+    {
+        ERROR("Failed to parse Open vSwitch flow rule: %r", rc);
+        return rc;
+    }
+
+    if (rule.cookie == 0 && rule.table != 0)
+        return 0;
+
+    return te_string_append(result, "0x%"PRIx64" ", rule.cookie);
+}
+
+static te_errno
+ovs_bridge_flow_list(unsigned int  gid,
+                     const char   *oid,
+                     const char   *sub_id,
+                     char         **list,
+                     const char   *ovs,
+                     const char   *bridge_name)
+{
+    te_string  result = TE_STRING_INIT;
+    ovs_ctx_t *ctx;
+    te_errno   rc;
+
+    UNUSED(gid);
+    UNUSED(oid);
+    UNUSED(sub_id);
+
+    rc = ovs_bridge_pick(ovs, bridge_name, &ctx, NULL);
+    if (rc != 0)
+    {
+        ERROR("Failed to pick the bridge entry");
+        return rc;
+    }
+
+    rc = ovs_command(ctx, ovs_bridge_flow_list_handler, &result,
+                     "ovs-appctl bridge/dump-flows --offload-stats %s",
+                     bridge_name);
+
+    *list = result.ptr;
+
+    return rc;
+}
+
 RCF_PCH_CFG_NODE_COLLECTION(node_ovs_bridge_port_vlan_trunks, "trunk",
                             NULL, NULL,
                             ovs_bridge_port_vlan_trunks_add,
@@ -3852,8 +4267,14 @@ RCF_PCH_CFG_NODE_RW_COLLECTION(node_ovs_bridge_port, "port",
                                ovs_bridge_port_add, ovs_bridge_port_del,
                                ovs_bridge_port_list, NULL);
 
+RCF_PCH_CFG_NODE_RW_COLLECTION(node_ovs_bridge_flow, "flow",
+                               NULL, &node_ovs_bridge_port,
+                               ovs_bridge_flow_get, ovs_bridge_flow_set,
+                               ovs_bridge_flow_add, ovs_bridge_flow_del,
+                               ovs_bridge_flow_list, NULL);
+
 RCF_PCH_CFG_NODE_RW_COLLECTION(node_ovs_bridge, "bridge",
-                               &node_ovs_bridge_port, NULL,
+                               &node_ovs_bridge_flow, NULL,
                                ovs_bridge_get, NULL,
                                ovs_bridge_add, ovs_bridge_del,
                                ovs_bridge_list, NULL);

lib/meson.build
@@ -39,6 +39,7 @@ te_common_libs = [
     'rpcxdr',
     'rpc_dpdk',
     'rpc_types',
+    'ovs_flow',
 ]
 
 # Test Engine libraries

lib/ovs_flow/meson.build
@@ -1,0 +1,21 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2023 Advanced Micro Devices, Inc. All rights reserved.
+
+lib_ovs_flow = static_library('ovs_flow', 'ovs_flow_rule.c',
+                              install: install_lib,
+                              include_directories: includes,
+                              dependencies: [dep_lib_logger_core, dep_lib_tools])
+dep_lib_ovs_flow = declare_dependency(link_with: lib_ovs_flow,
+                                      dependencies: [dep_lib_logger_core, dep_lib_tools],
+                                      include_directories: includes)
+dep_lib_static_ovs_flow = dep_lib_ovs_flow
+
+headers += files(
+    'ovs_flow_rule.h',
+)
+sources += files(
+    'ovs_flow_rule.c',
+)
+te_libs += [
+    'tools',
+]

lib/ovs_flow/ovs_flow_rule.h
@@ -1,0 +1,102 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (C) 2023 Advanced Micro Devices, Inc. All rights reserved. */
+/** @file
+ * @brief OVS Flow Rule Processing Library
+ *
+ * Definition of the flow rule descriptor structure and the associated
+ * operations.
+ */
+
+#ifndef __TE_OVS_FLOW_RULE_H__
+#define __TE_OVS_FLOW_RULE_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @defgroup ovs_flow_rule Open vSwitch flow rule handling
+ * @ingroup ovs_flow
+ * @{
+ */
+
+/** Open vSwitch flow rule descriptor structure */
+typedef struct ovs_flow_rule {
+    uint64_t cookie;
+    uint64_t table;
+    uint64_t n_packets;
+    uint64_t n_bytes;
+    uint64_t n_offload_packets;
+    uint64_t n_offload_bytes;
+
+    char *body;
+} ovs_flow_rule;
+
+/**
+ * Parse the given string into an Open vSwitch flow rule.
+ *
+ * @param[in]  rule_str Flow rule string representation.
+ * @param[out] rule     Flow rule descriptor structure.
+ *
+ * @return Status code.
+ */
+extern te_errno ovs_flow_rule_parse(const char *rule_str,
+                                    ovs_flow_rule *rule);
+
+/**
+ * Free the additional memory used by the flow rule descriptor structure.
+ *
+ * @param[in]  rule     Flow rule descriptor structure.
+ *
+ * @return Nothing.
+ */
+extern void ovs_flow_rule_fini(ovs_flow_rule *rule);
+
+/**
+ * Format the flow rule including all metadata.
+ * The function allocates a memory buffer for the resulting string, it is the
+ * user's responsibility to free it.
+ *
+ * @param[in]  rule     Flow rule descriptor structure.
+ * @param[out] rule_str Where to store the resulting string.
+ *
+ * @return Status code.
+ */
+extern te_errno ovs_flow_rule_to_string(ovs_flow_rule *rule, char **rule_str);
+
+/**
+ * Format the flow rule according to Open vSwitch's ovs-ofctl expectations.
+ * The function allocates a memory buffer for the resulting string, it is the
+ * user's responsibility to free it.
+ *
+ * @param[in]  rule     Flow rule descriptor structure.
+ * @param[out] rule_str Where to store the resulting string.
+ *
+ * @return Status code.
+ */
+extern te_errno ovs_flow_rule_to_ofctl(ovs_flow_rule *rule, char **rule_str);
+
+/**
+ * Given a string representation of some flow rule, extract the value of the
+ * field with user-provided name.
+ * The function does not allocate anything, the value pointer is guaranteed to
+ * be within the same string buffer or NULL.
+ * If present, the mask will be included in the result.
+ *
+ * @param[in]  rule         Flow rule in string form.
+ * @param[in]  field        Field whose value needs to be extracted.
+ * @param[out] value        Value start address.
+ * @param[out] value_len    Length of the value.
+ *
+ * @return Status code.
+ */
+extern te_errno ovs_flow_rule_str_get_field(const char *rule, const char *field,
+                                        const char **value, size_t *value_len);
+
+/**@} <!-- END ovs_flow_rule --> */
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* !__TE_OVS_FLOW_RULE_H__ */

commit 18ab9bdbeafa57d09f60f782c0b2902008177d5a

common: remove last remnants of old boolean type
Recently some PRs have been merged that still used
the old and deprecated boolean type and by accident
it was left unnoticed. The patch cleans it up.

Link: https://github.com/oktetlabs/test-environment/pull/23
Fixes: 6c25f1139032 ("agents/unix: implement OvS flow handling")
Fixes: 2e419dea765b ("lib/tapi_tool: support --register-dump Ethtool option")
OL-Redmine-Id: 13768
Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

lib/ovs_flow/ovs_flow_rule.c
@@ -246,7 +246,7 @@ ovs_flow_rule_fini(ovs_flow_rule *rule)
 
 static te_errno
 ovs_flow_rule_to_string_impl(ovs_flow_rule *rule, char **rule_str,
-                             te_bool of_only)
+                             bool of_only)
 {
     unsigned int i;
     te_string    str = TE_STRING_INIT;
@@ -277,11 +277,11 @@ ovs_flow_rule_to_string_impl(ovs_flow_rule *rule, char **rule_str,
 te_errno
 ovs_flow_rule_to_string(ovs_flow_rule *rule, char **rule_str)
 {
-    return ovs_flow_rule_to_string_impl(rule, rule_str, FALSE);
+    return ovs_flow_rule_to_string_impl(rule, rule_str, false);
 }
 
 te_errno
 ovs_flow_rule_to_ofctl(ovs_flow_rule *rule, char **rule_str)
 {
-    return ovs_flow_rule_to_string_impl(rule, rule_str, TRUE);
+    return ovs_flow_rule_to_string_impl(rule, rule_str, true);
 }

lib/tapi_tool/tapi_ethtool.h
@@ -125,7 +125,7 @@ typedef struct tapi_ethtool_report {
                                 stderr output */
 
     /** @c true if something was printed to stdout. */
-    te_bool out;
+    bool out;
     /** Text printed to stdout. */
     te_string out_data;
 