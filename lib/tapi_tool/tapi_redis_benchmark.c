oktetlabs/test-environment

commit 3aefc366db51022182db73701a272207d44f95f7

lib/tapi_tool: add redis benchmark API and selftest
OL-Redmine-Id: 12198
Co-developed-by: Georgii Samoilov <georgii.samoilov@oktetlabs.ru>
Signed-off-by: Georgii Samoilov <georgii.samoilov@oktetlabs.ru>
Signed-off-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>
Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Daniil Byshenko <daniil.byshenko@oktetlabs.ru>
Reviewed-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>

lib/tapi_tool/tapi_redis_benchmark.c
@@ -1,0 +1,390 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/** @file
+ * @brief redis-benchmark tool TAPI
+ *
+ * TAPI to handle redis-benchmark tool.
+ *
+ * Copyright (C) 2022 OKTET Labs Ltd. All rights reserved.
+ */
+
+#include "tapi_redis_benchmark.h"
+#include "te_errno.h"
+#include "te_alloc.h"
+#include "te_str.h"
+#include "logger_api.h"
+
+/**
+ * Path to redis-benchmark exec in the case of
+ * tapi_redis_benchmark_opt::exec_path is @c NULL.
+ */
+static const char *redis_benchmark_path = "redis-benchmark";
+
+/* Supported redis-benchmark command line arguments. */
+static const tapi_job_opt_bind redis_benchmark_binds[] = TAPI_JOB_OPT_SET(
+    TAPI_JOB_OPT_SOCKADDR_PTR("-h", FALSE, tapi_redis_benchmark_opt, server),
+    TAPI_JOB_OPT_SOCKPORT_PTR("-p", FALSE, tapi_redis_benchmark_opt, server),
+    TAPI_JOB_OPT_STRING("-s", FALSE, tapi_redis_benchmark_opt, socket),
+    TAPI_JOB_OPT_UINT_T("-c", FALSE, NULL, tapi_redis_benchmark_opt, clients),
+    TAPI_JOB_OPT_UINT_T("-n", FALSE, NULL, tapi_redis_benchmark_opt, requests),
+    TAPI_JOB_OPT_UINT_T("-d", FALSE, NULL, tapi_redis_benchmark_opt, size),
+    TAPI_JOB_OPT_UINT_T("--dbnum", FALSE, NULL, tapi_redis_benchmark_opt,
+                        dbnum),
+    TAPI_JOB_OPT_UINT_T("-k", FALSE, NULL, tapi_redis_benchmark_opt,
+                        keep_alive),
+    TAPI_JOB_OPT_UINT_T("-r", FALSE, NULL, tapi_redis_benchmark_opt,
+                        keyspacelen),
+    TAPI_JOB_OPT_UINT_T("-P", FALSE, NULL, tapi_redis_benchmark_opt, pipelines),
+    TAPI_JOB_OPT_BOOL("-e", tapi_redis_benchmark_opt, show_srv_errors),
+    TAPI_JOB_OPT_UINT_T("--threads", FALSE, NULL, tapi_redis_benchmark_opt,
+                        threads),
+    TAPI_JOB_OPT_STRING("-t", FALSE, tapi_redis_benchmark_opt, tests),
+    TAPI_JOB_OPT_BOOL("-I", tapi_redis_benchmark_opt, idle)
+);
+
+/* Default values of redis-benchmark command line arguments. */
+const tapi_redis_benchmark_opt tapi_redis_benchmark_default_opt = {
+    .server             = NULL,
+    .socket             = NULL,
+    .clients            = TAPI_JOB_OPT_UINT_UNDEF,
+    .requests           = TAPI_JOB_OPT_UINT_UNDEF,
+    .size               = TAPI_JOB_OPT_UINT_UNDEF,
+    .dbnum              = TAPI_JOB_OPT_UINT_UNDEF,
+    .keep_alive         = TAPI_JOB_OPT_UINT_UNDEF,
+    .keyspacelen        = TAPI_JOB_OPT_UINT_UNDEF,
+    .pipelines          = TAPI_JOB_OPT_UINT_UNDEF,
+    .show_srv_errors    = TRUE,
+    .threads            = TAPI_JOB_OPT_UINT_UNDEF,
+    .tests              = NULL,
+    .idle               = FALSE,
+    .exec_path          = NULL
+};
+
+/* See description in tapi_redis_benchmark.h */
+te_errno
+tapi_redis_benchmark_create(tapi_job_factory_t *factory,
+                            const tapi_redis_benchmark_opt *opt,
+                            tapi_redis_benchmark_app **app)
+{
+    te_errno rc;
+    te_vec args = TE_VEC_INIT(char *);
+    tapi_redis_benchmark_app *new_app;
+    const char *path = redis_benchmark_path;
+
+    if (factory == NULL || opt == NULL || app == NULL)
+    {
+        ERROR("%s() arguments cannot be NULL", __func__);
+        return TE_RC(TE_TAPI, TE_EINVAL);
+    }
+
+    new_app = TE_ALLOC(sizeof(tapi_redis_benchmark_app));
+
+    if (opt->exec_path != NULL)
+        path = opt->exec_path;
+
+    rc = tapi_job_opt_build_args(path, redis_benchmark_binds, opt, &args);
+    if (rc != 0)
+    {
+        ERROR("Failed to build redis-benchmark command line arguments: %r", rc);
+        goto cleanup;
+    }
+
+    rc = tapi_job_simple_create(factory,
+                       &(tapi_job_simple_desc_t){
+                            .program    = path,
+                            .argv       = (const char **)args.data.ptr,
+                            .job_loc    = &new_app->job,
+                            .stdout_loc = &new_app->out_chs[0],
+                            .stderr_loc = &new_app->out_chs[1],
+                            .filters    = TAPI_JOB_SIMPLE_FILTERS(
+                                {
+                                    .use_stdout = TRUE,
+                                    .readable = TRUE,
+                                    .re = "======\\s*([^=]*)\\s======",
+                                    .extract = 1,
+                                    .filter_var = &new_app->filter_test_name,
+                                },
+                                {
+                                    .use_stdout = TRUE,
+                                    .readable = TRUE,
+                                    .re = "completed in ([0-9.]*) seconds",
+                                    .extract = 1,
+                                    .filter_var = &new_app->filter_time,
+                                },
+                                {
+                                    .use_stdout = TRUE,
+                                    .readable = TRUE,
+                                    .re = "([0-9.]*) requests per second",
+                                    .extract = 1,
+                                    .filter_var = &new_app->filter_rps,
+                                },
+                                {
+                                    .use_stdout  = TRUE,
+                                    .readable    = TRUE,
+                                    .log_level   = TE_LL_RING,
+                                    .filter_name = "redis-benchmark stdout"
+                                },
+                                {
+                                    .use_stderr  = TRUE,
+                                    .readable    = FALSE,
+                                    .log_level   = TE_LL_WARN,
+                                    .filter_name = "redis-benchmark stderr"
+                                }
+                            )
+                        });
+    if (rc != 0)
+        ERROR("Failed to create %s job: %r", path, rc);
+
+cleanup:
+    te_vec_deep_free(&args);
+    if (rc == 0)
+        *app = new_app;
+    else
+        free(new_app);
+
+    return rc;
+}
+
+/* See description in tapi_redis_benchmark.h */
+te_errno
+tapi_redis_benchmark_start(const tapi_redis_benchmark_app *app)
+{
+    if (app == NULL)
+    {
+        ERROR("Redis-benchmark app to start job can't be NULL");
+        return TE_RC(TE_TAPI, TE_EINVAL);
+    }
+
+    return tapi_job_start(app->job);
+}
+
+/* See description in tapi_redis_benchmark.h */
+te_errno
+tapi_redis_benchmark_wait(const tapi_redis_benchmark_app *app, int timeout_ms)
+{
+    te_errno rc;
+    tapi_job_status_t status;
+
+    if (app == NULL)
+    {
+        ERROR("Redis-benchmark app to wait job can't be NULL");
+        return TE_RC(TE_TAPI, TE_EINVAL);
+    }
+
+    rc = tapi_job_wait(app->job, timeout_ms, &status);
+    if (rc != 0)
+    {
+        if (TE_RC_GET_ERROR(rc) == TE_EINPROGRESS)
+            RING("Job was still in process at the end of the wait");
+
+        return TE_RC(TE_TAPI, rc);
+    }
+
+    TAPI_JOB_CHECK_STATUS(status);
+    return 0;
+}
+
+/* See description in tapi_redis_benchmark.h */
+te_errno
+tapi_redis_benchmark_stop(const tapi_redis_benchmark_app *app)
+{
+    if (app == NULL)
+    {
+        ERROR("Redis-benchmark app to stop job can't be NULL");
+        return TE_RC(TE_TAPI, TE_EINVAL);
+    }
+
+    return tapi_job_stop(app->job, SIGTERM, TAPI_REDIS_BENCHMARK_TIMEOUT_MS);
+}
+
+/* See description in tapi_redis_benchmark.h */
+te_errno
+tapi_redis_benchmark_kill(const tapi_redis_benchmark_app *app, int signum)
+{
+    if (app == NULL)
+    {
+        ERROR("Redis-benchmark app to kill job can't be NULL");
+        return TE_RC(TE_TAPI, TE_EINVAL);
+    }
+
+    return tapi_job_kill(app->job, signum);
+}
+
+/* See description in tapi_redis_benchmark.h */
+te_errno
+tapi_redis_benchmark_destroy(tapi_redis_benchmark_app *app)
+{
+    te_errno rc;
+
+    if (app == NULL)
+        return 0;
+
+    rc = tapi_job_destroy(app->job, TAPI_REDIS_BENCHMARK_TIMEOUT_MS);
+    if (rc != 0)
+    {
+        ERROR("Failed to destroy redis-benchmark job: %r", rc);
+        return TE_RC(TE_TAPI, rc);
+    }
+
+    free(app);
+    return 0;
+}
+
+/* See description in tapi_redis_benchmark.h */
+void
+tapi_redis_benchmark_destroy_report(tapi_redis_benchmark_report *entry)
+{
+    tapi_redis_benchmark_stat *node;
+
+    while (!SLIST_EMPTY(entry))
+    {
+        node = SLIST_FIRST(entry);
+        SLIST_REMOVE_HEAD(entry, next);
+        free(node->test_name);
+        free(node);
+    }
+}
+
+/* See description in tapi_redis_benchmark.h */
+te_errno
+tapi_redis_benchmark_get_report(tapi_redis_benchmark_app *app,
+                                tapi_redis_benchmark_report *report)
+{
+    te_errno rc;
+    unsigned int i;
+    tapi_redis_benchmark_stat *stat;
+    tapi_redis_benchmark_stat *prev = NULL;
+    tapi_redis_benchmark_report result = SLIST_HEAD_INITIALIZER(result);
+
+    tapi_job_buffer_t *buf_names = NULL;
+    tapi_job_buffer_t *buf_times = NULL;
+    tapi_job_buffer_t *buf_rps = NULL;
+
+    unsigned int count_names = 0;
+    unsigned int count_times = 0;
+    unsigned int count_rps = 0;
+
+    if (app == NULL || report == NULL)
+    {
+        ERROR("%s() arguments cannot be NULL", __func__);
+        return TE_RC(TE_TAPI, TE_EINVAL);
+    }
+
+    rc = tapi_job_receive_many(TAPI_JOB_CHANNEL_SET(app->filter_test_name),
+                               -1, &buf_names, &count_names);
+    if (rc == 0)
+        rc = tapi_job_receive_many(TAPI_JOB_CHANNEL_SET(app->filter_time),
+                                   -1, &buf_times, &count_times);
+    if (rc == 0)
+        rc = tapi_job_receive_many(TAPI_JOB_CHANNEL_SET(app->filter_rps),
+                                   -1, &buf_rps, &count_rps);
+    if (rc != 0)
+    {
+        ERROR("tapi_job_receive_many() returned unexpected result: %r", rc);
+        goto cleanup;
+    }
+
+    if (count_names != count_times || count_names != count_rps)
+    {
+        ERROR("%s() the number of filtered items must match", __FUNCTION__);
+        rc = TE_RC(TE_TAPI, TE_ERANGE);
+        goto cleanup;
+    }
+
+    for (i = 0; i < count_names; i++)
+    {
+        if (buf_names[i].eos)
+            break;
+
+        stat = TE_ALLOC(sizeof(tapi_redis_benchmark_stat));
+
+        stat->test_name = strdup(buf_names[i].data.ptr);
+
+        rc = te_strtod(buf_times[i].data.ptr, &stat->time);
+        if (rc == 0)
+            rc = te_strtod(buf_rps[i].data.ptr, &stat->rps);
+
+        if (rc != 0)
+        {
+            ERROR("%s() conversion failed with error: %r", __FUNCTION__, rc);
+            break;
+        }
+
+        if (prev == NULL)
+            SLIST_INSERT_HEAD(&result, stat, next);
+        else
+            SLIST_INSERT_AFTER(prev, stat, next);
+
+        prev = stat;
+    }
+
+cleanup:
+    tapi_job_buffers_free(buf_names, count_names);
+    tapi_job_buffers_free(buf_times, count_times);
+    tapi_job_buffers_free(buf_rps, count_rps);
+
+    if (rc == 0)
+        *report = result;
+    else
+        tapi_redis_benchmark_destroy_report(&result);
+
+    return rc;
+}
+
+/* See description in tapi_redis_benchmark.h */
+tapi_redis_benchmark_stat *
+tapi_redis_benchmark_report_get_stat(tapi_redis_benchmark_report *report,
+                                     const char *test_name)
+{
+    tapi_redis_benchmark_stat *iter;
+    tapi_redis_benchmark_stat *stat;
+
+    if (report == NULL || test_name == NULL)
+    {
+        ERROR("%s() arguments connot be NULL", __func__);
+        return NULL;
+    }
+
+    SLIST_FOREACH(iter, report, next)
+    {
+        if (strcmp(iter->test_name, test_name) == 0)
+        {
+            stat = iter;
+            break;
+        }
+    }
+
+    return stat;
+}
+
+/* See description in tapi_redis_benchmark.h */
+te_errno
+tapi_redis_benchmark_report_mi_log(te_mi_logger *logger,
+                                   tapi_redis_benchmark_report *report)
+{
+    tapi_redis_benchmark_stat *iter;
+    te_string str = TE_STRING_INIT;
+
+    if (logger == NULL || report == NULL)
+    {
+        ERROR("%s() arguments connot be NULL", __func__);
+        return TE_RC(TE_TAPI, TE_EINVAL);
+    }
+
+    SLIST_FOREACH(iter, report, next)
+    {
+        te_string_reset(&str);
+        te_string_append(&str, "Execution time for test %s", iter->test_name);
+        te_mi_logger_add_meas(logger, NULL, TE_MI_MEAS_LATENCY, str.ptr,
+                              TE_MI_MEAS_AGGR_SINGLE, iter->time,
+                              TE_MI_MEAS_MULTIPLIER_PLAIN);
+
+        te_string_reset(&str);
+        te_string_append(&str, "Requests per second in test %s", iter->test_name);
+        te_mi_logger_add_meas(logger, NULL, TE_MI_MEAS_RPS, str.ptr,
+                              TE_MI_MEAS_AGGR_SINGLE, iter->rps,
+                              TE_MI_MEAS_MULTIPLIER_PLAIN);
+    }
+    te_string_free(&str);
+
+    return 0;
+}

lib/tapi_tool/meson.build
@@ -11,6 +11,7 @@ headers += files(
     'tapi_netperf.h',
     'tapi_nptcp.h',
     'tapi_ping.h',
+    'tapi_redis_benchmark.h',
     'tapi_redis_srv.h',
     'tapi_sfnt_pingpong.h',
     'tapi_ssh.h',
@@ -27,6 +28,7 @@ sources += files(
     'tapi_netperf.c',
     'tapi_nptcp.c',
     'tapi_ping.c',
+    'tapi_redis_benchmark.c',
     'tapi_redis_srv.c',
     'tapi_sfnt_pingpong.c',
     'tapi_ssh.c',

lib/tapi_tool/tapi_redis_benchmark.h
@@ -1,0 +1,212 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/** @file
+ * @prief redis-benchmark tool TAPI
+ *
+ * @defgroup tapi_redis_benchmark redis-benchmark tool TAPI (tapi_redis_benchmark)
+ * @ingroup te_ts_tapi
+ * @{
+ *
+ * TAPI to handle redis-benchmark tool.
+ *
+ * Copyright (C) 2022 OKTET Labs Ltd. All rights reserved.
+ */
+
+#ifndef __TE_TAPI_REDIS_BENCHMARK_H__
+#define __TE_TAPI_REDIS_BENCHMARK_H__
+
+#include "tapi_job_opt.h"
+#include "tapi_job.h"
+#include "tapi_rpc_stdio.h"
+#include "te_errno.h"
+#include "te_mi_log.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define TAPI_REDIS_BENCHMARK_RECEIVE_TIMEOUT_MS     1000
+#define TAPI_REDIS_BENCHMARK_TIMEOUT_MS             10000
+
+/** Redis-benchmark tool information. */
+typedef struct tapi_redis_benchmark_app {
+    /** TAPI job handle */
+    tapi_job_t         *job;
+    /** output channel handles (stdout and stderr) */
+    tapi_job_channel_t *out_chs[2];
+    /** Filters list: */
+    /** filter for test names */
+    tapi_job_channel_t *filter_test_name;
+    /** filter for test duration */
+    tapi_job_channel_t *filter_time;
+    /** filter for test RPS */
+    tapi_job_channel_t *filter_rps;
+} tapi_redis_benchmark_app;
+
+/** Specific redis-benchmark options. */
+typedef struct tapi_redis_benchmark_opt {
+    /** IP and port of the server under test */
+    const struct sockaddr  *server;
+    /** Server socket. */
+    const char             *socket;
+    /** Number of parallel connections. */
+    tapi_job_opt_uint_t     clients;
+    /** Total number of requests. */
+    tapi_job_opt_uint_t     requests;
+    /** Data size of SET/GET value, in bytes. */
+    tapi_job_opt_uint_t     size;
+    /** SELECT the specified db number. */
+    tapi_job_opt_uint_t     dbnum;
+    /** Keep alive or reconnect. */
+    tapi_job_opt_uint_t     keep_alive;
+    /**
+     * Use random keys for SET/GET/INCR, random values for SADD.
+     * Using this option the benchmark will expand the string __rand_int__
+     * inside an argument with a @c 12 digits number in the specified range
+     * from 0 to keyspacelen-1. The substitution changes every time a command
+     * is executed.
+     */
+    tapi_job_opt_uint_t     keyspacelen;
+    /** Number of pipline requests. */
+    tapi_job_opt_uint_t     pipelines;
+    /** If server replies with errors, show them on stdout. */
+    te_bool                 show_srv_errors;
+    /** Number of threads to use. */
+    tapi_job_opt_uint_t     threads;
+    /** Only run the comma separated list of tests. */
+    const char             *tests;
+    /** Idle mode. */
+    te_bool                 idle;
+    /** Path to redis-benchmark exec (if @c NULL then "redis-benchmark"). */
+    const char             *exec_path;
+} tapi_redis_benchmark_opt;
+
+/** List node with redis-benchmark statistics. */
+typedef struct tapi_redis_benchmark_stat {
+    SLIST_ENTRY(tapi_redis_benchmark_stat) next;
+    /** test name */
+    char *test_name;
+    /** requests per second */
+    double rps;
+    /** execution time */
+    double time;
+} tapi_redis_benchmark_stat;
+
+/** Test stat list entry. */
+typedef SLIST_HEAD(tapi_redis_benchmark_report,
+                   tapi_redis_benchmark_stat) tapi_redis_benchmark_report;
+
+/** Default redis-benchmark options initializer. */
+extern const tapi_redis_benchmark_opt tapi_redis_benchmark_default_opt;
+
+/**
+ * Create redis-benchmark app.
+ *
+ * @param[in]  factory      job factory
+ * @param[in]  opt          redis-benchmark options
+ * @param[out] app          redis-benchmark app handle
+ *
+ * @return Status code.
+ */
+extern te_errno tapi_redis_benchmark_create(tapi_job_factory_t *factory,
+                                            const tapi_redis_benchmark_opt *opt,
+                                            tapi_redis_benchmark_app **app);
+
+/**
+ * Start redis-benchmark.
+ *
+ * @param app   redis-benchmark app handle
+ *
+ * @return Status code.
+ */
+extern te_errno tapi_redis_benchmark_start(const tapi_redis_benchmark_app *app);
+
+/**
+ * Wait for redis-benchmark completion.
+ *
+ * @param app           redis-benchmark app handle
+ * @param timeout_ms    wait timeout in milliseconds
+ *
+ * @return Status code.
+ */
+extern te_errno tapi_redis_benchmark_wait(const tapi_redis_benchmark_app *app,
+                                          int timeout_ms);
+
+/**
+ * Stop redis-benchmark. It can be started over with
+ * @ref tapi_redis_benchmark_start.
+ *
+ * @param app   redis-benchmark app handle
+ *
+ * @return Status code.
+ */
+extern te_errno tapi_redis_benchmark_stop(const tapi_redis_benchmark_app *app);
+
+/**
+ * Send a signal to redis-benchmark.
+ *
+ * @param app       redis-benchmark app handle
+ * @param signum    signal to send
+ *
+ * @return Status code.
+ */
+extern te_errno tapi_redis_benchmark_kill(const tapi_redis_benchmark_app *app,
+                                          int signum);
+
+/**
+ * Destroy redis-benchmark.
+ *
+ * @param app   redis-benchmark app handle
+ *
+ * @return Status code.
+ */
+extern te_errno tapi_redis_benchmark_destroy(tapi_redis_benchmark_app *app);
+
+/**
+ * Empty redis-benchmark report list and free it entries.
+ *
+ * @param entry     report list entry
+ */
+extern void tapi_redis_benchmark_destroy_report(
+                tapi_redis_benchmark_report *entry);
+
+/**
+ * Get redis-benchmark report.
+ *
+ * @param[in]  app      redis-benchmark app handle
+ * @param[out] report   redis-benchmark statistics report
+ *
+ * @return Status code.
+ */
+extern te_errno tapi_redis_benchmark_get_report(
+                    tapi_redis_benchmark_app *app,
+                    tapi_redis_benchmark_report *report);
+
+/**
+ * Get redis-benchmark statistics for a test name.
+ *
+ * @param report    redis-benchmark report
+ * @param test_name test name
+ *
+ * @return redis-benchmark statistic or @c NULL in case of failure
+ */
+extern tapi_redis_benchmark_stat *
+tapi_redis_benchmark_report_get_stat(tapi_redis_benchmark_report *report,
+                                     const char *test_name);
+/**
+ * Add redis-benchmark report to MI logger.
+ *
+ * @param report        redis-benchmark statistics report
+ *
+ * @return Status code.
+ */
+extern te_errno tapi_redis_benchmark_report_mi_log(
+                    te_mi_logger *logger,
+                    tapi_redis_benchmark_report *report);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* __TE_TAPI_REDIS_BENCHMARK_H__ */
+
+/**@} <!-- END tapi_redis --> */

suites/selftest/conf/trc/tapi/tool/redis.trc.xml
@@ -3,12 +3,25 @@
 <!-- Copyright (C) 2022 OKTET Labs Ltd. All rights reserved. -->
 
 <test name="redis" type="package">
-    <objective>TAPI redis-server implementation</objective>
+    <objective>TAPI redis implementation</objective>
     <iter result="PASSED">
         <test name="redis_srv">
             <objective/>
             <iter result="PASSED">
                 <arg name="env"/>
+            </iter>
+        </test>
+         <test name="redis_benchmark">
+            <objective/>
+            <iter result="PASSED">
+                <arg name="env"/>
+                <arg name="clients"/>
+                <arg name="requests"/>
+                <arg name="size"/>
+                <arg name="keyspacelen"/>
+                <arg name="pipelines"/>
+                <arg name="threads"/>
+                <arg name="tests"/>
             </iter>
         </test>
     </iter>

suites/selftest/ts/tapi/tool/redis/meson.build
@@ -4,6 +4,7 @@
 install_headers('redis_srv.h')
 
 tests = [
+    'redis_benchmark',
     'redis_srv',
 ]
 

suites/selftest/ts/tapi/tool/redis/package.xml
@@ -15,5 +15,35 @@
                 <value>{{{'iut_rpcs':IUT},addr:'iut_addr':inet:unicast}}</value>
             </arg>
         </run>
+        <run>
+            <script name="redis_benchmark"/>
+            <arg name="env">
+                <value>{{{'iut_rpcs':IUT},addr:'iut_addr':inet:unicast}}</value>
+            </arg>
+            <arg name="clients">
+                <value>200</value>
+            </arg>
+            <arg name="requests">
+                <value>100000</value>
+            </arg>
+            <arg name="size">
+                <value>128</value>
+            </arg>
+            <arg name="keyspacelen">
+                <value>100000</value>
+            </arg>
+            <arg name="pipelines">
+                <value>128</value>
+            </arg>
+            <arg name="threads">
+                <value>1</value>
+            </arg>
+            <arg name="tests">
+                <value>GET</value>
+                <value>SET</value>
+                <!-- do not pass any test name -->
+                <value>-</value>
+            </arg>
+        </run>
     </session>
 </package>

suites/selftest/ts/tapi/tool/redis/redis_benchmark.c
@@ -1,0 +1,117 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/** @file
+ * @brief TAPI redis_benchmark test
+ *
+ * Demonstrate the usage of TAPI redis_benchmark.
+ *
+ * Copyright (C) 2022 OKTET Labs Ltd. All rights reserved.
+ */
+
+#define TE_TEST_NAME "redis_benchmark"
+
+#include "redis_srv.h"
+#include "tapi_job_factory_rpc.h"
+#include "tapi_redis_srv.h"
+#include "tapi_redis_benchmark.h"
+#include "tapi_sockaddr.h"
+#include "tapi_env.h"
+#include "tapi_test.h"
+
+/* How long test checks that redis is running in seconds. */
+#define REDIS_SRV_WAIT_TIMEOUT 5
+
+int
+main(int argc, char **argv)
+{
+    te_errno rc_wait = 0;
+
+    rcf_rpc_server *iut_rpcs = NULL;
+    const struct sockaddr *iut_addr = NULL;
+
+    te_mi_logger *logger = NULL;
+    tapi_job_factory_t *srv_factory = NULL;
+    tapi_job_factory_t *bm_factory = NULL;
+
+    tapi_redis_srv_app *redis_srv_app = NULL;
+    tapi_redis_srv_opt redis_srv_opt = tapi_redis_srv_default_opt;
+
+    tapi_redis_benchmark_app *redis_bm_app = NULL;
+    tapi_redis_benchmark_opt redis_bm_opt = tapi_redis_benchmark_default_opt;
+    tapi_redis_benchmark_report redis_bm_report;
+
+    unsigned int clients;
+    unsigned int requests;
+    unsigned int size;
+    unsigned int keyspacelen;
+    unsigned int pipelines;
+    unsigned int threads;
+    const char *tests;
+
+    TEST_START;
+    TEST_GET_PCO(iut_rpcs);
+    TEST_GET_ADDR(iut_rpcs, iut_addr);
+    TEST_GET_UINT_PARAM(clients);
+    TEST_GET_UINT_PARAM(requests);
+    TEST_GET_UINT_PARAM(size);
+    TEST_GET_UINT_PARAM(keyspacelen);
+    TEST_GET_UINT_PARAM(pipelines);
+    TEST_GET_UINT_PARAM(threads);
+    TEST_GET_STRING_PARAM(tests);
+
+    TEST_STEP("Configure and start redis-server on IUT.");
+    redis_srv_opt.server = (const struct sockaddr *)iut_addr;
+    CHECK_RC(tapi_job_factory_rpc_create(iut_rpcs, &srv_factory));
+    CHECK_RC(tapi_redis_srv_create(srv_factory, &redis_srv_opt,
+                                   &redis_srv_app));
+    CHECK_RC(tapi_redis_srv_start(redis_srv_app));
+
+    TEST_STEP("Check that redis-server is running.");
+    rc_wait = tapi_redis_srv_wait(redis_srv_app,
+                                  TE_SEC2MS(REDIS_SRV_WAIT_TIMEOUT));
+    if (rc_wait != 0 && TE_RC_GET_ERROR(rc_wait) != TE_EINPROGRESS)
+        TEST_FAIL("Redis-server is not running");
+
+    TEST_STEP("Configure and start redis-benchmark on IUT.");
+    redis_bm_opt.server = (const struct sockaddr *)iut_addr;
+    redis_bm_opt.clients = TAPI_JOB_OPT_UINT_VAL(clients);
+    redis_bm_opt.requests = TAPI_JOB_OPT_UINT_VAL(requests);
+    redis_bm_opt.size = TAPI_JOB_OPT_UINT_VAL(size);
+    redis_bm_opt.keyspacelen = TAPI_JOB_OPT_UINT_VAL(keyspacelen);
+    redis_bm_opt.pipelines = TAPI_JOB_OPT_UINT_VAL(pipelines);
+    redis_bm_opt.threads = TAPI_JOB_OPT_UINT_VAL(threads);
+    if (strcmp(tests, "-") != 0)
+        redis_bm_opt.tests = tests;
+
+    CHECK_RC(tapi_job_factory_rpc_create(iut_rpcs, &bm_factory));
+    CHECK_RC(tapi_redis_benchmark_create(bm_factory, &redis_bm_opt,
+                                         &redis_bm_app));
+    CHECK_RC(tapi_redis_benchmark_start(redis_bm_app));
+
+    TEST_STEP("Wait for redis-benchmark completion.");
+    CHECK_RC(tapi_redis_benchmark_wait(redis_bm_app, -1));
+
+    TEST_STEP("Stop redis-server on IUT.");
+    CHECK_RC(tapi_redis_srv_stop(redis_srv_app));
+
+    TEST_STEP("Get redis-benchmark report on IUT.");
+    CHECK_RC(tapi_redis_benchmark_get_report(redis_bm_app, &redis_bm_report));
+
+    TEST_STEP("Log redis-benchmark statistics in MI format.");
+    CHECK_RC(te_mi_logger_meas_create("redis-benchmark", &logger));
+    CHECK_RC(tapi_redis_benchmark_report_mi_log(logger, &redis_bm_report));
+
+    TEST_STEP("Stop redis-benchmark on IUT.");
+    CHECK_RC(tapi_redis_benchmark_stop(redis_bm_app));
+
+    TEST_SUCCESS;
+
+cleanup:
+    CLEANUP_CHECK_RC(tapi_redis_srv_destroy(redis_srv_app));
+    CLEANUP_CHECK_RC(tapi_redis_benchmark_destroy(redis_bm_app));
+    te_mi_logger_destroy(logger);
+    tapi_redis_benchmark_destroy_report(&redis_bm_report);
+    tapi_job_factory_destroy(srv_factory);
+    tapi_job_factory_destroy(bm_factory);
+
+    TEST_END;
+}

commit dee972fb9cd01551bf4031b448051383c8aa9b73

lib/tapi_tool: fix potentially uninit return value
Fixes: 3aefc366db51 ("lib/tapi_tool: add redis benchmark API and selftest")
OL-Redmine-Id: 12206
Signed-off-by: Daniil Byshenko <daniil.byshenko@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>

lib/tapi_tool/tapi_redis_benchmark.c
@@ -336,7 +336,7 @@ tapi_redis_benchmark_report_get_stat(tapi_redis_benchmark_report *report,
                                      const char *test_name)
 {
     tapi_redis_benchmark_stat *iter;
-    tapi_redis_benchmark_stat *stat;
+    tapi_redis_benchmark_stat *stat = NULL;
 
     if (report == NULL || test_name == NULL)
     {