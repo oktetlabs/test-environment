oktetlabs/test-environment

commit 2de9e3336a25a71f5949bc69cfabc51b49e59f9d

lib: replace malloc/calloc with TE allocators
TE_ALLOC usage avoids additional checks on the return value.

OL-Redmine-Id: 13239
Signed-off-by: Daniil Byshenko <daniil.byshenko@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>

lib/rpcs_dpdk/eal.c
@@ -34,13 +34,7 @@ TARPC_FUNC(rte_eal_init, {},
 
     if (in->argc > 0)
     {
-        argv = calloc(in->argc, sizeof(*argv));
-        if (argv == NULL)
-        {
-            out->common._errno = TE_RC(TE_RPCS, TE_ENOMEM);
-            out->retval = -out->common._errno;
-            goto done;
-        }
+        argv = TE_ALLOC(in->argc * sizeof(*argv));
         for (i = 0; i < in->argc; ++i)
             argv[i] = in->argv.argv_val[i].str;
     }
@@ -136,15 +130,7 @@ TARPC_FUNC(rte_epoll_wait,
     int i;
 
     if (out->events.events_len != 0)
-    {
-        events = calloc(in->maxevents, sizeof(*events));
-        if (events == NULL)
-        {
-            out->common._errno = TE_RC(TE_RPCS, TE_ENOMEM);
-            out->retval = -out->common._errno;
-            goto done;
-        }
-    }
+        events = TE_ALLOC(in->maxevents * sizeof(*events));
 
     MAKE_CALL(out->retval = func(in->epfd,
                                  events,

lib/rpcc_dpdk/eal.c
@@ -559,7 +559,7 @@ rpc_rte_eal_init(rcf_rpc_server *rpcs,
 
     in.argc = argc;
     in.argv.argv_len = argc;
-    in.argv.argv_val = calloc(argc, sizeof(*in.argv.argv_val));
+    in.argv.argv_val = TE_ALLOC(argc * sizeof(*in.argv.argv_val));
     for (i = 0; i < argc; ++i)
         in.argv.argv_val[i].str = argv[i];
 

commit d18338b06d1807536ee558565603c8a3a25fccc6

lib/rpcs_dpdk: add a forgotten header file
In process of replacing malloc/calloc calls with TE allocators,
"te_alloc.h" header was forgotten.

Fixes: 2de9e3336a25 ("lib: replace malloc/calloc with TE allocators")
OL-Redmine-Id: 13239
Signed-off-by: Daniil Byshenko <daniil.byshenko@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>

lib/rpcs_dpdk/eal.c
@@ -26,6 +26,7 @@
 #include "rpc_server.h"
 #include "rpcs_dpdk.h"
 
+#include "te_alloc.h"
 
 TARPC_FUNC(rte_eal_init, {},
 {