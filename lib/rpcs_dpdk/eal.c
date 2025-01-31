oktetlabs/test-environment

commit 2de9e3336a25a71f5949bc69cfabc51b49e59f9d

lib: replace malloc/calloc with TE allocators
TE_ALLOC usage avoids additional checks on the return value.

OL-Redmine-Id: 13239
Signed-off-by: Daniil Byshenko <daniil.byshenko@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>

lib/rpcs_dpdk/eal.c
@@ -34,13 +34,7 @@ TARPC_FUNC(rte_eal_init, {},
 
     if (in->argc > 0)
     {
-        argv = calloc(in->argc, sizeof(*argv));
-        if (argv == NULL)
-        {
-            out->common._errno = TE_RC(TE_RPCS, TE_ENOMEM);
-            out->retval = -out->common._errno;
-            goto done;
-        }
+        argv = TE_ALLOC(in->argc * sizeof(*argv));
         for (i = 0; i < in->argc; ++i)
             argv[i] = in->argv.argv_val[i].str;
     }
@@ -136,15 +130,7 @@ TARPC_FUNC(rte_epoll_wait,
     int i;
 
     if (out->events.events_len != 0)
-    {
-        events = calloc(in->maxevents, sizeof(*events));
-        if (events == NULL)
-        {
-            out->common._errno = TE_RC(TE_RPCS, TE_ENOMEM);
-            out->retval = -out->common._errno;
-            goto done;
-        }
-    }
+        events = TE_ALLOC(in->maxevents * sizeof(*events));
 
     MAKE_CALL(out->retval = func(in->epfd,
                                  events,

lib/rpcs_dpdk/ethdev.c
@@ -2352,15 +2352,7 @@ TARPC_FUNC(rte_eth_fec_get_capability,
     int                      i;
 
     if (out->speed_fec_capa.speed_fec_capa_len != 0)
-    {
-        speed_fec_capa = calloc(in->num, sizeof(*speed_fec_capa));
-        if (speed_fec_capa == NULL)
-        {
-            out->common._errno = TE_RC(TE_RPCS, TE_ENOMEM);
-            out->retval = -out->common._errno;
-            goto done;
-        }
-    }
+        speed_fec_capa = TE_ALLOC(in->num * sizeof(*speed_fec_capa));
 
     MAKE_CALL(out->retval = func(in->port_id, speed_fec_capa, in->num));
 
@@ -2438,13 +2430,7 @@ TARPC_FUNC(rte_eth_dev_get_reg_info,
 
     if (out->info.info_len != 0)
     {
-        info = calloc(out->info.info_len, sizeof(*info));
-        if (info == NULL)
-        {
-            out->common._errno = TE_RC(TE_RPCS, TE_ENOMEM);
-            out->retval = -out->common._errno;
-            goto done;
-        }
+        info = TE_ALLOC(out->info.info_len * sizeof(*info));
         info->data = out->info.info_val->data.data_val;
         info->offset = out->info.info_val->offset;
         info->length = out->info.info_val->length;

lib/rpcc_dpdk/eal.c
@@ -559,7 +559,7 @@ rpc_rte_eal_init(rcf_rpc_server *rpcs,
 
     in.argc = argc;
     in.argv.argv_len = argc;
-    in.argv.argv_val = calloc(argc, sizeof(*in.argv.argv_val));
+    in.argv.argv_val = TE_ALLOC(argc * sizeof(*in.argv.argv_val));
     for (i = 0; i < argc; ++i)
         in.argv.argv_val[i].str = argv[i];
 

lib/rpcc_dpdk/ethdev.c
@@ -21,6 +21,7 @@
 #include "tapi_rpc_rte_mbuf.h"
 #include "rpcc_dpdk.h"
 #include "rpc_dpdk_offloads.h"
+#include "te_alloc.h"
 #include "te_str.h"
 
 
@@ -2154,8 +2155,8 @@ rpc_rte_eth_dev_rss_reta_query(rcf_rpc_server *rpcs, uint16_t port_id,
     {
         in.reta_conf.reta_conf_len =
             TE_DIV_ROUND_UP(reta_size, RPC_RTE_RETA_GROUP_SIZE);
-        in.reta_conf.reta_conf_val = calloc(in.reta_conf.reta_conf_len,
-                                            sizeof(*reta_conf));
+        in.reta_conf.reta_conf_val = TE_ALLOC(in.reta_conf.reta_conf_len *
+                                              sizeof(*reta_conf));
         for (cur_group = 0; cur_group < in.reta_conf.reta_conf_len; cur_group++)
             in.reta_conf.reta_conf_val[cur_group].mask = reta_conf[cur_group].mask;
     }
@@ -2689,8 +2690,8 @@ rpc_rte_eth_dev_rss_reta_update(rcf_rpc_server *rpcs, uint16_t port_id,
     {
         in.reta_conf.reta_conf_len =
             TE_DIV_ROUND_UP(reta_size, RPC_RTE_RETA_GROUP_SIZE);
-        in.reta_conf.reta_conf_val = calloc(in.reta_conf.reta_conf_len,
-                                            sizeof(*reta_conf));
+        in.reta_conf.reta_conf_val = TE_ALLOC(in.reta_conf.reta_conf_len *
+                                              sizeof(*reta_conf));
         if (in.reta_conf.reta_conf_val == NULL)
         {
             rc = TE_RC(TE_RPC, TE_ENOMEM);

commit c8603e7eb8b149ca62d49128f03318745c81e526

lib/rpcs_dpdk: remove unused goto labels
In process of replacing malloc/calloc calls with TE allocators,
unused goto labels were left out. Now compilers generate warnings
about it. This patch fixes this.

Fixes: 2de9e3336a25 ("lib: replace malloc/calloc with TE allocators")
OL-Redmine-Id: 13239
Signed-off-by: Daniil Byshenko <daniil.byshenko@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>

lib/rpcs_dpdk/eal.c
@@ -49,9 +49,6 @@ TARPC_FUNC(rte_eal_init, {},
     neg_errno_h2rpc(&out->retval);
 
     free(argv);
-
-done:
-    ;
 })
 
 TARPC_FUNC(rte_eal_process_type, {},
@@ -143,7 +140,4 @@ TARPC_FUNC(rte_epoll_wait,
 
     free(events);
     neg_errno_h2rpc(&out->retval);
-
-done:
-    ;
 })

lib/rpcs_dpdk/ethdev.c
@@ -2368,9 +2368,6 @@ TARPC_FUNC(rte_eth_fec_get_capability,
 
     free(speed_fec_capa);
     neg_errno_h2rpc(&out->retval);
-
-done:
-    ;
 })
 
 TARPC_FUNC(rte_eth_fec_get,
@@ -2450,6 +2447,5 @@ TARPC_FUNC(rte_eth_dev_get_reg_info,
 
     neg_errno_h2rpc(&out->retval);
 
-done:
     free(info);
 })