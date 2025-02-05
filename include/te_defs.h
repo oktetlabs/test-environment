oktetlabs/test-environment

commit 15eee681c6fba7cbcc9ef33a8c6a91de6e1934f1

lib/tools: add wrappers for generic type dispatching
C11 does support compile-time type dispatching via _Generic
and pre-C11 gcc/clang support it via a set of builtins.
Both interfaces are used in TE which may easily lead to
problems (and we still do support gcc's which are not C11
by default). Therefore we provide macros for some forms
of type dispatching that may use either C11 or gcc-specific
extensions.

OL-Redmine-Id: 13314
Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>

include/te_defs.h
@@ -529,23 +529,19 @@ te_round_up_pow2(unsigned long long num)
  * The rationale for the macro is twofold:
  * - the only legal ISO C way to cast away constness is through
  *   an intermediate cast to `uintptr_t` and some compilers in some
- *   modes __do__ enforce this
+ *   modes __do__ enforce this;
  * - the macro tries to be type-safe and assert at compile time that
- *   the object type is indeed compatible
+ *   the object type is indeed compatible.
  *
- * @note Casting away constness may still be dangerous, use with caution
+ * @note Casting away constness may still be dangerous, use with caution.
  *
- * @param _type  The expected type of the pointer @p _ptr
- * @param _ptr   Object pointer
+ * @param type_  The expected type of the pointer @p ptr_.
+ * @param ptr_   Object pointer.
+ *
+ * @return @p ptr_ casted to a pointer to non-const @p type_.
  */
-#ifdef __GNUC__
-#define TE_CONST_PTR_CAST(_type, _ptr) \
-    (TE_COMPILE_TIME_ASSERT_EXPR(                                        \
-        __builtin_types_compatible_p(TE_TYPEOF(_ptr), const _type *)) ? \
-     ((_type *)(uintptr_t)(_ptr)) : NULL)
-#else
-#define TE_CONST_PTR_CAST(_type, _ptr) ((_type *)(uintptr_t)(_ptr))
-#endif
+#define TE_CONST_PTR_CAST(type_, ptr_) \
+    ((type_ *)(uintptr_t)TE_TYPE_ASSERT(const type_ *, ptr_))
 
 /**
  * Void function pointer. Can be casted to any other function

Doxyfile
@@ -2062,7 +2062,8 @@ INCLUDE_FILE_PATTERNS  =
 # recursively expanded use the := operator instead of the = operator.
 # This tag requires that the tag ENABLE_PREPROCESSING is set to YES.
 
-PREDEFINED             = __attribute__(x)= __has_attribute(x)=1
+PREDEFINED             = __attribute__(x)= __has_attribute(x)=1 \
+                         __has_builtin(x)=1 __STDC_VERSION__=201112L
 
 # If the MACRO_EXPANSION and EXPAND_ONLY_PREDEF tags are set to YES then this
 # tag can be used to specify a list of macro names that should be expanded. The

include/te_compiler.h
@@ -26,6 +26,22 @@
 #endif /* __GNUC__ */
 #endif
 
+#ifndef __has_builtin
+/*
+ * __has_attribute has been supported in GCC since 5.x
+ * and in clang since 2.9, so we may assume all modern
+ * GCC-compatible compilers support it, but since RHEL 7
+ * still use 4.x by default, we'd better support it as well.
+ */
+#ifndef __GNUC__
+#define __has_builtin(bi_) 0
+#else
+#define __has_builtin(bi_) __has_builtin_oldgcc_##bi_
+#define __has_builtin_oldgcc___builtin_types_compatible_p 1
+#define __has_builtin_oldgcc___builtin_choose_expr 1
+#endif /* __GNUC__ */
+#endif
+
 #if __has_attribute(format)
 /**
  * Declare that a function is similiar to @c printf, taking
@@ -135,5 +151,73 @@
  */
 #define TE_CAST_TYPEOF_PTR(obj_, src_) ((TE_TYPEOF(obj_) *)(src_))
 
+#if __STDC_VERSION__ >= 201112L
+/*
+ * Check that the type of @p obj_ is compatible with @p type_.
+ *
+ * Unlike a cast, both type and value of @p obj_ always remain
+ * unchanged, but a compile-time error is raised if @p obj_ is
+ * not compatible with @p type_.
+ *
+ * If generic type dispatching is not supported by the compiler,
+ * the macro is a no-op.
+ *
+ * @param type_   type to check
+ * @param obj_    some value
+ *
+ * @return @p obj_ unchanged.
+ */
+#define TE_TYPE_ASSERT(type_, obj_) _Generic((obj_), type_: (obj_))
+#elif __has_builtin(__builtin_types_compatible_p) && \
+    __has_builtin(__builtin_choose_expr)
+#define TE_TYPE_ASSERT(type_, obj_) \
+    __builtin_choose_expr(__builtin_types_compatible_p(TE_TYPEOF(obj_), \
+                                                       type_),          \
+                          (obj_), (void)0)
+#else
+#define TE_TYPE_ASSERT(type_, obj_) (obj_)
+#endif
+
+#if __STDC_VERSION__ >= 201112L
+/**
+ * Choose one of expressions depending on the type of @p obj_.
+ *
+ * If the type of @p obj_ is compatible with @p type1_, @p expr1_
+ * is returned, otherwise if the type is compatible with @p type2_,
+ * @p expr2_ is returned, otherwise a compilation error is raised.
+ *
+ * @p type1_ and @p type2_ must not be compatible with each other.
+ *
+ * @p obj_ is never evaluated, only its static type is considered
+ * and @p expr2_ is not evaluated if @p expr1_ is chosen and vice versa.
+ *
+ * If the compiler does not support type dispatching, @p expr1_ is
+ * tentatively returned.
+ *
+ * @param obj_    selector object
+ * @param type1_  first type
+ * @param expr1_  first alternative
+ * @param type2_  second type
+ * @param expr2_  second alternative
+ *
+ * @return @p expr1_ or @p expr2_ depending on the type of @p obj_.
+ */
+#define TE_TYPE_ALTERNATIVE(obj_, type1_, expr1_, type2_, expr2_)   \
+    _Generic((obj_), type1_: (expr1_), type2_: (expr2_))
+#elif __has_builtin(__builtin_types_compatible_p) &&    \
+    __has_builtin(__builtin_choose_expr)
+#define TE_TYPE_ALTERNATIVE(obj_, type1_, expr1_, type2_, expr2_) \
+    __builtin_choose_expr(__builtin_types_compatible_p(TE_TYPEOF(obj_), \
+                                                       type1_),         \
+                          (expr1_),                                     \
+                          __builtin_choose_expr(                        \
+                              __builtin_types_compatible_p(             \
+                                  TE_TYPEOF(obj_),                      \
+                                  type2_),                              \
+                              (expr2_),                                 \
+                              ((struct undefined *)NULL)))
+#else
+#define TE_TYPE_ALTERNATIVE(obj_, type1_, expr1_, type2_, expr2_) (expr1_)
+#endif
 
 #endif /* __TE_COMPILER_H__ */

lib/tools/te_vector.h
@@ -267,18 +267,9 @@ te_vec_size(const te_vec *vec)
  *
  * @return Pointer to the content of an element.
  */
-#if __STDC_VERSION__ >= 201112L
 #define te_vec_get(vec_, index_) \
-    (_Generic((vec_),                                               \
-              te_vec*: te_vec_get_mutable,                          \
-              const te_vec*: te_vec_get_immutable)(vec_, index_))
-#else
-#define te_vec_get(vec_, index_) \
-    (__builtin_choose_expr(                                             \
-        __builtin_types_compatible_p(TE_TYPEOF(vec_), const te_vec *), \
-        te_vec_get_immutable,                                           \
-        te_vec_get_mutable)(vec_, index_))
-#endif
+    (TE_TYPE_ALTERNATIVE(vec_, te_vec *, te_vec_get_mutable,            \
+                         const te_vec *, te_vec_get_immutable)(vec_, index_))
 
 static inline const void *
 te_vec_get_immutable(const te_vec *vec, size_t index)
@@ -305,20 +296,10 @@ te_vec_get_mutable(te_vec *vec, size_t index)
  *
  * @return Pointer to element.
  */
-#if __STDC_VERSION__ >= 201112L
 #define te_vec_get_safe(vec_, index_, element_size_) \
-    (_Generic((vec_),                                   \
-              te_vec*: te_vec_get_safe_mutable,         \
-              const te_vec*: te_vec_get_safe_immutable) \
+    (TE_TYPE_ALTERNATIVE(vec_, te_vec *, te_vec_get_safe_mutable,      \
+                         const te_vec *, te_vec_get_safe_immutable)    \
      (vec_, index_, element_size_))
-#else
-#define te_vec_get_safe(vec_, index_, element_size_) \
-    (__builtin_choose_expr(                                         \
-        __builtin_types_compatible_p(                               \
-            TE_TYPEOF(vec_), const te_vec *),                       \
-        te_vec_get_safe_immutable,                                  \
-        te_vec_get_safe_mutable)(vec_, index_, element_size_))
-#endif
 
 static inline const void *
 te_vec_get_safe_immutable(const te_vec *vec, size_t index, size_t element_size)

commit c90a732a0e3c975c104ceee612208b8d39756c18

include: allow casting away constness for non-const pointers
Casting aways constness of an already non-const pointer is perfectly
safe and the ability to do so is useful for macros that may accept
both const and non-const pointers, but this was completely overlooked
during the initial implementation.

OL-Redmine-Id: 13314
Fixes: 15eee681c6fb ("lib/tools: add wrappers for generic type dispatching")
Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

include/te_defs.h
@@ -592,13 +592,17 @@ te_round_up_pow2(unsigned long long num)
  *
  * @note Casting away constness may still be dangerous, use with caution.
  *
+ * @note If @ptr_ does already have a compatible non-const pointer type,
+ *       the macro effectively does nothing.
+ *
  * @param type_  The expected type of the pointer @p ptr_.
  * @param ptr_   Object pointer.
  *
  * @return @p ptr_ casted to a pointer to non-const @p type_.
  */
 #define TE_CONST_PTR_CAST(type_, ptr_) \
-    ((type_ *)(uintptr_t)TE_TYPE_ASSERT(const type_ *, ptr_))
+    TE_TYPE_ALTERNATIVE(ptr_, const type_ *, (type_ *)(uintptr_t)(ptr_), \
+                        type_ *, ptr_)
 
 /**
  * Void function pointer. Can be casted to any other function