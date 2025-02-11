oktetlabs/test-environment

commit ebc5b2fd184a2ba62642226c1155dcb8d9fefa06

lib/tools: sanitize substrings API
The substrings API was quite ill-thought over:
most functions returned an almost meaningless
error code which was always zero for all practical
purposes. The patch changes signatures of these
functions to return a boolean success flag
(i.e. whether a replacement has actually taken place
or the substring start has shifted etc), removing
in many cases the need for additional checks at the
caller side.

OL-Redmine-Id: 14058
Breaks: the boolean status of most functions has inverted:
        0 used to mean no error, i.e. success, not it means
        false, i.e. failure. So users of substrings API that
        cared to check for an (always zero) status code would
        need to be patched.
Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

agents/unix/conf/base/conf_mem.c
@@ -345,23 +345,17 @@ split_proc_mounts_line(char *str, te_vec *vec)
     return rc;
 }
 
-static te_errno
+static void
 convert_mountpoint_name(const char *mountpoint, te_string *name, bool decode)
 {
-    te_errno rc;
-
     te_string_reset(name);
 
-    rc = te_string_append(name, "%s", mountpoint);
-    if (rc != 0)
-        return rc;
+    te_string_append(name, "%s", mountpoint);
 
     if (decode)
-        rc = te_string_replace_all_substrings(name, "/", PATH_DELIMITER);
+        te_string_replace_all_substrings(name, "/", PATH_DELIMITER);
     else
-        rc = te_string_replace_all_substrings(name, PATH_DELIMITER, "/");
-
-    return rc;
+        te_string_replace_all_substrings(name, PATH_DELIMITER, "/");
 }
 
 static te_errno
@@ -473,12 +467,7 @@ update_mountpoint_info(struct hugepage_info *hp_info, te_vec *mount_args)
     if (mp_info != NULL)
         return 0;
 
-    rc = convert_mountpoint_name(name, &encode_name, false);
-    if (rc != 0)
-    {
-        ERROR("%s(): Cannot encode mountpoint name: %r", __FUNCTION__, rc);
-        return rc;
-    }
+    convert_mountpoint_name(name, &encode_name, false);
 
     if (!rcf_pch_rsrc_accessible("/agent:%s/mem:/hugepages:%u/mountpoint:%s",
                                  ta_name, hp_info->size, encode_name.ptr))
@@ -679,12 +668,7 @@ hugepages_mountpoint_add(unsigned int gid, const char *oid, const char *value,
         return TE_RC(TE_TA_UNIX, TE_EPERM);
     }
 
-    rc = convert_mountpoint_name(mountpoint, &decode_name, true);
-    if (rc != 0)
-    {
-        ERROR("%s(): Failed to decode mountpoint name: %r", __FUNCTION__, rc);
-        return TE_RC(TE_TA_UNIX, rc);
-    }
+    convert_mountpoint_name(mountpoint, &decode_name, true);
 
     rc = scan_mounts_file(hp_info);
     if (rc != 0)
@@ -729,12 +713,7 @@ hugepages_mountpoint_del(unsigned int gid, const char *oid, const char *unused,
     if (rc != 0)
         return TE_RC(TE_TA_UNIX, rc);
 
-    rc = convert_mountpoint_name(mountpoint, &decode_name, true);
-    if (rc != 0)
-    {
-        ERROR("%s(): Failed to decode mountpoint name: %r", __FUNCTION__, rc);
-        return TE_RC(TE_TA_UNIX, rc);
-    }
+    convert_mountpoint_name(mountpoint, &decode_name, true);
 
     mp_info = find_mountpoint_info(hp_info, decode_name.ptr);
     te_string_free(&decode_name);

agents/unix/conf/base/conf.c
@@ -7876,13 +7876,12 @@ env_list(unsigned int gid, const char *oid,
     return 0;
 }
 
-static te_errno
+static void
 path_substitution_process(te_string *value, const char *subst,
                           const char *replaced_value,
                           const char *env_var)
 {
     te_substring_t iter = TE_SUBSTRING_INIT(value);
-    te_errno rc = 0;
     char *cur_pos = value->ptr;
     char *end_pos;
     char *ch;
@@ -7896,8 +7895,7 @@ path_substitution_process(te_string *value, const char *subst,
             {
                 iter.start = cur_pos - value->ptr;
                 iter.len = strlen(replaced_value);
-                rc = te_substring_replace(&iter, subst);
-                if (rc != 0)
+                if (!te_substring_replace(&iter, "%s", subst))
                     break;
             }
         }
@@ -7908,48 +7906,32 @@ path_substitution_process(te_string *value, const char *subst,
 
         cur_pos = ch + 1;
     }
-
-    if (rc != 0)
-    {
-        ERROR("Failed to make a substitution in '%s' env variable: %r",
-              env_var, rc);
-    }
-
-    return rc;
 }
 
 static te_errno
 env_subst_path_process(te_string *value, const char *subst,
                        const char *replaced_value)
 {
-    return path_substitution_process(value, subst, replaced_value, "PATH");
+    path_substitution_process(value, subst, replaced_value, "PATH");
+    return 0;
 }
 
 static te_errno
 env_subst_ld_lib_path_process(te_string *value, const char *subst,
                               const char *replaced_value)
 {
-    return path_substitution_process(value, subst, replaced_value,
-                                     "LD_LIBRARY_PATH");
+    path_substitution_process(value, subst, replaced_value, "LD_LIBRARY_PATH");
+    return 0;
 }
 
 static te_errno
 env_subst_underscore_process(te_string *value, const char *subst,
                              const char *replaced_value)
 {
-    te_errno rc = 0;
-    te_substring_t iter = TE_SUBSTRING_INIT(value);
-
     if (strcmp_start(replaced_value, value->ptr) == 0)
-    {
-        iter.len = strlen(replaced_value);
-        rc = te_substring_replace(&iter, subst);
-    }
+        te_string_replace(value, 0, strlen(replaced_value), "%s", subst);
 
-    if (rc != 0)
-        ERROR("Failed to make a substitution in '_' env variable: %r", rc);
-
-    return rc;
+    return 0;
 }
 
 /**

agents/unix/conf/process/conf_process.c
@@ -1808,19 +1808,13 @@ static te_errno
 subst_process(te_string *value, const char *subst,
               const char *replaced_value)
 {
-    te_errno rc = 0;
-    te_substring_t iter = TE_SUBSTRING_INIT(value);
-
     if (strcmp_start(replaced_value, value->ptr) == 0)
     {
-        iter.len = strlen(replaced_value);
-        rc = te_substring_replace(&iter, subst);
+        te_string_replace(value, 0, strlen(replaced_value),
+                          "%s", subst);
     }
 
-    if (rc != 0)
-        ERROR("Failed to make a substitution in '%s': %r", value, rc);
-
-    return rc;
+    return 0;
 }
 
 static const rcf_pch_cfg_substitution subst[] = RCF_PCH_CFG_SUBST_SET(

engine/configurator/conf_db.c
@@ -187,27 +187,21 @@ replace_substitutions_to_values(te_string *str, bool *is_subs_found)
 
     while (1)
     {
-        te_substring_find(&iter, CS_SUBSTITUTION_DELIMITER);
-
-        if (!te_substring_is_valid(&iter))
+        if (!te_substring_find(&iter, CS_SUBSTITUTION_DELIMITER))
             break;
 
         *is_subs_found = true;
         end = iter;
         oid_p = iter;
         te_substring_advance(&end);
-        te_substring_find(&end, CS_SUBSTITUTION_DELIMITER);
-
-        if (!te_substring_is_valid(&iter))
+        if (!te_substring_find(&end, CS_SUBSTITUTION_DELIMITER))
             break;
 
         te_substring_advance(&oid_p);
         te_substring_limit(&oid_p, &end);
 
-        rc = te_string_append(&oid, "%.*s", oid_p.len,
-                              oid_p.base->ptr + oid_p.start);
-        if (rc != 0)
-            break;
+        te_string_append(&oid, "%.*s", oid_p.len,
+                         oid_p.base->ptr + oid_p.start);
 
         rc = get_value_for_substitution(oid.ptr, &value);
         if (rc != 0)
@@ -221,13 +215,8 @@ replace_substitutions_to_values(te_string *str, bool *is_subs_found)
         te_substring_advance(&end);
         te_substring_limit(&iter, &end);
 
-        rc = te_substring_replace(&iter, value);
-        if (rc != 0)
-        {
-            ERROR("Failed to replace the substitution in %s: %r",
-                  str->ptr, rc);
-            break;
-        }
+        te_substring_replace(&iter, "%s", value);
+
         free(value);
         value = NULL;
     }

lib/tapi/test_params.c
@@ -1084,34 +1084,6 @@ test_get_double_param(int argc, char **argv, const char *name)
     return value;
 }
 
-/**
- * Replace slash symbols on underscore ones in a test name.
- *
- * @note Configurator uses symbol '/' as a service one
- *       during config files parsing, so we should avoid slashes
- *       in tests names. As an alternative for replacement
- *       we choose underscore '_'.
- *
- * @param test_name           Test name with slashes.
- * @param modified_test_name  Modified test name with underscores.
- *
- * @return Status code.
- */
-static te_errno
-modify_test_name_for_configurator(const char *test_name,
-                                  te_string *modified_test_name)
-{
-    te_errno rc;
-
-    rc = te_string_append(modified_test_name, "%s", test_name);
-    if (rc != 0)
-        return rc;
-
-    rc = te_string_replace_all_substrings(modified_test_name, "_", "/");
-
-    return rc;
-}
-
 /* See description in tapi_test.h */
 char *
 test_get_default_string_param(const char *test_name,
@@ -1121,15 +1093,8 @@ test_get_default_string_param(const char *test_name,
     char *value = NULL;
     te_string modified_test_name = TE_STRING_INIT;
 
-    rc = modify_test_name_for_configurator(test_name,
-                                           &modified_test_name);
-    if (rc != 0)
-    {
-        te_string_free(&modified_test_name);
-
-        TEST_FAIL("Cannot modify test name '%s' for configurator sake",
-                  test_name);
-    }
+    te_string_append(&modified_test_name, "%s", test_name);
+    te_string_replace_all_substrings(&modified_test_name, "_", "/");
 
     rc = cfg_get_instance_string_fmt(&value,
                                      "/local:/test:/testname:%s/default:%s",

lib/tools/te_string.c
@@ -24,6 +24,7 @@
 #include "te_intset.h"
 #include "te_defs.h"
 #include "te_alloc.h"
+#include "te_str.h"
 
 void
 te_string_free_heap(te_string *str)
@@ -750,100 +751,112 @@ raw2string(const uint8_t *data, size_t size)
     return str.ptr;
 }
 
-void
+bool
 te_substring_find(te_substring_t *substr, const char *str)
 {
-    char *ch;
+    const char *ch;
+    bool result;
 
     if (!te_substring_is_valid(substr))
-        return;
+        return false;
 
     ch = strstr(substr->base->ptr + substr->start, str);
     if (ch == NULL)
     {
         substr->start = SIZE_MAX;
         substr->len = 0;
+        result = false;
     }
     else
     {
        substr->start = ch - substr->base->ptr;
        substr->len = strlen(str);
+       result = true;
     }
 
-    return;
+    return result;
 }
 
-te_errno
-te_substring_replace(te_substring_t *substr, const char *str)
+
+size_t
+te_substring_replace_va(te_substring_t *substr, const char *fmt, va_list args)
 {
-    size_t rep_len = str == NULL ? 0 : strlen(str);
-    if (substr->start + substr->len > substr->base->len)
-    {
-        ERROR("Substring position out of bounds");
-        return TE_EINVAL;
-    }
+    if (!te_substring_is_valid(substr))
+        return 0;
 
-    te_string_replace_buf(substr->base, substr->start, substr->len,
-                          str, rep_len);
-
-    substr->start += rep_len;
-    substr->len = 0;
-
-    return 0;
+    substr->len = te_string_replace_va(substr->base, substr->start, substr->len,
+                                       fmt, args);
+    return substr->len;
 }
 
-void
+size_t
+te_substring_replace(te_substring_t *substr, const char *fmt, ...)
+{
+    size_t rep_len;
+    va_list args;
+
+    va_start(args, fmt);
+    rep_len = te_substring_replace_va(substr, fmt, args);
+    va_end(args);
+
+    return rep_len;
+}
+
+bool
 te_substring_advance(te_substring_t *substr)
 {
-    substr->start += substr->len;
-    substr->len = 0;
-}
-
-void
-te_substring_limit(te_substring_t *substr, const te_substring_t *limit)
-{
-    substr->len = limit->start - substr->start;
-}
-
-static void
-replace_substring(te_substring_t *substr, const char *new,
-                  const char *old)
-{
-    te_substring_find(substr, old);
+    bool advanced = false;
 
     if (!te_substring_is_valid(substr))
-        return;
+        return advanced;
 
-    /*
-     * No need to check for return code:
-     * substr is known to be valid.
-     */
-    te_substring_replace(substr, new);
+    advanced = substr->len > 0;
+    substr->start += substr->len;
+    substr->len = 0;
+
+    return advanced;
 }
 
-te_errno
+bool
+te_substring_limit(te_substring_t *substr, const te_substring_t *limit)
+{
+    if (!te_substring_is_valid(substr) || !te_substring_is_valid(limit) ||
+        limit->base != substr->base || limit->start < substr->start)
+        return false;
+    substr->len = limit->start - substr->start;
+    return true;
+}
+
+static bool
+replace_next_substring(te_substring_t *substr, const char *new,
+                       const char *old)
+{
+    if (!te_substring_find(substr, old))
+        return false;
+
+    te_substring_replace(substr, "%s", te_str_empty_if_null(new));
+
+    return true;
+}
+
+bool
 te_string_replace_substring(te_string *str, const char *new,
                             const char *old)
 {
     te_substring_t iter = TE_SUBSTRING_INIT(str);
 
-    replace_substring(&iter, new, old);
-    return 0;
+    return replace_next_substring(&iter, new, old);
 }
 
-te_errno
+size_t
 te_string_replace_all_substrings(te_string *str, const char *new,
                                  const char *old)
 {
     te_substring_t iter = TE_SUBSTRING_INIT(str);
+    size_t count = 0;
 
-    while (1)
-    {
-        replace_substring(&iter, new, old);
+    while (replace_next_substring(&iter, new, old))
+        count++;
 
-        if (!te_substring_is_valid(&iter))
-            break;
-    }
-
-    return 0;
+    return count;
 }

lib/tools/te_string.h
@@ -741,90 +741,158 @@ typedef struct te_substring_t {
 /**
  * Substring initializer
  *
- * @param _base Pointer to the base string
+ * @param base_ Pointer to the base string.
+ *
+ * @bug @p base_ may be a pointer to const #te_string without
+ *      a warning from the compiler, but in this case a user
+ *      shall not call te_substring_replace() on it and currenly
+ *      this is not enforced --- but that may be fixed later.
  */
-#define TE_SUBSTRING_INIT(_base) \
-    (te_substring_t){ .base = _base, .len = 0, .start = 0 }
+#define TE_SUBSTRING_INIT(base_) \
+    (te_substring_t){                                                   \
+        .base = TE_CONST_PTR_CAST(te_string, (base_)),                  \
+        .len = 0,                                                       \
+        .start = 0                                                      \
+    }
 
 /**
- * Check that substring is valid
+ * Check that substring is valid.
  *
- * @param substr Substring
+ * The substring is considered valid if it is completely
+ * contained within its base string or if it has a length of
+ * zero and points right after the end of its base string.
  *
- * @return @c true or @c false
+ * @param substr Substring.
+ *
+ * @return @c true if the substring is valid.
  */
 static inline bool
 te_substring_is_valid(const te_substring_t *substr)
 {
-    return substr->start < substr->base->len;
+    return substr != NULL && substr->base != NULL &&
+        substr->start <= substr->base->len &&
+        substr->start + substr->len <= substr->base->len;
 }
 
 /**
- * Find a @p str starting at @p substr position and update it accordingly
+ * Find a @p str starting at @p substr position and update it accordingly.
  *
- * @param substr Substring
- * @param str    The string to find
+ * @param substr Substring.
+ * @param str    The string to find.
+ *
+ * @return @c true if @p str has been found.
+ *
+ * @note If @p str has not been found, te_substring_is_valid() will also
+ *       return @c false after this call.
+ *
+ * @note If te_substring_find() is called the second time without calling
+ *       te_substring_advance(), it will effectively do nothing.
  */
-extern void te_substring_find(te_substring_t *substr, const char *str);
+extern bool te_substring_find(te_substring_t *substr, const char *str);
 
 /**
  * Replace a substring at a given position, modifying
- * the underlying `te_string`. If @p str is @c NULL,
- * the content of the substring is deleted.
+ * the underlying #te_string. If the substring is not valid,
+ * nothing happens. The starting position of the substring is
+ * moved past the end of the newly replaced segment.
+ *
+ * The replacement string is constructed by applying @c printf()
+ * format to the arguments.
+ *
+ * If @p fmt is @c NULL, the content of the substring is deleted.
+ * No variadic arguments shall be present in this case.
  *
  * @param substr Substring.
- * @param str    Replacement string (may be @c NULL).
+ * @param fmt    Replacement format (may be @c NULL).
+ * @param ...    Format arguments.
  *
- * @return Status code.
- * @retval TE_EINVAL  Substring position is out of bounds.
+ * @return The length of the replacement string.
+ *
+ * @since 1.45.0. Before that version, it accepted a fixed replacement
+ *        string, not a format. It also used to return a pretty
+ *        uninformative status code instead of the length.
  */
-extern te_errno te_substring_replace(te_substring_t *substr,
-                                     const char *str);
+extern size_t te_substring_replace(te_substring_t *substr,
+                                   const char *fmt, ...) TE_LIKE_PRINTF(2, 3);
 
 /**
- * Move the position by the length of the previously substring
+ * Same as te_substring_replace() but accepts a variadic list argument.
+ * If @p fmt is @c NULL, the content of the substring is deleted.
  *
- * @param substr Substring
+ * @param substr Substring.
+ * @param fmt    Replacement format (may be @c NULL).
+ * @param ...    Format arguments.
+ *
+ * @return The length of the replacement string.
  */
-extern void te_substring_advance(te_substring_t *substr);
+extern size_t te_substring_replace_va(te_substring_t *substr,
+                                      const char *fmt,
+                                      va_list args) TE_LIKE_VPRINTF(2);
 
 /**
- * Limit the length of the @p substr to position @p limit
- * so that it ended right before @p limit
+ * Move the position of a substring by its length.
  *
- * @param substr Substring
- * @param limit Limiting substring
+ * In other words, after this call the substring will point
+ * after the point where the substring ended initially.
+ * If the string was not valid initially, nothing happens.
+ *
+ * @param substr Substring.
+ *
+ * @return @c true if the substring has been advanced.
  */
-extern void te_substring_limit(te_substring_t *substr,
+extern bool te_substring_advance(te_substring_t *substr);
+
+/**
+ * Limit the length of the @p substr so that it would end
+ * right before the start of @p limit.
+ *
+ * If any of the substrings is not valid of if they have
+ * different base strings or if @p limit starts before
+ * @p substr, nothing happens.
+ *
+ * @param substr Substring.
+ * @param limit  Limiting substring.
+ *
+ * @return @c true if the substring has been limited.
+ */
+extern bool te_substring_limit(te_substring_t *substr,
                                const te_substring_t *limit);
+
+/**
+ * Replace all occurrences of a substring in a string.
+ *
+ * @param str The string in which to replace.
+ * @param new The replacement string (may be @c NULL).
+ * @param old The substring to be replaced.
+ *
+ * @return The number of performed replacements.
+ *
+ * @since 1.45.0. Before that version the function returned
+ *        a meaningless status code instead of the count.
+ */
+extern size_t te_string_replace_all_substrings(te_string *str,
+                                               const char *new,
+                                               const char *old);
+
+/**
+ * Replace the first occurrence of substring in a string.
+ *
+ * @param str The string in which to replace.
+ * @param new The replacement (may be @c NULL).
+ * @param old The substring to be replaced.
+ *
+ * @return @c true if a substring has been replaced.
+ *
+ * @since 1.45.0. Before that version the function returned
+ *        a meaningless status code instead of the success
+ *        flag.
+ */
+extern bool te_string_replace_substring(te_string *str,
+                                        const char *new,
+                                        const char *old);
 
 /**@} <!-- END te_tools_te_substring --> */
 
-/**
- * Replace all the substrings in a string
- *
- * @param str The string in which to replace.
- * @param new The new substring to replace (may be @c NULL).
- * @param old The substring to be replaced.
- *
- * @return Status code (always 0).
- */
-extern te_errno te_string_replace_all_substrings(te_string *str,
-                                                 const char *new,
-                                                 const char *old);
-
-/**
- * Replace the substring in a string
- *
- * @param str The string in which to replace.
- * @param new The new substring to replace (may be @c NULL).
- * @param old The substring to be replaced.
- *
- * @return Status code (always 0).
- */
-extern te_errno te_string_replace_substring(te_string *str,
-                                            const char *new,
-                                            const char *old);
 #ifdef __cplusplus
 } /* extern "C" */
 #endif

suites/selftest/ts/tools/substrings.c
@@ -53,7 +53,8 @@ find_substrings(const char *base, const char *search, unsigned int n_repl)
         if (!te_substring_is_valid(&substr))
             TEST_VERDICT("The substring is not valid in the middle of search");
 
-        te_substring_find(&substr, search);
+        if (te_substring_find(&substr, search) != (n_repl != 0))
+            TEST_VERDICT("Unexpected result of finding a substring");
     } while(n_repl-- > 0);
 
     if (te_substring_is_valid(&substr))
@@ -94,9 +95,20 @@ replace_substrings(char *base, char *chunk,
     exp_vec[2 * n_repl].iov_len++;
 
     if (repl_all)
-        te_string_replace_all_substrings(&str, repl, chunk);
+    {
+        size_t performed = te_string_replace_all_substrings(&str, repl, chunk);
+
+        if (performed != n_repl)
+        {
+            TEST_VERDICT("Number of actual replacements"
+                         "differ from the expected");
+        }
+    }
     else
-        te_string_replace_substring(&str, repl, chunk);
+    {
+        if (!te_string_replace_substring(&str, repl, chunk))
+            TEST_VERDICT("The replacement is reported not to happen");
+    }
     if (!te_compare_iovecs(1 + 2 * n_repl, exp_vec,
                            1, (const struct iovec[1]){
                                {.iov_base = str.ptr,
@@ -108,6 +120,27 @@ replace_substrings(char *base, char *chunk,
     te_string_free(&str);
 }
 
+static void
+replace_none(const char *base, const char *chunk, const char *rep)
+{
+    te_string str = TE_STRING_INIT;
+
+    te_string_append(&str, "%s", base);
+
+    if (te_string_replace_substring(&str, rep, chunk))
+        TEST_VERDICT("Replacement is reported to happen");
+
+    if (strcmp(str.ptr, base) != 0)
+        TEST_VERDICT("Unexpected substring replacement");
+
+    if (te_string_replace_all_substrings(&str, rep, chunk) != 0)
+        TEST_VERDICT("Replacements are reported to happen");
+
+    if (strcmp(str.ptr, base) != 0)
+        TEST_VERDICT("Unexpected substring replacement");
+
+    te_string_free(&str);
+}
 
 int
 main(int argc, char **argv)
@@ -129,7 +162,7 @@ main(int argc, char **argv)
         char *base = te_make_printable_buf(min_len, max_len + 1, NULL);
         char *chunk = te_make_printable_buf(min_len, max_len + 1, NULL);
         char *rep = te_make_printable_buf(min_len, max_len + 1, NULL);
-        unsigned int n_repl = rand_range(0, max_repl);
+        unsigned int n_repl = rand_range(1, max_repl);
 
         /*
          * Extremely unlikely if min_len is not too small,
@@ -154,6 +187,9 @@ main(int argc, char **argv)
 
         TEST_SUBSTEP("Check for total replacement");
         replace_substrings(base, chunk, rep, n_repl, true);
+
+        TEST_SUBSTEP("Check for non-replacement");
+        replace_none(base, chunk, rep);
 
         free(base);
         free(chunk);

commit f58e6482c015599738e7bd0b5d36ec81bb8cdc64

agents/unix: fix substrings API usage in conf memory module
The substrings API has been changed, and a non-zero return code
no longer indicates an error.

Fixes: ebc5b2fd184a ("lib/tools: sanitize substrings API")
Signed-off-by: Denis Pryazhennikov <denis.pryazhennikov@arknetworks.am>
Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>

agents/unix/conf/base/conf_mem.c
@@ -780,10 +780,7 @@ hugepages_mountpoint_list(unsigned int gid, const char *oid, const char *sub_id,
 
     if (rc == 0)
     {
-        rc = te_string_replace_all_substrings(&result, PATH_DELIMITER, "/");
-        if (rc != 0)
-            return rc;
-
+        te_string_replace_all_substrings(&result, PATH_DELIMITER, "/");
         *list = result.ptr;
     }
     else