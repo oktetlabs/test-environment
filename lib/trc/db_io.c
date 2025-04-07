oktetlabs/test-environment

commit 5bf79e055dd20041e8e454f70c81fcbe0111d252

lib/trc: keep iterations order when saving TRC DB
Avoid iterations reordering when saving updated TRC DB to file.

GitHub-TE-Id: 53
Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

lib/trc/db_io.c
@@ -1597,7 +1597,18 @@ trc_update_iters(trc_test_iters *iters, int flags, int uid,
     char           *user_attr;
     xmlNodePtr      node;
     xmlNodePtr      prev_node;
+    xmlNodePtr      prev_iter_node = NULL;
+    xmlNodePtr      first_iter_node = NULL;
     bool renew_content;
+
+    TAILQ_FOREACH(p, &iters->head, links)
+    {
+        if (p->node != NULL)
+        {
+            first_iter_node = p->node;
+            break;
+        }
+    }
 
     TAILQ_FOREACH(p, &iters->head, links)
     {
@@ -1631,12 +1642,24 @@ trc_update_iters(trc_test_iters *iters, int flags, int uid,
             {
                 INFO("Add node for iteration %p node=%p", iters,
                      iters->node);
-                p->node = p->tests.node = xmlNewChild(iters->node, NULL,
-                                                      BAD_CAST "iter",
-                                                      NULL);
+                p->node = p->tests.node = xmlNewNode(NULL,
+                                                     BAD_CAST "iter");
                 if (p->tests.node == NULL)
                 {
-                    ERROR("xmlNewChild() failed for 'iter'");
+                    ERROR("xmlNewNode() failed for 'iter'");
+                    return TE_ENOMEM;
+                }
+
+                if (prev_iter_node != NULL)
+                    node = xmlAddNextSibling(prev_iter_node, p->node);
+                else if (first_iter_node != NULL)
+                    node = xmlAddPrevSibling(first_iter_node, p->node);
+                else
+                    node = xmlAddChild(iters->node, p->node);
+
+                if (node == NULL)
+                {
+                    ERROR("Failed to add 'iter' node to the tree");
                     return TE_ENOMEM;
                 }
             }
@@ -1799,6 +1822,8 @@ trc_update_iters(trc_test_iters *iters, int flags, int uid,
                     return rc;
             }
         }
+
+        prev_iter_node = p->node;
     }
 
     return 0;

commit 1baf2925eb458ab3cdefc6701a8cecd189dc1118

lib/trc: fix iterations update in TRC
Fix errors in code used to determine where to insert a new iteration
in trc_update_iters().

Fixes: 5bf79e055dd2 ("lib/trc: keep iterations order when saving TRC DB")
OL-Redmine-Id: 14306
Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

lib/trc/db_io.c
@@ -2504,6 +2504,9 @@ trc_update_iters(te_trc_db *db, trc_test_iters *iters, int flags, int uid,
 
         if (!is_saved && p->node != NULL)
         {
+            if (first_iter_node == p->node)
+                first_iter_node = xmlNodeNext(p->node);
+
             xmlUnlinkNode(p->node);
             xmlFreeNode(p->node);
             p->node = NULL;
@@ -2694,7 +2697,8 @@ trc_update_iters(te_trc_db *db, trc_test_iters *iters, int flags, int uid,
             }
         }
 
-        prev_iter_node = p->node;
+        if (p->node != NULL)
+            prev_iter_node = p->node;
     }
 
     return 0;