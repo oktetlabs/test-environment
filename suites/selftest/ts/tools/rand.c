oktetlabs/test-environment

commit bd996be73cebff5d1ad3fec71d66860a947794b8

lib/tools: provide a high-quality RNG
The function rand_range() that is used extensively in
TE has some drawbacks:
- since it is based on C standard rand(), on a typical
  Linux system it only produces 31-bit values, and using
  several consecutive rand() calls to produce a wider
  value may result in unfortunate bit correlation;
- rand() is not the best RNG in itself and the way
  rand_range() is implemented on top of it may make
  lower bits of the result significantly less random.

This patch introduces a fair 64-bit PRNG that is based
on a recent algorithm called xoshiro256++ and some utility
functions around it. It also provide a self test that
ensures the PRNG passes the statistical test for uniformity.

Link: http://vigna.di.unimi.it/ftp/papers/ScrambledLinear.pdf
OL-Redmine-Id: 14268
Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

suites/selftest/ts/tools/rand.c
@@ -1,0 +1,303 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (C) 2025 OKTET Labs Ltd. All rights reserved. */
+/** @file
+ * @brief Test for pseudo random number generator.
+ *
+ * Testing the correctness of pseudo random number generator.
+ */
+
+/** @page tools_rand PRNG test
+ *
+ * @objective Testing the sanity of PRNG routines.
+ *
+ * The test is not a replacement for real PRNG burnout tests:
+ * basically it uses Kolmogorov-Smirnov test to verify that
+ * generated random values are uniformly distributed.
+ *
+ * @par Test sequence:
+ */
+
+/** Logging subsystem entity name */
+#define TE_TEST_NAME    "tools/rand"
+
+#include "te_config.h"
+
+#include <math.h>
+#include "tapi_test.h"
+#include "te_rand.h"
+#include "te_numeric.h"
+
+typedef struct prng_params {
+    intmax_t min;
+    intmax_t max;
+    size_t bitstart;
+    size_t bitlen;
+} prng_params;
+
+static int
+compare_intmax(const void *v1, const void *v2)
+{
+    intmax_t i1 = *(intmax_t *)v1;
+    intmax_t i2 = *(intmax_t *)v2;
+
+    if (i1 == i2)
+        return 0;
+
+    return i1 > i2 ? 1 : -1;
+}
+
+static void
+generate_random(const prng_params *params, unsigned int n, intmax_t seq[n])
+{
+    /*
+     * This is a critical value for Kolmogorov distribution
+     * assuming n > 40 for a significance level 99.5%,
+     * see
+     * e.g. https://www.york.ac.uk/depts/maths/tables/kolmogorovsmirnov.pdf
+     */
+    static const long double k995 = 1.63;
+
+    unsigned int i;
+    unsigned int span;
+    long double maxd = 0.0L;
+
+    /*
+     * Believe it or not, but it may actually happen if a test
+     * is run under valgrind.
+     */
+    if ((params->bitlen == 0 || params->bitlen == TE_BITSIZE(intmax_t)) &&
+        (long double)params->max - (long double)params->min < 1.0L)
+    {
+        WARN("Cannot represent the difference between %jd and %jd",
+             params->min, params->max);
+        return;
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        intmax_t r = te_rand_signed(params->min, params->max);
+
+        if (r < params->min)
+            TEST_VERDICT("Generated value is less than %jd", params->min);
+        if (r > params->max)
+            TEST_VERDICT("Generated value is greater than %jd", params->max);
+
+        if (params->bitlen != 0 && params->bitlen < TE_BITSIZE(intmax_t))
+            r = TE_EXTRACT_BITS(r, params->bitstart, params->bitlen);
+
+        seq[i] = r;
+    }
+
+    /*
+     * The following is doing one-sample Kolmogorov-Smirnov test
+     * to ensure the drawn samples agree with the uniform distribution.
+     *
+     * See https://en.wikipedia.org/wiki/Kolmogorov%E2%80%93Smirnov_test
+     * for an introduction to the topic.
+     */
+    qsort(seq, n, sizeof(*seq), compare_intmax);
+
+    for (i = 0; i < n; i += span)
+    {
+        /*
+         * All calculations must be with long doubles, because
+         * plain double cannot represent the full range of int64_t
+         * precisely enough.
+         */
+        long double ecdf;
+        long double ucdf;
+        long double d;
+
+        for (span = 1; i + span < n; span++)
+        {
+            if (seq[i + span] != seq[i])
+                break;
+        }
+
+        ecdf = (long double)(i + span) / (long double)n;
+
+        if (params->bitlen == 0 || params->bitlen == TE_BITSIZE(intmax_t))
+        {
+            ucdf = ((long double)seq[i] - (long double)params->min + 1.0L) /
+                ((long double)params->max - (long double)params->min + 1.0L);
+        }
+        else
+        {
+            ucdf = ((long double)seq[i] + 1.0L) /
+                (long double)(1ull << params->bitlen);
+        }
+
+        d = fabsl(ecdf - ucdf);
+        if (d > maxd)
+            maxd = d;
+    }
+
+    if (sqrtl((long double)n) * maxd > k995)
+    {
+        TEST_VERDICT("The sample is not uniform: %Lf > %Lf",
+                     sqrtl((long double)n) * maxd, k995);
+    }
+    RING("Max difference between ECDF and uniform CDF = %Lf (< %Lf)",
+         maxd, k995 / sqrtl((long double)n));
+}
+
+static void
+test_random(unsigned int n)
+{
+    static const prng_params variants[] = {
+        {
+            .min = INTMAX_MIN, .max = INTMAX_MAX
+        },
+        {
+            .min = INTMAX_MIN, .max = INTMAX_MAX,
+            .bitstart = 0, .bitlen = 1
+        },
+        {
+            .min = INTMAX_MIN, .max = INTMAX_MAX,
+            .bitstart = 0, .bitlen = 8
+        },
+        {
+            .min = INTMAX_MIN, .max = INTMAX_MAX,
+            .bitstart = TE_BITSIZE(intmax_t) - 8, .bitlen = 8
+        },
+        {
+            .min = INTMAX_MIN, .max = INTMAX_MAX,
+            .bitstart = TE_BITSIZE(intmax_t) - 1, .bitlen = 1
+        },
+        {
+            .min = 0, .max = INTMAX_MAX
+        },
+        {
+            .min = 0, .max = 1
+        },
+        {
+            .min = -1, .max = 1
+        },
+        {
+            .min = 0, .max = UINT8_MAX
+        },
+        {
+            .min = 0, .max = UINT8_MAX,
+            .bitstart = 0, .bitlen = 1
+        },
+        {
+            .min = -1, .max = INTMAX_MAX
+        },
+        {
+            .min = INTMAX_MIN, .max = 0
+        },
+        {
+            .min = INTMAX_MIN, .max = INTMAX_MIN + 1
+        },
+    };
+    unsigned int i;
+    intmax_t *seq = TE_ALLOC(n * sizeof(*seq));
+
+    for (i = 0; i < TE_ARRAY_LEN(variants); i++)
+    {
+        TEST_SUBSTEP("Generating %jd..%jd, testing bits %zu..%zu",
+                     variants[i].min, variants[i].max,
+                     variants[i].bitstart,
+                     variants[i].bitstart +
+                     (variants[i].bitlen == 0 ? TE_BITSIZE(intmax_t) :
+                      variants[i].bitlen) - 1);
+        generate_random(&variants[i], n, seq);
+    }
+
+    free(seq);
+}
+
+static void
+test_scalar_random(unsigned int n_numbers)
+{
+    unsigned int i;
+    intmax_t i8_sum = 0;
+    uintmax_t u8_sum = 0;
+    uintmax_t b_sum = 0;
+
+    for (i = 0; i < n_numbers; i++)
+    {
+        int8_t i8;
+        uint8_t u8;
+        bool b;
+
+        te_scalar_random(TE_SCALAR_TYPE_INT8_T, &i8);
+        te_scalar_random(TE_SCALAR_TYPE_UINT8_T, &u8);
+        te_scalar_random(TE_SCALAR_TYPE_BOOL, &b);
+
+        i8_sum += i8;
+        u8_sum += u8;
+        b_sum += b;
+    }
+
+    /* The thresholds below are somewhat arbitrary. */
+    if (fabs((double)i8_sum / (double)n_numbers) > 1.0)
+        TEST_VERDICT("Generator of signed bytes is skewed");
+
+    if (fabs((double)u8_sum / (double)n_numbers -
+             (double)UINT8_MAX / 2.0) > 1.0)
+    {
+        TEST_VERDICT("Generator of signed bytes is skewed");
+    }
+
+    if (fabs((double)b_sum / (double)n_numbers - 0.5) > 0.01)
+        TEST_VERDICT("Generator of booleans is skewed");
+}
+
+static void
+test_random_div(unsigned int n_numbers)
+{
+    unsigned int i;
+
+    for (i = 0; i < n_numbers; i++)
+    {
+        unsigned j;
+
+        for (j = 1; j < 7; j++)
+        {
+            unsigned k;
+
+            for (k = 0; k < j; k++)
+            {
+                uintmax_t uv = te_rand_unsigned_div(0, UINTMAX_MAX, j, k);
+                intmax_t v = te_rand_signed_div(-(int)j - (int)k,
+                                                (int)j + (int)k, j, k);
+
+                if (uv % j != k)
+                    TEST_VERDICT("Invalid unsigned value");
+                if ((v < -(int)j - (int)k) || (v > (int)j + (int)k))
+                    TEST_VERDICT("Signed value not in range: %jd", v);
+                if (v % j != (v > 0 ? (intmax_t)k : -(intmax_t)k))
+                    TEST_VERDICT("Invalid signed value");
+            }
+        }
+    }
+}
+
+int
+main(int argc, char **argv)
+{
+    unsigned int n_numbers;
+
+    TEST_START;
+    TEST_GET_UINT_PARAM(n_numbers);
+
+    RING("The first random number "
+         "(must be the same if te_rand_seed is the same): %ju",
+         te_rand_unsigned(0, UINTMAX_MAX));
+
+    TEST_STEP("Checking random number generator");
+    test_random(n_numbers);
+
+    TEST_STEP("Checking random scalar values");
+    test_scalar_random(n_numbers);
+
+    TEST_STEP("Checking random number generator with a given remainder");
+    test_random_div(n_numbers);
+
+    TEST_SUCCESS;
+
+cleanup:
+
+    TEST_END;
+}

include/te_defs.h
@@ -371,6 +371,8 @@ extern "C" {
  * @param max     range upper limit
  *
  * @return Random number from the range [min, max]
+ *
+ * @deprecated Use te_rand_unsigned() or te_rand_signed() instead.
  */
 static inline int
 rand_range(int min, int max)

lib/tools/te_numeric.c
@@ -12,6 +12,7 @@
 #include <math.h>
 
 #include "te_numeric.h"
+#include "te_rand.h"
 #include "logger_api.h"
 
 const te_enum_map te_scalar_type_names[] = {
@@ -198,4 +199,23 @@ te_scalar_dynamic_cast(te_scalar_type src_type, const void *src,
 #undef CASE
 
     return rc;
+}
+
+void
+te_scalar_random(te_scalar_type type, void *dst)
+{
+    if (te_scalar_type_is_signed(type))
+    {
+        intmax_t val = te_rand_signed(te_scalar_type_min(type),
+                                      (intmax_t)te_scalar_type_max(type));
+
+        te_scalar_dynamic_cast(TE_SCALAR_TYPE_INTMAX_T, &val, type, dst);
+    }
+    else
+    {
+        uintmax_t val = te_rand_signed((uintmax_t)te_scalar_type_min(type),
+                                       te_scalar_type_max(type));
+
+        te_scalar_dynamic_cast(TE_SCALAR_TYPE_UINTMAX_T, &val, type, dst);
+    }
 }

lib/tools/te_numeric.h
@@ -158,6 +158,21 @@ extern te_errno te_scalar_type_fit_size(bool is_signed, size_t size,
  */
 extern te_errno te_scalar_dynamic_cast(te_scalar_type src_type, const void *src,
                                        te_scalar_type dst_type, void *dst);
+
+
+/**
+ * Generate a random value that fits the type @p type.
+ *
+ * The function use te_rand_signed() or te_rand_unsigned()
+ * depending on the signedness of the type and appropriate
+ * limits for that type.
+ *
+ * @param[in]   type    Type of a generated value.
+ * @param[out]  dst     Location for a generated value.
+ *
+ */
+extern void te_scalar_random(te_scalar_type type, void *dst);
+
 /**@}*/
 
 /**

lib/tools/te_rand.c
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: Apache-2.0 */
-/* Copyright (C) 2023 OKTET Labs Ltd. All rights reserved. */
+/* Copyright (C) 2023-2025 OKTET Labs Ltd. All rights reserved. */
 
 /** @file
  * @brief API for random numbers
@@ -15,6 +15,156 @@
 #include "te_defs.h"
 #include "logger_api.h"
 
+/* Just to be on the safe side... */
+#if UINTMAX_MAX > UINT64_MAX
+#warning "uintmax_t is bigger than uint64_t: this may break PRNG routines"
+#endif
+
+/*
+ * The functions below are adapted from
+ * https://prng.di.unimi.it/xoshiro256plusplus.c and
+ * https://prng.di.unimi.it/splitmix64.c
+ *
+ * No substantial changes have been made to the code,
+ * only some renaming and reformatting.
+ *
+ * In particular, all magic numbers come from the original.
+ *
+ * See also http://vigna.di.unimi.it/ftp/papers/ScrambledLinear.pdf
+ * for a theoretical discussion of the algorithm.
+ */
+
+/*
+ * splitmix64:
+ * Written in 2015 by Sebastiano Vigna (vigna@acm.org).
+ *
+ * xoshiro256pp:
+ * Written in 2019 by David Blackman and Sebastiano Vigna (vigna@acm.org).
+ *
+ * To the extent possible under law, the author has dedicated all copyright
+ * and related and neighboring rights to this software to the public domain
+ * worldwide.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+ * IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+static uint64_t
+rotl(uint64_t x, int k)
+{
+    return (x << k) | (x >> (64 - k));
+}
+
+static uint64_t
+xoshiro256pp(uint64_t state[static 4])
+{
+    uint64_t result = rotl(state[0] + state[3], 23) + state[0];
+    uint64_t t = state[1] << 17;
+
+    state[2] ^= state[0];
+    state[3] ^= state[1];
+    state[1] ^= state[2];
+    state[0] ^= state[3];
+
+    state[2] ^= t;
+
+    state[3] = rotl(state[3], 45);
+
+    return result;
+}
+
+static uint64_t
+splitmix64(uint64_t *seed)
+{
+    uint64_t z;
+
+    *seed += 0x9e3779b97f4a7c15ull;
+    z = *seed;
+    z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9ull;
+    z = (z ^ (z >> 27)) * 0x94d049bb133111ebull;
+
+    return z ^ (z >> 31);
+}
+
+static uint64_t global_xoshiro_state[4];
+static bool xoshiro_state_seeded;
+
+/* See description in te_rand.h */
+uintmax_t
+te_rand_unsigned(uintmax_t min, uintmax_t max)
+{
+    uintmax_t r;
+
+    if (!xoshiro_state_seeded)
+    {
+        /*
+         * As stated by its designers, xoshiro algorithm
+         * cannot be reliably seeded from a 32-bit seed,
+         * so we need the second different PRNG to seed
+         * 256 bit of state from the standard rand().
+         */
+        uint64_t seed = (uint64_t)rand();
+        unsigned int i;
+
+        for (i = 0; i < TE_ARRAY_LEN(global_xoshiro_state); i++)
+            global_xoshiro_state[i] = splitmix64(&seed);
+
+        xoshiro_state_seeded = true;
+    }
+
+    r = xoshiro256pp(global_xoshiro_state);
+
+    /* Treat full range specially to avoid overflows */
+    if (min == 0 && max == UINTMAX_MAX)
+        return r;
+
+    /*
+     * It has always been recommended to use higher order bits
+     * of a random value instead of lower order ones for
+     * downscaling, because the former tend to be more
+     * random than the latter. However, the algorithm we use
+     * does generate pretty random lower bits.
+     *
+     * In addition, we cannot do the same thing as it's usually done
+     * with @c rand(), namely `rand() * (max - min + 1) / RAND_MAX`,
+     * because our generator is 64-bit, and so this would require
+     * genuine 128-bit multiplication and division for this
+     * to work, possible but cumbersome.
+     *
+     * So for the time being we just plainly take the value modulo
+     * the range. As demonstrated by @c suites/selftest/rand.c,
+     * the obtained values do pass the Kolmogorov-Smirnov test for
+     * uniformity, so they are random enough.
+     *
+     * However, if the algorithm is ever to change, the decision
+     * should be re-examined.
+     */
+
+    return r % (max - min + 1) + min;
+}
+
+/* See description in te_rand.h */
+intmax_t
+te_rand_signed(intmax_t min, intmax_t max)
+{
+    /*
+     * If min is positive, all values fit safely in uintmax_t range.
+     */
+    if (min >= 0)
+        return (intmax_t)te_rand_unsigned((uintmax_t)min, (uintmax_t)max);
+
+    return (intmax_t)te_rand_unsigned(0, (uintmax_t)max +
+                                      (uintmax_t)-min) + min;
+}
+
 /* See description in te_rand.h */
 int
 te_rand_range_exclude(int min, int max, int exclude)
@@ -25,7 +175,7 @@ te_rand_range_exclude(int min, int max, int exclude)
         TE_FATAL_ERROR("incorrect range limits");
 
     if (exclude < min || exclude > max)
-        return rand_range(min, max);
+        return (int)te_rand_signed(min, max);
 
     if (min == max)
         TE_FATAL_ERROR("no eligible values remain");
@@ -44,7 +194,7 @@ te_rand_range_exclude(int min, int max, int exclude)
      * This way exclude value is excluded, and any other number
      * from [min, max] has the same chance of being chosen.
      */
-    value = rand_range(min, max - 1);
+    value = (int)te_rand_signed(min, max - 1);
     if (value >= exclude)
         value++;
 

lib/tools/te_rand.h
@@ -20,6 +20,94 @@
 extern "C" {
 #endif
 
+#include <inttypes.h>
+
+/**
+ * Generate a random unsigned number in a given range.
+ *
+ * @param min      Minimum value (inclusive).
+ * @param max      Maximum value (inclusive).
+ *
+ * @return The next pseudo-random number between @p min and @p max.
+ *
+ * @alg The function uses xoshiro256++ algorithm that is
+ *      able to generate the full range of 64-bit numbers,
+ *      see http://vigna.di.unimi.it/ftp/papers/ScrambledLinear.pdf.
+ *
+ *      The PRNG state is seeded based on the standard @c rand(), so
+ *      that applications that use @c srand() would get reproducible
+ *      results with this function as well.
+ *
+ * @todo The current implementation is not well suited for multithreaded
+ *       applications, just like the standard @c rand() used by
+ *       rand_range(), however, the algorithm does support creation of
+ *       robust parallel PRNGs, so it may be implemented, should it ever
+ *       be needed.
+ */
+extern uintmax_t te_rand_unsigned(uintmax_t min, uintmax_t max);
+
+/**
+ * Generate a random signed number in a given range.
+ *
+ * See te_rand_unsigned() for details.
+ *
+ * @param min      Minimum value (inclusive).
+ * @param max      Maximum value (inclusive).
+ *
+ * @return The next pseudo-random number between @p min and @p max.
+ */
+extern intmax_t te_rand_signed(intmax_t min, intmax_t max);
+
+/**
+ * Generate a random unsigned number in a given range
+ * such that its value modulo @p div is @p rem.
+ *
+ * @param min      Minimum value (inclusive).
+ * @param max      Maximum value (inclusive).
+ * @param div      Divisor.
+ * @param rem      Expected remainder.
+ *
+ * @return A random value such that it's equal to @p rem
+ *         modulo @p div.
+ */
+static inline uintmax_t
+te_rand_unsigned_div(uintmax_t min, uintmax_t max,
+                     unsigned int div, unsigned int rem)
+{
+    return (te_rand_unsigned(min, max) / div) * div + rem;
+}
+
+/**
+ * Generate a random signed number in a given range
+ * such that its value modulo @p div is @p rem.
+ *
+ * If @p rem is zero, the value will always be in the
+ * range between @p min and @p max. Otherwise, it may fall
+ * out of this range if @p min or @p max themselves are not
+ * equal to @p rem modulo @p div.
+ *
+ * The function takes into account that in C the remainder
+ * is negative if the quotient is negative, so if the result
+ * of the function is negative, it will actually be so that:
+ * `te_rand_signed_div(min, max, div, mod) % div == -mod`.
+ *
+ * @param min      Minimum value (inclusive).
+ * @param max      Maximum value (inclusive).
+ * @param div      Divisor.
+ * @param rem      Expected remainder.
+ *
+ * @return A random value such that it's equal to @p rem
+ *         modulo @p div.
+ */
+static inline intmax_t
+te_rand_signed_div(intmax_t min, intmax_t max,
+                   unsigned int div, unsigned int mod)
+{
+    intmax_t v = (te_rand_signed(min, max) / div) * div;
+
+    return v >= 0 ? v + mod : v - mod;
+}
+
 /**
  * Choose a random value from a range excluding some value inside
  * that range.

commit a2e91e7b9664eb57a411ba035f261e18cd893618

suites/selftest: fix PRNG selftest
It turns out that the Kolmogorov-Smirnov test that was
originally used to validate the uniformity of the PRNG
has some caveats when used on discrete distributions
that lead to an excessive number of false negatives.
The patch replaces it with a more universal Pearson's
chi-square test with its signifance level chosen in such
a way as to minimize the chance of false negative ands
still allow to catch genuine errors.

Fixes: bd996be73ceb ("lib/tools: provide a high-quality RNG")
OL-Redmine-Id: 14348
Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

suites/selftest/ts/tools/rand.c
@@ -11,7 +11,7 @@
  * @objective Testing the sanity of PRNG routines.
  *
  * The test is not a replacement for real PRNG burnout tests:
- * basically it uses Kolmogorov-Smirnov test to verify that
+ * basically it uses Pearson's chi square test to verify that
  * generated random values are uniformly distributed.
  *
  * @par Test sequence:
@@ -27,11 +27,25 @@
 #include "te_rand.h"
 #include "te_numeric.h"
 
+/*
+ * The following critical values for chi square test with
+ * significance level of 0.9999 were precalculated using
+ * SciPy's stats module. The part of the name after the
+ * `chi` prefix denotes the degrees of freedom.
+ */
+static long double chi1 = 15.136705226623606L;
+static long double chi2 = 18.420680743952584L;
+static long double chi255 = 347.6542127045896L;
+static long double chi2_63 = 9.22337205282783E+18;
+static long double chi2_64 = 1.844674409629886E+19;
+
+
 typedef struct prng_params {
     intmax_t min;
     intmax_t max;
     size_t bitstart;
     size_t bitlen;
+    long double chi;
 } prng_params;
 
 static int
@@ -46,20 +60,55 @@ compare_intmax(const void *v1, const void *v2)
     return i1 > i2 ? 1 : -1;
 }
 
+/*
+ * The following is doing Pearson's chi-square goodness-of-fit test
+ * for the uniform distribution.
+ *
+ * See https://en.wikipedia.org/wiki/Pearson%27s_chi-squared_test
+ * for an introduction to the topic.
+ */
+static void
+validate_random(const prng_params *params, unsigned int n, intmax_t seq[n])
+{
+    long double n_bins;
+    long double sum = 0.0L;
+    unsigned int span = 1;
+    unsigned int i;
+
+    if (params->bitlen == 0 || params->bitlen == TE_BITSIZE(intmax_t))
+        n_bins = (long double)params->max - (long double)params->min + 1.0L;
+    else
+        n_bins = (long double)(1ull << params->bitlen);
+
+    qsort(seq, n, sizeof(*seq), compare_intmax);
+
+    for (i = 0; i < n; i += span)
+    {
+        long double expected;
+        for (span = 1; i + span < n && seq[i + span] == seq[i]; span++)
+            ;
+        expected = (long double)n / n_bins;
+        sum += (((long double)span - expected) *
+                ((long double)span - expected)) / expected;
+    }
+
+    if (sum > params->chi)
+    {
+        TEST_VERDICT("The sampling of %zu..%zu bits of %jd..%jd "
+                     "appears non-uniform: %Lf > %Lf",
+                     params->bitstart,
+                     params->bitstart +
+                     (params->bitlen > 0 ? params->bitlen - 1 :
+                      TE_BITSIZE(intmax_t) - 1),
+                     params->min, params->max,
+                     sum, params->chi);
+    }
+}
+
 static void
 generate_random(const prng_params *params, unsigned int n, intmax_t seq[n])
 {
-    /*
-     * This is a critical value for Kolmogorov distribution
-     * assuming n > 40 for a significance level 99.5%,
-     * see
-     * e.g. https://www.york.ac.uk/depts/maths/tables/kolmogorovsmirnov.pdf
-     */
-    static const long double k995 = 1.63;
-
     unsigned int i;
-    unsigned int span;
-    long double maxd = 0.0L;
 
     /*
      * Believe it or not, but it may actually happen if a test
@@ -88,106 +137,70 @@ generate_random(const prng_params *params, unsigned int n, intmax_t seq[n])
         seq[i] = r;
     }
 
-    /*
-     * The following is doing one-sample Kolmogorov-Smirnov test
-     * to ensure the drawn samples agree with the uniform distribution.
-     *
-     * See https://en.wikipedia.org/wiki/Kolmogorov%E2%80%93Smirnov_test
-     * for an introduction to the topic.
-     */
-    qsort(seq, n, sizeof(*seq), compare_intmax);
-
-    for (i = 0; i < n; i += span)
-    {
-        /*
-         * All calculations must be with long doubles, because
-         * plain double cannot represent the full range of int64_t
-         * precisely enough.
-         */
-        long double ecdf;
-        long double ucdf;
-        long double d;
-
-        for (span = 1; i + span < n; span++)
-        {
-            if (seq[i + span] != seq[i])
-                break;
-        }
-
-        ecdf = (long double)(i + span) / (long double)n;
-
-        if (params->bitlen == 0 || params->bitlen == TE_BITSIZE(intmax_t))
-        {
-            ucdf = ((long double)seq[i] - (long double)params->min + 1.0L) /
-                ((long double)params->max - (long double)params->min + 1.0L);
-        }
-        else
-        {
-            ucdf = ((long double)seq[i] + 1.0L) /
-                (long double)(1ull << params->bitlen);
-        }
-
-        d = fabsl(ecdf - ucdf);
-        if (d > maxd)
-            maxd = d;
-    }
-
-    if (sqrtl((long double)n) * maxd > k995)
-    {
-        TEST_VERDICT("The sample is not uniform: %Lf > %Lf",
-                     sqrtl((long double)n) * maxd, k995);
-    }
-    RING("Max difference between ECDF and uniform CDF = %Lf (< %Lf)",
-         maxd, k995 / sqrtl((long double)n));
+    validate_random(params, n, seq);
 }
 
 static void
 test_random(unsigned int n)
 {
-    static const prng_params variants[] = {
+    const prng_params variants[] = {
         {
-            .min = INTMAX_MIN, .max = INTMAX_MAX
+            .min = INTMAX_MIN, .max = INTMAX_MAX,
+            .chi = chi2_64,
         },
         {
             .min = INTMAX_MIN, .max = INTMAX_MAX,
-            .bitstart = 0, .bitlen = 1
+            .bitstart = 0, .bitlen = 1,
+            .chi = chi1,
         },
         {
             .min = INTMAX_MIN, .max = INTMAX_MAX,
-            .bitstart = 0, .bitlen = 8
+            .bitstart = 0, .bitlen = 8,
+            .chi = chi255,
         },
         {
             .min = INTMAX_MIN, .max = INTMAX_MAX,
-            .bitstart = TE_BITSIZE(intmax_t) - 8, .bitlen = 8
+            .bitstart = TE_BITSIZE(intmax_t) - 8, .bitlen = 8,
+            .chi = chi255,
+
         },
         {
             .min = INTMAX_MIN, .max = INTMAX_MAX,
-            .bitstart = TE_BITSIZE(intmax_t) - 1, .bitlen = 1
+            .bitstart = TE_BITSIZE(intmax_t) - 1, .bitlen = 1,
+            .chi = chi1,
         },
         {
-            .min = 0, .max = INTMAX_MAX
+            .min = 0, .max = INTMAX_MAX,
+            .chi = chi2_63,
         },
         {
-            .min = 0, .max = 1
+            .min = 0, .max = 1,
+            .chi = chi1,
         },
         {
-            .min = -1, .max = 1
-        },
-        {
-            .min = 0, .max = UINT8_MAX
+            .min = -1, .max = 1,
+            .chi = chi2,
         },
         {
             .min = 0, .max = UINT8_MAX,
-            .bitstart = 0, .bitlen = 1
+            .chi = chi255,
         },
         {
-            .min = -1, .max = INTMAX_MAX
+            .min = 0, .max = UINT8_MAX,
+            .bitstart = 0, .bitlen = 1,
+            .chi = chi1,
         },
         {
-            .min = INTMAX_MIN, .max = 0
+            .min = -1, .max = INTMAX_MAX,
+            .chi = chi2_63,
         },
         {
-            .min = INTMAX_MIN, .max = INTMAX_MIN + 1
+            .min = INTMAX_MIN, .max = 0,
+            .chi = chi2_63,
+        },
+        {
+            .min = INTMAX_MIN, .max = INTMAX_MIN + 1,
+            .chi = chi1,
         },
     };
     unsigned int i;
@@ -211,9 +224,9 @@ static void
 test_scalar_random(unsigned int n_numbers)
 {
     unsigned int i;
-    intmax_t i8_sum = 0;
-    uintmax_t u8_sum = 0;
-    uintmax_t b_sum = 0;
+    intmax_t signed_bytes[n_numbers];
+    intmax_t unsigned_bytes[n_numbers];
+    intmax_t bools[n_numbers];
 
     for (i = 0; i < n_numbers; i++)
     {
@@ -225,23 +238,20 @@ test_scalar_random(unsigned int n_numbers)
         te_scalar_random(TE_SCALAR_TYPE_UINT8_T, &u8);
         te_scalar_random(TE_SCALAR_TYPE_BOOL, &b);
 
-        i8_sum += i8;
-        u8_sum += u8;
-        b_sum += b;
+        signed_bytes[i] = i8;
+        unsigned_bytes[i] = u8;
+        bools[i] = b;
     }
 
-    /* The thresholds below are somewhat arbitrary. */
-    if (fabs((double)i8_sum / (double)n_numbers) > 1.0)
-        TEST_VERDICT("Generator of signed bytes is skewed");
-
-    if (fabs((double)u8_sum / (double)n_numbers -
-             (double)UINT8_MAX / 2.0) > 1.0)
-    {
-        TEST_VERDICT("Generator of signed bytes is skewed");
-    }
-
-    if (fabs((double)b_sum / (double)n_numbers - 0.5) > 0.01)
-        TEST_VERDICT("Generator of booleans is skewed");
+    validate_random(&(const prng_params){.min = INT8_MIN, .max = INT8_MAX,
+                                         .chi = chi255},
+                    n_numbers, signed_bytes);
+    validate_random(&(const prng_params){.min = 0, .max = UINT8_MAX,
+                                         .chi = chi255},
+                    n_numbers, unsigned_bytes);
+    validate_random(&(const prng_params){.min = 0, .max = 1,
+                                         .chi = chi1},
+                    n_numbers, bools);
 }
 
 static void