oktetlabs/test-environment

commit 52ca52e42f8b46622f6e0733eedf11482212d76c

agent/unix: support binding to vfio-pci with driver_override
As per the link below, on Linux kernels >= 3.15, there is a new way for
binding drivers to devices. Use the interface when available.

Link: https://github.com/oktetlabs/test-environment/pull/65
Link: https://github.com/DPDK/dpdk/blob/1b3bf1128d9bda5595861814792f74b8f57160c8/usertools/dpdk-devbind.py#L376
Signed-off-by: Ivan Malov <ivan.malov@arknetworks.am>
Reviewed-by: Denis Pryazhennikov <denis.pryazhennikov@arknetworks.am>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>

agents/unix/conf/base/conf_pci.c
@@ -2057,6 +2057,48 @@ maybe_create_device(const pci_device *dev, const char *drvname)
 }
 
 static te_errno
+try_override(const pci_device *dev, const char *drv)
+{
+    te_string sys = TE_STRING_INIT;
+    te_string id = TE_STRING_INIT;
+    te_errno rc;
+    ssize_t ret;
+    int fd;
+
+    rc = format_device_address(&id, &dev->address);
+    if (rc != 0)
+    {
+        te_string_free(&id);
+        return rc;
+    }
+
+    te_string_append(&sys, "/sys/bus/pci/devices/%s/driver_override", id.ptr);
+    te_string_free(&id);
+
+    if (access(sys.ptr, F_OK) != 0)
+    {
+        rc = (errno == ENOENT) ? TE_RC(TE_TA_UNIX, TE_EOPNOTSUPP) :
+                                 TE_OS_RC(TE_TA_UNIX, errno);
+        te_string_free(&sys);
+        return rc;
+    }
+
+    fd = open(sys.ptr, O_WRONLY);
+
+    rc = TE_OS_RC(TE_TA_UNIX, errno);
+    te_string_free(&sys);
+
+    if (fd < 0)
+        return rc;
+
+    ret = write(fd, drv, strlen(drv));
+    rc = (ret < 0) ? TE_OS_RC(TE_TA_UNIX, errno) : 0;
+
+    close(fd);
+    return rc;
+}
+
+static te_errno
 pci_driver_set(unsigned int gid, const char *oid, const char *value,
                const char *unused1, const char *unused2,
                const char *addr_str)
@@ -2102,15 +2144,31 @@ pci_driver_set(unsigned int gid, const char *oid, const char *value,
         rc = maybe_load_driver(value);
         if (rc != 0)
             return rc;
-        for (i = 0; i < TE_ARRAY_LEN(generic_drivers) && !is_gen_driver; i++)
-            is_gen_driver = (strcmp(generic_drivers[i], value) == 0);
 
-        if (is_gen_driver)
+        /* Available on kernels >= 3.15. */
+        rc = try_override(dev, value);
+
+        if (TE_RC_GET_ERROR(rc) == TE_EOPNOTSUPP)
         {
-            rc = let_generic_driver_know_pci_device(dev, value);
-            if (rc != 0)
-                return rc;
+            for (i = 0;
+                 i < TE_ARRAY_LEN(generic_drivers) && !is_gen_driver;
+                 i++)
+            {
+                is_gen_driver = (strcmp(generic_drivers[i], value) == 0);
+            }
+
+            if (is_gen_driver)
+            {
+                rc = let_generic_driver_know_pci_device(dev, value);
+                if (rc != 0)
+                    return rc;
+            }
         }
+        else if (rc != 0)
+        {
+            return rc;
+        }
+
         rc = bind_pci_device(dev, value);
         if (rc != 0)
             return rc;

commit 1d9d52ea2ea33556ea43475c9a0620f698a9bf4f

agent/unix: clear PCI driver override when unbinding
Leaving the driver override after unbinding the device driver can
prevent automatic binding when a module with a compatible driver is
loaded later. This can lead to surprises to the end user of the test
suite. Fix the problem by always explicitly clearing the override.

Fixes: 52ca52e42f8b ("agent/unix: support binding to vfio-pci with driver_override")
Signed-off-by: Viacheslav Galaktionov <viacheslav.galaktionov@arknetworks.am>
Reviewed-by: Ivan Malov <ivan.malov@arknetworks.am>

agents/unix/conf/base/conf_pci.c
@@ -2091,6 +2091,10 @@ try_override(const pci_device *dev, const char *drv)
     if (fd < 0)
         return rc;
 
+    /* Driver override is cleared only when a newline symbol is written. */
+    if (*drv == '\0')
+        drv = "\n";
+
     ret = write(fd, drv, strlen(drv));
     rc = (ret < 0) ? TE_OS_RC(TE_TA_UNIX, errno) : 0;
 
@@ -2182,6 +2186,13 @@ pci_driver_set(unsigned int gid, const char *oid, const char *value,
 
         rc = maybe_create_device(dev, value);
         if (rc != 0)
+            return rc;
+    }
+    else
+    {
+        /* Clear the driver override we might have set previously. */
+        rc = try_override(dev, value);
+        if (rc != 0 && TE_RC_GET_ERROR(rc) != TE_EOPNOTSUPP)
             return rc;
     }
 