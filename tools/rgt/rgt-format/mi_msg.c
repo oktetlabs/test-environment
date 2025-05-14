oktetlabs/test-environment

commit cf714d0e70f61ce3157c1bcc46a00b5e17a9aad1

engine/tester: save TRC expectations status in MI message
Save TRC expectations status in 'test_end' MI message to make it
clear whether 'expected' field is absent because obtained result
is expected or because there is no expected results.

OL-Redmine-Id: 13965
Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

tools/rgt/rgt-format/mi_msg.c
@@ -1065,6 +1065,7 @@ te_rgt_parse_mi_test_end_message(te_rgt_mi *mi)
     json_t *obtained = NULL;
     json_t *exp_key = NULL;
     json_t *exp_notes = NULL;
+    json_t *exp_status = NULL;
     json_t *expected = NULL;
     json_t *result = NULL;
     json_t *tags_expr = NULL;
@@ -1089,7 +1090,8 @@ te_rgt_parse_mi_test_end_message(te_rgt_mi *mi)
 
     data->plan_id = -1;
     ret = json_unpack_ex(root, &err, JSON_STRICT,
-                         "{s:i, s:i, s?i, s?s, s?o, s?o, s?o, s?o, s?o, s?o}",
+                         "{s:i, s:i, s?i, s?s, s?o, s?o, s?o, "
+                         "s?o, s?o, s?o, s?o}",
                          "id", &data->node_id,
                          "parent", &data->parent_id,
                          "plan_id", &data->plan_id,
@@ -1114,6 +1116,7 @@ te_rgt_parse_mi_test_end_message(te_rgt_mi *mi)
         !check_json_type(mi, &tags_expr, JSON_STRING, "tags_expr") ||
         !check_json_type(mi, &exp_key, JSON_STRING, "exp_key") ||
         !check_json_type(mi, &exp_notes, JSON_STRING, "exp_notes") ||
+        !check_json_type(mi, &exp_status, JSON_STRING, "exp_status") ||
         !check_json_type(mi, &error, JSON_STRING, "error"))
         goto cleanup;
 
@@ -1144,6 +1147,8 @@ te_rgt_parse_mi_test_end_message(te_rgt_mi *mi)
         data->exp_key = json_string_value(exp_key);
     if (exp_notes != NULL)
         data->exp_notes = json_string_value(exp_notes);
+    if (exp_status != NULL)
+        data->exp_status = json_string_value(exp_status);
     if (error != NULL)
         data->error = json_string_value(error);
 

engine/tester/run.c
@@ -2120,6 +2120,7 @@ log_test_result(test_id parent, tester_test_result *result, int plan_id)
     const trc_exp_result_entry *exp_entry = NULL;
     const char *exp_key = NULL;
     const char *exp_notes = NULL;
+    const char *exp_status = NULL;
 
     if (result->exp_result != NULL)
     {
@@ -2129,6 +2130,8 @@ log_test_result(test_id parent, tester_test_result *result, int plan_id)
         exp_key = exp_result->key;
         exp_notes = exp_result->notes;
     }
+
+    exp_status = trc_verdict2str(result->exp_status);
 
     /* If the result was unexpected, pack the expected results */
     if (exp_result != NULL && exp_entry == NULL)
@@ -2186,7 +2189,8 @@ log_test_result(test_id parent, tester_test_result *result, int plan_id)
      * jansson-2.10, which is currently the newest version available on
      * CentOS/RHEL-7.x
      */
-    json = json_pack("{s:i, s:i, s:i, s:s?, s:o?, s:s?, s:s?, s:s?, s:o?}",
+    json = json_pack("{s:i, s:i, s:i, s:s?, s:o?, s:s?, s:s?, "
+                     "s:s?, s:s?, s:o?}",
                      "id", result->id,
                      "parent", parent,
                      "plan_id", plan_id,
@@ -2195,6 +2199,7 @@ log_test_result(test_id parent, tester_test_result *result, int plan_id)
                      "tags_expr", tags,
                      "exp_key", exp_key,
                      "exp_notes", exp_notes,
+                     "exp_status", exp_status,
                      "expected", expected);
 
     if (json == NULL)

include/te_test_result.h
@@ -114,6 +114,30 @@ typedef enum trc_verdict {
                                  expected results */
 } trc_verdict;
 
+/**
+ * Get string representation of trc_verdict.
+ *
+ * @param v   trc_verdict value.
+ *
+ * @return String representation.
+ */
+static inline const char *
+trc_verdict2str(trc_verdict v)
+{
+    switch (v)
+    {
+        case TRC_VERDICT_UNKNOWN:
+            return "UNKNOWN";
+
+        case TRC_VERDICT_UNEXPECTED:
+            return "UNEXPECTED";
+
+        case TRC_VERDICT_EXPECTED:
+            return "EXPECTED";
+    }
+
+    return "<UNEXPECTED_VALUE>";
+}
 
 /** Initialize test result by defaults. */
 static inline void

tools/rgt/rgt-core/log_msg.c
@@ -215,7 +215,7 @@ rgt_process_tester_control_message_json(log_msg *msg)
          */
         err_code = json_unpack_ex(msg_json, &json_error, JSON_STRICT,
                                   "{s:i, s:i, s?s, s?o, s?o, s?o, "
-                                  "s?o, s?o, s?o, s?o}",
+                                  "s?o, s?o, s?o, s?o, s?o}",
                                   "id",        &node_id,
                                   "parent",    &parent_id,
                                   "status",    &status,
@@ -229,6 +229,7 @@ rgt_process_tester_control_message_json(log_msg *msg)
                                   "plan_id",   &ignored,
                                   "exp_key",   &ignored,
                                   "exp_notes", &ignored,
+                                  "exp_status", &ignored,
                                   "expected",  &ignored,
                                   "tags_expr", &ignored);
         if (err_code != 0)

tools/rgt/rgt-format/mi_msg.h
@@ -168,6 +168,12 @@ typedef struct te_rgt_mi_test_end {
     const char            *exp_notes;    /**< Expected results notes */
     te_rgt_mi_test_result *expected;     /**< Array of expected results */
     size_t                 expected_num; /**< Number of expected results */
+
+    /**
+     * TRC expectations status of obtained result
+     * (UNKNOWN/UNEXPECTED/EXPECTED)
+     */
+    const char            *exp_status;
 } te_rgt_mi_test_end;
 
 /** Description of a TRC tag */

commit 41ecbc03688b60884bec05dc3af626bcf67ac3e9

tools/rgt: add expected status to parsing test end message
Signed-off-by: Nikolai Kosovskii <nikolai.kosovskii@arknetworks.am>
Fixes: cf714d0e70f6 ("engine/tester: save TRC expectations status in MI message")

tools/rgt/rgt-format/mi_msg.c
@@ -1099,6 +1099,7 @@ te_rgt_parse_mi_test_end_message(te_rgt_mi *mi)
                          "obtained", &obtained,
                          "exp_key", &exp_key,
                          "exp_notes", &exp_notes,
+                         "exp_status", &exp_status,
                          "expected", &expected,
                          "tags_expr", &tags_expr,
                          "error", &error);