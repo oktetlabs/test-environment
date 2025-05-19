oktetlabs/test-environment

commit 9cc5081ae6debcb0cf05934d3287ef82f670a3fd

tools/rgt: do not pass hexadecimal floats to JavaScript
Hexadecimal floats are not supported by JavaScript but
were inserted into JS data arrays when binaries multipliers
were used. A self test is added to verify that all types
of multipliers are rendered correctly.

OL-Redmine-Id: 13674
Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Damir Mansurov <damir.mansurov@oktetlabs.ru>

tools/rgt/rgt-format/xml2html-multi/xml2html-multi.c
@@ -19,6 +19,7 @@
 #include "te_raw_log.h"
 #include "te_dbuf.h"
 #include "te_string.h"
+#include "te_str.h"
 
 #include "capture.h"
 
@@ -781,13 +782,29 @@ print_mi_meas_param_vals_array(FILE *fd, te_rgt_mi_meas_param *param)
 
         if (value->defined && value->specified)
         {
+            double multiplier_value = 1.0;
+
             if (!first_val)
                 fprintf(fd, ", ");
 
-            fprintf(fd, "%f * %s", value->value,
-                    (value->multiplier != NULL &&
-                     *(value->multiplier) != '\0' ?
-                     value->multiplier : "1"));
+            /*
+             * The multiplier may be a hexadecimal float which
+             * is not understood by JavaScript, so here we re-parse
+             * it and always print as an ordinary float.
+             */
+            if (value->multiplier != NULL && *value->multiplier != '\0')
+            {
+                te_errno rc = te_strtod(value->multiplier, &multiplier_value);
+
+                if (rc != 0)
+                {
+                    fprintf(stderr, "Invalid multiplier value '%s'",
+                            value->multiplier);
+                    exit(1);
+                }
+            }
+
+            fprintf(fd, "%f * %f", value->value, multiplier_value);
             first_val = FALSE;
         }
     }

suites/selftest/conf/trc/minimal.trc.xml
@@ -33,6 +33,14 @@
                 <notes/>
             </iter>
         </test>
+        <test name="mi_meas_multipliers" type="script">
+            <objective>Demo of using all possible measurement multipliers.</objective>
+            <notes/>
+            <iter result="PASSED">
+                <arg name="n_values" />
+                <notes/>
+            </iter>
+        </test>
         <test name="verdict" type="script">
             <objective/>
             <notes/>

suites/selftest/ts/minimal/meson.build
@@ -7,6 +7,7 @@ tests = [
     'helloworld',
     'mi_meas_line_graph',
     'mi_meas_highrange',
+    'mi_meas_multipliers',
     'parameters',
     'verdict'
 ]

suites/selftest/ts/minimal/mi_meas_multipliers.c
@@ -1,0 +1,96 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (C) 2024 OKTET Labs Ltd. All rights reserved. */
+/** @file
+ * @brief Plotting a graph with different multiplier values.
+ *
+ * The test causes a graph to be plotted with all known measurement
+ * multiplier values.
+ */
+/** @page minimal_mi_meas_multipliers Test for MI measurement multipliers.
+ *
+ * @objective Demo of using all possible measurement multipliers.
+ *
+ * @par Test sequence:
+ *
+ */
+
+#ifndef DOXYGEN_TEST_SPEC
+
+#define TE_TEST_NAME    "mi_meas_multipliers"
+
+#include "te_config.h"
+
+#include "tapi_test.h"
+#include "te_str.h"
+#include "te_mi_log.h"
+
+int
+main(int argc, char **argv)
+{
+    static const char *multi_names[] = {
+        [TE_MI_MEAS_MULTIPLIER_NANO] = "Nano",
+        [TE_MI_MEAS_MULTIPLIER_MICRO] = "Micro",
+        [TE_MI_MEAS_MULTIPLIER_MILLI] = "Milli",
+        [TE_MI_MEAS_MULTIPLIER_PLAIN] = "Plain",
+        [TE_MI_MEAS_MULTIPLIER_KILO] = "Kilo",
+        [TE_MI_MEAS_MULTIPLIER_KIBI] = "Kibi",
+        [TE_MI_MEAS_MULTIPLIER_MEGA] = "Mega",
+        [TE_MI_MEAS_MULTIPLIER_MEBI] = "Mebi",
+        [TE_MI_MEAS_MULTIPLIER_GIGA] = "Giga",
+        [TE_MI_MEAS_MULTIPLIER_GIBI] = "Gibi",
+    };
+    te_mi_logger *logger;
+    unsigned int n_values = 0;
+    te_mi_meas_multiplier multi;
+
+    TEST_START;
+    TEST_GET_UINT_PARAM(n_values);
+
+    TEST_STEP("Create a MI logger.");
+    CHECK_RC(te_mi_logger_meas_create("High range", &logger));
+
+    TEST_STEP("Add measurements");
+    for (multi = TE_MI_MEAS_MULTIPLIER_NANO;
+         multi < TE_MI_MEAS_MULTIPLIER_END;
+         multi++)
+    {
+        unsigned int i;
+        for (i = 0; i < n_values; i++)
+        {
+            te_mi_logger_add_meas(logger, NULL, TE_MI_MEAS_PPS,
+                                  multi_names[multi],
+                                  TE_MI_MEAS_AGGR_SINGLE,
+                                  (double)i, multi);
+        }
+    }
+
+    TEST_STEP("Add a line-graph view to show different multipliers");
+    te_mi_logger_add_meas_view(logger, NULL, TE_MI_MEAS_VIEW_LINE_GRAPH,
+                               "graph1", "Kinds of multipliers");
+    te_mi_logger_meas_graph_axis_add_name(
+                                      logger, NULL,
+                                      TE_MI_MEAS_VIEW_LINE_GRAPH, "graph1",
+                                      TE_MI_GRAPH_AXIS_X,
+                                      TE_MI_GRAPH_AUTO_SEQNO);
+    for (multi = TE_MI_MEAS_MULTIPLIER_NANO;
+         multi < TE_MI_MEAS_MULTIPLIER_END;
+         multi++)
+    {
+        te_mi_logger_meas_graph_axis_add_name(logger, NULL,
+                                              TE_MI_MEAS_VIEW_LINE_GRAPH,
+                                              "graph1",
+                                              TE_MI_GRAPH_AXIS_Y,
+                                              multi_names[multi]);
+    }
+
+    TEST_STEP("Log MI measurement artifact");
+    te_mi_logger_destroy(logger);
+
+    TEST_SUCCESS;
+
+cleanup:
+
+    TEST_END;
+}
+
+#endif /* !DOXYGEN_TEST_SPEC */

suites/selftest/ts/minimal/package.xml
@@ -25,6 +25,12 @@
             </arg>
         </run>
         <run>
+            <script name="mi_meas_multipliers"/>
+            <arg name="n_values">
+                <value>10</value>
+            </arg>
+        </run>
+        <run>
             <script name="env"/>
             <arg name="env">
                 <value>{{{'pco_iut':IUT},if:'iut_if'}}</value>

commit 494b1d5d7ecd38d80af3d02140608f0f246069be

tools/rgt: fix nano multiplier processing in MI graph view
Explicitly set precision for multipliers when printing measurement
data for JS array. The default precision (6) doesn't work for "1e-9"
multipliers. Use 9 instead.

Fixes: 9cc5081ae6de ("tools/rgt: do not pass hexadecimal floats to JavaScript")
Signed-off-by: Sergey Nikitin <sergey.nikitin@oktet.tech>

tools/rgt/rgt-format/xml2html-multi/xml2html-multi.c
@@ -809,7 +809,7 @@ print_mi_meas_param_vals_array(FILE *fd, te_rgt_mi_meas_param *param)
                 }
             }
 
-            fprintf(fd, "%f * %f", value->value, multiplier_value);
+            fprintf(fd, "%f * %.9f", value->value, multiplier_value);
             first_val = false;
         }
     }