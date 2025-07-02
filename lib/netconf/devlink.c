oktetlabs/test-environment

commit eb9c55050ccff76ae5f696765c21d3d22beba77d

netconf: add minimal eswitch support
Some NICs require users to activate switchdev mode before it allows any
representors to be created. Implement the necessary devlink interface:
querying and setting eswitch mode.

Link: https://github.com/oktetlabs/test-environment/pull/24
Signed-off-by: Viacheslav Galaktionov <viacheslav.galaktionov@arknetworks.am>
Reviewed-by: Ivan Malov <ivan.malov@arknetworks.am>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Acked-by: Artem Andreev <artem.andreev@oktetlabs.ru>

lib/netconf/devlink.c
@@ -168,6 +168,208 @@ netconf_devlink_get_info(netconf_handle nh, const char *bus,
 
 #endif /* HAVE_DECL_DEVLINK_CMD_INFO_GET */
 
+#if HAVE_DECL_DEVLINK_CMD_ESWITCH_GET
+
+/* Convert native configuration mode to netconf constant */
+static netconf_devlink_param_cmode
+devlink_eswitch_mode_h2netconf(uint16_t val)
+{
+#define CHECK_MODE(_name) \
+    case DEVLINK_ESWITCH_MODE_ ## _name:                 \
+        return NETCONF_DEVLINK_ESWITCH_MODE_ ## _name
+
+    switch (val)
+    {
+        CHECK_MODE(LEGACY);
+        CHECK_MODE(SWITCHDEV);
+
+        default:
+            return NETCONF_DEVLINK_ESWITCH_MODE_UNDEF;
+    }
+#undef CHECK_MODE
+}
+
+/* Process attributes of DEVLINK_CMD_ESWITCH_GET message */
+static te_errno
+eswitch_attr_cb(struct nlattr *na, void *cb_data)
+{
+    netconf_devlink_eswitch *eswitch = cb_data;
+    uint16_t res;
+
+    switch (na->nla_type)
+    {
+        case DEVLINK_ATTR_ESWITCH_MODE:
+            netconf_get_uint16_attr(na, &res);
+            eswitch->mode = devlink_eswitch_mode_h2netconf(res);
+            break;
+    }
+
+    return 0;
+}
+
+/* Process device information message received from netlink */
+static int
+eswitch_cb(struct nlmsghdr *h, netconf_list *list, void *cookie)
+{
+    netconf_devlink_eswitch *eswitch;
+    te_errno rc = 0;
+
+    UNUSED(cookie);
+
+    if (netconf_list_extend(list, NETCONF_NODE_DEVLINK_ESWITCH) != 0)
+        return -1;
+
+    eswitch = &(list->tail->data.devlink_eswitch);
+
+    rc = netconf_gn_process_attrs(h, eswitch_attr_cb, eswitch);
+    if (rc != 0)
+        return -1;
+
+    return 0;
+}
+
+/* See description in netconf.h */
+te_errno
+netconf_devlink_get_eswitch(netconf_handle nh, const char *bus,
+                            const char *dev, netconf_list **list)
+{
+    int os_rc;
+    te_errno rc = 0;
+    char req[NETCONF_MAX_REQ_LEN] = { 0, };
+    struct nlmsghdr *h;
+    uint16_t req_flags = NLM_F_REQUEST;
+    netconf_list *list_ptr = NULL;
+
+    if (bus == NULL || dev == NULL)
+    {
+        if (dev != bus)
+        {
+            ERROR("%s(): either specify both bus and dev or none of them",
+                  __FUNCTION__);
+            return TE_EINVAL;
+        }
+
+        req_flags |= NLM_F_DUMP;
+    }
+
+    GET_CHECK_DEVLINK_FAMILY(nh);
+
+    h = (struct nlmsghdr *)req;
+
+    rc = netconf_gn_init_hdrs(req, sizeof(req), devlink_family, req_flags,
+                              DEVLINK_CMD_ESWITCH_GET, DEVLINK_GENL_VERSION,
+                              nh);
+    if (rc != 0)
+        return rc;
+
+    if (bus != NULL)
+    {
+        rc = netconf_append_attr(req, sizeof(req), DEVLINK_ATTR_BUS_NAME,
+                                 bus, strlen(bus) + 1);
+        if (rc != 0)
+            return rc;
+
+        rc = netconf_append_attr(req, sizeof(req), DEVLINK_ATTR_DEV_NAME,
+                                 dev, strlen(dev) + 1);
+        if (rc != 0)
+            return rc;
+    }
+
+    list_ptr = TE_ALLOC(sizeof(*list_ptr));
+    if (list_ptr == NULL)
+        return TE_ENOMEM;
+
+    os_rc = netconf_talk(nh, req, h->nlmsg_len, eswitch_cb, NULL, list_ptr);
+    if (os_rc != 0)
+    {
+        rc = te_rc_os2te(errno);
+        netconf_list_free(list_ptr);
+    }
+    else
+    {
+        *list = list_ptr;
+    }
+
+    return rc;
+}
+
+#endif /* HAVE_DECL_DEVLINK_CMD_ESWITCH_GET */
+
+#if HAVE_DECL_DEVLINK_CMD_ESWITCH_SET
+
+/* Convert native configuration mode from netconf constant */
+static te_errno
+devlink_eswitch_mode_netconf2h(uint8_t mode, uint16_t *val)
+{
+#define CHECK_MODE(_name) \
+    case NETCONF_DEVLINK_ESWITCH_MODE_ ## _name:         \
+        *val = DEVLINK_ESWITCH_MODE_ ## _name;           \
+        break
+
+    switch (mode)
+    {
+        CHECK_MODE(LEGACY);
+        CHECK_MODE(SWITCHDEV);
+
+        default:
+            return TE_ENOENT;
+    }
+
+    return 0;
+#undef CHECK_MODE
+}
+
+/* See description in netconf.h */
+te_errno
+netconf_devlink_eswitch_mode_set(netconf_handle nh, const char *bus,
+                                 const char *dev,
+                                 netconf_devlink_eswitch_mode mode)
+{
+    int os_rc;
+    te_errno rc = 0;
+    char req[NETCONF_MAX_REQ_LEN] = { 0, };
+    struct nlmsghdr *h;
+    uint16_t native_mode;
+
+    GET_CHECK_DEVLINK_FAMILY(nh);
+
+    h = (struct nlmsghdr *)req;
+
+    rc = devlink_eswitch_mode_netconf2h(mode, &native_mode);
+    if (rc != 0)
+        return rc;
+
+    rc = netconf_gn_init_hdrs(req, sizeof(req), devlink_family,
+                              NLM_F_REQUEST | NLM_F_ACK,
+                              DEVLINK_CMD_ESWITCH_SET, DEVLINK_GENL_VERSION,
+                              nh);
+    if (rc != 0)
+        return rc;
+
+    rc = netconf_append_attr(req, sizeof(req), DEVLINK_ATTR_BUS_NAME,
+                             bus, strlen(bus) + 1);
+    if (rc != 0)
+        return rc;
+
+    rc = netconf_append_attr(req, sizeof(req), DEVLINK_ATTR_DEV_NAME,
+                             dev, strlen(dev) + 1);
+    if (rc != 0)
+        return rc;
+
+    rc = netconf_append_attr(req, sizeof(req), DEVLINK_ATTR_ESWITCH_MODE,
+                             &native_mode, sizeof(native_mode));
+    if (rc != 0)
+        return rc;
+
+    os_rc = netconf_talk(nh, req, h->nlmsg_len, NULL, NULL, NULL);
+    if (os_rc != 0)
+        rc = te_rc_os2te(errno);
+
+    return rc;
+}
+
+#endif /* HAVE_DECL_DEVLINK_CMD_ESWITCH_SET */
+
 #if HAVE_DECL_DEVLINK_CMD_PARAM_GET
 
 /*
@@ -793,4 +995,31 @@ netconf_devlink_param_value_data_mv(
 
     memcpy(dst, src, sizeof(*src));
     memset(src, 0, sizeof(*src));
+}
+
+/* See description in netconf.h */
+const char *
+netconf_devlink_eswitch_mode_netconf2str(netconf_devlink_eswitch_mode mode)
+{
+    switch (mode)
+    {
+        case NETCONF_DEVLINK_ESWITCH_MODE_LEGACY:
+            return "legacy";
+        case NETCONF_DEVLINK_ESWITCH_MODE_SWITCHDEV:
+            return "switchdev";
+        default:
+            return "<unknown>";
+    }
+}
+
+/* See description in netconf.h */
+netconf_devlink_eswitch_mode
+netconf_devlink_eswitch_mode_str2netconf(const char *mode)
+{
+    if (strcmp(mode, "legacy") == 0)
+        return NETCONF_DEVLINK_ESWITCH_MODE_LEGACY;
+    else if (strcmp(mode, "switchdev") == 0)
+        return NETCONF_DEVLINK_ESWITCH_MODE_SWITCHDEV;
+
+    return NETCONF_DEVLINK_ESWITCH_MODE_UNDEF;
 }

lib/netconf/meson.build
@@ -62,6 +62,8 @@ if cc.has_header('linux/devlink.h')
     conf_data.set('HAVE_LINUX_DEVLINK_H', 1)
 
     devlink_cmds = [
+        'DEVLINK_CMD_ESWITCH_GET',
+        'DEVLINK_CMD_ESWITCH_SET',
         'DEVLINK_CMD_INFO_GET',
         'DEVLINK_CMD_PARAM_GET',
         'DEVLINK_CMD_PARAM_SET',

lib/netconf/netconf.h
@@ -383,6 +383,38 @@ typedef struct netconf_devlink_param {
     netconf_devlink_param_value values[NETCONF_DEVLINK_PARAM_CMODES];
 } netconf_devlink_param;
 
+typedef enum netconf_devlink_eswitch_mode {
+    NETCONF_DEVLINK_ESWITCH_MODE_LEGACY,    /**< Legacy eswitch mode */
+    NETCONF_DEVLINK_ESWITCH_MODE_SWITCHDEV, /**< Switchdev eswitch mode */
+    /* This should be the last in enum */
+    NETCONF_DEVLINK_ESWITCH_MODE_UNDEF,     /**< Not defined */
+} netconf_devlink_eswitch_mode;
+
+/** Information about device eswitch obtained from devlink */
+typedef struct netconf_devlink_eswitch {
+    netconf_devlink_eswitch_mode mode;
+} netconf_devlink_eswitch;
+
+/**
+ * Get string name of device eswitch mode.
+ *
+ * @param mode          Eswitch mode.
+ *
+ * @return Pointer to statically allocated string.
+ */
+extern const char *netconf_devlink_eswitch_mode_netconf2str(
+                                            netconf_devlink_eswitch_mode mode);
+
+/**
+ * Parse name of device eswitch mode.
+ *
+ * @param mode          Name to parse.
+ *
+ * @return One of the values from netconf_devlink_eswitch_mode.
+ */
+extern netconf_devlink_eswitch_mode netconf_devlink_eswitch_mode_str2netconf(
+                                            const char *mode);
+
 /** Type of nodes in the list */
 typedef enum netconf_node_type {
     NETCONF_NODE_UNSPEC,                /**< Unspecified */
@@ -404,6 +436,8 @@ typedef enum netconf_node_type {
     NETCONF_NODE_DEVLINK_INFO,          /**< Device information obtained
                                              from devlink */
     NETCONF_NODE_DEVLINK_PARAM,         /**< Device parameters data obtained
+                                             from devlink */
+    NETCONF_NODE_DEVLINK_ESWITCH,       /**< Eswitch information obtained
                                              from devlink */
 } netconf_node_type;
 
@@ -429,6 +463,7 @@ typedef struct netconf_node {
 
         netconf_devlink_info     devlink_info;
         netconf_devlink_param    devlink_param;
+        netconf_devlink_eswitch  devlink_eswitch;
     } data;                             /**< Network data */
     struct netconf_node  *next;         /**< Next node of the list */
     struct netconf_node  *prev;         /**< Previous node of the list */
@@ -1113,6 +1148,42 @@ extern te_errno netconf_devlink_get_info(netconf_handle nh,
                                          const char *bus,
                                          const char *dev,
                                          netconf_list **list);
+
+/**
+ * Get device eswitch information from devlink.
+ *
+ * @note @p bus and @p dev should be either both @c NULL (to retrieve
+ *       information about all devices) or not @c NULL (to retrieve
+ *       information about specific device).
+ *
+ * @param nh        Netconf session handle.
+ * @param bus       Bus name.
+ * @param dev       Device name (PCI address for PCI device).
+ * @param list      Where to save pointer to the list of retrieved
+ *                  device data (caller should release it).
+ *
+ * @return Status code.
+ */
+extern te_errno netconf_devlink_get_eswitch(netconf_handle nh,
+                                            const char *bus,
+                                            const char *dev,
+                                            netconf_list **list);
+
+/**
+ * Set eswitch mode for a device via devlink.
+ *
+ * @param nh        Netconf session handle.
+ * @param bus       Bus name.
+ * @param dev       Device name (PCI address for PCI device).
+ * @param mode      Eswitch mode to set.
+ *
+ * @return Status code.
+ */
+extern te_errno netconf_devlink_eswitch_mode_set(
+                            netconf_handle nh,
+                            const char *bus,
+                            const char *dev,
+                            netconf_devlink_eswitch_mode mode);
 
 /**
  * Get list of supported device parameters from devlink.

commit b4b10eda100f213c9c3eeea99fe941ec4d8a34fe

lib/netconf: fix unused variable warning
If headers are available, but some functions are not declared
devlink_family may be unused and compiler warns about it.
Define it if and only if at least one of its user is built.

Fixes: afaada7087af ("Import the first public release")
Fixes: eb9c55050ccf ("netconf: add minimal eswitch support")
Signed-off-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
Reviewed-by: Elena Vengerova <elena.vengerova@oktetlabs.ru>

lib/netconf/devlink.c
@@ -22,6 +22,12 @@
 #include "linux/genetlink.h"
 #include "linux/devlink.h"
 
+#if HAVE_DECL_DEVLINK_CMD_INFO_GET || \
+    HAVE_DECL_DEVLINK_CMD_ESWITCH_GET || \
+    HAVE_DECL_DEVLINK_CMD_ESWITCH_SET || \
+    HAVE_DECL_DEVLINK_CMD_PARAM_GET || \
+    HAVE_DECL_DEVLINK_CMD_PARAM_SET
+
 /*
  * Devlink family ID. It can be different on different hosts and
  * should be determined with CTRL_CMD_GETFAMILY request.
@@ -47,6 +53,8 @@ static int devlink_family = -1;
             devlink_family = _family;                             \
         }                                                         \
     } while (0)
+
+#endif
 
 #if HAVE_DECL_DEVLINK_CMD_INFO_GET
 