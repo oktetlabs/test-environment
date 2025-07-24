oktetlabs/test-environment

commit 7b9e6e2bc93a007eb252172a65650d79ae78ca81

engine/tester: relax ordering in script specification
Allow any order of 'req', 'objective' and 'execute' in script section in
package.xml.

Signed-off-by: Yurij Plotnikov <yurij.plotnikov@arknetworks.am>
Reviewed-by: Viacheslav Galaktionov <viacheslav.galaktionov@arknetworks.am>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>

engine/tester/config_parse.c
@@ -1011,6 +1011,8 @@ get_script(xmlNodePtr node, tester_cfg *cfg, test_script *script)
 {
     te_errno            rc;
     const test_info    *ti;
+    bool                objective_found = false;
+    bool                execute_found = false;
 
     /* 'name' is mandatory */
     script->name = XML2CHAR(xmlGetProp(node, CONST_CHAR2XML("name")));
@@ -1027,54 +1029,65 @@ get_script(xmlNodePtr node, tester_cfg *cfg, test_script *script)
 
     node = xmlNodeChildren(node);
 
-    /* Get optional 'objective' */
-    if (node != NULL &&
-        (rc = get_node_with_text_content(&node, "objective",
-                                         &script->objective)) != 0)
+    while (node != NULL)
     {
-        if (rc != TE_ENOENT)
-            return rc;
+        /* Get optional 'objective' */
+        if (xmlStrcmp(node->name, CONST_CHAR2XML("objective")) == 0)
+        {
+            if (objective_found)
+            {
+                ERROR("Multiple 'objective' elements in script '%s'",
+                      script->name);
+                return TE_RC(TE_TESTER, TE_EINVAL);
+            }
+            if ((rc = get_node_with_text_content(&node, "objective",
+                                                 &script->objective)) != 0)
+                return rc;
+            objective_found = true;
+
+            ti = find_test_info(cfg->cur_pkg->ti, script->name);
+            if (ti != NULL)
+            {
+                if (script->objective == NULL)
+                {
+                    script->objective = TE_STRDUP(ti->objective);
+                }
+                script->page = TE_STRDUP(ti->page);
+            }
+            continue;
+        }
+
+        /* Get optional test requirements */
+        if (xmlStrcmp(node->name, CONST_CHAR2XML("req")) == 0)
+        {
+            rc = alloc_and_get_requirement(node, &script->reqs, false);
+            if (rc != 0)
+            {
+                ERROR("Failed to get requirements of the script '%s'",
+                      script->name);
+                return rc;
+            }
+            node = xmlNodeNext(node);
+            continue;
+        }
+        /* Get optional 'execute' parameter */
+        if (xmlStrcmp(node->name, CONST_CHAR2XML("execute")) == 0)
+        {
+            if (execute_found)
+            {
+                ERROR("Munltiple 'execute' elements in script '%s'",
+                      script->name);
+                return TE_RC(TE_TESTER, TE_EINVAL);
+            }
+            execute_found = true;
+            script->execute = XML2CHAR_DUP(node->content);
+            node = xmlNodeNext(node);
+            continue;
+        }
+        break;
     }
 
-    ti = find_test_info(cfg->cur_pkg->ti, script->name);
-    if (ti != NULL)
-    {
-        if (ti->objective != NULL && script->objective == NULL)
-        {
-            script->objective = strdup(ti->objective);
-            if (script->objective == NULL)
-            {
-                ERROR("strdup(%s) failed", ti->objective);
-                return TE_RC(TE_TESTER, TE_ENOMEM);
-            }
-        }
-        if (ti->page != NULL)
-        {
-            script->page = strdup(ti->page);
-            if (script->page == NULL)
-            {
-                ERROR("strdup(%s) failed", ti->page);
-                return TE_RC(TE_TESTER, TE_ENOMEM);
-            }
-        }
-    }
-
-    /* Get optional set of tested 'requirement's */
-    rc = get_requirements(&node, &script->reqs, false);
-    if (rc != 0)
-    {
-        ERROR("Failed to get requirements of the script '%s'",
-              script->name);
-        return rc;
-    }
-    /* Get optional 'execute' parameter */
-    if (node != NULL &&
-        xmlStrcmp(node->name, CONST_CHAR2XML("execute")) == 0)
-    {
-        script->execute = XML2CHAR_DUP(node->content);
-        node = xmlNodeNext(node);
-    }
-    else
+    if (!execute_found)
     {
         script->execute = name_to_path(cfg, script->name, false);
     }

doc/xsd/tester_defs.xsd
@@ -341,31 +341,33 @@
             </documentation>
         </annotation>
         <sequence>
-            <element name="description" type="string" minOccurs="0">
-                <annotation>
-                    <documentation>
-                        Description of the test provided by the script.
-                    </documentation>
-                </annotation>
-            </element>
-            <element name="req" type="Requirement" minOccurs="0"
-                                                   maxOccurs="unbounded">
-                <annotation>
-                    <documentation>
-                        Complete set of requiremetns verified by the
-                        script.
-                    </documentation>
-                </annotation>
-            </element>
-            <element name="execute" type="string" minOccurs="0">
-                <annotation>
-                    <documentation>
-                        Name of the file to execute in current package
-                        directory. If the element is skipped, it's
-                        equal to the 'name' attribute.
-                    </documentation>
-                </annotation>
-            </element>
+            <choice minOccurs="0" maxOccurs="unbounded">
+                <element name="description" type="string" minOccurs="0">
+                    <annotation>
+                        <documentation>
+                            Description of the test provided by the script.
+                        </documentation>
+                    </annotation>
+                </element>
+                <element name="req" type="Requirement" minOccurs="0"
+                                                       maxOccurs="unbounded">
+                    <annotation>
+                        <documentation>
+                            Complete set of requiremetns verified by the
+                            script.
+                        </documentation>
+                    </annotation>
+                </element>
+                <element name="execute" type="string" minOccurs="0">
+                    <annotation>
+                        <documentation>
+                            Name of the file to execute in current package
+                            directory. If the element is skipped, it's
+                            equal to the 'name' attribute.
+                        </documentation>
+                    </annotation>
+                </element>
+            </choice>
         </sequence>
         <attribute name="name" type="Name">
             <annotation>

commit 4ad0d251aa6a9ae1e86751b1533f9fdb46203d98

engine/tester: fix script objective and page information
Objective and page information from tests-info.xml should always be
used, not only when objective in package.xml is found.

Fixes: 7b9e6e2bc93a ("engine/tester: relax ordering in script specification")
Signed-off-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>

engine/tester/config_parse.c
@@ -1249,7 +1249,6 @@ static te_errno
 get_script(xmlNodePtr node, tester_cfg *cfg, run_item *ritem)
 {
     te_errno            rc;
-    const test_info    *ti;
     bool                objective_found = false;
     bool                execute_found = false;
     test_script        *script = &ritem->u.script;
@@ -1280,6 +1279,17 @@ get_script(xmlNodePtr node, tester_cfg *cfg, run_item *ritem)
         node = xmlNodeChildren(node);
     }
 
+    if (script->name != NULL)
+    {
+        const test_info *ti = find_test_info(cfg->cur_pkg->ti, script->name);
+
+        if (ti != NULL)
+        {
+            script->objective = TE_STRDUP(ti->objective);
+            script->page = TE_STRDUP(ti->page);
+        }
+    }
+
     while (node != NULL)
     {
         /* Get optional 'objective' */
@@ -1291,20 +1301,17 @@ get_script(xmlNodePtr node, tester_cfg *cfg, run_item *ritem)
                       script->name);
                 return TE_RC(TE_TESTER, TE_EINVAL);
             }
+
+            /*
+             * Objective from package.xml is more specific and overrides objective
+             * from tests-info.xml (which is extracted from c-file documentation).
+             */
+            free(script->objective);
+            script->objective = NULL;
             if ((rc = get_node_with_text_content(&node, "objective",
                                                  &script->objective)) != 0)
                 return rc;
             objective_found = true;
-
-            ti = find_test_info(cfg->cur_pkg->ti, script->name);
-            if (ti != NULL)
-            {
-                if (script->objective == NULL)
-                {
-                    script->objective = TE_STRDUP(ti->objective);
-                }
-                script->page = TE_STRDUP(ti->page);
-            }
             continue;
         }
 