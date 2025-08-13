oktetlabs/test-environment

commit cd671dc88bf6f6c90f28393ba1947405fc9e68ec

build: fix Test Agent build for OpenWRT/musl
Build Test Agent for OpenWRT using aarch64_cortex-a53_gcc-13.3.0_musl
(pre-built, loaded from OpenWRT).

 - invent conditional compilation and corresponding checks for random_r,
   sysv_signal and open64
 - add include for some missing headers
 - avoid using type that is not defined always

Signed-off-by: Elena Vengerova <elena.vengerova@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

lib/agentlib/l4_port.c
@@ -70,12 +70,19 @@ static pthread_mutex_t alloc_lock = PTHREAD_MUTEX_INITIALIZER;
 static te_errno
 agent_port_alloc_init(void)
 {
+#ifdef HAVE_RANDOM_R
     struct random_data buf = {0};
+#endif
     char statebuf[8];
     int32_t rnd;
 
+#ifdef HAVE_RANDOM_R
     if (initstate_r(getpid(), statebuf, sizeof(statebuf), &buf) != 0 ||
         random_r(&buf, &rnd) != 0)
+#else
+    rnd = random();
+    if (initstate(getpid(), statebuf, sizeof(statebuf)) != 0)
+#endif
     {
         te_errno rc = TE_OS_RC(TE_TA_UNIX, errno);
 

agents/unix/conf/util/conf_serial_parser.c
@@ -16,6 +16,10 @@
 #include "config.h"
 #endif
 
+#ifdef HAVE_PTHREAD_H
+#include <pthread.h>
+#endif
+
 #include "te_serial_parser.h"
 #include "te_kernel_log.h"
 #include "te_alloc.h"

lib/agentlib/agentlib_config.h.meson
@@ -53,3 +53,15 @@
 
 /* Define to 1 if you have getpwnam_r function file. */
 #mesondefine HAVE_GETPWNAM_R
+
+/* Define to 1 if you have random_r function. */
+#mesondefine HAVE_RANDOM_R
+
+/* Define to 1 if you have sysv_signal function. */
+#mesondefine HAVE_SYSV_SIGNAL
+
+/* Define to 1 if you have open64 function. */
+#mesondefine HAVE_OPEN64
+
+/* Define to 1 if you have off64_t type. */
+#mesondefine HAVE_OFF64_T

lib/agentlib/meson.build
@@ -29,6 +29,21 @@ if cc.has_function('getpwnam_r')
    conf.set('HAVE_PWNAM_R', 1)
 endif
 
+if cc.has_function('random_r')
+   conf.set('HAVE_RANDOM_R', 1)
+endif
+
+if cc.has_function('sysv_signal')
+   conf.set('HAVE_SYSV_SIGNAL', 1)
+endif
+
+if cc.has_function('open64')
+   conf.set('HAVE_OPEN64', 1)
+endif
+
+if cc.has_type('off64_t', prefix: '#define _GNU_SOURCE\n#include <unistd.h>')
+    conf.set('HAVE_OFF64_T', 1)
+endif
 
 agentlib_config_h = configure_file(input: 'agentlib_config.h.meson',
                                    output: 'agentlib_config.h',

lib/rpcserver/tarpc_imp.c
@@ -3650,6 +3650,7 @@ TARPC_FUNC(bsd_signal,
 
 /*-------------- sysv_signal() --------------------------------*/
 
+#ifdef HAVE_SYSV_SIGNAL
 TARPC_FUNC(sysv_signal,
 {
     if (in->signum == RPC_SIGINT)
@@ -3723,6 +3724,7 @@ TARPC_FUNC(__sysv_signal,
     }
 }
 )
+#endif /* HAVE_SYSV_SIGNAL */
 
 /*-------------- siginterrupt() --------------------------------*/
 
@@ -6319,6 +6321,7 @@ TARPC_FUNC(open, {},
 )
 #endif
 
+#ifdef HAVE_OPEN64
 /*-------------- open64() --------------------------------*/
 TARPC_FUNC(open64, {},
 {
@@ -6328,6 +6331,7 @@ TARPC_FUNC(open64, {},
                                  file_mode_flags_rpc2h(in->mode)));
 }
 )
+#endif /* HAVE_OPEN64 */
 
 /*-------------- fopen() --------------------------------*/
 TARPC_FUNC(fopen, {},
@@ -8880,8 +8884,13 @@ TARPC_FUNC(splice,
     COPY_ARG(off_out);
 },
 {
+#ifdef HAVE_OFF64_T
+    off64_t off_in = 0;
+    off64_t off_out = 0;
+#else
     off_t off_in = 0;
     off_t off_out = 0;
+#endif
 
     if (out->off_in.off_in_len > 0)
         off_in = *out->off_in.off_in_val;
@@ -8890,9 +8899,9 @@ TARPC_FUNC(splice,
 
     MAKE_CALL(out->retval =
         func(in->fd_in,
-             out->off_in.off_in_len == 0 ? NULL : (off64_t *)&off_in,
+             out->off_in.off_in_len == 0 ? NULL : &off_in,
              in->fd_out,
-             out->off_out.off_out_len == 0 ? NULL : (off64_t *)&off_out,
+             out->off_out.off_out_len == 0 ? NULL : &off_out,
              in->len, splice_flags_rpc2h(in->flags)));
     if (out->off_in.off_in_len > 0)
         out->off_in.off_in_val[0] = (tarpc_off_t)off_in;

lib/tad/tad_eth_sap.c
@@ -58,8 +58,12 @@
 #if HAVE_LINUX_IF_PACKET_H
 #include <linux/if_packet.h>
 #endif
+#ifdef HAVE_NETINET_IF_ETHER_H
+#include <netinet/if_ether.h>
+#else
 #if HAVE_LINUX_IF_ETHER_H
 #include <linux/if_ether.h>
+#endif
 #endif
 
 #if defined(USE_PF_PACKET) && defined(WITH_PACKET_MMAP_RX_RING)

lib/tools/te_timer.h
@@ -31,6 +31,8 @@
 #ifndef __TE_TIMER_H__
 #define __TE_TIMER_H__
 
+#include <time.h>
+
 #include "te_defs.h"
 #include "te_errno.h"
 

commit db1a5e001844405ef230a64bc14e9de55afaeb78

lib/agentlib: fix random port allocation
Fix initstate() return code check, it returns a pointer to the previous
state array, on error, errno is set to indicate the cause.

Move random() after initstate() that allows a state array state to be
initialised for use by random()

Fixes: cd671dc88bf6 ("build: fix Test Agent build for OpenWRT/musl")
Signed-off-by: Michael Kochkin <michael.kochkin@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

lib/agentlib/l4_port.c
@@ -80,8 +80,9 @@ agent_port_alloc_init(void)
     if (initstate_r(getpid(), statebuf, sizeof(statebuf), &buf) != 0 ||
         random_r(&buf, &rnd) != 0)
 #else
-    rnd = random();
-    if (initstate(getpid(), statebuf, sizeof(statebuf)) != 0)
+    errno = 0;
+    initstate(getpid(), statebuf, sizeof(statebuf));
+    if (errno != 0)
 #endif
     {
         te_errno rc = TE_OS_RC(TE_TA_UNIX, errno);
@@ -89,6 +90,10 @@ agent_port_alloc_init(void)
         ERROR("Failed to initialize random number");
         return rc;
     }
+
+#ifndef HAVE_RANDOM_R
+    rnd = random();
+#endif
 
     port_offset = (rnd % BUCKETS_COUNT) * PORTS_PER_BUCKET_COUNT;
     initialization_needed = false;