oktetlabs/test-environment

commit 40cf70ff32884c688076953363da5bd5b208401b

engine/tester: support compound values in Tester configs
Compound or structured values provide a way to define
a set of tightly coupled parameters, like fields of a record
in programming languages. For test scripts, logging and TRC
separate fields of a structured parameter would appear as
separate parameters with specially constructed names.
However, Tester will always consider them together for the
purpose of building test plans, satisfying requirements and
selecting test iterations.

The present implementation provide support for one-dimensional
arrays, for key-value pairs and for multi-valued key-value
pairs.

Lists of test parameters provide a similiar functionality,
however structured values offer some advantages:
- related fields come together in the package file, which
  makes it less error-prone than parameter lists;
- a structured value may be put into enum, providing a way
  to re-use iteration of several related parameters across
  the tests.

OL-Redmine-Id: 14028
Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

engine/tester/enumerate.c
@@ -27,6 +27,8 @@
 
 #include "te_queue.h"
 #include "te_errno.h"
+#include "te_str.h"
+#include "te_compound.h"
 #include "logger_api.h"
 
 #include "tester_conf.h"
@@ -116,6 +118,59 @@ typedef struct test_run_item_find_arg_cb_data {
     const test_var_arg *found;      /**< Found argument */
 } test_run_item_find_arg_cb_data;
 
+static te_errno
+has_compound_name(char *field, size_t idx, char *value,
+                  bool has_more, void *user)
+{
+    UNUSED(field);
+    UNUSED(idx);
+    UNUSED(has_more);
+    UNUSED(user);
+    return TE_EOK;
+}
+
+static bool has_value_with_field_name(const char *fullname, const char *stem,
+                                      const test_entity_values *values);
+
+static bool
+has_field_name(const char *fullname, const char *stem,
+               const test_entity_value *v)
+{
+    if (v->plain != NULL)
+    {
+        return te_compound_dereference_str(v->plain, stem, fullname,
+                                           has_compound_name, NULL) == 0;
+    }
+
+    if (v->ref != NULL)
+        return has_field_name(fullname, stem, v->ref);
+
+    if (v->type != NULL)
+        return has_value_with_field_name(fullname, stem, &v->type->values);
+
+    return false;
+}
+
+static bool
+has_value_with_field_name(const char *fullname, const char *stem,
+                          const test_entity_values *values)
+{
+    const test_entity_value *v;
+
+    if (te_str_strip_prefix(fullname, stem) == NULL)
+        return false;
+
+    TAILQ_FOREACH(v, &values->head, links)
+    {
+        if (has_field_name(fullname, stem, v))
+        {
+            RING("found");
+            return true;
+        }
+    }
+    return false;
+}
+
 /**
  * Callback function for test_run_item_enum_args() routine to find
  * argument by name.
@@ -130,6 +185,7 @@ test_run_item_find_arg_cb(const test_var_arg *va, void *opaque)
 {
     test_run_item_find_arg_cb_data *data = opaque;
     const test_var_arg_list        *list = NULL;
+    const test_entity_values *values = test_var_arg_values(va);
 
     if (va->list != NULL)
     {
@@ -142,10 +198,10 @@ test_run_item_find_arg_cb(const test_var_arg *va, void *opaque)
     }
     else
     {
-        data->n_values = test_var_arg_values(va)->num;
+        data->n_values = values->num;
     }
 
-    if (strcmp(data->name, va->name) != 0)
+    if (!has_value_with_field_name(data->name, va->name, values))
     {
         if ((list == NULL) || (list->n_iters == data->n_iters))
         {

doc/xsd/tester_defs.xsd
@@ -78,51 +78,76 @@
         </xsd:attribute>
     </xsd:complexType>
 
-    <xsd:complexType name="Value">
+    <xsd:complexType name="Field">
         <xsd:annotation>
             <xsd:documentation>
-                Representation of value for session variable or run
-                argument.
+                Type for structured value field description.
             </xsd:documentation>
         </xsd:annotation>
         <xsd:simpleContent>
             <xsd:extension base="xsd:string">
-                <xsd:attribute name="name" type="xsd:Name">
+                <xsd:attribute name="name" type="xsd:string">
                     <xsd:annotation>
                         <xsd:documentation>
-                            Identifier to be referred from 'preferred'
-                            attribute of the parent or 'ref'
-                            attribute of another value of the parent.
-                        </xsd:documentation>
-                    </xsd:annotation>
-                </xsd:attribute>
-                <xsd:attribute name="ref" type="xsd:Name">
-                    <xsd:annotation>
-                        <xsd:documentation>
-                            Identifier of another value of the argument
-                            or reference to external value (name of the
-                            variable of the parent session or name of
-                            the argument passed to the sesiion).
-                        </xsd:documentation>
-                    </xsd:annotation>
-                </xsd:attribute>
-                <xsd:attribute name="reqs" type="xsd:string">
-                    <xsd:annotation>
-                        <xsd:documentation>
-                            Comman separated list of requirement IDs
-                            associated with this value.
+                            The name of a field.
                         </xsd:documentation>
                     </xsd:annotation>
                 </xsd:attribute>
                 <xsd:attribute name="type" type="xsd:Name">
                     <xsd:annotation>
                         <xsd:documentation>
-                            Defines the type of a certain value.
+                            The type of a field.
                         </xsd:documentation>
                     </xsd:annotation>
                 </xsd:attribute>
             </xsd:extension>
         </xsd:simpleContent>
+    </xsd:complexType>
+
+    <xsd:complexType name="Value" mixed="true">
+        <xsd:annotation>
+            <xsd:documentation>
+                Representation of value for session variable or run
+                argument.
+            </xsd:documentation>
+        </xsd:annotation>
+        <xsd:sequence>
+            <xsd:element name="field"  type="Field" minOccurs="0" maxOccurs="unbounded" />
+        </xsd:sequence>
+        <xsd:attribute name="name" type="xsd:Name">
+            <xsd:annotation>
+                <xsd:documentation>
+                    Identifier to be referred from 'preferred'
+                    attribute of the parent or 'ref'
+                    attribute of another value of the parent.
+                </xsd:documentation>
+            </xsd:annotation>
+        </xsd:attribute>
+        <xsd:attribute name="ref" type="xsd:Name">
+            <xsd:annotation>
+                <xsd:documentation>
+                    Identifier of another value of the argument
+                    or reference to external value (name of the
+                    variable of the parent session or name of
+                    the argument passed to the sesiion).
+                </xsd:documentation>
+            </xsd:annotation>
+        </xsd:attribute>
+        <xsd:attribute name="reqs" type="xsd:string">
+            <xsd:annotation>
+                <xsd:documentation>
+                    Comman separated list of requirement IDs
+                    associated with this value.
+                </xsd:documentation>
+            </xsd:annotation>
+        </xsd:attribute>
+        <xsd:attribute name="type" type="xsd:Name">
+            <xsd:annotation>
+                <xsd:documentation>
+                    Defines the type of a certail value.
+                </xsd:documentation>
+            </xsd:annotation>
+        </xsd:attribute>
     </xsd:complexType>
 
     <xsd:complexType name="TestParameter">

engine/tester/config_parse.c
@@ -41,6 +41,7 @@
 #include "te_alloc.h"
 #include "te_param.h"
 #include "te_expand.h"
+#include "te_compound.h"
 #include "te_str.h"
 #include "tester_conf.h"
 #include "type_lib.h"
@@ -1329,9 +1330,111 @@ parse_value_reqs(test_entity_value *value, xmlNodePtr node)
 }
 
 static te_errno
-process_plain_value(test_entity_value *value, xmlNodePtr node)
+process_simple_plain_value(te_string *dest, const char *content)
 {
-    const char *content;
+    te_compound_kind kind = te_compound_classify(dest);
+
+    if (kind != TE_COMPOUND_NULL && kind != TE_COMPOUND_PLAIN)
+    {
+        if (te_str_isspace(content))
+            return 0;
+
+        ERROR("Simple text '%s' follows subvalue definitions", content);
+        return TE_RC(TE_TESTER, TE_EINVAL);
+    }
+    te_string_append(dest, "%s", content);
+    return 0;
+}
+
+static te_errno
+process_subvalue(te_string *dest, xmlNodePtr field_node, const test_session *session)
+{
+    te_string collect = TE_STRING_INIT;
+    const struct test_value_type *type = NULL;
+    char *typename;
+    char *name;
+    xmlNodePtr child;
+
+    if (xmlStrcmp(field_node->name, CONST_CHAR2XML("field")) != 0)
+    {
+        ERROR("Unexpected element <%s> inside <value>",
+              (const char *)field_node->name);
+        te_string_free(&collect);
+        return TE_RC(TE_TESTER, TE_EINVAL);
+    }
+
+    if (te_compound_classify(dest) == TE_COMPOUND_PLAIN)
+    {
+        if (!te_str_isspace(dest->ptr))
+        {
+            ERROR("<field> follows simple text");
+            te_string_free(&collect);
+            return TE_RC(TE_TESTER, TE_EINVAL);
+        }
+        te_string_reset(dest);
+    }
+
+    typename = XML2CHAR(xmlGetProp(field_node, CONST_CHAR2XML("type")));
+    if (typename != NULL)
+    {
+        type = tester_find_type(session, typename);
+        if (type == NULL)
+        {
+            ERROR("Type '%s' not found", typename);
+            xmlFree(typename);
+            te_string_free(&collect);
+
+            return TE_RC(TE_TESTER, TE_ESRCH);
+        }
+        xmlFree(typename);
+    }
+
+    for (child = field_node->children; child != NULL; child = child->next)
+    {
+        switch (child->type)
+        {
+            case XML_COMMENT_NODE:
+                /* Just skip comments */
+                break;
+            case XML_TEXT_NODE:
+                te_string_append(&collect, (const char *)child->content);
+                break;
+            case XML_ELEMENT_NODE:
+                ERROR("Unexpected element <%s> inside <field>",
+                      (const char *)child->name);
+                te_string_free(&collect);
+                return TE_RC(TE_TESTER, TE_EINVAL);
+            default:
+                ERROR("Something strange inside <field>, node type = %d",
+                      child->type);
+                te_string_free(&collect);
+                return TE_RC(TE_TESTER, TE_EINVAL);
+        }
+    }
+    if (type != NULL)
+    {
+        if (tester_type_check_plain_value(type, collect.ptr) == NULL)
+        {
+            ERROR("Subvalue '%s' does not conform to type '%s'",
+                  collect.ptr, type->name);
+            te_string_free(&collect);
+            return TE_RC(TE_TESTER, TE_EINVAL);
+        }
+    }
+
+    name = XML2CHAR(xmlGetProp(field_node, CONST_CHAR2XML("name")));
+    te_compound_set(dest, name, TE_COMPOUND_MOD_OP_APPEND, "%s", collect.ptr);
+    xmlFree(name);
+    te_string_free(&collect);
+    return 0;
+}
+
+static te_errno
+process_plain_value(test_entity_value *value, xmlNodePtr node, const test_session *session)
+{
+    te_errno rc = 0;
+    te_string compound = TE_STRING_INIT;
+    xmlNodePtr child;
 
     if (value->ref != NULL || value->ext != NULL)
     {
@@ -1340,45 +1443,57 @@ process_plain_value(test_entity_value *value, xmlNodePtr node)
             ERROR("Plain value used together with a reference '%s'",
                   value->ext != NULL ? value->ext :
                   te_str_empty_if_null(value->ref->name));
+            te_string_free(&compound);
             return TE_RC(TE_TESTER, TE_EINVAL);
         }
     }
 
-    if (node->children == NULL)
-        return 0;
-
-    if ((node->children->type != XML_TEXT_NODE) ||
-        (node->children->content == NULL))
+    for (child = node->children; child != NULL && rc == 0; child = child->next)
     {
-        ERROR("'value' content is empty or not text");
-        return TE_RC(TE_TESTER, TE_EINVAL);
-    }
-    if (node->children != node->last)
-    {
-        ERROR("Too many children in 'value' element");
-        return TE_RC(TE_TESTER, TE_EINVAL);
-    }
-
-    content = (const char *)node->children->content;
-    if (value->type == NULL)
-        value->plain = TE_STRDUP(content);
-    else
-    {
-        const test_entity_value *tv =
-            tester_type_check_plain_value(value->type, content);
-
-        VERB("%s(): Checked value '%s' by type '%s' -> %p",
-             __FUNCTION__, content, value->type->name, tv);
-        if (tv == NULL)
+        switch (child->type)
         {
-            ERROR("Plain value '%s' does not conform to type '%s'",
-                  content, value->type->name);
-            return TE_RC(TE_TESTER, TE_EINVAL);
+            case XML_COMMENT_NODE:
+                /* Just skip comments */
+                break;
+            case XML_TEXT_NODE:
+                rc = process_simple_plain_value(&compound,
+                                                (const char *)child->content);
+                break;
+            case XML_ELEMENT_NODE:
+                rc = process_subvalue(&compound, child, session);
+                break;
+            default:
+                ERROR("Something strange inside <value>, node type = %d",
+                      child->type);
+                rc = TE_RC(TE_TESTER, TE_EINVAL);
+                break;
         }
-        value->ref = tv;
     }
 
-    return 0;
+    if (rc == 0 && compound.len > 0)
+    {
+        if (value->type == NULL)
+            te_string_move(&value->plain, &compound);
+        else
+        {
+            const test_entity_value *tv =
+                tester_type_check_plain_value(value->type, compound.ptr);
+
+            VERB("%s(): Checked value '%s' by type '%s' -> %p",
+                 __FUNCTION__, compound.ptr, value->type->name, tv);
+            if (tv != NULL)
+                value->ref = tv;
+            else
+            {
+                ERROR("Plain value '%s' does not conform to type '%s'",
+                      compound.ptr, value->type->name);
+                rc = TE_RC(TE_TESTER, TE_EINVAL);
+            }
+        }
+    }
+
+    te_string_free(&compound);
+    return rc;
 }
 
 static void
@@ -1426,7 +1541,7 @@ alloc_and_get_value(xmlNodePtr node, const test_session *session,
     resolve_value_reference(value, node, values);
     parse_value_reqs(value, node);
 
-    rc = process_plain_value(value, node);
+    rc = process_plain_value(value, node, session);
     if (rc != 0)
     {
         free_test_entity_value(value);

engine/tester/run.c
@@ -41,6 +41,7 @@
 #include "te_alloc.h"
 #include "tq_string.h"
 #include "te_str.h"
+#include "te_compound.h"
 #include "conf_api.h"
 #include "log_bufs.h"
 #include "te_trc.h"
@@ -207,6 +208,18 @@ typedef struct tester_run_data {
     SLIST_HEAD(, tester_ctx)    ctxs;       /**< Stack of contexts */
 
 } tester_run_data;
+
+/**
+ * Callback data to process compound values.
+ */
+typedef struct compound_arg_data {
+    const test_iter_arg *arg;
+    union {
+        te_string *dest;
+        json_t *json_result;
+        te_vec *params;
+    };
+} compound_arg_data;
 
 static enum interactive_mode_opts tester_run_interactive(
                                       tester_run_data *gctx);
@@ -1215,6 +1228,24 @@ persons_info_to_json(const persons_info *persons)
     return result;
 }
 
+static te_errno
+param_to_string(char *key, size_t idx, char *value, bool has_more, void *user)
+{
+    compound_arg_data *data = user;
+
+
+    VERB("%s(): parameter %s %s[%zu]=%s", __func__, data->arg->name,
+         te_str_empty_if_null(key), idx, value);
+
+    te_string_append(data->dest, " ");
+    te_compound_build_name(data->dest, data->arg->name, key, idx);
+    te_string_append(data->dest, "=");
+    te_string_append_shell_arg_as_is(data->dest, value);
+
+    UNUSED(has_more);
+    return 0;
+}
+
 /**
  * Convert test parameters to string representation using te_string.
  * The first symbol is a space, if @p str is not NULL.
@@ -1241,13 +1272,41 @@ test_params_to_te_string(te_string *str, const unsigned int n_args,
         if (p->variable)
             continue;
 
-        VERB("%s(): parameter %s=%s", __FUNCTION__, p->name, p->value);
-        te_string_append(str, " %s=", p->name);
-        te_string_append_shell_arg_as_is(str, p->value);
+        te_compound_iterate_str(p->value, param_to_string,
+                                &(compound_arg_data){.arg = p, .dest = str});
     }
 
     VERB("%s(): %s", __FUNCTION__, str->ptr);
 }
+
+static te_errno
+param_to_json(char *key, size_t idx, char *value, bool has_more, void *user)
+{
+    te_string json_key = TE_STRING_INIT;
+    compound_arg_data *data = user;
+    json_t *item;
+
+    te_compound_build_name(&json_key, data->arg->name, key, idx);
+    item = json_pack("[ss]", json_key.ptr, value);
+    te_string_free(&json_key);
+
+    if (item == NULL)
+    {
+        ERROR("%s(): failed to pack into JSON array", __func__);
+        return TE_EFAIL;
+    }
+
+    if (json_array_append_new(data->json_result, item) != 0)
+    {
+        ERROR("%s: failed to add item to the array", __func__);
+        json_decref(item);
+        return TE_EFAIL;
+    }
+
+    UNUSED(has_more);
+    return 0;
+}
+
 
 /**
  * Convert test parameters to JSON representation.
@@ -1261,7 +1320,6 @@ static json_t *
 test_params_to_json(const unsigned int n_args, const test_iter_arg *args)
 {
     json_t       *result;
-    json_t       *item;
     unsigned int  i;
 
     if (n_args == 0)
@@ -1279,19 +1337,12 @@ test_params_to_json(const unsigned int n_args, const test_iter_arg *args)
         if (args[i].variable)
             continue;
 
-        item = json_pack("[ss]", args[i].name, args[i].value);
-        if (item != NULL)
+        if (te_compound_iterate_str(args[i].value, param_to_json,
+                                    &(compound_arg_data){
+                                        .arg = &args[i],
+                                        .json_result = result
+                                    }) == TE_EFAIL)
         {
-            if (json_array_append_new(result, item) != 0)
-            {
-                ERROR("%s: failed to add item to the array", __FUNCTION__);
-                json_decref(result);
-                return NULL;
-            }
-        }
-        else
-        {
-            ERROR("%s: failed to pack into JSON array", __FUNCTION__);
             json_decref(result);
             return NULL;
         }
@@ -1306,6 +1357,117 @@ test_params_to_json(const unsigned int n_args, const test_iter_arg *args)
 
     return result;
 }
+
+static te_errno
+param_stem_to_json(char *key, size_t idx, char *value, bool has_more, void *user)
+{
+    te_string json_key = TE_STRING_INIT;
+    compound_arg_data *data = user;
+    json_t *jstr;
+
+    if (key == NULL && idx == 0)
+    {
+        te_string_free(&json_key);
+        return 0;
+    }
+    te_compound_build_name(&json_key, data->arg->name, key, idx);
+    jstr = json_string(data->arg->name);
+    if (jstr == NULL)
+    {
+        ERROR("%s(): failed to create a JSON string", __func__);
+        te_string_free(&json_key);
+        return TE_EFAIL;
+    }
+    if (json_object_set_new(data->json_result, json_key.ptr, jstr) != 0)
+    {
+        ERROR("%s(): failed to add item to the array", __func__);
+        json_decref(jstr);
+        te_string_free(&json_key);
+        return TE_EFAIL;
+    }
+
+    UNUSED(has_more);
+    te_string_free(&json_key);
+    return 0;
+}
+
+static te_errno
+param_field_to_json(char *key, size_t idx, char *value, bool has_more, void *user)
+{
+    te_string json_key = TE_STRING_INIT;
+    compound_arg_data *data = user;
+    json_t *jstr;
+
+    if (key == NULL)
+    {
+        te_string_free(&json_key);
+        return 0;
+    }
+    te_compound_build_name(&json_key, data->arg->name, key, idx);
+    jstr = json_string(key);
+    if (jstr == NULL)
+    {
+        ERROR("%s(): failed to create a JSON string", __func__);
+        te_string_free(&json_key);
+        return TE_EFAIL;
+    }
+    if (json_object_set_new(data->json_result, json_key.ptr, jstr) != 0)
+    {
+        ERROR("%s(): failed to add item to the array", __func__);
+        json_decref(jstr);
+        te_string_free(&json_key);
+        return TE_EFAIL;
+    }
+
+    UNUSED(has_more);
+    UNUSED(idx);
+    te_string_free(&json_key);
+    return 0;
+}
+
+
+static json_t *
+test_param_names_to_json(const unsigned int n_args, const test_iter_arg *args,
+                         te_compound_iter_fn *callback)
+{
+    json_t       *result;
+    unsigned int  i;
+
+    if (n_args == 0)
+        return NULL;
+
+    result = json_object();
+    if (result == NULL)
+    {
+        ERROR("%s(): failed to allocate memory for mapping", __func__);
+        return NULL;
+    }
+
+    for (i = 0; i < n_args; i++)
+    {
+        if (args[i].variable)
+            continue;
+
+        if (te_compound_iterate_str(args[i].value, callback,
+                                    &(compound_arg_data){
+                                        .arg = &args[i],
+                                        .json_result = result
+                                    }) == TE_EFAIL)
+        {
+            json_decref(result);
+            return NULL;
+        }
+    }
+
+    if (json_object_size(result) == 0)
+    {
+        json_decref(result);
+        return NULL;
+    }
+
+    return result;
+}
+
 
 /**
  * Add requirements to a strings queue ensuring that every requirement
@@ -1585,6 +1747,8 @@ log_test_start(unsigned int flags,
     json_t                 *result;
     json_t                 *authors;
     json_t                 *tmp;
+    json_t                 *param_stems;
+    json_t                 *param_fields;
 
     te_string   params_str  = TE_STRING_INIT;
     char       *hash_str    = NULL;
@@ -1642,6 +1806,15 @@ log_test_start(unsigned int flags,
         ERROR("JSON object creation failed in %s", __FUNCTION__);
         return;
     }
+
+    param_stems = test_param_names_to_json(ri->n_args, ctx->args,
+                                           param_stem_to_json);
+    if (param_stems != NULL)
+        SET_NEW_JSON(result, "param_stems", param_stems);
+    param_fields = test_param_names_to_json(ri->n_args, ctx->args,
+                                             param_field_to_json);
+    if (param_fields != NULL)
+        SET_NEW_JSON(result, "param_fields", param_fields);
 
     if (name == NULL && ri->type == RUN_ITEM_SESSION)
         name = "session";
@@ -2062,6 +2235,24 @@ tester_test_status_to_te_test_result(tester_test_status status,
     }
 }
 
+static te_errno
+add_test_script_value(char *key, size_t idx, char *value,
+                      bool has_more, void *user)
+{
+    te_string param_name = TE_STRING_INIT;
+    compound_arg_data *data = user;
+
+    te_compound_build_name(&param_name, data->arg->name, key, idx);
+    VERB("%s(): parameter %s=%s", __func__, param_name.ptr, value);
+
+    te_vec_append_str_fmt(data->params, "%s=%s",
+                          param_name.ptr, value);
+    te_string_free(&param_name);
+
+    UNUSED(has_more);
+    return 0;
+}
+
 static void
 prepare_test_script_arguments(te_vec *params, tester_flags flags,
                               const test_script *script,
@@ -2111,10 +2302,12 @@ prepare_test_script_arguments(te_vec *params, tester_flags flags,
     {
         if (args[i].variable)
             continue;
-        VERB("%s(): parameter %s=%s", __FUNCTION__,
-             args[i].name, args[i].value);
-        te_vec_append_str_fmt(params, "%s=%s",
-                              args[i].name, args[i].value);
+        te_compound_iterate_str(args[i].value,
+                                add_test_script_value,
+                                &(compound_arg_data){
+                                    .arg = &args[i],
+                                    .params = params
+                                });
     }
     te_vec_append(params, NULL);
 

commit fb2d5a68cb16414c9c924fdc4f8a8db49f3388b0

engine/tester: fix handling of global references
In function has_field_name(): take into account that variable value can be
not only in plain field but in ext field too.

Fixes: 40cf70ff3288 ("engine/tester: support compound values in Tester configs")
Signed-off-by: Yurij Plotnikov <yurij.plotnikov@arknetworks.am>
Reviewed-by: Viacheslav Galaktionov <viacheslav.galaktionov@arknetworks.am>

engine/tester/enumerate.c
@@ -136,9 +136,11 @@ static bool
 has_field_name(const char *fullname, const char *stem,
                const test_entity_value *v)
 {
-    if (v->plain != NULL)
+    char *plain_or_ext = v->plain != NULL ? v->plain : v->ext;
+
+    if (plain_or_ext != NULL)
     {
-        return te_compound_dereference_str(v->plain, stem, fullname,
+        return te_compound_dereference_str(plain_or_ext, stem, fullname,
                                            has_compound_name, NULL) == 0;
     }
 