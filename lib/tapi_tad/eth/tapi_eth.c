oktetlabs/test-environment

commit 442c2526aabbb1482fc7a4519a52960a59fa2df0

lib/tapi_tad: add helpers for ICMP packet encapsulation
Introduce API providing a unified way to construct ICMP-encapsulated
templates, useful for verifying ICMP packets generated in response
to specific traffic.

Signed-off-by: Denis Pryazhennikov <denis.pryazhennikov@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

lib/tapi_tad/eth/tapi_eth.c
@@ -521,3 +521,54 @@ out:
 
     return TE_RC(TE_TAPI, err);
 }
+
+te_errno
+tapi_eth_header_free(asn_value *tmpl, size_t *n_tags, uint16_t *vid,
+                     uint16_t *prio, uint16_t *cfi)
+{
+    asn_child_desc_t *eth_hdrs = NULL;
+    asn_value        *pdus = NULL;
+    asn_value        *eth = NULL;
+    unsigned int      n_hdrs;
+    te_errno          rc;
+
+    if (tmpl == NULL)
+        return 0;
+
+    pdus = asn_find_descendant(tmpl, &rc, "pdus");
+    if (rc != 0)
+    {
+        ERROR("Failed to get pdus: %r", rc);
+        return rc;
+    }
+
+    rc = asn_find_child_choice_values(pdus, TE_PROTO_ETH,
+                                      &eth_hdrs, &n_hdrs);
+    if (rc != 0)
+    {
+        ERROR("Failed to get eth PDU from template");
+        return rc;
+    }
+    if (n_hdrs < 1)
+    {
+        ERROR("No ethernet headers found in template");
+        return TE_ENOENT;
+    }
+
+    eth = eth_hdrs[n_hdrs - 1].value;
+    rc = tapi_ndn_eth_read_vlan_tci(eth, n_tags, vid, prio, cfi);
+    if (rc != 0)
+    {
+        ERROR("Failed to read VLAN TCI");
+        return rc;
+    }
+
+    rc = asn_remove_indexed(pdus, eth_hdrs[n_hdrs - 1].index, "");
+    if (rc != 0)
+    {
+        ERROR("Failed to remove ethernet header from template: %r", rc);
+        return rc;
+    }
+
+    return 0;
+}

lib/tapi_tad/eth/tapi_eth.h
@@ -285,6 +285,28 @@ extern te_errno tapi_eth_based_csap_create_by_tmpl(const char      *ta_name,
                                                    const asn_value *tmpl,
                                                    csap_handle_t   *eth_csap);
 
+/**
+ * Remove the last Ethernet header from a packet template with extracting
+ * VLAN TCI parameters.
+ *
+ * @param[in,out] tmpl    Packet template containing PDUs.
+ * @param[out]    n_tags  Number of VLAN tags found in the Ethernet header
+ *                        (may be @c NULL if not needed).
+ * @param[out]    vid     VLAN identifier extracted from the Ethernet header
+ *                        (may be @c NULL if not needed).
+ * @param[out]    prio    VLAN priority extracted from the Ethernet
+ *                        header (may be @c NULL if not needed).
+ * @param[out]    cfi     CFI value extracted from the Ethernet header
+ *                        (may be @c NULL if not needed).
+ *
+ * @return Status code:
+ * @retval 0           Success.
+ * @retval TE_ENOENT   No Ethernet headers were found.
+ */
+extern te_errno tapi_eth_header_free(asn_value *tmpl, size_t *n_tags,
+                                     uint16_t *vid, uint16_t *prio,
+                                     uint16_t *cfi);
+
 #endif /* __TE_TAPI_ETH_H__ */
 
 /**@} <!-- END tapi_tad_eth --> */

lib/tapi_tad/ipstack/tapi_icmp.c
@@ -147,3 +147,105 @@ tapi_tcp_ip_icmp_ip_eth_csap_create(const char    *ta_name,
                                         msg_loc_saddr, msg_rem_saddr,
                                         af, IPPROTO_TCP, tcp_csap);
 }
+
+static te_errno
+tmpl_icmp_wrap(asn_value *tmpl,
+               const void *src_eth, const void *dst_eth,
+               const struct sockaddr *src_addr,
+               const struct sockaddr *dst_addr,
+               int ttl_hoplimit, int ip4_tos, int af,
+               uint8_t icmp_type, uint8_t icmp_code,
+               icmp6_msg_body *msg_body)
+{
+    te_errno rc = 0;
+
+    switch (af)
+    {
+        case AF_INET:
+            rc = tapi_icmp4_wrap_tmpl(tmpl, src_eth, dst_eth,
+                                      (uint8_t *)&(SIN(src_addr)->sin_addr),
+                                      (uint8_t *)&(SIN(dst_addr)->sin_addr),
+                                      ttl_hoplimit, ip4_tos, icmp_type,
+                                      icmp_code);
+            break;
+
+        case AF_INET6:
+        {
+            icmp6_msg_body *body_ptr = msg_body;
+            icmp6_msg_body body = {};
+
+            if (body_ptr == NULL)
+            {
+                body.msg_type = icmp_type;
+                body_ptr = &body;
+            }
+
+            rc = tapi_icmp6_wrap_tmpl(tmpl, src_eth, dst_eth,
+                                      (uint8_t *)&(SIN6(src_addr)->sin6_addr),
+                                      (uint8_t *)&(SIN6(dst_addr)->sin6_addr),
+                                      ttl_hoplimit, icmp_type, icmp_code,
+                                      body_ptr);
+            break;
+        }
+
+        default:
+            ERROR("Invalid IP address family: %d", af);
+            rc = TE_EINVAL;
+    }
+
+    return rc;
+}
+
+te_errno
+tapi_icmp_tmpl_encap_ext(const asn_value *tmpl,
+                         const void *src_eth, const void *dst_eth,
+                         const struct sockaddr *src_addr,
+                         const struct sockaddr *dst_addr,
+                         int ttl_hoplimit, int ip4_tos, int af,
+                         uint8_t icmp_type, uint8_t icmp_code,
+                         icmp6_msg_body *msg_body,
+                         asn_value **icmp_tmpl)
+{
+    asn_value          *tmpl_copy = NULL;
+    uint16_t            prio[2] = {0};
+    uint16_t            vid[2] = {0};
+    uint16_t            cfi[2] = {0};
+    asn_value          *pdus = NULL;
+    size_t              n_tags;
+    te_errno            rc = 0;
+
+    tmpl_copy = asn_copy_value(tmpl);
+    if (tmpl_copy == NULL)
+    {
+        ERROR("Failed to copy original ASN.1 template");
+        return TE_EFAIL;
+    }
+
+    n_tags = TE_ARRAY_LEN(vid);
+    rc = tapi_eth_header_free(tmpl_copy, &n_tags, vid, prio, cfi);
+    if (rc != 0)
+        goto err;
+
+    rc = tmpl_icmp_wrap(tmpl_copy, src_eth, dst_eth, src_addr, dst_addr,
+                        ttl_hoplimit, ip4_tos, af, icmp_type, icmp_code,
+                        msg_body);
+    if (rc != 0)
+        goto err;
+
+    pdus = asn_find_descendant(tmpl_copy, NULL, "pdus");
+    rc = tapi_ndn_pdus_inject_vlan_tags(pdus, vid, prio, cfi, n_tags);
+    if (rc != 0)
+    {
+        ERROR("Failed to reinject VLAN tags for ICMP packet: %r", rc);
+        goto err;
+    }
+
+    *icmp_tmpl = tmpl_copy;
+
+    return 0;
+
+err:
+    asn_free_value(tmpl_copy);
+
+    return rc;
+}

lib/tapi_tad/ipstack/tapi_icmp.h
@@ -94,6 +94,64 @@ extern te_errno tapi_tcp_ip_icmp_ip_eth_csap_create(
                                 int                    af,
                                 csap_handle_t         *tcp_csap);
 
+/**
+ * Encapsulate packet template in ICMP header.
+ *
+ * @param[in]  tmpl         Packet template to encapsulate.
+ * @param[in]  src_eth      Source MAC address for ETH header.
+ * @param[in]  dst_eth      Destination address for ETH header.
+ * @param[in]  src_addr     Source IP address for IP header.
+ * @param[in]  dst_addr     Destination IP address for IP header.
+ * @param[in]  ttl_hoplimit TTL or Hop Limit for IP header.
+ * @param[in]  ip4_tos      TOS field for IP header. For IPv4 only.
+ *                          (may be @c -1 for the default value).
+ * @param[in]  af           Address family.
+ * @param[in]  icmp_type    ICMP type.
+ * @param[in]  icmp_code    ICMP code.
+ * @param[in]  msg_body     ICMP message body. For IPv6 only.
+ *                          (may be @c NULL for the default value).
+ * @param[out] icmp_tmpl    Resulting packet template.
+ *
+ * @return Status code
+ */
+extern te_errno tapi_icmp_tmpl_encap_ext(
+                    const asn_value *tmpl,
+                    const void *src_eth, const void *dst_eth,
+                    const struct sockaddr *src_addr,
+                    const struct sockaddr *dst_addr,
+                    int ttl_hoplimit, int ip4_tos, int af,
+                    uint8_t icmp_type, uint8_t icmp_code,
+                    icmp6_msg_body *msg_body,
+                    asn_value **icmp_tmpl);
+
+/**
+ * Wrapper for @b tapi_icmp_tmpl_encap_ext with default options.
+ *
+ * @param[in]  tmpl         Packet template to encapsulate.
+ * @param[in]  src_eth      Source MAC address for ETH header.
+ * @param[in]  dst_eth      Destination address for ETH header.
+ * @param[in]  src_addr     Source IP address for IP header.
+ * @param[in]  dst_addr     Destination IP address for IP header.
+ * @param[in]  af           Address family.
+ * @param[in]  icmp_type    ICMP type.
+ * @param[in]  icmp_code    ICMP code.
+ * @param[out] icmp_tmpl    Resulting packet template.
+ *
+ * @return Status code
+ */
+static inline te_errno
+tapi_icmp_tmpl_encap(const asn_value *tmpl,
+                      const void *src_eth, const void *dst_eth,
+                      const struct sockaddr *src_addr,
+                      const struct sockaddr *dst_addr,
+                      int af, uint8_t icmp_type, uint8_t icmp_code,
+                      asn_value **icmp_tmpl)
+{
+    return tapi_icmp_tmpl_encap_ext(tmpl, src_eth, dst_eth, src_addr,
+                                     dst_addr, -1, -1, af, icmp_type,
+                                     icmp_code, NULL, icmp_tmpl);
+}
+
 #ifdef __cplusplus
 } /* extern "C" */
 #endif

lib/tapi_tad/ipstack/tapi_icmp4.c
@@ -332,3 +332,42 @@ tapi_icmp_ip4_csap_create(const char    *ta_name,
 
     return TE_RC(TE_TAPI, rc);
 }
+
+te_errno
+tapi_icmp4_wrap_tmpl(asn_value *tmpl,
+                    const void *src_eth, const void *dst_eth,
+                    uint8_t *ip4_src, uint8_t *ip4_dst,
+                    int ip4_ttl, int ip4_tos,
+                    uint8_t icmp_type, uint8_t icmp_code)
+{
+    uint16_t eth_type = ETHERTYPE_IP;
+    te_errno rc;
+
+    rc = tapi_icmp4_add_pdu(&tmpl, NULL, false, icmp_type, icmp_code);
+    if (rc != 0)
+    {
+        ERROR("Failed to add icmp4 PDU: %r", rc);
+        return rc;
+    }
+
+    rc = tapi_ip4_add_pdu(&tmpl, NULL, false,
+                          *(in_addr_t *)ip4_src,
+                          *(in_addr_t *)ip4_dst,
+                          IPPROTO_ICMP, ip4_ttl, ip4_tos);
+    if (rc != 0)
+    {
+        ERROR("Failed to add inner ipv4 PDU: %r", rc);
+        return rc;
+    }
+
+    rc = tapi_eth_add_pdu(&tmpl, NULL, false,
+                          dst_eth, src_eth, &eth_type,
+                          TE_BOOL3_ANY, TE_BOOL3_FALSE);
+    if (rc != 0)
+    {
+        ERROR("Failed to add eth PDU: %r", rc);
+        return rc;
+    }
+
+    return 0;
+}

lib/tapi_tad/ipstack/tapi_icmp4.h
@@ -224,6 +224,27 @@ tapi_icmp_ip4_csap_create(const char    *ta_name,
                           in_addr_t      dst_addr,
                           csap_handle_t *icmp_csap);
 
+/**
+ * Encapsulate packet template in ICMPv4 header, adding required IPv4 and
+ * Ethernet layers.
+ *
+ * @param[in,out] tmpl      Packet template to encapsulate.
+ * @param[in]     src_eth   Source MAC address for the Ethernet header.
+ * @param[in]     dst_eth   Destination MAC address for the Ethernet header.
+ * @param[in]     ip4_src   Source IPv4 address (in network byte order).
+ * @param[in]     ip4_dst   Destination IPv4 address (in network byte order).
+ * @param[in]     ip4_ttl   TTL value for the IPv4 header.
+ * @param[in]     ip4_tos   Type of Service field for the IPv4 header.
+ * @param[in]     icmp_type ICMPv4 type.
+ * @param[in]     icmp_code ICMPv4 code.
+ *
+ * @return Status code.
+ */
+extern te_errno tapi_icmp4_wrap_tmpl(asn_value *tmpl, const void *src_eth,
+                                     const void *dst_eth, uint8_t *ip4_src,
+                                     uint8_t *ip4_dst, int ip4_ttl,
+                                     int ip4_tos, uint8_t icmp_type,
+                                     uint8_t icmp_code);
 #ifdef __cplusplus
 } /* extern "C" */
 #endif

lib/tapi_tad/ipstack/tapi_icmp6.c
@@ -407,3 +407,39 @@ tapi_tcp_ip6_icmp_ip6_eth_csap_create(
                                     IPPROTO_TCP, tcp_csap);
 }
 
+te_errno
+tapi_icmp6_wrap_tmpl(asn_value *tmpl,
+                     const void *src_eth, const void *dst_eth,
+                     uint8_t *ip6_src, uint8_t *ip6_dst, int hop_limit,
+                     uint8_t icmp_type, uint8_t icmp_code,
+                     icmp6_msg_body *msg_body)
+{
+    uint16_t eth_type = ETHERTYPE_IPV6;
+    te_errno rc = 0;
+
+    rc = tapi_icmp6_add_pdu(&tmpl, NULL, false, icmp_type, icmp_code,
+                            msg_body, NULL);
+    if (rc != 0)
+    {
+        ERROR("Failed to add icmp6 PDU: %r", rc);
+        return rc;
+    }
+
+    rc = tapi_ip6_add_pdu(&tmpl, NULL, false, ip6_src, ip6_dst,
+                          IPPROTO_ICMPV6, hop_limit);
+    if (rc != 0)
+    {
+        ERROR("Failed to add inner ip6 PDU: %r", rc);
+        return rc;
+    }
+
+    rc = tapi_eth_add_pdu(&tmpl, NULL, false, dst_eth, src_eth, &eth_type,
+                          TE_BOOL3_ANY, TE_BOOL3_FALSE);
+    if (rc != 0)
+    {
+        ERROR("Failed to add eth PDU: %r", rc);
+        return rc;
+    }
+
+    return 0;
+}

lib/tapi_tad/ipstack/tapi_icmp6.h
@@ -302,6 +302,28 @@ extern te_errno tapi_tcp_ip6_icmp_ip6_eth_csap_create(
                             const struct sockaddr_in6  *msg_rem_saddr,
                             csap_handle_t              *tcp_csap);
 
+/**
+ * Encapsulate packet template in ICMPv6 header, adding required IPv6 and
+ * Ethernet layers.
+ *
+ * @param[in,out] tmpl       Packet template to encapsulate.
+ * @param[in]     src_eth    Source MAC address for the Ethernet header.
+ * @param[in]     dst_eth    Destination MAC address for the Ethernet header.
+ * @param[in]     ip6_src    Source IPv6 address (in network byte order).
+ * @param[in]     ip6_dst    Destination IPv6 address (in network byte order).
+ * @param[in]     hop_limit  Hop Limit value for the IPv6 header.
+ * @param[in]     icmp_type  ICMPv6 type.
+ * @param[in]     icmp_code  ICMPv6 code.
+ * @param[in]     msg_body   Optional ICMPv6 message body (may be @c NULL).
+ *
+ * @return Status code.
+ */
+extern te_errno tapi_icmp6_wrap_tmpl(asn_value *tmpl, const void *src_eth,
+                                     const void *dst_eth, uint8_t *ip6_src,
+                                     uint8_t *ip6_dst, int hop_limit,
+                                     uint8_t icmp_type, uint8_t icmp_code,
+                                     icmp6_msg_body *msg_body);
+
 #ifdef __cplusplus
 } /* extern "C" */
 #endif

commit 68720172ee9b63405b4e23fe546e3cea373842da

lib/tapi_tad: fix memory leak while ICMP encapsulation
Fixes: 442c2526aabb ("lib/tapi_tad: add helpers for ICMP packet encapsulation")
Signed-off-by: Denis Pryazhennikov <denis.pryazhennikov@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>

lib/tapi_tad/eth/tapi_eth.c
@@ -530,7 +530,7 @@ tapi_eth_header_free(asn_value *tmpl, size_t *n_tags, uint16_t *vid,
     asn_value        *pdus = NULL;
     asn_value        *eth = NULL;
     unsigned int      n_hdrs;
-    te_errno          rc;
+    te_errno          rc = 0;
 
     if (tmpl == NULL)
         return 0;
@@ -560,15 +560,18 @@ tapi_eth_header_free(asn_value *tmpl, size_t *n_tags, uint16_t *vid,
     if (rc != 0)
     {
         ERROR("Failed to read VLAN TCI");
-        return rc;
+        goto out;
     }
 
     rc = asn_remove_indexed(pdus, eth_hdrs[n_hdrs - 1].index, "");
     if (rc != 0)
     {
         ERROR("Failed to remove ethernet header from template: %r", rc);
-        return rc;
+        goto out;
     }
 
-    return 0;
+out:
+    free(eth_hdrs);
+
+    return rc;
 }