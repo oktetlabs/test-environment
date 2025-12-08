20705841bd6899fe0f0c11ae76bd76ef6d9ab408
Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
2025-12-08 13:53:03 +0300

    lib/tapi_packetdrill: remove unnecessary return value checks
    te_string_append() always returns 0.
    
    Signed-off-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
    Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>

--- lib/tapi_packetdrill/tapi_packetdrill.c
+++ lib/tapi_packetdrill/tapi_packetdrill.c
@@ -103,10 +103,9 @@ copy_test(tapi_packetdrill_opts *opts, tapi_packetdrill_app *app)
 
     CHECK_RC(cfg_get_instance_string_fmt(&agt_dir, "/agent:%s/dir:", ta));
 
-    CHECK_RC(te_string_append(&src_path, "%s/%s", opts->src_test_dir,
-                              opts->short_test_name));
-    CHECK_RC(te_string_append(&dst_path, "%s/%s", agt_dir,
-                              opts->short_test_name));
+    te_string_append(&src_path, "%s/%s", opts->src_test_dir,
+                     opts->short_test_name);
+    te_string_append(&dst_path, "%s/%s", agt_dir, opts->short_test_name);
     if (access(src_path.ptr, F_OK) < 0)
     {
         TEST_FAIL("There is no such test: %s, errno=%r", opts->short_test_name,
