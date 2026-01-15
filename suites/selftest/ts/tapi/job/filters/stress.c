oktetlabs/test-environment

commit ca6742df27e4aa0d900540a26945de4250d59d61

suites/selftest: provide tests for job filters
The filter functionality in Job Management subsystem
is pretty complex, yet it lacked thorough dedicated
testing. This patch provides an initial set of such
tests.

OL-Redmine-Id: 14456
Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

suites/selftest/ts/tapi/job/filters/stress.c
@@ -1,0 +1,211 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (C) 2025 OKTET Labs Ltd. All rights reserved. */
+/** @file
+ * @brief TAPI Job filters test: stress testing
+ *
+ * TAPI Job filters test: stress testing
+ */
+
+/** @page filters-stress Stress testing TAPI Job filters
+ *
+ * @objective Verify that TAPI Job does not break on large random data.
+ *
+ * @param minlen                 Minimum length of a single line.
+ * @param maxlen                 Maximum length of a single line.
+ * @param n_false_starts         Number of partial matches that do not end
+ *                               in a full match before a full match.
+ * @param num_matches            Number of matches.
+ * @param extract                Subexpression to extract (must be 0, 1 or 2).
+ * @param lookbehind             Use a regexp with a lookbehind constraint.
+ * @param wait_before_receive    Wait for the job completion before trying
+ *                               to receive matches.
+ * @param ascii                  Use only ASCII characters if true, or
+ *                               the full 8-bit range if false.
+ *
+ * @par Scenario:
+ *
+ */
+
+#define TE_TEST_NAME "filters/stress"
+
+#include "filters_suite.h"
+#include "tapi_file.h"
+#include "te_rand.h"
+
+#define NEEDLE_PFX "<<< "
+#define NEEDLE_1 "Find"
+#define NEEDLE_SEP
+#define NEEDLE_2 "This"
+#define NEEDLE_SFX ">>>"
+#define NEEDLE NEEDLE_PFX NEEDLE_1 NEEDLE_SEP NEEDLE_2 NEEDLE_SFX
+#define CONTEXT "After That --->"
+
+#define NEEDLE_RE \
+    NEEDLE_PFX "(" NEEDLE_1 ")" NEEDLE_SEP "(" NEEDLE_2 ")" NEEDLE_SFX
+
+#define FAKE_NEEDLE \
+    CONTEXT NEEDLE_PFX NEEDLE_1 NEEDLE_SEP
+
+static void
+make_chunks(const char *ta, const char *filename,
+            size_t minlen, size_t maxlen,
+            size_t n_false_starts, size_t num_matches,
+            bool ascii)
+{
+    const char *spec = ascii ? "[\x1-\x7F^\n]" : "[^`\0\n]";
+    tapi_file_chunk_spec body[] = {
+        {
+            .kind = TAPI_FILE_CHUNK_SPEC_KIND_COMPOUND,
+            .minlen = num_matches,
+            .maxlen = num_matches,
+            .u = {
+                .nested = (const tapi_file_chunk_spec[]){
+                    {
+                        .kind = TAPI_FILE_CHUNK_SPEC_KIND_COMPOUND,
+                        .minlen = n_false_starts,
+                        .maxlen = n_false_starts,
+                        .u = {
+                            .nested = (const tapi_file_chunk_spec[]){
+                                {
+                                    .kind = TAPI_FILE_CHUNK_SPEC_KIND_PATTERN,
+                                    .minlen = minlen,
+                                    .maxlen = maxlen,
+                                    .u = {.spec = spec},
+                                },
+                                {
+                                    .kind = TAPI_FILE_CHUNK_SPEC_KIND_LITERAL,
+                                    .u = {.spec = FAKE_NEEDLE},
+                                },
+                                {.kind = TAPI_FILE_CHUNK_SPEC_KIND_END}
+                            }
+                        }
+                    },
+                    {
+                        .kind = TAPI_FILE_CHUNK_SPEC_KIND_PATTERN,
+                        .minlen = minlen,
+                        .maxlen = maxlen,
+                        .u = {.spec = spec},
+                    },
+                    {
+                        .kind = TAPI_FILE_CHUNK_SPEC_KIND_LITERAL,
+                        .u = {.spec = CONTEXT NEEDLE},
+                    },
+                    {.kind = TAPI_FILE_CHUNK_SPEC_KIND_END}
+                }
+            },
+        },
+        {.kind = TAPI_FILE_CHUNK_SPEC_KIND_END}
+    };
+
+    CHECK_RC(tapi_file_create_by_spec_ta(ta, filename, body));
+}
+
+int
+main(int argc, char **argv)
+{
+    rcf_rpc_server *pco_iut = NULL;
+    tapi_job_factory_t *factory = NULL;
+    size_t minlen;
+    size_t maxlen;
+    size_t n_false_starts;
+    size_t num_matches;
+    unsigned int extract;
+    bool lookbehind;
+    bool wait_before_receive;
+    bool ascii;
+    static const char *expected[] = {
+        [0] = NEEDLE,
+        [1] = NEEDLE_1,
+        [2] = NEEDLE_2,
+    };
+
+    tapi_job_t *sed_job = NULL;
+    tapi_job_channel_t *filter_handle = NULL;
+    tapi_job_channel_t *output_channel = NULL;
+
+    tapi_job_status_t status;
+    size_t i;
+    te_string rfile = TE_STRING_INIT;
+    tapi_job_buffer_t buffer = TAPI_JOB_BUFFER_INIT;
+
+    TEST_START;
+
+    TEST_GET_PCO(pco_iut);
+    TEST_GET_UINT_PARAM(minlen);
+    TEST_GET_UINT_PARAM(maxlen);
+    TEST_GET_UINT_PARAM(n_false_starts);
+    TEST_GET_UINT_PARAM(num_matches);
+    TEST_GET_UINT_PARAM(extract);
+    TEST_GET_BOOL_PARAM(lookbehind);
+    TEST_GET_BOOL_PARAM(wait_before_receive);
+    TEST_GET_BOOL_PARAM(ascii);
+
+    TEST_STEP("Initialize factory");
+    CHECK_RC(tapi_job_factory_rpc_create(pco_iut, &factory));
+
+    TEST_STEP("Create data dumping job");
+    tapi_file_make_name(&rfile);
+
+    CHECK_RC(tapi_job_simple_create(factory, &(tapi_job_simple_desc_t){
+                .program = "/usr/bin/sed",
+                .argv = (const char *[]){"sed", "-e", "", "-u",
+                                         rfile.ptr, NULL},
+                .job_loc = &sed_job,
+                .stdout_loc = &output_channel,
+                .filters = TAPI_JOB_SIMPLE_FILTERS(
+                    {.readable = true,
+                     .use_stdout = true,
+                     .re = (lookbehind ?
+                            "(?<=" CONTEXT ")" NEEDLE_RE :
+                            NEEDLE_RE),
+                     .extract = extract,
+                     .filter_var = &filter_handle,
+                    }),
+            }));
+
+    TEST_STEP("Create the data file");
+    make_chunks(pco_iut->ta, rfile.ptr, minlen, maxlen, n_false_starts,
+                num_matches, ascii);
+
+    TEST_STEP("Start the job");
+    CHECK_RC(tapi_job_start(sed_job));
+
+    if (wait_before_receive)
+        CHECK_RC(tapi_job_wait(sed_job, -1, &status));
+
+    TEST_STEP("Get the matching output");
+    for (i = 0; i < num_matches; i++)
+    {
+        tapi_job_simple_receive(TAPI_JOB_CHANNEL_SET(filter_handle),
+                                wait_before_receive ? 0 : -1,
+                                &buffer);
+        if (buffer.eos)
+            TEST_VERDICT("Not enough messages");
+
+        if (strcmp(expected[extract], buffer.data.ptr) != 0)
+        {
+            TEST_VERDICT("The %zu'th  matched string differs "
+                         "from the expected one", i);
+        }
+    }
+
+    tapi_job_simple_receive(TAPI_JOB_CHANNEL_SET(filter_handle), 0, &buffer);
+    if (!buffer.eos)
+        TEST_VERDICT("Too many messages");
+
+    if (!wait_before_receive)
+        CHECK_RC(tapi_job_wait(sed_job, -1, &status));
+
+    if (status.type != TAPI_JOB_STATUS_EXITED || status.value != 0)
+        TEST_VERDICT("The script did not terminate correctly");
+
+    TEST_SUCCESS;
+
+cleanup:
+    CLEANUP_CHECK_RC(tapi_job_destroy(sed_job, -1));
+    tapi_job_factory_destroy(factory);
+    te_string_free(&buffer.data);
+    te_string_free(&rfile);
+
+    TEST_END;
+}

lib/ta_job/ta_job.c
@@ -1161,6 +1161,7 @@ out:
         return te_rc;
     }
 
+    assert(future_start_offset <= subject_length);
     cut = MAX(future_start_offset - regexp->max_lookbehind, 0);
     filter->start_offset = future_start_offset - cut;
     if (cut != 0)

suites/selftest/conf/trc/tapi/job.trc.xml
@@ -1,6 +1,6 @@
 <?xml version="1.0"?>
 <!-- SPDX-License-Identifier: Apache-2.0 -->
-<!-- Copyright (C) 2022-2022 OKTET Labs Ltd. All rights reserved. -->
+<!-- Copyright (C) 2022-2025 OKTET Labs Ltd. All rights reserved. -->
 <test name="job" type="package">
     <objective>Test TAPI Job implemetation</objective>
     <iter result="PASSED">
@@ -20,6 +20,73 @@
             <objective>Check support of TAPI Job operations for jobs created by CFG factory</objective>
 
             <iter result="PASSED"/>
+        </test>
+
+        <test name="filters" type="package">
+            <objective>Test TAPI Job filters</objective>
+            <iter result="PASSED">
+                <test name="single_filter" type="script">
+                    <objective>Validate tapi_job_receive() in various circumstances.</objective>
+                    <notes/>
+                    <iter result="PASSED">
+                        <arg name="env" />
+                        <arg name="filter" />
+                        <arg name="use_stdout" />
+                        <arg name="delay" />
+                        <arg name="wait_before_receive" />
+                    </iter>
+                </test>
+
+                <test name="stress" type="script">
+                    <objective>Verify that TAPI Job does not break on large random data.</objective>
+                    <notes/>
+                    <iter result="PASSED">
+                        <arg name="env" />
+                        <arg name="minlen" />
+                        <arg name="maxlen" />
+                        <arg name="n_false_starts" />
+                        <arg name="num_matches" />
+                        <arg name="extract" />
+                        <arg name="lookbehind" />
+                        <arg name="wait_before_receive" />
+                        <arg name="ascii" />
+                    </iter>
+                </test>
+
+                <test name="binary" type="script">
+                    <objective>Verify that TAPI Job does not break on large random data.</objective>
+                    <notes/>
+                    <iter result="PASSED">
+                        <arg name="env" />
+                        <arg name="minlen" />
+                        <arg name="maxlen" />
+                        <arg name="block_size" />
+                        <arg name="binary_needle" />
+                        <results>
+                            <result key="OL 14466" notes="ta_job cannot handle binary data" value="FAILED">
+                            </result>
+                        </results>
+                    </iter>
+                </test>
+
+                <test name="utf8" type="script">
+                    <objective>Verify that TAPI Job can handle UTF-8 data properly.</objective>
+                    <notes/>
+                    <iter result="PASSED">
+                        <arg name="env" />
+                        <arg name="minlen" />
+                        <arg name="maxlen" />
+                        <arg name="block_size" />
+                        <arg name="utf8_len" />
+                        <results>
+                            <result key="OL 14467" notes="invalid handling of UTF-8" value="FAILED">
+                            </result>
+                            <result key="OL 14467" notes="but sometimes it works" value="PASSED">
+                            </result>
+                        </results>
+                    </iter>
+                </test>
+            </iter>
         </test>
     </iter>
 </test>

suites/selftest/ts/tapi/job/filters/binary.c
@@ -1,0 +1,146 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (C) 2025 OKTET Labs Ltd. All rights reserved. */
+/** @file
+ * @brief TAPI Job filters test: binary data
+ *
+ * TAPI Job filters test: binary data
+ */
+
+/** @page filters-binary Testing filters on binary data
+ *
+ * @objective Verify that TAPI Job can handle binary data properly
+ *
+ * @param minlen                 Mininum length of a random chunk.
+ * @param maxlen                 Maximum length of a random chunk.
+ * @param block_size             Block size of the output.
+ * @param binary_needle          Use binary zeroes in the needle string
+ *                               as well as in context.
+ *
+ * @par Scenario:
+ *
+ */
+
+#define TE_TEST_NAME "filters/binary"
+
+#include "filters_suite.h"
+#include "tapi_file.h"
+
+#define NEEDLE_PFX "<<< Find Me: "
+#define NEEDLE_SFX ">>>"
+
+#define NEEDLE_RE NEEDLE_PFX "[^>]+" NEEDLE_SFX
+
+int
+main(int argc, char **argv)
+{
+    rcf_rpc_server *pco_iut = NULL;
+    tapi_job_factory_t *factory = NULL;
+    size_t minlen;
+    size_t maxlen;
+    size_t block_size;
+    bool binary_needle;
+    tapi_job_t *dd_job = NULL;
+    tapi_job_channel_t *filter_handle = NULL;
+    tapi_job_channel_t *output_channel = NULL;
+    size_t needle_len;
+    char *needle = NULL;
+
+    tapi_job_status_t status;
+    te_string rfile = TE_STRING_INIT;
+    te_string dd_if_param = TE_STRING_INIT;
+    te_string dd_bs_param = TE_STRING_INIT;
+    te_string matched = TE_STRING_INIT;
+
+    TEST_START;
+
+    TEST_GET_PCO(pco_iut);
+    TEST_GET_UINT_PARAM(minlen);
+    TEST_GET_UINT_PARAM(maxlen);
+    TEST_GET_UINT_PARAM(block_size);
+    TEST_GET_BOOL_PARAM(binary_needle);
+
+    TEST_STEP("Initialize factory");
+    CHECK_RC(tapi_job_factory_rpc_create(pco_iut, &factory));
+
+    TEST_STEP("Create data dumping job");
+    tapi_file_make_name(&rfile);
+
+    te_string_append(&dd_if_param, "if=%s", rfile.ptr);
+    te_string_append(&dd_bs_param, "bs=%zu", block_size);
+
+    CHECK_RC(tapi_job_simple_create(factory, &(tapi_job_simple_desc_t){
+                .program = "/usr/bin/dd",
+                .argv = (const char *[]){"dd",
+                                         dd_if_param.ptr,
+                                         dd_bs_param.ptr,
+                                         NULL},
+                .job_loc = &dd_job,
+                .stdout_loc = &output_channel,
+                .filters = TAPI_JOB_SIMPLE_FILTERS(
+                    {.readable = true,
+                     .use_stdout = true,
+                     .re = NEEDLE_RE,
+                     .extract = 0,
+                     .filter_var = &filter_handle,
+                    }),
+            }));
+
+    TEST_STEP("Create the data file");
+    needle = te_make_spec_buf(minlen, maxlen, &needle_len,
+                              binary_needle ?
+                              NEEDLE_PFX "([^>])" NEEDLE_SFX :
+                              NEEDLE_PFX  "([^`\0>])" NEEDLE_PFX);
+
+    CHECK_RC(tapi_file_create_by_spec_ta(
+                 pco_iut->ta, rfile.ptr,
+                 (const tapi_file_chunk_spec[]){
+                     {
+                         .kind = TAPI_FILE_CHUNK_SPEC_KIND_PATTERN,
+                         .minlen = minlen,
+                         .maxlen = maxlen,
+                         .u = {.spec = "[]"},
+                     },
+                     {
+                         .kind = TAPI_FILE_CHUNK_SPEC_KIND_LITERAL,
+                         .maxlen = needle_len,
+                         .u = {.spec = needle},
+                     },
+                     {
+                         .kind = TAPI_FILE_CHUNK_SPEC_KIND_PATTERN,
+                         .minlen = minlen,
+                         .maxlen = maxlen,
+                         .u = {.spec = "[]"},
+                     },
+                     {.kind = TAPI_FILE_CHUNK_SPEC_KIND_END}
+                 }));
+
+    TEST_STEP("Start the job");
+    CHECK_RC(tapi_job_start(dd_job));
+
+    TEST_STEP("Get the matching output");
+    CHECK_RC(tapi_job_receive_single(filter_handle, &matched, -1));
+
+    if (!te_compare_bufs(needle, needle_len, 1, matched.ptr, matched.len,
+                         TE_LL_ERROR))
+    {
+        TEST_VERDICT("Invalid matched string");
+    }
+
+    CHECK_RC(tapi_job_wait(dd_job, -1, &status));
+
+    if (status.type != TAPI_JOB_STATUS_EXITED || status.value != 0)
+        TEST_VERDICT("The script did not terminate correctly");
+
+    TEST_SUCCESS;
+
+cleanup:
+    CLEANUP_CHECK_RC(tapi_job_destroy(dd_job, -1));
+    tapi_job_factory_destroy(factory);
+    free(needle);
+    te_string_free(&matched);
+    te_string_free(&dd_if_param);
+    te_string_free(&dd_bs_param);
+    te_string_free(&rfile);
+
+    TEST_END;
+}

suites/selftest/ts/tapi/job/filters/filters_suite.h
@@ -1,0 +1,24 @@
+#ifndef __FILTERS_SUITE_H__
+#define __FILTERS_SUITE_H__ 1
+
+#ifndef TEST_START_VARS
+#define TEST_START_VARS TEST_START_ENV_VARS
+#endif
+
+#ifndef TEST_START_SPECIFIC
+#define TEST_START_SPECIFIC TEST_START_ENV
+#endif
+
+#ifndef TEST_END_SPECIFIC
+#define TEST_END_SPECIFIC TEST_END_ENV
+#endif
+
+#include "te_config.h"
+
+#include "tapi_job.h"
+#include "tapi_job_factory_rpc.h"
+#include "tapi_test.h"
+#include "tapi_env.h"
+#include "te_string.h"
+
+#endif /* __FILTERS_SUITE_H__ */

suites/selftest/ts/tapi/job/filters/meson.build
@@ -1,10 +1,11 @@
 # SPDX-License-Identifier: Apache-2.0
-# Copyright (C) 2022-2025 OKTET Labs Ltd. All rights reserved.
+# Copyright (C) 2025 OKTET Labs Ltd. All rights reserved.
 
 tests = [
-    'cfg_basic_operations',
-    'epilogue',
-    'prologue',
+  'binary',
+  'single_filter',
+  'stress',
+  'utf8',
 ]
 
 foreach test : tests
@@ -23,14 +24,3 @@ tests_info_xml = custom_target(package_dir.underscorify() + 'tests-info-xml',
                                           meson.current_source_dir() ])
 
 install_data([ 'package.xml' ], install_dir: package_dir)
-
-packages = [
-    'filters',
-]
-
-tools_mydir = package_dir
-foreach package : packages
-    package_dir = join_paths(tools_mydir, package)
-    package_tests_c = []
-    subdir(package)
-endforeach

suites/selftest/ts/tapi/job/filters/package.xml
@@ -1,0 +1,107 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- SPDX-License-Identifier: Apache-2.0 -->
+<!-- Copyright (C) 2025 OKTET Labs Ltd. All rights reserved. -->
+<package version="1.0">
+    <description>TAPI Job Filters self-tests</description>
+    <author mailto="te-maint@oktetlabs.ru"/>
+    <session>
+        <var name="env">
+            <value>{{{'pco_iut':IUT}}}</value>
+        </var>
+        <run>
+            <script name="single_filter"/>
+            <arg name="env" ref="env" />
+            <arg name="delay">
+                <value>0</value>
+                <value>1</value>
+            </arg>
+            <arg name="use_stdout" type="boolean" />
+            <arg name="wait_before_receive"  type="boolean" />
+            <arg name="filter">
+                <value>
+                    <field name="chunks">simple</field>
+                    <field name="regexp">simple</field>
+                    <field name="extract">0</field>
+                    <field name="expected">simple</field>
+                    <field name="newline">TRUE</field>
+                </value>
+                <value>
+                    <field name="chunks">prefix sim,sim,ple</field>
+                    <field name="regexp">simple</field>
+                    <field name="extract">0</field>
+                    <field name="expected">simple</field>
+                    <field name="newline">FALSE</field>
+                </value>
+                <value>
+                    <field name="chunks">start,start,start</field>
+                    <field name="regexp">^start</field>
+                    <field name="extract">0</field>
+                    <field name="expected">start,start,start</field>
+                    <field name="newline">TRUE</field>
+                </value>
+                <value>
+                    <field name="chunks">value(1),va,lue(2),value,(3),value(,4)</field>
+                    <field name="regexp">value\((\d+)\)</field>
+                    <field name="extract">1</field>
+                    <field name="expected">1,2,3,4</field>
+                    <field name="newline">FALSE</field>
+                </value>
+                <value>
+                    <field name="chunks">value 1 2 3 4 5 6 7 8 9,va,lue 11 12 13 14 15 16 17 18 19</field>
+                    <field name="regexp">value (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+)</field>
+                    <field name="extract">9</field>
+                    <field name="expected">9,19</field>
+                    <field name="newline">FALSE</field>
+                </value>
+                <value>
+                    <field name="chunks">prefix 123,other 123,prefix 4,56,pre,fix 789</field>
+                    <field name="regexp">(?&lt;=prefix )\d+</field>
+                    <field name="extract">0</field>
+                    <field name="expected">123,456,789</field>
+                    <field name="newline">FALSE</field>
+                </value>
+            </arg>
+        </run>
+        <run>
+            <script name="stress"/>
+            <arg name="env" ref="env" />
+            <arg name="minlen"><value>1</value></arg>
+            <arg name="maxlen"><value>65536</value></arg>
+            <arg name="n_false_starts"><value>2</value></arg>
+            <arg name="num_matches"><value>1000</value></arg>
+            <arg name="extract">
+                <value>0</value>
+                <value>1</value>
+                <value>2</value>
+            </arg>
+            <arg name="lookbehind"  type="boolean" />
+            <arg name="wait_before_receive"  type="boolean" />
+            <arg name="ascii"  type="boolean" />
+        </run>
+
+        <run>
+            <script name="binary" />
+            <arg name="env" ref="env" />
+            <arg name="minlen"><value>1024</value></arg>
+            <arg name="maxlen"><value>8192</value></arg>
+            <arg name="block_size"><value>512</value></arg>
+            <arg name="binary_needle" type="boolean" />
+        </run>
+
+        <run>
+            <script name="utf8" />
+            <arg name="env" ref="env" />
+            <arg name="minlen"><value>1024</value></arg>
+            <arg name="maxlen"><value>8192</value></arg>
+            <arg name="utf8_len">
+                <value>2</value>
+                <value>3</value>
+                <value>4</value>
+            </arg>
+            <arg name="block_size">
+                <value>521</value>
+                <value>600</value>
+            </arg>
+        </run>
+    </session>
+</package>

suites/selftest/ts/tapi/job/filters/single_filter.c
@@ -1,0 +1,170 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (C) 2025 OKTET Labs Ltd. All rights reserved. */
+/** @file
+ * @brief TAPI Job filters test: single filter
+ *
+ * TAPI Job filters test: single filter
+ */
+
+/** @page filters-single-filter Test for a single filter in TAPI Job filters.
+ *
+ * @objective Validate tapi_job_receive() in various circumstances.
+ *
+ * @param filter_chunks        List of output segments.
+ * @param filter_newline       Separate output segments with newline if true.
+ * @param filter_regexp        Regexp to look for.
+ * @param filter_extract       Regexp group to extract
+ *                             (0 means the whole mathed substring).
+ * @param use_stdout           Use stdout if true, otherwise stderr.
+ * @param delay                Delay in seconds between output segments.
+ * @param wait_before_receive  Wait for the job completion before
+ *                             trying to get matches.
+ *
+ * @par Scenario:
+ *
+ */
+
+#define TE_TEST_NAME "filters/match_single"
+
+#include "filters_suite.h"
+
+static void
+build_script(te_string *dest, size_t n_lines, char **lines,
+             unsigned int delay, bool use_stdout, bool newline)
+{
+    size_t i;
+
+    for (i = 0; i < n_lines; i++)
+    {
+        if (delay > 0)
+            te_string_append(dest, "sleep %u; ", delay);
+        te_string_append(dest, "echo %s",
+                         newline || i + 1 == n_lines ? "" : "-n ");
+        te_string_append_shell_arg_as_is(dest, lines[i]);
+        te_string_append(dest, "%s\n", use_stdout ? "" : " >&2");
+    }
+}
+
+int
+main(int argc, char **argv)
+{
+    rcf_rpc_server *pco_iut = NULL;
+    tapi_job_factory_t *factory = NULL;
+    char **filter_chunks;
+    size_t n_filter_chunks;
+    bool filter_newline;
+    const char *filter_regexp;
+    unsigned int filter_extract;
+    char **filter_expected;
+    size_t n_filter_expected;
+    bool use_stdout;
+
+    unsigned int delay;
+    bool wait_before_receive;
+
+    tapi_job_t *shell_job = NULL;
+    tapi_job_channel_t *filter_handle = NULL;
+    tapi_job_channel_t *output_channel = NULL;
+    tapi_job_simple_filter_t filter_desc[] = {
+        {.readable = true,
+         .log_level = TE_LL_RING,
+         .filter_var = &filter_handle,
+        },
+        {.use_stdout = false, .use_stderr = false},
+    };
+    tapi_job_simple_desc_t desc = {
+        .program = "/bin/sh",
+        .job_loc = &shell_job,
+        .filters = filter_desc,
+    };
+    tapi_job_status_t status;
+    te_string script = TE_STRING_INIT;
+    int timeout_ms;
+    size_t i;
+    tapi_job_buffer_t buffer = TAPI_JOB_BUFFER_INIT;
+
+    TEST_START;
+
+    TEST_GET_PCO(pco_iut);
+    TEST_GET_STRING_LIST_PARAM(filter_chunks, n_filter_chunks);
+    TEST_GET_BOOL_PARAM(filter_newline);
+    TEST_GET_STRING_PARAM(filter_regexp);
+    TEST_GET_UINT_PARAM(filter_extract);
+    TEST_GET_STRING_LIST_PARAM(filter_expected, n_filter_expected);
+    TEST_GET_BOOL_PARAM(use_stdout);
+    TEST_GET_INT_PARAM(delay);
+    TEST_GET_BOOL_PARAM(wait_before_receive);
+
+    timeout_ms = TE_SEC2MS(delay * n_filter_chunks + 1);
+
+    build_script(&script, n_filter_chunks, filter_chunks, delay,
+                 use_stdout, filter_newline);
+
+    desc.argv = (const char *[]){"sh", "-c", script.ptr, NULL};
+    filter_desc[0].use_stdout = use_stdout;
+    filter_desc[0].use_stderr = !use_stdout;
+    filter_desc[0].re = filter_regexp;
+    filter_desc[0].extract = filter_extract;
+    if (use_stdout)
+        desc.stdout_loc = &output_channel;
+    else
+        desc.stderr_loc = &output_channel;
+
+    TEST_STEP("Initialize factory");
+    CHECK_RC(tapi_job_factory_rpc_create(pco_iut, &factory));
+
+    TEST_STEP("Create scripting job");
+    CHECK_RC(tapi_job_simple_create(factory, &desc));
+
+    TEST_STEP("Start the job");
+    CHECK_RC(tapi_job_start(shell_job));
+
+    if (wait_before_receive)
+    {
+        TEST_STEP("Waiting for the job to complete");
+        CHECK_RC(tapi_job_wait(shell_job, timeout_ms, &status));
+    }
+
+    TEST_STEP("Get the matching output");
+    for (i = 0; i < n_filter_expected; i++)
+    {
+        tapi_job_simple_receive(TAPI_JOB_CHANNEL_SET(filter_handle),
+                                wait_before_receive ? 0 : timeout_ms,
+                                &buffer);
+        if (buffer.eos)
+            TEST_VERDICT("Not enough messages");
+
+        if (strcmp(filter_expected[i], buffer.data.ptr) != 0)
+        {
+            TEST_VERDICT("The %zu'th  matched string differs "
+                         "from the expected one", i);
+        }
+    }
+
+    if (!wait_before_receive)
+    {
+        TEST_STEP("Waiting for the job to complete");
+        CHECK_RC(tapi_job_wait(shell_job, timeout_ms, &status));
+    }
+
+    tapi_job_simple_receive(TAPI_JOB_CHANNEL_SET(filter_handle), 0, &buffer);
+    if (!buffer.eos)
+        TEST_VERDICT("Too many messages");
+
+    if (status.type != TAPI_JOB_STATUS_EXITED || status.value != 0)
+        TEST_VERDICT("The script did not terminate correctly");
+
+    TEST_SUCCESS;
+
+cleanup:
+    CLEANUP_CHECK_RC(tapi_job_destroy(shell_job, -1));
+    tapi_job_factory_destroy(factory);
+    te_string_free(&buffer.data);
+    te_string_free(&script);
+
+    /* FIXME: there should be a more elegant way */
+    free(filter_chunks[0]);
+    free(filter_chunks);
+
+    TEST_END;
+}

commit 3910b91b671d9974d030b17e12d4e10abbb6f45e

suites/selftest: fix several iterations run failure
In tapi/job/filters/stress test: remove large file created by this test
after each iteration or several ierations run leads to ENOSPC.

Fixes: ca6742df27e4 ("suites/selftest: provide tests for job filters")
OL-Redmine-Id: 14731
Signed-off-by: Yurij Plotnikov <yurij.plotnikov@oktetlabs.ru>
Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>

suites/selftest/ts/tapi/job/filters/stress.c
@@ -113,6 +113,7 @@ main(int argc, char **argv)
     bool lookbehind;
     bool wait_before_receive;
     bool ascii;
+    bool file_created = false;
     static const char *expected[] = {
         [0] = NEEDLE,
         [1] = NEEDLE_1,
@@ -166,6 +167,7 @@ main(int argc, char **argv)
     TEST_STEP("Create the data file");
     make_chunks(pco_iut->ta, rfile.ptr, minlen, maxlen, n_false_starts,
                 num_matches, ascii);
+    file_created = true;
 
     TEST_STEP("Start the job");
     CHECK_RC(tapi_job_start(sed_job));
@@ -202,6 +204,8 @@ main(int argc, char **argv)
     TEST_SUCCESS;
 
 cleanup:
+    if (file_created)
+        CLEANUP_CHECK_RC(tapi_file_ta_unlink_fmt(pco_iut->ta, rfile.ptr));
     CLEANUP_CHECK_RC(tapi_job_destroy(sed_job, -1));
     tapi_job_factory_destroy(factory);
     te_string_free(&buffer.data);