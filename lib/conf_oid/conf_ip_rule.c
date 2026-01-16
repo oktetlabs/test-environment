oktetlabs/test-environment

commit a64e7992db29cb210ea10e79fda904e364d5633d

common: use standard ISO C boolean type
The boolean type is part of ISO C since C99 and
it is going to be part of the core language in C23,
so there is absolutely no sense in keeping a legacy
custom boolean type. The patch removes all uses of
te_bool and corresponding constants and mark its
definition as deprecated. No visible behaviour may
have changed, since the type was already defined as
an alias to ISO C bool.

OL-Redmine-Id: 13768
Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>

lib/conf_oid/conf_ip_rule.c
@@ -48,7 +48,7 @@ static const size_t ifnamesiz = IFNAMSIZ;
 static const te_conf_obj ip_rule[] = {
     ITEM(uint32_t, priority,   PRIORITY),
     ITEM(uint8_t,  family,     FAMILY  ),
-    ITEM(te_bool,  invert,     INVERT  ),
+    ITEM(bool,     invert,     INVERT  ),
     ITEM(uint32_t, type,       TYPE    ),
     ITEM(uint8_t,  srclen,     SRCLEN  ),
     ITEM(uint8_t,  dstlen,     DSTLEN  ),
@@ -105,7 +105,7 @@ te_conf_ip_rule_compare(uint32_t required, const te_conf_ip_rule *object_a,
 /* See the description in conf_ip_rule.h */
 void
 te_conf_ip_rule_set_invert(te_conf_ip_rule *rule,
-                           te_bool invert)
+                           bool invert)
 {
     rule->invert = invert;
     if (invert)
@@ -115,7 +115,7 @@ te_conf_ip_rule_set_invert(te_conf_ip_rule *rule,
 }
 
 /* See the description in conf_ip_rule.h */
-te_bool
+bool
 te_conf_ip_rule_get_invert(const te_conf_ip_rule *rule)
 {
     return !!(rule->mask & TE_IP_RULE_FLAG_INVERT);

lib/conf_oid/conf_ip_rule.h
@@ -28,7 +28,7 @@ extern "C" {
 typedef struct te_conf_ip_rule {
     uint8_t         family;             /**< Address family of rule */
     te_ip_type      type;               /**< Type of rule entry */
-    te_bool         invert;             /**< Inversion flag (works as
+    bool invert;             /**< Inversion flag (works as
                                              ip rule not ... ). */
     uint8_t         srclen;             /**< Prefix length of source */
     uint8_t         dstlen;             /**< Prefix length of destination */
@@ -124,19 +124,19 @@ extern te_conf_obj_compare_result te_conf_ip_rule_compare(
  * Set inversion flag for IP rule.
  *
  * @param rule    IP rule structure.
- * @param invert  If TRUE, set the flag; otherwise clear it.
+ * @param invert  If @c true, set the flag; otherwise clear it.
  */
 extern void te_conf_ip_rule_set_invert(te_conf_ip_rule *rule,
-                                       te_bool invert);
+                                       bool invert);
 
 /**
  * Get state of inversion flag for IP rule.
  *
  * @param rule    IP rule structure.
  *
- * @return TRUE if flag is set, FALSE otherwise.
+ * @return @c true if flag is set, @c false otherwise.
  */
-extern te_bool te_conf_ip_rule_get_invert(const te_conf_ip_rule *rule);
+extern bool te_conf_ip_rule_get_invert(const te_conf_ip_rule *rule);
 
 #ifdef __cplusplus
 } /* extern "C" */

lib/conf_oid/conf_object.c
@@ -19,7 +19,7 @@
 #include <arpa/inet.h>
 
 /**
- * Convert @b te_bool field to string.
+ * Convert boolean field to string.
  *
  * @param [in,out] str   String to append with converted value.
  * @param [in]    name   Field name.
@@ -31,14 +31,14 @@ static te_errno
 te_bool_to_str(te_string *str, const char *name,
                const void *arg)
 {
-    te_bool value = *(te_bool *)arg;
+    bool value = *(bool *)arg;
 
     return te_string_append(str, "%s=%s",
                             name, (value ? "true" : "false"));
 }
 
 /**
- * Obtain @b te_bool field value from string.
+ * Obtain boolean field value from string.
  *
  * @param [in]  value    String.
  * @param [out] arg      Pointer to the field value.
@@ -53,9 +53,9 @@ te_bool_from_str(const char *value, void *arg,
     UNUSED(options);
 
     if (strcmp(value, "true") == 0)
-        *(te_bool *)arg = TRUE;
+        *(bool *)arg = true;
     else if (strcmp(value, "false") == 0)
-        *(te_bool *)arg = FALSE;
+        *(bool *)arg = false;
     else
         return TE_EINVAL;
 
@@ -63,7 +63,7 @@ te_bool_from_str(const char *value, void *arg,
 }
 
 /**
- * Compare values of two @b te_bool fields.
+ * Compare values of two boolean fields.
  *
  * @param a     Pointer to the first field.
  * @param b     Pointer to the second field.
@@ -75,7 +75,7 @@ te_bool_from_str(const char *value, void *arg,
 static int
 te_bool_compare(const void *a, const void *b)
 {
-    return (!!(*(te_bool *)a) == !!(*(te_bool *)b));
+    return (!!(*(bool *)a) == !!(*(bool *)b));
 }
 
 /**
@@ -255,7 +255,7 @@ str_compare(const void *a, const void *b)
     return strcmp((const char*)a, (const char*)b) == 0;
 }
 
-te_conf_obj_methods te_conf_obj_methods_te_bool = {
+te_conf_obj_methods te_conf_obj_methods__Bool = {
     .to_str   = te_bool_to_str,
     .from_str = te_bool_from_str,
     .compare  = te_bool_compare,
@@ -447,9 +447,9 @@ te_conf_obj_compare(const te_conf_obj *fields, size_t fields_number,
 
     for (i = 0; i < fields_number; i++)
     {
-        const te_bool required_for_a = (required & fields[i].flag) != 0;
-        const te_bool filled_for_a = (mask_a  & fields[i].flag) != 0;
-        const te_bool filled_for_b = (mask_b  & fields[i].flag) != 0;
+        const bool required_for_a = (required & fields[i].flag) != 0;
+        const bool filled_for_a = (mask_a & fields[i].flag) != 0;
+        const bool filled_for_b = (mask_b & fields[i].flag) != 0;
 
         if (!required_for_a)
         {

lib/conf_oid/conf_object.h
@@ -130,7 +130,7 @@ typedef struct te_conf_obj {
 
 /** @name Declaration of common data types */
 /** @{ */
-extern te_conf_obj_methods te_conf_obj_methods_te_bool;
+extern te_conf_obj_methods te_conf_obj_methods__Bool;
 extern te_conf_obj_methods te_conf_obj_methods_uint8_t;
 extern te_conf_obj_methods te_conf_obj_methods_uint32_t;
 extern te_conf_obj_methods te_conf_obj_methods_sockaddr;

commit 94a1d3a2912efc24f519b886461322ffa1702f5c

lib/conf_oid: use a macro-safe boolean-like type
The reintroduced type te_bool is used instead of plain
bool to overcome unspecified behaviour in C23 regarding
using bool with macro concatenation and stringification.

Fixes: a64e7992db29 ("common: use standard ISO C boolean type")
OL-Redmine-Id: 14711
Signed-off-by: Artem Andreev <artem.andreev@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
Reviewed-by: Alexandra Kossovsky <alexandra.kossovsky@oktetlabs.ru>

lib/conf_oid/conf_ip_rule.c
@@ -48,7 +48,7 @@ static const size_t ifnamesiz = IFNAMSIZ;
 static const te_conf_obj ip_rule[] = {
     ITEM(uint32_t, priority,   PRIORITY),
     ITEM(uint8_t,  family,     FAMILY  ),
-    ITEM(bool,     invert,     INVERT  ),
+    ITEM(te_bool,  invert,     INVERT  ),
     ITEM(uint32_t, type,       TYPE    ),
     ITEM(uint8_t,  srclen,     SRCLEN  ),
     ITEM(uint8_t,  dstlen,     DSTLEN  ),
@@ -105,7 +105,7 @@ te_conf_ip_rule_compare(uint32_t required, const te_conf_ip_rule *object_a,
 /* See the description in conf_ip_rule.h */
 void
 te_conf_ip_rule_set_invert(te_conf_ip_rule *rule,
-                           bool invert)
+                           te_bool invert)
 {
     rule->invert = invert;
     if (invert)
@@ -115,7 +115,7 @@ te_conf_ip_rule_set_invert(te_conf_ip_rule *rule,
 }
 
 /* See the description in conf_ip_rule.h */
-bool
+te_bool
 te_conf_ip_rule_get_invert(const te_conf_ip_rule *rule)
 {
     return !!(rule->mask & TE_IP_RULE_FLAG_INVERT);

lib/conf_oid/conf_ip_rule.h
@@ -28,7 +28,7 @@ extern "C" {
 typedef struct te_conf_ip_rule {
     uint8_t         family;             /**< Address family of rule */
     te_ip_type      type;               /**< Type of rule entry */
-    bool invert;             /**< Inversion flag (works as
+    te_bool         invert;             /**< Inversion flag (works as
                                              ip rule not ... ). */
     uint8_t         srclen;             /**< Prefix length of source */
     uint8_t         dstlen;             /**< Prefix length of destination */
@@ -127,7 +127,7 @@ extern te_conf_obj_compare_result te_conf_ip_rule_compare(
  * @param invert  If @c true, set the flag; otherwise clear it.
  */
 extern void te_conf_ip_rule_set_invert(te_conf_ip_rule *rule,
-                                       bool invert);
+                                       te_bool invert);
 
 /**
  * Get state of inversion flag for IP rule.
@@ -136,7 +136,7 @@ extern void te_conf_ip_rule_set_invert(te_conf_ip_rule *rule,
  *
  * @return @c true if flag is set, @c false otherwise.
  */
-extern bool te_conf_ip_rule_get_invert(const te_conf_ip_rule *rule);
+extern te_bool te_conf_ip_rule_get_invert(const te_conf_ip_rule *rule);
 
 #ifdef __cplusplus
 } /* extern "C" */

lib/conf_oid/conf_object.c
@@ -31,7 +31,7 @@ static te_errno
 te_bool_to_str(te_string *str, const char *name,
                const void *arg)
 {
-    bool value = *(bool *)arg;
+    te_bool value = *(te_bool *)arg;
 
     te_string_append(str, "%s=%s", name, (value ? "true" : "false"));
     return 0;
@@ -53,9 +53,9 @@ te_bool_from_str(const char *value, void *arg,
     UNUSED(options);
 
     if (strcmp(value, "true") == 0)
-        *(bool *)arg = true;
+        *(te_bool *)arg = true;
     else if (strcmp(value, "false") == 0)
-        *(bool *)arg = false;
+        *(te_bool *)arg = false;
     else
         return TE_EINVAL;
 
@@ -75,7 +75,7 @@ te_bool_from_str(const char *value, void *arg,
 static int
 te_bool_compare(const void *a, const void *b)
 {
-    return (!!(*(bool *)a) == !!(*(bool *)b));
+    return (!!(*(te_bool *)a) == !!(*(te_bool *)b));
 }
 
 /**
@@ -257,7 +257,7 @@ str_compare(const void *a, const void *b)
     return strcmp((const char*)a, (const char*)b) == 0;
 }
 
-te_conf_obj_methods te_conf_obj_methods__Bool = {
+te_conf_obj_methods te_conf_obj_methods_te_bool = {
     .to_str   = te_bool_to_str,
     .from_str = te_bool_from_str,
     .compare  = te_bool_compare,

lib/conf_oid/conf_object.h
@@ -130,7 +130,7 @@ typedef struct te_conf_obj {
 
 /** @name Declaration of common data types */
 /** @{ */
-extern te_conf_obj_methods te_conf_obj_methods__Bool;
+extern te_conf_obj_methods te_conf_obj_methods_te_bool;
 extern te_conf_obj_methods te_conf_obj_methods_uint8_t;
 extern te_conf_obj_methods te_conf_obj_methods_uint32_t;
 extern te_conf_obj_methods te_conf_obj_methods_sockaddr;