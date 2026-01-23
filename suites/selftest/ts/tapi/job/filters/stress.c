oktetlabs/test-environment

commit 3910b91b671d9974d030b17e12d4e10abbb6f45e

suites/selftest: fix several iterations run failure
In tapi/job/filters/stress test: remove large file created by this test
after each iteration or several ierations run leads to ENOSPC.

Fixes: ca6742df27e4 ("suites/selftest: provide tests for job filters")
OL-Redmine-Id: 14731
Signed-off-by: Yurij Plotnikov <yurij.plotnikov@oktetlabs.ru>
Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>

suites/selftest/ts/tapi/job/filters/stress.c
@@ -113,6 +113,7 @@ main(int argc, char **argv)
     bool lookbehind;
     bool wait_before_receive;
     bool ascii;
+    bool file_created = false;
     static const char *expected[] = {
         [0] = NEEDLE,
         [1] = NEEDLE_1,
@@ -166,6 +167,7 @@ main(int argc, char **argv)
     TEST_STEP("Create the data file");
     make_chunks(pco_iut->ta, rfile.ptr, minlen, maxlen, n_false_starts,
                 num_matches, ascii);
+    file_created = true;
 
     TEST_STEP("Start the job");
     CHECK_RC(tapi_job_start(sed_job));
@@ -202,6 +204,8 @@ main(int argc, char **argv)
     TEST_SUCCESS;
 
 cleanup:
+    if (file_created)
+        CLEANUP_CHECK_RC(tapi_file_ta_unlink_fmt(pco_iut->ta, rfile.ptr));
     CLEANUP_CHECK_RC(tapi_job_destroy(sed_job, -1));
     tapi_job_factory_destroy(factory);
     te_string_free(&buffer.data);

commit 7d3af42f1ba60167f54162f7e085ed5e0f46db2e

suites/selftest: fix error in build on Ubuntu20.04
Fix calling of tapi_file_ta_unlink_fmt() function.

Fixes: 3910b91b671d ("suites/selftest: fix several iterations run failure")
OL-Redmine-Id: 14731
Signed-off-by: Yurij Plotnikov <yurij.plotnikov@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

suites/selftest/ts/tapi/job/filters/stress.c
@@ -205,7 +205,7 @@ main(int argc, char **argv)
 
 cleanup:
     if (file_created)
-        CLEANUP_CHECK_RC(tapi_file_ta_unlink_fmt(pco_iut->ta, rfile.ptr));
+        CLEANUP_CHECK_RC(tapi_file_ta_unlink_fmt(pco_iut->ta, "%s", rfile.ptr));
     CLEANUP_CHECK_RC(tapi_job_destroy(sed_job, -1));
     tapi_job_factory_destroy(factory);
     te_string_free(&buffer.data);