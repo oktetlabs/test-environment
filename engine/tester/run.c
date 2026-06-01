oktetlabs/test-environment

commit d56bcce39d3ff5c99034000a2a84f7ca7bdc4072

engine: replace malloc/calloc with TE allocators
TE_ALLOC usage avoids additional checks on the return value.

OL-Redmine-Id: 13239
Signed-off-by: Daniil Byshenko <daniil.byshenko@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>

engine/tester/run.c
@@ -754,7 +754,7 @@ tester_run_free_ctx(tester_ctx *ctx)
  * @param flags         Initial flags
  * @param targets       Initial target requirements expression
  *
- * @return Allocated context.
+ * @return Allocated context (cannot return @c NULL).
  */
 static tester_ctx *
 tester_run_new_ctx(tester_flags flags, const logic_expr *targets)
@@ -806,16 +806,12 @@ tester_run_new_ctx(tester_flags flags, const logic_expr *targets)
  * @param ctx           Tester context to be cloned.
  * @param new_group     Clone context for a new group
  *
- * @return Pointer to new Tester context.
+ * @return Pointer to new Tester context (cannot return @c NULL).
  */
 static tester_ctx *
 tester_run_clone_ctx(const tester_ctx *ctx, bool new_group)
 {
-    te_errno    rc;
     tester_ctx *new_ctx = tester_run_new_ctx(ctx->flags, ctx->targets);
-
-    if (new_ctx == NULL)
-        return NULL;
 
     if (new_group)
     {
@@ -835,13 +831,7 @@ tester_run_clone_ctx(const tester_ctx *ctx, bool new_group)
 #endif
     }
 
-    rc = test_requirements_clone(&ctx->reqs, &new_ctx->reqs);
-    if (rc != 0)
-    {
-        ERROR("%s(): failed to clone requirements: %r", __FUNCTION__, rc);
-        tester_run_free_ctx(new_ctx);
-        return NULL;
-    }
+    test_requirements_clone(&ctx->reqs, &new_ctx->reqs);
 
 #if WITH_TRC
     new_ctx->trc_walker = ctx->trc_walker;
@@ -910,15 +900,12 @@ tester_run_destroy_ctx(tester_run_data *data)
  *
  * @param data          Global Tester context data
  *
- * @return Allocated context.
+ * @return Allocated context (cannot return @c NULL).
  */
 static tester_ctx *
 tester_run_first_ctx(tester_run_data *data)
 {
     tester_ctx *new_ctx = tester_run_new_ctx(data->flags, data->targets);
-
-    if (new_ctx == NULL)
-        return NULL;
 
     if (!(data->flags & (TESTER_PRERUN | TESTER_ASSEMBLE_PLAN)))
         new_ctx->group_result.id = tester_get_id();
@@ -953,7 +940,7 @@ tester_run_first_ctx(tester_run_data *data)
  * @param data          Global Tester context data
  * @param new_group     Clone context for a new group
  *
- * @return Allocated context.
+ * @return Allocated context (cannot return @c NULL).
  */
 static tester_ctx *
 tester_run_more_ctx(tester_run_data *data, bool new_group)
@@ -962,12 +949,6 @@ tester_run_more_ctx(tester_run_data *data, bool new_group)
 
     assert(!SLIST_EMPTY(&data->ctxs));
     new_ctx = tester_run_clone_ctx(SLIST_FIRST(&data->ctxs), new_group);
-    if (new_ctx == NULL)
-    {
-        SLIST_FIRST(&data->ctxs)->current_result.status =
-            TESTER_TEST_ERROR;
-        return NULL;
-    }
 
     SLIST_INSERT_HEAD(&data->ctxs, new_ctx, links);
 
@@ -1134,18 +1115,12 @@ static char *
 persons_info_to_string(const persons_info *persons)
 {
     size_t              total = TESTER_STR_BULK;
-    char               *res = malloc(total);
+    char               *res = TE_ALLOC(total);
     char               *s = res;
     size_t              rest = total;
     const person_info  *p;
     int                 printed;
     bool again;
-
-    if (res == NULL)
-    {
-        ERROR("%s(): Memory allocation failure", __FUNCTION__);
-        return NULL;
-    }
 
     s[0] = '\0';
     TAILQ_FOREACH(p, persons, links)
@@ -1453,7 +1428,7 @@ test_reqs_to_json(const test_requirements *sticky_reqs,
  *
  * @param param   Parameter value to normalize
  *
- * @return Allocated string or NULL.
+ * @return Allocated string (cannot return @c NULL).
  */
 char *
 test_params_normalise(const char *param)
@@ -1466,9 +1441,7 @@ test_params_normalise(const char *param)
     if (param == NULL)
         return NULL;
 
-    str = (char *)calloc(1, strlen(param) + 1);
-    if (str == NULL)
-        return NULL;
+    str = TE_ALLOC(strlen(param) + 1);
 
     for (p = param, q = str; *p != '\0'; p++)
     {
@@ -1502,7 +1475,7 @@ test_params_normalise(const char *param)
  * @param args   Array of test parameters
  * @param n_args Number of of test parameters
  *
- * @return Allocated string or NULL.
+ * @return Allocated string (cannot return @c NULL).
  */
 char *
 test_params_hash(test_iter_arg *args, unsigned int n_args)
@@ -1514,16 +1487,11 @@ test_params_hash(test_iter_arg *args, unsigned int n_args)
     unsigned int  j;
     unsigned int  k;
     unsigned char digest[EVP_MAX_MD_SIZE];
-    char *hash_str = calloc(1, sizeof(digest) * 2 + 1);
-    int  *sorted = calloc(n_args, sizeof(int));
+    char *hash_str = TE_ALLOC(sizeof(digest) * 2 + 1);
+    int  *sorted = TE_ALLOC(n_args * sizeof(int));
     char  buf[8192] = {0, };
     int   len = 0;
 
-    if (sorted == NULL)
-    {
-        free(hash_str);
-        return NULL;
-    }
     for (k = 0; k < n_args; k++)
         sorted[k] = k;
 
@@ -1551,14 +1519,6 @@ test_params_hash(test_iter_arg *args, unsigned int n_args)
     {
         const char *name = args[sorted[i]].name;
         char *value = test_params_normalise(args[sorted[i]].value);
-
-        if (value == NULL)
-        {
-            free(sorted);
-            free(hash_str);
-            EVP_MD_CTX_free(md5);
-            return NULL;
-        }
 
         VERB("%s %s", name, value);
         len += snprintf(buf + len, sizeof(buf) - len,
@@ -1721,19 +1681,16 @@ log_test_start(unsigned int flags,
             }
 
             hash_str = test_params_hash(ctx->args, ri->n_args);
-            if (hash_str != NULL)
+            tmp = json_string(hash_str);
+            free(hash_str);
+            if (tmp == NULL)
             {
-                tmp = json_string(hash_str);
-                free(hash_str);
-                if (tmp == NULL)
-                {
-                    ERROR("%s: json_string failed for hash_str",
-                          __FUNCTION__);
-                    json_decref(result);
-                    return;
-                }
-                SET_NEW_JSON(result, "hash", tmp);
+                ERROR("%s: json_string failed for hash_str",
+                      __FUNCTION__);
+                json_decref(result);
+                return;
             }
+            SET_NEW_JSON(result, "hash", tmp);
 
 
             if (flags & TESTER_CFG_WALK_OUTPUT_PARAMS)
@@ -2716,8 +2673,6 @@ run_cfg_start(tester_cfg *cfg, unsigned int cfg_id_off, void *opaque)
 
     /* Clone Tester context */
     ctx = tester_run_more_ctx(gctx, false);
-    if (ctx == NULL)
-        return TESTER_CFG_WALK_FAULT;
 
     if (cfg->targets != NULL)
     {
@@ -2852,12 +2807,7 @@ run_item_start(run_item *ri, unsigned int cfg_id_off, unsigned int flags,
     {
         unsigned int    i;
 
-        ctx->args = calloc(ri->n_args, sizeof(*ctx->args));
-        if (ctx->args == NULL)
-        {
-            ctx->current_result.status = TESTER_TEST_ERROR;
-            return TESTER_CFG_WALK_FAULT;
-        }
+        ctx->args = TE_ALLOC(ri->n_args * sizeof(*ctx->args));
         ctx->n_args = ri->n_args;
         for (i = 0; i < ctx->n_args; ++i)
             TAILQ_INIT(&ctx->args[i].reqs);
@@ -2920,7 +2870,6 @@ run_pkg_start(run_item *ri, test_package *pkg,
 {
     tester_run_data    *gctx = opaque;
     tester_ctx         *ctx;
-    te_errno            rc;
 
     UNUSED(ri);
 
@@ -2928,19 +2877,10 @@ run_pkg_start(run_item *ri, test_package *pkg,
     LOG_WALK_ENTRY(cfg_id_off, gctx);
 
     ctx = tester_run_more_ctx(gctx, true);
-    if (ctx == NULL)
-        return TESTER_CFG_WALK_FAULT;
 
     assert(~ctx->flags & TESTER_INLOGUE);
 
-    rc = tester_get_sticky_reqs(&ctx->reqs, &pkg->reqs);
-    if (rc != 0)
-    {
-        ERROR("%s(): tester_get_sticky_reqs() failed: %r",
-              __FUNCTION__, rc);
-        ctx->current_result.status = rc;
-        return TESTER_CFG_WALK_FAULT;
-    }
+    tester_get_sticky_reqs(&ctx->reqs, &pkg->reqs);
 
     EXIT("CONT");
     return TESTER_CFG_WALK_CONT;
@@ -2952,7 +2892,6 @@ run_session_start(run_item *ri, test_session *session,
 {
     tester_run_data    *gctx = opaque;
     tester_ctx         *ctx;
-    te_errno            rc;
 
     UNUSED(ri);
     UNUSED(session);
@@ -2968,20 +2907,9 @@ run_session_start(run_item *ri, test_session *session,
      * created in run_pkg_start() routine.
      */
     if (ri->type == RUN_ITEM_SESSION)
-    {
         ctx = tester_run_more_ctx(gctx, true);
-        if (ctx == NULL)
-            return TESTER_CFG_WALK_FAULT;
-    }
 
-    rc = tester_get_sticky_reqs(&ctx->reqs, &session->reqs);
-    if (rc != 0)
-    {
-        ERROR("%s(): tester_get_sticky_reqs() failed: %r",
-              __FUNCTION__, rc);
-        ctx->current_result.status = rc;
-        return TESTER_CFG_WALK_FAULT;
-    }
+    tester_get_sticky_reqs(&ctx->reqs, &session->reqs);
 
 #if WITH_TRC
     if (~ctx->flags & TESTER_NO_TRC)
@@ -3056,8 +2984,6 @@ run_prologue_start(run_item *ri, unsigned int cfg_id_off, void *opaque)
     }
 
     ctx = tester_run_more_ctx(gctx, false);
-    if (ctx == NULL)
-        return TESTER_CFG_WALK_FAULT;
 
     VERB("Running test session prologue...");
     ctx->flags |= TESTER_INLOGUE;
@@ -3194,8 +3120,6 @@ run_epilogue_start(run_item *ri, unsigned int cfg_id_off, void *opaque)
     }
 
     ctx = tester_run_more_ctx(gctx, false);
-    if (ctx == NULL)
-        return TESTER_CFG_WALK_FAULT;
 
     VERB("Running test session epilogue...");
     ctx->flags |= TESTER_INLOGUE;
@@ -3263,14 +3187,8 @@ run_keepalive_start(run_item *ri, unsigned int cfg_id_off, void *opaque)
     }
 
     if (ctx->keepalive_ctx == NULL)
-    {
         ctx->keepalive_ctx = tester_run_clone_ctx(ctx, false);
-        if (ctx->keepalive_ctx == NULL)
-        {
-            ctx->current_result.status = TESTER_TEST_ERROR;
-            return TESTER_CFG_WALK_FAULT;
-        }
-    }
+
     ctx = ctx->keepalive_ctx;
 
     if ((~ctx->flags & TESTER_ASSEMBLE_PLAN) &&
@@ -3373,8 +3291,6 @@ run_exception_start(run_item *ri, unsigned int cfg_id_off, void *opaque)
 
     /* Exception handler is always run in a new context */
     ctx = tester_run_more_ctx(gctx, false);
-    if (ctx == NULL)
-        return TESTER_CFG_WALK_FAULT;
 
     /* Create configuration backup in a new context */
     if (run_create_cfg_backup(ctx, test_get_attrs(ri)->track_conf) != 0)
@@ -3486,8 +3402,7 @@ run_get_value(const test_entity_value *value,
 
         if (i < n_args)
         {
-            if (test_requirements_clone(&args[i].reqs, reqs) != 0)
-                return NULL;
+            test_requirements_clone(&args[i].reqs, reqs);
 
             return args[i].value;
         }
@@ -3511,7 +3426,7 @@ run_get_value(const test_entity_value *value,
  * argument value.
  *
  * The function complies with test_entity_value_enum_error_cb()
- * prototype.
+ * prototype. Always returns @c 0.
  */
 static te_errno
 run_prepare_arg_value_collect_reqs(const test_entity_value *value,
@@ -3520,9 +3435,8 @@ run_prepare_arg_value_collect_reqs(const test_entity_value *value,
     test_requirements *reqs = opaque;
 
     if (TE_RC_GET_ERROR(status) == TE_EEXIST)
-    {
-        return test_requirements_clone(&value->reqs, reqs);
-    }
+        test_requirements_clone(&value->reqs, reqs);
+
     return 0;
 }
 
@@ -3601,9 +3515,7 @@ run_prepare_arg_cb(const test_var_arg *va, void *opaque)
 
         if (ri_list != NULL)
         {
-            iter_list = malloc(sizeof(*iter_list));
-            if (iter_list == NULL)
-                return TE_RC(TE_TESTER, TE_ENOMEM);
+            iter_list = TE_ALLOC(sizeof(*iter_list));
 
             iter_list->name = ri_list->name;
             iter_list->index = i_value;
@@ -3864,12 +3776,11 @@ run_repeat_start(run_item *ri, unsigned int cfg_id_off, unsigned int flags,
     {
         hash_str = test_params_hash(ctx->args, ri->n_args);
 
-        if (hash_str == NULL || strcmp(hash_str, gctx->act->hash) != 0)
+        if (strcmp(hash_str, gctx->act->hash) != 0)
         {
             ctx->current_result.status = TESTER_TEST_INCOMPLETE;
             ctx->group_step = true;
-            if (hash_str != NULL)
-                free(hash_str);
+            free(hash_str);
             return TESTER_CFG_WALK_SKIP;
         }
         free(hash_str);

engine/configurator/conf_dh.c
@@ -389,12 +389,7 @@ cfg_dh_process_copy(xmlNodePtr node, te_kvpair_h *expand_vars)
         oid_len = strlen(oid);
 
         len = sizeof(cfg_copy_msg) + oid_len + 1;
-        if ((msg = calloc(1, len)) == NULL)
-        {
-            ERROR("Cannot allocate memory");
-            rc = TE_ENOMEM;
-            goto cleanup;
-        }
+        msg = TE_ALLOC(len);
 
         memcpy(msg->dst_oid, oid, oid_len + 1);
         msg->is_obj = (strchr(oid, ':') == NULL) ? true : false;
@@ -482,8 +477,8 @@ cfg_dh_process_add(xmlNodePtr node, const te_kvpair_h *expand_vars)
 
         len = sizeof(cfg_add_msg) + CFG_MAX_INST_VALUE +
               strlen((char *)oid) + 1;
-        if ((msg = (cfg_add_msg *)calloc(1, len)) == NULL)
-            RETERR(TE_ENOMEM, "Cannot allocate memory");
+
+        msg = TE_ALLOC(len);
 
         msg->type = CFG_ADD;
         msg->len = sizeof(cfg_add_msg);
@@ -594,10 +589,7 @@ cfg_dh_process_file(xmlNodePtr node, te_kvpair_h *expand_vars,
                                            (const xmlChar *)"ta")) == NULL)
                 RETERR(TE_EINVAL, "Incorrect reboot command format");
 
-            if ((msg = (cfg_reboot_msg *)calloc(1, sizeof(*msg) +
-                                                strlen(attr) + 1))
-                    == NULL)
-                RETERR(TE_ENOMEM, "Cannot allocate memory");
+            msg = TE_ALLOC(sizeof(*msg) + strlen(attr) + 1);
 
             msg->type = CFG_REBOOT;
             msg->len = sizeof(*msg) + strlen(attr) + 1;
@@ -665,8 +657,7 @@ cfg_dh_process_file(xmlNodePtr node, te_kvpair_h *expand_vars,
                 len = sizeof(cfg_register_msg) + strlen((char *)oid) + 1 +
                       (val_s == NULL ? 0 : strlen((char *)val_s) + 1);
 
-                if ((msg = (cfg_register_msg *)calloc(1, len)) == NULL)
-                    RETERR(TE_ENOMEM, "Cannot allocate memory");
+                msg = TE_ALLOC(len);
 
                 msg->type = CFG_REGISTER;
                 msg->len = len;
@@ -880,8 +871,7 @@ cfg_dh_process_file(xmlNodePtr node, te_kvpair_h *expand_vars,
                     return rc;
 
                 len = sizeof(cfg_set_msg) + CFG_MAX_INST_VALUE;
-                if ((msg = (cfg_set_msg *)calloc(1, len)) == NULL)
-                    RETERR(TE_ENOMEM, "Cannot allocate memory");
+                msg = TE_ALLOC(len);
 
                 msg->handle = handle;
                 msg->type = CFG_SET;
@@ -941,8 +931,7 @@ cfg_dh_process_file(xmlNodePtr node, te_kvpair_h *expand_vars,
                 if (!CFG_IS_INST(handle))
                     RETERR(TE_EINVAL, "OID %s is not instance", oid);
 
-                if ((msg = (cfg_del_msg *)calloc(1, sizeof(*msg))) == NULL)
-                    RETERR(TE_ENOMEM, "Cannot allocate memory");
+                msg = TE_ALLOC(sizeof(*msg));
 
                 msg->type = CFG_DEL;
                 msg->len = sizeof(*msg);
@@ -1124,8 +1113,7 @@ cfg_dh_attach_backup(char *filename)
 {
     cfg_backup *tmp;
 
-    if ((tmp = (cfg_backup *)malloc(sizeof(*tmp))) == NULL)
-        return TE_ENOMEM;
+    tmp = TE_ALLOC(sizeof(*tmp));
 
     if ((tmp->filename = strdup(filename)) == NULL)
     {
@@ -1322,15 +1310,8 @@ cfg_dh_restore_backup_ext(char *filename, bool hard_check, bool shutdown)
 
             case CFG_SET:
             {
-                cfg_set_msg *msg =
-                    (cfg_set_msg *)calloc(sizeof(cfg_set_msg) +
-                                          CFG_MAX_INST_VALUE, 1);
-
-                if (msg == NULL)
-                {
-                    ERROR("calloc() failed");
-                    return TE_ENOMEM;
-                }
+                cfg_set_msg *msg = TE_ALLOC(sizeof(cfg_set_msg) +
+                                            CFG_MAX_INST_VALUE);
 
                 if ((rc = cfg_db_find(tmp->old_oid, &msg->handle)) != 0)
                 {
@@ -1378,16 +1359,9 @@ cfg_dh_restore_backup_ext(char *filename, bool hard_check, bool shutdown)
 
             case CFG_DEL:
             {
-                cfg_add_msg *msg = (cfg_add_msg *)
-                                       calloc(sizeof(cfg_add_msg) +
-                                              CFG_MAX_INST_VALUE +
-                                              strlen(tmp->old_oid) + 1, 1);
-
-                if (msg == NULL)
-                {
-                    ERROR("calloc() failed");
-                    return TE_ENOMEM;
-                }
+                cfg_add_msg *msg = TE_ALLOC(sizeof(cfg_add_msg) +
+                                            CFG_MAX_INST_VALUE +
+                                            strlen(tmp->old_oid) + 1);
 
                 msg->type = CFG_ADD;
                 msg->len = sizeof(*msg);
@@ -1487,20 +1461,9 @@ cfg_dh_restore_backup_on_shutdown()
 int
 cfg_dh_push_command(cfg_msg *msg, bool local, const cfg_inst_val *old_val)
 {
-    cfg_dh_entry *entry = (cfg_dh_entry *)calloc(sizeof(cfg_dh_entry), 1);
+    cfg_dh_entry *entry = TE_ALLOC(sizeof(cfg_dh_entry));
 
-    if (entry == NULL)
-    {
-        ERROR("Memory allocation failure");
-        return TE_ENOMEM;
-    }
-
-    if ((entry->cmd = (cfg_msg *)calloc(msg->len, 1)) == NULL)
-    {
-        ERROR("Memory allocation failure");
-        free(entry);
-        return TE_ENOMEM;
-    }
+    entry->cmd = TE_ALLOC(msg->len);
 
     /* Local commands are not comitted yet, and vice versa */
     entry->committed = !local;

engine/rcf/rcf.c
@@ -58,6 +58,7 @@
 
 #include <dlfcn.h>
 
+#include "te_alloc.h"
 #include "te_stdint.h"
 #include "te_printf.h"
 #include "te_str.h"
@@ -278,11 +279,7 @@ parse_config_ta(xmlNodePtr ta_node)
     if (attribute_contains_yes(ta_node, "disabled"))
         return 0;
 
-    if ((agent = (ta *)calloc(1, sizeof(ta))) == NULL)
-    {
-        ERROR("calloc failed: %d", errno);
-        return TE_RC(TE_RCF, TE_ENOMEM);
-    }
+    agent = TE_ALLOC(sizeof(ta));
 
     agent->next = agents;
     agents = agent;
@@ -543,12 +540,7 @@ parse_config_ta(xmlNodePtr ta_node)
             free(condition);
         }
 
-        ta_task = malloc(sizeof(*ta_task));
-        if (ta_task == NULL)
-        {
-            ERROR("malloc failed: %d", errno);
-            return TE_RC(TE_RCF, TE_EFAIL);
-        }
+        ta_task = TE_ALLOC(sizeof(*ta_task));
         ta_task->mode =
             (xmlStrcmp(task->name , (const xmlChar *)"thread") == 0 ?
              RCF_THREAD :
@@ -564,7 +556,7 @@ parse_config_ta(xmlNodePtr ta_node)
         ta_task->entry = attr;
 
         ta_task->argc = 0;
-        ta_task->argv = malloc(sizeof(*ta_task->argv) * RCF_MAX_PARAMS);
+        ta_task->argv = TE_ALLOC(sizeof(*ta_task->argv) * RCF_MAX_PARAMS);
         for (arg = task->xmlChildrenNode; arg != NULL; arg = arg->next)
         {
             if (xmlStrcmp(arg->name, (const xmlChar *)"arg") != 0)
@@ -1467,13 +1459,7 @@ process_reply(ta *agent)
 
                 if (balen < 4096)
                 {
-                    new_msg = (rcf_msg *)calloc(1, sizeof(rcf_msg) + balen);
-                    if (new_msg == NULL)
-                    {
-                        msg->error = TE_RC(TE_RCF, TE_ENOMEM);
-                        rcf_answer_user_request(req);
-                        return;
-                    }
+                    new_msg = TE_ALLOC(sizeof(rcf_msg) + balen);
                     memset(new_msg, 0, sizeof(rcf_msg) + balen);
                     *new_msg = *msg;
                     free(msg);
@@ -1560,14 +1546,8 @@ process_reply(ta *agent)
 
                 if (msg->intparm < (int)n && n > INSIDE_LEN)
                 {
-                    rcf_msg *new_msg =
-                        (rcf_msg *)malloc(n + sizeof(*msg) - INSIDE_LEN);
+                    rcf_msg *new_msg = TE_ALLOC(n + sizeof(*msg) - INSIDE_LEN);
 
-                    if (new_msg == NULL)
-                    {
-                        msg->error = TE_RC(TE_RCF, TE_EINVAL);
-                        break;
-                    }
                     *new_msg = *msg;
                     free(msg);
                     msg = req->message = new_msg;
@@ -2172,14 +2152,8 @@ rcf_alloc_usrreq(void)
 {
     usrreq *req;
 
-    if ((req = (usrreq *)calloc(1, sizeof(usrreq))) == NULL)
-        return NULL;
-
-    if ((req->message = (rcf_msg *)calloc(1, sizeof(rcf_msg))) == NULL)
-    {
-        free(req);
-        return NULL;
-    }
+    req = TE_ALLOC(sizeof(usrreq));
+    req->message = TE_ALLOC(sizeof(rcf_msg));
 
     return req;
 }
@@ -2272,7 +2246,6 @@ rcf_ta_check_start(void)
 {
     ta             *agent;
     usrreq         *req;
-    int             rc = 0;
     const char     *target = NULL;
 
     assert(ta_checker.req != NULL);
@@ -2292,15 +2265,6 @@ rcf_ta_check_start(void)
             continue;
 
         req = rcf_alloc_usrreq();
-        if (req == NULL)
-        {
-            rc = TE_RC(TE_RCF, TE_ENOMEM);
-            ERROR("Unable to allocate memory "
-                  "to check TA '%s' state, error=%r",
-                  agent->name, rc);
-            break;
-        }
-
         req->user = NULL;
         req->timeout = RCF_SHUTDOWN_TIMEOUT;
 
@@ -2406,13 +2370,7 @@ process_user_request(usrreq *req)
         {
             rcf_msg *new_msg;
 
-            new_msg = (rcf_msg *)calloc(1, sizeof(rcf_msg) + names_len);
-            if (new_msg == NULL)
-            {
-                msg->error = TE_RC(TE_RCF, TE_ENOMEM);
-                rcf_answer_user_request(req);
-                return;
-            }
+            new_msg = TE_ALLOC(sizeof(rcf_msg) + names_len);
             *new_msg = *msg;
             free(msg);
             msg = req->message = new_msg;
@@ -2444,13 +2402,7 @@ process_user_request(usrreq *req)
 
                 rcf_msg *new_msg;
 
-                if ((new_msg = (rcf_msg *)calloc(1, sizeof(rcf_msg) +
-                                                    names_len)) == NULL)
-                {
-                    msg->error = TE_RC(TE_RCF, TE_ENOMEM);
-                    break; /** Leave 'do/while' block */
-                }
-
+                new_msg = TE_ALLOC(sizeof(rcf_msg) + names_len);
                 *new_msg = *msg;
                 free(msg);
                 msg = req->message = new_msg;
@@ -2479,12 +2431,7 @@ process_user_request(usrreq *req)
                     break; /** Leave 'do/while' block */
                 }
 
-                if ((agent = (ta *)calloc(1, sizeof(ta))) == NULL)
-                {
-                    msg->error = TE_RC(TE_RCF, TE_ENOMEM);
-                    break; /** Leave 'do/while' block */
-                }
-
+                agent = TE_ALLOC(sizeof(ta));
                 agent->dynamic = true;
                 agent->next = agents;
                 agent->flags = msg->flags | TA_DEAD;
@@ -2553,15 +2500,7 @@ process_user_request(usrreq *req)
 
                 rcf_msg *new_msg;
 
-                new_msg = (rcf_msg *)calloc(1, sizeof(rcf_msg) + names_len);
-
-                if (new_msg == NULL)
-                {
-                    msg->error = TE_RC(TE_RCF, TE_ENOMEM);
-                    rcf_answer_user_request(req);
-                    return;
-                }
-
+                new_msg = TE_ALLOC(sizeof(rcf_msg) + names_len);
                 *new_msg = *msg;
                 free(msg);
                 msg = req->message = new_msg;
@@ -3064,8 +3003,7 @@ main(int argc, const char *argv[])
         {
             len = sizeof(rcf_msg);
 
-            if ((req = rcf_alloc_usrreq()) == NULL)
-                goto exit;
+            req = rcf_alloc_usrreq();
 
             rc = ipc_receive_message(server, req->message,
                                      &len, &(req->user));

engine/rcf/rcf.h
@@ -324,6 +324,8 @@ extern int rcf_send_cmd(ta *agent, usrreq *req);
 
 /**
  * Allocate memory for user request.
+ *
+ * @note Cannot return @c NULL.
  */
 extern usrreq *rcf_alloc_usrreq(void);
 

engine/tester/config_prepare.c
@@ -29,6 +29,7 @@
 #include <assert.h>
 #endif
 
+#include "te_alloc.h"
 #include "te_defs.h"
 #include "te_errno.h"
 #include "te_queue.h"
@@ -142,12 +143,7 @@ config_prepare_new_ctx(config_prepare_data *gctx)
     config_prepare_ctx *cur_ctx = SLIST_FIRST(&gctx->ctxs);
     config_prepare_ctx *new_ctx;
 
-    new_ctx = calloc(1, sizeof(*new_ctx));
-    if (new_ctx == NULL)
-    {
-        gctx->rc = TE_RC(TE_TESTER, TE_ENOMEM);
-        return NULL;
-    }
+    new_ctx = TE_ALLOC(sizeof(*new_ctx));
 
     if (cur_ctx != NULL)
     {
@@ -371,9 +367,7 @@ prepare_arg_cb(const test_var_arg *va, void *opaque)
         }
         else
         {
-            p = malloc(sizeof(*p));
-            if (p == NULL)
-                return TE_RC(TE_TESTER, TE_ENOMEM);
+            p = TE_ALLOC(sizeof(*p));
 
             p->name = va->list;
             p->len = n_values;

engine/tester/reqs.c
@@ -27,6 +27,8 @@
 
 #include "logger_api.h"
 
+#include "te_alloc.h"
+
 #include "tester_conf.h"
 #include "tester_reqs.h"
 #include "tester_run.h"
@@ -64,15 +66,7 @@ tester_new_target_reqs(logic_expr **targets, const char *req)
     }
     else
     {
-        parent = calloc(1, sizeof(*parent));
-        if (parent == NULL)
-        {
-            rc = TE_OS_RC(TE_TESTER, errno);;
-            logic_expr_free(parsed);
-            ERROR("%s(): calloc(1, %u) failed",
-                  __FUNCTION__, sizeof(*parent));
-            return rc;
-        }
+        parent = TE_ALLOC(sizeof(*parent));
 
         parent->type = LOGIC_EXPR_AND;
         parent->u.binary.lhv = *targets;
@@ -89,25 +83,17 @@ tester_new_target_reqs(logic_expr **targets, const char *req)
  *
  * @param req             Requirement to be cloned
  *
- * @return Pointer to allocated requirement clone or NULL.
+ * @return Pointer to allocated requirement clone (cannot return @c NULL).
  */
 static test_requirement *
 test_requirement_clone(const test_requirement *req)
 {
-    test_requirement *p = calloc(1, sizeof(*p));
+    test_requirement *p = TE_ALLOC(sizeof(*p));
 
     assert(req->ref == NULL);
-    if (p != NULL)
-    {
-        p->id = strdup(req->id);
-        if (p->id != NULL)
-        {
-            return p;
-        }
-        free(p);
-    }
-    ERROR("%s(): Memory allocation failed", __FUNCTION__);
-    return NULL;
+    p->id = TE_STRDUP(req->id);
+
+    return p;
 }
 
 /**
@@ -125,7 +111,7 @@ test_requirement_free(test_requirement *req)
 
 
 /* See description in tester_reqs.h */
-te_errno
+void
 test_requirements_clone(const test_requirements *reqs,
                         test_requirements *new_reqs)
 {
@@ -135,14 +121,8 @@ test_requirements_clone(const test_requirements *reqs,
     TAILQ_FOREACH(p, reqs, links)
     {
         q = test_requirement_clone(p);
-        if (q == NULL)
-        {
-            ERROR("%s(): test_requirement_clone() failed", __FUNCTION__);
-            return TE_RC(TE_TESTER, TE_ENOMEM);
-        }
         TAILQ_INSERT_TAIL(new_reqs, q, links);
     }
-    return 0;
 }
 
 /* See description in tester_reqs.h */
@@ -602,8 +582,6 @@ tester_get_sticky_reqs(test_requirements       *sticky_reqs,
         if (p->sticky)
         {
             q = test_requirement_clone(p);
-            if (q == NULL)
-                return TE_RC(TE_TESTER, TE_ENOMEM);
             TAILQ_INSERT_TAIL(sticky_reqs, q, links);
         }
     }

commit 2aff40910741413fa492871c9d46a216613c78ed

engine/tester: avoid unnecessary memory allocation
Avoid memory allocation for args sorting when n_args is 0.
TE_ALLOC(0) gives WARN("Attempted to allocate a zero buffer at ...")

Fixes: d56bcce39d3f ("engine: replace malloc/calloc with TE allocators")
Signed-off-by: Michael Kochkin <michael.kochkin@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

engine/tester/run.c
@@ -1701,7 +1701,7 @@ test_params_hash(test_iter_arg *args, unsigned int n_args)
     unsigned int  k;
     unsigned char digest[EVP_MAX_MD_SIZE];
     char *hash_str = TE_ALLOC(sizeof(digest) * 2 + 1);
-    int  *sorted = TE_ALLOC(n_args * sizeof(int));
+    int  *sorted = (n_args > 0) ? TE_ALLOC(n_args * sizeof(int)) : NULL;
     char  buf[8192] = {0, };
     int   len = 0;
 