oktetlabs/test-environment

commit 9c12389d1db0c767526d927201dff8ca8c148e95

lib/ta_job: avoid build-time log level filtering
Messages generated by job output filters should be always included
in logs regardless its level.

Signed-off-by: Yurij Plotnikov <yurij.plotnikov@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

lib/ta_job/ta_job.c
@@ -979,13 +979,14 @@ match_callback(ta_job_manager_t *manager, filter_t *filter,
          * instead of the next one.
          */
 #if 0
-        LGR_MESSAGE(filter->log_level, log_user, "%.*s", log_size, buf);
+        TE_LOG(filter->log_level, TE_LGR_ENTITY, log_user, "%.*s", log_size,
+               buf);
 #else
         te_string message = TE_STRING_INIT;
 
         te_string_append(&message, "%.*s", log_size, buf);
 
-        LGR_MESSAGE(filter->log_level, log_user, "%s", message.ptr);
+        TE_LOG(filter->log_level, TE_LGR_ENTITY, log_user, "%s", message.ptr);
 
         te_string_free(&message);
 #endif

commit f1c5f252cb969e912e5c0e45e25a2d564c1da6d3

lib/ta_job: don't log message with zero log level
Fixes: 9c12389d1db0 ("lib/ta_job: avoid build-time log level filtering")
Signed-off-by: Yurij Plotnikov <yurij.plotnikov@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

lib/ta_job/ta_job.c
@@ -961,7 +961,7 @@ match_callback(ta_job_manager_t *manager, filter_t *filter,
     int log_size = size > (size_t)INT_MAX ? INT_MAX : (int)size;
     te_errno rc;
 
-    if (!eos)
+    if (!eos && filter->log_level != 0)
     {
         char log_user[MAX_LOG_USER_SIZE];
 