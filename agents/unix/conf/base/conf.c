oktetlabs/test-environment

commit ca8d3ab27e47a056d5d43863f7506224828944a4

common: fix const-qualifier string search warnings
On Ubuntu 26.04, glibc strchr/strrchr/strstr preserve const qualifier
of the input pointer, so assignments to char * trigger
-Wdiscarded-qualifiers warnings. Switch such pointers to const char *
where strings are only inspected, and update other ones to parse a
writable copy instead of modifying input data.

Signed-off-by: Denis Pryazhennikov <denis.pryazhennikov@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
Reviewed-by: Artem Andreev <artem.andreev@oktetlabs.ru>

agents/unix/conf/base/conf.c
@@ -182,6 +182,7 @@ typedef struct pam_message const pam_message_t;
 #include "conf_common.h"
 #include "te_shell_cmd.h"
 #include "te_string.h"
+#include "te_alloc.h"
 
 #include "conf_daemons.h"
 
@@ -1258,8 +1259,8 @@ static te_errno
 interface_grab(const char *name)
 {
 #ifndef DISABLE_NETWORKMANAGER_CHECK
-    te_errno  rc;
-    char     *ifname;
+    const char *ifname;
+    te_errno rc;
 
     /* Get resource instance name */
     ifname = strrchr(name, ':');
@@ -3176,7 +3177,7 @@ switchdev_name_get(unsigned int gid, const char *oid, char *value,
     if (id == NULL || *id == '\0')
         return TE_RC(TE_TA_UNIX, TE_EINVAL);
 
-    char *sep = strchr(id, ':');
+    const char *sep = strchr(id, ':');
     if (sep == NULL)
         return TE_RC(TE_TA_UNIX, TE_EINVAL);
 
@@ -5781,7 +5782,8 @@ broadcast_set(unsigned int gid, const char *oid, const char *value,
 int
 link_addr_a2n(uint8_t *lladdr, int len, const char *str)
 {
-    const char *arg = str;
+    char       *buf = TE_STRDUP(str);
+    char       *arg = buf;
     int         i;
 
     for (i = 0; i < len; i++)
@@ -5796,18 +5798,15 @@ link_addr_a2n(uint8_t *lladdr, int len, const char *str)
         {
             ERROR("%s: \"%s\" is invalid lladdr",
                   __FUNCTION__, arg);
-            if (cp != NULL)
-                *cp = ':';
+            free(buf);
             return -1;
         }
-
-        if (cp != NULL)
-            *cp = ':';
 
         if (temp > 255)
         {
             ERROR("%s:\"%s\" is invalid lladdr",
                   __FUNCTION__, arg);
+            free(buf);
             return -1;
         }
 
@@ -5816,8 +5815,10 @@ link_addr_a2n(uint8_t *lladdr, int len, const char *str)
         if (cp == NULL)
             break;
 
-        arg = ++cp;
+        arg = cp + 1;
     }
+
+    free(buf);
     return i + 1;
 }
 

agents/unix/conf/base/conf_cpu.c
@@ -871,7 +871,7 @@ add_index_name(const char *pattern, const char *pathname, void *data)
     cache_location *location = data;
     cpu_item_list *root = location->root;
     const char *name = location->name;
-    char *index_name = strrchr(pathname, '/') + 1;
+    const char *index_name = strrchr(pathname, '/') + 1;
     cache_item *cache = NULL;
     te_errno rc = 0;
 

agents/unix/conf/base/conf_iptables.c
@@ -813,7 +813,7 @@ iptables_rules_set(unsigned int  gid, const char *oid,
     FILE *fp = NULL;
     int   in_fd;
     char  buf[IPTABLES_CMD_BUF_SIZE];
-    char *p = NULL;
+    const char *p = NULL;
     pid_t pid;
     int   status;
 

agents/unix/conf/base/conf_ns_net.c
@@ -744,7 +744,7 @@ RCF_PCH_CFG_NODE_COLLECTION(node_netns, "net", &node_interface, NULL,
 static te_errno
 netns_rsrc_grab(const char *obj)
 {
-    char *ns_name = strrchr(obj, ':');
+    const char *ns_name = strrchr(obj, ':');
 
     if (ns_name == NULL)
     {
@@ -766,7 +766,7 @@ netns_rsrc_grab(const char *obj)
 static te_errno
 netns_rsrc_release(const char *obj)
 {
-    char *ns_name = strrchr(obj, ':');
+    const char *ns_name = strrchr(obj, ':');
 
     if (ns_name == NULL)
     {

agents/unix/conf/daemons/dhcp/dhcp_server.c
@@ -1353,8 +1353,8 @@ ds_dhcpserver_save_conf(void)
 static bool
 check_dhcpd_pid(const char *pid_filename)
 {
-    int     rc = 0;
-    char   *name;
+    const char *name;
+    int rc = 0;
 
     sprintf(buf, "cat %s 2>/dev/null 1>/dev/null", pid_filename);
 
@@ -1365,7 +1365,7 @@ check_dhcpd_pid(const char *pid_filename)
     }
 
     if ((name = strrchr(dhcp_server_exec, '/')) == NULL)
-        name = (char *)dhcp_server_exec;
+        name = dhcp_server_exec;
     else
         name++;
 

agents/unix/conf/daemons/l2tp/l2tp_server.c
@@ -2231,7 +2231,7 @@ l2tp_lns_range_add_routine(const char *lns_name, const char *option_name,
                            const char *range)
 {
     char                 buf_ip[INET_ADDRSTRLEN] = "/0";
-    char                *hyphen;
+    const char          *hyphen;
 
     te_l2tp_option      *l2tp_option;
     te_l2tp_ipv4_range  *l2tp_range;

agents/unix/conf/util/conf_sniffer.c
@@ -596,7 +596,8 @@ static te_errno
 sniffer_parse_sniff_id(const char* buf, sniffer_id *id)
 {
     unsigned     strl;
-    char        *ptr;
+    const char  *ptr;
+    char        *endptr;
 
 /**
  * Skip spaces in the command.
@@ -633,8 +634,8 @@ sniffer_parse_sniff_id(const char* buf, sniffer_id *id)
     buf = ptr;
 
     SNIF_SK_SPS(buf);
-    id->ssn = strtoll(buf, &ptr, 10);
-    if (buf == ptr)
+    id->ssn = strtoll(buf, &endptr, 10);
+    if (buf == endptr)
     {
         WARN("Wrong sniffer SSN in the sniffer id.");
         return TE_RC(TE_TA_UNIX, TE_EINVAL);

apps/ta_core_watcher/main.c
@@ -392,7 +392,7 @@ main(int argc, const char *argv[])
     const char *ta_dir = NULL;
     const char *core_dir = NULL;
 
-    char *p = NULL;
+    const char *p = NULL;
     char host_name[HOST_NAME_MAX];
     te_string out_str = TE_STRING_INIT;
     te_string log_str = TE_STRING_INIT;

commit 4ab4a196d5801c6eff5292a1cbcdbf5086e9aace

agents/unix: remove duplicate include
Fixes: ca8d3ab27e47 ("common: fix const-qualifier string search warnings")
Signed-off-by: Nikolai Kosovskii <nikolai.kosovskii@arknetworks.am>

agents/unix/conf/base/conf.c
@@ -182,7 +182,6 @@ typedef struct pam_message const pam_message_t;
 #include "conf_common.h"
 #include "te_shell_cmd.h"
 #include "te_string.h"
-#include "te_alloc.h"
 
 #include "conf_daemons.h"
 